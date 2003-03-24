897b9abf71c0df65cc9a4c9dffa72a0a38e455fd
David Hovemeyer <david.hovemeyer@gmail.com>
2003-03-24 21:12:13 +0000

    Added javadoc.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@22 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/FindBugsMessageFormat.java
+++ findbugs/src/java/edu/umd/cs/findbugs/FindBugsMessageFormat.java
@@ -1,12 +1,40 @@
 package edu.umd.cs.findbugs;
 
+/**
+ * Format the message for a BugInstance.
+ * This class works in much the same way as <code>java.text.MessageFormat</code>;
+ * however, each placeholder may have an optional "key" which specifies
+ * how the object at that position should be formatted.
+ *
+ * <p> Example:
+ * <pre>
+ *     new FindBugsMessageFormat("BUG: {1.shortMethod} does something bad to field {2}")
+ * </pre>
+ * In this example, the method annotation at position 1 is formatted using
+ * the string "shortMethod" as the key.  Hypothetically, this would indicate
+ * that we want the short form of the method.  The field annotation at position 2
+ * is formatted with the empty (default) key.
+ *
+ * @see BugInstance
+ * @author David Hovemeyer
+ */
 public class FindBugsMessageFormat {
 	private String pattern;
 
+	/**
+	 * Constructor.
+	 * @param pattern the pattern for the message
+	 */
 	public FindBugsMessageFormat(String pattern) {
 		this.pattern = pattern;
 	}
 
+	/**
+	 * Format the message using the given array of BugAnnotations as arguments
+	 * to bind to the placeholders in the pattern string.
+	 * @param args the BugAnnotations used as arguments
+	 * @return the formatted message
+	 */
 	public String format(BugAnnotation[] args) {
 		String pat = pattern;
 		StringBuffer result = new StringBuffer();
