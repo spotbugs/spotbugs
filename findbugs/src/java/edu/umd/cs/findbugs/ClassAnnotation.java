b149f8da07f54bca8760e2080fd8b116856bcb30
David Hovemeyer <david.hovemeyer@gmail.com>
2003-03-24 17:15:09 +0000

    Added superclassName attribute.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/ClassAnnotation.java
+++ findbugs/src/java/edu/umd/cs/findbugs/ClassAnnotation.java
@@ -2,15 +2,21 @@ package edu.umd.cs.findbugs;
 
 public class ClassAnnotation implements BugAnnotation {
 	private String className;
+	private String superclassName;
 	private String sourceFile;
 
-	public ClassAnnotation(String className, String sourceFile) {
+	public ClassAnnotation(String className, String superclassName, String sourceFile) {
 		this.className = className;
+		this.superclassName = superclassName;
 		this.sourceFile = sourceFile;
 	}
 
 	public String getClassName() {
 		return className;
+	}
+
+	public String getSuperclassName() {
+		return superclassName;
 	}
 
 	public String getSourceFile() {
