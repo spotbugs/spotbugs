39741ab9786a623f995db023bda008e2624de086
David Hovemeyer <david.hovemeyer@gmail.com>
2003-03-24 17:14:54 +0000

    More implementation.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/BugInstance.java
+++ findbugs/src/java/edu/umd/cs/findbugs/BugInstance.java
@@ -1,16 +1,20 @@
 package edu.umd.cs.findbugs;
 
 import java.util.*;
+import edu.umd.cs.pugh.visitclass.BetterVisitor;
 
 public class BugInstance {
 	private String type;
 	private int priority;
+	private int count;
 	private ArrayList<BugAnnotation> annotationList;
 	private ClassAnnotation primaryClassAnnotation;
+	private MethodAnnotation primaryMethodAnnotation;
 
 	public BugInstance(String type, int priority) {
 		this.type = type;
 		this.priority = priority;
+		this.count = count;
 		annotationList = new ArrayList<BugAnnotation>();
 		primaryClassAnnotation = null;
 	}
@@ -23,12 +27,28 @@ public class BugInstance {
 		return priority;
 	}
 
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public ClassAnnotation getPrimaryClass() {
+		return primaryClassAnnotation;
+	}
+
+	public MethodAnnotation getPrimaryMethod() {
+		return primaryMethodAnnotation;
+	}
+
 	public Iterator<BugAnnotation> annotationIterator() {
 		return annotationList.iterator();
 	}
 
-	public BugInstance addClass(String className, String sourceFile) {
-		ClassAnnotation classAnnotation = new ClassAnnotation(className, sourceFile);
+	public BugInstance addClass(String className, String superclassName, String sourceFile) {
+		ClassAnnotation classAnnotation = new ClassAnnotation(className, superclassName, sourceFile);
 		if (primaryClassAnnotation == null)
 			primaryClassAnnotation = classAnnotation;
 		add(classAnnotation);
@@ -40,8 +60,29 @@ public class BugInstance {
 		return this;
 	}
 
-	public ClassAnnotation getPrimaryClass() {
-		return primaryClassAnnotation;
+	public BugInstance addMethod(String className, String methodName, String methodSig) {
+		MethodAnnotation methodAnnotation = new MethodAnnotation(className, methodName, methodSig);
+		if (primaryMethodAnnotation == null)
+			primaryMethodAnnotation = methodAnnotation;
+		add(methodAnnotation);
+		return this;
+	}
+
+	public BugInstance addClassAndMethod(BetterVisitor betterVisitor) {
+		String className = betterVisitor.getBetterClassName();
+		String superclassName = betterVisitor.getSuperclassName();
+		String sourceFile = betterVisitor.getSourceFile();
+		String methodName = betterVisitor.getMethodName();
+		String methodSig = betterVisitor.getMethodSig();
+
+		addClass(className, superclassName, sourceFile);
+		addMethod(className, methodName, methodSig);
+		return this;
+	}
+
+	public String getMessage() {
+		// TODO:
+		return "";
 	}
 
 	private void add(BugAnnotation annotation) {
