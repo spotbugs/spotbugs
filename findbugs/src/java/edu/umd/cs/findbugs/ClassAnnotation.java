c472dd87d51e31ee5c0fa35d188f4613245a9f43
David Hovemeyer <david.hovemeyer@gmail.com>
2003-03-24 20:58:00 +0000

    Added javadoc.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@19 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/ClassAnnotation.java
+++ findbugs/src/java/edu/umd/cs/findbugs/ClassAnnotation.java
@@ -1,13 +1,28 @@
 package edu.umd.cs.findbugs;
 
+/**
+ * A BugAnnotation object specifying a Java class involved in the bug.
+ *
+ * @see BugAnnotation
+ * @see BugInstance
+ * @author David Hovemeyer
+ */
 public class ClassAnnotation extends PackageMemberAnnotation {
 	private String sourceFile;
 
+	/**
+	 * Constructor.
+	 * @param className the name of the class
+	 * @param sourceFile the name of the source file where the class is defined
+	 */
 	public ClassAnnotation(String className, String sourceFile) {
 		super(className);
 		this.sourceFile = sourceFile;
 	}
 
+	/**
+	 * Get the source file.
+	 */
 	public String getSourceFile() {
 		return sourceFile;
 	}
