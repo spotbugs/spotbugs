3b9df15c34ec73cfc433cc9ead0f6e42f16f5f9e
David Hovemeyer <david.hovemeyer@gmail.com>
2003-03-24 17:14:16 +0000

    Two new visit methods
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/BugAnnotationVisitor.java
+++ findbugs/src/java/edu/umd/cs/findbugs/BugAnnotationVisitor.java
@@ -1,8 +1,9 @@
 package edu.umd.cs.findbugs;
 
 public interface BugAnnotationVisitor {
-	public void visitFieldAnnotation(FieldAnnotation fieldAnnotation);
 	public void visitClassAnnotation(ClassAnnotation classAnnotation);
+	public void visitFieldAnnotation(FieldAnnotation fieldAnnotation);
+	public void visitMethodAnnotation(MethodAnnotation methodAnnotation);
 }
 
 // vim:ts=4
