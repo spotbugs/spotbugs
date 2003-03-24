1aa74cb71be1cfeceba7b7f962cc103ebc888181
David Hovemeyer <david.hovemeyer@gmail.com>
2003-03-24 20:57:37 +0000

    Improved shorten() method, added javadoc.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@18 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/PackageMemberAnnotation.java
+++ findbugs/src/java/edu/umd/cs/findbugs/PackageMemberAnnotation.java
@@ -1,14 +1,32 @@
 package edu.umd.cs.findbugs;
 
+/**
+ * Abstract base class for BugAnnotations describing constructs
+ * which are contained in a Java package.  Specifically,
+ * this includes classes, methods, and fields.
+ *
+ * @see BugAnnotation
+ * @author David Hovemeyer
+ */
 public abstract class PackageMemberAnnotation implements BugAnnotation {
 	protected String className;
 
+	/**
+	 * Constructor.
+	 * @param className name of the class
+	 */
 	public PackageMemberAnnotation(String className) {
 		this.className = className;
 	}
 
+	/**
+	 * Get the class name.
+	 */
 	public String getClassName() { return className; }
 
+	/**
+	 * Get the package name.
+	 */
 	public String getPackageName() {
 		int lastDot = className.lastIndexOf('.');
 		if (lastDot < 0)
@@ -17,6 +35,14 @@ public abstract class PackageMemberAnnotation implements BugAnnotation {
 			return className.substring(0, lastDot);
 	}
 
+	/**
+	 * Format the annotation.
+	 * Note that this version (defined by PackageMemberAnnotation)
+	 * only handles the "class" and "package" keys, and calls
+	 * formatPackageMember() for all other keys.
+	 * @param key the key
+	 * @return the formatted annotation
+	 */
 	public final String format(String key) {
 		if (key.equals("class"))
 			return className;
@@ -26,14 +52,25 @@ public abstract class PackageMemberAnnotation implements BugAnnotation {
 			return formatPackageMember(key);
 	}
 
+	/**
+	 * Shorten a type name of remove extraneous components.
+	 * Candidates for shortening are classes in same package as this annotation and
+	 * classes in the <code>java.lang</code> package.
+	 */
 	protected static String shorten(String pkgName, String typeName) {
-		if (typeName.startsWith(pkgName + "."))
-			typeName = typeName.substring(pkgName.length() + 1);
-		else if (typeName.startsWith("java.lang."))
-			typeName = typeName.substring("java.lang.".length());
+		int index = typeName.lastIndexOf('.');
+		if (index >= 0 ) {
+			String otherPkg = typeName.substring(0, index);
+			if (otherPkg.equals(pkgName) || otherPkg.equals("java.lang"))
+				typeName = typeName.substring(index + 1);
+		}
 		return typeName;
 	}
 
+	/**
+	 * Do default and subclass-specific formatting.
+	 * @param key the key specifying how to do the formatting
+	 */
 	protected abstract String formatPackageMember(String key);
 }
 
