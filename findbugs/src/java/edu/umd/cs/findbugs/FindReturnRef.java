77a4572746a701d065e1fa611951ccd76c5bcd39
David Hovemeyer <david.hovemeyer@gmail.com>
2003-03-25 21:14:09 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@77 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/FindReturnRef.java
@@ -0,0 +1,80 @@
+package edu.umd.cs.findbugs;
+import org.apache.bcel.classfile.*;
+import edu.umd.cs.pugh.visitclass.Constants2;
+
+public class FindReturnRef extends BytecodeScanningDetector implements   Constants2 {
+    boolean check = false;
+    boolean thisOnTOS = false;
+    boolean fieldOnTOS = false;
+    boolean staticMethod = false;
+    String nameOnStack;
+    String classNameOnStack;
+    String sigOnStack;
+    private BugReporter bugReporter;
+
+    public FindReturnRef(BugReporter bugReporter) {
+	this.bugReporter = bugReporter;
+	}
+
+  public void visit(JavaClass obj)     {
+	super.visit(obj);
+	}
+
+    public void visit(Method obj) {
+        check =  (obj.getAccessFlags() & (ACC_PUBLIC )) != 0;
+        staticMethod =  (obj.getAccessFlags() & (ACC_STATIC)) != 0;
+        if (check) 
+		super.visit(obj);
+        thisOnTOS = false;
+        fieldOnTOS = false;
+	}
+
+
+    public void sawOpcode(int seen) {
+	/*
+	System.out.println("Saw " + OPCODE_NAMES[seen] + "	" 
+			+ thisOnTOS
+			+ "	"
+			+ fieldOnTOS
+			);
+	*/
+	if (seen == ALOAD_0)  {
+		thisOnTOS = true;
+		fieldOnTOS = false;
+		return;
+		}
+
+	if (thisOnTOS && seen == GETFIELD && classConstant == className)  {
+		fieldOnTOS = true;
+		thisOnTOS = false;
+		nameOnStack = nameConstant;
+		classNameOnStack = betterClassConstant;
+		sigOnStack = sigConstant;
+		// System.out.println("Saw getfield");
+		return;
+		}
+	if (seen == GETSTATIC && classConstant == className)  {
+		fieldOnTOS = true;
+		thisOnTOS = false;
+		nameOnStack = nameConstant;
+		classNameOnStack = betterClassConstant;
+		sigOnStack = sigConstant;
+		return;
+		}
+	thisOnTOS = false;
+	if (check && fieldOnTOS && seen == ARETURN 
+		&& !sigOnStack.equals("Ljava/lang/String;")
+		&& sigOnStack.indexOf("Exception") == -1
+		&& sigOnStack.indexOf("[") >= 0
+		) {
+			bugReporter.reportBug(new BugInstance(staticMethod ? "MS_EXPOSE_REP" : "EI_EXPOSE_REP", NORMAL_PRIORITY)
+				.addClassAndMethod(this)
+				.addField(classNameOnStack, nameOnStack, sigOnStack));
+		}
+
+	fieldOnTOS = false;
+	thisOnTOS = false;
+	}
+
+
+}	
