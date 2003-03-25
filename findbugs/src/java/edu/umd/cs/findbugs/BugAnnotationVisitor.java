4f205b5f51dc6b410f9cfaf5bd9f2b76124b6385
David Hovemeyer <david.hovemeyer@gmail.com>
2003-03-25 16:10:49 +0000

    Added visit method for IntAnnotations.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@44 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/BugAnnotationVisitor.java
+++ findbugs/src/java/edu/umd/cs/findbugs/BugAnnotationVisitor.java
@@ -4,6 +4,7 @@ public interface BugAnnotationVisitor {
 	public void visitClassAnnotation(ClassAnnotation classAnnotation);
 	public void visitFieldAnnotation(FieldAnnotation fieldAnnotation);
 	public void visitMethodAnnotation(MethodAnnotation methodAnnotation);
+	public void visitIntAnnotation(IntAnnotation intAnnotation);
 }
 
 // vim:ts=4
