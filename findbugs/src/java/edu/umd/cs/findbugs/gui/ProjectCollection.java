fa67df31d222d3a8742e35177b5d54cec787fe05
David Hovemeyer <david.hovemeyer@gmail.com>
2003-03-31 17:31:14 +0000

    Added methods to get projects.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@122 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/gui/ProjectCollection.java
+++ findbugs/src/java/edu/umd/cs/findbugs/gui/ProjectCollection.java
@@ -31,4 +31,22 @@ public class ProjectCollection {
 	projectList.add(project);
     }
     
+    /**
+     * Get number of projects.
+     * @return number of projects
+     */
+    public int getNumProjects() { return projectList.size(); }
+    
+    /**
+     * Get project whose number is given.
+     * @param num project number
+     * @return the project
+     */
+    public Project getProject(int num) {
+        return (Project) projectList.get(num);
+    }
+    
+    public String toString() {
+        return "Projects";
+    }
 }
