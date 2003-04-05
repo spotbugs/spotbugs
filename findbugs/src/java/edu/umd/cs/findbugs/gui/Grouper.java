a34bc57d90bda412c8b247e84a7e5b2d2cd000c0
David Hovemeyer <david.hovemeyer@gmail.com>
2003-04-05 21:24:19 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@187 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/gui/Grouper.java
@@ -0,0 +1,63 @@
+/*
+ * Grouper.java
+ *
+ * Created on April 5, 2003, 3:46 PM
+ */
+
+package edu.umd.cs.findbugs.gui;
+
+import java.util.*;
+
+/**
+ * Given a sorted Collection and a Comparator, produces groups of objects
+ * that compare as equal.  If the Collection is not sorted, this
+ * class will not work correctly.
+ *
+ * @author David Hovemeyer
+ */
+public class Grouper {
+    
+    public interface Callback {
+	public void startGroup();
+	public void addToGroup(Object member);
+    }
+    
+    private Callback callback;
+    
+    /**
+     * Creates a new instance of Grouper.
+     * @param callback the callback which receives the groups and elements
+     */
+    public Grouper(Callback callback) {
+	this.callback = callback;
+    }
+
+    /**
+     * Group elements of given collection according to given
+     * compartor's test for equality.  The groups are specified by
+     * calls to the Grouper's callback object.
+     *
+     * @param collection the collection
+     * @param comparator the comparator
+     */
+    public void group(Collection collection, Comparator comparator) {
+	Iterator i = collection.iterator();
+	Object last = null;
+	while (i.hasNext()) {
+	    Object current = i.next();
+	    if (last == null) {
+		callback.startGroup();
+		callback.addToGroup(current);
+	    } else if (comparator.compare(last, current) == 0) {
+		// Same group as before
+		callback.addToGroup(current);
+	    } else {
+		callback.startGroup();
+		callback.addToGroup(current);
+	    }
+	    
+	    last = current;
+	}
+    }
+    
+}
