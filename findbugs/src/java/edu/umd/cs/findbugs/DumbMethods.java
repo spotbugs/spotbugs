3ec58b5fe8b2e5fe24d41b901625a2e5a3f6ea03
David Hovemeyer <david.hovemeyer@gmail.com>
2003-04-07 19:41:40 +0000

    Added source line information.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@220 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/DumbMethods.java
+++ findbugs/src/java/edu/umd/cs/findbugs/DumbMethods.java
@@ -21,19 +21,25 @@ public class DumbMethods extends BytecodeScanningDetector implements   Constants
 				&& nameConstant.equals("<init>")
 				&& sigConstant.equals("(Ljava/lang/String;)V"))
 		if (alreadyReported.add(betterMethodName))
-			bugReporter.reportBug(new BugInstance("DM_STRING_CTOR", NORMAL_PRIORITY).addClassAndMethod(this));
+			bugReporter.reportBug(new BugInstance("DM_STRING_CTOR", NORMAL_PRIORITY)
+				.addClassAndMethod(this)
+				.addSourceLine(this));
 	if ((seen == INVOKESPECIAL)
 				&& classConstant.equals("java/lang/String")
 				&& nameConstant.equals("<init>")
 				&& sigConstant.equals("()V"))
 		if (alreadyReported.add(betterMethodName))
-			bugReporter.reportBug(new BugInstance("DM_STRING_VOID_CTOR", NORMAL_PRIORITY).addClassAndMethod(this));
+			bugReporter.reportBug(new BugInstance("DM_STRING_VOID_CTOR", NORMAL_PRIORITY)
+				.addClassAndMethod(this)
+				.addSourceLine(this));
 	if ((seen == INVOKESPECIAL)
 				&& classConstant.equals("java/lang/Boolean")
 				&& nameConstant.equals("<init>")
 				&& !className.equals("java/lang/Boolean")
 				)
 		if (alreadyReported.add(betterMethodName))
-			bugReporter.reportBug(new BugInstance("DM_BOOLEAN_CTOR", NORMAL_PRIORITY).addClassAndMethod(this));
+			bugReporter.reportBug(new BugInstance("DM_BOOLEAN_CTOR", NORMAL_PRIORITY)
+				.addClassAndMethod(this)
+				.addSourceLine(this));
 	}
 }
