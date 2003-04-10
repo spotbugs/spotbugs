20fcbb9f8b3de50f92852264654fa967c3675a45
David Hovemeyer <david.hovemeyer@gmail.com>
2003-04-10 17:48:39 +0000

    Changed so it uses HTML to display the about information and the software license.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@278 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/gui/AboutDialog.java
+++ findbugs/src/java/edu/umd/cs/findbugs/gui/AboutDialog.java
@@ -9,18 +9,21 @@ package edu.umd.cs.findbugs.gui;
 import javax.swing.ImageIcon;
 
 /**
- *
- * @author  daveho
+ * The Help:About dialog.
+ * @author David Hovemeyer
  */
 public class AboutDialog extends javax.swing.JDialog {
     
-    private ImageIcon bugLogo;// = new ImageIcon(getClass().getResource("edu/umd/cs/findbugs/gui/bug-logo.png"));
+    private FindBugsFrame parent;
+    private java.net.URL aboutURL;
+    private java.net.URL licenseURL;
     
     /** Creates new form AboutDialog */
-    public AboutDialog(java.awt.Frame parent, boolean modal) {
+    public AboutDialog(FindBugsFrame parent, boolean modal) {
 	super(parent, modal);
-	ClassLoader classLoader = getClass().getClassLoader();
-	bugLogo = new ImageIcon(classLoader.getResource("edu/umd/cs/findbugs/gui/bug-logo.png"));
+        this.parent = parent;
+        aboutURL = getClass().getClassLoader().getResource("edu/umd/cs/findbugs/gui/help/About.html");
+        licenseURL = getClass().getClassLoader().getResource("edu/umd/cs/findbugs/gui/help/License.html");
 	initComponents();
     }
     
@@ -32,40 +35,54 @@ public class AboutDialog extends javax.swing.JDialog {
     private void initComponents() {//GEN-BEGIN:initComponents
         java.awt.GridBagConstraints gridBagConstraints;
 
-        bugLogoLabel = new javax.swing.JLabel();
+        aboutTabPane = new javax.swing.JTabbedPane();
+        aboutScrollPane = new javax.swing.JScrollPane();
+        aboutEditorPane = new javax.swing.JEditorPane();
+        licenseScrollPane = new javax.swing.JScrollPane();
+        licenseEditorPane = new javax.swing.JEditorPane();
         jSeparator1 = new javax.swing.JSeparator();
         okButton = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
 
         getContentPane().setLayout(new java.awt.GridBagLayout());
 
+        setTitle("About FindBugs");
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent evt) {
                 closeDialog(evt);
             }
         });
 
-        bugLogoLabel.setIcon(bugLogo);
+        try {
+            aboutEditorPane.setPage(aboutURL);
+        } catch (java.io.IOException e) {
+            parent.getLogger().logMessage(ConsoleLogger.ERROR, "Error loading about URL: " + e.getMessage());
+        }
+        aboutScrollPane.setViewportView(aboutEditorPane);
+
+        aboutTabPane.addTab("About", aboutScrollPane);
+
+        try {
+            licenseEditorPane.setPage(licenseURL);
+        } catch (java.io.IOException e) {
+            parent.getLogger().logMessage(ConsoleLogger.ERROR, "Error loading license URL: " + e.getMessage());
+        }
+        licenseScrollPane.setViewportView(licenseEditorPane);
+
+        aboutTabPane.addTab("License", licenseScrollPane);
+
         gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridheight = 7;
-        gridBagConstraints.insets = new java.awt.Insets(10, 10, 10, 0);
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
         gridBagConstraints.weightx = 1.0;
         gridBagConstraints.weighty = 1.0;
-        getContentPane().add(bugLogoLabel, gridBagConstraints);
+        getContentPane().add(aboutTabPane, gridBagConstraints);
 
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 7;
-        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.gridy = 1;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         getContentPane().add(jSeparator1, gridBagConstraints);
 
+        okButton.setMnemonic('O');
         okButton.setText("OK");
         okButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -75,72 +92,14 @@ public class AboutDialog extends javax.swing.JDialog {
 
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 8;
-        gridBagConstraints.gridwidth = 2;
-        getContentPane().add(okButton, gridBagConstraints);
-
-        jLabel1.setText("FindBugs");
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.insets = new java.awt.Insets(0, 10, 0, 10);
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
-        getContentPane().add(jLabel1, gridBagConstraints);
-
-        jLabel2.setFont(new java.awt.Font("Dialog", 0, 12));
-        jLabel2.setText("Copyright (C) 2003, University of Maryland");
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 2;
-        gridBagConstraints.insets = new java.awt.Insets(0, 10, 0, 10);
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
-        getContentPane().add(jLabel2, gridBagConstraints);
-
-        jLabel3.setFont(new java.awt.Font("Dialog", 0, 12));
-        jLabel3.setText("Authors:");
-        jLabel3.setToolTipText("");
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 3;
-        gridBagConstraints.insets = new java.awt.Insets(0, 10, 0, 10);
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
-        getContentPane().add(jLabel3, gridBagConstraints);
-
-        jLabel4.setFont(new java.awt.Font("Dialog", 0, 12));
-        jLabel4.setText("William Pugh");
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 4;
-        gridBagConstraints.insets = new java.awt.Insets(0, 10, 0, 10);
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
-        getContentPane().add(jLabel4, gridBagConstraints);
-
-        jLabel5.setFont(new java.awt.Font("Dialog", 0, 12));
-        jLabel5.setText("David Hovemeyer");
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 5;
-        gridBagConstraints.insets = new java.awt.Insets(0, 10, 0, 10);
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
-        getContentPane().add(jLabel5, gridBagConstraints);
-
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.fill = java.awt.GridBagConstraints.VERTICAL;
-        gridBagConstraints.weighty = 0.5;
-        getContentPane().add(jLabel6, gridBagConstraints);
-
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 6;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.VERTICAL;
-        gridBagConstraints.weighty = 0.5;
-        getContentPane().add(jLabel7, gridBagConstraints);
+        getContentPane().add(okButton, gridBagConstraints);
 
         pack();
     }//GEN-END:initComponents
 
     private void okButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_okButtonActionPerformed
-	closeDialog();
+        closeDialog();
     }//GEN-LAST:event_okButtonActionPerformed
     
     /** Closes the dialog */
@@ -153,25 +112,14 @@ public class AboutDialog extends javax.swing.JDialog {
 	dispose();
     }
     
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-	new AboutDialog(new javax.swing.JFrame(), true).show();
-    }
-    
-    
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane licenseScrollPane;
     private javax.swing.JButton okButton;
-    private javax.swing.JLabel jLabel7;
+    private javax.swing.JScrollPane aboutScrollPane;
+    private javax.swing.JEditorPane licenseEditorPane;
     private javax.swing.JSeparator jSeparator1;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel bugLogoLabel;
-    private javax.swing.JLabel jLabel5;
+    private javax.swing.JEditorPane aboutEditorPane;
+    private javax.swing.JTabbedPane aboutTabPane;
     // End of variables declaration//GEN-END:variables
     
 }
