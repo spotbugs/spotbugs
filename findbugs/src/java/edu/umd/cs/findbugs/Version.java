c7441a584e5ad1a2baa826401cd23a201bcb471a
David Hovemeyer <david.hovemeyer@gmail.com>
2003-04-15 19:03:51 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@312 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/Version.java
@@ -0,0 +1,61 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs;
+
+/**
+ * Version number and release date information.
+ */
+public class Version {
+	/** Major version number. */
+	public static final int MAJOR = 0;
+
+	/** Minor version number. */
+	public static final int MINOR = 5;
+
+	/** Patch level. */
+	public static final int PATCHLEVEL = 0;
+
+	/** Release version string. */
+	public static final String RELEASE = MAJOR + "." + MINOR + "." + PATCHLEVEL;
+
+	/** Release date. */
+	public static final String DATE = "April 5, 2003";
+
+	public static void main(String[] argv) {
+		if (argv.length != 1)
+			usage();
+
+		String arg = argv[0];
+
+		if (arg.equals("-release"))
+			System.out.println(RELEASE);
+		else if (arg.equals("-date"))
+			System.out.println(DATE);
+		else
+			usage();
+	}
+
+	private static void usage() {
+		System.err.println("Usage: edu.umd.cs.findbugs.Version (-release|-date)");
+		System.exit(1);
+	}
+}
+
+// vim:ts=4
