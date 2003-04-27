28e65c5cae84103194d730adb9991f6d8c8362a7
David Hovemeyer <david.hovemeyer@gmail.com>
2003-04-27 17:42:37 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@427 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/DominatorsAnalysis.java
@@ -0,0 +1,104 @@
+/*
+ * Bytecode Analysis Framework
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.daveho.ba;
+
+import java.util.*;
+import org.apache.bcel.generic.InstructionHandle;
+
+/**
+ * A dataflow analysis to compute dominator relationships between
+ * basic blocks.  Use the {@link getResultFact} method to get the dominator
+ * set for a given basic block.  The dominator sets are represented using
+ * the {@link java.util.BitSet} class, with the individual bits
+ * corresponding to the IDs of basic blocks.
+ *
+ * @see DataflowAnalysis
+ * @see CFG
+ * @see BasicBlock
+ * @author David Hovemeyer
+ */
+public class DominatorsAnalysis implements DataflowAnalysis<BitSet> {
+	private final CFG cfg;
+
+	/**
+	 * Constructor.
+	 * @param CFG the CFG to compute dominator relationships for
+	 */
+	public DominatorsAnalysis(CFG cfg) {
+		this.cfg = cfg;
+	}
+
+	public BitSet createFact() {
+		return new BitSet();
+	}
+
+	public void copy(BitSet source, BitSet dest) {
+		dest.clear();
+		dest.or(source);
+	}
+
+	public void initEntryFact(BitSet result) {
+		// No blocks dominate the entry block
+		result.clear();
+	}
+
+	public void initResultFact(BitSet result) {
+		makeFactTop(result);
+	}
+
+	public void makeFactTop(BitSet fact) {
+		fact.set(0, cfg.getNumBasicBlocks());
+	}
+
+	public boolean isForwards() {
+		return true;
+	}
+
+	public BlockOrder getBlockOrder(CFG cfg) {
+		return new ReversePostfixOrder(cfg);
+	}
+
+	public boolean same(BitSet fact1, BitSet fact2) {
+		return fact1.equals(fact2);
+	}
+
+	public void transfer(BasicBlock basicBlock, InstructionHandle end, BitSet start, BitSet result) throws DataflowAnalysisException {
+		// Start with intersection of dominators of predecessors
+		copy(start, result);
+
+		// Every block dominates itself
+		result.set(basicBlock.getId());
+	}
+
+	public void meetPredecessorFacts(BasicBlock basicBlock, List<Edge> predEdgeList, List<BitSet> predFactList, BitSet start)
+		throws DataflowAnalysisException {
+
+		// start has already been set to TOP
+
+		// Take intersection of dominators of all predecessors
+		Iterator<BitSet> i = predFactList.iterator();
+		while (i.hasNext()) {
+			BitSet pred = i.next();
+			start.and(pred);
+		}
+	}
+}
+
+// vim:ts=4
---
+++ findbugs/test/Ex.java
@@ -0,0 +1,13 @@
+public class Ex {
+
+	public void frotz(Object o, int y) {
+		if (y == 6) {
+			synchronized (o) {
+				y += o.hashCode();
+				if ((y & 1) == 0)
+					throw new RuntimeException();
+			}
+		}
+	}
+
+}
