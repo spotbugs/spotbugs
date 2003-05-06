5c45064a48593fed70a180c80785a909cc3a0db0
David Hovemeyer <david.hovemeyer@gmail.com>
2003-05-06 20:00:55 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@533 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/BugCode.java
@@ -0,0 +1,35 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs;
+
+public class BugCode {
+	private String abbrev;
+	private String description;
+
+	public BugCode(String abbrev, String description) {
+		this.abbrev = abbrev;
+		this.description = description;
+	}
+
+	public String getAbbrev() { return abbrev; }
+	public String getDescription() { return description; }
+}
+
+// vim:ts=4
