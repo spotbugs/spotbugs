f9746d33e6f41ad21dad236721a17add5c61d9f7
David Hovemeyer <david.hovemeyer@gmail.com>
2003-05-13 20:40:35 +0000

    Updated release date.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@627 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/Version.java
+++ findbugs/src/java/edu/umd/cs/findbugs/Version.java
@@ -36,7 +36,7 @@ public class Version {
 	public static final String RELEASE = MAJOR + "." + MINOR + "." + PATCHLEVEL;
 
 	/** Release date. */
-	public static final String DATE = "May 1, 2003";
+	public static final String DATE = "May 13, 2003";
 
 	public static void main(String[] argv) {
 		if (argv.length != 1)
