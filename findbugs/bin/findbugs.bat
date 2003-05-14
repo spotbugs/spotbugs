579bc437d99752dbce2c26e233b310fb57dae557
David Hovemeyer <david.hovemeyer@gmail.com>
2003-05-14 19:38:12 +0000

    Try to deal with quoted arguments better.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@642 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/bin/findbugs.bat
+++ findbugs/bin/findbugs.bat
@@ -22,21 +22,21 @@ shift
 
 :loop
 
-if "%1"=="-gui" set appjar=findbugsGUI.jar
-if "%1"=="-gui" goto shift1
+if [%1]==[-gui] set appjar=findbugsGUI.jar
+if [%1]==[-gui] goto shift1
 
-if "%1"=="-textui" set appjar=findbugs.jar
-if "%1"=="-textui" goto shift1
+if [%1]==[-textui] set appjar=findbugs.jar
+if [%1]==[-textui] goto shift1
 
-if "%1"=="-home" set FINDBUGS_HOME=%2
-if "%1"=="-home" goto shift2
+if [%1]==[-home] set FINDBUGS_HOME=%2
+if [%1]==[-home] goto shift2
 
-if "%1"=="-jvmArgs" set jvmargs=%2
-if "%1"=="-jvmArgs" goto shift2
+if [%1]==[-jvmArgs] set jvmargs=%2
+if [%1]==[-jvmArgs] goto shift2
 
-if "%1"=="-help" goto help
+if [%1]==[-help] goto help
 
-if "%1"=="" goto launch
+if [%1]==[] goto launch
 
 set args=%args% %1
 goto shift1
@@ -46,7 +46,12 @@ goto shift1
 :: ----------------------------------------------------------------------
 :launch
 :: Make sure FINDBUGS_HOME is set.
-if "%FINDBUGS_HOME%"=="" goto homeNotSet
+if [%FINDBUGS_HOME%]==[] goto homeNotSet
+
+:: Try to remove quote characters from FINDBUGS_HOME and jvmargs.
+:: This will only work on Windows NT and later systems.
+if [%OS%]==[Windows_NT] set FINDBUGS_HOME=%FINDBUGS_HOME:"=%
+if [%OS%]==[Windows_NT] set jvmargs=%jvmargs:"=%
 
 :: echo FINDBUGS_HOME is %FINDBUGS_HOME%
 :: echo appjar is %appjar%
