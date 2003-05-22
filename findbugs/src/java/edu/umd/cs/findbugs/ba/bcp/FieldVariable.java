18b7b60fb259cbdce113d7b726cf61f1feaaa128
David Hovemeyer <david.hovemeyer@gmail.com>
2003-05-22 19:24:36 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@686 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/bcp/FieldVariable.java
@@ -0,0 +1,45 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.bcp;
+
+import edu.umd.cs.daveho.ba.ValueNumber;
+
+public class FieldVariable implements Variable {
+	private final ValueNumber ref;
+	private final String className;
+	private final String fieldName;
+
+	public FieldVariable(ValueNumber ref, String className, String fieldName) {
+		this.ref = ref;
+		this.className = className;
+		this.fieldName = fieldName;
+	}
+
+	public boolean sameAs(Variable other) {
+		if (!(other instanceof FieldVariable))
+			return false;
+		FieldVariable otherField = (FieldVariable) other;
+		return ref.equals(otherField.ref)
+			&& className.equals(otherField.className)
+			&& fieldName.equals(otherField.fieldName);
+	}
+}
+
+// vim:ts=4
