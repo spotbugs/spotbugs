da27d6f04d1df8d378fc0e2bbe5ff7e3f0dd8c2d
David Hovemeyer <david.hovemeyer@gmail.com>
2003-05-23 13:49:11 +0000

    Made it immutable with a "prev" link, so we can build them up using consing.  This makes it trivial to implement non-determinism; the common prefix of two matches is shared.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@700 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/ba/bcp/PatternElementMatch.java
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/bcp/PatternElementMatch.java
@@ -22,24 +22,61 @@ package edu.umd.cs.daveho.ba.bcp;
 import java.util.*;
 import org.apache.bcel.generic.InstructionHandle;
 
+/**
+ * PatternElementMatch represents matching a PatternElement against
+ * a single instruction.  The "prev" field points to the previous
+ * PatternElementMatch.  By building up sequences of PatternElementMatch objects
+ * in this way, we can implement nondeterministic matching without
+ * having to copy anything.
+ */
 public class PatternElementMatch {
-	private PatternElement patternElement;
-	private List<InstructionHandle> matchedInstructionList;
+	private final PatternElement patternElement;
+	private final InstructionHandle matchedInstruction;
+	private final int matchCount;
+	private final PatternElementMatch prev;
 
-	public PatternElementMatch(PatternElement patternElement) {
+	/**
+	 * Constructor.
+	 * @param patternElement the PatternElement being matched
+	 * @param matchedInstruction the instruction which matched the PatternElement
+	 * @param matchCount the index (starting at zero) of the instructions
+	 *   matching the PatternElement; multiple instructions can match the
+	 *   same PatternElement
+	 * @param prev the previous PatternElementMatch
+	 */
+	public PatternElementMatch(PatternElement patternElement, InstructionHandle matchedInstruction,
+		int matchCount, PatternElementMatch prev) {
 		this.patternElement = patternElement;
-		this.matchedInstructionList = new LinkedList<InstructionHandle>();
+		this.matchedInstruction = matchedInstruction;
+		this.matchCount = matchCount;
+		this.prev = prev;
 	}
 
-	public void addMatchedInstruction(InstructionHandle handle) {
-		matchedInstructionList.add(handle);
+	/** Get the PatternElement. */
+	public PatternElement getPatternElement() {
+		return patternElement;
 	}
 
-	public int getNumMatchedInstructions() {
-		return matchedInstructionList.size();
+	/** Get the matched instruction. */
+	public InstructionHandle getMatchedInstructionInstructionHandle() {
+		return matchedInstruction;
 	}
 
-	// TODO: add a duplicate() method
+	/*
+	 * Get the index of this instruction in terms of how many instructions
+	 * have matched this PatternElement.  (0 for the first instruction to
+	 * match the PatternElement, etc.)
+	 */
+	public int getMatchCount() {
+		return matchCount;
+	}
+
+	/**
+	 * Get the previous PatternMatchElement.
+	 */
+	public PatternElementMatch getPrev() {
+		return prev;
+	}
 }
 
 // vim:ts=4
