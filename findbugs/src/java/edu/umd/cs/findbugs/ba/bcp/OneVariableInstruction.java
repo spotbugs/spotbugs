770475173f95600fd16bee365483e78b54b36df5
David Hovemeyer <david.hovemeyer@gmail.com>
2003-05-28 16:51:34 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@720 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/bcp/OneVariableInstruction.java
@@ -0,0 +1,64 @@
+/*
+ * Bytecode Analysis Framework
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.daveho.ba.bcp;
+
+/**
+ * Abstract PatternElement subclass for matching single instructions which have
+ * a single Variable.
+ *
+ * @see PatternElement
+ */
+public abstract class OneVariableInstruction extends SingleInstruction {
+	private String varName;
+
+	/**
+	 * Constructor.
+	 * @param varName the name of the Variable used in this instruction
+	 */
+	public OneVariableInstruction(String varName) {
+		this.varName = varName;
+	}
+
+	/**
+	 * Add a variable definition to the given BindingSet, or if
+	 * there is an existing definitions, make sure it is consistent with
+	 * the new definition.
+	 * @param variable the Variable which should be added or checked for consistency
+	 * @param bindingSet the existing set of bindings
+	 * @return the updated BindingSet (if the variable is consistent with the
+	 *   previous bindings), or null if the new variable is inconsistent with
+	 *   the previous bindings
+	 */
+	protected BindingSet addOrCheckDefinition(Variable variable, BindingSet bindingSet) {
+		Variable existingVariable = lookup(varName, bindingSet);
+		if (existingVariable == null) {
+			bindingSet = new BindingSet(new Binding(varName, existingVariable), bindingSet);
+		} else {
+			if (!existingVariable.equals(variable))
+				return null;
+		}
+
+		return bindingSet;
+	}
+
+
+}
+
+// vim:ts=4
---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/bcp/SingleInstruction.java
@@ -0,0 +1,41 @@
+/*
+ * Bytecode Analysis Framework
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.daveho.ba.bcp;
+
+import org.apache.bcel.generic.InstructionHandle;
+import edu.umd.cs.daveho.ba.Edge;
+
+/**
+ * An abstract PatternElement subclass for pattern elements which
+ * must match exactly one instruction and accept any kind of branch.
+ * (Subclasses may override acceptBranch() to implement more selective
+ * handling of branches.)
+ *
+ * @see PatternElement
+ * @author David Hovemeyer
+ */
+public abstract class SingleInstruction extends PatternElement {
+	public boolean acceptBranch(Edge edge) { return true; }
+	public int minOccur() { return 1; }
+	public int maxOccur() { return 1; }
+
+}
+
+// vim:ts=4
