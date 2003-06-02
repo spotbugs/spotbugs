b64a567f9bb0c969213173f37fff7596cbd659c8
David Hovemeyer <david.hovemeyer@gmail.com>
2003-06-02 23:54:18 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@786 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/bcp/MatchResult.java
@@ -0,0 +1,55 @@
+/*
+ * Bytecode Analysis Framework
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.daveho.ba.bcp;
+
+/**
+ * The result of matching a single PatternElement against a single instruction.
+ * Specifies the PatternElement and the (possibly updated) BindingSet.
+ * The reason we need this class is because some kinds of PatternElements,
+ * such as MatchAny, may use it to indicate that a child PatternElement was
+ * the one that actually matched the instruction.
+ *
+ * @see PatternElement
+ * @see BindingSet
+ * @author David Hovemeyer
+ */
+public class MatchResult {
+	private PatternElement patternElement;
+	private BindingSet bindingSet;
+
+	/**
+	 * Constructor.
+	 * @param patternElement the PatternElement that matched the instruction
+	 * @param bindingSet the possibly updated BindingSet
+	 */
+	public MatchResult(PatternElement patternElement, BindingSet bindingSet) {
+		this.patternElement = patternElement;
+		this.bindingSet = bindingSet;
+	}
+
+	/** Get the PatternElement. */
+	public PatternElement getPatternElement() { return patternElement; }
+
+	/** Get the BindingSet. */
+	public BindingSet getBindingSet() { return bindingSet; }
+
+}
+
+// vim:ts=4
