c566e9134cdf7aab197269a7c63853c7cf4ac866
David Hovemeyer <david.hovemeyer@gmail.com>
2003-06-02 01:59:14 +0000

    Changed to inherit from ByteCodePatternDetector.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@764 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/BCPDoubleCheck.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/BCPDoubleCheck.java
@@ -1,3 +1,22 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 package edu.umd.cs.findbugs.detect;
 
 import java.util.*;
@@ -7,8 +26,19 @@ import edu.umd.cs.findbugs.*;
 import edu.umd.cs.daveho.ba.*;
 import edu.umd.cs.daveho.ba.bcp.*;
 
-public class BCPDoubleCheck implements Detector {
-	private static final boolean DEBUG = Boolean.getBoolean("bcpdc.debug");
+/**
+ * A bug detector that uses a ByteCodePattern to find instances of
+ * the double check idiom.  This class serves as a good example of
+ * how ByteCodePatterns can be used to simplify the task of implementing
+ * Detectors.
+ *
+ * @see ByteCodePattern
+ * @author David Hovemeyer
+ */
+public class BCPDoubleCheck extends ByteCodePatternDetector {
+	// FIXME: prescreen for the existence of
+	// MONITORENTER, GETFIELD/GETSTATIC, and PUTFIELD/PUTSTATIC
+	// to avoid scanning a lot of methods
 
 	private BugReporter bugReporter;
 
@@ -25,7 +55,18 @@ public class BCPDoubleCheck implements Detector {
 	private static final int CREATE_OBJ_WILD = 40;
 
 	/**
+	 * Constructor.
+	 * @param bugReporter
+	 */
+	public BCPDoubleCheck(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
+
+	/**
 	 * The doublecheck pattern.
+	 * The variable "h" represents the field.
+	 * "x" and "y" are local values resulting from loading
+	 * the field.
 	 */
 	private static final ByteCodePattern pattern = new ByteCodePattern();
 	static {
@@ -40,73 +81,22 @@ public class BCPDoubleCheck implements Detector {
 			.add(new Store("h", pattern.dummyVariable()));
 	}
 
-	public BCPDoubleCheck(BugReporter bugReporter) {
-		this.bugReporter = bugReporter;
+	public ByteCodePattern getPattern() {
+		return pattern;
 	}
 
-	public void visitClassContext(ClassContext classContext) {
-		// FIXME: prescreen for the existence of
-		// MONITORENTER, GETFIELD/GETSTATIC, and PUTFIELD/PUTSTATIC
-		// to avoid scanning a lot of methods
+	public void reportMatch(MethodGen methodGen, ByteCodePatternMatch match) {
+		BindingSet bindingSet = match.getBindingSet();
 
-		try {
-			JavaClass jclass = classContext.getJavaClass();
-			Method[] methodList = jclass.getMethods();
-			for (int i = 0; i < methodList.length; ++i) {
-				Method method = methodList[i];
-				if (method.isAbstract() || method.isNative())
-					continue;
-	
-				MethodGen methodGen = classContext.getMethodGen(method);
-				ConstantPoolGen cpg = methodGen.getConstantPool();
-				CFG cfg = classContext.getCFG(method);
-				DepthFirstSearch dfs = classContext.getDepthFirstSearch(method);
-				ValueNumberDataflow vnaDataflow = classContext.getValueNumberDataflow(method);
+		// Note that the lookup of "h" cannot fail, and
+		// it is guaranteed to be bound to a FieldVariable.
+		Binding binding = bindingSet.lookup("h");
+		FieldVariable field = (FieldVariable) binding.getVariable();
 
-				if (DEBUG) {
-					System.out.print(
-						"=====================================================================\n"+
-						"Method " + methodGen.getName() + "\n" +
-						"=====================================================================\n");
-				}
-
-				PatternMatcher matcher = new PatternMatcher(pattern, cfg, cpg, dfs, vnaDataflow);
-				matcher.execute();
-	
-				Iterator<ByteCodePatternMatch> j = matcher.byteCodePatternMatchIterator();
-				while (j.hasNext()) {
-					ByteCodePatternMatch match = j.next();
-					BindingSet bindingSet = match.getBindingSet();
-	
-					Binding binding = bindingSet.lookup("h");
-					if (binding == null) {
-						bugReporter.logError("Unknown field for BCPDoubleCheck in method " + methodGen);
-						continue;
-					}
-					FieldVariable field = (FieldVariable) binding.getVariable();
-					if (field == null) throw new IllegalStateException("Hosed!");
-
-					if (DEBUG) {
-						System.out.println("Pattern match:");
-						Iterator<PatternElementMatch> pemIter = match.patternElementMatchIterator();
-						while (pemIter.hasNext()) {
-							PatternElementMatch pem = pemIter.next();
-							System.out.println("\t" + pem.toString());
-						}
-					}
-	
-					bugReporter.reportBug(new BugInstance("BCPDC_DOUBLECHECK", NORMAL_PRIORITY)
-						.addClass(methodGen.getClassName())
-						.addMethod(methodGen)
-						.addField(field.getClassName(), field.getFieldName(), field.getFieldSig(), field.isStatic()));
-				}
-			}
-		} catch (DataflowAnalysisException e) {
-			throw new AnalysisException("BCPDoubleCheck caught exception", e);
-		}
-	}
-
-	public void report() {
+		bugReporter.reportBug(new BugInstance("BCPDC_DOUBLECHECK", NORMAL_PRIORITY)
+			.addClass(methodGen.getClassName())
+			.addMethod(methodGen)
+			.addField(field.getClassName(), field.getFieldName(), field.getFieldSig(), field.isStatic()));
 	}
 }
 
