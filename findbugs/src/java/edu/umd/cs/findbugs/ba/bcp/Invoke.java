f0c87016dcb408ee1f0af5fe379d6c8528cbad04
David Hovemeyer <david.hovemeyer@gmail.com>
2003-06-03 01:24:19 +0000

    Allow subclass matches for class name, and regular expression matches for method name and signature.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@792 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/ba/bcp/Invoke.java
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/bcp/Invoke.java
@@ -19,6 +19,9 @@
 
 package edu.umd.cs.daveho.ba.bcp;
 
+import java.util.regex.*;
+import org.apache.bcel.Repository;
+import org.apache.bcel.Constants;
 import org.apache.bcel.generic.*;
 import edu.umd.cs.daveho.ba.*;
 
@@ -33,44 +36,91 @@ import edu.umd.cs.daveho.ba.*;
  * @author David Hovemeyer
  */
 public class Invoke extends PatternElement {
+	private boolean acceptSubclass;
 	private String className;
-	private String methodName;
-	private String methodSig;
+	private Pattern methodNameRE;
+	private Pattern methodSigRE;
 	private boolean isStatic;
 
 	/**
 	 * Constructor.
-	 * @param className the class name of the method
-	 * @param methodName the name of the method
-	 * @param methodSig the signature of the method
+	 * @param className the class name of the method; if it begins with a "+"
+	 *  character, then any subclass of the named class is accepted
+	 * @param methodName the name of the method; if it begins with a "/" character,
+	 *  then the rest of the string is treated as a regular expression
+	 * @param methodSig the signature of the method; if it begins with a "/" character,
+	 *  then the rest of the string is treated as a regular expression
 	 * @param isStatic true if the method is static, false otherwise
 	 */
 	public Invoke(String className, String methodName, String methodSig, boolean isStatic) {
+		if (className.startsWith("+")) {
+			this.acceptSubclass = true;
+			className = className.substring(1);
+		} else
+			this.acceptSubclass = false;
 		this.className = className;
-		this.methodName = methodName;
-		this.methodSig = methodSig;
+
+		this.methodNameRE = createRE(methodName);
+		this.methodSigRE = createRE(methodSig);
+
 		this.isStatic = isStatic;
+	}
+
+	private Pattern createRE(String name) {
+		String pattern;
+		if (name.startsWith("/")) {
+			// Regular expression match
+			pattern = name.substring(1);
+		} else {
+			// Quote metacharacters, so we match the literal value of the string
+			pattern = "\\Q" + name + "\\E";
+		}
+		return Pattern.compile(pattern);
 	}
 
 	public MatchResult match(InstructionHandle handle, ConstantPoolGen cpg,
 		ValueNumberFrame before, ValueNumberFrame after, BindingSet bindingSet) throws DataflowAnalysisException {
+
+		// See if the instruction is an InvokeInstruction
 		Instruction ins = handle.getInstruction();
 		if (!(ins instanceof InvokeInstruction))
 			return null;
 		InvokeInstruction inv = (InvokeInstruction) ins;
 
+		// Check that it's static or non-static, as appropriate
+		boolean isStatic = inv.getOpcode() == Constants.INVOKESTATIC;
+		if (this.isStatic != isStatic)
+			return null;
+
+		// Check class name, method name, and method signature...
 		String className = inv.getClassName(cpg);
 		String methodName = inv.getMethodName(cpg);
 		String methodSig = inv.getSignature(cpg);
-		boolean isStatic = inv instanceof INVOKESTATIC;
 
-		if (this.className.equals(className) &&
-			this.methodName.equals(methodName) &&
-			this.methodSig.equals(methodSig) &&
-			this.isStatic == isStatic)
-			return new MatchResult(this, bindingSet);
-		else
-			return null;	
+		// Check class name
+		if (acceptSubclass) {
+			// See if the current invoked class is a subclass of the one we want
+			try {
+				if (!Repository.instanceOf(className, this.className))
+					return null;
+			} catch (ClassNotFoundException e) {
+				return null;
+			}
+		} else {
+			// Force exact class match
+			if (!this.className.equals(className))
+				return null;
+		}
+
+		// Check method name and signature
+		if (!methodNameRE.matcher(methodName).matches())
+			return null;
+		if (!methodSigRE.matcher(methodSig).matches())
+			return null;
+
+		// It's a match!
+		return new MatchResult(this, bindingSet);
+
 	}
 
 	public boolean acceptBranch(Edge edge, InstructionHandle source) {
