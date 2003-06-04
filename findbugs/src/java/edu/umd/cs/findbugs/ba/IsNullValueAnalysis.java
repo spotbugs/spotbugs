352bd6327ea240c5ef447b4c383717885f4105c5
David Hovemeyer <david.hovemeyer@gmail.com>
2003-06-04 18:12:26 +0000

    Downgrade "definitely null on some path" to "not definitely null" when a block has multiple non-exception successors.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@832 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/ba/IsNullValueAnalysis.java
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/IsNullValueAnalysis.java
@@ -19,17 +19,22 @@
 
 package edu.umd.cs.daveho.ba;
 
+import java.util.*;
 import org.apache.bcel.generic.MethodGen;
 import org.apache.bcel.generic.InstructionHandle;
 
 public class IsNullValueAnalysis extends ForwardDataflowAnalysis<IsNullValueFrame> {
 
 	private MethodGen methodGen;
+	private CFG cfg;
 	private ValueNumberDataflow vnaDataflow;
+	private IdentityHashMap<BasicBlock, Integer> nonExceptionSuccCountMap;
 
-	public IsNullValueAnalysis(MethodGen methodGen, ValueNumberDataflow vnaDataflow) {
+	public IsNullValueAnalysis(MethodGen methodGen, CFG cfg, ValueNumberDataflow vnaDataflow) {
 		this.methodGen = methodGen;
+		this.cfg = cfg;
 		this.vnaDataflow = vnaDataflow;
+		this.nonExceptionSuccCountMap = new IdentityHashMap<BasicBlock, Integer>();
 	}
 
 	public IsNullValueFrame createFact() {
@@ -61,6 +66,43 @@ public class IsNullValueAnalysis extends ForwardDataflowAnalysis<IsNullValueFram
 
 	public boolean same(IsNullValueFrame fact1, IsNullValueFrame fact2) {
 		return fact1.sameAs(fact2);
+	}
+
+	public void transfer(BasicBlock basicBlock, InstructionHandle end, IsNullValueFrame start, IsNullValueFrame result)
+		throws DataflowAnalysisException {
+
+		super.transfer(basicBlock, end, start, result);
+
+		// Special case: if this block has multiple non-exception successors,
+		// we downgrade "definitely null on some path" values to
+		// "not definitely null".  This should eliminate false positives
+		// due to (non-exception) infeasible paths.
+		// TODO: figure out whether exception edges should really be ignored
+		if (getNumNonExceptionSuccessors(basicBlock) > 1) {
+			int numSlots = result.getNumSlots();
+			for (int i = 0; i < numSlots; ++i) {
+				IsNullValue value = result.getValue(i);
+				if (value == IsNullValue.definitelyNullOnSomePath())
+					result.setValue(i, IsNullValue.notDefinitelyNull());
+			}
+		}
+	}
+
+	private int getNumNonExceptionSuccessors(BasicBlock basicBlock) {
+		Integer count = nonExceptionSuccCountMap.get(basicBlock);
+		if (count == null) {
+			int nonExceptionSuccCount = 0;
+			Iterator<Edge> i = cfg.outgoingEdgeIterator(basicBlock);
+			while (i.hasNext()) {
+				Edge edge = i.next();
+				int edgeType = edge.getType();
+				if (edgeType != EdgeTypes.UNHANDLED_EXCEPTION_EDGE && edgeType != EdgeTypes.HANDLED_EXCEPTION_EDGE)
+					++nonExceptionSuccCount;
+			}
+			count = new Integer(nonExceptionSuccCount);
+			nonExceptionSuccCountMap.put(basicBlock, count);
+		}
+		return count.intValue();
 	}
 
 	public void transferInstruction(InstructionHandle handle, BasicBlock basicBlock, IsNullValueFrame fact)
