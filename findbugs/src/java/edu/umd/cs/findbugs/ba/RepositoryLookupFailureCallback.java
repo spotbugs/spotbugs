3f798ea0f24d42ec7dd889c9bd52c2adaf16b394
David Hovemeyer <david.hovemeyer@gmail.com>
2003-06-06 13:30:09 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@854 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/RepositoryLookupFailureCallback.java
@@ -0,0 +1,49 @@
+/*
+ * Bytecode Analysis Framework
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.daveho.ba;
+
+/**
+ * An interface which Repository class lookup failures are reported to.
+ * Some of the analysis classes make use of class hierarchy information.
+ * In collecting this information, errors can result because some
+ * classes in the hierarchy can't be found; e.g., when the runtime
+ * classpath is incomplete.  When possible, the analysis classes
+ * will be conservative in the event of a lookup failure.  However,
+ * it is important to report such lookup failures to the user.
+ * So, classes that use the Repository should have a callback object
+ * to report lookup failures to.
+ *
+ * @author David Hovemeyer
+ */
+public interface RepositoryLookupFailureCallback {
+	/**
+	 * Called to report a lookup failure.
+	 * @param message a message describing the failure
+	 */
+	public void lookupFailure(String message);
+
+	/**
+	 * Called to report a lookup failure.
+	 * @param ex a ClassNotFoundException resulting from the failure
+	 */
+	public void lookupFailure(ClassNotFoundException ex);
+}
+
+// vim:ts=4
