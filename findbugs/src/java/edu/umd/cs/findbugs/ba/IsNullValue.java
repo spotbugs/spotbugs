3e72b9c92a3fbf6bdf6deca238e7754f2eda4571
David Hovemeyer <david.hovemeyer@gmail.com>
2003-06-24 00:40:54 +0000

    Added WEAK_NULL and WEAK_NN values, which are used for increasing precision for code guarded by IFNULL and IFNONNULL instructions. We don't want the more precise information to pollute downstream code (after control has merged again).
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@917 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/ba/IsNullValue.java
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/IsNullValue.java
@@ -23,28 +23,38 @@ package edu.umd.cs.daveho.ba;
  * A class to abstractly represent values in stack slots,
  * indicating whether thoses values can be null, non-null,
  * null on some incoming path, or unknown.
+ * Null and non-null values produced by flow-sensitivity are treated specially
+ * so that they don't flow out of the regions protected by the condition
+ * which makes them precise (the WEAK_NULL and WEAK_NN values).
  *
  * @see IsNullValueFrame
  * @see IsNullValueAnalysis
  * @author David Hovemeyer
  */
 public class IsNullValue {
-	private static final int NULL = 0;
-	private static final int NN   = 1;
-	private static final int NSP  = 2;
-	private static final int DNR  = 3;
+	private static final int NULL      = 0;
+	private static final int WEAK_NULL = 1;
+	private static final int NN        = 2;
+	private static final int WEAK_NN   = 3;
+	private static final int NSP       = 4;
+	private static final int DNR       = 5;
 
+	// TODO: think about this some more
 	private static final int[][] mergeMatrix = {
-		// NULL, NN,   NSP,  DNR
-		{ NULL },                    // NULL
-		{ NSP,   NN },               // NN
-		{ NSP,   DNR,  DNR },        // NSP
-		{ NSP,   DNR,  DNR,  DNR  }  // DNR
+		// NULL, WEAK_NULL, NN,   WEAK_NN, NSP,  DNR
+		{ NULL                                         }, // NULL
+		{ NULL,  WEAK_NULL                             }, // WEAK_NULL
+		{ NSP,   NN,        NN                         }, // NN
+		{ NSP,   DNR,       NN,   WEAK_NN,             }, // WEAK_NN
+		{ NSP,   DNR,       DNR,  DNR,     DNR         }, // NSP
+		{ NSP,   DNR,       DNR,  DNR,     DNR,  DNR   }  // DNR
 	};
 
 	private static IsNullValue[] instanceList = {
 		new IsNullValue(NULL),
+		new IsNullValue(WEAK_NULL),
 		new IsNullValue(NN),
+		new IsNullValue(WEAK_NN),
 		new IsNullValue(NSP),
 		new IsNullValue(DNR)
 	};
@@ -58,6 +68,12 @@ public class IsNullValue {
 	/** Get the instance representing values that are definitely null. */
 	public static IsNullValue nullValue() {
 		return instanceList[NULL];
+	}
+
+	/** Get the instance representing values which we know are null by virtual
+	    of having been made more precise by an IFNULL or IFNONNULL instruction. */
+	public static IsNullValue weakNullValue() {
+		return instanceList[WEAK_NULL];
 	}
 
 	/** Get the instance representing values that are definitely not null. */
@@ -81,6 +97,14 @@ public class IsNullValue {
 		return instanceList[DNR];
 	}
 
+	public static IsNullValue flowSensitiveNullValue(IsNullValue conditionValue) {
+		return conditionValue.kind == NULL ? instanceList[NULL] : instanceList[WEAK_NULL];
+	}
+
+	public static IsNullValue flowSensitiveNonNullValue(IsNullValue conditionValue) {
+		return conditionValue.kind == NN ? instanceList[NN] : instanceList[WEAK_NN];
+	}
+
 	/** Merge two values. */
 	public static IsNullValue merge(IsNullValue a, IsNullValue b) {
 		// Left hand value should be >=, since it is used
@@ -96,12 +120,26 @@ public class IsNullValue {
 		return instanceList[result];
 	}
 
+	/** Is this value definitely null? */
+	public boolean isDefinitelyNull() {
+		return kind == NULL || kind == WEAK_NULL;
+	}
+
+	/** Is this value null on some path? */
+	public boolean isNullOnSomePath() {
+		return kind == NSP;
+	}
+
 	public String toString() {
 		switch (kind) {
 		case NULL:
 			return "n";
+		case WEAK_NULL:
+			return "w";
 		case NN:
 			return "N";
+		case WEAK_NN:
+			return "W";
 		case NSP:
 			return "S";
 		case DNR:
