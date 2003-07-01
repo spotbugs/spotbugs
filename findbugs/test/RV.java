7f8c179de70d7b93c3fafe4273e81e1b12e465ae
Bill Pugh <bill.pugh@gmail.com>
2003-07-01 15:46:42 +0000

    Added more test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@969 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/test/RV.java
+++ findbugs/test/RV.java
@@ -1,8 +1,10 @@
-public class RV {
+public class RV extends Thread {
 	public static void main(String[] argv) {
 		String s = new String("Hello");
 		s.trim();
+		new RV();
 	}
+
 
 	public void f(java.security.MessageDigest d, byte[] b) {
 		d.digest(b);
