537e0259fa64f712ab8105788ff9cf65434937f7
David Hovemeyer <david.hovemeyer@gmail.com>
2003-07-03 15:24:49 +0000

    Two streams, one closed and the other not
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1008 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/test/OpenStream.java
+++ findbugs/test/OpenStream.java
@@ -2,10 +2,23 @@ import java.io.*;
 
 public class OpenStream {
 	public static void main(String[] argv) throws Exception {
-		FileInputStream in = new FileInputStream(argv[0]);
+		FileInputStream in = null;
 
-		if (Boolean.getBoolean("foobar")) {
-			in.close();
+		try {
+			in = new FileInputStream(argv[0]);
+		} finally {
+			// Not guaranteed to be closed here!
+			if (Boolean.getBoolean("inscrutable"))
+				in.close();
+		}
+
+		FileInputStream in2 = null;
+		try {
+			in2 = new FileInputStream(argv[1]);
+		} finally {
+			// This one will be closed
+			if (in2 != null)
+				in2.close();
 		}
 
 		// oops!  exiting the method without closing the stream
