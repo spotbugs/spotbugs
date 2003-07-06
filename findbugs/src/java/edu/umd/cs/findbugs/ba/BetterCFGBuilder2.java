aed42ca3ce5218ae6338ed2d3d3434018f1afab7
David Hovemeyer <david.hovemeyer@gmail.com>
2003-07-06 16:52:04 +0000

    Minor tweaks.  Appears to be constructing top level context correctly.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1017 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/ba/BetterCFGBuilder2.java
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/BetterCFGBuilder2.java
@@ -1,3 +1,22 @@
+/*
+ * Bytecode Analysis Framework
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 package edu.umd.cs.daveho.ba;
 
 import java.util.*;
@@ -103,6 +122,7 @@ public class BetterCFGBuilder2 implements CFGBuilder, EdgeTypes {
 			if (block == null) {
 				block = allocateBasicBlock();
 				blockMap.put(start, block);
+				addItem(new WorkListItem(start, block));
 			}
 			return block;
 		}
@@ -191,6 +211,7 @@ public class BetterCFGBuilder2 implements CFGBuilder, EdgeTypes {
 			// Add exception handler block (ETB) for exception-throwing instructions
 			if (isPEI(handle)) {
 				handleExceptions(context, handle, basicBlock);
+				basicBlock.setExceptionThrower(handle);
 				BasicBlock body = context.allocateBasicBlock();
 				context.addEdge(basicBlock, body, FALL_THROUGH_EDGE);
 				basicBlock = body;
