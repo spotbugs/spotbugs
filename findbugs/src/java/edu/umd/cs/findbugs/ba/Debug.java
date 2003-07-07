4449ef9d0e32ce55844c4edaa99c096426dd82e3
David Hovemeyer <david.hovemeyer@gmail.com>
2003-07-07 13:29:56 +0000

    Use a property to set VERIFY_INTEGRITY.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1027 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/ba/Debug.java
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/Debug.java
@@ -24,5 +24,5 @@ public interface Debug {
      * Set this to true to enable data structure integrity checks.
      * These checks are somewhat expensive, but hey, computers are fast.
      */
-    public static final boolean VERIFY_INTEGRITY = false /*true*/;
+    public static final boolean VERIFY_INTEGRITY = Boolean.getBoolean("cfg.verifyIntegrity");
 }
