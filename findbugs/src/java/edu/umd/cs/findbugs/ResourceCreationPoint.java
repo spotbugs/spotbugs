535b47e8d695aa14fdc79bc6966907aa8afb7874
David Hovemeyer <david.hovemeyer@gmail.com>
2003-07-09 16:30:37 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1060 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ResourceCreationPoint.java
@@ -0,0 +1,56 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs;
+
+import edu.umd.cs.daveho.ba.Location;
+
+/**
+ * A resource creation point.
+ * This serves as an embodiment of the resource for use with ResourceValueAnalysis.
+ *
+ * @see ResourceValueAnalysis
+ * @see ResourceTrackingDetector
+ * @author David Hovemeyer
+ */
+public class ResourceCreationPoint {
+	/** Location in the method where the resource is created. */
+	private final Location location;
+
+	/** The type of the resource. */
+	private final String resourceClass;
+
+	/**
+	 * Constructor.
+	 * @param location location where resource is created
+	 * @param resourceClass the name of the resource's class
+	 */
+	public ResourceCreationPoint(Location location, String resourceClass) {
+		this.location = location;
+		this.resourceClass = resourceClass;
+	}
+
+	/** Get the location where the resource is created. */
+	public Location getLocation() { return location; }
+
+	/** Get the name of the resource's class. */
+	public String getResourceClass() { return resourceClass; }
+}
+
+// vim:ts=4
