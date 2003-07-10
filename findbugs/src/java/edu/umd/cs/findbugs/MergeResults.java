9d97ccc0abfa7bdb9b961d83872ff3dc4556b2ee
David Hovemeyer <david.hovemeyer@gmail.com>
2003-07-10 13:38:28 +0000

    Take name of output file as an argument.  Print number of annotations preserved, lost, and lost with annotations.  Everything seems to be completely working now.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1073 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/MergeResults.java
+++ findbugs/src/java/edu/umd/cs/findbugs/MergeResults.java
@@ -21,16 +21,26 @@ package edu.umd.cs.findbugs;
 
 import java.util.*;
 
+/**
+ * Merge a saved results file (containing annotations) with a new results file.
+ * This is useful when re-running FindBugs after changing the detectors
+ * (e.g., to fix false positives).  All of the annotations from the original
+ * run for bugs still present in the new run are preserved in the output file
+ * (whose bugs are identical to the new run).  Note that some annotations
+ * can be lost, if those bugs are not present in the new run.
+ *
+ * @author David Hovemeyer
+ */
 public class MergeResults {
 	public static void main(String[] argv) throws Exception {
-
-		if (argv.length != 2) {
-			System.err.println("Usage: " + MergeResults.class.getName() + " <orig results> <new results>");
+		if (argv.length != 3) {
+			System.err.println("Usage: " + MergeResults.class.getName() + " <orig results> <new results> <output file>");
 			System.exit(1);
 		}
 
 		String origResultsFile = argv[0];
 		String newResultsFile = argv[1];
+		String outputFile = argv[2];
 
 		HashMap<String, String> classToSourceFileMap = new HashMap<String, String>();
 
@@ -40,17 +50,28 @@ public class MergeResults {
 		origCollection.readXML(origResultsFile, new HashMap<String,String>());
 		newCollection.readXML(newResultsFile, classToSourceFileMap);
 
+		int numPreserved = 0;
+		int numLost = 0;
+		int numLostWithAnnotations = 0;
+
 		Iterator<BugInstance> i = origCollection.iterator();
 		while (i.hasNext()) {
 			BugInstance orig = i.next();
 			if (newCollection.contains(orig)) {
 				BugInstance matching = newCollection.getMatching(orig);
 				matching.setAnnotationText(orig.getAnnotationText());
+				numPreserved++;
+			} else {
+				numLost++;
+				if (!orig.getAnnotationText().equals(""))
+					numLostWithAnnotations++;
 			}
 		}
 
-		newCollection.writeXML(System.out, classToSourceFileMap);
+		System.out.println(numPreserved + " preserved, " +
+			numLost + " lost (" + numLostWithAnnotations + " lost with annotations)");
 
+		newCollection.writeXML(outputFile, classToSourceFileMap);
 	}
 }
 
