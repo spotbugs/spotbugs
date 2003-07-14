c75f64d39afa74563f89985174a8bc4257f3ae76
Bill Pugh <bill.pugh@gmail.com>
2003-07-14 18:12:06 +0000

    Added detector for comparing String values against a constant using == or =!
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1082 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/EqStringTest.java
@@ -0,0 +1,75 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.detect;
+import edu.umd.cs.findbugs.*;
+import java.util.*;
+import java.io.PrintStream;
+import org.apache.bcel.classfile.*;
+import java.util.zip.*;
+import java.io.*;
+
+import edu.umd.cs.pugh.visitclass.DismantleBytecode;
+import edu.umd.cs.pugh.visitclass.Constants2;
+
+public class EqStringTest extends BytecodeScanningDetector implements   Constants2 {
+    boolean constantOnTOS = false;
+    boolean callToInternSeen = false;
+    private BugReporter bugReporter;
+    String stringOnTop;
+
+  public EqStringTest(BugReporter bugReporter) {
+	this.bugReporter = bugReporter;
+  }
+
+  public void visit(JavaClass obj)     {
+	super.visit(obj);
+	}
+
+    public void visit(Method obj) {
+        super.visit(obj);
+        constantOnTOS = false;
+        callToInternSeen = false;
+	}
+
+
+    public void sawOpcode(int seen) {
+
+	switch (seen) {
+	case LDC:
+		constantOnTOS = true;
+		stringOnTop = stringConstant;
+		return;
+ 	case INVOKEVIRTUAL:
+		if (refConstant.equals("java.lang.String.intern : ()Ljava.lang.String;"))
+			callToInternSeen = true;
+		break;
+ 	case IF_ACMPEQ:
+	case IF_ACMPNE:
+		if (constantOnTOS && !callToInternSeen) 
+                  bugReporter.reportBug(new BugInstance("COMPARING_STRINGS_WITH_EQ", NORMAL_PRIORITY)
+                        .addClassAndMethod(this)
+                        .addSourceLine(this, PC));
+		break;
+	default: 
+		break;
+	}
+	constantOnTOS = false;
+	}	
+}
---
+++ findbugs/test/StringComparision.java
@@ -0,0 +1,14 @@
+
+
+class StringComparision {
+	int x,y;
+	public String toString() {
+		return x + "," + y;
+		}
+	public boolean isOrigin() {
+		return toString() == "0,0";
+		}
+	public boolean betterIsOrigin() {
+		return toString().intern() == "0,0";
+		}
+	}
