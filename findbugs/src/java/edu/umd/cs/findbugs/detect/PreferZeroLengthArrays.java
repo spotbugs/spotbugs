0948818eaa00ac0019337e4f3b6a21b3c679464d
Bill Pugh <bill.pugh@gmail.com>
2003-07-15 18:41:00 +0000

    Check for methods with array return type that return null
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1085 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/PreferZeroLengthArrays.java
@@ -0,0 +1,63 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.detect;
+import edu.umd.cs.findbugs.*;
+import java.util.*;
+import java.io.PrintStream;
+import org.apache.bcel.classfile.*;
+import java.util.zip.*;
+import java.io.*;
+
+import edu.umd.cs.pugh.visitclass.DismantleBytecode;
+import edu.umd.cs.pugh.visitclass.Constants2;
+
+public class PreferZeroLengthArrays extends BytecodeScanningDetector implements   Constants2 {
+    boolean nullOnTOS = false;
+    private BugReporter bugReporter;
+
+  public PreferZeroLengthArrays(BugReporter bugReporter) {
+	this.bugReporter = bugReporter;
+  }
+
+   public void visit(Code obj) {
+	String returnType = methodSig.substring(methodSig.indexOf(")")+1);
+	if (returnType.startsWith("[")) {
+		nullOnTOS = false;
+		super.visit(obj);
+		}
+	}
+
+
+    public void sawOpcode(int seen) {
+
+	switch (seen) {
+	case ACONST_NULL:
+		nullOnTOS = true;
+		return;
+	case ARETURN:
+		if (nullOnTOS) 
+                  bugReporter.reportBug(new BugInstance("PZLA_PREFER_ZERO_LENGTH_ARRAYS", LOW_PRIORITY)
+                        .addClassAndMethod(this)
+                        .addSourceLine(this, PC));
+		break;
+	}
+	nullOnTOS = false;
+	}
+}
