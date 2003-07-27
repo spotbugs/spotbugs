9affd3a739ab05bae65791336f21c93673630d49
David Hovemeyer <david.hovemeyer@gmail.com>
2003-07-27 19:08:09 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1160 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/DelegatingBugReporter.java
@@ -0,0 +1,70 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs;
+
+public class DelegatingBugReporter implements BugReporter {
+	private BugReporter realBugReporter;
+
+	public DelegatingBugReporter(BugReporter realBugReporter) {
+		this.realBugReporter = realBugReporter;
+	}
+
+	public BugReporter getRealBugReporter() {
+		return realBugReporter;
+	}
+
+	public void setRealBugReporter(BugReporter realBugReporter) {
+		this.realBugReporter = realBugReporter;
+	}
+
+	public void setErrorVerbosity(int level) {
+		realBugReporter.setErrorVerbosity(level);
+	}
+
+	public void reportBug(BugInstance bugInstance) {
+		realBugReporter.reportBug(bugInstance);
+	}
+
+	public void logError(String message) {
+		realBugReporter.logError(message);
+	}
+
+	public void reportMissingClass(ClassNotFoundException ex) {
+		realBugReporter.reportMissingClass(ex);
+	}
+
+	public void mapClassToSource(String className, String sourceFileName) {
+		realBugReporter.mapClassToSource(className, sourceFileName);
+	}
+
+	public String getSourceForClass(String className) {
+		return realBugReporter.getSourceForClass(className);
+	}
+
+	public void finish() {
+		realBugReporter.finish();
+	}
+
+	public void reportQueuedErrors() {
+		realBugReporter.reportQueuedErrors();
+	}
+}
+
+// vim:ts=4
