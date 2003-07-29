ed615fe3d8246bb87d2eaa8c532c3d9cb09972e8
David Hovemeyer <david.hovemeyer@gmail.com>
2003-07-29 18:02:54 +0000

    Updated for 0.6.3
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1169 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/Version.java
+++ findbugs/src/java/edu/umd/cs/findbugs/Version.java
@@ -30,13 +30,13 @@ public class Version {
 	public static final int MINOR = 6;
 
 	/** Patch level. */
-	public static final int PATCHLEVEL = 2;
+	public static final int PATCHLEVEL = 3;
 
 	/** Release version string. */
 	public static final String RELEASE = MAJOR + "." + MINOR + "." + PATCHLEVEL;
 
 	/** Release date. */
-	public static final String DATE = "July 25, 2003";
+	public static final String DATE = "August 4, 2003";
 
 	public static void main(String[] argv) {
 		if (argv.length != 1)
