0761eea1c22ae98897d9d8a1598d5402c8173292
David Hovemeyer <david.hovemeyer@gmail.com>
2003-07-29 18:04:16 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1170 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/FindUnreleasedLock.java
@@ -0,0 +1,173 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.detect;
+
+import java.util.*;
+import edu.umd.cs.findbugs.*;
+import edu.umd.cs.daveho.ba.*;
+import org.apache.bcel.Constants;
+import org.apache.bcel.Repository;
+import org.apache.bcel.classfile.*;
+import org.apache.bcel.generic.*;
+
+class Lock extends ResourceCreationPoint {
+	public Lock(Location location, String lockClass) {
+		super(location, lockClass);
+	}
+}
+
+public class FindUnreleasedLock extends ResourceTrackingDetector<Lock> {
+
+	/* ----------------------------------------------------------------------
+	 * Helper classes
+	 * ---------------------------------------------------------------------- */
+
+	private static class LockFrameModelingVisitor extends ResourceValueFrameModelingVisitor {
+		private LockResourceTracker resourceTracker;
+		private Lock lock;
+
+		public LockFrameModelingVisitor(ConstantPoolGen cpg, LockResourceTracker resourceTracker, Lock lock) {
+			super(cpg);
+			this.resourceTracker = resourceTracker;
+			this.lock = lock;
+		}
+
+		public void transferInstruction(InstructionHandle handle, BasicBlock basicBlock) throws DataflowAnalysisException {
+			final Instruction ins = handle.getInstruction();
+			final ConstantPoolGen cpg = getCPG();
+			final ResourceValueFrame frame = getFrame();
+
+			int status = -1;
+
+			// Is a lock acquired or released by this instruction?
+			Location creationPoint = lock.getLocation();
+			if (handle == creationPoint.getHandle() && basicBlock == creationPoint.getBasicBlock()) {
+				status = ResourceValueFrame.OPEN;
+			} else if (resourceTracker.isResourceClose(basicBlock, handle, cpg, lock, frame)) {
+				status = ResourceValueFrame.CLOSED;
+			}
+
+			// Model use of instance values in frame slots
+			ins.accept(this);
+
+			// If needed, update frame status
+			if (status != -1) {
+				frame.setStatus(status);
+				if (status == ResourceValueFrame.CREATED)
+					frame.setValue(frame.getNumSlots() - 1, ResourceValue.instance());
+			}
+		}
+
+		protected boolean instanceEscapes(InvokeInstruction inv, int instanceArgNum) {
+			return false;
+		}
+	}
+
+	private static class LockResourceTracker implements ResourceTracker<Lock> {
+		private RepositoryLookupFailureCallback lookupFailureCallback;
+
+		public LockResourceTracker(RepositoryLookupFailureCallback lookupFailureCallback) {
+			this.lookupFailureCallback = lookupFailureCallback;
+		}
+
+		public Lock isResourceCreation(BasicBlock basicBlock, InstructionHandle handle, ConstantPoolGen cpg) {
+			Instruction ins = handle.getInstruction();
+			if (!(ins instanceof INVOKEVIRTUAL))
+				return null;
+			INVOKEVIRTUAL inv = (INVOKEVIRTUAL) ins;
+
+			String className = inv.getClassName(cpg);
+			String methodName = inv.getName(cpg);
+			String methodSig = inv.getSignature(cpg);
+
+			try {
+				if (Repository.instanceOf(className, "java.util.concurrent.Lock") &&
+					methodName.equals("lock") &&
+					methodSig.equals("()V")) {
+					return new Lock(new Location(handle, basicBlock), className);
+				}
+			} catch (ClassNotFoundException e) {
+				lookupFailureCallback.reportMissingClass(e);
+			}
+			return null;
+		}
+
+		public boolean isResourceClose(BasicBlock basicBlock, InstructionHandle handle, ConstantPoolGen cpg, Lock resource,
+			ResourceValueFrame frame) throws DataflowAnalysisException {
+			Instruction ins = handle.getInstruction();
+			if (!(ins instanceof INVOKEVIRTUAL))
+				return false;
+			INVOKEVIRTUAL inv = (INVOKEVIRTUAL) ins;
+
+			String className = inv.getClassName(cpg);
+			String methodName = inv.getName(cpg);
+			String methodSig = inv.getSignature(cpg);
+
+			ResourceValue topValue = frame.getTopValue();
+			if (!topValue.isInstance())
+				return false;
+
+			try {
+				if (Repository.instanceOf(className, "java.util.concurrent.Lock") &&
+					methodName.equals("unlock") &&
+					methodSig.equals("()V")) {
+					return true;
+				}
+			} catch (ClassNotFoundException e) {
+				lookupFailureCallback.reportMissingClass(e);
+			}
+
+			return false;
+		}
+
+		public ResourceValueFrameModelingVisitor createVisitor(Lock resource, ConstantPoolGen cpg) {
+			return new LockFrameModelingVisitor(cpg, this, resource);
+		}
+	}
+
+	/* ----------------------------------------------------------------------
+	 * Fields
+	 * ---------------------------------------------------------------------- */
+
+	private BugReporter bugReporter;
+
+	/* ----------------------------------------------------------------------
+	 * Implementation
+	 * ---------------------------------------------------------------------- */
+
+	public FindUnreleasedLock(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
+
+	public boolean prescreen(ClassContext classContext, Method method) {
+		BitSet bytecodeSet = classContext.getBytecodeSet(method);
+		return bytecodeSet.get(Constants.NEW);
+	}
+
+	public ResourceTracker<Lock> getResourceTracker() {
+		return new LockResourceTracker(bugReporter);
+	}
+
+	public void inspectResult(JavaClass javaClass, MethodGen methodGen, CFG cfg,
+		Dataflow<ResourceValueFrame> dataflow, Lock resource) {
+	}
+}
+
+// vim:ts=4
