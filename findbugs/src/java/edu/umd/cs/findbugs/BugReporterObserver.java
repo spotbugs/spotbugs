4965237f5ea880332aaf6681c5923676be246f67
David Hovemeyer <david.hovemeyer@gmail.com>
2003-07-30 14:25:50 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1190 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/BugReporterObserver.java
@@ -0,0 +1,40 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs;
+
+/**
+ * Observer to determine when a BugReporter reports a bug.
+ * By adding an observer, it is possible to determine which
+ * bugs a BugReporter is actually reporting, because due to
+ * filtering, priorities, etc., not all bugs sent to a BugReporter
+ * will actually be processed.
+ *
+ * @see BugReporter
+ * @author David Hovemeyer
+ */
+public interface BugReporterObserver {
+	/**
+	 * Called when a BugReporter reports a bug.
+	 * @param bugInstance the BugInstance
+	 */
+	public void reportBug(BugInstance bugInstance);
+}
+
+// vim:ts=4
