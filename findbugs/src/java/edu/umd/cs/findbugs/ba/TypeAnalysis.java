d7763f204d9f79f1401198635875a6ebef62f9b9
David Hovemeyer <david.hovemeyer@gmail.com>
2003-08-03 18:28:51 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1217 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/TypeAnalysis.java
@@ -0,0 +1,124 @@
+/*
+ * Bytecode Analysis Framework
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.daveho.ba;
+
+import java.util.*;
+
+// We require BCEL 5.0 or later.
+import org.apache.bcel.*;
+import org.apache.bcel.classfile.*;
+import org.apache.bcel.generic.*;
+
+/**
+ * A forward dataflow analysis to determine the types of all values
+ * in the Java stack frame at all points in a Java method.
+ * The values include local variables and values on the Java operand stack.
+ *
+ * @see Dataflow
+ * @see DataflowAnalysis
+ * @see TypeFrame
+ * @author David Hovemeyer
+ */
+public class TypeAnalysis extends ForwardDataflowAnalysis<TypeFrame> {
+	private MethodGen methodGen;
+	private RepositoryLookupFailureCallback lookupFailureCallback;
+
+	/**
+	 * Constructor.
+	 * @param methodGen the MethodGen whose CFG we'll be analyzing
+	 */
+	public TypeAnalysis(MethodGen methodGen, RepositoryLookupFailureCallback lookupFailureCallback) {
+		this.methodGen = methodGen;
+		this.lookupFailureCallback = lookupFailureCallback;
+	}
+
+	public TypeFrame createFact() {
+		return new TypeFrame(methodGen.getMaxLocals(), lookupFailureCallback);
+	}
+
+	public void initEntryFact(TypeFrame result) {
+		int slot = 0;
+
+		// Clear the stack slots in the frame
+		result.clearStack();
+
+		// Add local for "this" pointer, if present
+		if (!methodGen.isStatic())
+			result.setValue(slot++, new ObjectType(methodGen.getClassName()));
+
+		// Add locals for parameters
+		Type[] argumentTypes = methodGen.getArgumentTypes();
+		for (int i = 0; i < argumentTypes.length; ++i)
+			result.setValue(slot++, argumentTypes[i]);
+
+		// Set remaining parameters to BOTTOM; this will cause any
+		// uses of them to be flagged
+		while (slot < methodGen.getMaxLocals())
+			result.setValue(slot++, TypeFrame.getBottomType());
+	}
+
+	public void copy(TypeFrame source, TypeFrame dest) {
+		dest.copyFrom(source);
+	}
+
+	public void initResultFact(TypeFrame result) {
+		// This is important.  Sometimes we need to use a result value
+		// before having a chance to initialize it.  We don't want such
+		// values to corrupt other TypeFrame values that we merge them with.
+		// So, all result values must be TOP.
+		result.setTop();
+	}
+
+	public void makeFactTop(TypeFrame fact) {
+		fact.setTop();
+	}
+
+	public boolean isFactValid(TypeFrame fact) {
+		return fact.isValid();
+	}
+
+	public boolean same(TypeFrame fact1, TypeFrame fact2) {
+		return fact1.sameAs(fact2);
+	}
+
+	public void transferInstruction(InstructionHandle handle, BasicBlock basicBlock, TypeFrame fact) throws DataflowAnalysisException {
+		TypeFrameModelingVisitor visitor = new TypeFrameModelingVisitor(fact, methodGen.getConstantPool());
+		handle.getInstruction().accept(visitor);
+	}
+
+	public void meetInto(TypeFrame fact, Edge edge, TypeFrame result) throws DataflowAnalysisException {
+		BasicBlock basicBlock = edge.getDest();
+		if (basicBlock.isExceptionHandler() && fact.isValid()) {
+			// Special case: when merging predecessor facts for entry to
+			// an exception handler, we clear the stack and push a
+			// single entry for the exception object.  That way, the locals
+			// can still be merged.
+			CodeExceptionGen exceptionGen = basicBlock.getExceptionGen(); 
+			TypeFrame tmpFact = createFact();
+			tmpFact.copyFrom(fact);
+			tmpFact.clearStack();
+			tmpFact.pushValue(exceptionGen.getCatchType());
+			fact = tmpFact;
+		}
+		result.mergeWith(fact);
+	}
+}
+
+// vim:ts=4
