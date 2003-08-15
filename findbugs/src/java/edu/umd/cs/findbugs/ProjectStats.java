afa1379ad9f70d4c9a2db80083a9f98975137b61
David Hovemeyer <david.hovemeyer@gmail.com>
2003-08-15 19:26:25 +0000

    Split out from FindBugsSummaryStats detector.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1335 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ProjectStats.java
@@ -0,0 +1,93 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2003, Mike Fagan <mfagan@tde.com>
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs;
+
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.dom4j.Element;
+import org.dom4j.Branch;
+
+public class ProjectStats implements XMLConvertible {
+  private HashMap<String, PackageStats> packageStatsMap;
+  private int totalErrors;
+  private int totalClasses;
+  private Date date;
+
+  public ProjectStats() {
+    this.packageStatsMap = new HashMap<String, PackageStats>();
+    this.totalErrors = 0;
+    this.totalClasses = 0;
+    this.date = new Date();
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  public void addClass(String className, boolean isInterface) {
+    String packageName;
+    int lastDot = className.lastIndexOf('.');
+    if (lastDot < 0)
+      packageName = "";
+    else
+      packageName = className.substring(0, lastDot);
+    PackageStats stat = getPackageStats( packageName );
+    stat.addClass( className, isInterface );
+    totalClasses++;
+  }
+
+  public void addBug(BugInstance bug) {
+    PackageStats stat = getPackageStats( bug.getPrimaryClass().getPackageName() );
+    stat.addError( bug );
+    totalErrors++;
+  }
+
+  public Element toElement(Branch parent) {
+     Element root = parent.addElement("FindBugsSummary");
+     DateFormat df = new SimpleDateFormat( "EEE, d MMM yyyy HH:mm:ss Z" );
+     root.addAttribute("timestamp", df.format(date ));
+     root.addAttribute( "total_classes", String.valueOf( totalClasses ) );
+     root.addAttribute( "total_bugs", String.valueOf( totalErrors ) );
+     root.addAttribute( "num_packages", String.valueOf( packageStatsMap.size() ) );
+
+     Iterator<PackageStats> i = packageStatsMap.values().iterator();
+     while( i.hasNext() ) {
+       PackageStats stats = i.next();
+       stats.toElement( root );
+     } 
+
+     return root;
+  }
+
+  private PackageStats getPackageStats( String packageName ) {
+    PackageStats stat = packageStatsMap.get( packageName );
+    if ( stat == null ) {
+      stat = new PackageStats( packageName );
+      packageStatsMap.put( packageName, stat );
+    }
+    return stat;
+  }
+}
