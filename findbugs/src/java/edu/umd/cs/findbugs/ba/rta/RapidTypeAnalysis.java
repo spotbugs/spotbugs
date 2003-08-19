cc87f396070338ee618948cec5a048d92b01736b
David Hovemeyer <david.hovemeyer@gmail.com>
2003-08-19 19:39:57 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1367 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/rta/RapidTypeAnalysis.java
@@ -0,0 +1,52 @@
+/*
+ * Bytecode Analysis Framework
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.daveho.ba.rta;
+
+import edu.umd.cs.daveho.ba.*;
+import java.util.*;
+import org.apache.bcel.classfile.JavaClass;
+
+/**
+ * Driver for performing Rapid Type Analysis (RTA) on a collection of
+ * classes.  RTA is an algorithm devised by David Bacon to compute
+ * an accurate call graph for an object-oriented program.
+ */
+public class RapidTypeAnalysis implements ClassObserver {
+	// Set of classes observed.
+	private HashSet<JavaClass> observedClassSet;
+
+	/**
+	 * Constructor.
+	 */
+	public RapidTypeAnalysis() {
+		this.observedClassSet = new HashSet<JavaClass>();
+	}
+
+	public void execute() {
+		// TODO: implement
+	}
+
+	public void observeClass(JavaClass javaClass) {
+		observedClassSet.add(javaClass);
+	}
+
+}
+
+// vim:ts=4
