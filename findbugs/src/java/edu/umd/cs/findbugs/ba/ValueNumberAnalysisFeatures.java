ce399b150da54c3d8b8192b190e7f6e8823176b7
David Hovemeyer <david.hovemeyer@gmail.com>
2003-08-21 21:03:21 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1393 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/ValueNumberAnalysisFeatures.java
@@ -0,0 +1,47 @@
+/*
+ * Bytecode Analysis Framework
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.daveho.ba;
+
+/**
+ * Global flags controlling ValueNumberAnalysis.
+ */
+public interface ValueNumberAnalysisFeatures {
+	/**
+	 * When set, perform redundant load elimination and forward substitution.
+	 * Note that we do <em>not</em> do this in a correctness-preserving way!
+	 * For example, we don't kill loads when methods are called, even though
+	 * those methods could change heap values.  The intent here is simply
+	 * to try to handle situations where a field is read multiple times,
+	 * where the intent of the programmer is clearly that the loaded values
+	 * will be the same in each case.
+	 *
+	 * <p> Eventually, we might do interprocedural analysis that would allow
+	 * accurate modeling of which fields a called method could modify,
+	 * which would allow a more correct implementation.
+	 */
+	public static final boolean REDUNDANT_LOAD_ELIMINATION = Boolean.getBoolean("vna.rle");
+
+	/**
+	 * Debug redundant load elimination.
+	 */
+	public static final boolean RLE_DEBUG = Boolean.getBoolean("vna.rle.debug");
+}
+
+// vim:ts=4
