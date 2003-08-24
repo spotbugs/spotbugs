3d8521391965078d9d75448e46dfaca25cb914e3
David Hovemeyer <david.hovemeyer@gmail.com>
2003-08-24 16:29:31 +0000

    Changed to use XField instead of InstanceField.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1416 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/ba/AvailableLoad.java
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/AvailableLoad.java
@@ -19,20 +19,59 @@
 
 package edu.umd.cs.daveho.ba;
 
+/**
+ * An AvailableLoad indicates a field and (optionally) object reference
+ * for which a value is available.  It is used to implement
+ * redundant load elimination and forward substitution in ValueNumberAnalysis.
+ * The idea is that programmers often reload fields unnecessarily when they
+ * "know" that the value will not change.  In order to deduce the intended
+ * meaning of such code, our analyses need to figure out that such
+ * loads return the same value.
+ *
+ * <p> AvailableLoad objects may be used as keys in both hash and tree
+ * sets and maps.
+ *
+ * @see ValueNumberAnalysis
+ * @author David Hovemeyer
+ */
 public class AvailableLoad implements Comparable<AvailableLoad> {
 	private final ValueNumber reference;
-	private final InstanceField field;
+	private final XField field;
 
+	/**
+	 * Constructor from static field.
+	 * @param staticField the StaticField
+	 */
+	public AvailableLoad(StaticField staticField) {
+		this.reference = null;
+		this.field = staticField;
+	}
+
+	/**
+	 * Constructor from object reference and instance field.
+	 * @param reference the ValueNumber of the object reference
+	 * @param field the InstanceField
+	 */
 	public AvailableLoad(ValueNumber reference, InstanceField field) {
+		if (reference == null) throw new IllegalArgumentException();
 		this.reference = reference;
 		this.field = field;
 	}
 
+	/**
+	 * Get the ValueNumber of the object reference.
+	 * @return the ValueNumber, or null if this is a an available
+	 *   static field load
+	 */
 	public ValueNumber getReference() {
 		return reference;
 	}
 
-	public InstanceField getField() {
+	/**
+	 * Get the field for which a load is available.
+	 * @return the XField
+	 */
+	public XField getField() {
 		return field;
 	}
 
@@ -44,7 +83,7 @@ public class AvailableLoad implements Comparable<AvailableLoad> {
 	}
 
 	public int hashCode() {
-		return reference.hashCode() + field.hashCode();
+		return reference.hashCode() ^ field.hashCode();
 	}
 
 	public boolean equals(Object o) {
