819470139f16fec1779f832abaf0a67794c1a396
David Hovemeyer <david.hovemeyer@gmail.com>
2003-09-04 16:14:34 +0000

    Re-added.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1483 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/graph/GraphToolkit.java
@@ -0,0 +1,55 @@
+/*
+ * Generic graph library
+ * Copyright (C) 2000,2003 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.daveho.graph;
+
+/**
+ * An interface for creating Graph objects,
+ * and for copying auxiliary information in vertices and edges.
+ */
+public interface GraphToolkit
+	<
+	GraphType extends Graph<EdgeType, VertexType>,
+	EdgeType extends GraphEdge<EdgeType, VertexType>,
+	VertexType extends GraphVertex<VertexType>
+	> {
+
+	/**
+	 * Create a new empty graph (no vertices or edges).
+	 * @return the new graph
+	 */
+	public GraphType createGraph();
+
+	/**
+	 * Copy auxiliary information from one vertex to another.
+	 * @param source the source vertex
+	 * @param dest the destination vertex
+	 */
+	public void copyVertex(VertexType source, VertexType dest);
+
+	/**
+	 * Copy auxiliary information from one edge to another.
+	 * @param source the source edge
+	 * @param dest the destination edge
+	 */
+	public void copyEdge(EdgeType source, EdgeType dest);
+
+}
+
+// vim:ts=4
