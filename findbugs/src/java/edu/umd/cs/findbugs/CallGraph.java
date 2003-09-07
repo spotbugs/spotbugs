2df71fa96dc27998142c431e6f4b2fbf29d3926f
David Hovemeyer <david.hovemeyer@gmail.com>
2003-09-07 17:36:57 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1506 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/CallGraph.java
@@ -0,0 +1,47 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs;
+
+import edu.umd.cs.daveho.graph.AbstractGraph;
+import org.apache.bcel.classfile.Method;
+
+public class CallGraph extends AbstractGraph<CallGraphEdge, CallGraphNode> {
+	public CallGraphEdge addEdge(CallGraphNode source, CallGraphNode target, CallSite callSite) {
+		CallGraphEdge edge = createEdge(source, target);
+		edge.setCallSite(callSite);
+		return edge;
+	}
+
+	public CallGraphNode addNode(Method method) {
+		CallGraphNode node = createVertex();
+		node.setMethod(method);
+		return node;
+	}
+
+	protected CallGraphEdge createEdge(CallGraphNode source, CallGraphNode target) {
+		return new CallGraphEdge(source, target);
+	}
+
+	protected CallGraphNode createVertex() {
+		return new CallGraphNode();
+	}
+}
+
+// vim:ts=4
---
+++ findbugs/src/java/edu/umd/cs/findbugs/CallGraphEdge.java
@@ -0,0 +1,49 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs;
+
+import edu.umd.cs.daveho.graph.AbstractEdge;
+import org.apache.bcel.classfile.Method;
+
+public class CallGraphEdge extends AbstractEdge<CallGraphEdge, CallGraphNode> {
+	private CallSite callSite;
+
+	CallGraphEdge(CallGraphNode source, CallGraphNode target) {
+		super(source, target);
+	}
+
+	void setCallSite(CallSite callSite) {
+		this.callSite = callSite;
+	}
+
+	public CallSite getCallSite() {
+		return callSite;
+	}
+
+	public Method getCallingMethod() {
+		return getSource().getMethod();
+	}
+
+	public Method getCalledMethod() {
+		return getTarget().getMethod();
+	}
+}
+
+// vim:ts=4
