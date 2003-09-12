c637852389e37f4171f90aca21b32e4164eb95f6
David Hovemeyer <david.hovemeyer@gmail.com>
2003-09-12 16:26:54 +0000

    Completely reimplemented to use CFGs to ensure that we only report load/store pairs that are consecutive within the same basic block.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1552 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/FindLocalSelfAssignment.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/FindLocalSelfAssignment.java
@@ -19,67 +19,79 @@
 
 package edu.umd.cs.findbugs.detect;
 
-import edu.umd.cs.findbugs.BugInstance;
-import edu.umd.cs.findbugs.BugReporter;
-import edu.umd.cs.findbugs.BytecodeScanningDetector;
-import edu.umd.cs.findbugs.Detector;
-import org.apache.bcel.Constants;
-import org.apache.bcel.classfile.Code;
+import edu.umd.cs.daveho.ba.*;
+import edu.umd.cs.findbugs.*;
+import java.util.Iterator;
+import org.apache.bcel.classfile.*;
+import org.apache.bcel.generic.*;
 
 /**
  * Find places where a local variable is assigned to itself.
  * Suggested by Jeff Martin.
  */
-public class FindLocalSelfAssignment extends BytecodeScanningDetector implements Constants {
-
+public class FindLocalSelfAssignment implements Detector {
 	private BugReporter bugReporter;
-	private int lastLoadedLocal;
 
 	public FindLocalSelfAssignment(BugReporter bugReporter) {
 		this.bugReporter = bugReporter;
 	}
 
-	public void visit(Code obj) { 
-		lastLoadedLocal = -1;
-		super.visit(obj);
+	public void visitClassContext(ClassContext classContext) {
+		try {
+			JavaClass javaClass = classContext.getJavaClass();
+			Method[] methodList = javaClass.getMethods();
+
+			for (int i = 0; i < methodList.length; ++i) {
+				Method method = methodList[i];
+				if (classContext.getMethodGen(method) == null)
+					continue;
+
+				CFG cfg = classContext.getCFG(method);
+				for (Iterator<BasicBlock> j = cfg.blockIterator(); j.hasNext(); ) {
+					BasicBlock basicBlock = j.next();
+					analyzeBasicBlock(classContext, method, basicBlock);
+				}
+			}
+
+		} catch (CFGBuilderException e) {
+			throw new AnalysisException("FindLocalSelfAssignment caught exception: " + e, e);
+		}
 	}
 
-	public void sawOpcode(int seen) {
-		int loadedLocal = -1, storedLocal = -1;
+	private void analyzeBasicBlock(ClassContext classContext, Method method, BasicBlock basicBlock) {
+		int lastLoaded = -1;
 
-		switch (seen) {
-		case ILOAD: case LLOAD: case FLOAD: case DLOAD: case ALOAD:
-			loadedLocal = register; break;
-		case ILOAD_0: case LLOAD_0: case FLOAD_0: case DLOAD_0: case ALOAD_0:
-			loadedLocal = 0; break;
-		case ILOAD_1: case LLOAD_1: case FLOAD_1: case DLOAD_1: case ALOAD_1:
-			loadedLocal = 1; break;
-		case ILOAD_2: case LLOAD_2: case FLOAD_2: case DLOAD_2: case ALOAD_2:
-			loadedLocal = 2; break;
-		case ILOAD_3: case LLOAD_3: case FLOAD_3: case DLOAD_3: case ALOAD_3:
-			loadedLocal = 3; break;
+		for (Iterator<InstructionHandle> i = basicBlock.instructionIterator(); i.hasNext(); ) {
+			InstructionHandle handle = i.next();
+			Instruction ins = handle.getInstruction();
 
-		case ISTORE: case LSTORE: case FSTORE: case DSTORE: case ASTORE:
-			storedLocal = register; break;
-		case ISTORE_0: case LSTORE_0: case FSTORE_0: case DSTORE_0: case ASTORE_0:
-			storedLocal = 0; break;
-		case ISTORE_1: case LSTORE_1: case FSTORE_1: case DSTORE_1: case ASTORE_1:
-			storedLocal = 1; break;
-		case ISTORE_2: case LSTORE_2: case FSTORE_2: case DSTORE_2: case ASTORE_2:
-			storedLocal = 2; break;
-		case ISTORE_3: case LSTORE_3: case FSTORE_3: case DSTORE_3: case ASTORE_3:
-			storedLocal = 3; break;
+			int loaded = -1, stored = -1;
+
+			if (ins instanceof LoadInstruction) {
+				LoadInstruction load = (LoadInstruction) ins;
+				loaded = load.getIndex();
+			} else if (ins instanceof StoreInstruction) {
+				StoreInstruction store = (StoreInstruction) ins;
+				stored = store.getIndex();
+			}
+
+			if (stored >= 0 && stored == lastLoaded) {
+				JavaClass javaClass = classContext.getJavaClass();
+				MethodGen methodGen = classContext.getMethodGen(method);
+				String sourceFile = javaClass.getSourceFileName();
+
+				bugReporter.reportBug(new BugInstance("SA_LOCAL_SELF_ASSIGNMENT", NORMAL_PRIORITY)
+					.addClass(javaClass)
+					.addMethod(methodGen, sourceFile)
+					.addSourceLine(methodGen, sourceFile, handle));
+			}
+
+			lastLoaded = (loaded >= 0) ? loaded : -1;
 		}
-
-		if (storedLocal >= 0 && storedLocal == lastLoadedLocal) {
-			bugReporter.reportBug(new BugInstance("SA_LOCAL_SELF_ASSIGNMENT", NORMAL_PRIORITY)
-				.addClassAndMethod(this)
-				.addSourceLine(this));
-		}
-
-		lastLoadedLocal = (loadedLocal >= 0) ? loadedLocal : -1;
 	}
 
+	public void report() {
+	}
 }
 
 // vim:ts=4
