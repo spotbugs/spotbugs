b1ef7c5bebb057093bca2cfe3495b1fb9499fb35
David Hovemeyer <david.hovemeyer@gmail.com>
2003-09-23 00:42:49 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1629 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/CountBugs.java
@@ -0,0 +1,53 @@
+package edu.umd.cs.findbugs;
+
+
+import java.util.*;
+
+public class CountBugs {
+
+	private static HashSet<String> categorySet = new HashSet<String>();
+	private static TreeMap<String, Integer> countMap = new TreeMap<String, Integer>();
+
+	public static void main(String[] argv) throws Exception {
+		if (argv.length < 1) {
+			System.err.println("Usage: " + CountBugs.class.getName() + " <results file>");
+			System.exit(1);
+		}
+
+		DetectorFactoryCollection.instance(); // load plugins
+
+		int arg = 0;
+		if (argv[0].equals("-categories")) {
+			StringTokenizer tok = new StringTokenizer(argv[1], ",");
+			while (tok.hasMoreTokens())
+				categorySet.add(tok.nextToken());
+			arg = 2;
+		}
+
+		String filename = argv[arg];
+
+		BugCollection bugCollection = new SortedBugCollection();
+		bugCollection.readXML(filename, new Project());
+
+		Iterator<BugInstance> i = bugCollection.iterator();
+		while (i.hasNext()) {
+			BugInstance bugInstance = i.next();
+			String category = bugInstance.getAbbrev();
+
+			if (categorySet.size() > 0 && !categorySet.contains(category))
+				continue;
+
+			Integer count = countMap.get(category);
+			if (count == null)
+				count = new Integer(0);
+			countMap.put(category, new Integer(count.intValue() + 1));
+		}
+
+		Iterator<Map.Entry<String, Integer>> j = countMap.entrySet().iterator();
+		while(j.hasNext()) {
+			Map.Entry<String,Integer> entry = j.next();
+			System.out.println(entry.getKey() + ": " + entry.getValue());
+		}
+
+	}
+}
