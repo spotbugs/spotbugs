34c589124c696818b69d80f251a3a513cc41290c
Bill Pugh <bill.pugh@gmail.com>
2003-10-07 00:41:27 +0000

    Some test cases for null dereference
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1647 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/test/NullDeref3.java
@@ -0,0 +1,25 @@
+
+
+class NullDeref3 {
+  // A Null pointer bug from 
+  //    Jakarta commons-modeler-1.1
+  //    org/apache/commons/modeler/util/IntrospectionUtils.java
+  static boolean same1(int a[], int b[]) {
+	if (a == null) 
+	  if (b == null || b.length == 0) return true;
+	if (b == null) 
+	  if (a == null || a.length == 0) return true;
+	// Bug is here. If one but not both of a & b are null,
+	// we will get a null pointer exception
+	if (a.length != b.length) return false;	
+	return true;
+	}
+  // A null pointer bug from commons-collection-2.1
+  //   org/apache/commons/collections/CollectionUtils.java
+  static boolean same2(Object a, Object b) {
+	if ((null == a && null == b) || a.equals(b))
+		return true;
+	else
+		return false;
+	}
+ }
