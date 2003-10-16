381d3a1503c2c2975b43651c44d276ebb63f5d53
David Hovemeyer <david.hovemeyer@gmail.com>
2003-10-16 17:50:18 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1713 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/ReturnPath.java
@@ -0,0 +1,83 @@
+/*
+ * Bytecode Analysis Framework
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.daveho.ba;
+
+public class ReturnPath {
+	/** Top value. */
+	public static final int TOP = 0;
+	/** Method "returns" by exiting the process. */
+	public static final int EXIT = 1;
+	/** Method returns by throwing an unhandled exception. */
+	public static final int UE = 2;
+	/** Method returns either by exiting or throwing an unhandled exception. */
+	public static final int EXIT_UE = 3;
+	/** Method may return normally. */
+	public static final int RETURNS = 4;
+
+	private int kind;
+
+	public ReturnPath(int kind) {
+		this.kind = kind;
+	}
+
+	public int getKind() {
+		return kind;
+	}
+
+	public void setKind(int kind) {
+		this.kind = kind;
+	}
+
+	public void copyFrom(ReturnPath other) {
+		this.kind = other.kind;
+	}
+
+	public boolean sameAs(ReturnPath other) {
+		return this.kind == other.kind;
+	}
+
+	private static final int[][] mergeMatrix = {
+		// TOP      EXIT      UE       EXIT_UE   RETURNS
+		{  TOP,                                          }, // TOP
+		{  EXIT,    EXIT,                                }, // EXIT
+		{  UE,      EXIT_UE,  UE,                        }, // UE
+		{  EXIT_UE, EXIT_UE,  EXIT_UE, EXIT_UE,          }, // EXIT_UE
+		{  RETURNS, RETURNS,  RETURNS, RETURNS, RETURNS  }, // RETURNS
+	};
+
+	public void mergeWith(ReturnPath other) {
+		int max = Math.max(this.kind, other.kind);
+		int min = Math.min(this.kind, other.kind);
+		this.kind = mergeMatrix[max][min];
+	}
+
+	public String toString() {
+		switch (kind) {
+		case TOP: return "[TOP]";
+		case EXIT: return "[EXIT]";
+		case UE: return "[UE]";
+		case EXIT_UE: return "[EXIT_UE]";
+		case RETURNS: return "[RETURNS]";
+		default: throw new IllegalStateException();
+		}
+	}
+}
+
+// vim:ts=4
---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/ReturnPathAnalysis.java
@@ -0,0 +1,92 @@
+/*
+ * Bytecode Analysis Framework
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.daveho.ba;
+
+import org.apache.bcel.classfile.Method;
+import org.apache.bcel.generic.InstructionHandle;
+
+public class ReturnPathAnalysis extends ForwardDataflowAnalysis<ReturnPath> implements EdgeTypes {
+	public ReturnPathAnalysis(DepthFirstSearch dfs) {
+		super(dfs);
+	}
+
+	public ReturnPath createFact() {
+		return new ReturnPath(ReturnPath.TOP);
+	}
+
+	public void copy(ReturnPath source, ReturnPath dest) {
+		dest.copyFrom(source);
+	}
+
+	public void initEntryFact(ReturnPath fact) {
+		fact.setKind(ReturnPath.RETURNS);
+	}
+
+	public void initResultFact(ReturnPath result) {
+		result.setKind(ReturnPath.TOP);
+	}
+
+	public void makeFactTop(ReturnPath fact) {
+		fact.setKind(ReturnPath.TOP);
+	}
+
+	public boolean same(ReturnPath fact1, ReturnPath fact2) {
+		return fact1.sameAs(fact2);
+	}
+
+	public void transferInstruction(InstructionHandle handle, BasicBlock basicBlock, ReturnPath fact)
+		throws DataflowAnalysisException {
+		// Nothing to do
+	}
+
+	public boolean isFactValid(ReturnPath fact) { return true; }
+
+	public void meetInto(ReturnPath fact, Edge edge, ReturnPath result) throws DataflowAnalysisException {
+		switch (edge.getType()) {
+		case UNHANDLED_EXCEPTION_EDGE:
+			fact = new ReturnPath(ReturnPath.UE);
+			break;
+		case EXIT_EDGE:
+			fact = new ReturnPath(ReturnPath.EXIT);
+			break;
+		}
+
+		result.mergeWith(fact);
+	}
+
+	public static void main(String[] argv) throws Exception {
+		if (argv.length != 1) {
+			System.err.println("Usage: " + ReturnPathAnalysis.class.getName() + " <classfile>");
+			System.exit(1);
+		}
+
+		DataflowTestDriver<ReturnPath, ReturnPathAnalysis> driver = new DataflowTestDriver<ReturnPath, ReturnPathAnalysis>() {
+			public Dataflow<ReturnPath, ReturnPathAnalysis>
+				createDataflow(ClassContext classContext, Method method)
+				throws CFGBuilderException, DataflowAnalysisException {
+				return classContext.getReturnPathDataflow(method);
+			}
+		};
+
+		driver.execute(argv[0]);
+	}
+}
+
+// vim:ts=4
