e9ed371d2133874325f497eca1413d7b0a89190c
Bill Pugh <bill.pugh@gmail.com>
2003-11-02 04:23:06 +0000

    Check for self assignment of fields
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1765 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/FindFieldSelfAssignment.java
@@ -0,0 +1,88 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.detect;
+import edu.umd.cs.findbugs.*;
+import java.util.*;
+import java.io.PrintStream;
+import org.apache.bcel.classfile.*;
+import java.util.zip.*;
+import java.io.*;
+
+import edu.umd.cs.pugh.visitclass.DismantleBytecode;
+import edu.umd.cs.pugh.visitclass.Constants2;
+
+public class FindFieldSelfAssignment extends BytecodeScanningDetector implements   Constants2 {
+    private BugReporter bugReporter;
+    int state;
+
+
+  public FindFieldSelfAssignment(BugReporter bugReporter) {
+	this.bugReporter = bugReporter;
+  }
+
+   JavaClass jClass;
+   public void visit(JavaClass obj) {
+	jClass = obj;
+	}
+   public void visit(Code obj) {
+	state = 0;
+	super.visit(obj);
+	}
+
+
+    String f;
+    public void sawOpcode(int seen) {
+
+	switch (state) {
+	case 0:
+		if (seen == ALOAD_0)
+			state = 1;
+		break;
+	case 1:
+		if (seen == ALOAD_0)
+			state = 2;
+		else
+			state = 0;
+		break;
+	case 2:
+		if (seen == GETFIELD) {
+			state = 3;
+			f = refConstant;
+			}
+		else
+			state = 0;
+		break;
+	case 3:
+		if (seen == PUTFIELD && refConstant.equals(f)) {
+
+                                bugReporter.reportBug(new BugInstance("SA_FIELD_SELF_ASSIGNMENT", NORMAL_PRIORITY)
+                                        .addClass(className)
+                                        .addMethod(className, methodName, methodSig)
+                                        .addField(betterClassConstant, nameConstant, 
+						betterSigConstant, false)
+                                        .addSourceLine(this)
+
+					);
+			}
+		state = 0;
+		}
+	}
+		
+}
