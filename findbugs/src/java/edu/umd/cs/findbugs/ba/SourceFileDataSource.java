00da82914ef5a48b74593fa6468b803d62f5ff31
David Hovemeyer <david.hovemeyer@gmail.com>
2003-11-12 15:46:04 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1769 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/SourceFileDataSource.java
@@ -0,0 +1,35 @@
+/*
+ * Bytecode Analysis Framework
+ * Copyright (C) 2003, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.daveho.ba;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * A data source which can produce a stream for a source file.
+ */
+public interface SourceFileDataSource {
+	/** Open an InputStream on the source file. */
+	public InputStream open() throws IOException;
+	/** Get the full filename of the source file. */
+	public String getFullFileName();
+}
+
+// vim:ts=4
