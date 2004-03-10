6203414ff68b8f97d116454428eded58b8c06c34
David Hovemeyer <david.hovemeyer@gmail.com>
2004-03-10 16:34:21 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1894 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/BugCollectionBugReporter.java
@@ -0,0 +1,68 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2003,2004 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs;
+
+import java.io.StringWriter;
+import org.apache.bcel.classfile.JavaClass;
+
+public abstract class BugCollectionBugReporter extends TextUIBugReporter {
+	private SortedBugCollection bugCollection = new SortedBugCollection();
+	private Project project;
+
+	public BugCollectionBugReporter(Project project) {
+		this.project = project;
+	}
+
+	public Project getProject() {
+		return project;
+	}
+
+	public BugCollection getBugCollection() {
+		return bugCollection;
+	}
+
+	public void observeClass(JavaClass javaClass) {
+	}
+
+	public void logError(String message) {
+		bugCollection.addError(message);
+		super.logError(message);
+	}
+
+	public void reportMissingClass(ClassNotFoundException ex) {
+		bugCollection.addMissingClass(getMissingClassName(ex));
+		super.reportMissingClass(ex);
+	}
+
+	public void doReportBug(BugInstance bugInstance) {
+		if (bugCollection.add(bugInstance))
+			notifyObservers(bugInstance);
+	}
+
+	protected void generateSummary() throws Exception {
+		StringWriter writer = new StringWriter();
+		ProjectStats stats = getProjectStats();
+		stats.transformSummaryToHTML(writer);
+		String html = writer.toString();
+		bugCollection.setSummaryHTML(html);
+	}
+}
+
+// vim:ts=4
