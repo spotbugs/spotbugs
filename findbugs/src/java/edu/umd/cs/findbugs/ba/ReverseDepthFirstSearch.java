972a7e00f6c7a0ebb97a1b5b380486498db82e6e
David Hovemeyer <david.hovemeyer@gmail.com>
2004-03-18 02:09:38 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1922 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/ReverseDepthFirstSearch.java
@@ -0,0 +1,55 @@
+/*
+ * Bytecode Analysis Framework
+ * Copyright (C) 2003,2004 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.ba;
+
+import java.util.Iterator;
+
+/**
+ * Perform a reverse depth first search of a control flow graph.
+ * (I.e., depth first search of reversed CFG.)
+ *
+ * @see CFG
+ * @see AbstractDepthFirstSearch
+ * @author David Hovemeyer
+ */
+public class ReverseDepthFirstSearch extends AbstractDepthFirstSearch {
+	public ReverseDepthFirstSearch(CFG cfg) {
+		super(cfg);
+	}
+
+	protected BasicBlock getEntry(CFG cfg) {
+		return cfg.getExit();
+	}
+
+	protected Iterator<Edge> outgoingEdgeIterator(CFG cfg, BasicBlock basicBlock) {
+		return cfg.incomingEdgeIterator(basicBlock);
+	}
+
+	protected BasicBlock getTarget(Edge edge) {
+		return edge.getSource();
+	}
+
+	protected BasicBlock getSource(Edge edge) {
+		return edge.getTarget();
+	}
+
+}
+
+// vim:ts=4
