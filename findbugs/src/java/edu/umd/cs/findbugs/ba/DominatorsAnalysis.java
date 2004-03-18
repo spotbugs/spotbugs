5b41a390533e9e1c730cb2d0742f292172ce7f00
David Hovemeyer <david.hovemeyer@gmail.com>
2004-03-18 03:15:43 +0000

    Modified to inherit from AbstractDominatorsAnalysis.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@1926 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/ba/DominatorsAnalysis.java
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/DominatorsAnalysis.java
@@ -23,80 +23,25 @@ import java.util.*;
 import org.apache.bcel.classfile.ClassParser;
 import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.classfile.Method;
-import org.apache.bcel.generic.InstructionHandle;
 
 /**
- * A dataflow analysis to compute dominator relationships between
- * basic blocks.  Use the {@link #getResultFact} method to get the dominator
- * set for a given basic block.  The dominator sets are represented using
- * the {@link java.util.BitSet} class, with the individual bits
- * corresponding to the IDs of basic blocks.
- *
- * <p> Exception edges may be ignored, in which case the domination
- * results only take non-exception control flow into account.
- *
- * @see DataflowAnalysis
+ * Dataflow analysis to compute dominator sets for a CFG.
  * @see CFG
- * @see BasicBlock
+ * @see AbstractDominatorsAnalysis
  * @author David Hovemeyer
  */
-public class DominatorsAnalysis implements DataflowAnalysis<BitSet> {
-	private final CFG cfg;
-	private final DepthFirstSearch dfs;
-	private final boolean ignoreExceptionEdges;
-	private final IdentityHashMap<BasicBlock, BitSet> startFactMap;
-	private final IdentityHashMap<BasicBlock, BitSet> resultFactMap;
+public class DominatorsAnalysis extends AbstractDominatorsAnalysis {
+	private DepthFirstSearch dfs;
 
 	/**
 	 * Constructor.
 	 * @param cfg the CFG to compute dominator relationships for
 	 * @param dfs the DepthFirstSearch on the CFG
+	 * @param ignoreExceptionEdges true if exception edges should be ignored
 	 */
 	public DominatorsAnalysis(CFG cfg, DepthFirstSearch dfs, boolean ignoreExceptionEdges) {
-		this.cfg = cfg;
+		super(cfg, ignoreExceptionEdges);
 		this.dfs = dfs;
-		this.ignoreExceptionEdges = ignoreExceptionEdges;
-		this.startFactMap = new IdentityHashMap<BasicBlock, BitSet>();
-		this.resultFactMap = new IdentityHashMap<BasicBlock, BitSet>();
-	}
-
-	public BitSet createFact() {
-		return new BitSet();
-	}
-
-	public BitSet getStartFact(BasicBlock block) {
-		return lookupOrCreateFact(startFactMap, block);
-	}
-
-	public BitSet getResultFact(BasicBlock block) {
-		return lookupOrCreateFact(resultFactMap, block);
-	}
-
-	private BitSet lookupOrCreateFact(Map<BasicBlock, BitSet> map, BasicBlock block) {
-		BitSet fact = map.get(block);
-		if (fact == null) {
-			fact = createFact();
-			map.put(block, fact);
-		}
-		return fact;
-	}
-
-	public void copy(BitSet source, BitSet dest) {
-		dest.clear();
-		dest.or(source);
-	}
-
-	public void initEntryFact(BitSet result) {
-		// No blocks dominate the entry block
-		result.clear();
-	}
-
-	public void initResultFact(BitSet result) {
-		makeFactTop(result);
-	}
-
-	public void makeFactTop(BitSet fact) {
-		fact.set(0, cfg.getNumBasicBlocks());
 	}
 
 	public boolean isForwards() {
@@ -105,26 +50,6 @@ public class DominatorsAnalysis implements DataflowAnalysis<BitSet> {
 
 	public BlockOrder getBlockOrder(CFG cfg) {
 		return new ReversePostfixOrder(cfg, dfs);
-	}
-
-	public boolean same(BitSet fact1, BitSet fact2) {
-		return fact1.equals(fact2);
-	}
-
-	public void transfer(BasicBlock basicBlock, InstructionHandle end, BitSet start, BitSet result) throws DataflowAnalysisException {
-		// Start with intersection of dominators of predecessors
-		copy(start, result);
-
-		// Every block dominates itself
-		result.set(basicBlock.getId());
-	}
-
-	public void meetInto(BitSet fact, Edge edge, BitSet result) throws DataflowAnalysisException {
-		if (ignoreExceptionEdges && edge.isExceptionEdge())
-			return;
-
-		// Meet is intersection
-		result.and(fact);
 	}
 
 	public static void main(String[] argv) throws Exception {
