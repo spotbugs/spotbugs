4f958c27c222a19f6c14c0e8c9308e220a68a19d
Bill Pugh <bill.pugh@gmail.com>
2004-04-10 13:46:27 +0000

    Handle variant case of spin loop code
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2024 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/FindSpinLoop.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/FindSpinLoop.java
@@ -26,6 +26,10 @@ public class FindSpinLoop extends BytecodeScanningDetector implements   Constant
     private static final boolean DEBUG = Boolean.getBoolean("findspinloop.debug");
 
     int stage = 0;
+    int stage1 = 0;
+    int stage2 = 0;
+    int distance = 0;
+    int distance2 = 0;
     int start;
     private BugReporter bugReporter;
 
@@ -36,9 +40,54 @@ public class FindSpinLoop extends BytecodeScanningDetector implements   Constant
     public void visit(Method obj) {
 	if (DEBUG) System.out.println("Saw " + betterMethodName);
 	stage = 0;
+	stage1 = 0;
+	stage2 = 0;
+	distance = 1000000;
+	distance2 = 1000000;
 	}
 
     public void sawOpcode(int seen) {
+	/* prototype for short-circuit bug */
+	/*
+	distance++;
+	switch (seen) {
+		case ICONST_1: 
+			stage1 = 1;
+			break;
+		case GOTO:
+			if (stage1 == 1) stage1 = 2;
+			else stage1 = 0;
+			break;
+		case ICONST_0:
+			if (stage1 == 2) 
+				distance = 0;
+			stage1 = 0;
+		default:
+			stage1 = 0;
+		}
+	switch (seen) {
+		case IAND: 
+		case IOR: 
+			if (distance < 4)  {
+				distance2 = distance;
+				stage2 = 1;
+				}
+			else stage2 = 0;
+			break;
+		case IFEQ: 
+		case IFNE: 
+			if (stage2 == 1)  
+				System.out.println("non-short-circuit logic at distance " + distance2 + " in " + betterMethodName);
+			stage2 = 0;
+			break;
+		default:
+			stage2 = 0;
+			break;
+		};
+
+	*/
+
+	// System.out.println("PC: " + PC + ", stage: " + stage1);
 	switch (seen) {
 		case ALOAD_0: 
 			if (DEBUG) System.out.println("   ALOAD_0 at PC " + PC);
@@ -52,16 +101,20 @@ public class FindSpinLoop extends BytecodeScanningDetector implements   Constant
 				}
 			else stage = 0;
 			break;
+		case GOTO:
 		case IFNE:
 		case IFEQ:
 		case IFNULL:
 		case IFNONNULL:
 			if (DEBUG) System.out.println("   conditional branch in stage " + stage + " to " + branchTarget );
-			if (stage == 2 && branchTarget == start)
+			if (stage == 2 && branchTarget == start) {
 				bugReporter.reportBug(new BugInstance("SP_SPIN_ON_FIELD", NORMAL_PRIORITY)
 					.addClassAndMethod(this)
 					.addReferencedField(this));
-			stage = 0;
+				stage = 0;
+				}
+			else if (branchTarget < PC)
+				stage = 0;
 			break;
 		default:
 			stage = 0;
