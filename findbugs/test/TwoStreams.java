cb6e44ad90cbf12f0569ec2a5777f74425d5d1be
Bill Pugh <bill.pugh@gmail.com>
2004-05-04 14:58:21 +0000

    Check for potential null pointer exception
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2103 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/test/TwoStreams.java
+++ findbugs/test/TwoStreams.java
@@ -1,6 +1,7 @@
 import java.io.*;
 
 public class TwoStreams {
+	int x;
 	public void twoStreamsWrong() throws IOException {
 		BufferedReader r = null;
 		Writer w = null;
@@ -23,6 +24,13 @@ public class TwoStreams {
 		}
 	}
 
+	public void nullDereferenceCheck(TwoStreams o) throws IOException {
+		BufferedReader r = new BufferedReader(
+			new InputStreamReader(
+			new FileInputStream("hello")));
+		int i = o.x;
+		r.close();
+		}
 	public void twoStreamsRight() throws IOException {
 		BufferedReader r = null;
 		Writer w = null;
