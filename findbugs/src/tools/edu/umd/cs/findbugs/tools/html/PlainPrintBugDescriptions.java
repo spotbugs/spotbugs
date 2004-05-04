2ac898eff1d614fedf891d8295da4ad451d5836e
David Hovemeyer <david.hovemeyer@gmail.com>
2004-05-04 16:43:05 +0000

    Initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2110 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/tools/edu/umd/cs/findbugs/tools/html/PlainPrintBugDescriptions.java
@@ -0,0 +1,64 @@
+/*
+ * Generate HTML file containing bug descriptions
+ * Copyright (C) 2004, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.tools.html;
+
+import edu.umd.cs.findbugs.BugPattern;
+import edu.umd.cs.findbugs.DetectorFactoryCollection;
+import edu.umd.cs.findbugs.I18N;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import java.util.Iterator;
+
+public class PlainPrintBugDescriptions extends PrintBugDescriptions {
+	private String docTitle;
+	private PrintStream out;
+
+	public PlainPrintBugDescriptions(String docTitle, OutputStream out) {
+		this.docTitle = docTitle;
+		this.out = new PrintStream(out);
+	}
+
+	protected void prologue() {
+		out.println("<html><head><title>" + docTitle + "</title></head><body>");
+		out.println("<h1>" + docTitle + "</h1>");
+	}
+
+	protected void emit(BugPattern bugPattern) {
+		out.println("<h2>" + bugPattern.getAbbrev() + ": " +
+			bugPattern.getShortDescription() + "</h2>");
+		out.println(bugPattern.getDetailText());
+	}
+
+	protected void epilogue() {
+		out.println("</body></html>");
+	}
+
+	public static void main(String[] args) throws Exception {
+		String docTitle = "FindBugs Bug Descriptions";
+		if (args.length > 0)
+			docTitle = args[0];
+		new PlainPrintBugDescriptions(docTitle, System.out).print();
+	}
+}
+
+// vim:ts=3
