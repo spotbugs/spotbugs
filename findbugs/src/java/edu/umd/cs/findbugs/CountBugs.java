0be0662ef06b78c962134cf19f63057c2fec42fd
David Hovemeyer <david.hovemeyer@gmail.com>
2004-05-11 01:45:47 +0000

    Changed to be an object rather than just static methods.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2191 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/CountBugs.java
+++ findbugs/src/java/edu/umd/cs/findbugs/CountBugs.java
@@ -1,36 +1,79 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2003,2004 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 package edu.umd.cs.findbugs;
 
+import java.io.IOException;
 
 import java.util.*;
 
+import org.dom4j.DocumentException;
+
+/**
+ * Count bugs in a result file by category.
+ */
 public class CountBugs {
+	private SortedBugCollection bugCollection;
+	private Project project;
+	private HashSet<String> categorySet;
+	private TreeMap<String, Integer> countMap;
 
-	private static HashSet<String> categorySet = new HashSet<String>();
-	private static TreeMap<String, Integer> countMap = new TreeMap<String, Integer>();
+	public CountBugs(String resultsFileName) throws IOException, DocumentException {
+		this(new SortedBugCollection(), new Project());
 
-	public static void main(String[] argv) throws Exception {
-		if (argv.length < 1) {
-			System.err.println("Usage: " + CountBugs.class.getName() + " <results file>");
-			System.exit(1);
+		bugCollection.readXML(resultsFileName, project);
+	}
+
+	public CountBugs(SortedBugCollection bugCollection, Project project) {
+		this.bugCollection = bugCollection;
+		this.project = project;
+		this.categorySet = new HashSet<String>();
+		this.countMap = new TreeMap<String, Integer>();
+	}
+
+	public SortedBugCollection getBugCollection() {
+		return bugCollection;
+	}
+
+	public Project getProject() {
+		return project;
+	}
+
+	public void addCategory(String category) {
+		categorySet.add(category);
+	}
+
+	public void setCategories(String categoryList) {
+		StringTokenizer tok = new StringTokenizer(categoryList, ",");
+		while (tok.hasMoreTokens()) {
+			String category = tok.nextToken();
+			categorySet.add(category);
+			countMap.put(category, new Integer(0));
 		}
+	}
 
+	public Iterator<Map.Entry<String, Integer>> entryIterator() {
+		return countMap.entrySet().iterator();
+	}
+
+	public void execute() {
 		DetectorFactoryCollection.instance(); // load plugins
-
-		int arg = 0;
-		if (argv[0].equals("-categories")) {
-			StringTokenizer tok = new StringTokenizer(argv[1], ",");
-			while (tok.hasMoreTokens()) {
-				String category = tok.nextToken();
-				categorySet.add(category);
-				countMap.put(category, new Integer(0));
-			}
-			arg = 2;
-		}
-
-		String filename = argv[arg];
-
-		BugCollection bugCollection = new SortedBugCollection();
-		bugCollection.readXML(filename, new Project());
 
 		Iterator<BugInstance> i = bugCollection.iterator();
 		while (i.hasNext()) {
@@ -45,8 +88,48 @@ public class CountBugs {
 				count = new Integer(0);
 			countMap.put(category, new Integer(count.intValue() + 1));
 		}
+	}
 
-		Iterator<Map.Entry<String, Integer>> j = countMap.entrySet().iterator();
+	private static void usage() {
+		System.out.println("Usage:");
+		System.out.println(CountBugs.class.getName() + " [-categories <categories>] <results file>");
+		System.out.println(CountBugs.class.getName() + " [-categories <categories>] -diff <orig results> <new results>");
+		System.exit(1);
+	}
+
+	public static void main(String[] argv) throws Exception {
+		if (argv.length < 1) {
+			usage();
+		}
+
+		int arg = 0;
+		String categoryList = "";
+		boolean diffMode = false;
+
+		while (arg < argv.length - 1) {
+			String option = argv[arg];
+
+			if (option.equals("-categories")) {
+				++arg;
+				if (arg >= argv.length)
+					throw new IllegalArgumentException("-categories option requires argument");
+				categoryList = argv[arg];
+			} else if (option.equals("-diff")) {
+				diffMode = true;
+			} else
+				break;
+
+			++arg;
+		}
+
+		if (arg >= argv.length)
+			usage();
+
+		String filename = argv[arg];
+		CountBugs countBugs = new CountBugs(filename);
+		countBugs.execute();
+
+		Iterator<Map.Entry<String, Integer>> j = countBugs.entryIterator();
 		while(j.hasNext()) {
 			Map.Entry<String,Integer> entry = j.next();
 			System.out.println(entry.getKey() + ": " + entry.getValue());
@@ -54,3 +137,5 @@ public class CountBugs {
 
 	}
 }
+
+// vim:ts=3
