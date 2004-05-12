c85dbd281f965cbfbe4691e1a37e2258eba8f1dd
David Hovemeyer <david.hovemeyer@gmail.com>
2004-05-12 20:24:10 +0000

    Initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2209 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/AddAnnotation.java
@@ -0,0 +1,91 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2004, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs;
+
+import java.io.IOException;
+
+import java.util.Iterator;
+
+import org.dom4j.DocumentException;
+
+/**
+ * Add an annotation string to every BugInstance in a BugCollection.
+ */
+public class AddAnnotation {
+	private BugCollection bugCollection;
+	private Project project;
+	private String annotation;
+
+	public AddAnnotation(BugCollection bugCollection, Project project, String annotation) {
+		this.bugCollection = bugCollection;
+		this.project = project;
+		this.annotation = annotation;
+	}
+
+	public AddAnnotation(String resultsFile, String annotation)
+		throws IOException, DocumentException {
+		this(new SortedBugCollection(), new Project(), annotation);
+		bugCollection.readXML(resultsFile, project);
+	}
+
+	public BugCollection getBugCollection() {
+		return bugCollection;
+	}
+
+	public Project getProject() {
+		return project;
+	}
+
+	public void execute() {
+		for (Iterator<BugInstance> i = bugCollection.iterator(); i.hasNext(); ) {
+			BugInstance bugInstance = i.next();
+
+			// Don't add the annotation if it is already present
+			if (bugInstance.annotationTextContainsWord(this.annotation))
+				continue;
+
+			String annotation = bugInstance.getAnnotationText();
+			StringBuffer buf = new StringBuffer();
+			if (!annotation.equals("")) {
+				buf.append(annotation);
+				buf.append('\n');
+			}
+			buf.append(this.annotation);
+			bugInstance.setAnnotationText(buf.toString());
+		}
+	}
+
+	public static void main(String[] argv) throws Exception {
+		if (argv.length != 2) {
+			System.err.println("Usage: " + AddAnnotation.class.getName() + " <results file> <annotation>");
+			System.exit(1);
+		}
+
+		String filename = argv[0];
+		String annotation = argv[1];
+
+		AddAnnotation addAnnotation = new AddAnnotation(filename, annotation);
+		addAnnotation.execute();
+
+		addAnnotation.getBugCollection().writeXML(filename, addAnnotation.getProject());
+	}
+}
+
+// vim:ts=3
