accee15d6192de9f5c78db625ab74e8610f0db2e
Bill Pugh <bill.pugh@gmail.com>
2004-05-24 15:07:24 +0000

    Check more cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2303 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/test/NonShortCircuit.java
+++ findbugs/test/NonShortCircuit.java
@@ -1,7 +1,12 @@
 
 class NonShortCircuit {
+  boolean b;
   boolean and( boolean x, boolean y) {
 	return x & y;
+	}
+  void orIt( boolean x, boolean y) {
+	x |= y;
+	b |= x;
 	}
   boolean ordered(int x, int y, int z) {
 	if (x >= y | y >= z) System.out.println("Not ordered");
@@ -10,4 +15,12 @@ class NonShortCircuit {
   boolean nonEmpty(Object o[]) {
 	return o != null & o.length > 0;
 	}
+
+    public static final int BIT0 = 1;   // 1st bit
+
+    protected int m_iType;
+
+    public NonShortCircuit( boolean available ) {
+        m_iType |= available ? BIT0 : 0;
+    }
 }
--- findbugs/test/UncalledPrivateMethod.java
+++ findbugs/test/UncalledPrivateMethod.java
@@ -4,4 +4,19 @@ public class UncalledPrivateMethod {
 	private void foo(String s) {
 		this.s = s;
 	}
+
+
+  private void debug(String s) {
+	System.out.println(s);
+	}
+
+  private void foobar(int i) {
+	}
+  private void foobar(double d) {
+	}
+
+  public void f(double d) {
+	foobar(d);
+	}
+
 }
