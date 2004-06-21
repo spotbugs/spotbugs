8312a01ae38766ad81f6b7093825dc15bdb83db8
Bill Pugh <bill.pugh@gmail.com>
2004-06-21 20:52:11 +0000

    Another test for string concatenation
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2382 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/test/SBConcatTest.java
+++ findbugs/test/SBConcatTest.java
@@ -27,6 +27,7 @@ public class SBConcatTest
 	
 	public String doConcat3( String[] tokens )
 	{
+		// should not complain
 		StringBuffer result = new StringBuffer();
 		result.append("Results:" );
 		for (int i = 0; i < tokens.length; i++)
@@ -36,11 +37,19 @@ public class SBConcatTest
 	
 	public String doConcat4( String[] tokens )
 	{
+		// should not complain
 		int a = 0, b = 1, c = 2, d = 3, e = 4, f = 5, g = 6;
 		StringBuffer result = new StringBuffer();
 		result.append("Results:" );
 		for (int i = 0; i < tokens.length; i++)
 			result.append( tokens[i] );
 		return result.toString();
+	}
+
+	public void doConcat5( String[] tokens ) 
+	{
+		// should not complain
+		for(int i = 0; i < tokens.length; i++) 
+			tokens[i] += i;
 	}
 }
