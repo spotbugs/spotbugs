c4024b0f287f6e252b81e7d293276818a6ac3dbc
David Hovemeyer <david.hovemeyer@gmail.com>
2004-06-21 16:08:54 +0000

    Initial checkin, from Dave Brosius <dbrosius@users.sourceforge.net>
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2379 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/test/SBConcatTest.java
@@ -0,0 +1,46 @@
+
+
+public class SBConcatTest
+{
+	public String doConcat1( String[] tokens )
+	{
+		//Detector should complain about the following
+		String result = "";
+		for (int i = 0; i < tokens.length; i++)
+			result += tokens[i];
+		return result;
+	}
+
+	public String doConcat2( String[] tokens )
+	{
+		//Detector should complain about the following
+		String result = "";
+		int i = 0; 
+		while (i < tokens.length)
+		{
+			tokens[i] = tokens[i].trim();
+			result = result + tokens[i];
+			i++;
+		}
+		return result;			
+	}
+	
+	public String doConcat3( String[] tokens )
+	{
+		StringBuffer result = new StringBuffer();
+		result.append("Results:" );
+		for (int i = 0; i < tokens.length; i++)
+			result.append( tokens[i] );
+		return result.toString();
+	}
+	
+	public String doConcat4( String[] tokens )
+	{
+		int a = 0, b = 1, c = 2, d = 3, e = 4, f = 5, g = 6;
+		StringBuffer result = new StringBuffer();
+		result.append("Results:" );
+		for (int i = 0; i < tokens.length; i++)
+			result.append( tokens[i] );
+		return result.toString();
+	}
+}
