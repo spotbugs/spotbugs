9e8df361843cfb0fa3aa48dbbac0d78b04578b7c
David Hovemeyer <david.hovemeyer@gmail.com>
2004-06-29 15:46:48 +0000

    Initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2428 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/test/ExceptionSetMerge.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+// This causes a fatal exception in FindBugs 0.8.1
+// Array types can be merged with exception objects
+
+public class ExceptionSetMerge {
+	public void f(InputStream in) {
+		Object o = new Object[0];
+
+		try {
+			System.out.println(in.read());
+		} catch (IOException e) {
+			o = e;
+		}
+
+		System.out.println(o.hashCode());
+	}
+}
+
+// vim:ts=4
