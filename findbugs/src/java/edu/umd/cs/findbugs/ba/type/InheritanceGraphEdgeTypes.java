d4d68c465b7f7e4fe6eee8908cbb7aa85ba537ae
David Hovemeyer <david.hovemeyer@gmail.com>
2004-07-12 19:02:04 +0000

    Initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2460 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/type/InheritanceGraphEdgeTypes.java
@@ -0,0 +1,9 @@
+package edu.umd.cs.findbugs.ba.type;
+
+public interface InheritanceGraphEdgeTypes {
+
+	public static final int EXTENDS_EDGE = 1;
+	public static final int IMPLEMENTS_EDGE = 2;
+}
+
+// vim:ts=4
