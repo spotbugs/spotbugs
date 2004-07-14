11f472bc7f2089948577f6a5476907ea3072548d
David Hovemeyer <david.hovemeyer@gmail.com>
2004-07-14 18:24:57 +0000

    Added some more tests.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2491 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/junit/edu/umd/cs/findbugs/ba/type/TypeRepositoryTest.java
+++ findbugs/src/junit/edu/umd/cs/findbugs/ba/type/TypeRepositoryTest.java
@@ -3,12 +3,17 @@ package edu.umd.cs.findbugs.ba.type;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
-import org.apache.bcel.Repository;
-
-import org.apache.bcel.util.ClassPath;
-import org.apache.bcel.util.SyntheticRepository;
-
 public class TypeRepositoryTest extends TestCase {
+
+	/**
+	 * A do-nothing class resolver that just throws
+	 * ClassNotFoundExceptions.
+	 */
+	static class NullClassResolver implements ClassResolver {
+		public void resolveClass(ClassType type, TypeRepository repos) throws ClassNotFoundException {
+			throw new ClassNotFoundException("TypeRepositoryTest does not use a ClassResolver");
+		}
+	}
 
 	TypeRepository repos;
 
@@ -19,10 +24,15 @@ public class TypeRepositoryTest extends TestCase {
 	ClassType myClassType;
 	ClassType mySuperclassType;
 	ClassType myInterfaceType;
+	ClassType mySubinterfaceType;
 
 	ArrayType myClassArrayType;
 	ArrayType mySuperclassArrayType;
 	ArrayType myInterfaceArrayType;
+	ArrayType mySubinterfaceArrayType;
+
+	ArrayType javaLangObjectArray1Type;
+	ArrayType javaLangObjectArray2Type;
 
 	void setClass(ClassType type) {
 		type.setIsInterface(false);
@@ -34,23 +44,19 @@ public class TypeRepositoryTest extends TestCase {
 	}
 
 	protected void setUp() {
-		// The BCEL resolver is an easy way to test stuff
-		// that is accessible on the system classpath.
-		ClassResolver resolver = new BCELRepositoryClassResolver();
-
-		// Clear the BCEL repository
-		Repository.clearCache();
-		SyntheticRepository synthRepos = SyntheticRepository.getInstance(ClassPath.SYSTEM_CLASS_PATH);
-		Repository.setRepository(synthRepos);
-
+		// In this test, all class types are explicitly marked as
+		// class vs. interface, and supertypes explicitly identified.
+		// So, dynamic class resolution should never be required.
+		ClassResolver resolver = new NullClassResolver();
 		this.repos = new TypeRepository(resolver);
 
+		// Create the common class library types needed
 		javaLangObjectType = repos.classTypeFromDottedClassName("java.lang.Object");
 		setClass(javaLangObjectType);
 		javaIoSerializableType = repos.classTypeFromDottedClassName("java.io.Serializable");
 		setInterface(javaIoSerializableType);
 		javaLangCloneableType = repos.classTypeFromDottedClassName("java.lang.Cloneable");
-		setClass(javaLangCloneableType);
+		setInterface(javaLangCloneableType);
 
 		// Class types should have a unique representation
 		myClassType = repos.classTypeFromSlashedClassName("com/foobar/MyClass");
@@ -61,14 +67,20 @@ public class TypeRepositoryTest extends TestCase {
 		setClass(mySuperclassType);
 		myInterfaceType = repos.classTypeFromDottedClassName("com.foobar.MyInterface");
 		setInterface(myInterfaceType);
+		mySubinterfaceType = repos.classTypeFromDottedClassName("com.foobar.SubInterface");
+		setInterface(mySubinterfaceType);
 		repos.addSuperclassLink(myClassType, mySuperclassType);
 		repos.addSuperclassLink(mySuperclassType, javaLangObjectType);
 		repos.addInterfaceLink(mySuperclassType, myInterfaceType);
+		repos.addInterfaceLink(mySubinterfaceType, myInterfaceType);
 
 		// Array classes
 		myClassArrayType = repos.arrayTypeFromDimensionsAndElementType(1, myClassType);
 		mySuperclassArrayType = repos.arrayTypeFromDimensionsAndElementType(1, mySuperclassType);
 		myInterfaceArrayType = repos.arrayTypeFromDimensionsAndElementType(1, myInterfaceType);
+		mySubinterfaceArrayType = repos.arrayTypeFromDimensionsAndElementType(1, mySubinterfaceType);
+		javaLangObjectArray1Type = repos.arrayTypeFromDimensionsAndElementType(1, javaLangObjectType);
+		javaLangObjectArray2Type = repos.arrayTypeFromDimensionsAndElementType(2, javaLangObjectType);
 	}
 
 	public void testClassFromSlashedClassName() {
@@ -91,14 +103,25 @@ public class TypeRepositoryTest extends TestCase {
 	}
 
 	public void testIsInterface() {
-		Assert.assertTrue(!myClassType.isInterface());
-		Assert.assertTrue(!mySuperclassType.isInterface());
 		Assert.assertTrue(myInterfaceType.isInterface());
+		Assert.assertTrue(mySubinterfaceType.isInterface());
+		Assert.assertTrue(javaIoSerializableType.isInterface());
+		Assert.assertTrue(javaLangCloneableType.isInterface());
+	}
+
+	public void testIsNotInterface() {
+		Assert.assertFalse(myClassType.isInterface());
+		Assert.assertFalse(mySuperclassType.isInterface());
+		Assert.assertFalse(myClassArrayType.isInterface());
 	}
 
 	public void testIsDirectClassSubtype() throws ClassNotFoundException {
 		Assert.assertTrue(repos.isSubtype(myClassType, mySuperclassType));
 		Assert.assertTrue(repos.isSubtype(mySuperclassType, javaLangObjectType));
+	}
+
+	public void testIndirectClassSubtype() throws ClassNotFoundException {
+		Assert.assertTrue(repos.isSubtype(myClassType, javaLangObjectType));
 	}
 
 	public void testArrayIsSubtypeOfObject() throws ClassNotFoundException {
@@ -108,6 +131,7 @@ public class TypeRepositoryTest extends TestCase {
 	public void testArrayIsSubtypeOfArraysOfElementSupertypes() throws ClassNotFoundException {
 		Assert.assertTrue(repos.isSubtype(myClassArrayType, mySuperclassArrayType));
 		Assert.assertTrue(repos.isSubtype(myClassArrayType, myInterfaceArrayType));
+		Assert.assertTrue(repos.isSubtype(mySubinterfaceArrayType, myInterfaceArrayType));
 	}
 
 	public void testArrayTypeIsSerializable() throws ClassNotFoundException {
@@ -121,7 +145,25 @@ public class TypeRepositoryTest extends TestCase {
 		Assert.assertTrue(repos.isSubtype(mySuperclassArrayType, javaLangCloneableType));
 		Assert.assertTrue(repos.isSubtype(myInterfaceArrayType, javaLangCloneableType));
 	}
-	
+
+	public void testNoInvalidSubtypes() throws ClassNotFoundException {
+		Assert.assertFalse(repos.isSubtype(mySuperclassType, myClassType));
+		Assert.assertFalse(repos.isSubtype(mySuperclassArrayType, myClassArrayType));
+		Assert.assertFalse(repos.isSubtype(myInterfaceType, mySuperclassType));
+		Assert.assertFalse(repos.isSubtype(javaLangObjectType, mySuperclassType));
+	}
+
+	public void testInterfaceIsSubtypeOfObject() throws ClassNotFoundException {
+		Assert.assertTrue(repos.isSubtype(myInterfaceType, javaLangObjectType));
+		Assert.assertTrue(repos.isSubtype(javaIoSerializableType, javaLangObjectType));
+		Assert.assertTrue(repos.isSubtype(javaLangCloneableType, javaLangObjectType));
+	}
+
+	public void testArrayOfObjectSupertypes() throws ClassNotFoundException {
+		Assert.assertTrue(repos.isSubtype(javaLangObjectArray2Type, javaLangObjectArray1Type));
+		Assert.assertTrue(repos.isSubtype(javaLangObjectArray1Type, javaLangObjectType));
+	}
+
 }
 
 // vim:ts=4
