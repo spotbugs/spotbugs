b79bf01f0dde3c9532dc4d65830e7f4e6c6ab92a
David Hovemeyer <david.hovemeyer@gmail.com>
2004-07-14 15:34:05 +0000

    Added some fake types to the repository in the test fixture. Added some hierarchy tests.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2485 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/junit/edu/umd/cs/findbugs/ba/type/TypeRepositoryTest.java
+++ findbugs/src/junit/edu/umd/cs/findbugs/ba/type/TypeRepositoryTest.java
@@ -12,6 +12,25 @@ public class TypeRepositoryTest extends TestCase {
 
 	TypeRepository repos;
 
+	ClassType javaLangObjectType;
+
+	ClassType myClassType;
+	ClassType myClassType2;
+	ClassType myClassType3;
+	ClassType myClassType4;
+
+	ClassType mySuperclassType;
+	ClassType myInterfaceType;
+
+	void setClass(ClassType type) {
+		type.setIsInterface(false);
+	}
+
+	void setInterface(ClassType type) {
+		type.setIsInterface(true);
+		repos.addInterfaceLink(type, javaLangObjectType);
+	}
+
 	protected void setUp() {
 		// The BCEL resolver is an easy way to test stuff
 		// that is accessible on the system classpath.
@@ -23,30 +42,54 @@ public class TypeRepositoryTest extends TestCase {
 		Repository.setRepository(synthRepos);
 
 		this.repos = new TypeRepository(resolver);
+
+		javaLangObjectType = repos.classTypeFromDottedClassName("java.lang.Object");
+		setClass(javaLangObjectType);
+
+		// Class types should have a unique representation
+		myClassType = repos.classTypeFromSlashedClassName("com/foobar/MyClass");
+		myClassType2 = repos.classTypeFromDottedClassName("com.foobar.MyClass");
+		myClassType3 = repos.classTypeFromSlashedClassName("com/foobar/MyClass");
+		myClassType4 = repos.classTypeFromDottedClassName("com.foobar.MyClass");
+		setClass(myClassType);
+
+		// Fake hierarchy classes
+		mySuperclassType = repos.classTypeFromDottedClassName("com.foobar.MySuperClass");
+		setClass(mySuperclassType);
+		myInterfaceType = repos.classTypeFromDottedClassName("com.foobar.MyInterface");
+		setInterface(myInterfaceType);
+		repos.addSuperclassLink(myClassType, mySuperclassType);
+		repos.addSuperclassLink(mySuperclassType, javaLangObjectType);
+		repos.addInterfaceLink(mySuperclassType, myInterfaceType);
 	}
 
 	public void testClassFromSlashedClassName() {
-		ClassType type = repos.classTypeFromSlashedClassName("com/foobar/MyClass");
-		Assert.assertEquals(type.getSignature(), "Lcom/foobar/MyClass;");
-		Assert.assertEquals(type.getClassName(), "com.foobar.MyClass");
+		Assert.assertEquals(myClassType.getSignature(), "Lcom/foobar/MyClass;");
+		Assert.assertEquals(myClassType.getClassName(), "com.foobar.MyClass");
 	}
 
 	public void testClassFromDottedClassName() {
-		ClassType type = repos.classTypeFromDottedClassName("com.foobar.MyClass");
-		Assert.assertEquals(type.getSignature(), "Lcom/foobar/MyClass;");
-		Assert.assertEquals(type.getClassName(), "com.foobar.MyClass");
+		ClassType myClassType = repos.classTypeFromDottedClassName("com.foobar.MyClass");
+		Assert.assertEquals(myClassType.getSignature(), "Lcom/foobar/MyClass;");
+		Assert.assertEquals(myClassType.getClassName(), "com.foobar.MyClass");
 	}
 
-	public void testUniqueClass() {
-		ClassType type1 = repos.classTypeFromSlashedClassName("com/foobar/MyClass");
-		ClassType type2 = repos.classTypeFromDottedClassName("com.foobar.MyClass");
-		ClassType type3 = repos.classTypeFromSlashedClassName("com/foobar/MyClass");
-		ClassType type4 = repos.classTypeFromDottedClassName("com.foobar.MyClass");
-		Assert.assertTrue(type1 == type2);
-		Assert.assertTrue(type1 == type3);
-		Assert.assertTrue(type1 == type4);
+	public void testClassTypeUnique() {
+		Assert.assertTrue(myClassType == myClassType2);
+		Assert.assertTrue(myClassType == myClassType3);
+		Assert.assertTrue(myClassType == myClassType4);
 	}
 
+	public void testIsInterface() {
+		Assert.assertTrue(!myClassType.isInterface());
+		Assert.assertTrue(!mySuperclassType.isInterface());
+		Assert.assertTrue(myInterfaceType.isInterface());
+	}
+
+	public void testIsDirectClassSubtype() throws ClassNotFoundException {
+		Assert.assertTrue(repos.isSubtype(myClassType, mySuperclassType));
+		Assert.assertTrue(repos.isSubtype(mySuperclassType, javaLangObjectType));
+	}
 	
 }
 
