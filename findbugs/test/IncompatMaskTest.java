8df6a2fc26167dff683cad7a9a92876dc92284f3
David Hovemeyer <david.hovemeyer@gmail.com>
2004-07-19 15:55:05 +0000

    New detector from Tom Truscott <trt@unx.sas.com> to find comparisons involving values computed with bitwise operators that are fixed at compile time.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2519 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/test/IncompatMaskTest.java
@@ -0,0 +1,45 @@
+/**
+ * test of MASK detector
+ */
+public class IncompatMaskTest {
+    public static void foo() {
+         int i = 2;
+
+         if ((i & 16)  == 2) System.out.println("warn");
+         if ((i & 16)  != 2) System.out.println("warn");
+         if ((i | 16)  == 2) System.out.println("warn");
+         if ((i | 16)  != 2) System.out.println("warn");
+
+         if ((i & 16)  < 2) System.out.println("unsupp");
+         if ((i | 16)  < 2) System.out.println("unsupp");
+
+         if ((i & 3) == 2) System.out.println("bogus");
+         if ((i & 3) != 2) System.out.println("bogus");
+         if ((i | 3) == 7) System.out.println("bogus");
+         if ((i | 3) == 7) System.out.println("bogus");
+
+//       if ((i & 16L) == 2) System.out.println("warn");
+//       if ((i & 16L) != 2) System.out.println("warn");
+//       if ((i | 16L) == 2) System.out.println("warn");
+//       if ((i | 16L) != 2) System.out.println("warn");
+    }
+
+    public static void bar() {
+        int i = 2;
+                                                                                
+        if ((i & 16)  == 2) return;  /* always unequal */
+        if ((i & 16)  != 2) return;  /* always unequal */
+        if ((i | 16)  == 2) return;  /* always unequal */
+        if ((i | 16)  != 0) return;  /* always unequal */
+        if ((i & 0 )  == 0) return;  /* always   equal */
+        if ((i & 0 )  != 0) return;  /* always   equal */
+                                                                                
+        if ((i & 16L)  == 2) return;  /* always unequal */
+        if ((i & 16L)  != 2) return;  /* always unequal */
+        if ((i | 16L)  == 2) return;  /* always unequal */
+        if ((i | 16L)  != 0) return;  /* always unequal */
+        if ((i & 0L )  == 0) return;  /* always   equal */
+        if ((i & 0L )  != 0) return;  /* always   equal */
+        }
+
+}
