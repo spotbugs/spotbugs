55d47add284aa75ad39bd5ea6e9999bf989fd26c
David Hovemeyer <david.hovemeyer@gmail.com>
2004-07-19 14:54:19 +0000

    Patch from Dave Brosius <dbrosius@users.sourceforge.net> to find calls to Collection.toArray() with a zero-length array as an argument.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2516 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/InefficientToArray.java
@@ -0,0 +1,116 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2004 Dave Brosius <dbrosius@users.sourceforge.net>
+ * Copyright (C) 2004 University of Maryland
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.detect;
+
+import edu.umd.cs.findbugs.*;
+import java.util.*;
+import org.apache.bcel.classfile.*;
+import org.apache.bcel.Repository;
+import edu.umd.cs.findbugs.visitclass.DismantleBytecode;
+import edu.umd.cs.findbugs.visitclass.Constants2;
+
+/**
+ * Find occurrences of collection.toArray( new Foo[0] );
+ * This causes another memory allocation through reflection
+ * Much better to do collection.toArray( new Foo[collection.size()] );
+ *
+ * @author Dave Brosius
+ */
+public class InefficientToArray extends BytecodeScanningDetector implements   Constants2 {
+	private static final boolean DEBUG
+		= Boolean.getBoolean("ita.debug") || true;
+
+	static final int SEEN_NOTHING = 0;
+	static final int SEEN_ICONST_0 = 1;
+	static final int SEEN_ANEWARRAY = 2;
+
+	private static JavaClass collectionClass;
+
+	private BugReporter bugReporter;
+	private int state = SEEN_NOTHING;
+
+	static {
+		try {
+			collectionClass = Repository.lookupClass("java.util.Collection");
+		}
+		catch (ClassNotFoundException cnfe) {
+			collectionClass = null;
+		}
+	}
+	public InefficientToArray(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
+
+	public void visit(Method obj) {
+		if (DEBUG)
+			System.out.println("------------------- Analyzing " + obj.getName() + " ----------------");
+		state = SEEN_NOTHING;
+		super.visit(obj);
+	}
+
+	public void sawOpcode(int seen) {
+
+		if (collectionClass == null)
+			return;
+
+		if (DEBUG) System.out.println("State: " + state + "  Opcode: " + OPCODE_NAMES[seen]);
+
+		switch (state) {
+			case SEEN_NOTHING:
+				if (seen == ICONST_0)
+					state = SEEN_ICONST_0;
+			break;
+
+			case SEEN_ICONST_0:
+				if (seen == ANEWARRAY) {
+					state = SEEN_ANEWARRAY;
+				} else
+					state = SEEN_NOTHING;
+			break;
+
+			case SEEN_ANEWARRAY:
+				if ((seen == INVOKEVIRTUAL)
+				&&  (getNameConstantOperand().equals("toArray"))
+				&&  (getSigConstantOperand().equals("([Ljava/lang/Object;)[Ljava/lang/Object;"))) {
+					try {
+						String clsName = getClassConstantOperand();
+						JavaClass cls = Repository.lookupClass(clsName);
+						if (cls.implementationOf(collectionClass))
+							bugReporter.reportBug(new BugInstance("ITA_INEFFICIENT_TO_ARRAY", NORMAL_PRIORITY)
+												.addClassAndMethod(this)
+												.addSourceLine(this));
+
+					}
+					catch (ClassNotFoundException cnfe) {
+						bugReporter.reportMissingClass(cnfe);
+					}
+				}
+				state = SEEN_NOTHING;
+			break;
+
+			default:
+				state = SEEN_NOTHING;
+			break;
+		}
+	}
+}
+
+// vim:ts=4
