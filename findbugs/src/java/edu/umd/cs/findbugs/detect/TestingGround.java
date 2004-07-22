157524bbcde63083c44f28e2a4a188c1d0cdba21
Bill Pugh <bill.pugh@gmail.com>
2004-07-22 15:42:57 +0000

    Added a new detector TestingGround.
    This is a detector designed for prototyping new detectors,
    so that writing a new detector doesn't require editing
    XML files, etc.
    
    In committed versions, this detector should be a no-op.
    
    
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2547 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/TestingGround.java
@@ -0,0 +1,51 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2004, Tom Truscott <trt@unx.sas.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.detect;
+import edu.umd.cs.findbugs.*;
+import org.apache.bcel.classfile.*;
+import java.util.zip.*;
+import java.io.*;
+
+import edu.umd.cs.findbugs.visitclass.DismantleBytecode;
+import edu.umd.cs.findbugs.visitclass.Constants2;
+
+public class TestingGround extends BytecodeScanningDetector implements Constants2 {
+
+	private BugReporter bugReporter;
+	private final boolean active = false;
+
+	public TestingGround(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
+
+
+	public void visit(Code obj) {
+		// unless active, don't bother dismantling bytecode
+		if (active) {
+			System.out.println("TestingGround: " + getFullyQualifiedMethodName());
+			super.visit(obj);
+			}
+		}
+
+
+	public void sawOpcode(int seen) {
+		System.out.println("  TestingGround: " +  OPCODE_NAMES[seen]);
+		}
+}
