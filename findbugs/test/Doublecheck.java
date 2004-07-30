14f024acd26008db8c5e9095c3ad863777f0d36f
Bill Pugh <bill.pugh@gmail.com>
2004-07-30 21:15:00 +0000

    More test cases for double checked locking
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2615 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/test/Doublecheck.java
+++ findbugs/test/Doublecheck.java
@@ -1,12 +1,63 @@
 public class Doublecheck {
-	public Object o;
+	static private Object o;
+	static private volatile Object v;
+	static private String s;
+	static private int i;
+	static private long j;
+	static private Object lock = new Object();
+	
 
-	public void t() {
+	static public Object standardDoubleCheck() {
 		if (o == null) {
-			synchronized (this) {
+			synchronized (lock) {
 				if (o == null)
 					o = new Object();
 			}
 		}
+		return o;
 	}
+	static public Object volatileDoubleCheck() {
+		if (v == null) {
+			synchronized (lock) {
+				if (v == null)
+					v = new Object();
+			}
+		}
+		return o;
+	}
+
+	static public String stringDoubleCheck() {
+		if (s == null) {
+			synchronized (lock) {
+				if (s == null)
+					s = Thread.currentThread().toString();
+			}
+		}
+		return s;
+	}
+
+
+	static public int intDoubleCheck() {
+		if (i == 0) {
+			synchronized (lock) {
+				if (i == 0)
+					i = Thread.currentThread().hashCode();
+			}
+		}
+		return i;
+	}
+
+	static public long longDoubleCheck() {
+		if (j == 0) {
+			synchronized (lock) {
+				if (j == 0)
+					j = System.currentTimeMillis();
+			}
+		}
+		return j;
+	}
+
+
+
+
 }
