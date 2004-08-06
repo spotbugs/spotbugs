71dbd02f37a554727c29868ee340f01098d6f49b
David Hovemeyer <david.hovemeyer@gmail.com>
2004-08-06 20:02:14 +0000

    Initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2694 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/AnyMethodReturnValueStreamFactory.java
@@ -0,0 +1,69 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2004, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.detect;
+
+import edu.umd.cs.findbugs.ba.Hierarchy;
+import edu.umd.cs.findbugs.ba.Location;
+import edu.umd.cs.findbugs.ba.RepositoryLookupFailureCallback;
+
+import org.apache.bcel.generic.ConstantPoolGen;
+import org.apache.bcel.generic.Instruction;
+import org.apache.bcel.generic.InvokeInstruction;
+import org.apache.bcel.generic.ObjectType;
+
+/**
+ * Factory for stream objects of a particular
+ * base class type returned by any method.
+ * This factory helps us keep track of streams returned
+ * by methods; we don't want to report them, but we do
+ * want to keep track of whether or not they are closed,
+ * to avoid reporting unclosed streams in the same
+ * equivalence class.
+ */
+public class AnyMethodReturnValueStreamFactory implements StreamFactory {
+	private ObjectType baseClassType;
+
+	public AnyMethodReturnValueStreamFactory(String streamBase) {
+		this.baseClassType = new ObjectType(streamBase);
+	}
+
+	public Stream createStream(Location location, ObjectType type, ConstantPoolGen cpg,
+		RepositoryLookupFailureCallback lookupFailureCallback) {
+
+		Instruction ins = location.getHandle().getInstruction();
+
+		try {
+			if (ins instanceof InvokeInstruction) {
+				if (!Hierarchy.isSubtype(type, baseClassType))
+					return null;
+	
+				return new Stream(location, type.getClassName(), baseClassType.getClassName())
+					.setIsOpenOnCreation(true)
+					.setIgnoreImplicitExceptions(true);
+			}
+		} catch (ClassNotFoundException e) {
+			lookupFailureCallback.reportMissingClass(e);
+		}
+
+		return null;
+	}
+}
+
+// vim:ts=4
