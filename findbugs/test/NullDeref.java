c7b0096e3483267aae12d857771cf7729d48d1d1
Bill Pugh <bill.pugh@gmail.com>
2004-08-06 13:29:07 +0000

    Check more cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2684 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/test/NullDeref.java
+++ findbugs/test/NullDeref.java
@@ -5,6 +5,7 @@ public class NullDeref {
 	public static void main(String[] argv) {
 		NullDeref n = new NullDeref();
 
+		String s = "foo";
 		n = null;
 
 		if (null == n)
@@ -15,13 +16,39 @@ public class NullDeref {
 			System.out.println("Bad idea: " + n.x);
 		} else {
 			System.out.println("Safe to deref here: " + n.x);
+			s = null;
 		}
 
-		// We don't detect that this can fail, because
-		// currently we don't have a way of remembering that
-		// n was originally null and that the IF statement didn't
-		// modify it.
-		n.x = 42;
+		// s can't be null
+		int i = s.hashCode();
+		s = null;
+
+		if (argv.length > 1) 
+			System.out.println("argv.length > 1");
+		else
+			System.out.println("argv.length <= 1");
+
+		// We should catch this one:
+		int j = s.hashCode();
+
+		s = argv[0];
+
+		if (s == null) 
+			System.out.println("s is null");
+
+		// at this point we know s is null on some 
+		// simple path
+
+		if (argv.length > 2) 
+			System.out.println("argv.length > 2");
+		else
+			System.out.println("argv.length <= 2");
+
+		// at this point we don't know anything about the
+		// state of s
+
+		// won't generate a warning here. Might be nice if we did
+		int k = s.hashCode();
 	}
 
 }
