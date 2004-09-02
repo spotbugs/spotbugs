5e93dd721791983dadcd9e3512c585272fa35e90
Dave Brosius <dbrosius@mebigfatguy.com>
2004-09-02 00:22:03 +0000

    fix compile on jsr14
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2803 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/test/CloneStringArray.java
+++ findbugs/test/CloneStringArray.java
@@ -1,6 +1,6 @@
 class CloneStringArray {
 	public static void main(String args[]) {
-		String [] copy = args.clone();
+		String [] copy = (String[])args.clone();
 		System.out.println(copy.toString());
 	}
 }
