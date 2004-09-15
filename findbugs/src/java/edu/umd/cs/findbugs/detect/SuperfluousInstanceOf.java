680fac103389198940a38bfde503fa32607993b0
Dave Brosius <dbrosius@mebigfatguy.com>
2004-09-15 04:53:55 +0000

    add correctness detector for usage of the instanceof operator, when the relationship of the operands in question can be determined statically. This usually means that the inheritance has changed since the instanceof code was added, or that there is a fundamental misconception about inheritance, both of which are likely signs of bugs.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2837 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/SuperfluousInstanceOf.java
@@ -0,0 +1,105 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2004 Dave Brosius <dbrosius@users.sourceforge.net>
+ * Copyright (C) 2004 University of Maryland
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.detect;
+
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.BytecodeScanningDetector;
+import edu.umd.cs.findbugs.visitclass.Constants2;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.classfile.Method;
+import org.apache.bcel.classfile.LocalVariableTable;
+import org.apache.bcel.classfile.LocalVariable;
+import org.apache.bcel.util.Repository;
+
+/**
+ * Find occurrences of a instanceof b where it can be determined
+ * statically whether this is true or false. This may signal a misunderstanding
+ * of the inheritance hierarchy in use, and potential bugs.
+ *
+ * @author Dave Brosius
+ */
+public class SuperfluousInstanceOf extends BytecodeScanningDetector implements Constants2 {
+
+	private static final int SEEN_NOTHING = 0;
+	private static final int SEEN_ALOAD = 1;
+	
+	private BugReporter bugReporter;
+	private Repository repository;
+	private LocalVariableTable varTable;
+	private int state;
+	private int register;
+
+	public SuperfluousInstanceOf(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+		repository = org.apache.bcel.Repository.getRepository();
+	}
+	
+	public void visit(Method obj) {
+		state = SEEN_NOTHING;
+		varTable = obj.getLocalVariableTable();
+		if (varTable != null)
+			super.visit(obj);
+	}
+	
+	public void sawOpcode(int seen) {
+		if (varTable == null)
+			return;
+
+		switch (state) {
+			case SEEN_NOTHING:
+				if (seen == ALOAD)
+					register = getRegisterOperand();
+				else if ((seen >= ALOAD_0) && (seen <= ALOAD_3))
+					register = seen - ALOAD_0;
+				else
+					return;
+				state = SEEN_ALOAD;
+			break;
+			
+			case SEEN_ALOAD:
+				try {
+					if (seen == INSTANCEOF) {
+						LocalVariable lv = varTable.getLocalVariable(register);
+						String objSignature = lv.getSignature();
+						if (objSignature.charAt(0) == 'L') {
+							objSignature = objSignature.substring(1, objSignature.length()-1).replace('/', '.');
+							String clsSignature = getDottedClassConstantOperand();
+							
+							if (org.apache.bcel.Repository.instanceOf( objSignature, clsSignature )) {
+								bugReporter.reportBug(new BugInstance("SIO_SUPERFLUOUS_INSTANCEOF", LOW_PRIORITY)
+						        	.addClassAndMethod(this)
+						        	.addSourceLine(this));
+							}
+						}
+					}
+				} catch (ClassNotFoundException cnfe) {
+					bugReporter.reportMissingClass(cnfe);
+				}
+				
+				state = SEEN_NOTHING;
+			break;
+		}
+		
+	}
+}
+
+// vim:ts=4
