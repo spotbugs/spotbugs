e3500a0abfca60d4ef9188b04461082d8096f776
David Hovemeyer <david.hovemeyer@gmail.com>
2004-09-15 15:41:41 +0000

    Initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2840 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ClassScreener.java
@@ -0,0 +1,102 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2004, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Class to pre-screen class files, so that only a subset are
+ * analyzed.  This supports the -onlyAnalyze command line option.
+ *
+ * @see FindBugs
+ * @author David Hovemeyer
+ */
+public class ClassScreener {
+	private static final boolean DEBUG = Boolean.getBoolean("findbugs.classscreener.debug");
+
+	private LinkedList<Pattern> patternList;
+
+	/**
+	 * Constructor.
+	 * By default, the ClassScreener will match <em>all</em> class files.
+	 * Once addAllowedClass() and addAllowedPackage() are called,
+	 * the ClassScreener will only match the explicitly specified classes
+	 * and packages.
+	 */
+	public ClassScreener() {
+		this.patternList = new LinkedList<Pattern>();
+	}
+
+	/**
+	 * Add the name of a class that should be matched by the screener.
+	 *
+	 * @param className name of a class that should be matched
+	 */
+	public void addAllowedClass(String className) {
+		String classRegex = "^.*\\Q" + className.replace('.', '/') + "\\E\\.class$";
+		if (DEBUG) System.out.println("Class regex: " + classRegex);
+		patternList.add(Pattern.compile(classRegex));
+	}
+
+	/**
+	 * Add the name of a package that should be matched by the screener.
+	 * All class files that appear to be in the package should be matched.
+	 *
+	 * @param packageName name of the package that should be matched
+	 */
+	public void addAllowedPackage(String packageName) {
+		// Note: \u0024 is the dollar sign ("$")
+		String packageRegex = "^.*\\Q" + packageName.replace('.', '/') + "\\E" +
+			"\\/[A-Za-z_\\u0024][A-Za-z_\\u0024\\d]*\\.class$";
+		if (DEBUG) System.out.println("Package regex: " + packageRegex);
+		patternList.add(Pattern.compile(packageRegex));
+	}
+
+	/**
+	 * Return whether or not the name of the given file matches.
+	 */
+	public boolean matches(String fileName) {
+		// Special case: if no classes or packages have been defined,
+		// then the screener matches all class files.
+		if (patternList.isEmpty())
+			return true;
+
+		if (DEBUG) System.out.println("Matching: " + fileName);
+
+		// Scan through list of regexes
+		for (Iterator<Pattern> i = patternList.iterator(); i.hasNext(); ) {
+			Pattern pattern = i.next();
+			if (DEBUG) System.out.print("\tTrying [" + pattern.toString());
+			Matcher matcher = pattern.matcher(fileName);
+			if (matcher.matches()) {
+				if (DEBUG) System.out.println("]: yes!");
+				return true;
+			}
+			if (DEBUG) System.out.println("]: no");
+		}
+		return false;
+	}
+}
+
+// vim:ts=4
---
+++ findbugs/src/junit/edu/umd/cs/findbugs/ClassScreenerTest.java
@@ -0,0 +1,94 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2004, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class ClassScreenerTest extends TestCase {
+	private ClassScreener emptyScreener;
+	private ClassScreener particularClassScreener;
+	private ClassScreener particularPackageScreener;
+
+	private static String makeFileName(String className) {
+		return className.replace('.', '/') + ".class";
+	}
+
+	private static String makeJarURL(String fileName) {
+		return "jar:http://foo.com/bar.jar!/" + fileName;
+	}
+
+	private static final String FOOBAR_PACKAGE = "com.foobar";
+	private static final String FURRYLEMUR_PACKAGE = "org.furrylemur";
+
+	private static final String SOME_CLASS = FOOBAR_PACKAGE + ".SomeClass";
+	private static final String SOME_OTHER_CLASS = FOOBAR_PACKAGE + ".SomeOtherClass";
+	private static final String UNRELATED_THING_CLASS = FURRYLEMUR_PACKAGE + ".UnrelatedThing";
+
+	private static final String SOME_CLASS_FILENAME = makeFileName(SOME_CLASS);
+	private static final String SOME_OTHER_CLASS_FILENAME = makeFileName(SOME_OTHER_CLASS);
+	private static final String UNRELATED_THING_CLASS_FILENAME = makeFileName(UNRELATED_THING_CLASS);
+
+	private static final String SOME_CLASS_JARFILENAME = makeJarURL(SOME_CLASS_FILENAME);
+	private static final String SOME_OTHER_CLASS_JARFILENAME = makeJarURL(SOME_OTHER_CLASS_FILENAME);
+	private static final String UNRELATED_THING_CLASS_JARFILENAME = makeJarURL(UNRELATED_THING_CLASS_FILENAME);
+
+	protected void setUp() {
+		emptyScreener = new ClassScreener();
+
+		particularClassScreener = new ClassScreener();
+		particularClassScreener.addAllowedClass(SOME_CLASS);
+
+		particularPackageScreener = new ClassScreener();
+		particularPackageScreener.addAllowedPackage(FOOBAR_PACKAGE);
+	}
+
+	public void testEmptyClassScreener() {
+		Assert.assertTrue(emptyScreener.matches(SOME_CLASS_FILENAME));
+		Assert.assertTrue(emptyScreener.matches(SOME_OTHER_CLASS_FILENAME));
+		Assert.assertTrue(emptyScreener.matches(UNRELATED_THING_CLASS_FILENAME));
+
+		Assert.assertTrue(emptyScreener.matches(SOME_CLASS_JARFILENAME));
+		Assert.assertTrue(emptyScreener.matches(SOME_OTHER_CLASS_JARFILENAME));
+		Assert.assertTrue(emptyScreener.matches(UNRELATED_THING_CLASS_JARFILENAME));
+	}
+
+	public void testParticularClassScreener() {
+		Assert.assertTrue(particularClassScreener.matches(SOME_CLASS_FILENAME));
+		Assert.assertFalse(particularClassScreener.matches(SOME_OTHER_CLASS_FILENAME));
+		Assert.assertFalse(particularClassScreener.matches(UNRELATED_THING_CLASS_FILENAME));
+
+		Assert.assertTrue(particularClassScreener.matches(SOME_CLASS_JARFILENAME));
+		Assert.assertFalse(particularClassScreener.matches(SOME_OTHER_CLASS_JARFILENAME));
+		Assert.assertFalse(particularClassScreener.matches(UNRELATED_THING_CLASS_JARFILENAME));
+	}
+
+	public void testParticularPackageScreener() {
+		Assert.assertTrue(particularPackageScreener.matches(SOME_CLASS_FILENAME));
+		Assert.assertTrue(particularPackageScreener.matches(SOME_OTHER_CLASS_FILENAME));
+		Assert.assertFalse(particularPackageScreener.matches(UNRELATED_THING_CLASS_FILENAME));
+
+		Assert.assertTrue(particularPackageScreener.matches(SOME_CLASS_JARFILENAME));
+		Assert.assertTrue(particularPackageScreener.matches(SOME_OTHER_CLASS_JARFILENAME));
+		Assert.assertFalse(particularPackageScreener.matches(UNRELATED_THING_CLASS_JARFILENAME));
+	}
+}
+
+// vim:ts=4
