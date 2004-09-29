fc02f032d31879b4c3fce804aa5b3bf34beaa618
David Hovemeyer <david.hovemeyer@gmail.com>
2004-09-29 14:03:24 +0000

    Added javadoc
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2890 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/ba/LoadedFieldSet.java
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/LoadedFieldSet.java
@@ -27,35 +27,82 @@ import java.util.Set;
 
 import org.apache.bcel.generic.InstructionHandle;
 
+/**
+ * Object which stores which fields are loaded and stored
+ * by the instructions in a method (including through inner-class
+ * access methods), and also which fields are loaded/stored
+ * by the overall method.  The main purpose is for doing
+ * redundant load elimination and forward substitution
+ * more efficiently, but it might be useful in other situations.
+ *
+ * @author David Hovemeyer
+ */
 public class LoadedFieldSet {
 	private Set<XField> loadedFieldSet;
 	private Map<InstructionHandle, XField> handleToFieldMap;
 	private BitSet loadHandleSet;
 
+	/**
+	 * Constructor.
+	 * Constructs an empty object.
+	 */
 	public LoadedFieldSet() {
 		this.loadedFieldSet = new HashSet<XField>();
 		this.handleToFieldMap = new HashMap<InstructionHandle, XField>();
 		this.loadHandleSet = new BitSet();
 	}
 
+	/**
+	 * Add a load of given field at given instruction.
+	 *
+	 * @param handle the instruction
+	 * @param field  the field
+	 */
 	public void addLoad(InstructionHandle handle, XField field) {
 		loadedFieldSet.add(field);
 		handleToFieldMap.put(handle, field);
 		loadHandleSet.set(handle.getPosition());
 	}
 
+	/**
+	 * Add a store of given field at given instruction.
+	 *
+	 * @param handle the instruction
+	 * @param field  the field
+	 */
 	public void addStore(InstructionHandle handle, XField field) {
 		handleToFieldMap.put(handle, field);
 	}
 
+	/**
+	 * Get the field loaded or stored at given instruction, if any.
+	 *
+	 * @param handle the instruction
+	 * @return the field loaded or stored at the instruction, or null
+	 *         if the instruction is not a load or store
+	 */
 	public XField getField(InstructionHandle handle) {
 		return handleToFieldMap.get(handle);
 	}
 
+	/**
+	 * Return whether or not the given field is loaded by any
+	 * instruction in the method.
+	 *
+	 * @param field the field
+	 * @return true if the field is loaded somewhere in the method,
+	 *         false if it is never loaded
+	 */
 	public boolean isLoaded(XField field) {
 		return loadedFieldSet.contains(field);
 	}
 
+	/**
+	 * Return whether or not the given instruction is a load.
+	 * 
+	 * @param handle the instruction
+	 * @return true if the instruction is a load, false if not
+	 */
 	public boolean instructionIsLoad(InstructionHandle handle) {
 		return loadHandleSet.get(handle.getPosition());
 	}
