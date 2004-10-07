0403fad29d29fcdf24f6d733e186e37eeb0b80c4
Bill Pugh <bill.pugh@gmail.com>
2004-10-07 15:40:33 +0000

    Improve accuracy. Make medium priority only if field is initialized and never changed
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3000 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/VolatileUsage.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/VolatileUsage.java
@@ -33,7 +33,7 @@ import edu.umd.cs.findbugs.visitclass.Constants2;
 import org.apache.bcel.Repository;
 import org.apache.bcel.classfile.*;
 
-public class VolatileUsage extends PreorderVisitor implements Detector, 
+public class VolatileUsage extends BytecodeScanningDetector implements 
 		Constants2 {
 	  private BugReporter bugReporter;
 	private AnalysisContext analysisContext;
@@ -49,18 +49,73 @@ public class VolatileUsage extends PreorderVisitor implements Detector,
                 this.analysisContext = analysisContext;
         }
 
+static class FieldRecord {
+                String className;
+                String name;
+                String signature;
+                boolean isStatic;
+        }
+
+
+	HashMap<String,FieldRecord> fieldInfo = new HashMap<String,FieldRecord>();
+	HashSet<String> initializationWrites = new HashSet<String>();
+	HashSet<String> otherWrites = new HashSet<String>();
+	
+
 	public void visit(Field obj) {
 		super.visit(obj);
 		int flags = obj.getAccessFlags();
 		if ((flags & ACC_VOLATILE) == 0) return;
-		if (getFieldSig().charAt(0) == '[')
-		   bugReporter.reportBug(
-			new BugInstance("VO_VOLATILE_REFERENCE_TO_ARRAY", 
-					NORMAL_PRIORITY)
-				.addClass(this)
-		.addField(FieldAnnotation.fromVisitedField(this)));
+		if (getFieldSig().charAt(0) == '[') {
+		   FieldRecord f = new FieldRecord();
+			f.className = getDottedClassName();
+			f.name = getFieldName();
+			f.signature = getDottedFieldSig();
+			f.isStatic = !((flags & ACC_STATIC) == 0);
+			fieldInfo.put(getDottedClassName() + "." + getFieldName(),
+					f);
+		}
 	}
 
+    public void sawOpcode(int seen) {
+                switch (seen) {
+                case PUTSTATIC:
+			{
+                        String name = (getClassConstantOperand() 
+					+ "." + getNameConstantOperand())
+                                .replace('/', '.');
+			if (getMethodName().equals("<clinit>"))
+				initializationWrites.add(name);
+			else otherWrites.add(name);
+			break;
+			}
+                case PUTFIELD:
+                        {
+			String name = (getClassConstantOperand() 
+					+ "." + getNameConstantOperand())
+                                .replace('/', '.');
+			if (getMethodName().equals("<init>"))
+				initializationWrites.add(name);
+			else otherWrites.add(name);
+			break;
+			}
+		}
+		}
+				
+
 	public void report() {
+
+		for(Map.Entry<String, FieldRecord> r : fieldInfo.entrySet()) {	
+		   String name = r.getKey();
+		   FieldRecord f = r.getValue();
+		   int priority = LOW_PRIORITY;
+		   if (initializationWrites.contains(name) 
+			&& !otherWrites.contains(name))
+		     priority = NORMAL_PRIORITY;
+		   bugReporter.reportBug(
+			new BugInstance("VO_VOLATILE_REFERENCE_TO_ARRAY", priority)
+			 .addClass(f.className)
+			 .addField(f.className, f.name, f.signature, f.isStatic));
+		}
 		}
 }
