2c660c19bd4935fddcc168bc77cd575200430908
David Hovemeyer <david.hovemeyer@gmail.com>
2004-10-07 18:44:41 +0000

    Added some DNR tests
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3007 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/test/DeadStore.java
+++ findbugs/test/DeadStore.java
@@ -5,4 +5,39 @@ public class DeadStore {
 
 		System.out.println(live);
 	}
+
+	public int finalLocalDNR(int a) {
+		final int SCAN = 0;    //<<---- complains about this line
+		final int STAR = 1;
+		final int DONE = 2;
+		//int state = SCAN;
+
+		a += SCAN;
+		a += STAR;
+		a += DONE;
+
+		return a;
+	}
+
+	public int storeNullDNR(int a) {
+		Object foo = null;
+		return a;
+	}
+
+	public int storeZeroDNR(int a) {
+		int count = 0;
+
+		return a;
+	}
+
+	public int killedByStoreDNR(int a) {
+		int b = 3;
+
+		if (a > 1) {
+			b = 4;
+			a += b;
+		}
+
+		return a;
+	}
 }
