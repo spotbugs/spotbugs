8192e4b4beb93dcdcc3effb4d13bc3ccddbd5fba
Phil Crosby <philc@umd.edu>
2004-10-07 07:18:30 +0000

    Support hyperlinks for the "leenux fellars"
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@2996 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/gui/AboutDialog.java
+++ findbugs/src/java/edu/umd/cs/findbugs/gui/AboutDialog.java
@@ -202,18 +202,47 @@ public class AboutDialog extends javax.swing.JDialog {
         try {
             if (evt.getEventType().equals( HyperlinkEvent.EventType.ACTIVATED)) {
                 URL url = evt.getURL();
-                String os = System.getProperty("os.name").toLowerCase();
-                Runtime rt = Runtime.getRuntime();
-                if (os.indexOf( "win" ) >= 0) {
-                    rt.exec( "rundll32 url.dll,FileProtocolHandler " + url.toString());
-                } else if (os.indexOf( "mac" ) >= 0) {
-                    rt.exec( "open " + url.toString());
-                }
+                showInBrowser(url.toString());
             }
         }
         catch (Exception e) {
         }
     }//GEN-LAST:event_editorPaneHyperlinkUpdate
+    
+    /**
+     * Attempts to show the given URL in the OS's web browser.
+     * @param url url to show
+     * @return true if the show operation was successful, false otherwise.
+     */
+    private static boolean showInBrowser(String url){
+    	String os = System.getProperty("os.name").toLowerCase();
+        Runtime rt = Runtime.getRuntime();
+        try{
+	        if (os.indexOf( "win" ) >= 0) {
+	        	// this doesn't support showing urls in the form of "page.html#nameLink" 
+	            rt.exec( "rundll32 url.dll,FileProtocolHandler " + url.toString());
+	        } else if (os.indexOf( "mac" ) >= 0) {
+	            rt.exec( "open " + url.toString());
+	        } else if (os.indexOf( "nix") >=0 || os.indexOf( "nux") >=0) {
+	        	// Do a best guess on unix until we get a platform independent way
+	        	// Build a list of browsers to try, in this order.
+	        	String[] browsers = {"epiphany", "firefox", "mozilla", "konqueror",
+	        			"netscape","opera","links","lynx"};
+	        	
+	        	// Build a command string which looks like "browser1 "url" || browser2 "url" ||..."
+	        	StringBuffer cmd = new StringBuffer();
+	        	for (int i=0; i<browsers.length; i++)
+	        		cmd.append( (i==0  ? "" : " || " ) + browsers[i] +" \"" + url + "\" ");
+	        	
+	        	rt.exec(new String[] { "sh", "-c", cmd.toString() });
+	        } else {
+	        	return false;
+	        }
+        }catch (IOException e){
+        	return false;
+        }
+        return true;
+    }
 
 	private void okButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_okButtonActionPerformed
 		closeDialog();
