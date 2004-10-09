9fdfdda3be8978c80f1daa785881e4cead41c0e9
Dave Brosius <dbrosius@mebigfatguy.com>
2004-10-09 05:56:24 +0000

    bah, do it right
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3020 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/test/InheritanceUnsafeClass.java
+++ findbugs/test/InheritanceUnsafeClass.java
@@ -3,25 +3,21 @@ import java.io.InputStream;
 
 public class InheritanceUnsafeClass
 {
+	//This detector only reports one per class, so rearrange to check
+	
+	public InputStream getResourceOK(String r)
+	{//This should not report a bug
+		return InheritanceUnsafeClass.class.getResourceAsStream(r);
+	}
+
     public URL getResource(String r)
     {
         return getClass().getResource(r);
     }
-}
 
-class InheritanceUnsafeClass2
-{	
 	public InputStream getResourceBAD(String r)
 	{
 		return getClass().getResourceAsStream(r);
-	}
-}
-
-class InheritanceUnsafeClass3
-{
-	public InputStream getResourceOK(String r)
-	{//This should not report a bug
-		return InheritanceUnsafeClass.class.getResourceAsStream(r);
 	}
 }
 
