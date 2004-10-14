d1ca476179898f96cc994c0c8f9c986171d68259
Bill Pugh <bill.pugh@gmail.com>
2004-10-14 01:38:37 +0000

    Detector to look for infinite recursive loops. Might not occur much in production code, but I've been seeing a lot of it in student code.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3045 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/InfiniteRecursiveLoop.java
@@ -0,0 +1,73 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2004, Tom Truscott <trt@unx.sas.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.detect;
+
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.BytecodeScanningDetector;
+import edu.umd.cs.findbugs.visitclass.Constants2;
+import org.apache.bcel.classfile.*;
+
+public class InfiniteRecursiveLoop extends BytecodeScanningDetector implements Constants2 {
+
+	private BugReporter bugReporter;
+	private boolean seenTransferOfControl;
+
+	public InfiniteRecursiveLoop(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
+
+
+	public void visit(JavaClass obj) {
+	}
+
+	public void visit(Method obj) {
+		seenTransferOfControl = false;
+	}
+
+	public void sawOffset(int offset) {
+		seenTransferOfControl = true;
+	}
+
+	public void sawOpcode(int seen) {
+
+		switch(seen) {
+			case ARETURN:
+			case IRETURN:
+			case LRETURN:
+			case RETURN:
+			case DRETURN:
+			case FRETURN:
+				seenTransferOfControl = true;
+			}
+		if (seenTransferOfControl) return;
+		
+		if ((seen == INVOKEVIRTUAL) || (seen == INVOKESPECIAL) || (seen == INVOKEINTERFACE) || (seen == INVOKESTATIC)) {
+			if (getClassConstantOperand().equals(getClassName())
+			    && getNameConstantOperand().equals(getMethodName())
+			    && getSigConstantOperand().equals(getMethodSig()))
+
+				bugReporter.reportBug(new BugInstance("IL_INFINITE_RECURSIVE_LOOP", HIGH_PRIORITY)
+				        .addClassAndMethod(this));
+			}
+
+	}
+
+}
