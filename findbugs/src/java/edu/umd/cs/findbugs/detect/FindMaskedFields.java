6b01caa04805d5f85ec6e74bcf4fa3f3271f2725
David Hovemeyer <david.hovemeyer@gmail.com>
2004-11-19 17:22:12 +0000

    Moved back to detect pacakge
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3183 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/FindMaskedFields.java
@@ -0,0 +1,166 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2004 Dave Brosius <dbrosius@users.sourceforge.net>
+ * Copyright (C) 2003,2004 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.detect;
+
+import java.util.*;
+
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.BytecodeScanningDetector;
+import edu.umd.cs.findbugs.FieldAnnotation;
+import edu.umd.cs.findbugs.visitclass.Constants2;
+import edu.umd.cs.findbugs.ba.ClassContext;
+import org.apache.bcel.classfile.*;
+
+public class FindMaskedFields extends BytecodeScanningDetector implements Constants2 {
+	private BugReporter bugReporter;
+	private int numParms;
+	private Set<Field> maskedFields = new HashSet<Field>();
+	private Map<String, Field> classFields = new HashMap<String, Field>();
+	private boolean staticMethod;
+
+	public FindMaskedFields(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
+
+	public void visitClassContext(ClassContext classContext) {
+		JavaClass obj = classContext.getJavaClass();
+		if (!obj.isInterface())
+			classContext.getJavaClass().accept(this);
+	}
+
+	public void visit(JavaClass obj) {
+		classFields.clear();
+
+		Field[] fields = obj.getFields();
+		String fieldName;
+		for (int f = 0; f < fields.length; f++) {
+			fieldName = fields[f].getName();
+			classFields.put(fieldName, fields[f]);
+		}
+		
+		// Walk up the super class chain, looking for name collisions
+		try {
+			JavaClass[] superClasses = org.apache.bcel.Repository.getSuperClasses(obj);
+			for (int c = 0; c < superClasses.length; c++) {
+				fields = superClasses[c].getFields();
+				for (int f = 0; f < fields.length; f++) {
+					Field fld = fields[f];
+					if (!fld.isStatic()
+					        && !maskedFields.contains(fld)
+					        && (fld.isPublic() || fld.isProtected())) {
+						fieldName = fld.getName();
+						if (fieldName.length() == 1)
+							continue;
+						if (fieldName.equals("serialVersionUID"))
+							continue;
+						if (classFields.containsKey(fieldName)) {
+							maskedFields.add(fld);
+							Field maskingField = classFields.get(fieldName);
+							FieldAnnotation fa = new FieldAnnotation(getDottedClassName(),
+							        maskingField.getName(),
+							        maskingField.getSignature(),
+							        maskingField.isStatic());
+							int priority = NORMAL_PRIORITY;
+							if (maskingField.isStatic()
+							        || maskingField.isFinal())
+								priority++;
+							else if (fld.getSignature().charAt(0) == 'L'
+							        && !fld.getSignature().startsWith("Ljava/lang/")
+							        || fld.getSignature().charAt(0) == '[')
+								priority--;
+							if (fld.getAccessFlags()
+							        != maskingField.getAccessFlags())
+								priority++;
+							if (!fld.getSignature().equals(maskingField.getSignature()))
+								priority++;
+
+							FieldAnnotation maskedFieldAnnotation
+							        = FieldAnnotation.fromBCELField(superClasses[c].getClassName(), fld);
+							bugReporter.reportBug(new BugInstance(this, "MF_CLASS_MASKS_FIELD",
+							        priority)
+							        .addClass(this)
+							        .addField(maskedFieldAnnotation)
+							        .describe("FIELD_MASKED")
+							        .addField(fa)
+							        .describe("FIELD_MASKING"));
+						}
+					}
+				}
+			}
+		} catch (ClassNotFoundException e) {
+			bugReporter.reportMissingClass(e);
+		}
+
+		super.visit(obj);
+	}
+
+	public void visit(Method obj) {
+		super.visit(obj);
+		numParms = obj.getArgumentTypes().length;
+		if (!obj.isStatic()) numParms++;
+		// System.out.println(obj);
+		// System.out.println(numParms);
+		staticMethod = obj.isStatic();
+	}
+
+	/**
+	 * This property enables production of warnings for
+	 * locals which obscure fields.
+	 */
+	private static final boolean ENABLE_LOCALS =
+		Boolean.getBoolean("findbugs.maskedfields.locals");
+
+	public void visit(LocalVariableTable obj) {
+		if (ENABLE_LOCALS) {
+			if (staticMethod)
+				return;
+
+			LocalVariable[] vars = obj.getLocalVariableTable();
+			// System.out.println("Num params = " + numParms);
+			for (int v = 0; v < vars.length; v++) {
+				LocalVariable var = vars[v];
+				if (var.getIndex() < numParms)
+					continue;
+				String varName = var.getName();
+				if (varName.equals("serialVersionUID"))
+					continue;
+				Field f = classFields.get(varName);
+				// System.out.println("Checking " + varName);
+				// System.out.println(" got " + f);
+				// TODO: we could distinguish between obscuring a field in the same class
+				// vs. obscuring a field in a superclass.  Not sure how important that is.
+				if (f != null) {
+					FieldAnnotation fa
+					        = FieldAnnotation.fromBCELField(getClassName(), f);
+					if (true || var.getStartPC() > 0)
+						bugReporter.reportBug(new BugInstance(this, "MF_METHOD_MASKS_FIELD", LOW_PRIORITY)
+						        .addClassAndMethod(this)
+						        .addField(fa)
+						        .addSourceLine(this, var.getStartPC() - 1));
+				}
+			}
+		}
+		super.visit(obj);
+	}
+}
+
+// vim:ts=4
---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/WrongMapIterator.java
@@ -0,0 +1,219 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2004 Dave Brosius <dbrosius@users.sourceforge.net>
+ * Copyright (C) 2004 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+ 
+package edu.umd.cs.findbugs.detect;
+
+import edu.umd.cs.findbugs.*;
+import java.util.*;
+import org.apache.bcel.classfile.*;
+import edu.umd.cs.findbugs.visitclass.DismantleBytecode;
+import edu.umd.cs.findbugs.visitclass.Constants2;
+
+public class WrongMapIterator extends BytecodeScanningDetector implements   Constants2 {
+    private BugReporter bugReporter;
+    private static final int SAW_NOTHING = 0;
+    private static final int SAW_MAP_LOAD1 = 1;
+    private static final int SAW_KEYSET = 2;
+    private static final int SAW_KEYSET_STORE = 3;    
+    private static final int SAW_ITERATOR = 4;
+    private static final int SAW_ITERATOR_STORE = 5;
+    private static final int SAW_ITERATOR_LOAD = 6;
+    private static final int SAW_NEXT = 7;
+    private static final int SAW_CHECKCAST_ON_NEXT = 8;
+    private static final int SAW_KEY_STORE = 9;
+    private static final int NEED_KEYSET_LOAD = 10;
+	private static final int SAW_MAP_LOAD2 = 11;    
+	private static final int SAW_KEY_LOAD = 12;
+    private static final int SAW_MAP_GET = 13;
+    
+    private int state;
+    private int loadedRegister;
+    private int mapRegister;
+    private int keySetRegister;
+    private int iteratorRegister;
+    private int keyRegister;
+    
+
+    public WrongMapIterator(BugReporter bugReporter) {
+        this.bugReporter = bugReporter;
+    }
+    
+    public void visit(Method obj) {
+    	state = SAW_NOTHING;
+    	loadedRegister = -1;
+    	mapRegister = -1;
+    	keySetRegister = -1;
+    	iteratorRegister = -1;
+    	keyRegister = -1;
+    }
+    
+    public void sawOpcode(int seen) {
+    	switch (state)
+    	{
+    		case SAW_NOTHING:
+    			loadedRegister = getLoadStoreRegister( seen, true );
+    			if (loadedRegister >= 0)
+    				state = SAW_MAP_LOAD1;
+    		break;
+    		
+    		case SAW_MAP_LOAD1:
+    			if (((seen == INVOKEINTERFACE) || (seen == INVOKEVIRTUAL))
+    			&&  ("keySet".equals(getNameConstantOperand()))
+    			&&  ("()Ljava/util/Set;".equals(getSigConstantOperand()))) {
+    				mapRegister = loadedRegister;
+    				state = SAW_KEYSET;
+    			}
+    			else {
+    				state = SAW_NOTHING;
+    			}
+    		break;
+    		
+    		case SAW_KEYSET:
+    			keySetRegister = getLoadStoreRegister( seen, false );
+    			if (keySetRegister >= 0)
+    				state = SAW_KEYSET_STORE;
+				else if ((seen == INVOKEINTERFACE)
+    			&&  ("iterator".equals(getNameConstantOperand()))
+    			&&  ("()Ljava/util/Iterator;".equals(getSigConstantOperand())))
+    				state = SAW_ITERATOR;
+    			else
+    				state = SAW_NOTHING;
+    		break;
+    		
+    		case SAW_KEYSET_STORE:	
+    			if ((seen == INVOKEINTERFACE)
+    			&&  ("iterator".equals(getNameConstantOperand()))
+    			&&  ("()Ljava/util/Iterator;".equals(getSigConstantOperand())))
+    				state = SAW_ITERATOR;
+    			else
+    				state = NEED_KEYSET_LOAD;
+    		break;
+    		
+    		case NEED_KEYSET_LOAD:
+    			loadedRegister = getLoadStoreRegister( seen, true );
+    			if (loadedRegister == iteratorRegister)
+    				state = SAW_ITERATOR;
+    		break;
+    		
+    		case SAW_ITERATOR:
+    			iteratorRegister = getLoadStoreRegister( seen, false );
+    			if (iteratorRegister >= 0)
+    				state = SAW_ITERATOR_STORE;
+    			else
+    				state = SAW_NOTHING;
+    		break;
+    		
+    		case SAW_ITERATOR_STORE:
+    			loadedRegister = getLoadStoreRegister( seen, true );
+    			if (loadedRegister == iteratorRegister)
+    				state = SAW_ITERATOR_LOAD;
+    		break;
+    		
+    		case SAW_ITERATOR_LOAD:
+    			if ((seen == INVOKEINTERFACE)
+    			&&  ("next".equals(getNameConstantOperand()))
+    			&&  ("()Ljava/lang/Object;".equals(getSigConstantOperand())))
+    				state = SAW_NEXT;
+    			else
+    				state = SAW_ITERATOR_STORE;
+    		break;
+    		
+    		case SAW_NEXT:
+    			if (seen == CHECKCAST)
+    				state = SAW_CHECKCAST_ON_NEXT;
+   				else {
+   					keyRegister = getLoadStoreRegister( seen, false );
+   					if (keyRegister >= 0)
+   						state = SAW_KEY_STORE;
+   					else
+   						seen = SAW_NOTHING;
+   				}
+     		break;
+    		
+    		case SAW_CHECKCAST_ON_NEXT:
+    			keyRegister = getLoadStoreRegister( seen, false );
+    			if (keyRegister >= 0)
+    				state = SAW_KEY_STORE;
+    		break;
+    		
+    		case SAW_KEY_STORE:
+    			loadedRegister = getLoadStoreRegister( seen, true );
+    			if (loadedRegister == mapRegister)
+    				state = SAW_MAP_LOAD2;
+    		break;
+    		
+    		case SAW_MAP_LOAD2:
+    			loadedRegister = getLoadStoreRegister( seen, true );
+    			if (loadedRegister == keyRegister)
+    				state = SAW_KEY_LOAD;
+    			else
+    				state = SAW_KEY_STORE;
+    		break;
+    		
+    		case SAW_KEY_LOAD:
+    			if (((seen == INVOKEINTERFACE) || (seen == INVOKEVIRTUAL))
+    			&&  ("get".equals(getNameConstantOperand()))
+    			&&  ("(Ljava/lang/Object;)Ljava/lang/Object;".equals(getSigConstantOperand()))) {
+					MethodAnnotation ma = MethodAnnotation.fromVisitedMethod(this);
+			    	bugReporter.reportBug(
+						new BugInstance("WMI_WRONG_MAP_ITERATOR", NORMAL_PRIORITY)
+			                                        	.addClass(this)
+			                                        	.addMethod(ma)
+			                                        	.addSourceLine(this, getPC()));
+			    	state = SAW_NOTHING;
+				}
+			break;
+    	}
+    }
+    
+    private int getLoadStoreRegister(int seen, boolean doLoad)
+    {
+    	switch (seen)
+    	{
+    		case ALOAD_0:
+    		case ALOAD_1:
+    		case ALOAD_2:
+    		case ALOAD_3:
+    			if (doLoad)
+    				return seen - ALOAD_0;
+    		break;
+    		
+    		case ALOAD:
+    			if (doLoad)
+    				return getRegisterOperand();
+    		break;
+    			
+    		case ASTORE_0:
+    		case ASTORE_1:
+    		case ASTORE_2:
+    		case ASTORE_3:
+    			if (!doLoad)
+    				return seen - ASTORE_0;
+    		break;
+     			
+    		case ASTORE:
+    			if (!doLoad)
+    				return getRegisterOperand();
+    		break;
+    	}
+    	
+    	return -1;
+    }
+}
