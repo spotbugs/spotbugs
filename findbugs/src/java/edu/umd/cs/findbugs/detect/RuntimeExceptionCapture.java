b53f8e618ad4635cb1839316d862b8ea2b9ec6a2
Dave Brosius <dbrosius@mebigfatguy.com>
2004-12-11 05:11:40 +0000

    add copyright/license stuff
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3297 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/RuntimeExceptionCapture.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/RuntimeExceptionCapture.java
@@ -1,3 +1,23 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2004 Brian Goetz <briangoetz@users.sourceforge.net>
+ * Copyright (C) 2004 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 package edu.umd.cs.findbugs.detect;
 
 import java.util.*;
@@ -19,7 +39,7 @@ public class RuntimeExceptionCapture extends BytecodeScanningDetector implements
 	private List<CaughtException> catchList;
 	private List<ThrownException> throwList;
 
-	private class CaughtException {
+	private static class CaughtException {
 		public String exceptionClass;
 		public int startOffset, endOffset, sourcePC;
 		public boolean seen = false;
@@ -32,7 +52,7 @@ public class RuntimeExceptionCapture extends BytecodeScanningDetector implements
 		}
 	}
 
-	private class ThrownException {
+	private static class ThrownException {
 		public String exceptionClass;
 		public int offset;
 
