1d2a726b75eb06c67d5dfda13796f6d6d8d80ccf
David Hovemeyer <david.hovemeyer@gmail.com>
2004-12-16 21:01:27 +0000

    Initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3339 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/BlockType.java
@@ -0,0 +1,239 @@
+/*
+ * Bytecode Analysis Framework
+ * Copyright (C) 2004, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.ba;
+
+import java.util.BitSet;
+
+/**
+ * Dataflow value representing the current nesting of
+ * catch and finally blocks.  We assume that any catch block
+ * with a non-empty catch type is a user catch block,
+ * and any catch block with an empty catch type (i.e., catch all
+ * exceptions) is a finally block.  This assumption isn't quite
+ * accurate, but it seems to be a reasonable first approximation.
+ *
+ * <p>If valid (isValid() returns true),
+ * a BlockType value is a stack of elements, which are either CATCH
+ * or FINALLY values.  Call getDepth() to get the current
+ * nesting depth.  Call get(int <i>n</i>) to get the
+ * <i>n</i>th stack item. Call getTopValue() to get the current
+ * top of the stack.</p>
+ *
+ * <p>If invalid (isValid() returns false),
+ * a BlockType value is either <i>top</i> or <i>bottom</i>.
+ * These are the special values at the top and bottom of
+ * the dataflow lattice.</p>
+ *
+ * <p>The dataflow lattice is effectively finite-height because
+ * real Java methods are guarnateed to have a finite 
+ * catch and finally block nesting level.</p>
+ *
+ * @see BlockTypeAnalysis
+ * @author David Hovemeyer
+ */
+public class BlockType extends BitSet {
+	public static final boolean CATCH = false;
+	public static final boolean FINALLY = true;
+
+	private boolean isValid;
+	private boolean isTop;
+	private int depth;
+
+	/**
+	 * Constructor.
+	 * Should only be called by BlockTypeAnalysis.
+	 */
+	BlockType() {
+	}
+
+	/**
+	 * Return whether or not this value is valid,
+	 * meaning it contains a valid representation of the
+	 * nesting of catch and finally blocks.
+	 */
+	public boolean isValid() {
+		return isValid;
+	}
+
+	/**
+	 * Get the current nesting depth.
+	 * The value must be valid.
+	 */
+	public int getDepth() {
+		if (!isValid) throw new IllegalStateException();
+		return depth;
+	}
+
+	/**
+	 * Get the top value on the catch and finally block nesting stack.
+	 */
+	public boolean getTopValue() {
+		if (depth == 0) throw new IllegalStateException();
+		return get(depth - 1);
+	}
+
+	/**
+	 * Return whether or not this value represents "normal" control-flow.
+	 * Normal control flow are all blocks outside any catch or finally block.
+	 */
+	public boolean isNormal() {
+		if (!isValid) throw new IllegalStateException();
+		return getDepth() == 0;
+	}
+
+	/**
+	 * Make this value represent "normal" control flow.
+	 */
+	public void setNormal() {
+		this.isValid = true;
+		this.depth = 0;
+	}
+
+	/**
+	 * Return whether or not this is the special "top" dataflow value.
+	 */
+	public boolean isTop() {
+		return !isValid && isTop;
+	}
+
+	/**
+	 * Make this the special "top" dataflow value.
+	 */
+	public void setTop() {
+		this.isValid = false;
+		this.isTop = true;
+	}
+
+	/**
+	 * Return whether or not this is the special "bottom" dataflow value.
+	 */
+	public boolean isBottom() {
+		return !isValid && !isTop;
+	}
+
+	/**
+	 * Make this the special "bottom" dataflow value.
+	 */
+	public void setBottom() {
+		this.isValid = false;
+		this.isTop = false;
+	}
+
+	/**
+	 * Make this object an exact duplicate of given object.
+	 *
+	 * @param other the other BlockType object
+	 */
+	public void copyFrom(BlockType other) {
+		this.isValid = other.isValid;
+		this.isTop = other.isTop;
+		if (isValid) {
+			this.depth = other.depth;
+			this.clear();
+			this.or(other);
+		}
+	}
+
+	/**
+	 * Return whether or not this object is identical to the one given.
+	 *
+	 * @param other the other BlockType object
+	 * @return true if this object is identical to the one given,
+	 *         false otherwise
+	 */
+	public boolean sameAs(BlockType other) {
+		if (!this.isValid) {
+			return !other.isValid
+				&& (this.isTop == other.isTop);
+		} else {
+			if (!other.isValid)
+				return false;
+			else {
+				// Both facts are valid
+				if (this.depth != other.depth)
+					return false;
+
+				// Compare bits
+				for (int i = 0; i < this.depth; ++i) {
+					if (this.get(i) != other.get(i))
+						return false;
+				}
+
+				return true;
+			}
+		}
+	}
+
+	/**
+	 * Merge other dataflow value into this value.
+	 *
+	 * @param other the other BlockType value
+	 */
+	public void mergeWith(BlockType other) {
+		if (this.isTop() || other.isBottom()) {
+			copyFrom(other);
+		} else if (isValid()) {
+			// To merge, we take the common prefix
+			int pfxLen = Math.min(this.depth, other.depth);
+			int commonLen;
+			for (commonLen = 0; commonLen < pfxLen; ++commonLen) {
+				if (this.get(commonLen) != other.get(commonLen))
+					break;
+			}
+			this.depth = commonLen;
+		}
+	}
+
+	/**
+	 * Enter a catch block.
+	 */
+	public void pushCatch() {
+		push(CATCH);
+	}
+
+	/**
+	 * Enter a finally block.
+	 */
+	public void pushFinally() {
+		push(FINALLY);
+	}
+
+	public String toString() {
+		if (isTop())
+			return "<top>";
+		else if (isBottom())
+			return "<bottom>";
+		else {
+			StringBuffer buf = new StringBuffer();
+			buf.append("N");
+			for (int i = 0; i < depth; ++i) {
+				buf.append(get(i) == CATCH ? "C" : "F");
+			}
+			return buf.toString();
+		}
+	}
+
+	private void push(boolean value) {
+		set(depth, value);
+		++depth;
+	}
+}
+
+// vim:ts=4
---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/BlockTypeAnalysis.java
@@ -0,0 +1,184 @@
+/*
+ * Bytecode Analysis Framework
+ * Copyright (C) 2004, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.ba;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.bcel.classfile.ClassParser;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.classfile.Method;
+
+import org.apache.bcel.generic.CodeExceptionGen;
+import org.apache.bcel.generic.InstructionHandle;
+import org.apache.bcel.generic.ObjectType;
+
+/**
+ * Dataflow analysis to determine the nesting of catch and finally
+ * blocks within a method.
+ *
+ * @see BlockType
+ * @author David Hovemeyer
+ */
+public class BlockTypeAnalysis implements DataflowAnalysis<BlockType> {
+	private DepthFirstSearch dfs;
+	private Map<BasicBlock, BlockType> startFactMap;
+	private Map<BasicBlock, BlockType> resultFactMap;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param dfs a DepthFirstSearch for the method to be analyzed
+	 */
+	public BlockTypeAnalysis(DepthFirstSearch dfs) {
+		this.dfs = dfs;
+		this.startFactMap = new HashMap<BasicBlock, BlockType>();
+		this.resultFactMap = new HashMap<BasicBlock, BlockType>();
+	}
+
+	public BlockType createFact() {
+		return new BlockType();
+	}
+
+	public BlockType getStartFact(BasicBlock block) {
+		return lookupOrCreateFact(startFactMap, block);
+	}
+
+	public BlockType getResultFact(BasicBlock block) {
+		return lookupOrCreateFact(resultFactMap, block);
+	}
+
+	public void copy(BlockType source, BlockType dest) {
+		dest.copyFrom(source);
+	}
+
+	public void initEntryFact(BlockType result) throws DataflowAnalysisException {
+		result.setNormal();
+	}
+
+	public void initResultFact(BlockType result) {
+		makeFactTop(result);
+	}
+
+	public void makeFactTop(BlockType fact) {
+		fact.setTop();
+	}
+
+	public boolean isForwards() {
+		return true;
+	}
+
+	public BlockOrder getBlockOrder(CFG cfg) {
+		return new ReversePostfixOrder(cfg, dfs);
+	}
+
+	public boolean same(BlockType fact1, BlockType fact2) {
+		return fact1.sameAs(fact2);
+	}
+
+	public void transfer(BasicBlock basicBlock, InstructionHandle end, BlockType start, BlockType result)
+			throws DataflowAnalysisException {
+		result.copyFrom(start);
+
+		if (start.isValid()) {
+			if (basicBlock.isExceptionHandler()) {
+				CodeExceptionGen exceptionGen = basicBlock.getExceptionGen();
+				ObjectType catchType = exceptionGen.getCatchType();
+				if (catchType == null) {
+					// Probably a finally block, or a synchronized block
+					// exception-compensation catch block.
+					result.pushFinally();
+				} else {
+					// Catch type was explicitly specified:
+					// this is probably a programmer-written catch block
+					result.pushCatch();
+				}
+			}
+		}
+	}
+
+	public void meetInto(BlockType fact, Edge edge, BlockType result) throws DataflowAnalysisException {
+		result.mergeWith(fact);
+	}
+
+	private BlockType lookupOrCreateFact(Map<BasicBlock, BlockType> map, BasicBlock block) {
+		BlockType fact = map.get(block);
+		if (fact == null) {
+			fact = createFact();
+			map.put(block, fact);
+		}
+		return fact;
+	}
+
+	public static void main(String[] argv) throws Exception {
+		if (argv.length != 1) {
+			System.err.println("Usage: " + BlockTypeAnalysis.class.getName() + " <classfile>");
+			System.exit(1);
+		}
+
+		RepositoryLookupFailureCallback lookupFailureCallback = new RepositoryLookupFailureCallback() {
+			public void reportMissingClass(ClassNotFoundException ex) {
+				ex.printStackTrace();
+				System.exit(1);
+			}
+		};
+
+		AnalysisContext analysisContext = new AnalysisContext(lookupFailureCallback);
+
+		JavaClass jclass = new ClassParser(argv[0]).parse();
+		ClassContext classContext = analysisContext.getClassContext(jclass);
+
+		String methodName = System.getProperty("blocktype.method");
+
+		Method[] methodList = jclass.getMethods();
+		for (int i = 0; i < methodList.length; ++i) {
+			Method method = methodList[i];
+
+			if (method.isNative() || method.isAbstract())
+				continue;
+
+			if (methodName != null && !methodName.equals(method.getName()))
+				continue;
+
+			System.out.println("Method: " + method.getName());
+
+			CFG cfg = classContext.getCFG(method);
+			DepthFirstSearch dfs = classContext.getDepthFirstSearch(method);
+
+			final BlockTypeAnalysis analysis = new BlockTypeAnalysis(dfs);
+			Dataflow<BlockType, BlockTypeAnalysis> dataflow =
+			        new Dataflow<BlockType, BlockTypeAnalysis>(cfg, analysis);
+			dataflow.execute();
+
+			if (Boolean.getBoolean("blocktype.printcfg")) {
+				CFGPrinter cfgPrinter = new CFGPrinter(cfg) {
+					public String blockAnnotate(BasicBlock block) {
+						BlockType blockType = analysis.getResultFact(block);
+						return " [Block type: " + blockType.toString() + "]";
+					}
+				};
+				cfgPrinter.print(System.out);
+			}
+		}
+	}
+}
+
+// vim:ts=4
