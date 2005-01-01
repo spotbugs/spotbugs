0eb5f85ad8494d5386ba25d9e8a7fd716e1bbc3d
Dave Brosius <dbrosius@mebigfatguy.com>
2005-01-01 08:39:25 +0000

    A first attempt at sorting the detector table columns. No gui representation yet.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3415 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/gui/DefaultSortedTableModel.java
+++ findbugs/src/java/edu/umd/cs/findbugs/gui/DefaultSortedTableModel.java
@@ -10,21 +10,28 @@
  * 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
  * Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA	 02111-1307	 USA
  */
 
 
 package edu.umd.cs.findbugs.gui;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.AbstractTableModel;
-import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableModel;
 
@@ -35,51 +42,59 @@ import javax.swing.table.TableModel;
  */
 public class DefaultSortedTableModel extends AbstractTableModel
 {
-	AbstractTableModel baseModel;
-	JTableHeader baseHeader;
+	public static final int SORT_NO_ORDER = 0;
+	public static final int SORT_ASCENDING_ORDER = 1;
+	public static final int SORT_DESCENDING_ORDER = 2;
+	public static final int NUM_SORT_DIREECTIONS = 3;
 	
-	public DefaultSortedTableModel( AbstractTableModel model )
-	{
+	private AbstractTableModel baseModel;
+	private JTableHeader baseHeader = null;
+	private MouseListener headerListener = new HeaderListener();
+	private List<Integer> viewToModelMapping;
+	private int sortDirection = SORT_ASCENDING_ORDER;
+	private int sortColumn = 0;
+	
+	
+	public DefaultSortedTableModel( AbstractTableModel model ) {
 		baseModel = model;
+		setupMapping();
 	}
 	
 	// Base Model handling
 	
-	public TableModel getBaseTableModel()
-	{
+	public TableModel getBaseTableModel() {
 		return baseModel;
 	}
 	
-	public int getBaseModelIndex( int viewIndex )
-	{
-		return viewIndex;
+	public int getBaseModelIndex( int viewIndex ) {
+		return viewToModelMapping.get(viewIndex).intValue();
 	}
 	
-	public void setBaseTableHeader( JTableHeader header )
-	{
+	public void setBaseTableHeader( JTableHeader header ) {
+		if (baseHeader != null)
+			baseHeader.removeMouseListener(headerListener);
 		baseHeader = header;
+		baseHeader.addMouseListener(headerListener);
+		
 	}
 	
 	// Listener handling
 	
-	public void addTableModelListener( TableModelListener tml )
-	{
+	public void addTableModelListener( TableModelListener tml ) {
 		if (baseModel != null)
 			return;
 			
 		baseModel.addTableModelListener(tml);
 	}
 	
-	public void removeTableModelListener( TableModelListener tml )
-	{
+	public void removeTableModelListener( TableModelListener tml ) {
 		if (baseModel != null)
 			return;
 			
 		baseModel.removeTableModelListener(tml);
 	}
 	
-	public TableModelListener[] getTableModelListeners()
-	{
+	public TableModelListener[] getTableModelListeners() {
 		if (baseModel == null)
 			return new TableModelListener[0];
 			
@@ -88,125 +103,157 @@ public class DefaultSortedTableModel extends AbstractTableModel
 	
 	// Event handling
 	
-	public void fireTableCellUpdated( int row, int col )
-	{
+	public void fireTableCellUpdated( int row, int col ) {
 		if (baseModel == null)
 			return;
 			
 		baseModel.fireTableCellUpdated(row, col);
+		setupMapping();
 	}
 
-	public void fireTableChanged( TableModelEvent e )
-	{
+	public void fireTableChanged( TableModelEvent e ) {
 		if (baseModel == null)
 			return;
 			
 		baseModel.fireTableChanged(e);
+		setupMapping();
 	}
 
-	public void fireTableDataChanged()
-	{
+	public void fireTableDataChanged() {
 		if (baseModel == null)
 			return;
 			
 		baseModel.fireTableDataChanged();
+		setupMapping();
 	}
 	
-	public void fireTableRowsDeleted( int first, int last )
-	{
+	public void fireTableRowsDeleted( int first, int last ) {
 		if (baseModel == null)
 			return;
 			
 		baseModel.fireTableRowsDeleted(first, last);
+		setupMapping();
 	}
 
-	public void fireTableRowsInserted( int first, int last )
-	{
+	public void fireTableRowsInserted( int first, int last ) {
 		if (baseModel == null)
 			return;
 			
 		baseModel.fireTableRowsInserted(first, last);
+		setupMapping();
 	}
 
-	public void fireTableRowsUpdated( int first, int last )
-	{
+	public void fireTableRowsUpdated( int first, int last ) {
 		if (baseModel == null)
 			return;
 			
 		baseModel.fireTableRowsUpdated(first, last);
+		setupMapping();
 	}
 
-	public void fireTableStructureChanged()
-	{
+	public void fireTableStructureChanged() {
 		if (baseModel == null)
 			return;
 			
 		baseModel.fireTableStructureChanged();
+		setupMapping();
 	}
 	
 	// accessors
 	
-	public int findColumn( String columnName )
-	{
+	public int findColumn( String columnName ) {
 		if (baseModel == null)
 			return -1;
 	
 		return baseModel.findColumn(columnName);	
 	}
 	
-	public int getColumnCount()
-	{
+	public int getColumnCount() {
 		if (baseModel == null)
 			return 0;
 			
 		return baseModel.getColumnCount();
 	}
 	
-	public int getRowCount()
-	{
+	public int getRowCount() {
 		if (baseModel == null)
 			return 0;
 			
 		return baseModel.getRowCount();
 	}
 	
-	public Class getColumnClass( int column )
-	{
+	public Class getColumnClass( int column ) {
 		if (baseModel == null)
 			return null;
 			
 		return baseModel.getColumnClass(column);
 	}
 	
-	public String getColumnName( int column )
-	{
+	public String getColumnName( int column ) {
 		if (baseModel == null)
 			return null;
 			
 		return baseModel.getColumnName(column);
 	}
 	
-	public boolean isCellEditable( int row, int col )
-	{
+	public boolean isCellEditable( int row, int col ) {
 		if (baseModel == null)
 			return false;
 			
 		return baseModel.isCellEditable( row, col );
 	}
 	
-	public Object getValueAt( int row, int col )
-	{
+	public Object getValueAt( int row, int col ) {
 		if (baseModel == null)
 			return null;
 			
-		return baseModel.getValueAt(row, col);
+		return baseModel.getValueAt(viewToModelMapping.get(row).intValue(), col);
 	}
 	
-	public void setValueAt( Object value, int row, int col )
-	{
+	public void setValueAt( Object value, int row, int col ) {
 		if (baseModel == null)
 			return;
 			
-		baseModel.setValueAt( value, row, col );
-	}	
+		baseModel.setValueAt( value, viewToModelMapping.get(row).intValue(), col );
+	}
+	
+	private void setupMapping() {
+		int numRows = baseModel.getRowCount();
+		viewToModelMapping = new ArrayList(numRows);
+		for (int i = 0; i < numRows; i++)
+			viewToModelMapping.add(new Integer(i));
+		
+		Collections.sort( viewToModelMapping, new Comparator<Integer>() {
+			public int compare( Integer a, Integer b ) 
+			{
+				if ((sortDirection == SORT_NO_ORDER) || (sortColumn == -1))
+					return a.compareTo(b);
+				
+				Comparable first = (Comparable)baseModel.getValueAt( a.intValue(), sortColumn );
+				Comparable second = (Comparable)baseModel.getValueAt( b.intValue(), sortColumn );
+				
+				if (sortDirection == SORT_ASCENDING_ORDER) 
+					return first.compareTo(second);
+				else
+					return second.compareTo(first);
+			}
+		});
+		
+	}
+	
+	private class HeaderListener extends MouseAdapter
+	{
+		public void mouseClicked(MouseEvent e) {
+			JTableHeader header = (JTableHeader)e.getSource();
+			int column = header.columnAtPoint(e.getPoint());
+			if (column != sortColumn) {
+				sortColumn = column;
+				sortDirection = SORT_ASCENDING_ORDER;
+			} else {
+				sortDirection = (sortDirection + 1) % NUM_SORT_DIREECTIONS;
+			}
+			super.mouseClicked(e);
+			DefaultSortedTableModel.this.fireTableDataChanged();
+		}
+}
 }
