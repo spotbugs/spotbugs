721723a7b1b77ba10ee35f9c52401faba299e374
Dave Brosius <dbrosius@mebigfatguy.com>
2005-01-13 15:32:38 +0000

    add a two current false-postive examples to cleanup
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3494 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/test/TestBooleanReturn.java
+++ findbugs/test/TestBooleanReturn.java
@@ -1,22 +1,47 @@
+/* generates
 
+TestingGround: [0002]  if_icmpge
+TestingGround: [0005]  iconst_1
+TestingGround: [0006]  ireturn
+TestingGround: [0007]  iconst_0
+TestingGround: [0008]  ireturn
+
+*/
 
 public class TestBooleanReturn
 {
-	public boolean test(int a, int b)
+	public boolean test1(int a, int b)
 	{
 		if (a < b)
 			return true;
 		else
 			return false;
 	}
+	
+	public boolean test2(int a, int b)
+	{
+		//this should not be reported
+		while (a < b) {
+			a++;
+			if (a == 0)
+				return true;
+		}
+		return false;
+	}
+	
+	public boolean test3(int a, int b)
+	{
+		//this should not be reported
+		switch (a)
+		{
+			case 0:
+				if (b == 0)
+					return true;
+			case 1:
+				return false;
+		}
+		
+		return false;
+	}
 }
 
-/* generates
-
-  TestingGround: [0002]  if_icmpge
-  TestingGround: [0005]  iconst_1
-  TestingGround: [0006]  ireturn
-  TestingGround: [0007]  iconst_0
-  TestingGround: [0008]  ireturn
-  
-*/
