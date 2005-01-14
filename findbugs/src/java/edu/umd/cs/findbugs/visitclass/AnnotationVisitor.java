73d4d132de1e4fd2ccda9b7dd5a6ca007f092f0f
Dave Brosius <dbrosius@mebigfatguy.com>
2005-01-14 04:26:26 +0000

    add copyright comment
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3504 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/visitclass/AnnotationVisitor.java
+++ findbugs/src/java/edu/umd/cs/findbugs/visitclass/AnnotationVisitor.java
@@ -1,9 +1,37 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2003-2005 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package edu.umd.cs.findbugs.visitclass;
-import java.util.*;
-import java.io.PrintStream;
-import org.apache.bcel.classfile.*;
-import java.util.zip.*;
-import java.io.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.bcel.classfile.Attribute;
+import org.apache.bcel.classfile.Constant;
+import org.apache.bcel.classfile.ConstantDouble;
+import org.apache.bcel.classfile.ConstantFloat;
+import org.apache.bcel.classfile.ConstantInteger;
+import org.apache.bcel.classfile.ConstantLong;
+import org.apache.bcel.classfile.ConstantUtf8;
+import org.apache.bcel.classfile.Unknown;
 
 public class AnnotationVisitor extends PreorderVisitor {
 
