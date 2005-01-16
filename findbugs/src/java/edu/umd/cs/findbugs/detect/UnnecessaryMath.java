7a14771c6f64d506c11700926a43c6d7ddd7fced
Dave Brosius <dbrosius@mebigfatguy.com>
2005-01-16 05:49:59 +0000

    unnecessary math on constants detector back as a style detector
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3516 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/UnnecessaryMath.java
@@ -0,0 +1,145 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2005 Dave Brosius <dbrosius@users.sourceforge.net>
+ * Copyright (C) 2005 University of Maryland
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.detect;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.bcel.classfile.Constant;
+import org.apache.bcel.classfile.ConstantDouble;
+import org.apache.bcel.classfile.ConstantFloat;
+import org.apache.bcel.classfile.ConstantLong;
+import org.apache.bcel.classfile.Method;
+
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.BytecodeScanningDetector;
+import edu.umd.cs.findbugs.StatelessDetector;
+import edu.umd.cs.findbugs.visitclass.Constants2;
+
+/**
+ * Find occurrences of Math using constants, where the result of the
+ * calculation can be determined statically. Replacing the math formula
+ * with the constant performs better, and sometimes is more accurate.
+ *
+ * @author Dave Brosius
+ */
+public class UnnecessaryMath extends BytecodeScanningDetector implements Constants2, StatelessDetector {
+	static final int SEEN_NOTHING = 0;
+	static final int SEEN_DCONST = 1;
+
+	private BugReporter bugReporter;
+	private int state = SEEN_NOTHING;
+	private double constValue;
+	
+	private static final Set<String> zeroMethods = new HashSet<String>() 
+											{{ add("acos");
+											   add("asin");
+											   add("atan");
+											   add("atan2");
+											   add("cbrt");
+											   add("cos");
+											   add("cosh");
+											   add("exp");
+											   add("expm1");
+											   add("log");
+											   add("log10");
+											   add("pow");
+											   add("sin");
+											   add("sinh");
+											   add("sqrt");
+											   add("tan");
+											   add("tanh");
+											   add("toDegrees");
+											   add("toRadians");
+											}};
+	private static final Set<String> oneMethods = new HashSet<String>()
+											{{ add("acos");
+											   add("asin");
+											   add("atan");
+											   add("cbrt");
+											   add("exp");
+											   add("log");
+											   add("log10");
+											   add("pow");
+											   add("sqrt");
+											   add("toDegrees");
+											}};
+	private static final Set<String> anyMethods = new HashSet<String>()
+											{{ add("abs");
+											   add("ceil");
+											   add("floor");
+											   add("rint");
+											   add("round");
+											}};
+
+	public UnnecessaryMath(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
+	
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+
+	public void visit(Method obj) {
+		state = SEEN_NOTHING;
+		super.visit(obj);
+	}
+
+	public void sawOpcode(int seen) {
+		if (state == SEEN_NOTHING) {
+			if ((seen == DCONST_0) || (seen == DCONST_1)) {
+				constValue = (double) (seen - DCONST_0);
+				state = SEEN_DCONST;
+			}			
+			else if ((seen == LDC2_W) || (seen == LDC_W)) {
+				state = SEEN_DCONST;
+				Constant c = this.getConstantRefOperand();
+				if (c instanceof ConstantDouble)
+					constValue = ((ConstantDouble)c).getBytes();
+				else if (c instanceof ConstantFloat)
+					constValue = ((ConstantFloat)c).getBytes();
+				else if (c instanceof ConstantLong)
+					constValue = ((ConstantLong)c).getBytes();
+				else
+					state = SEEN_NOTHING;
+			}
+		} else if (state == SEEN_DCONST) {
+			if (seen == INVOKESTATIC) {
+				state = SEEN_NOTHING;
+				if (getDottedClassConstantOperand().equals("java.lang.Math")) {
+					String methodName = getNameConstantOperand();
+					
+					if (((constValue == 0.0) && zeroMethods.contains(methodName))
+					||  ((constValue == 1.0) && oneMethods.contains(methodName))
+					||   (anyMethods.contains(methodName))) {
+						bugReporter.reportBug(new BugInstance("UM_UNNECESSARY_MATH", LOW_PRIORITY)
+												.addClassAndMethod(this)
+												.addSourceLine(this));				
+					}
+				}
+			}
+			state = SEEN_NOTHING;
+		}
+	}
+}
+
+// vim:ts=4
---
+++ findbugs/test/UnnecessaryMathTest.java
@@ -0,0 +1,42 @@
+
+
+public class UnnecessaryMathTest
+{
+	public void test()
+	{
+		double a;
+		
+		a = Math.abs(1.5);
+		a = Math.acos(0.0);
+		a = Math.acos(1.0);
+		a = Math.asin(0.0);
+		a = Math.asin(1.0);
+		a = Math.atan(0.0);
+		a = Math.atan(1.0);
+		a = Math.cbrt(0.0);
+		a = Math.cbrt(1.0);
+		a = Math.ceil(1.5);
+		a = Math.cos(0.0);
+		a = Math.cosh(0.0);
+		a = Math.exp(0.0);
+		a = Math.exp(1.0);
+		a = Math.floor(1.5);
+		a = Math.log(0.0);
+		a = Math.log(1.0);
+		a = Math.log10(0.0);
+		a = Math.log10(1.0);
+		a = Math.pow(10.0, 0.0);
+		a = Math.pow(10.0, 1.0);
+		a = Math.rint(1.5);
+		a = Math.round(1.5);
+		a = Math.sin(0.0);
+		a = Math.sinh(0.0);
+		a = Math.sqrt(0.0);
+		a = Math.sqrt(1.0);
+		a = Math.tan(0.0);
+		a = Math.tanh(0.0);
+		a = Math.toDegrees(0.0);
+		a = Math.toDegrees(1.0);
+		a = Math.toRadians(0.0);
+	}
+}
