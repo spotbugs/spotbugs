d45d94e85156f7f0d7c0a70e7c62936a135b607e
Dave Brosius <dbrosius@mebigfatguy.com>
2005-01-21 05:41:21 +0000

    Add new Style Detector, as per SF Request, to report Classes that declare implementation of interfaces that are already implemented by superclasses.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3528 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/RedundantInterfaces.java
@@ -0,0 +1,78 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2005 Dave Brosius <dbrosius@users.sourceforge.net>
+ * Copyright (C) 2005 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package edu.umd.cs.findbugs.detect;
+
+import org.apache.bcel.Repository;
+import org.apache.bcel.classfile.JavaClass;
+
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.Detector;
+import edu.umd.cs.findbugs.StatelessDetector;
+import edu.umd.cs.findbugs.ba.AnalysisContext;
+import edu.umd.cs.findbugs.ba.ClassContext;
+import edu.umd.cs.findbugs.visitclass.Constants2;
+import edu.umd.cs.findbugs.visitclass.PreorderVisitor;
+
+public class RedundantInterfaces extends PreorderVisitor implements Detector, Constants2, StatelessDetector
+{
+	private BugReporter bugReporter;
+	
+	public RedundantInterfaces(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
+	
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+	
+	public void visitClassContext(ClassContext classContext) {
+		JavaClass obj = classContext.getJavaClass();
+
+		String superClassName = obj.getSuperclassName();
+		if (superClassName.equals("java.lang.Object"))
+			return;
+		
+		String[] interfaceNames = obj.getInterfaceNames();
+		if ((interfaceNames == null) || (interfaceNames.length == 0))
+			return;
+
+		try {
+			JavaClass superObj = obj.getSuperClass();
+			
+			for (int i = 0; i < interfaceNames.length; i++) {
+				JavaClass inf = Repository.lookupClass(interfaceNames[i]);
+				if (superObj.instanceOf(inf)) {
+					bugReporter.reportBug( new BugInstance( this, "RI_REDUNDANT_INTERFACES", LOW_PRIORITY )
+						.addClass(obj)
+						.addClass(inf));
+				}
+			}
+		} catch (ClassNotFoundException cnfe) {
+			bugReporter.reportMissingClass(cnfe);
+		}
+	}
+
+	public void setAnalysisContext(AnalysisContext analysisContext) {
+	}
+	
+	public void report() {
+	}
+}
