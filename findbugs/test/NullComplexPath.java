5ce8d169ef3f6bdd2db663dc6bab0ff0cd91263a
David Hovemeyer <david.hovemeyer@gmail.com>
2005-02-22 18:57:18 +0000

    Initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3623 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/test/NullComplexPath.java
@@ -0,0 +1,16 @@
+public class NullComplexPath {
+	public static void main(String[] argv) {
+		if (argv.length < 1)
+			return;
+
+		Object o = null;
+
+		if (argv[0].equals("S")) {
+			o = new Object();
+		}
+
+		if (argv[0].equals("S")) {
+			System.out.println("Hash code is " + o.hashCode());
+		}
+	}
+}
