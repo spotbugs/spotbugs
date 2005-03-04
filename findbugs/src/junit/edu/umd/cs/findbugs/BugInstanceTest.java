e48356495e9d3f045783b21220b1fca4dd3a7667
David Hovemeyer <david.hovemeyer@gmail.com>
2005-03-04 16:38:51 +0000

    Test removing a property through an Iterator.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3688 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/junit/edu/umd/cs/findbugs/BugInstanceTest.java
+++ findbugs/src/junit/edu/umd/cs/findbugs/BugInstanceTest.java
@@ -22,18 +22,47 @@ public class BugInstanceTest extends TestCase {
 	}
 	
 	public void testPropertyIterator() {
-		Iterator<BugProperty> i = b.propertyIterator();
-		Assert.assertTrue(i.hasNext());
-		checkProperty(i.next(), "A", "a");
-		Assert.assertTrue(i.hasNext());
-		checkProperty(i.next(), "B", "b");
-		Assert.assertTrue(i.hasNext());
-		checkProperty(i.next(), "C", "c");
-		Assert.assertFalse(i.hasNext());
+		checkPropertyIterator(b.propertyIterator(),
+				new String[]{"A","B","C"}, new String[]{"a","b","c"});
+	}
+	
+	public void testRemoveThroughIterator1() {
+		removeThroughIterator(b.propertyIterator(), "A");
+		checkPropertyIterator(b.propertyIterator(), new String[]{"B","C"}, new String[]{"b","c"});
+	}
+	
+	public void testRemoveThroughIterator2() {
+		removeThroughIterator(b.propertyIterator(), "B");
+		checkPropertyIterator(b.propertyIterator(), new String[]{"A","C"}, new String[]{"a","c"});
+	}
+	
+	public void testRemoveThroughIterator3() {
+		removeThroughIterator(b.propertyIterator(), "C");
+		checkPropertyIterator(b.propertyIterator(), new String[]{"A","B"}, new String[]{"a","b"});
 	}
 
+	private void checkPropertyIterator(Iterator<BugProperty> iter, String[] names, String[] values) {
+		if (names.length != values.length)
+			throw new IllegalArgumentException();
+		for (int i = 0; i < names.length; ++i) {
+			Assert.assertTrue(iter.hasNext());
+			String name = names[i];
+			String value = values[i];
+			checkProperty(iter.next(), name, value);
+		}
+		Assert.assertFalse(iter.hasNext());
+	}
+	
 	private void checkProperty(BugProperty property, String name, String value) {
 		Assert.assertEquals(property.getName(), name);
 		Assert.assertEquals(property.getValue(), value);
+	}
+	
+	private void removeThroughIterator(Iterator<BugProperty> iter, String name) {
+		while (iter.hasNext()) {
+			BugProperty prop = iter.next();
+			if (prop.getName().equals(name))
+				iter.remove();
+		}
 	}
 }
