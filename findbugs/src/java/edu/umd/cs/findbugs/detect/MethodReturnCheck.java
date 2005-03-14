15be9a413c4c943f49b5e62f66753fe7faccf814
David Hovemeyer <david.hovemeyer@gmail.com>
2005-03-14 17:13:22 +0000

    Removed unneeded code.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3761 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/MethodReturnCheck.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/MethodReturnCheck.java
@@ -19,9 +19,6 @@
 package edu.umd.cs.findbugs.detect;
 
 import java.util.BitSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 
 import org.apache.bcel.Constants;
 import org.apache.bcel.classfile.Code;
@@ -41,10 +38,6 @@ import edu.umd.cs.findbugs.ba.ClassContext;
  */
 public class MethodReturnCheck extends BytecodeScanningDetector {
 	private static boolean DEBUG = Boolean.getBoolean("mrc.debug");
-//	private static boolean CHECK_CONTROL_FLOW =
-//		Boolean.getBoolean("mrc.checkControlFlow");
-//	private static boolean COUNT_POP_BRANCH_TARGETS =
-//		Boolean.getBoolean("mrc.countPopBranchTargets");
 	
 	private static final int SCAN =0;
 	private static final int SAW_INVOKE = 1;
@@ -56,38 +49,16 @@ public class MethodReturnCheck extends BytecodeScanningDetector {
 		INVOKE_OPCODE_SET.set(Constants.INVOKESTATIC);
 		INVOKE_OPCODE_SET.set(Constants.INVOKEVIRTUAL);
 	}
-	
-	private static class QueuedWarning {
-		BugInstance warning;
-		int popPC;
-		QueuedWarning(BugInstance warning, int popPC) {
-			this.warning = warning;
-			this.popPC = popPC;
-		}
-	}
 
 	private BugReporter bugReporter;
-//	private BitSet branchTargetSet;
-	private List<QueuedWarning> queuedWarningList;
 	private ClassContext classContext;
 	private Method method;
 	private int state;
 	private int callPC;
 	private String className, methodName, signature;
-//	private List<Integer> popList;
-//	private List<Integer> retValPopList;
-//	private int popIsBranchTarget;
-//	private int retValPopIsBranchTarget;
-//	private boolean lastIsInvoke;
 	
 	public MethodReturnCheck(BugReporter bugReporter) {
 		this.bugReporter = bugReporter;
-//		this.branchTargetSet = new BitSet();
-		this.queuedWarningList = new LinkedList<QueuedWarning>();
-//		if (COUNT_POP_BRANCH_TARGETS) {
-//			this.popList = new LinkedList<Integer>();
-//			this.retValPopList = new LinkedList<Integer>();
-//		}
 	}
 	
 	public void visitClassContext(ClassContext classContext) {
@@ -103,77 +74,27 @@ public class MethodReturnCheck extends BytecodeScanningDetector {
 	public void visitCode(Code code) {
 		// Prescreen to find methods with POP or POP2 instructions,
 		// and at least one method invocation
-		BitSet bytecodeSet = classContext.getBytecodeSet(method);
-		if (!(bytecodeSet.get(Constants.POP) || bytecodeSet.get(Constants.POP2)))
+		if (!prescreen())
 			return;
-		if (!(bytecodeSet.get(Constants.INVOKEINTERFACE) ||
-				bytecodeSet.get(Constants.INVOKESPECIAL) ||
-				bytecodeSet.get(Constants.INVOKESTATIC) ||
-				bytecodeSet.get(Constants.INVOKEVIRTUAL)))
-			return;
-
-		if (DEBUG) System.out.println("Visiting " + method);
-		reset();
-		super.visitCode(code);
-
-		if (DEBUG)System.out.println(
-				"After visit: " +
-				queuedWarningList.size() + " queued warnings");
-		for (Iterator<QueuedWarning> i = queuedWarningList.iterator(); i.hasNext();) {
-			QueuedWarning qw = i.next();
-
-//			if (CHECK_CONTROL_FLOW && branchTargetSet.get(qw.popPC))
-//				continue;
-			
-			bugReporter.reportBug(qw.warning);
-		}
-//		if (COUNT_POP_BRANCH_TARGETS) {
-//			for (Iterator<Integer> i = popList.iterator(); i.hasNext();) {
-//				int pc = i.next().intValue();
-//				if (branchTargetSet.get(pc))
-//					++popIsBranchTarget;
-//			}
-//		}
 		
+		if (DEBUG) System.out.println("Visiting " + method);
+		super.visitCode(code);
 	}
 
-	private void reset() {
-		if (DEBUG) System.out.println("resetting state");
-//		branchTargetSet.clear();
-		queuedWarningList.clear();
-//		if (COUNT_POP_BRANCH_TARGETS) {
-//			popList.clear();
-//			retValPopList.clear();
-//			lastIsInvoke = false;
-//		}
-		method = null;
+	private boolean prescreen() {
+		BitSet bytecodeSet = classContext.getBytecodeSet(method);
+		if (!(bytecodeSet.get(Constants.POP) || bytecodeSet.get(Constants.POP2))) {
+			return false;
+		} else if (!(bytecodeSet.get(Constants.INVOKEINTERFACE) ||
+				bytecodeSet.get(Constants.INVOKESPECIAL) ||
+				bytecodeSet.get(Constants.INVOKESTATIC) ||
+				bytecodeSet.get(Constants.INVOKEVIRTUAL))) {
+			return false;
+		}
+		return true;
 	}
 	
 	public void sawOpcode(int seen) {
-		// Mark branch and goto targets
-//		if (CHECK_CONTROL_FLOW) {
-//			if (isBranch(seen)) {
-//				markBranch(getBranchTarget());
-//				if (seen != GOTO && seen != GOTO_W) {
-//					markBranch(getBranchFallThrough());
-//				}
-//			} else if (isSwitch(seen)) {
-//				markBranch(getDefaultSwitchOffset());
-//				int[] switchOffsetList = getSwitchOffsets();
-//				for (int i = 0; i < switchOffsetList.length; ++i) {
-//					markBranch(switchOffsetList[i]);
-//				}
-//			}
-//		}
-		
-//		if (COUNT_POP_BRANCH_TARGETS) {
-//			if (isPop(seen)) {
-//				popList.add(new Integer(getPC()));
-//				if (lastIsInvoke)
-//					retValPopList.add(new Integer(getPC()));
-//			}
-//		}
-		
 		boolean redo;
 		
 		do {
@@ -202,8 +123,7 @@ public class MethodReturnCheck extends BytecodeScanningDetector {
 							.addClassAndMethod(this)
 							.addMethod(className, methodName, signature).describe("METHOD_CALLED")
 							.addSourceLine(this, callPC);
-					queuedWarningList.add(new QueuedWarning(warning, popPC));
-					if (DEBUG) System.out.println(queuedWarningList.size() + " queued warnings so far");
+					bugReporter.reportBug(warning);
 				} else {
 					// This instruction might be an invocation, too.
 					// So redo processing this instruction.
@@ -215,10 +135,6 @@ public class MethodReturnCheck extends BytecodeScanningDetector {
 			default:
 			}
 		} while (redo);
-
-//		if (COUNT_POP_BRANCH_TARGETS) {
-//			lastIsInvoke = INVOKE_OPCODE_SET.get(seen);
-//		}
 
 	}
 
@@ -234,22 +150,5 @@ public class MethodReturnCheck extends BytecodeScanningDetector {
 		// FIXME: just look for String methods for now
 		return className.equals("java.lang.String")
 			&& signature.endsWith(")Ljava/lang/String;");
-	}
-
-//	private void markBranch(int branchTarget) {
-//		if (branchTarget >= 0) {
-//			branchTargetSet.set(branchTarget);
-//		}
-//	}
-	
-	//@Override
-	public void report() {
-//		if (COUNT_POP_BRANCH_TARGETS) {
-//			System.out.println(
-//					"Observed:\n" +
-//					"\t" + popIsBranchTarget + " pops which were branch targets\n" +
-//					"\t" + retValPopIsBranchTarget + " return value pops which were branch targets"
-//					);
-//		}
 	}
 }
