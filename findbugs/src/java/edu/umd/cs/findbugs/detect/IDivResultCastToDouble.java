1acd0a2230c5dc5feceb74e12bd2f1bd37ab9b2c
David Hovemeyer <david.hovemeyer@gmail.com>
2005-03-16 21:53:06 +0000

    New detector to find integer division results cast to double. Disabled by default for now.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3764 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/IDivResultCastToDouble.java
@@ -0,0 +1,61 @@
+package edu.umd.cs.findbugs.detect;
+
+import org.apache.bcel.Constants;
+import org.apache.bcel.classfile.Method;
+
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.BytecodeScanningDetector;
+
+public class IDivResultCastToDouble extends BytecodeScanningDetector {
+	private static final boolean DEBUG = Boolean.getBoolean("idcd.debug");
+	
+	private static final int SCAN = 0;
+	private static final int SAW_IDIV = 1;
+	
+	private BugReporter bugReporter;
+	private int state;
+	
+	public IDivResultCastToDouble(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
+	
+	//@Override
+	public void visit(Method obj) {
+		System.out.println("Visiting " + obj);
+		state = SCAN;
+	}
+
+	public void sawOpcode(int seen) {
+		boolean redo = false;
+		
+		if (DEBUG) System.out.println("Saw opcode " + seen);
+		
+		do {
+			switch (state) {
+			case SCAN:
+				if (seen == Constants.IDIV) {
+					if (DEBUG) System.out.println("Saw IDIV @" + getPC());
+					state = SAW_IDIV;
+				}
+				break;
+				
+			case SAW_IDIV:
+				if (seen == Constants.I2D) {
+					if (DEBUG) System.out.println("Saw I2D @" + getPC());
+					bugReporter.reportBug(new BugInstance(this, "IDCD_IDIV_CAST_TO_DOUBLE", NORMAL_PRIORITY)
+							.addClassAndMethod(this)
+							.addSourceLine(this));
+				} else {
+					// This instruction might also be an IDIV: process again in SCAN state
+					redo = true;
+				}
+				state = SCAN;
+				break;
+			
+			default:
+				break;
+			}
+		} while (redo);
+	}
+}
