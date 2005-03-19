1fd91aa1c6b6b2f4c0603a99c6128a667e1f43a2
David Hovemeyer <david.hovemeyer@gmail.com>
2005-03-19 18:28:46 +0000

    Added javadoc.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3774 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/DeadLocalStoreProperty.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/DeadLocalStoreProperty.java
@@ -3,6 +3,11 @@ package edu.umd.cs.findbugs.detect;
 import edu.umd.cs.findbugs.props.PriorityAdjustment;
 import edu.umd.cs.findbugs.props.WarningProperty;
 
+/**
+ * Warning property for FindDeadLocalStores.
+ * 
+ * @author David Hovemeyer
+ */
 public class DeadLocalStoreProperty implements WarningProperty {
 	private PriorityAdjustment priorityAdjustment;
 	private String name;
@@ -12,26 +17,37 @@ public class DeadLocalStoreProperty implements WarningProperty {
 		this.priorityAdjustment = priorityAdjustment;
 	}
 	
+	/** Store is killed by a subsequent store. */
 	public static final DeadLocalStoreProperty KILLED_BY_SUBSEQUENT_STORE =
 		new DeadLocalStoreProperty("KILLED_BY_SUBSEQUENT_STORE",PriorityAdjustment.LOWER_PRIORITY);
+	/** Dead store is of a defense programming constant value. */
 	public static final DeadLocalStoreProperty DEFENSIVE_CONSTANT_OPCODE =
 		new DeadLocalStoreProperty("DEFENSIVE_CONSTANT_OPCODE",PriorityAdjustment.FALSE_POSITIVE);
+	/** Dead store is likely to be the exception object in an exception handler. */
 	public static final DeadLocalStoreProperty EXCEPTION_HANDLER =
 		new DeadLocalStoreProperty("EXCEPTION_HANDLER",PriorityAdjustment.FALSE_POSITIVE);
+	/** The dead store is an increment. */
 	public static final DeadLocalStoreProperty DEAD_INCREMENT =
 		new DeadLocalStoreProperty("DEAD_INCREMENT",PriorityAdjustment.LOWER_PRIORITY);
+	/** The dead store is an increment: the only one in the method. */
 	public static final DeadLocalStoreProperty SINGLE_DEAD_INCREMENT =
 		new DeadLocalStoreProperty("SINGLE_DEAD_INCREMENT",PriorityAdjustment.RAISE_PRIORITY);
+	/** Dead store is of a newly allocated object. */
 	public static final DeadLocalStoreProperty DEAD_OBJECT_STORE =
 		new DeadLocalStoreProperty("DEAD_OBJECT_STORE",PriorityAdjustment.RAISE_PRIORITY);
+	/** Method contains two stores and multiple loads of this local. */
 	public static final DeadLocalStoreProperty TWO_STORES_MULTIPLE_LOADS =
 		new DeadLocalStoreProperty("TWO_STORES_MULTIPLE_LOADS",PriorityAdjustment.RAISE_PRIORITY);
+	/** There is only one store of this local. (Maybe it's final?) */
 	public static final DeadLocalStoreProperty SINGLE_STORE =
 		new DeadLocalStoreProperty("SINGLE_STORE",PriorityAdjustment.LOWER_PRIORITY);
+	/** There are no loads of this local. (Maybe it's final?). */
 	public static final DeadLocalStoreProperty NO_LOADS =
 		new DeadLocalStoreProperty("NO_LOADS",PriorityAdjustment.LOWER_PRIORITY);
+	/** This local is a parameter which is dead on entry to the method. */
 	public static final DeadLocalStoreProperty PARAM_DEAD_ON_ENTRY =
 		new DeadLocalStoreProperty("PARAM_DEAD_ON_ENTRY",PriorityAdjustment.RAISE_PRIORITY);
+	/** Name of the local variable. */
 	public static final DeadLocalStoreProperty LOCAL_NAME =
 		new DeadLocalStoreProperty("LOCAL_NAME",PriorityAdjustment.NO_ADJUSTMENT);
 
