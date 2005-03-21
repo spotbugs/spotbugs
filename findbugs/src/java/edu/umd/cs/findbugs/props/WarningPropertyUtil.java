b47153dc8937de855883ed5141d64253424c2f1e
David Hovemeyer <david.hovemeyer@gmail.com>
2005-03-21 17:05:42 +0000

    Method to add RECEIVER_OBJECT_TYPE property from method and PC value.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3780 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/props/WarningPropertyUtil.java
+++ findbugs/src/java/edu/umd/cs/findbugs/props/WarningPropertyUtil.java
@@ -18,10 +18,15 @@
  */
 package edu.umd.cs.findbugs.props;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.apache.bcel.classfile.Method;
 import org.apache.bcel.generic.ReferenceType;
 import org.apache.bcel.generic.Type;
 
+import edu.umd.cs.findbugs.ba.CFG;
 import edu.umd.cs.findbugs.ba.CFGBuilderException;
 import edu.umd.cs.findbugs.ba.ClassContext;
 import edu.umd.cs.findbugs.ba.DataflowAnalysisException;
@@ -53,10 +58,77 @@ public abstract class WarningPropertyUtil {
 			Location location) throws DataflowAnalysisException, CFGBuilderException {
 		TypeDataflow typeDataflow = classContext.getTypeDataflow(method);
 		TypeFrame frame = typeDataflow.getFactAtLocation(location);
-		Type type = frame.getInstance(
-				location.getHandle().getInstruction(), classContext.getConstantPoolGen());
-		if (type instanceof ReferenceType) {
-			propertySet.setProperty(GeneralWarningProperty.RECEIVER_OBJECT_TYPE, type.toString());
+		if (frame.isValid()) {
+			Type type = frame.getInstance(
+					location.getHandle().getInstruction(), classContext.getConstantPoolGen());
+			if (type instanceof ReferenceType) {
+				propertySet.setProperty(GeneralWarningProperty.RECEIVER_OBJECT_TYPE, type.toString());
+			}
+		}
+	}
+	
+	/**
+	 * Add a RECEIVER_OBJECT_TYPE warning property for a particular
+	 * instruction in a method to given warning property set.
+	 * 
+	 * @param propertySet  the property set
+	 * @param classContext ClassContext of the class containing the method 
+	 * @param method       the method
+	 * @param pc           PC value of the instruction
+	 * @throws DataflowAnalysisException
+	 * @throws CFGBuilderException
+	 */
+	public static void addReceiverObjectType(
+			WarningPropertySet propertySet,
+			ClassContext classContext,
+			Method method,
+			int pc) throws CFGBuilderException, DataflowAnalysisException {
+		CFG cfg = classContext.getCFG(method);
+		
+		// Get all Locations for the given PC value.
+		// There may be more than one because of JSR subroutines.
+		List<Location> locationList = new LinkedList<Location>();
+		for (Iterator<Location> i = cfg.locationIterator(); i.hasNext();) {
+			Location location = i.next();
+			if (location.getHandle().getPosition() == pc) {
+				locationList.add(location);
+			}
+		}
+		
+		TypeDataflow typeDataflow = classContext.getTypeDataflow(method);
+		
+		// Get all types for those locations
+		List<Type> typeList = new LinkedList<Type>();
+		for (Iterator<Location> i = locationList.iterator(); i.hasNext();) {
+			Location location = i.next();
+			TypeFrame frame = typeDataflow.getFactAtLocation(location);
+			if (frame.isValid()) {
+				Type type = frame.getInstance(
+						location.getHandle().getInstruction(), classContext.getConstantPoolGen());
+				typeList.add(type);
+			}
+		}
+		
+		if (!typeList.isEmpty()) {
+			// Find least upper bound of collected types
+			ReferenceType lub = null;
+			for (Iterator<Type> i = typeList.iterator(); i.hasNext();) {
+				Type type = i.next();
+				if (!(type instanceof ReferenceType))
+					return;
+				ReferenceType refType = (ReferenceType) type;
+				if (lub == null) {
+					lub = refType;
+				} else {
+					try {
+						lub = lub.getFirstCommonSuperclass(refType);
+					} catch (ClassNotFoundException e) {
+						return;
+					}
+				}
+			}
+			
+			propertySet.setProperty(GeneralWarningProperty.RECEIVER_OBJECT_TYPE, lub.toString());
 		}
 	}
 }
