2a881e27d9185b16936f4dd98352129011fb45f7
David Hovemeyer <david.hovemeyer@gmail.com>
2005-03-23 19:22:54 +0000

    Initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3785 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/constant/ConstantDataflow.java
@@ -0,0 +1,45 @@
+/*
+ * Bytecode Analysis Framework
+ * Copyright (C) 2005, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package edu.umd.cs.findbugs.ba.constant;
+
+import edu.umd.cs.findbugs.ba.CFG;
+import edu.umd.cs.findbugs.ba.Dataflow;
+import edu.umd.cs.findbugs.ba.DataflowAnalysisException;
+import edu.umd.cs.findbugs.ba.Location;
+
+/**
+ * Dataflow class for ConstantAnalysis.
+ * 
+ * @see edu.umd.cs.findbugs.ba.Dataflow
+ * @see edu.umd.cs.findbugs.ba.constant.ConstantAnalysis
+ * @author David Hovemeyer
+ */
+public class ConstantDataflow extends Dataflow<ConstantFrame, ConstantAnalysis> {
+	public ConstantDataflow(CFG cfg, ConstantAnalysis analysis) {
+		super(cfg, analysis);
+	}
+	
+	public ConstantFrame getFactAtLocation(Location location) throws DataflowAnalysisException {
+		return getAnalysis().getFactAtLocation(location);
+	}
+	
+	public ConstantFrame getFactAfterLocation(Location location) throws DataflowAnalysisException {
+		return getAnalysis().getFactAfterLocation(location);
+	}
+}
