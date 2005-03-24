00759bd97d88179c77ca5735d6a073bd6ff79189
David Hovemeyer <david.hovemeyer@gmail.com>
2005-03-24 21:20:19 +0000

    Initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3810 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/ca/CallListDataflow.java
@@ -0,0 +1,38 @@
+/*
+ * Bytecode Analysis Framework
+ * Copyright (C) 2005, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package edu.umd.cs.findbugs.ba.ca;
+
+import edu.umd.cs.findbugs.ba.CFG;
+import edu.umd.cs.findbugs.ba.Dataflow;
+import edu.umd.cs.findbugs.ba.DataflowAnalysisException;
+import edu.umd.cs.findbugs.ba.Location;
+
+public class CallListDataflow extends Dataflow<CallList, CallListAnalysis> {
+	public CallListDataflow(CFG cfg, CallListAnalysis analysis) {
+		super(cfg, analysis);
+	}
+	
+	public CallList getFactAtLocation(Location location) throws DataflowAnalysisException {
+		return getAnalysis().getFactAtLocation(location);
+	}
+	
+	public CallList getFactAfterLocation(Location location) throws DataflowAnalysisException {
+		return getAnalysis().getFactAfterLocation(location);
+	}
+}
