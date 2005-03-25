8192f475b757d74db6d973c5bfb7bf67092e3557
David Hovemeyer <david.hovemeyer@gmail.com>
2005-03-25 14:05:38 +0000

    Initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3824 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/junit/edu/umd/cs/findbugs/config/UserPreferencesTest.java
@@ -0,0 +1,20 @@
+package edu.umd.cs.findbugs.config;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class UserPreferencesTest extends TestCase {
+	UserPreferences prefs;
+	
+	//@Override
+	protected void setUp() throws Exception {
+		prefs = UserPreferences.createDefaultUserPreferences();
+	}
+	
+	public void testClone() {
+		UserPreferences clone = (UserPreferences) prefs.clone();
+		
+		Assert.assertEquals(prefs, clone);
+		Assert.assertEquals(prefs.getClass(), clone.getClass());
+	}
+}
