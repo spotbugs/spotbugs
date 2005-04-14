401ca953720186cd14a5d4a59bbd04e495c51317
David Hovemeyer <david.hovemeyer@gmail.com>
2005-04-14 18:13:20 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@3930 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/AnalysisError.java
@@ -0,0 +1,96 @@
+package edu.umd.cs.findbugs;
+
+import java.util.ArrayList;
+
+/**
+ * Object recording a recoverable error that occurred during analysis.
+ * 
+ * @author David Hovemeyer
+ */
+public class AnalysisError {
+	private String message;
+	private String exceptionMessage;
+	private String[] stackTrace;
+	
+	/**
+	 * Constructor.
+	 * 
+	 * @param message message describing the error
+	 */
+	public AnalysisError(String message) {
+		this(message, null);
+	}
+	
+	/**
+	 * Constructor.
+	 * 
+	 * @param message   message describing the error
+	 * @param exception exception which is the cause of the error
+	 */
+	public AnalysisError(String message, Throwable exception) {
+		this.message = message;
+		if (exception != null) {
+			exceptionMessage = exception.toString();
+			StackTraceElement[] exceptionStackTrace = exception.getStackTrace();
+			ArrayList<String> arr = new ArrayList<String>();
+			for (int i = 0; i < exceptionStackTrace.length; ++i) {
+				arr.add(exceptionStackTrace[i].toString());
+			}
+			stackTrace = arr.toArray(new String[arr.size()]);
+		}
+	}
+
+	/**
+	 * Set the message describing the error.
+	 * 
+	 * @param message message describing the error
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	/**
+	 * Get the message describing the error.
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * Set the exception message.  This is the value returned by
+	 * calling toString() on the original exception object.
+	 * 
+	 * @param exceptionMessage the exception message
+	 */
+	public void setExceptionMessage(String exceptionMessage) {
+		this.exceptionMessage = exceptionMessage;
+	}
+	
+	/**
+	 * Get the exception message.  This is the value returned by
+	 * calling toString() on the original exception object.
+	 */
+	public String getExceptionMessage() {
+		return exceptionMessage;
+	}
+
+	/**
+	 * Set the stack trace elements.
+	 * These are the strings returned by calling toString()
+	 * on each StackTraceElement in the original exception.
+	 * 
+	 * @param stackTraceList the stack trace elements
+	 */
+	public void setStackTrace(String[] stackTraceList) {
+		this.stackTrace = stackTraceList;
+	}
+
+	/**
+	 * Get the stack trace elements.
+	 * These are the strings returned by calling toString()
+	 * on each StackTraceElement in the original exception.
+	 */
+	public String[] getStackTrace() {
+		return stackTrace;
+	}
+}
