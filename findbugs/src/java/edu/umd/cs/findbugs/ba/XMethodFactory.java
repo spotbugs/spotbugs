79d2443f38e54bf3dbd0d3220a7435b0de419f08
David Hovemeyer <david.hovemeyer@gmail.com>
2005-05-11 18:16:09 +0000

    Initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4160 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/XMethodFactory.java
@@ -0,0 +1,47 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2005, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.ba;
+
+import org.apache.bcel.classfile.Method;
+
+/**
+ * Factory methods for creating XMethod objects.
+ * 
+ * @author David Hovemeyer
+ */
+public abstract class XMethodFactory {
+	/**
+	 * Create an XMethod object from a BCEL Method.
+	 * 
+	 * @param classContext the ClassContext of the class to which the Method belongs
+	 * @param method       the Method
+	 * @return an XMethod representing the Method
+	 */
+	public static XMethod createXMethod(ClassContext classContext, Method method) {
+		String className = classContext.getJavaClass().getClassName();
+		String methodName = method.getName();
+		String methodSig = method.getSignature();
+		int accessFlags = method.getAccessFlags();
+		if (method.isStatic())
+			return new StaticMethod(className, methodName, methodSig, accessFlags);
+		else
+			return new InstanceMethod(className, methodName, methodSig, accessFlags);
+	}
+}
