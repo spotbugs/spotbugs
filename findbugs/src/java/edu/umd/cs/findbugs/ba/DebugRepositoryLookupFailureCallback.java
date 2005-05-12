6034fd74320cdf6ac37a2198a88370fbcf661cce
David Hovemeyer <david.hovemeyer@gmail.com>
2005-05-12 16:58:54 +0000

    Initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4176 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/DebugRepositoryLookupFailureCallback.java
@@ -0,0 +1,57 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2005, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.ba;
+
+/**
+ * DebugRepositoryLookupFailureCallback implementation for debugging.
+ * (Test drivers, etc.)  It just prints a message and exits.
+ * 
+ * @author David Hovemeyer
+ */
+public class DebugRepositoryLookupFailureCallback implements
+		RepositoryLookupFailureCallback {
+
+	/* (non-Javadoc)
+	 * @see edu.umd.cs.findbugs.ba.RepositoryLookupFailureCallback#reportMissingClass(java.lang.ClassNotFoundException)
+	 */
+	public void reportMissingClass(ClassNotFoundException ex) {
+		System.out.println("Missing class");
+		ex.printStackTrace();
+		System.exit(1);
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.umd.cs.findbugs.ba.RepositoryLookupFailureCallback#logError(java.lang.String)
+	 */
+	public void logError(String message) {
+		System.err.println("Error: " + message);
+		System.exit(1);
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.umd.cs.findbugs.ba.RepositoryLookupFailureCallback#logError(java.lang.String, java.lang.Throwable)
+	 */
+	public void logError(String message, Throwable e) {
+		System.err.println("Error: " + message);
+		e.printStackTrace();
+		System.exit(1);
+	}
+
+}
