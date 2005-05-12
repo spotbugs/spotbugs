421e41867757750e44bd5dae2055161a76718521
Dave Brosius <dbrosius@mebigfatguy.com>
2005-05-12 04:33:05 +0000

    embellish DuplicateBranches detector to look at switch statements. TODO: Need to convert relative offsets to absolute offsets so that branches inside the case statement match when the target is the same.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4173 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/DuplicateBranches.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/DuplicateBranches.java
@@ -1,7 +1,9 @@
 package edu.umd.cs.findbugs.detect;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.bcel.classfile.Method;
 import org.apache.bcel.generic.GotoInstruction;
@@ -48,64 +50,120 @@ public class DuplicateBranches extends PreorderVisitor implements Detector, Stat
 			Iterator<BasicBlock> bbi = cfg.blockIterator();
 			while (bbi.hasNext()) {
 				BasicBlock bb = bbi.next();
-				BasicBlock thenBB = null, elseBB = null;
 				
-				if (cfg.getNumOutgoingEdges(bb) == 2) {
-					Iterator<Edge> iei = cfg.outgoingEdgeIterator(bb);
-					while (iei.hasNext()) {
-						Edge e = iei.next();
-						if (e.getType() == EdgeTypes.IFCMP_EDGE) {
-							elseBB = e.getTarget();
-						}
-						else if (e.getType() == EdgeTypes.FALL_THROUGH_EDGE) {
-							thenBB = e.getTarget();
-						}
-					}
-				}
+				int numOutgoing = cfg.getNumOutgoingEdges(bb);
+				if (numOutgoing == 2)
+					findIfElseDuplicates(cfg, method, bb);
+				else if (numOutgoing > 2)
+					findSwitchDuplicates(cfg, method, bb);
+			}
+		} catch (Exception e) {
+			bugReporter.logError("Failure examining basic blocks in Duplicate Branches detector", e);
+		}
+	}
+	
+	private void findIfElseDuplicates(CFG cfg, Method method, BasicBlock bb) {
+		BasicBlock thenBB = null, elseBB = null;
+
+		Iterator<Edge> iei = cfg.outgoingEdgeIterator(bb);
+		while (iei.hasNext()) {
+			Edge e = iei.next();
+			if (e.getType() == EdgeTypes.IFCMP_EDGE) {
+				elseBB = e.getTarget();
+			}
+			else if (e.getType() == EdgeTypes.FALL_THROUGH_EDGE) {
+				thenBB = e.getTarget();
+			}
+		}
+		
+		if ((thenBB == null) || (elseBB == null) 
+		||  (thenBB.getFirstInstruction() == null) || (elseBB.getFirstInstruction() == null))
+			return;
+		
+		int thenStartPos = thenBB.getFirstInstruction().getPosition();
+		int elseStartPos = elseBB.getFirstInstruction().getPosition();
+		
+		BasicBlock thenFinishBlock = findThenFinish(cfg, thenBB, elseStartPos);
+		
+		if (thenFinishBlock == null)
+			return;
+		
+		Instruction lastFinishIns = thenFinishBlock.getLastInstruction().getInstruction();
+		if (!(lastFinishIns instanceof GotoInstruction))
+			return;
+		
+		int thenFinishPos = thenFinishBlock.getLastInstruction().getPosition();
+		int elseFinishPos = ((GotoInstruction) lastFinishIns).getTarget().getPosition();
+		
+		if (thenFinishPos >= elseStartPos)
+			return;
+		
+		if ((thenFinishPos - thenStartPos) != (elseFinishPos - elseStartPos))
+			return;
+		
+		byte[] thenBytes = getCodeBytes(method, thenStartPos, thenFinishPos);
+		byte[] elseBytes = getCodeBytes(method, elseStartPos, elseFinishPos);
+		
+		if (!Arrays.equals(thenBytes, elseBytes))
+			return;
+		
+		bugReporter.reportBug(new BugInstance(this, "DB_DUPLICATE_BRANCHES", LOW_PRIORITY)
+				.addClass(classContext.getJavaClass())
+				.addMethod(classContext.getJavaClass().getClassName(), method.getName(), method.getSignature())
+				.addSourceLineRange(this, 
+						thenBB.getFirstInstruction().getPosition(),
+						thenBB.getLastInstruction().getPosition())
+				.addSourceLineRange(this, 
+						elseBB.getFirstInstruction().getPosition(),
+						elseBB.getLastInstruction().getPosition()));		
+	}
+	
+	private void findSwitchDuplicates(CFG cfg, Method method, BasicBlock bb) {		
+		Iterator<Edge> iei = cfg.outgoingEdgeIterator(bb);
+		
+		List<Integer> switchPos = new ArrayList<Integer>();
+		int defaultPos = 0;
+		
+		while (iei.hasNext()) {
+			Edge e = iei.next();
+			if (EdgeTypes.SWITCH_EDGE == e.getType()) {
+				BasicBlock target = e.getTarget();
+				switchPos.add(new Integer(target.getFirstInstruction().getPosition()));
+			} else if (EdgeTypes.SWITCH_DEFAULT_EDGE == e.getType()) {
+				BasicBlock target = e.getTarget();
+				defaultPos = target.getFirstInstruction().getPosition();
+			} else
+				return;
+		}
+		
+		if (switchPos.size() < 2)
+			return;
+		
+		if (defaultPos > 0)
+			switchPos.add(new Integer(defaultPos));
+		
+		//NOTE: We really need to walk the whole block and convert relative branches to absolute branches
+		// otherwise we miss many duplicate branches. Will work on this next
+		// for now we drop off the last byte of the block. This is really incorrect, but ok for now
+		for (int i = 0; i < switchPos.size()-2; i++) {
+			for (int j = i+1; j < switchPos.size()-1; j++) {
+				byte[] s1Bytes = getCodeBytes(method, switchPos.get(i).intValue(), switchPos.get(i+1).intValue()-1);
+				byte[] s2Bytes = getCodeBytes(method, switchPos.get(j).intValue(), switchPos.get(j+1).intValue()-1);
 				
-				if ((thenBB == null) || (elseBB == null) 
-				||  (thenBB.getFirstInstruction() == null) || (elseBB.getFirstInstruction() == null))
-					continue;
-				
-				int thenStartPos = thenBB.getFirstInstruction().getPosition();
-				int elseStartPos = elseBB.getFirstInstruction().getPosition();
-				
-				BasicBlock thenFinishBlock = findThenFinish(cfg, thenBB, elseStartPos);
-				
-				if (thenFinishBlock == null)
-					continue;
-				
-				Instruction lastFinishIns = thenFinishBlock.getLastInstruction().getInstruction();
-				if (!(lastFinishIns instanceof GotoInstruction))
-					continue;
-				
-				int thenFinishPos = thenFinishBlock.getLastInstruction().getPosition();
-				int elseFinishPos = ((GotoInstruction) lastFinishIns).getTarget().getPosition();
-				
-				if (thenFinishPos >= elseStartPos)
-					continue;
-				
-				if ((thenFinishPos - thenStartPos) != (elseFinishPos - elseStartPos))
-					continue;
-				
-				byte[] thenBytes = getCodeBytes(method, thenStartPos, thenFinishPos);
-				byte[] elseBytes = getCodeBytes(method, elseStartPos, elseFinishPos);
-				
-				if (!Arrays.equals(thenBytes, elseBytes))
+				if (!Arrays.equals(s1Bytes, s2Bytes))
 					continue;
 				
 				bugReporter.reportBug(new BugInstance(this, "DB_DUPLICATE_BRANCHES", LOW_PRIORITY)
 						.addClass(classContext.getJavaClass())
 						.addMethod(classContext.getJavaClass().getClassName(), method.getName(), method.getSignature())
 						.addSourceLineRange(this, 
-								thenBB.getFirstInstruction().getPosition(),
-								thenBB.getLastInstruction().getPosition())
+								switchPos.get(i).intValue(),
+								switchPos.get(i+1).intValue())
 						.addSourceLineRange(this, 
-								elseBB.getFirstInstruction().getPosition(),
-								elseBB.getLastInstruction().getPosition()));
+								switchPos.get(j).intValue(),
+								switchPos.get(j+1).intValue()));
+				j = switchPos.size();
 			}
-		} catch (Exception e) {
-			bugReporter.logError("Failure examining basic blocks in Duplicate Branches detector", e);
 		}
 	}
 	
--- findbugs/test/DuplicateBranches.java
+++ findbugs/test/DuplicateBranches.java
@@ -1,7 +1,7 @@
 
 public class DuplicateBranches
 {
-	public static int doit(boolean b, int i, int j)
+	public int doit(boolean b, int i, int j)
 	{
 		int z;
 		if (b)
@@ -15,5 +15,59 @@ public class DuplicateBranches
 			z = k / 100;
 		}
 		return z;
+	}
+	
+	public int doit2(int i, int j)
+	{
+		switch(i)
+		{
+			case 0:
+				j = j + 1;
+				if (i < j)
+					j = i;
+			break;
+				
+			case 1:
+				j = j + 1;
+				if (i < j)
+					j = i;
+			break;
+			
+			case 2:
+				j = j + 1;
+				if (i < j)
+					j = i;
+			break;
+			
+			default:
+				j = j + 1;
+				if (i < j)
+					j = i;
+			break;
+		}
+		return j;
+	}
+	
+	public int doit3(int i, int j)
+	{
+		switch(i)
+		{
+			case 1:
+				j = i + 1;
+			break;
+				
+			case 10:
+				j = i + 1;
+			break;
+			
+			case 100:
+				j = i + 1;
+			break;
+			
+			default:
+				j = i + 1;
+			break;
+		}
+		return j;
 	}
 }
