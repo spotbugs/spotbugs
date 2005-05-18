25836e888f687c6858d601ed02952e64a0edc068
David Hovemeyer <david.hovemeyer@gmail.com>
2005-05-18 15:46:55 +0000

    Initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4235 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/AbstractDataflow.java
@@ -0,0 +1,66 @@
+/*
+ * Bytecode analysis framework
+ * Copyright (C) 2005, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.ba;
+
+/**
+ * Convenience class for defining Dataflow classes which use an
+ * AbstractDataflowAnalysis subtype.  The main functionality is offering
+ * getFact{At,After}Location() methods which forward to the actual
+ * analysis object.
+ * 
+ * @see edu.umd.cs.findbugs.ba.Dataflow
+ * @see edu.umd.cs.findbugs.ba.AbstractDataflowAnalysis
+ * @author David Hovemeyer
+ */
+public class AbstractDataflow<Fact, AnalysisType extends AbstractDataflowAnalysis<Fact>>
+		extends Dataflow<Fact, AnalysisType> {
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param cfg      CFG of the method on which dfa is performed
+	 * @param analysis the dataflow analysis
+	 */
+	public AbstractDataflow(CFG cfg, AnalysisType analysis) {
+		super(cfg, analysis);
+	}
+
+	/**
+	 * Get dataflow fact at given Location.
+	 * 
+	 * @param location the Location
+	 * @return the dataflow fact
+	 * @throws DataflowAnalysisException
+	 */
+	public Fact getFactAtLocation(Location location) throws DataflowAnalysisException {
+		return getAnalysis().getFactAtLocation(location);
+	}
+	
+	/**
+	 * Get dataflow fact after given Location.
+	 * 
+	 * @param location the Location
+	 * @return the dataflow fact
+	 * @throws DataflowAnalysisException
+	 */
+	public Fact getFactAfterLocation(Location location) throws DataflowAnalysisException {
+		return getAnalysis().getFactAfterLocation(location);
+	}
+}
