ad245c882f29ba158e98ae8aa8e27f1c5ce82522
David Hovemeyer <david.hovemeyer@gmail.com>
2005-05-21 18:57:16 +0000

    CheckCalls detector, which is just for debugging method call resolution.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4262 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/CheckCalls.java
@@ -0,0 +1,128 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2005, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.detect;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.apache.bcel.classfile.Method;
+import org.apache.bcel.generic.Instruction;
+import org.apache.bcel.generic.InvokeInstruction;
+import org.apache.bcel.generic.MethodGen;
+
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.Detector;
+import edu.umd.cs.findbugs.ba.CFG;
+import edu.umd.cs.findbugs.ba.CFGBuilderException;
+import edu.umd.cs.findbugs.ba.ClassContext;
+import edu.umd.cs.findbugs.ba.DataflowAnalysisException;
+import edu.umd.cs.findbugs.ba.Hierarchy;
+import edu.umd.cs.findbugs.ba.Location;
+import edu.umd.cs.findbugs.ba.SignatureConverter;
+import edu.umd.cs.findbugs.ba.XMethod;
+
+/**
+ * This is just for debugging method call resolution.
+ * 
+ * @author David Hovemeyer
+ */
+public class CheckCalls implements Detector {
+	
+	private static final String METHOD = System.getProperty("checkcalls.method");
+	private static final String TARGET_METHOD = System.getProperty("checkcalls.targetmethod");
+	
+	BugReporter bugReporter;
+	
+	public CheckCalls(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.umd.cs.findbugs.Detector#visitClassContext(edu.umd.cs.findbugs.ba.ClassContext)
+	 */
+	public void visitClassContext(ClassContext classContext) {
+		Method[] methodList = classContext.getJavaClass().getMethods();
+		for (int i = 0; i < methodList.length; ++i) {
+			Method method = methodList[i];
+			if (method.getCode() == null)
+				continue;
+			
+			//System.out.println("--> " + method.getName());
+			if (METHOD != null && !method.getName().equals(METHOD))
+				continue;
+
+			try {
+				System.out.println("Analyzing " + 
+						SignatureConverter.convertMethodSignature(classContext.getJavaClass(), method)
+				);
+				analyzeMethod(classContext, method);
+			} catch (CFGBuilderException e) {
+				bugReporter.logError("Error", e);
+			} catch (DataflowAnalysisException e) {
+				bugReporter.logError("Error", e);
+			} catch (ClassNotFoundException e) {
+				bugReporter.reportMissingClass(e);
+			}
+		}
+	}
+
+	private void analyzeMethod(ClassContext classContext, Method method)
+			throws CFGBuilderException, ClassNotFoundException, DataflowAnalysisException {
+		CFG cfg = classContext.getCFG(method);
+		MethodGen methodGen = classContext.getMethodGen(method);
+		for (Iterator<Location> i = cfg.locationIterator(); i.hasNext();) {
+			Location location = i.next();
+			Instruction ins = location.getHandle().getInstruction();
+
+			if (ins instanceof InvokeInstruction) {
+				if (TARGET_METHOD != null
+						&& !((InvokeInstruction)ins).getMethodName(classContext.getConstantPoolGen()).equals(TARGET_METHOD))
+					continue;
+				
+				System.out.println("Method invocation: " + location.getHandle());
+				System.out.println("\tInvoking: " +
+						SignatureConverter.convertMethodSignature((InvokeInstruction)ins,classContext.getConstantPoolGen()));
+				
+				Method proto = Hierarchy.findPrototypeMethod(
+						(InvokeInstruction) ins,
+						classContext.getConstantPoolGen());
+				if (proto == null) {
+					System.out.println("\tUnknown prototype method");
+				} else {
+					System.out.println("\tPrototype method: " + SignatureConverter.convertMethodSignature(
+							classContext.getJavaClass(), method));
+				}
+				Set<XMethod> calledMethodSet = Hierarchy.resolveMethodCallTargets(
+						(InvokeInstruction) ins,
+						classContext.getTypeDataflow(method).getFactAtLocation(location),
+						classContext.getConstantPoolGen()
+				);
+				System.out.println("\tTarget method set: " + calledMethodSet);
+			}
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.umd.cs.findbugs.Detector#report()
+	 */
+	public void report() {
+	}
+
+}
