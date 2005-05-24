d98c9d7e111e286449c7bb987f7b8f5ffffe9b17
David Hovemeyer <david.hovemeyer@gmail.com>
2005-05-24 15:42:09 +0000

    Added method to get the load type for the field, based on the types we saw stored into it.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4297 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/ba/type/FieldStoreType.java
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/type/FieldStoreType.java
@@ -22,21 +22,77 @@ package edu.umd.cs.findbugs.ba.type;
 import java.util.HashSet;
 import java.util.Iterator;
 
+import org.apache.bcel.classfile.ClassFormatException;
+import org.apache.bcel.generic.ReferenceType;
+import org.apache.bcel.generic.Type;
+
+import edu.umd.cs.findbugs.ba.AnalysisContext;
+import edu.umd.cs.findbugs.ba.Hierarchy;
+
 /**
+ * Field property storing the types of values stored
+ * in a field.  The idea is that we may be able to determine
+ * a more precise type for values loaded from the field
+ * than the field type alone would indicate.
+ * 
  * @author David Hovemeyer
  */
 public class FieldStoreType {
 	private HashSet<String> typeSignatureSet;
+	private ReferenceType loadType;
 	
 	public FieldStoreType() {
 		this.typeSignatureSet = new HashSet<String>();
 	}
 	
+	// TODO: type may be exact
 	public void addTypeSignature(String signature) {
+		loadType = null;
 		typeSignatureSet.add(signature);
 	}
 	
 	public Iterator<String> signatureIterator() {
 		return typeSignatureSet.iterator();
+	}
+	
+	public ReferenceType getLoadType(ReferenceType fieldType) {
+		if (loadType == null) {
+			computeLoadType(fieldType);
+		}
+		return loadType;
+	}
+
+	private void computeLoadType(ReferenceType fieldType) {
+		ReferenceType leastSupertype = null;
+		
+		for (Iterator<String> i = signatureIterator(); i.hasNext();) {
+			try {
+				String signature = i.next();
+				Type type = Type.getType(signature);
+				if (!(type instanceof ReferenceType))
+					continue;
+				
+				// FIXME: this will mangle interface types, since
+				// getFirstCommonSuperclass() ignores interfaces.
+				leastSupertype = (leastSupertype == null)
+					? (ReferenceType) type
+					: leastSupertype.getFirstCommonSuperclass((ReferenceType) type);
+					
+			} catch (ClassFormatException e) {
+				// Bad signature: ignore
+			} catch (ClassNotFoundException e) {
+				AnalysisContext.currentAnalysisContext().getLookupFailureCallback().reportMissingClass(e);
+			}
+		}
+		
+		try {
+			if (leastSupertype != null && Hierarchy.isSubtype(leastSupertype, fieldType))
+				loadType = leastSupertype;
+		} catch (ClassNotFoundException e) {
+			AnalysisContext.currentAnalysisContext().getLookupFailureCallback().reportMissingClass(e);
+		}
+		
+		if (loadType == null)
+			loadType = fieldType;
 	}
 }
