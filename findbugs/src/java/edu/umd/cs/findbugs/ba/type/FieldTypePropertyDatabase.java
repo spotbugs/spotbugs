859f2dda22a5bcdb87deba1761ce21c242444633
David Hovemeyer <david.hovemeyer@gmail.com>
2005-05-24 01:30:36 +0000

    Added a field property database for keeping track of what types are stored in fields.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4290 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/type/FieldTypePropertyDatabase.java
@@ -0,0 +1,57 @@
+/*
+ * Bytecode analysis framework
+ * Copyright (C) 2005, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.ba.type;
+
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import edu.umd.cs.findbugs.ba.interproc.FieldPropertyDatabase;
+import edu.umd.cs.findbugs.ba.interproc.PropertyDatabaseFormatException;
+
+/**
+ * @author David Hovemeyer
+ */
+public class FieldTypePropertyDatabase
+	extends FieldPropertyDatabase<TypeSet> {
+
+	//@Override
+	protected TypeSet decodeProperty(String propStr) throws PropertyDatabaseFormatException {
+		TypeSet property = new TypeSet();
+		StringTokenizer t = new StringTokenizer(propStr, ",");
+		while (t.hasMoreTokens()) {
+			String signature = t.nextToken();
+			property.addTypeSignature(signature);
+		}
+		return property;
+	}
+
+	//@Override
+	protected String encodeProperty(TypeSet property) {
+		StringBuffer buf = new StringBuffer();
+		for (Iterator<String> i = property.signatureIterator(); i.hasNext();) {
+			if (buf.length() > 0) {
+				buf.append(',');
+			}
+			buf.append(i.next());
+		}
+		return buf.toString();
+	}
+
+}
