91d487844320976c733e673c9487a3239c14cc95
David Hovemeyer <david.hovemeyer@gmail.com>
2005-05-25 19:30:27 +0000

    Added default serialVersionUID
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4321 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/gui/RunAnalysisDialog.java
+++ findbugs/src/java/edu/umd/cs/findbugs/gui/RunAnalysisDialog.java
@@ -40,7 +40,8 @@ import edu.umd.cs.findbugs.FindBugsProgress;
  * @author David Hovemeyer
  */
 public class RunAnalysisDialog extends javax.swing.JDialog {
-	
+	private static final long serialVersionUID = 1L;
+
 	private class RunAnalysisProgress implements FindBugsProgress {
 		private int goal, count;
 
