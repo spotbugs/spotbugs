abd4ad190e0a16089faf0b10efb95e6f9de7adf2
Dave Brosius <dbrosius@mebigfatguy.com>
2005-05-26 02:12:38 +0000

    New detector to find inner class write access of owning class'es private member variables, which uses the synthesized static accessor method.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4330 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/InefficientMemberAccess.java
@@ -0,0 +1,91 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2005 Dave Brosius <dbrosius@users.sourceforge.net>
+ * Copyright (C) 2005 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package edu.umd.cs.findbugs.detect;
+
+import org.apache.bcel.classfile.Code;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.generic.Type;
+
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.BytecodeScanningDetector;
+import edu.umd.cs.findbugs.OpcodeStack;
+import edu.umd.cs.findbugs.StatelessDetector;
+import edu.umd.cs.findbugs.ba.ClassContext;
+import edu.umd.cs.findbugs.visitclass.Constants2;
+
+public class InefficientMemberAccess extends BytecodeScanningDetector implements Constants2, StatelessDetector {
+	
+	public static final String ACCESS_PREFIX = "access$";
+	private BugReporter bugReporter;
+	private String clsName;
+	
+	public InefficientMemberAccess(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
+
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+	
+	public void visitClassContext(ClassContext classContext) {
+		JavaClass cls = classContext.getJavaClass();
+		clsName = cls.getClassName();
+		if (clsName.indexOf("$") >= 0)
+			super.visitClassContext(classContext);
+	}
+	
+	public void visit(Code obj) {
+		super.visit(obj);
+	}
+	
+	public void sawOpcode(int seen) {
+		int varSlot;
+		if (seen == INVOKESTATIC) {
+			String methodName = getNameConstantOperand();
+			if (!methodName.startsWith(ACCESS_PREFIX))
+				return;
+			try {
+				varSlot = Integer.valueOf(methodName.substring(ACCESS_PREFIX.length()));
+			}
+			catch (NumberFormatException nfe) {
+				return;
+			}
+			String methodSig = getSigConstantOperand();
+			Type[] argTypes = Type.getArgumentTypes(methodSig);
+			if (argTypes.length != 2)
+				return;
+			String parCls = argTypes[0].getSignature();
+			parCls = parCls.substring(1, parCls.length() - 1);
+			if (!parCls.equals(getClassConstantOperand()))
+				return;
+			if (!argTypes[1].getSignature().equals(Type.getReturnType(methodSig).getSignature()))
+				return;
+			
+			bugReporter.reportBug(new BugInstance(this, "IMA_INEFFICIENT_MEMBER_ACCESS", LOW_PRIORITY)
+				.addClassAndMethod(this)
+				.addSourceLine(this));
+		}
+	}
+	
+	
+	
+
+}
