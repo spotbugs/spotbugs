6a1ffaa07697028cf42b9d1277d266176d9a1e8f
David Hovemeyer <david.hovemeyer@gmail.com>
2005-05-30 02:07:02 +0000

    Added an ad-hoc means of loading and saving training data for CheckReturnValue annotations.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4405 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/NoteCheckReturnValue.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/NoteCheckReturnValue.java
@@ -19,36 +19,52 @@
 
 package edu.umd.cs.findbugs.detect;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.NonReportingDetector;
-import edu.umd.cs.findbugs.StatelessDetector;
 import edu.umd.cs.findbugs.ba.ClassContext;
+import edu.umd.cs.findbugs.ba.XMethod;
+import edu.umd.cs.findbugs.ba.XMethodFactory;
 import edu.umd.cs.findbugs.ba.bcp.Invoke;
 import edu.umd.cs.findbugs.visitclass.AnnotationVisitor;
 import edu.umd.cs.findbugs.visitclass.Constants2;
 
 public class NoteCheckReturnValue extends AnnotationVisitor 
-  implements NonReportingDetector, Constants2, StatelessDetector {
+  implements NonReportingDetector, Constants2 {
+	
+	// XXX: Hack, for now
+	private static final String LOAD_TRAINING = System.getProperty("findbugs.checkreturn.loadtraining");
+	private static final String SAVE_TRAINING = System.getProperty("findbugs.checkreturn.savetraining");
 
 	private BugReporter bugReporter;
-
+	private boolean checkLoad;
+	private Set<XMethod> checkReturnValueDatabase;
 
 	public NoteCheckReturnValue(BugReporter bugReporter) {
 
 		this.bugReporter = bugReporter;
-	}
-
-	public Object clone() throws CloneNotSupportedException {
-		return super.clone();
+		if (SAVE_TRAINING != null) {
+			checkReturnValueDatabase = new HashSet<XMethod>();
+		}
 	}
 
 	public void visitClassContext(ClassContext classContext) {
+		if (LOAD_TRAINING != null && !checkLoad) {
+			loadTraining();
+			checkLoad = true;
+		}
 		classContext.getJavaClass().accept(this);
 	}
 
-        public void visitAnnotation(String annotationClass, Map<String, Object> map,
+		public void visitAnnotation(String annotationClass, Map<String, Object> map,
  boolean runtimeVisible)  {
 		if (!annotationClass.endsWith("CheckReturnValue")) return;
 		if (!visitingMethod()) return;
@@ -57,9 +73,69 @@ public class NoteCheckReturnValue extends AnnotationVisitor
 			getMethodName(),
 			getMethodSig(),
 			getThisClass().isStatic() ? Invoke.STATIC : Invoke.ANY);
+		
+		if (SAVE_TRAINING != null) {
+			checkReturnValueDatabase.add(XMethodFactory.createXMethod(this));
+		}
 		}
 
 	public void report() {
+		if (SAVE_TRAINING != null) {
+			saveTraining();
 		}
+		}
+
+	private void loadTraining() {
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(LOAD_TRAINING));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] tuple = line.split(",");
+				if (tuple.length != 4)
+					continue;
+				BCPMethodReturnCheck.addMethodWhoseReturnMustBeChecked(
+						tuple[0], tuple[1], tuple[2],
+						Boolean.valueOf(tuple[3]).booleanValue() ? Invoke.STATIC : Invoke.ANY);
+			}
+		} catch (IOException e) {
+			bugReporter.logError("Couldn't load check return database");
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					// Ignore
+				}
+			}
+		}
+	}
+
+	private void saveTraining() {
+		BufferedWriter writer = null;
+		try {
+			writer = new BufferedWriter(new FileWriter(SAVE_TRAINING));
+			for (XMethod xmethod : checkReturnValueDatabase) {
+				writer.write(xmethod.getClassName());
+				writer.write(",");
+				writer.write(xmethod.getName());
+				writer.write(",");
+				writer.write(xmethod.getSignature());
+				writer.write(",");
+				writer.write(String.valueOf(xmethod.getAccessFlags()));
+				writer.write("\n");
+			}
+		} catch (IOException e) {
+			bugReporter.logError("Couldn't write check return value training data", e);
+		} finally {
+			if (writer != null) {
+				try {
+					writer.close();
+				} catch (IOException e) {
+					// ignore
+				}
+			}
+		}
+	}
 
 }
