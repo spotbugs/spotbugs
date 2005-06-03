0565fc6c81a4951a63b6a6682a0623077401b69a
Dave Brosius <dbrosius@mebigfatguy.com>
2005-06-03 02:49:17 +0000

    New detector to find direct allocation of XML parsers/transformers rather than using the prefered factory methods to create these objects. By specifying an implementation class, the ability to switch parsers at runtime is removed.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4432 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/XMLFactoryBypass.java
@@ -0,0 +1,98 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2005 Dave Brosius <dbrosius@users.sourceforge.net>
+ * Copyright (C) 2005 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package edu.umd.cs.findbugs.detect;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.bcel.Repository;
+import org.apache.bcel.classfile.JavaClass;
+
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.BytecodeScanningDetector;
+import edu.umd.cs.findbugs.StatelessDetector;
+import edu.umd.cs.findbugs.visitclass.Constants2;
+
+public class XMLFactoryBypass extends BytecodeScanningDetector implements Constants2, StatelessDetector {
+    private BugReporter bugReporter;
+    private static final Set<String> xmlInterfaces = new HashSet<String>()
+    {{
+        add("javax.xml.parsers.DocumentBuilder");
+        add("org.w3c.dom.Document");
+        add("javax.xml.parsers.SAXParser");
+        add("org.xml.sax.XMLReader");
+        add("org.xml.sax.XMLFilter");
+        add("javax.xml.transform.Transformer");
+    }};
+
+    public XMLFactoryBypass(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
+    
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+	
+	public void sawOpcode(int seen) {
+	    try {
+		    if (seen == INVOKESPECIAL) {
+		        String newClsName = getClassConstantOperand();
+		        if (newClsName.startsWith("java/") || newClsName.startsWith("javax/"))
+		            return;
+		        
+		        if (!getNameConstantOperand().equals("<init>"))
+		            return;
+		        
+		        String invokerClsName = this.getClassName();
+		        if (samePackageBase(invokerClsName, newClsName))
+		            return;
+		        
+		        JavaClass newCls = Repository.lookupClass(getClassConstantOperand());
+		        JavaClass[] infs = newCls.getAllInterfaces();
+		        for (int i = 0; i < infs.length; i++) {
+		            if (xmlInterfaces.contains(infs[i].getClassName())) {
+		                bugReporter.reportBug( new BugInstance(this, "XFB_XML_FACTORY_BYPASS", LOW_PRIORITY)
+		                	.addClassAndMethod(this)
+		                	.addSourceLine(this));
+		            }
+		        }
+		    }
+	    } catch (ClassNotFoundException cnfe) {
+	        bugReporter.reportMissingClass(cnfe);
+	    }
+	}
+	
+	public boolean samePackageBase(String invokerClsName, String newClsName)
+	{
+	    String[] invokerParts = invokerClsName.split("/");
+	    String[] newClsParts = newClsName.split("/");
+	    
+	    if (newClsParts.length < 3)
+	        return false;
+	    if (invokerParts.length < 3)
+	        return false;
+	    
+	    if (!invokerParts[0].equals(newClsParts[0]))
+	        return false;
+	    
+	    return invokerParts[1].equals(newClsParts[1]);
+	}
+}
