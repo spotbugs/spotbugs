b73baa3491947c69903aea1214b27a32326c9714
David Hovemeyer <david.hovemeyer@gmail.com>
2005-06-16 13:28:42 +0000

    Started working on hash comparisions for classes and methods.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4528 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/FuzzyBugComparator.java
+++ findbugs/src/java/edu/umd/cs/findbugs/FuzzyBugComparator.java
@@ -21,6 +21,9 @@ package edu.umd.cs.findbugs;
 
 import java.util.Comparator;
 
+import edu.umd.cs.findbugs.ba.ClassHash;
+import edu.umd.cs.findbugs.ba.MethodHash;
+
 /**
  * @author David Hovemeyer
  */
@@ -31,7 +34,7 @@ public class FuzzyBugComparator implements Comparator<BugInstance> {
 		this.bugCollection = bugCollection;
 	}
 	
-	public int compare(BugInstance o1, BugInstance o2) {
+	public int compare(BugInstance a, BugInstance b) {
 		
 		
 		// TODO
@@ -39,6 +42,45 @@ public class FuzzyBugComparator implements Comparator<BugInstance> {
 	}
 	
 	// Compare classes: either exact fully qualified name must match, or class hash must match
+	private boolean sameClass(ClassAnnotation aClass, ClassAnnotation bClass) {
+		if (aClass == null || bClass == null) {
+			return aClass == null && bClass == null;
+		}
+		
+		if (aClass.getClassName().equals(bClass.getClassName()))
+			return true;
+		
+		// Get class hashes
+		ClassHash aHash = bugCollection.getClassHash(aClass.getClassName());
+		ClassHash bHash = bugCollection.getClassHash(bClass.getClassName());
+		if (aHash == null || bHash == null)
+			return false;
+		
+		return aHash.isSameHash(bHash);
+	}
 	
 	// Compare methods: either exact name and signature must match, or method hash must match
+	private boolean sameMethod(MethodAnnotation aMethod, MethodAnnotation bMethod) {
+		if (aMethod == null || bMethod == null) {
+			return aMethod == null && bMethod == null;
+		}
+		
+		if (aMethod.getMethodName().equals(bMethod.getMethodName()) &&
+				aMethod.getMethodSignature().equals(bMethod.getMethodSignature()))
+			return true;
+		
+		// Get class hashes for primary classes
+		ClassHash aClassHash = bugCollection.getClassHash(aMethod.getClassName());
+		ClassHash bClassHash = bugCollection.getClassHash(bMethod.getClassName());
+		if (aClassHash == null || bClassHash == null)
+			return false;
+		
+		// Look up method hashes
+		MethodHash aHash = aClassHash.getMethodHash(aMethod.toXMethod());
+		MethodHash bHash = bClassHash.getMethodHash(bMethod.toXMethod());
+		if (aHash == null || bHash == null)
+			return false;
+		
+		return aHash.isSameHash(bHash);
+	}
 }
