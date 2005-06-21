a976f2adc607dd441b222c62990aba859717072d
David Hovemeyer <david.hovemeyer@gmail.com>
2005-06-21 01:34:45 +0000

    Initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4558 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/CountClasses.java
@@ -0,0 +1,53 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2005, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+import edu.umd.cs.findbugs.ba.ClassHash;
+
+/**
+ * @author David Hovemeyer
+ */
+public class CountClasses {
+	public static void main(String[] args) throws Exception {
+		if (args.length != 1) {
+			System.err.println("Usage: " + CountClasses.class.getName() + " <bug collection>");
+			System.exit(1);
+		}
+		
+		SortedBugCollection bugCollection = new SortedBugCollection();
+		bugCollection.readXML(args[0], new Project());
+		
+		Set<String> classesSeen = new TreeSet<String>();
+		Set<ClassHash> classHashesSeen = new TreeSet<ClassHash>();
+		
+		for (Iterator<ClassHash> i = bugCollection.classHashIterator(); i.hasNext(); ) {
+			ClassHash classHash = i.next();
+			classesSeen.add(classHash.getClassName());
+			classHashesSeen.add(classHash);
+		}
+		
+		System.out.println(classesSeen.size() + " classes");
+		System.out.println(classHashesSeen.size() + " class hash values seen");
+	}
+}
