33935d58452ffb49d8dd7f9554ee094137ce4f40
David Hovemeyer <david.hovemeyer@gmail.com>
2005-06-23 14:17:03 +0000

    Initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4589 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/AppVersion.java
@@ -0,0 +1,99 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2005, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs;
+
+import java.io.IOException;
+
+import edu.umd.cs.findbugs.xml.XMLAttributeList;
+import edu.umd.cs.findbugs.xml.XMLOutput;
+import edu.umd.cs.findbugs.xml.XMLWriteable;
+
+/**
+ * A version of an analyzed application.
+ * Application versions are uniquely identified by a sequence number,
+ * which represents a run of FindBugs on the application.
+ * Timestamp is when FindBugs was run (according to System.currentTimeMillis()),
+ * and the release name is available if the user provided it.
+ * 
+ * @author David Hovemeyer
+ */
+public class AppVersion implements XMLWriteable {
+	/**
+	 * XML element name for a stored AppVersion object.
+	 */
+	private static final String ELEMENT_NAME = "AppVersion";
+	
+	private long sequence;
+	private long timestamp;
+	private String releaseName;
+	
+	public AppVersion(long sequence) {
+		this.sequence = sequence;
+		this.timestamp = -1;
+		this.releaseName = "";
+	}
+	
+	/**
+	 * @return Returns the sequence.
+	 */
+	public long getSequenceNumber() {
+		return sequence;
+	}
+	
+	/**
+	 * @return Returns the timestamp.
+	 */
+	public long getTimestamp() {
+		return timestamp;
+	}
+	
+	/**
+	 * @return Returns the releaseName.
+	 */
+	public String getReleaseName() {
+		return releaseName;
+	}
+	
+	/**
+	 * @param timestamp The timestamp to set.
+	 */
+	public AppVersion setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+		return this;
+	}
+	
+	/**
+	 * @param releaseName The releaseName to set.
+	 */
+	public AppVersion setReleaseName(String releaseName) {
+		this.releaseName = releaseName;
+		return this;
+	}
+	
+	/* (non-Javadoc)
+	 * @see edu.umd.cs.findbugs.xml.XMLWriteable#writeXML(edu.umd.cs.findbugs.xml.XMLOutput)
+	 */
+	public void writeXML(XMLOutput xmlOutput) throws IOException {
+		xmlOutput.openCloseTag(ELEMENT_NAME, new XMLAttributeList()
+				.addAttribute("sequence", String.valueOf(sequence))
+				.addAttribute("timestamp", String.valueOf(timestamp))
+				.addAttribute("release", releaseName));
+	}
+}
