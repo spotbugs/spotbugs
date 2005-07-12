6792c4972591cc98ad16c64a1b96c18976ac5f21
Bill Pugh <bill.pugh@gmail.com>
2005-07-12 19:53:05 +0000

    Handle corner case where the only way out of the IRL is to throw an exception
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4715 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/InfiniteRecursiveLoop.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/InfiniteRecursiveLoop.java
@@ -29,15 +29,23 @@ import edu.umd.cs.findbugs.BytecodeScanningDetector;
 import edu.umd.cs.findbugs.StatelessDetector;
 import edu.umd.cs.findbugs.visitclass.Constants2;
 
-public class InfiniteRecursiveLoop extends BytecodeScanningDetector implements Constants2, StatelessDetector {
+public class InfiniteRecursiveLoop extends BytecodeScanningDetector implements Constants2,
+		StatelessDetector {
 
 	private BugReporter bugReporter;
+
 	private boolean seenTransferOfControl;
+
 	private boolean seenReturn;
+
+	private boolean seenThrow;
+
 	private boolean seenStateChange;
+
 	private int largestBranchTarget;
 
 	private final static boolean DEBUG = Boolean.getBoolean("irl.debug");
+
 	public InfiniteRecursiveLoop(BugReporter bugReporter) {
 		this.bugReporter = bugReporter;
 	}
@@ -50,16 +58,18 @@ public class InfiniteRecursiveLoop extends BytecodeScanningDetector implements C
 	}
 
 	int parameters;
+
 	public void visit(Method obj) {
 		seenTransferOfControl = false;
 		seenStateChange = false;
 		seenReturn = false;
+		seenThrow = false;
 		largestBranchTarget = -1;
-                parameters = stack.resetForMethodEntry(this);
-		if (DEBUG ) {
-		System.out.println();
-		System.out.println(" --- " + getFullyQualifiedMethodName());
-		System.out.println();
+		parameters = stack.resetForMethodEntry(this);
+		if (DEBUG) {
+			System.out.println();
+			System.out.println(" --- " + getFullyQualifiedMethodName());
+			System.out.println();
 		}
 	}
 
@@ -67,11 +77,9 @@ public class InfiniteRecursiveLoop extends BytecodeScanningDetector implements C
 		if (largestBranchTarget < seen)
 			largestBranchTarget = seen;
 		seenTransferOfControl = true;
-		}
-
+	}
 
 	OpcodeStack stack = new OpcodeStack();
-
 
 	/** Signal an infinite loop if either:
 	 * we see a call to the same method with the same parameters, or
@@ -79,126 +87,122 @@ public class InfiniteRecursiveLoop extends BytecodeScanningDetector implements C
 	 * has been no transfer of control.	
 	 */
 	public void sawOpcode(int seen) {
-		
-		if (seenReturn && seenTransferOfControl && seenStateChange) return;
 
-	
-		if (DEBUG ) {
-		System.out.println(stack);	
-		System.out.println(getPC() + " : " + OPCODE_NAMES[seen]);
+		if (seenReturn && seenTransferOfControl && seenStateChange)
+			return;
+
+		if (DEBUG) {
+			System.out.println(stack);
+			System.out.println(getPC() + " : " + OPCODE_NAMES[seen]);
 		}
 
-	
 		if ((seen == INVOKEVIRTUAL || seen == INVOKEINTERFACE)
-			    && getNameConstantOperand().equals("add")
-			    && getSigConstantOperand().equals(
-				"(Ljava/lang/Object;)Z")
-			    && stack.getStackDepth() >= 2)  {
-				OpcodeStack.Item it0 = stack.getStackItem(0);
-				int r0 = it0.getRegisterNumber();
-				OpcodeStack.Item it1 = stack.getStackItem(1);
-				int r1 = it1.getRegisterNumber();
-				if (r0 == r1 && r0 > 0)
-				bugReporter.reportBug(new BugInstance(this, "IL_CONTAINER_ADDED_TO_ITSELF", NORMAL_PRIORITY)
-				        .addClassAndMethod(this)
-				        .addSourceLine(this)
-					);
+				&& getNameConstantOperand().equals("add")
+				&& getSigConstantOperand().equals("(Ljava/lang/Object;)Z")
+				&& stack.getStackDepth() >= 2) {
+			OpcodeStack.Item it0 = stack.getStackItem(0);
+			int r0 = it0.getRegisterNumber();
+			OpcodeStack.Item it1 = stack.getStackItem(1);
+			int r1 = it1.getRegisterNumber();
+			if (r0 == r1 && r0 > 0)
+				bugReporter.reportBug(new BugInstance(this, "IL_CONTAINER_ADDED_TO_ITSELF",
+						NORMAL_PRIORITY).addClassAndMethod(this).addSourceLine(this));
 		}
 
+		if ((seen == INVOKEVIRTUAL || seen == INVOKESPECIAL || seen == INVOKEINTERFACE || seen == INVOKESTATIC)
+				&& getNameConstantOperand().equals(getMethodName())
+				&& getSigConstantOperand().equals(getMethodSig())
+				&& ((seen == INVOKESTATIC) == getMethod().isStatic())
+				&& stack.getStackDepth() >= parameters) {
+			if (DEBUG) {
+				System.out.println("IL: Checking...");
+				System.out.println(getClassConstantOperand() + "." + getNameConstantOperand()
+						+ " : " + getSigConstantOperand());
+				System.out.println("vs. " + getClassName() + "." + getMethodName() + " : "
+						+ getMethodSig());
+			}
+			if (getClassConstantOperand().equals(getClassName()) || seen == INVOKEVIRTUAL
+					|| seen == INVOKEINTERFACE) {
+				// Invocation of same method
+				// Now need to see if parameters are the same
+				int firstParameter = 0;
+				if (getMethodName().equals("<init>"))
+					firstParameter = 1;
 
-		if ((seen == INVOKEVIRTUAL || seen == INVOKESPECIAL || seen == INVOKEINTERFACE || seen == INVOKESTATIC) 
-			    && getNameConstantOperand().equals(getMethodName())
-			    && getSigConstantOperand().equals(getMethodSig())
-			    && ((seen == INVOKESTATIC) == getMethod().isStatic())
-			    && stack.getStackDepth() >= parameters)  {
-		if (DEBUG ) {
-		System.out.println("IL: Checking...");
-		System.out.println(getClassConstantOperand() + "." + getNameConstantOperand() + " : " + getSigConstantOperand());
-		System.out.println("vs. " + getClassName() + "." + getMethodName() + " : " + getMethodSig());
-		}
-		if ( getClassConstantOperand().equals(getClassName())
-				|| seen == INVOKEVIRTUAL || seen == INVOKEINTERFACE) {
-			// Invocation of same method
-			// Now need to see if parameters are the same
-			int firstParameter = 0;
-			if (getMethodName().equals("<init>")) firstParameter = 1;
-			
-			// match1 should be true if it is any call to the exact same method
-			// and no state has been change. 
-			// if match1 is true, the method may not always perform
-			// a recursive infinite loop, but this particular method call is an infinite
-			// recursive loop
-			
-			boolean match1 = !seenStateChange;
-			for(int i = firstParameter; match1 && i < parameters; i++) {
-				OpcodeStack.Item it = stack.getStackItem(parameters-1-i);
-				if (!it.isInitialParameter() || it.getRegisterNumber() != i)
-					match1 = false;
-				}
-			
+				// match1 should be true if it is any call to the exact same method
+				// and no state has been change. 
+				// if match1 is true, the method may not always perform
+				// a recursive infinite loop, but this particular method call is an infinite
+				// recursive loop
 
-			boolean sameMethod = 
-				seen == INVOKESTATIC
-				|| getNameConstantOperand().equals("<init>");
-			if (!sameMethod) {
-				// Have to check if first parmeter is the same
-				// know there must be a this argument
-				OpcodeStack.Item p = stack.getStackItem(parameters-1);
-				sameMethod = p.isInitialParameter()
-					&& p.getRegisterNumber() == 0;
+				boolean match1 = !seenStateChange;
+				for (int i = firstParameter; match1 && i < parameters; i++) {
+					OpcodeStack.Item it = stack.getStackItem(parameters - 1 - i);
+					if (!it.isInitialParameter() || it.getRegisterNumber() != i)
+						match1 = false;
 				}
-			
-			// match2 and match3 are two different ways of seeing if the call site
-			// postdominates the method entry. 
-			
-			// technically, we use (!seenTransferOfControl || !seenReturn && largestBranchTarget < getPC())
-			// as a check that the call site postdominates the method entry. If those are true,
-			// and sameMethod is true, we have a guaranteed IL.
-			
-			boolean match2 = sameMethod && !seenTransferOfControl;
-			boolean match3 = sameMethod && !seenReturn && largestBranchTarget < getPC();
-			if (match1 || match2 || match3)   {
-				if (DEBUG) 
-					System.out.println("IL: " + match1 
-						+ " " + match2
-						+ " " + match3);
-				bugReporter.reportBug(new BugInstance(this, "IL_INFINITE_RECURSIVE_LOOP", HIGH_PRIORITY)
-				        .addClassAndMethod(this)
-				        .addSourceLine(this)
-					);
+
+				boolean sameMethod = seen == INVOKESTATIC
+						|| getNameConstantOperand().equals("<init>");
+				if (!sameMethod) {
+					// Have to check if first parmeter is the same
+					// know there must be a this argument
+					OpcodeStack.Item p = stack.getStackItem(parameters - 1);
+					sameMethod = p.isInitialParameter() && p.getRegisterNumber() == 0;
+				}
+
+				// match2 and match3 are two different ways of seeing if the call site
+				// postdominates the method entry. 
+
+				// technically, we use (!seenTransferOfControl || !seenReturn && largestBranchTarget < getPC())
+				// as a check that the call site postdominates the method entry. If those are true,
+				// and sameMethod is true, we have a guaranteed IL.
+
+				boolean match2 = sameMethod && !seenTransferOfControl;
+				boolean match3 = sameMethod && !seenReturn && largestBranchTarget < getPC();
+				if (match1 || match2 || match3) {
+					if (DEBUG)
+						System.out.println("IL: " + match1 + " " + match2 + " " + match3);
+					int priority = HIGH_PRIORITY;
+					if (!match1 && !match2 && seenThrow) priority = NORMAL_PRIORITY;
+					bugReporter.reportBug(new BugInstance(this, "IL_INFINITE_RECURSIVE_LOOP",
+							HIGH_PRIORITY).addClassAndMethod(this).addSourceLine(this));
 				}
 			}
 		}
 
-               switch(seen) {
-                       case ARETURN:
-                       case IRETURN:
-                       case LRETURN:
-                       case RETURN:
-                       case DRETURN:
-                       case FRETURN:
-                       case ATHROW:
-				seenReturn = true;
-                               seenTransferOfControl = true;
-				break;
-			case PUTSTATIC:
-			case PUTFIELD:
-			case IASTORE:
-			case AASTORE:
-			case DASTORE:
-			case FASTORE:
-			case LASTORE:
-			case SASTORE:
-			case CASTORE:
-			case BASTORE:
-			case INVOKEVIRTUAL:
-			case INVOKESPECIAL:
-			case INVOKEINTERFACE:
-			case INVOKESTATIC:
-				seenStateChange = true;
-				break;
-                       }
-		stack.sawOpcode(this,seen);
+		switch (seen) {
+		case ARETURN:
+		case IRETURN:
+		case LRETURN:
+		case RETURN:
+		case DRETURN:
+		case FRETURN:
+			seenReturn = true;
+			seenTransferOfControl = true;
+			break;
+		case ATHROW:
+			seenThrow = true;
+			seenTransferOfControl = true;
+			break;
+		case PUTSTATIC:
+		case PUTFIELD:
+		case IASTORE:
+		case AASTORE:
+		case DASTORE:
+		case FASTORE:
+		case LASTORE:
+		case SASTORE:
+		case CASTORE:
+		case BASTORE:
+		case INVOKEVIRTUAL:
+		case INVOKESPECIAL:
+		case INVOKEINTERFACE:
+		case INVOKESTATIC:
+			seenStateChange = true;
+			break;
+		}
+		stack.sawOpcode(this, seen);
 	}
 
 }
