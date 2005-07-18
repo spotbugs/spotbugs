09fd3d9f0ab408bb71252167d72e9233ca50aac7
David Hovemeyer <david.hovemeyer@gmail.com>
2005-07-18 19:13:12 +0000

    Initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4751 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/MissingClassException.java
@@ -0,0 +1,65 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2005, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.ba;
+
+/**
+ * DataflowAnalysisException variant to report a class lookup failure
+ * that caused dataflow analysis to abort.
+ * 
+ * @author David Hovemeyer
+ */
+public class MissingClassException extends DataflowAnalysisException {
+	private static final long serialVersionUID = 1L;
+	
+	private String className;
+
+	private MissingClassException(String className) {
+		super("Missing class" + (className != null ? ": " + className : ""));
+		this.className = className;
+	}
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param exception the ClassNotFoundException that caused this exception
+	 */
+	public MissingClassException(ClassNotFoundException exception) {
+		this(ClassNotFoundExceptionParser.getMissingClassName(exception));
+		initCause(exception);
+	}
+	
+	/**
+	 * Get the name of the missing class.
+	 * 
+	 * @return name of the missing class, or null if the missing class name is unknown
+	 */
+	public String getClassName() {
+		return className;
+	}
+
+	/**
+	 * Get the ClassNotFoundException that caused this exception.
+	 * 
+	 * @return the ClassNotFoundException that caused this exception
+	 */
+	public ClassNotFoundException getClassNotFoundException() {
+		return (ClassNotFoundException) getCause();
+	}
+}
