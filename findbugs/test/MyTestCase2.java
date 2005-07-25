99dad261cd6a91762783bf59c0ba18c3364940bb
Bill Pugh <bill.pugh@gmail.com>
2005-07-25 02:35:34 +0000

    Add a test case where it is important to call super in setUp and tearDown
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4777 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/test/MyTestCase2.java
@@ -0,0 +1,16 @@
+import junit.framework.Test;
+public class MyTestCase2 extends MyTestCase
+{
+	public void setUp()
+	{
+	}
+
+	public void tearDown()
+	{
+	}
+
+	public Test suite()
+	{
+		return null;
+	}
+}
