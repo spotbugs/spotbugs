999e230f18c09c758adf99ec9a12bbff1acd59de
Bill Pugh <bill.pugh@gmail.com>
2005-08-08 14:52:00 +0000

    import cleanup
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4806 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/InvalidJUnitTest.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/InvalidJUnitTest.java
@@ -30,7 +30,6 @@ import edu.umd.cs.findbugs.BugInstance;
 import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.BytecodeScanningDetector;
 import edu.umd.cs.findbugs.Lookup;
-import edu.umd.cs.findbugs.MethodAnnotation;
 import edu.umd.cs.findbugs.StatelessDetector;
 import edu.umd.cs.findbugs.ba.ClassContext;
 import edu.umd.cs.findbugs.visitclass.Constants2;
