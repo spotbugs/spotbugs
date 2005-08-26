9c748bd7a1595d3338408d2a30b65254ff9993ea
David Hovemeyer <david.hovemeyer@gmail.com>
2005-08-26 16:08:58 +0000

    "dist" target now automatically builds the plugin zipfile, using the plugin name and version to determine the filename. So, the dist target is now pretty much equivalent to a PDE export.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4945 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ eclipsePlugin/buildtools/de/tobject/findbugs/tools/PluginInfo.java
@@ -0,0 +1,36 @@
+package de.tobject.findbugs.tools;
+
+import java.io.File;
+
+import org.dom4j.Document;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+
+/**
+ * Extract information (plugin id, version, etc.)
+ * from the plugin's plugin.xml file,
+ * and print the information as java properties.
+ * This allows our Ant build.xml file to figure out
+ * what the deployable plugin zip file should be called.
+ */
+public class PluginInfo {
+	public static void main(String[] args) throws Exception {
+		if (args.length != 1) {
+			System.err.println("Usage: " + PluginInfo.class.getName() + " <plugin.xml file>");
+			System.exit(1);
+		}
+		
+		SAXReader reader = new SAXReader();
+		Document document = reader.read(new File(args[0]));
+		
+		emitProperty(document, "plugin.id", "/plugin/@id");
+		emitProperty(document, "plugin.version", "/plugin/@version");
+	}
+
+	private static void emitProperty(Document document, String propName, String xpath) {
+		Node node = document.selectSingleNode(xpath);
+		if (node == null)
+			throw new RuntimeException("No node found for xpath " + xpath);
+		System.out.println(propName + "=" + node.getText());
+	}
+}
