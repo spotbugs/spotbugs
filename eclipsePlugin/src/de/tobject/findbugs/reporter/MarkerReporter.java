24d3eed4ea95f14d2693b680e69984f8ff90f52e
Bill Pugh <bill.pugh@gmail.com>
2005-08-26 00:16:02 +0000

    Set marker priority based on bug priority
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@4938 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- eclipsePlugin/src/de/tobject/findbugs/reporter/MarkerReporter.java
+++ eclipsePlugin/src/de/tobject/findbugs/reporter/MarkerReporter.java
@@ -28,6 +28,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 
 import de.tobject.findbugs.marker.FindBugsMarker;
 import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.Detector;
 
 /**
  * Creates a FindBugs marker in a runnable window.
@@ -50,6 +51,11 @@ public class MarkerReporter implements IWorkspaceRunnable {
 		marker.setAttribute(FindBugsMarker.BUG_TYPE, bug.getType());
 		marker.setAttribute(IMarker.MESSAGE, bug.getMessage());
 		marker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_WARNING);
+		if (bug.getPriority() == Detector.HIGH_PRIORITY)
+			marker.setAttribute(IMarker.PRIORITY, IMarker.PRIORITY_HIGH);
+		else if (bug.getPriority() == Detector.NORMAL_PRIORITY)
+			marker.setAttribute(IMarker.PRIORITY, IMarker.PRIORITY_NORMAL);
+		else marker.setAttribute(IMarker.PRIORITY, IMarker.PRIORITY_LOW);
 		
 		// Set unique id of warning, so we can easily refer back
 		// to it later: for example, when the user classifies the warning.
