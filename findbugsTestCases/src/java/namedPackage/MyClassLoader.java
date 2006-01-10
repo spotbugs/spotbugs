a7d21fd36a7a830aff2eb7a015b71e547abf0f5e
Bill Pugh <bill.pugh@gmail.com>
2006-01-10 21:54:37 +0000

    Add testcase for classloaded
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5214 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/namedPackage/MyClassLoader.java
@@ -0,0 +1,8 @@
+package namedPackage;
+
+public class MyClassLoader extends ClassLoader {
+
+	static MyClassLoader instance = new MyClassLoader();
+	static StringBuilder sb = new StringBuilder();
+
+}
