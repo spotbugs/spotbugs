d178f312cd2f18eebda0f77f85e1552721f176f2
Bill Pugh <bill.pugh@gmail.com>
2006-02-01 18:23:03 +0000

    added additional test case added FIXME's to cases we don't currently get
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5317 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/BadRandomInt.java
+++ findbugsTestCases/src/java/BadRandomInt.java
@@ -4,7 +4,14 @@ class BadRandomInt {
 
 	static Random r = new Random();
 
+	// FIXME: We should recommend use of nextInt here instead
 	int nextInt(int n) {
 		return (int) (r.nextDouble() * n);
+	}
+	
+	// FIXME: We should generate a warning here about single-use random number generates
+	static int randomInt(int n) {
+		Random ran = new Random();  
+		return  ran.nextInt(n);
 	}
 }
