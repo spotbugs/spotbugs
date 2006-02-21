854e36cc667ed0199f8daa267cf867f8d1c6eda6
Bill Pugh <bill.pugh@gmail.com>
2006-02-21 21:33:57 +0000

    Check that type analysis understands that Throwable.initCause(Throwable) always returns the this object.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5360 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/UseInitCause.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
+
+public class UseInitCause {
+	
+	public String firstLine(BufferedReader r) {
+		try {
+			return r.readLine();
+		} catch (IOException e) {
+			throw (RuntimeException) new RuntimeException("IO Error").initCause(e);
+		}
+	}
+
+}
