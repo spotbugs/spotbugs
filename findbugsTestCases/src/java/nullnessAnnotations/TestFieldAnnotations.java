139e8da0ca4b8576f67de7e65b01cbb79655f36e
Bill Pugh <bill.pugh@gmail.com>
2006-02-21 21:32:58 +0000

    Test for nonnull annotations on fields
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5358 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/nullnessAnnotations/TestFieldAnnotations.java
@@ -0,0 +1,20 @@
+package nullnessAnnotations;
+
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+import edu.umd.cs.findbugs.annotations.NonNull;
+
+public class TestFieldAnnotations {
+	@NonNull Object x;
+	@CheckForNull Object y;
+	
+	void f() {
+		x = y;
+	}
+	void g() {
+		y = x;
+	}
+	void h() {
+		if (x == null) System.out.println("Huh?");
+	}
+
+}
