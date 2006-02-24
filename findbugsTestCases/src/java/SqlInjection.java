ad87318ae841c66c3ebe2533d06c65c6c67952fa
Bill Pugh <bill.pugh@gmail.com>
2006-02-24 19:26:02 +0000

    add test case where we are appending string constants and static fields (considered to be lower priority)
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5367 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/SqlInjection.java
+++ findbugsTestCases/src/java/SqlInjection.java
@@ -2,6 +2,7 @@ import java.sql.*;
 
 class SqlInjection {
 
+	static final String tableName = System.getProperty("XXX");
 	ResultSet f(Connection conn, String query) throws Exception {
 		Statement statement = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
 				ResultSet.CONCUR_READ_ONLY);
@@ -12,6 +13,11 @@ class SqlInjection {
 		Statement statement = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
 				ResultSet.CONCUR_READ_ONLY);
 		return statement.executeQuery("FOOBAR");
+	}
+	ResultSet g2(Connection conn) throws Exception {
+		Statement statement = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+				ResultSet.CONCUR_READ_ONLY);
+		return statement.executeQuery("FOOBAR where x = '" + tableName + "'");
 	}
 
 	ResultSet h(Connection conn, String name) throws Exception {
