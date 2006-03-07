4249076ac478f103ff88a51852ff12be3a1b0af3
Bill Pugh <bill.pugh@gmail.com>
2006-03-07 14:14:34 +0000

    first pass as detector for nonrecursive infinite loops; very, very primitive at this point, and disabled
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5380 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/InfiniteLoop.java
@@ -0,0 +1,127 @@
+/*
+ * FindBugs - Find bugs in Java programs
+ * Copyright (C) 2004,2005, Tom Truscott <trt@unx.sas.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.detect;
+
+import org.apache.bcel.classfile.Code;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.classfile.Method;
+
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.BytecodeScanningDetector;
+import edu.umd.cs.findbugs.OpcodeStack;
+import edu.umd.cs.findbugs.SourceLineAnnotation;
+
+public class InfiniteLoop extends BytecodeScanningDetector  {
+
+	private static final boolean active 
+		 = true || Boolean.getBoolean("findbugs.tg.active");
+	
+
+	BugReporter bugReporter;
+
+	OpcodeStack stack = new OpcodeStack();
+	public InfiniteLoop(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
+
+	int state = 0;
+	int age = 0;
+	int lastBranch;
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+
+	public void visit(JavaClass obj) {
+	}
+
+	public void visit(Method obj) {
+	}
+
+	public void visit(Code obj) {
+		// System.out.println(getFullyQualifiedMethodName());
+		// unless active, don't bother dismantling bytecode
+		if (active) {
+			for(int i = 0; i < lastUpdate.length; i++)
+				lastUpdate[i] = -1;
+			lastBranch = age = -1;
+			// System.out.println("TestingGround: " + getFullyQualifiedMethodName());
+                	stack.resetForMethodEntry(this);
+			super.visit(obj);
+		}
+	}
+
+	int lastUpdate[] = new int[256];
+
+	@Override
+	public void sawOffset(int offset) {
+		lastBranch  = getPC();
+	}
+	@Override
+	public void sawOpcode(int seen) {
+		stack.mergeJumps(this);
+		// System.out.println(getPC() + "\t" + OPCODE_NAMES[seen] + "\t" + state);
+		if (isRegisterStore()) lastUpdate[getRegisterOperand()] = getPC();
+		else {
+			switch(state) {
+			case 0:
+				if (isRegisterLoad()) {
+					state = 1;
+					age = lastUpdate[getRegisterOperand()];
+				}
+				break;
+			case 1: 
+				switch(seen) {
+				case ICONST_0:
+				case ICONST_1:
+				case ICONST_2:
+				case ICONST_3:
+				case ICONST_4:
+				case ICONST_5:
+				case ICONST_M1:
+				case BIPUSH:
+				case SIPUSH:
+					state = 2;
+				}
+				break;
+			case 2:
+				switch(seen) {
+				case IF_ICMPNE:
+				case IF_ICMPEQ:
+				case IF_ICMPGT:
+				case IF_ICMPLE:
+				case IF_ICMPLT:
+				case IF_ICMPGE:
+					if (getBranchOffset() < 0 && age <  getBranchTarget() && lastBranch < getBranchTarget()) {
+						BugInstance bug = new BugInstance(this, "IL_INFINITE_LOOP", HIGH_PRIORITY)
+				        .addClassAndMethod(this).addSourceLine(this,getPC());
+			
+						bugReporter.reportBug(bug);
+						
+					}
+				}
+				state = 0;
+					
+			}
+		}
+		
+		stack.sawOpcode(this,seen);
+	}
+}
