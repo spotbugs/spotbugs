f81bc4052f18db9ee1506fff0d6202cdcce4533b
Brian Cole <tuc@cs.umd.edu>
2006-03-09 21:33:20 +0000

    bug 1441505, but i am unable to reproduce
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5389 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/de/Pilhuhn.java
@@ -0,0 +1,23 @@
+package de;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public abstract class Pilhuhn {
+
+	public abstract void somethingDangerous() throws Exception;
+	
+	/** bug 1441505, but i am unable to reproduce */
+	void closem(FileInputStream fis, FileOutputStream fos) {
+		try { somethingDangerous(); }
+		catch( Exception exc ) {
+
+			if(fis!=null) try { fis.close(); } catch( Exception e2 ) {}
+
+			if(fos!=null) try { fos.close(); } catch( Exception e2 ) {} // DE_MIGHT_IGNORE
+
+			throw new RuntimeException(exc);
+		}
+	}
+
+}
