6f9b52cbd928d0f9cf08fc7f2863ac02abfcc3e3
Brian Cole <tuc@cs.umd.edu>
2006-03-20 23:03:28 +0000

    tries to better align columns by padding with spaces instead of using tabs, timestamps are formatted more tersely
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5412 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/workflow/MineBugHistory.java
+++ findbugs/src/java/edu/umd/cs/findbugs/workflow/MineBugHistory.java
@@ -21,6 +21,7 @@ package edu.umd.cs.findbugs.workflow;
 
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -53,6 +54,8 @@ public class MineBugHistory {
 	static final int ACTIVE_NOW = 6;
 	static final int TUPLE_SIZE = 7;
 	
+	static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd-HH:mm");
+
 	static class Version {
 		long sequence;
 		int tuple[] = new int[TUPLE_SIZE];
@@ -140,8 +143,8 @@ public class MineBugHistory {
 
 	
 	
-
-	public void dump(PrintStream out) {
+	/** This is how dump() was implemented up to and including version 0.9.5. */
+	public void dumpOriginal(PrintStream out) {
 		out.println("seq	version	time	classes	NCSS	added	newCode	fixed	removed	retained	dead	active");
 		for (int i = 0; i < versionList.length; ++i) {
 			Version version = versionList[i];
@@ -164,6 +167,75 @@ public class MineBugHistory {
 			for (int j = 0; j < TUPLE_SIZE; ++j) {
 				out.print('\t');
 				out.print(version.get(j));
+			}
+			out.println();
+		}
+	}
+
+	/** emit <code>width</code> space characters to <code>out</code> */
+	private static void pad(int width, PrintStream out) {
+		while (width-- > 0) out.print(' ');
+	}
+
+	/** equivalent to out.print(obj) except it may be padded on the left or right
+	 * @param width      padding will occur if the stringified oxj is shorter than this
+	 * @param alignRight true to pad on the left, false to pad on the right
+	 * @param out        the PrintStream printed to
+	 * @param obj        the value to print (may be an auto-boxed primitive) */
+	private static void print(int width, boolean alignRight, PrintStream out, Object obj) {
+		String s = String.valueOf(obj);
+		int padLen = width - s.length();
+		if (alignRight) pad(padLen, out);
+		out.print(s); // doesn't truncate if (s.length() > width)
+		if (!alignRight) pad(padLen, out);
+	}
+
+	/** This implementation of dump() tries to better align columns (when viewed
+	 *  with a fixed-width font) by padding with spaces instead of using tabs.
+	 *  Also, timestamps are formatted more tersely (-formatDates option).
+	 *  The bad news is that it requires a minimum of 112 columns.
+	 * @see dumpOriginal(PrintStream) */
+	public void dump(PrintStream out) {
+		//out.println("seq	version	time	classes	NCSS	added	newCode	fixed	removed	retained	dead	active");
+		print(3, true, out, "seq");
+		out.print(' ');
+		print(19, false, out, "version");
+		out.print(' ');
+		print(16, false, out, "time");
+		print(1+7, true, out, "classes");
+		print(1+7, true, out, "NCSS");
+		print(1+7, true, out, "added");
+		print(1+7, true, out, "newCode");
+		print(1+7, true, out, "fixed");
+		print(1+7, true, out, "removed");
+		print(1+8, true, out, "retained");
+		print(1+6, true, out, "dead");
+		print(1+7, true, out, "active");
+		out.println();
+		//note: if we were allowed to depend on JDK 1.5 we could use out.printf():
+		//Object line[] = { "seq", "version", "time", "classes", "NCSS", "added", "newCode", "fixed", "removed", "retained", "dead", "active" };
+		//out.printf("%3s %-19s %-16s %7s %7s %7s %7s %7s %7s %8s %6s %7s%n", line);
+		for (int i = 0; i < versionList.length; ++i) {
+			Version version = versionList[i];
+			AppVersion appVersion = sequenceToAppVersionMap.get(version.getSequence());
+			print(3, true, out, i); //out.print(i);
+			out.print(' '); // '\t'
+			print(19, false, out, appVersion != null ? appVersion.getReleaseName() : "");
+			out.print(' ');
+
+			long ts = (appVersion != null ? appVersion.getTimestamp(): 0L);
+			if (formatDates)
+				print(16, false, out, dateFormat.format(ts));
+			else print(16, false, out, ts);
+			out.print(' ');
+
+			print(7, true, out, appVersion != null ? appVersion.getNumClasses() : 0);
+			out.print(' ');
+			print(7, true, out, appVersion != null ? appVersion.getCodeSize() : 0);
+
+			for (int j = 0; j < TUPLE_SIZE; ++j) {
+				out.print(' ');
+				print(7, true, out, version.get(j));
 			}
 			out.println();
 		}
