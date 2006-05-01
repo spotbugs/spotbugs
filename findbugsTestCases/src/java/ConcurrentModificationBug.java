1a3b7827ebf687b7ff2c1a0a28c3ec893cff3631
Bill Pugh <bill.pugh@gmail.com>
2006-05-01 18:46:33 +0000

    Add test case for using remove on map while iterating through it
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5545 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/ConcurrentModificationBug.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+public class ConcurrentModificationBug  extends TestCase {
+
+	public void removeFoo(Map<String, Object> map) {
+
+		// FIXME: We should generate a warning here
+		for (String s : map.keySet())
+			if (s.startsWith("foo"))
+				map.remove(s);
+
+	}
+	
+	public void testRemoveFoo() {
+		HashMap<String,Object> s = new HashMap<String,Object>();
+		s.put("foo1", "foo1");
+		s.put("foo2", "foo1");
+		s.put("bar", "foo1");
+		removeFoo(s);
+		
+	}
+
+}
