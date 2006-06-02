ade87c4f7eb8b58d6c8b2394cf33fa84ad91f641
Bill Pugh <bill.pugh@gmail.com>
2006-06-02 14:27:49 +0000

    add more test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5654 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/BadRandomInt.java
+++ findbugsTestCases/src/java/BadRandomInt.java
@@ -9,6 +9,22 @@ class BadRandomInt {
 		return (int) (r.nextDouble() * n);
 	}
 	
+
+	int nextInt() {
+		return (int) (r.nextDouble() * 100);
+	}
+	
+
+	// FIXME: We should recommend use of nextInt here instead
+	int nextInt2(int n) {
+		return (int) (n * r.nextDouble());
+	}
+	
+
+	int nextInt2() {
+		return (int) (100 * r.nextDouble());
+	}
+	
 	// FIXME: We should generate a warning here about single-use random number generates
 	static int randomInt(int n) {
 		Random ran = new Random();  
