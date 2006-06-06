44a91316ecdc1f1024334d5753ca63767d8448ed
Reuven Lazarus <rlazarus@umd.edu>
2006-06-06 18:39:43 +0000

    CheckBoxSomething with more!
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5673 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- bluej/src/edu/umd/cs/findbugs/bluej/test/MenuBuilder.java
+++ bluej/src/edu/umd/cs/findbugs/bluej/test/MenuBuilder.java
@@ -1,5 +1,7 @@
 package edu.umd.cs.findbugs.bluej.test;
 import java.awt.event.ActionEvent;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.AbstractAction;
 import javax.swing.JMenuItem;
@@ -12,11 +14,14 @@ import bluej.extensions.BProject;
 import bluej.extensions.BlueJ;
 import bluej.extensions.ExtensionException;
 import bluej.extensions.MenuGenerator;
+import bluej.extensions.ProjectNotOpenException;
 
 public class MenuBuilder extends MenuGenerator
 {
 	private BlueJ bluej;
 
+	private Map<BProject, Boolean> isRunning = new HashMap<BProject, Boolean>();
+	
 	public MenuBuilder(BlueJ bluej)
 	{
 		this.bluej = bluej;
@@ -28,12 +33,58 @@ public class MenuBuilder extends MenuGenerator
 	@Override
 	public JMenuItem getToolsMenuItem(BPackage pckg)
 	{
-		JCheckBoxMenuItem result = new JCheckBoxMenuItem("CheckBox Something");
-		//JCheckBoxMenuItem result = new JCheckBoxMenuItem(new MenuAction());
-		//result.setText("CheckBox Something");
-		return result;
+		//JCheckBoxMenuItem result = new JCheckBoxMenuItem("CheckBox Something");
+		try
+		{
+			JCheckBoxMenuItem result = new JCheckBoxMenuItem(new CheckBoxMenuAction(pckg.getProject()));
+			result.setText("CheckBox Something");
+			return result;
+		}
+		catch (ProjectNotOpenException e)
+		{
+			Log.recordBug(e);
+			return null;
+		}
 	}
 
+	/**
+	 * Called when the menu is about to be displayed.  If and only if we're enabled on
+	 * this project, make sure the menu item is checked.  (If we've never seen this project
+	 * before, start it off checked.) 
+	 */
+	@Override
+	public void notifyPostToolsMenu(BPackage pckg, JMenuItem menu)
+	{
+		try
+		{
+			BProject project = pckg.getProject();
+			if (!isRunning.containsKey(project))
+				isRunning.put(project, true);
+			menu.setSelected(isRunning.get(project));
+		}
+		catch (ProjectNotOpenException notGonnaHappen)
+		{
+			JOptionPane.showMessageDialog(null, "Oh crap!");
+			return;
+		}
+	}
+	
+	@SuppressWarnings("serial")
+	class CheckBoxMenuAction extends AbstractAction
+	{
+		private BProject project;
+		
+		public CheckBoxMenuAction(BProject project)
+		{
+			this.project = project;
+		}
+		
+		public void actionPerformed(ActionEvent evt)
+		{
+			isRunning.put(project, ((JCheckBoxMenuItem)evt.getSource()).isSelected());
+		}
+	}
+	
 	@SuppressWarnings("serial")
 	class MenuAction extends AbstractAction
 	{
