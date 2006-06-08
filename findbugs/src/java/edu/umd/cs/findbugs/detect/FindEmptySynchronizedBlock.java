3695590cf13d40765afc517c4e3ae95532ef83ca
Bill Pugh <bill.pugh@gmail.com>
2006-06-08 19:31:22 +0000

    catch empty sync blocks the way eclipse generates the bytecode
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5709 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/FindEmptySynchronizedBlock.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/FindEmptySynchronizedBlock.java
@@ -37,7 +37,7 @@ public class FindEmptySynchronizedBlock
 
 	@Override
          public void sawOpcode(int seen) {
-		if (seen == MONITOREXIT && getPrevOpcode(2) == MONITORENTER)
+		if (seen == MONITOREXIT && (getPrevOpcode(2) == MONITORENTER || getPrevOpcode(1) == MONITORENTER) )
 		  bugReporter.reportBug(new BugInstance(this, "ESync_EMPTY_SYNC", NORMAL_PRIORITY)
                                 .addClassAndMethod(this)
                                 .addSourceLine(this));
