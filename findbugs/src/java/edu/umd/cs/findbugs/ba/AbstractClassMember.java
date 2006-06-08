e07add87f91ad101f3ba971456e9a78144e9e196
Bill Pugh <bill.pugh@gmail.com>
2006-06-08 19:18:28 +0000

    add assertion that className and signature are dotted rather than slashed
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5705 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/ba/AbstractClassMember.java
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/AbstractClassMember.java
@@ -28,6 +28,8 @@ public abstract class AbstractClassMember implements ClassMember {
 	private int cachedHashCode = 0;
 
 	protected AbstractClassMember(String className, String name, String signature, int accessFlags) {
+		assert className.indexOf('/') == -1;
+		assert signature.indexOf('/') == -1;
 		this.className = className;
 		this.name = name;
 		this.signature = signature;
