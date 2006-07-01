995e6a18cd5f247568cc213e8d67ad5f0355dc49
Bill Pugh <bill.pugh@gmail.com>
2006-07-01 22:49:14 +0000

    Add false positive test case for questionable boolean assignment; handle idiom where assignment used in assert statement
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5789 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/QuestionableBooleanAssignmentInAssertStatement.java
@@ -0,0 +1,9 @@
+
+public class QuestionableBooleanAssignmentInAssertStatement {
+
+	public static void main(String args[]) {
+		boolean debug = false;
+		assert debug = true;
+		System.out.println(debug);
+	}
+}
