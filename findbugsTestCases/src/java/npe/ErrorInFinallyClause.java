6d8d9a454e03abb2e03e431dc2ed61d90dd57c40
Bill Pugh <bill.pugh@gmail.com>
2006-07-14 21:45:46 +0000

    We should report a medium priority correctness warning here on the close of out.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5888 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/npe/ErrorInFinallyClause.java
@@ -0,0 +1,30 @@
+package npe;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class ErrorInFinallyClause {
+	public static void copy(String from, String to) throws Exception {
+		InputStream in = null;
+		OutputStream out = null;
+		try {
+			in = new FileInputStream(from);
+			out = new FileOutputStream(to);
+			byte [] buf = new byte[1024];
+
+			while (true) {
+				int sz = in.read(buf);
+				if (sz < 0) break;
+				out.write(buf,0,sz);
+			}
+			
+		} finally {
+			in.close();
+			out.close(); // TODO: we should report a medium priority NP warning here
+			
+		}
+	}
+
+}
