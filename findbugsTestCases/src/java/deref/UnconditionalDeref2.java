3a3a7703b2f0e6552541a83ffc8ad93d2ff756d9
Bill Pugh <bill.pugh@gmail.com>
2006-07-20 02:38:51 +0000

    test cases for unconditional dereference
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5932 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/deref/UnconditionalDeref2.java
@@ -0,0 +1,37 @@
+package deref;
+
+public class UnconditionalDeref2 {
+
+	private void f(Object x, Object y, Object z) {
+		System.out.println(y.hashCode());
+	}
+
+	private void g(Object x, Object y, Object z) {
+		System.out.println(y.hashCode());
+	}
+
+	void report() {
+		f(this, null, this);
+	}
+
+	void doNotReport() {
+		f(null, this, this);
+	}
+
+	void doNotReport2() {
+		f(this, this, null);
+	}
+
+	void reportStatic() {
+		g(this, null, this);
+	}
+
+	void doNotReportStatic() {
+		g(null, this, this);
+	}
+
+	void doNotReport2Static() {
+		g(this, this, null);
+	}
+
+}
