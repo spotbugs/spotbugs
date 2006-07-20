5426e9da6235b824d02aa7d7b1853fd604a723b4
Bill Pugh <bill.pugh@gmail.com>
2006-07-20 13:52:17 +0000

    add ICAST_INT_CAST_TO_FLOAT_PASSED_TO_ROUND
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5938 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/IDivResultCastToDouble.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/IDivResultCastToDouble.java
@@ -14,32 +14,50 @@ public class IDivResultCastToDouble extends BytecodeScanningDetector {
 		this.bugReporter = bugReporter;
 	}
 	
-	//@Override
+
 	@Override
          public void visit(Method obj) {
 		if (DEBUG) System.out.println("Visiting " + obj);
 	}
 
+	BugInstance pendingBug = null;
 	@Override
          public void sawOpcode(int seen) {
 		
-		if (DEBUG) System.out.println("Saw opcode " + seen);
+		if (DEBUG) System.out.println("Saw opcode " + OPCODE_NAMES[seen] + " " + pendingBug);
 	
 		if ((prevOpCode  == I2D || prevOpCode == L2D)
                         && seen == INVOKESTATIC
                                 && getClassConstantOperand().equals("java/lang/Math")
-                                && getNameConstantOperand().equals("ceil"))
+                                && getNameConstantOperand().equals("ceil")) {
 			bugReporter.reportBug(new BugInstance(this, 
 				"ICAST_INT_CAST_TO_DOUBLE_PASSED_TO_CEIL", 
 				HIGH_PRIORITY)
 					.addClassAndMethod(this)
 					.addSourceLine(this));
-
+			pendingBug = null;
+		}
+		else if ((prevOpCode  == I2F || prevOpCode == L2F)
+                && seen == INVOKESTATIC
+                        && getClassConstantOperand().equals("java/lang/Math")
+                        && getNameConstantOperand().equals("round")) {
+			bugReporter.reportBug(new BugInstance(this, 
+					"ICAST_INT_CAST_TO_FLOAT_PASSED_TO_ROUND", 
+					HIGH_PRIORITY)
+			.addClassAndMethod(this)
+			.addSourceLine(this));
+			pendingBug = null;
+		}
+		else if (pendingBug != null) {
+			bugReporter.reportBug(pendingBug);
+			pendingBug = null;
+		}
+		
 		if (prevOpCode  == IDIV && (seen == I2D|| seen == I2F)
 			|| prevOpCode  == LDIV && (seen == L2D || seen==L2F))
-			bugReporter.reportBug(new BugInstance(this, "ICAST_IDIV_CAST_TO_DOUBLE", NORMAL_PRIORITY)
+			pendingBug = new BugInstance(this, "ICAST_IDIV_CAST_TO_DOUBLE", NORMAL_PRIORITY)
 					.addClassAndMethod(this)
-					.addSourceLine(this));
+					.addSourceLine(this);
 
 		prevOpCode = seen;
 		}
