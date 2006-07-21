4a9eafdd22e3e697093df57265e98e12234927f4
Brian Cole <tuc@cs.umd.edu>
2006-07-21 09:10:06 +0000

    fix many serious flaws with duplicate switch case analysis
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5955 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/DuplicateBranches.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/DuplicateBranches.java
@@ -30,6 +30,9 @@ import org.apache.bcel.classfile.Method;
 import org.apache.bcel.generic.*;
 import org.apache.bcel.util.ByteSequence;
 
+/** @author Dave Brousius  4/2005  original author
+ *  @author Brian Cole     8/2006  serious reworking
+ */
 public class DuplicateBranches extends PreorderVisitor implements Detector
 {
 	private ClassContext classContext;
@@ -120,40 +123,76 @@ public class DuplicateBranches extends PreorderVisitor implements Detector
 						elseBB.getLastInstruction().getPosition()));		
 	}
 	
-	private void findSwitchDuplicates(CFG cfg, Method method, BasicBlock bb) {		
+	/** Like bb.getFirstInstruction() except that if null is
+	 *  returned it will follow the FALL_THROUGH_EDGE (if any) */
+	private static InstructionHandle getDeepFirstInstruction(CFG cfg, BasicBlock bb) {
+		InstructionHandle ih = bb.getFirstInstruction();
+		if (ih != null) return ih;
 		Iterator<Edge> iei = cfg.outgoingEdgeIterator(bb);
+		while (iei.hasNext()) {
+			Edge e = iei.next();
+		    String edgeString = e.toString();
+			if (EdgeTypes.FALL_THROUGH_EDGE == e.getType())
+				return getDeepFirstInstruction(cfg, e.getTarget());
+		}
+		return null;
+	}
+
+	private void findSwitchDuplicates(CFG cfg, Method method, BasicBlock bb) {		
 				
 		int[] switchPos = new int[cfg.getNumOutgoingEdges(bb)+1];
+		HashMap<Integer, InstructionHandle> prevHandle = new HashMap<Integer, InstructionHandle>();
+
+		Iterator<Edge> iei = cfg.outgoingEdgeIterator(bb);
 		int idx = 0;
 		
 		while (iei.hasNext()) {
 			Edge e = iei.next();
-			if (EdgeTypes.SWITCH_EDGE == e.getType()) {
+			int eType = e.getType();
+			if (eType == EdgeTypes.SWITCH_EDGE || eType == EdgeTypes.SWITCH_DEFAULT_EDGE) {
 				BasicBlock target = e.getTarget();
-				InstructionHandle firstIns = target.getFirstInstruction();
+				InstructionHandle firstIns = getDeepFirstInstruction(cfg, target);
 				if (firstIns == null)
-					return;
-				switchPos[idx++] = firstIns.getPosition();
-			} else if (EdgeTypes.SWITCH_DEFAULT_EDGE == e.getType()) {
-				BasicBlock target = e.getTarget();
-				InstructionHandle firstIns = target.getFirstInstruction();
-				if (firstIns == null)
-					return;
-				switchPos[idx++] = firstIns.getPosition();
-			} else
+					continue; // give up on this edge
+				int firstInsPosition = firstIns.getPosition();
+				switchPos[idx++] = firstInsPosition;
+				InstructionHandle prevIns = firstIns.getPrev(); // prev in bytecode, not flow
+				if (prevIns != null) prevHandle.put((Integer)firstInsPosition, prevIns);
+			} else {
+				// hmm, this must not be a switch statement, so give up
 				return;
+			}
 		}
 		
-		Arrays.sort(switchPos);
-		
-		if (switchPos.length < 2)
+		if (idx < 2) // need at least two edges to tango
 			return;
 						
+		Arrays.sort(switchPos, 0, idx); // sort the 'idx' switch positions
+		
+		// compute end position of final case (ok if set to 0 or <= switchPos[idx-1])
+		switchPos[idx] = getFinalTarget(cfg, switchPos[idx-1], prevHandle.values());
+		
 		HashMap<BigInteger, Collection<Integer>> map = new HashMap<BigInteger,Collection<Integer>>();
-		for (int i = 0; i < switchPos.length-1; i++) {
+		for (int i = 0; i < idx; i++) {
 			if (switchPos[i] +1 >=  switchPos[i+1]) continue;
-
-			byte[] clause = getCodeBytes(method, switchPos[i], switchPos[i+1]);
+			
+			int endPos = switchPos[i+1];
+			InstructionHandle last = prevHandle.get((Integer)switchPos[i+1]);
+			if (last == null) {
+				// should be default case -- leave endPos as is
+			} else if (last.getInstruction() instanceof GotoInstruction) {
+				endPos = last.getPosition(); // don't store the goto
+			} else if (last.getInstruction() instanceof ReturnInstruction) {
+				// leave endPos as is (store the return instruction)
+		//	} else if (last.getInstruction() instanceof ATHROW) {
+		//		// leave endPos as is (store the throw instruction)
+		// Don't do this since many cases may throw "not implemented".
+			} else {
+				if (i+2 < idx) continue; // falls through to next case, so don't store it at all
+				if (i+1 < idx && switchPos[idx]!=switchPos[idx-1]) continue; // also falls through unless switch has no default case
+			}
+			
+			byte[] clause = getCodeBytes(method, switchPos[i], endPos); //switchPos[i+1]);
 			
 			BigInteger clauseAsInt = new BigInteger(clause);
 			
@@ -177,6 +216,56 @@ public class DuplicateBranches extends PreorderVisitor implements Detector
 				bugReporter.reportBug(bug);
 			}
 		}
+	}
+	
+	/** determine the end position (exclusive) of the final case
+	 *  by looking at the gotos at the ends of the other cases */
+	private static int getFinalTarget(CFG cfg, int myPos, Collection<InstructionHandle> prevs) {
+		int maxGoto = 0;
+		BasicBlock myBB = null;
+		// note: InstructionHandle doesn't override equals(), so use prevs.contains() with caution.
+		Iterator<BasicBlock> bbi = cfg.blockIterator();
+		while (bbi.hasNext()) {
+			BasicBlock bb = bbi.next();
+			InstructionHandle last = bb.getLastInstruction(); // may be null
+			if (prevs.contains(last)) { // danger will robinson
+				Iterator<Edge> iei = cfg.outgoingEdgeIterator(bb);
+				while (iei.hasNext()) {
+					Edge e = iei.next();
+					int eType = e.getType();
+					String aab = e.toString();
+					if (eType == EdgeTypes.GOTO_EDGE) {
+						BasicBlock target = e.getTarget();
+						InstructionHandle targetFirst = getDeepFirstInstruction(cfg, target);
+						if (targetFirst != null) {
+							int targetPos = targetFirst.getPosition();
+							if (targetPos > maxGoto) maxGoto = targetPos;
+						}	
+					}
+				}
+			} else if (last!=null && myPos==bb.getFirstInstruction().getPosition()) {
+				// note: getFirstInstruction() may return null, but won't if last!=null.
+				myBB = bb; // used in case (c) below
+			}
+		}
+		/* ok, there are three cases:
+		 * a) maxGoto == myPos: There is no default case within the switch.
+		 * b) maxGoto > myPos: maxGoto is the end (exclusive) of the default case
+		 * c) maxGoto < myPos: all the cases do something like return or throw, so
+		 *                     who knows if there is a default case (and it's length)? */
+		if (maxGoto < myPos && myBB != null) {
+			/* We're in case (c), so let's guess that the end of the basic block
+			 * is the end of the default case (if it exists). This may give false
+			 * negatives (if the default case has branches, for example) but
+			 * shouldn't give false negatives (because if it matches one of the
+			 * cases, then it has also matched that case's return/throw). */
+			InstructionHandle last = myBB.getLastInstruction();
+			if (last != null) {
+				// note: last.getNext() may return null, so do it this way
+				return last.getPosition() + last.getInstruction().getLength();
+			}
+		}
+		return maxGoto;
 	}
 	
 	private byte[] getCodeBytes(Method m, int start, int end) {
