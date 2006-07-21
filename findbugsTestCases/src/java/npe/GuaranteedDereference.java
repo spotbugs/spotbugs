3dce4ad4f440f1f5d8131c8288079ba15a14a767
Bill Pugh <bill.pugh@gmail.com>
2006-07-21 20:31:08 +0000

    more test case tweaks
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5962 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/npe/GuaranteedDereference.java
+++ findbugsTestCases/src/java/npe/GuaranteedDereference.java
@@ -24,19 +24,13 @@ public class GuaranteedDereference {
 		String xAsString = null;
 		if (x instanceof String) 
 		  xAsString = (String) x;
-		
 		result.append(x.hashCode());
-		
 		if (xAsString != null)
 			value = xAsString.toLowerCase();
-		
-		
 		if (value == null) 
 			result.append(value);
 		else result.append("foo");
-		
 		result.append(" bar ");
-		
 		result.append(xAsString.trim());
 		return result.toString();
 	}
@@ -55,10 +49,15 @@ public class GuaranteedDereference {
 		return x.hashCode();
 	}
 	
-	int test6Report(Object x) {
+	int test6aReport(Object x) {
 		Object y = null;
 		if (x == null) throw new NullPointerException();
 		return y.hashCode();
+	}
+	int test6bReport(Object x) {
+		Object y = null;
+		if (x == null) throw new NullPointerException();
+		else return y.hashCode();
 	}
 	
 	int test7Report(Object x) {
@@ -96,13 +95,39 @@ public class GuaranteedDereference {
 		else result = 2;
 		// At this point y is null on a complex path
 		
-		// at this point, regardless of whether assertTrue is a thrower
-		// or not, we are guaranteed to dereference y
+		// at this point, we will either dereference y or
+		// terminate abnormally
 		if (x == null) throw new IllegalArgumentException("x should not be null");
 		result += y.hashCode();
 		return result;
 	}
 	
+	int test10IDontKnow(Object x, boolean b1, boolean b2, boolean b3) {
+		int result = 0;
+		Object y = null;
+		if (b1) y = new Object();
+		// At this point y is null on a simple path
+		// but guaranteed to be dereferenced
+		if (b2) result = 1;
+		else result = 2;
+		// At this point y is null on a complex path
+		
+		assertTrue(b1);
+		result += y.hashCode();
+		return result;
+	}
+
+	int test11Report(Object x, boolean b1, boolean b2) {
+		if (x == null)
+				System.out.println("x is null");
+		if (b1)
+			System.out.println("b is true");
+		if (b2) throw new IllegalArgumentException("b2 must be false");
+		
+		return x.hashCode();
+	}
+	
+
 	
 
 }
