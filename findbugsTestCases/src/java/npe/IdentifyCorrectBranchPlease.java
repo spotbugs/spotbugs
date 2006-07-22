95e138accd04ac5bbce0123612e0082cc6518326
Bill Pugh <bill.pugh@gmail.com>
2006-07-22 17:20:51 +0000

    Test cases for whether we can correctly identify the path leading to the NPE
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5978 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/npe/IdentifyCorrectBranchPlease.java
@@ -0,0 +1,26 @@
+package npe;
+
+public class IdentifyCorrectBranchPlease {
+	
+	int f(boolean b) {
+		Object x = null;
+		if (b) {
+			x = new Object();
+		} else {
+			b = true;
+		}
+		return x.hashCode();
+	}
+	int f(boolean b1, boolean b2) {
+		Object x = null;
+		if (b1) {
+			x = new Object();
+		} else {
+			b1 = true;
+		}
+		if (b2) 
+			System.out.println("b2 is true");
+		return x.hashCode();
+	}
+
+}
