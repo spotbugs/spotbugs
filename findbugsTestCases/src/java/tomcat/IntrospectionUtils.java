d2f8ff32747a6ce6a2456b066b5f437f90c6f9db
Bill Pugh <bill.pugh@gmail.com>
2006-07-22 17:11:24 +0000

    Current guaranteed null pointer analysis isn't getting this test case from the Reasoning tomcat report
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5976 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/tomcat/IntrospectionUtils.java
@@ -0,0 +1,36 @@
+package tomcat;
+
+import java.lang.reflect.Method;
+
+public class IntrospectionUtils {
+	public static Object callMethod1(Object target, String methodN,
+			Object param1, String typeParam1, ClassLoader cl) throws Exception {
+		if (target == null || param1 == null)
+			d("Assert: Illegal params " + target + " " + param1);
+		Class params[] = new Class[1];
+		if (typeParam1 == null)
+			params[0] = param1.getClass();
+		else
+			params[0] = cl.loadClass(typeParam1);
+		// TODO: We should be reporting a NP error with a possible dereference
+		// in the following line
+		Method m = findMethod(target.getClass(), methodN, params);
+		if (m == null)
+			throw new NoSuchMethodException(target.getClass().getName() + " "
+					+ methodN);
+		else
+			return m.invoke(target, new Object[] { param1 });
+	}
+
+	static void d(String s) {
+		System.out.println("IntrospectionUtils: " + s);
+	}
+
+	public static Method findMethod(Class c, String name, Class params[])
+			throws SecurityException, NoSuchMethodException {
+		// STUB Method; what happens here isn't relevant
+		return IntrospectionUtils.class.getMethod("d",
+				new Class[] { String.class });
+	}
+
+}
