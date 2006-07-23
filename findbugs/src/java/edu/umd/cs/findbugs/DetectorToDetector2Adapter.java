fef7189375523167664909e8f876860afbb230a3
David Hovemeyer <david.hovemeyer@gmail.com>
2006-07-23 15:55:10 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5990 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/DetectorToDetector2Adapter.java
@@ -0,0 +1,67 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2006, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs;
+
+import edu.umd.cs.findbugs.ba.ClassContext;
+import edu.umd.cs.findbugs.classfile.CheckedAnalysisException;
+import edu.umd.cs.findbugs.classfile.ClassDescriptor;
+import edu.umd.cs.findbugs.classfile.Global;
+import edu.umd.cs.findbugs.classfile.IAnalysisCache;
+
+/**
+ * An adapter allowing classes implementing the Detector interface
+ * to support the new Detector2 interface.
+ * 
+ * @author David Hovemeyer
+ */
+public class DetectorToDetector2Adapter implements Detector2 {
+	private Detector detector;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param detector the Detector we want to adapt
+	 */
+	public DetectorToDetector2Adapter(Detector detector) {
+		this.detector = detector;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.umd.cs.findbugs.Detector2#finishPass()
+	 */
+	public void finishPass() {
+		detector.report();
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.umd.cs.findbugs.Detector2#visitClass(edu.umd.cs.findbugs.classfile.ClassDescriptor)
+	 */
+	public void visitClass(ClassDescriptor classDescriptor)
+			throws CheckedAnalysisException {
+		
+		// Just get the ClassContext from the analysis cache
+		// and apply the detector to it.
+		
+		IAnalysisCache analysisCache = Global.getAnalysisCache();
+		ClassContext classContext = analysisCache.getClassAnalysis(ClassContext.class, classDescriptor);
+		detector.visitClassContext(classContext);
+	}
+
+}
---
+++ findbugs/src/java/edu/umd/cs/findbugs/classfile/Global.java
@@ -0,0 +1,52 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2006, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.classfile;
+
+/**
+ * Static methods for accessing objects that are global
+ * to an analysis session.  Hopefully, this will be
+ * limited to the analysis cache.
+ * 
+ * @author David Hovemeyer
+ */
+public class Global {
+	private static final InheritableThreadLocal<IAnalysisCache> analysisCacheThreadLocal =
+		new InheritableThreadLocal<IAnalysisCache>();
+	
+	/**
+	 * Set the analysis cache for the current thread.
+	 * This should be called before any detectors or analyses that
+	 * need the cache are used.
+	 * 
+	 * @param analysisCache the analysis cache to set for the current thread
+	 */
+	public static void setAnalysisCacheForCurrentThread(IAnalysisCache analysisCache) {
+		analysisCacheThreadLocal.set(analysisCache);
+	}
+
+	/**
+	 * Get the analysis cache for the current thread.
+	 * 
+	 * @return the analysis cache for the current thread
+	 */
+	public static IAnalysisCache getAnalysisCache() {
+		return analysisCacheThreadLocal.get();
+	}
+}
