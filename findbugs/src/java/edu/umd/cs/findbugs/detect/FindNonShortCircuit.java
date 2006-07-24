564a05c7bbfd31f95362313b888742b389a31512
Bill Pugh <bill.pugh@gmail.com>
2006-07-24 17:15:28 +0000

    Reorganize, improve prioritization
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@5999 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/FindNonShortCircuit.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/FindNonShortCircuit.java
@@ -19,74 +19,69 @@
 
 package edu.umd.cs.findbugs.detect;
 
-
 import edu.umd.cs.findbugs.*;
 import org.apache.bcel.classfile.Method;
 
-public class FindNonShortCircuit extends BytecodeScanningDetector implements  StatelessDetector {
+public class FindNonShortCircuit extends BytecodeScanningDetector implements
+		StatelessDetector {
 
 	int stage1 = 0;
 	int stage2 = 0;
 	int distance = 0;
-	//int distance2 = 0;
 	int operator;
-	boolean sawDereference = false;
-	boolean sawInvocation = false;
+
+	boolean sawDanger = false;
+	boolean sawDangerOld = false;
+
 	private BugReporter bugReporter;
 
 	public FindNonShortCircuit(BugReporter bugReporter) {
 		this.bugReporter = bugReporter;
 	}
 
-
-
 	@Override
-         public void visit(Method obj) {
+	public void visit(Method obj) {
 		stage1 = 0;
 		stage2 = 0;
 		distance = 1000000;
-		//distance2 = 1000000;
+		sawDanger = sawDangerOld = true;
 	}
 
 	@Override
-         public void sawOpcode(int seen) {
-		/* prototype for short-circuit bug */
+	public void sawOpcode(int seen) {
 		distance++;
+		scanForBooleanValue(seen);
+		scanForDanger(seen);
+		scanForShortCircuit(seen);
+	}
+
+	private void scanForDanger(int seen) {
 		switch (seen) {
-		case IFNONNULL:
-		case IFNULL:
-			stage1 = 3;
-			break;
-		case ICONST_1:
-			if (stage1 != 3)
-				stage1 = 1;
-			else stage1 = 0;
-			break;
-		case GOTO:
-			if (stage1 == 1)
-				stage1 = 2;
-			else
-				stage1 = 0;
-			break;
-		case ICONST_0:
-			if (stage1 == 2) {
-				distance = 0;
-				// System.out.println("saw 1; goto X; 0");
-			}
-			stage1 = 0;
-			break;
 		case INVOKEINTERFACE:
-		case INVOKEVIRTUAL:
 		case INVOKESPECIAL:
+		case INVOKEVIRTUAL:
 		case INVOKESTATIC:
-			String sig = getSigConstantOperand();
-			if (sig.endsWith(")Z"))
-					distance = 0;
-			stage1 = 0;
+		case ARRAYLENGTH:
+		case AALOAD:
+		case BALOAD:
+		case SALOAD:
+		case CALOAD:
+		case IALOAD:
+		case LALOAD:
+		case FALOAD:
+		case DALOAD:
+		case IDIV:
+		case IREM:
+		case GETFIELD:
+			sawDanger = true;
 			break;
 		default:
-			stage1 = 0;
+			break;
 		}
+
+	}
+
+	private void scanForShortCircuit(int seen) {
 		switch (seen) {
 		case IAND:
 		case IOR:
@@ -94,7 +89,6 @@ public class FindNonShortCircuit extends BytecodeScanningDetector implements  St
 
 			if (distance < 4) {
 				operator = seen;
-				//distance2 = distance;
 				stage2 = 1;
 			} else
 				stage2 = 0;
@@ -103,10 +97,7 @@ public class FindNonShortCircuit extends BytecodeScanningDetector implements  St
 		case IFNE:
 			if (stage2 == 1) {
 				// System.out.println("Found nsc");
-				bugReporter.reportBug(new BugInstance(this, "NS_NON_SHORT_CIRCUIT",
-				        NORMAL_PRIORITY)
-				        .addClassAndMethod(this)
-				        .addSourceLine(this, getPC()));
+				reportBug();
 			}
 			stage2 = 0;
 			break;
@@ -114,11 +105,7 @@ public class FindNonShortCircuit extends BytecodeScanningDetector implements  St
 		case PUTSTATIC:
 		case IRETURN:
 			if (operator == IAND && stage2 == 1) {
-				// System.out.println("Found nsc");
-				bugReporter.reportBug(new BugInstance(this, "NS_NON_SHORT_CIRCUIT",
-				        NORMAL_PRIORITY)
-				        .addClassAndMethod(this)
-				        .addSourceLine(this, getPC()));
+				reportBug();
 			}
 			stage2 = 0;
 			break;
@@ -126,7 +113,50 @@ public class FindNonShortCircuit extends BytecodeScanningDetector implements  St
 			stage2 = 0;
 			break;
 		}
+	}
 
+	private void reportBug() {
+		bugReporter.reportBug(new BugInstance(this, "NS_NON_SHORT_CIRCUIT",
+				sawDangerOld ? HIGH_PRIORITY : NORMAL_PRIORITY)
+				.addClassAndMethod(this).addSourceLine(this, getPC()));
+	}
+
+	private void scanForBooleanValue(int seen) {
+		switch (seen) {
+
+		case ICONST_1:
+			stage1 = 1;
+			break;
+		case GOTO:
+			if (stage1 == 1)
+				stage1 = 2;
+			else
+				stage1 = 0;
+			break;
+		case ICONST_0:
+			if (stage1 == 2) 
+				sawBooleanValue();
+			stage1 = 0;
+			break;
+		case INVOKEINTERFACE:
+		case INVOKEVIRTUAL:
+		case INVOKESPECIAL:
+		case INVOKESTATIC:
+			String sig = getSigConstantOperand();
+			if (sig.endsWith(")Z"))
+				sawBooleanValue();
+			stage1 = 0;
+			break;
+		default:
+			stage1 = 0;
+		}
+	}
+
+	private void sawBooleanValue() {
+		sawDangerOld = sawDanger;
+		sawDanger = false;
+		distance = 0;
+		stage1 = 0;
 
 	}
 }
