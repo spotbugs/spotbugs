be3bc2aa1a51790fcffbe2c2fb9dd8c9e6b13e8f
David Hovemeyer <david.hovemeyer@gmail.com>
2006-07-25 20:24:08 +0000

    Added another test case using a static field.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6029 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/npe/TrackFields.java
+++ findbugsTestCases/src/java/npe/TrackFields.java
@@ -24,4 +24,18 @@ class TrackFields {
         	x = new Object();
         return x.hashCode();
         }
+    
+    static Object y;
+    
+    static void setY(Object obj) {
+    	y = obj;
+    }
+    
+    int f3() {
+    	// Like f, but with a static field
+    	if (y == null) {
+    		return y.hashCode();
+    	}
+    	return 42;
+    }
 }
