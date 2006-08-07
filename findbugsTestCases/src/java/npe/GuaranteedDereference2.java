4be7242f99f2c60990fb26035546924ffdf2bb88
Bill Pugh <bill.pugh@gmail.com>
2006-08-07 04:55:51 +0000

    test case giving us some problems with the guaranteed null pointer dereference analysis
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6205 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/npe/GuaranteedDereference2.java
@@ -0,0 +1,22 @@
+package npe;
+
+import java.io.IOException;
+
+public class GuaranteedDereference2 {
+	static void f() throws IOException {
+	}
+
+	static int g(int y) {
+
+		Object x = null;
+		try {
+			f();
+			x = new Object();
+			int tmp = x.hashCode();
+		} catch (IOException e) {
+			// ignore it
+		}
+		return x.hashCode();
+	}
+
+}
