7bf1bea340de7ca96d44235e1004331c1ec61cae
David Hovemeyer <david.hovemeyer@gmail.com>
2006-08-09 00:03:56 +0000

    Initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6234 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/classfile/engine/asm/ClassNodeAnalysisEngine.java
@@ -0,0 +1,62 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2006, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.classfile.engine.asm;
+
+import edu.umd.cs.findbugs.classfile.CheckedAnalysisException;
+import edu.umd.cs.findbugs.classfile.ClassDescriptor;
+import edu.umd.cs.findbugs.classfile.IAnalysisCache;
+import edu.umd.cs.findbugs.classfile.IClassAnalysisEngine;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.tree.ClassNode;
+
+/**
+ * Analysis engine to produce the ClassNode (ASM tree format)
+ * for a class.
+ * 
+ * @author David Hovemeyer
+ */
+public class ClassNodeAnalysisEngine implements IClassAnalysisEngine {
+
+	/* (non-Javadoc)
+	 * @see edu.umd.cs.findbugs.classfile.IAnalysisEngine#analyze(edu.umd.cs.findbugs.classfile.IAnalysisCache, java.lang.Object)
+	 */
+	public Object analyze(IAnalysisCache analysisCache, ClassDescriptor descriptor) throws CheckedAnalysisException {
+		ClassReader classReader = analysisCache.getClassAnalysis(ClassReader.class, descriptor);
+		ClassNode cn = new ClassNode();
+		classReader.accept(cn, 0);
+		return cn;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.umd.cs.findbugs.classfile.IAnalysisEngine#registerWith(edu.umd.cs.findbugs.classfile.IAnalysisCache)
+	 */
+	public void registerWith(IAnalysisCache analysisCache) {
+		analysisCache.registerClassAnalysisEngine(ClassNode.class, this);
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.umd.cs.findbugs.classfile.IAnalysisEngine#retainAnalysisResults()
+	 */
+	public boolean retainAnalysisResults() {
+		// Perhaps we should retain these, like we do with JavaClass objects?
+		return false;
+	}
+}
