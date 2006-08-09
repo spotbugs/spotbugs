abcf76070f14d7738ea426d8ae5a22343e96e343
David Hovemeyer <david.hovemeyer@gmail.com>
2006-08-09 15:07:14 +0000

    Whitespace fix, added javadoc.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6242 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/classfile/ClassNameMismatchException.java
+++ findbugs/src/java/edu/umd/cs/findbugs/classfile/ClassNameMismatchException.java
@@ -20,18 +20,29 @@
 package edu.umd.cs.findbugs.classfile;
 
 /**
+ * Exception to indicate that the class name defined in
+ * a class file does not match its expected class name
+ * (as indicated by its resource name).
+ * 
  * @author David Hovemeyer
  */
 public class ClassNameMismatchException extends InvalidClassFileFormatException {
 	private ClassDescriptor loadedClassDescriptor;
 	
+	/**
+	 * Constructor.
+	 * 
+	 * @param expectedClassDescriptor class descriptor we were expected based on the resource name
+	 * @param loadedClassDescriptor   class descriptor actually found in the class file
+	 * @param codeBaseEntry           codebase entry the class was loaded from
+	 */
 	public ClassNameMismatchException(
 			ClassDescriptor expectedClassDescriptor,
 			ClassDescriptor loadedClassDescriptor,
 			ICodeBaseEntry codeBaseEntry) {
 		super("Expected class name " +
 				expectedClassDescriptor +
-				"does not match loaded class name " +
+				" does not match loaded class name " +
 				loadedClassDescriptor,
 				expectedClassDescriptor,
 				codeBaseEntry);
