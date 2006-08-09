0381172de8ef9a27f6ec421601317138ebe125c7
Bill Pugh <bill.pugh@gmail.com>
2006-08-09 00:15:06 +0000

    record false positive test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6237 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/MismatchedWaitFalsePositive.java
@@ -0,0 +1,28 @@
+public class MismatchedWaitFalsePositive {
+	Object lock;
+
+	MismatchedWaitFalsePositive(Object x) {
+		lock = x;
+	}
+
+	public void waitOnLock() {
+		synchronized (lock) {
+			while (true) {
+				try {
+					lock.wait();
+					return;
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public void notifyAllOnLock() {
+		synchronized (lock) {
+			lock.notifyAll();
+		}
+	}
+
+}
