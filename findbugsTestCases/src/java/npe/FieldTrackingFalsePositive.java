fe0320b67c405edd059c5caaad0ae1c25d9f21f9
David Hovemeyer <david.hovemeyer@gmail.com>
2006-08-11 13:39:23 +0000

    Initial checkin (from Bill's email)
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6283 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/npe/FieldTrackingFalsePositive.java
@@ -0,0 +1,14 @@
+package npe;
+
+public class FieldTrackingFalsePositive {
+	private Object x;
+
+	private void init() {
+		x = new Object();
+	}
+	public int getValue() {
+		if (x == null) init();
+		return x.hashCode();
+	}
+
+}
