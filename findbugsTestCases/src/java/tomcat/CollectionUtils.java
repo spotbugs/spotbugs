3e0b48a920862bc2871db271fc35284224c06b69
Bill Pugh <bill.pugh@gmail.com>
2006-08-13 04:03:53 +0000

    add test case where we are generating a false positive
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6292 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/tomcat/CollectionUtils.java
+++ findbugsTestCases/src/java/tomcat/CollectionUtils.java
@@ -1,5 +1,15 @@
 package tomcat;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.ListIterator;
+
 public class CollectionUtils {
+	public static List doNotReport(Collection collection) {
+		if (collection instanceof List) {
+			return (List) collection;
+		} else
+			return null;
+	}
 
 }
