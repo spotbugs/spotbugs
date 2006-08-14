e75a5919cbee18302ee6eca24cc5190840c78cef
Bill Pugh <bill.pugh@gmail.com>
2006-08-14 18:14:55 +0000

    New test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6315 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/npe/Color.java
@@ -0,0 +1,29 @@
+package npe;
+
+// Demonstrates a bug in a prototype of the guaranteed null dereference detector
+// Inspired by a situation where the bug exhibited in java.awt.Color, but
+// substantially cut down.
+
+public class Color {
+	float[] fvalue;
+	public Color(float components[]) {
+		int result= 0;
+		if (components == null)
+			result = 1;
+        Object x = new Object();
+        int n = 5;
+        fvalue = new float[n];
+        for (int i = 0; i < n; i++) {
+        	result++;
+            if (components[i] < 0.0) {
+                result += x.hashCode();
+            } else {
+            	float f = components[i];
+                fvalue[i] = f;
+            }
+        }
+       
+
+    }
+
+}
