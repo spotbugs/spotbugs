51f95d3e59609c0172406c5bcd496a5f22986604
Bill Pugh <bill.pugh@gmail.com>
2006-08-14 20:40:03 +0000

    Added test case where guaranteed null deref is not giving us want we want
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6316 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/npe/GuaranteedFieldDereference2.java
@@ -0,0 +1,28 @@
+package npe;
+
+public class GuaranteedFieldDereference2 {
+
+	Object x;
+
+	public GuaranteedFieldDereference2(Object x) {
+		this.x = x;
+	}
+
+	// We should generate a warning that if the if test fails,
+	// we are guaranteed to get a dereference when we compute
+	// the hashCode
+	
+	public int report(int b) {
+
+		int result;
+		if (x != null)
+			x = new Object();
+		if (b > 0)
+			result = b;
+		else
+			result = -b;
+		result += x.hashCode();
+		return result;
+	}
+
+}
