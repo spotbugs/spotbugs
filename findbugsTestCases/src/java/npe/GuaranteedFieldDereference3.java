4d39dc09b5f0a7e96c95dccb310c3b6a5ca80691
Bill Pugh <bill.pugh@gmail.com>
2006-08-15 12:28:38 +0000

    further simplification of test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6323 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/npe/GuaranteedFieldDereference3.java
@@ -0,0 +1,21 @@
+package npe;
+
+public class GuaranteedFieldDereference3 {
+
+
+	// We should generate a warning that if the if test fails,
+	// we are guaranteed to get a dereference when we compute
+	// the hashCode
+	
+	public int report(int b) {
+		if (x != null)
+			x = new Object();
+		return x.hashCode();
+	}
+
+	public GuaranteedFieldDereference3(Object x) {
+		this.x = x;
+	}
+	Object x;
+
+}
