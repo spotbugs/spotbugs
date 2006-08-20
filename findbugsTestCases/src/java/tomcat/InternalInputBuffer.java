cae319ea5927b00d75e4afa277f8a40f8cc6fcf2
Bill Pugh <bill.pugh@gmail.com>
2006-08-20 01:11:14 +0000

    false positive test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6395 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/tomcat/InternalInputBuffer.java
@@ -0,0 +1,29 @@
+package tomcat;
+
+// Inspired by false positive in org.apache.coyote.http11.InternalInputBuffer
+public class InternalInputBuffer {
+	
+	byte [] buf = new byte[100];
+	
+	Object foo() {
+		return new Object();
+	}
+	int falsePositive(boolean b) {
+		boolean colon = false;
+		int result;
+		Object headerValue = null;
+		int pos = 0;
+		// this should be a do-while rather than a while-do loop
+		while (!colon) {
+			if (buf[pos] == 58) {
+				headerValue = foo();
+				colon = true;
+			}
+			pos++;
+		}
+		if (b) result = 1;
+		else result = 2;
+		return result + headerValue.hashCode();
+	}
+
+}
