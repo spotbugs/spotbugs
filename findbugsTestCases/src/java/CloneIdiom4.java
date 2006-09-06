35b45abab8f056e4c0499694e621053ab77ca029
Brian Cole <tuc@cs.umd.edu>
2006-09-06 18:33:26 +0000

    test case inspired by bug#1550504
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6551 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/CloneIdiom4.java
@@ -0,0 +1,11 @@
+/** non-final class with final clone() method.
+ * 
+ *  This is probably bug, but the detector shoud be careful
+ *  that the final clone() implementation doesn't directly
+ *  or indirectly invoke some kind of non-final copy() method.
+ */
+public class CloneIdiom4 implements Cloneable {
+	final public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+}
