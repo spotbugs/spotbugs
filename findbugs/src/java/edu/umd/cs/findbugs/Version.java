63e115308110de33a20a40239444d8153ee23cd8
Brian Cole <tuc@cs.umd.edu>
2006-09-08 20:04:20 +0000

    1.1.0rc3
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6586 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/Version.java
+++ findbugs/src/java/edu/umd/cs/findbugs/Version.java
@@ -50,7 +50,7 @@ public class Version {
 	 * Release candidate number.
 	 * "0" indicates that the version is not a release candidate.
 	 */
-	public static final int RELEASE_CANDIDATE = 2;
+	public static final int RELEASE_CANDIDATE = 3;
 	
 	/**
 	 * Preview release number.
