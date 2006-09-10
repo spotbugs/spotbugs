24eb686ccb71f866960b9335961a2623e5c607a9
Bill Pugh <bill.pugh@gmail.com>
2006-09-10 11:39:09 +0000

    Add test case for @CheckForNull fields
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6603 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/npe/CheckForNullField.java
@@ -0,0 +1,17 @@
+package npe;
+
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+
+public class CheckForNullField {
+	@CheckForNull Object x;
+	 public int getNonNullXDoNotReport() {
+		if (x == null) 
+			x = new Object(); 
+		return x.hashCode();
+	}
+	 public Object getNonNullXDoNotReport2() {
+		if (x != null) return x.hashCode();
+		x = new Object(); 
+		return x.hashCode();
+	}
+}
