881728d4ee539dd5617c33f5fcbe93effc0afaae
Brian Cole <tuc@cs.umd.edu>
2006-09-11 21:51:53 +0000

    bump to rc4
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6650 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/Version.java
+++ findbugs/src/java/edu/umd/cs/findbugs/Version.java
@@ -50,7 +50,7 @@ public class Version {
 	 * Release candidate number.
 	 * "0" indicates that the version is not a release candidate.
 	 */
-	public static final int RELEASE_CANDIDATE = 3;
+	public static final int RELEASE_CANDIDATE = 4;
 	
 	/**
 	 * Preview release number.
