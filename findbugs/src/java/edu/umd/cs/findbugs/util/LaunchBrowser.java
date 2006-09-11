99d3467ec7f88e4188e7671be48abfc259da0ec6
Brian Cole <tuc@cs.umd.edu>
2006-09-11 17:35:46 +0000

    refactor show-in-browser code, attempt to use JNLP BasicService
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6640 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/util/LaunchBrowser.java
@@ -0,0 +1,141 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2006, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston MA 02111-1307, USA
+ */
+
+package edu.umd.cs.findbugs.util;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.lang.reflect.*;
+
+import edu.umd.cs.findbugs.SystemProperties;
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
+
+/**
+ * 
+ */
+public class LaunchBrowser {
+
+	private static final @CheckForNull Method jnlpShowMethod;
+	private static final Object jnlpShowObject; // will not be null if jnlpShowMethod!=null
+	static {
+		// attempt to set the JNLP BasicService object and its showDocument(URL) method
+		Method showMethod = null;
+		Object showObject = null;
+		try {
+			Class serviceManagerClass = Class.forName("javax.jnlp.ServiceManager");
+			Method lookupMethod = serviceManagerClass.getMethod("lookup", new Class[] { String.class });
+			showObject = lookupMethod.invoke(null, new Object[] { "javax.jnlp.BasicService" });
+			showMethod = showObject.getClass().getMethod("showDocument", new Class [] { URL.class });
+		} catch (Exception ex) {
+			// do nothing -- showMethod remains null
+		}
+		jnlpShowMethod = showMethod;
+		jnlpShowObject = showObject;
+	}
+
+
+
+	/** 
+	 * attempt to show the given URL.
+	 * will first attempt via the JNLP api, then will try eval().
+	 * @param url the URL
+	 * @return true on success
+	 */
+	public static boolean showDocument(URL url) {
+
+		if (jnlpShowMethod != null) try {
+			Object result = jnlpShowMethod.invoke(jnlpShowObject, new Object [] { url });
+			return (Boolean.TRUE.equals(result));
+		} catch (InvocationTargetException ite) {
+			// do nothing
+		} catch (IllegalAccessException iae) {
+			// do nothing
+		}
+
+	    // fallback to eval()
+		return showViaEval( url.toString() ); // or url.toExternalForm()
+	}
+
+
+	/** attempt to show the given URL string.
+	 *  will first attempt via the JNLP api, then will try eval().
+	 * @param url the url srtring
+	 * @return true on success
+	 */
+	public static boolean showDocument(String url) {
+		URL wrapper = null;
+		if (jnlpShowMethod != null) try {
+			wrapper = new URL(url);
+		} catch (MalformedURLException mue) {
+			wrapper = null;
+		}
+		
+		if (wrapper != null) try {
+			Object result = jnlpShowMethod.invoke(jnlpShowObject, new Object [] { url });
+			System.out.println("jnlp result is "+result);
+			return (Boolean.TRUE.equals(result));
+		} catch (InvocationTargetException ite) {
+			// do nothing
+		} catch (IllegalAccessException iae) {
+			// do nothing
+		}
+
+	    // fallback to eval()
+		return showViaEval(url);
+	}
+
+
+	/**
+     * Attempts to show the given URL in the OS's web browser.
+     * @param url url to show
+     * @return true if the show operation was successful, false otherwise.
+     */
+    private static boolean showViaEval(String url){
+    	String os = SystemProperties.getProperty("os.name").toLowerCase();
+        Runtime rt = Runtime.getRuntime();
+        try{
+	        if (os.indexOf( "win" ) >= 0) {
+	        	// this doesn't support showing urls in the form of "page.html#nameLink" 
+	            rt.exec( "rundll32 url.dll,FileProtocolHandler " + url);
+	        } else if (os.indexOf( "mac" ) >= 0) {
+	            rt.exec( "open " + url);
+	        } else if (os.indexOf( "nix") >=0 || os.indexOf( "nux") >=0) {
+	        	// Do a best guess on unix until we get a platform independent way
+	        	// Build a list of browsers to try, in this order.
+	        	String[] browsers = {"epiphany", "firefox", "mozilla", "konqueror",
+	        			"netscape","opera","links","lynx"};
+	        	
+	        	// Build a command string which looks like "browser1 "url" || browser2 "url" ||..."
+	        	StringBuffer cmd = new StringBuffer();
+	        	for (int i=0; i<browsers.length; i++)
+	        		cmd.append( (i==0  ? "" : " || " ) + browsers[i] +" \"" + url + "\" ");
+	        	
+	        	rt.exec(new String[] { "sh", "-c", cmd.toString() });
+	        } else {
+	        	return false;
+	        }
+        }catch (IOException e){
+        	return false;
+        }
+        return true;
+    }
+
+}
--- findbugs/src/java5/edu/umd/cs/findbugs/gui2/AboutDialog.java
+++ findbugs/src/java5/edu/umd/cs/findbugs/gui2/AboutDialog.java
@@ -40,6 +40,7 @@ import edu.umd.cs.findbugs.SystemProperties;
 import edu.umd.cs.findbugs.Version;
 import edu.umd.cs.findbugs.gui.ConsoleLogger;
 import edu.umd.cs.findbugs.gui.Logger;
+import edu.umd.cs.findbugs.util.LaunchBrowser;
 
 /**
  * The Help:About dialog.
@@ -213,47 +214,14 @@ public class AboutDialog extends javax.swing.JDialog {
         try {
             if (evt.getEventType().equals( HyperlinkEvent.EventType.ACTIVATED)) {
                 URL url = evt.getURL();
-                showInBrowser(url.toString());
+                //showInBrowser(url.toString());
+            	LaunchBrowser.showDocument(url);
             }
         }
         catch (Exception e) {
         }
     }//GEN-LAST:event_editorPaneHyperlinkUpdate
     
-    /**
-     * Attempts to show the given URL in the OS's web browser.
-     * @param url url to show
-     * @return true if the show operation was successful, false otherwise.
-     */
-    private static boolean showInBrowser(String url){
-    	String os = SystemProperties.getProperty("os.name").toLowerCase();
-        Runtime rt = Runtime.getRuntime();
-        try{
-	        if (os.indexOf( "win" ) >= 0) {
-	        	// this doesn't support showing urls in the form of "page.html#nameLink" 
-	            rt.exec( "rundll32 url.dll,FileProtocolHandler " + url);
-	        } else if (os.indexOf( "mac" ) >= 0) {
-	            rt.exec( "open " + url);
-	        } else if (os.indexOf( "nix") >=0 || os.indexOf( "nux") >=0) {
-	        	// Do a best guess on unix until we get a platform independent way
-	        	// Build a list of browsers to try, in this order.
-	        	String[] browsers = {"epiphany", "firefox", "mozilla", "konqueror",
-	        			"netscape","opera","links","lynx"};
-	        	
-	        	// Build a command string which looks like "browser1 "url" || browser2 "url" ||..."
-	        	StringBuffer cmd = new StringBuffer();
-	        	for (int i=0; i<browsers.length; i++)
-	        		cmd.append( (i==0  ? "" : " || " ) + browsers[i] +" \"" + url + "\" ");
-	        	
-	        	rt.exec(new String[] { "sh", "-c", cmd.toString() });
-	        } else {
-	        	return false;
-	        }
-        }catch (IOException e){
-        	return false;
-        }
-        return true;
-    }
 
 	private void okButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_okButtonActionPerformed
 		closeDialog();
