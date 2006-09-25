80159a1f7564dcd15a257a40d372ca79cb6926e0
Bill Pugh <bill.pugh@gmail.com>
2006-09-25 19:28:32 +0000

    add test case for Bug 1563719
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6789 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1563719.java
@@ -0,0 +1,26 @@
+package sfBugs;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/*Consider the following block of code:
+
+	1: List workingList = (List)getList().clone();
+	2: Iterator iter = workingList != null ?
+	workingList.iterator() : null;
+	3: for (int i = 0; i < workingList.size(); i++) {
+
+
+	FindBugs 1.1 gives a NP message for line 3. Instead, it
+	should give RCN for line 2 since a clone operation
+	could never return a null value.
+	*/
+public class Bug1563719 {
+
+	public static int f(ArrayList list) {
+		ArrayList workingList = (ArrayList)list.clone();
+		Iterator iter = workingList != null ?workingList.iterator() : null;
+		return  workingList.size();
+		}
+	}
+
