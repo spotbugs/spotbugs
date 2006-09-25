1597f7f8c924558cf9796617534c1605996edb6f
Bill Pugh <bill.pugh@gmail.com>
2006-09-25 14:00:19 +0000

    simplify
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6788 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/npe/FalsePositiveFromEclipseCommentRegion.java
+++ findbugsTestCases/src/java/npe/FalsePositiveFromEclipseCommentRegion.java
@@ -4,16 +4,16 @@ package npe;
 // org/eclipse/pde/internal/CommentRegion.formatRegion(final String indentation, final int width) {
 
 public class FalsePositiveFromEclipseCommentRegion {
-	Object foo() {
+	static Object foo() {
 		return new Object();
 	}
 
-	int f(int count) {
+	static int f() {
 
 		Object p;
 		Object n = null;
 
-		for (int i = 0; i < count; i++) {
+		for (int i = 5; i > 0; i--) {
 			p = n;
 			n = foo();
 		}
