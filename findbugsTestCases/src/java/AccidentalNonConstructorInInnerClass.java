d8a253fad1c625b57835de2f61eda8740caacc84
Bill Pugh <bill.pugh@gmail.com>
2006-11-18 01:58:09 +0000

    added test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6918 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/AccidentalNonConstructorInInnerClass.java
@@ -0,0 +1,36 @@
+public class AccidentalNonConstructorInInnerClass {
+
+	class Report {
+		void Report() {
+		};
+
+		class DeeplyNested {
+			void DeeplyNested() {
+			};
+		}
+	}
+
+	static class Report2 {
+		void Report2() {
+		};
+	}
+
+	class DoNotReport {
+		@Deprecated
+		void DoNotReport() {
+		}
+
+		DoNotReport() {
+		}
+	}
+
+	static class DoNotReport2 {
+		void DoNotReport2() {
+			throw new UnsupportedOperationException();
+		}
+
+		DoNotReport2() {
+		}
+	}
+
+}
