42331d7369fbf9331e2ca6ead93c330c75a61713
Bill Pugh <bill.pugh@gmail.com>
2006-11-22 17:49:47 +0000

    add more test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@6955 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/InfiniteIterativeLoop.java
+++ findbugsTestCases/src/java/InfiniteIterativeLoop.java
@@ -1,15 +1,52 @@
-
 public class InfiniteIterativeLoop {
 	int sum(int n) {
 		int result = 0;
-		for(int i = 0; i < n; )
+		for (int i = 0; i < n;)
 			result += i;
+		return result;
+	}
+
+	int sumDoWhile(int n) {
+		int result = 0;
+		int i = 0;
+		do {
+			result += i;
+		} while (i < n);
+		return result;
+	}
+
+	int sumWhileDo(int n) {
+		int result = 0;
+		int i = 0;
+		while (i < n) {
+			result += i;
+		}
 		return result;
 	}
 
 	int sumOfOdd(int n) {
 		int result = 0;
-		for(int i = 0; i < n; ) {
+		for (int i = 0; i < n;) {
+			if (i % 2 != 0)
+				result += i;
+		}
+		return result;
+	}
+
+	int sumOfOddDoWhile(int n) {
+		int result = 0;
+		int i = 0;
+		do {
+			if (i % 2 != 0)
+				result += i;
+		} while (i < n);
+		return result;
+	}
+
+	int sumOfOddWhileDo(int n) {
+		int result = 0;
+		int i = 0;
+		while (i < n) {
 			if (i % 2 != 0)
 				result += i;
 		}
