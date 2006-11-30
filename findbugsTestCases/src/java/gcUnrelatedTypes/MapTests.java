85bd70cc2da8f7e63258d0a7482720f5f4b3aa3e
Nat Ayewah <nat.ayewah@gmail.com>
2006-11-30 19:41:42 +0000

    GC Unrelated Types test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7010 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/gcUnrelatedTypes/MapTests.java
+++ findbugsTestCases/src/java/gcUnrelatedTypes/MapTests.java
@@ -3,6 +3,7 @@ package gcUnrelatedTypes;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MapTests {
 
@@ -28,16 +29,21 @@ public class MapTests {
 	public void test2Bugs(HashMap<CharSequence, CharSequence> map) {
 		map.containsValue(3);
 		map.remove(4);
+		map.get(5.0);
+		map.remove('r');
 	}
 
 	public void test3NoBugs(HashMap<? extends CharSequence, ? extends CharSequence> map) {
 		map.containsValue(new StringBuffer("Value"));
 		map.get(new StringBuffer("Get"));
+		map.remove(new StringBuffer("Remove"));
 	}
 
 	public void test3Bugs(HashMap<? extends CharSequence, ? extends CharSequence> map) {
 		map.containsValue(3);
 		map.containsKey(4.0);
+		map.get(5.0);
+		map.remove('r');
 	}
 
 	public void test4NoBugs(HashMap<? super CharSequence, ? super CharSequence> map) {
@@ -48,6 +54,18 @@ public class MapTests {
 	public void test4Bugs(HashMap<? super CharSequence, ? super CharSequence> map) {
 		map.containsValue(3);
 		map.containsKey('k');
+		map.get(5.0);
+		map.remove('r');
 	}
-
+	
+	ConcurrentHashMap<String, String> fieldMap;
+	
+	public String test5NoBugs() {
+		return fieldMap.get("Hello");
+	}
+	
+	public String test5Bugs() {
+		return fieldMap.get(new StringBuffer("Get"));
+	}
+	
 }
