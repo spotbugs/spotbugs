926e4635aa55fe538490729015f4618f943e4b82
Nat Ayewah <nat.ayewah@gmail.com>
2006-11-30 08:31:58 +0000

    GC Unrelated Types test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7007 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/gcUnrelatedTypes/MapTests.java
@@ -0,0 +1,53 @@
+package gcUnrelatedTypes;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class MapTests {
+
+	public void test1NoBugs(HashMap<String, String> map) {
+		map.containsKey("Key");
+		map.containsValue("Value");
+		map.get("Get");
+		map.remove("Remove");
+	}
+
+	public void test1Bugs(HashMap<String, String> map) {
+		map.containsKey(new StringBuffer("Key"));
+		map.containsValue(new StringBuffer("Value"));
+		map.get(new StringBuffer("Get"));
+		map.remove(new StringBuffer("Remove"));
+	}
+
+	public void test2NoBugs(HashMap<CharSequence, CharSequence> map) {
+		map.containsValue(new StringBuffer("Value"));
+		map.get(new StringBuffer("Get"));
+	}
+
+	public void test2Bugs(HashMap<CharSequence, CharSequence> map) {
+		map.containsValue(3);
+		map.remove(4);
+	}
+
+	public void test3NoBugs(HashMap<? extends CharSequence, ? extends CharSequence> map) {
+		map.containsValue(new StringBuffer("Value"));
+		map.get(new StringBuffer("Get"));
+	}
+
+	public void test3Bugs(HashMap<? extends CharSequence, ? extends CharSequence> map) {
+		map.containsValue(3);
+		map.containsKey(4.0);
+	}
+
+	public void test4NoBugs(HashMap<? super CharSequence, ? super CharSequence> map) {
+		map.containsValue(new StringBuffer("Value"));
+		map.get(new StringBuffer("Get"));
+	}
+
+	public void test4Bugs(HashMap<? super CharSequence, ? super CharSequence> map) {
+		map.containsValue(3);
+		map.containsKey('k');
+	}
+
+}
---
+++ findbugsTestCases/src/java/gcUnrelatedTypes/SetTests.java
@@ -0,0 +1,45 @@
+package gcUnrelatedTypes;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+
+public class SetTests {
+
+	public void test1NoBugs(Set<String> set) {
+		set.contains("Hello");
+		set.remove("Hello");
+	}
+
+	public void test1Bugs(Set<String> set) {
+		set.contains(new StringBuffer("Key"));
+		set.remove(new StringBuffer("Key"));
+	}
+
+	public void test2NoBugs(SortedSet<CharSequence> set) {
+		set.contains(new StringBuffer("Key"));
+	}
+
+	public void test2Bugs(SortedSet<CharSequence> set) {
+		set.contains(Integer.valueOf(3));
+	}
+
+	public void test3NoBugs(LinkedHashSet<? extends CharSequence> set) {
+		set.remove(new StringBuffer("Key"));		
+	}
+
+	public void test3Bugs(LinkedHashSet<? extends CharSequence> set) {
+		set.remove(Integer.valueOf(3));		
+	}
+
+	public void test4NoBugs(TreeSet<? super CharSequence> set) {
+		set.contains(new StringBuffer("Key"));		
+	}
+
+	public void test4Bugs(TreeSet<? super CharSequence> set) {
+		set.contains(Integer.valueOf(3));		
+	}
+
+}
