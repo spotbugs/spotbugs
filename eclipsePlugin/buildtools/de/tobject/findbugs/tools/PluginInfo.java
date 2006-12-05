be74377834868197db869f5981c0ccc48d9268ac
Bill Pugh <bill.pugh@gmail.com>
2006-12-05 14:53:58 +0000

    make it easier to build the eclipse plugin and get the plugin.xml file correct
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7039 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- eclipsePlugin/buildtools/de/tobject/findbugs/tools/PluginInfo.java
+++ eclipsePlugin/buildtools/de/tobject/findbugs/tools/PluginInfo.java
@@ -1,12 +1,17 @@
 package de.tobject.findbugs.tools;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.dom4j.Document;
 import org.dom4j.Node;
+import org.dom4j.io.OutputFormat;
 import org.dom4j.io.SAXReader;
+import org.dom4j.io.SAXWriter;
+import org.dom4j.io.XMLWriter;
 
 /**
  * Extract information (plugin id, version, etc.)
@@ -18,18 +23,30 @@ import org.dom4j.io.SAXReader;
 public class PluginInfo {
 	static final SimpleDateFormat eclipseDateFormat = new SimpleDateFormat("yyyyMMdd");
 	public static void main(String[] args) throws Exception {
-		if (args.length != 1) {
+		if (args.length < 1 || args.length > 2) {
 			System.err.println("Usage: " + PluginInfo.class.getName() + " <plugin.xml file>");
 			System.exit(1);
 		}
 		
 		SAXReader reader = new SAXReader();
+		SAXWriter writer = new SAXWriter();
 		Document document = reader.read(new File(args[0]));
 		String date = eclipseDateFormat.format(new Date());
 		emitProperty(document, "plugin.id", "/plugin/@id");
-		System.out.println("plugin.version" + "=" + getValue(document, "/plugin/@version").replace("qualifier", date));
+		String modifiedVersion = getValue(document, "/plugin/@version").replace("qualifier", date);
+		setValue(document, "/plugin/@version", modifiedVersion );
+		emitProperty(document, "plugin.version", "/plugin/@version");
+		if (args.length == 2)
+			serializetoXML(document, new FileOutputStream(args[1]));
 	}
 
+	public static  void serializetoXML(Document doc, OutputStream out) throws Exception {
+		   OutputFormat outformat = OutputFormat.createPrettyPrint();
+		   XMLWriter writer = new XMLWriter(out, outformat);
+		   writer.write(doc);
+		   writer.close();
+		 }
+	
 	private static void emitProperty(Document document, String propName, String xpath) {
 		System.out.println(propName + "=" + getValue(document, xpath));
 	}
@@ -38,5 +55,11 @@ public class PluginInfo {
 		if (node == null)
 			throw new RuntimeException("No node found for xpath " + xpath);
 		return  node.getText();
+	}
+	private static void setValue(Document document, String xpath, String value) {
+		Node node = document.selectSingleNode(xpath);
+		if (node == null)
+			throw new RuntimeException("No node found for xpath " + xpath);
+		node.setText(value);
 	}
 }
