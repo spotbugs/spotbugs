6e67718e7f47448304cf4d3694b3a0cd38f7e49f
Bill Pugh <bill.pugh@gmail.com>
2006-12-16 20:40:52 +0000

    add test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7128 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/googleBugs/Issue9.java
@@ -0,0 +1,35 @@
+package googleBugs;
+
+public class Issue9 {
+		private void testNamedInnerClass() {
+			class B implements Runnable {
+				public void run() {
+					String s = null;
+					s.equals(new Object() {
+						void test(){
+							String s = null;
+							s.equals(new Object() {
+
+							});
+						}
+					});
+				}
+			}
+		}
+		
+		private void testAnonymousClass() {
+			Thread t = new Thread(new Runnable (){
+				public void run() {
+					String s = null;
+					s.equals(new Object() {
+						void test(){
+							String s = null;
+							s.equals(new Object() {
+
+							});
+						}
+					});
+				}
+			});
+		}
+}
