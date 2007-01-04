e7ff3a313f5a157653d7ab07834c4c51fd5b8713
Bill Pugh <bill.pugh@gmail.com>
2007-01-04 15:39:48 +0000

    updates to work with FindBugs 1.1.3
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7273 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- bluej/src/edu/umd/cs/findbugs/bluej/RunFindbugs.java
+++ bluej/src/edu/umd/cs/findbugs/bluej/RunFindbugs.java
@@ -4,9 +4,13 @@ import java.io.IOException;
 
 import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.Detector;
+import edu.umd.cs.findbugs.DetectorFactoryCollection;
 import edu.umd.cs.findbugs.FindBugs;
+import edu.umd.cs.findbugs.FindBugs2;
+import edu.umd.cs.findbugs.IFindBugsEngine;
 import edu.umd.cs.findbugs.Project;
 import edu.umd.cs.findbugs.SortedBugCollection;
+import edu.umd.cs.findbugs.config.UserPreferences;
 
 public class RunFindbugs {
 	
@@ -24,12 +28,22 @@ public class RunFindbugs {
 			findBugsProject.addFile(f);
 		final SortedBugCollection bugs = new SortedBugCollection();
 		BugReporter reporter = new MyReporter(bugs);
-		
-		FindBugs findBugs = new FindBugs(reporter, findBugsProject);
+        System.setProperty("findbugs.jaws", "true");
+        // System.out.println(System.getProperty("findbugs.jaws"));
+        DetectorFactoryCollection.instance().ensureLoaded();
+        
+      
+		IFindBugsEngine findBugs = new FindBugs2();
+        findBugs.setBugReporter(reporter);
+        findBugs.setProject(findBugsProject);
+        findBugs.setDetectorFactoryCollection(DetectorFactoryCollection.instance());
+//      Honor current UserPreferences
+        findBugs.setUserPreferences(UserPreferences.getUserPreferences());
+        
 		reporter.setPriorityThreshold(Detector.NORMAL_PRIORITY);
 		
-		System.setProperty("findbugs.home", "/Users/pugh/Documents/eclipse-3.2/workspace/findbugs");
-		System.setProperty("findbugs.jaws", "true");
+		// System.setProperty("findbugs.home", "/Users/pugh/Documents/eclipse-3.2/workspace/findbugs");
+
 		
 		findBugs.execute();
 		
