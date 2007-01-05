940c9bcabfb72e0c4f09ab035d94900c9f828827
Bill Pugh <bill.pugh@gmail.com>
2007-01-05 20:16:45 +0000

    stick a fork in it
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7297 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/Version.java
+++ findbugs/src/java/edu/umd/cs/findbugs/Version.java
@@ -44,13 +44,13 @@ public class Version {
 	/**
 	 * Development version or release candidate?
 	 */
-	public static final boolean IS_DEVELOPMENT = true;
+	public static final boolean IS_DEVELOPMENT = false;
 
 	/**
 	 * Release candidate number.
 	 * "0" indicates that the version is not a release candidate.
 	 */
-	public static final int RELEASE_CANDIDATE = 3;
+	public static final int RELEASE_CANDIDATE = 0;
 	
 	/**
 	 * Preview release number.
