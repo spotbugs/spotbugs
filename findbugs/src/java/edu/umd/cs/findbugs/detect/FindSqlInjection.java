27a65549838d73835ac18d2341f0ec90537787a7
Bill Pugh <bill.pugh@gmail.com>
2007-01-08 15:47:12 +0000

    Hi,
    I'm going to be using the FindSqlInjection check as one example in a class
    on automated code analysis I'll be giving at RSA 2007 and BlackHat Europe
    2007 (http://wiki.yak.net/712). (I gave the same class at BlackHat USA
    last year.) The class focuses on finding exploitable security bugs, which
    is why I chose the FindSqlInjection detector.
    
    To make it easier to walk through in the class, I needed to refactor the
    code a bit. I've attached a diff against current SVN, but in case it gets
    stripped out by the list, here's a link:
    http://www.clock.org/~matt/bugreport/sqlinjection-refactor.diff
    
    Most of the changes were to decompose the analyzeMethod() function into
    several smaller, easier to understand (for me) functions. Looking at other
    detectors, I can see how some of these new methods could be reused (with
    few/no modifications) by making Detector an abstract class and pulling
    them up. I also fixed up indentation and made use of braces more
    consistent, which should help people who work in vi like myself :)
    
    I didn't see any unit tests for individual detectors, nor system tests to
    enhance/run, so I tested on a few applications I work on, and the number
    of Sql Injection bugs remained the same before and after I ran them. If
    there are other tests I should/could run, please let me know as I may
    contribute more diffs like this in the future if this goes well :)
    
    matt@use.net
    
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7303 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/FindSqlInjection.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/FindSqlInjection.java
@@ -20,6 +20,7 @@
 package edu.umd.cs.findbugs.detect;
 
 import java.util.Iterator;
+import java.lang.IllegalArgumentException;
 
 import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.classfile.Method;
@@ -51,6 +52,39 @@ import edu.umd.cs.findbugs.ba.constant.ConstantFrame;
  * @author Bill Pugh
  */
 public class FindSqlInjection implements Detector {
+	private static class StringAppendState {
+		boolean sawOpenQuote = false;
+		boolean sawCloseQuote = false;
+		boolean sawComma = false;
+		boolean sawAppend = false;
+		boolean sawUnsafeAppend = false;
+
+		public boolean getSawOpenQuote() { return sawOpenQuote; }
+		public boolean getSawCloseQuote() { return sawCloseQuote; }
+		public boolean getSawComma() { return sawComma; }
+		public boolean getSawAppend() { return sawAppend; }
+		public boolean getSawUnsafeAppend() { return sawUnsafeAppend; }
+
+		public void setSawOpenQuote (boolean saw) { 
+			sawOpenQuote = saw; 
+		}
+
+		public void setSawCloseQuote (boolean saw) {
+			sawCloseQuote = saw; 
+		}
+		
+		public void setSawComma (boolean saw) { 
+			sawComma = saw; 
+		}
+		
+		public void setSawAppend (boolean saw) { 
+			sawAppend = saw; 
+		}
+
+		public void setSawUnsafeAppend (boolean saw) { 
+			sawUnsafeAppend = saw; 
+		}
+	}
 
 	BugReporter bugReporter;
 	
@@ -77,99 +111,187 @@ public class FindSqlInjection implements Detector {
 			try {
 				analyzeMethod(classContext, method);
 			} catch (DataflowAnalysisException e) {
-				bugReporter.logError("FindSqlInjection caught exception while analyzing " + methodGen, e);
+				bugReporter.logError(
+					"FindSqlInjection caught exception while analyzing " + 
+					methodGen, e);
 			} catch (CFGBuilderException e) {
-				bugReporter.logError("FindSqlInjection caught exception while analyzing " + methodGen, e);
+				bugReporter.logError(
+					"FindSqlInjection caught exception while analyzing " + 
+					methodGen, e);
+			} catch (RuntimeException e) {
+				System.out.println(
+					"Exception while checking for SQL injection in " 
+					+ methodGen + " in " + javaClass.getSourceFileName());
+					e.printStackTrace(System.out);
 			}
 		}
 	}
-	
+
+	private boolean isStringAppend(Instruction ins, ConstantPoolGen cpg) {
+		if (ins instanceof INVOKEVIRTUAL) {
+			INVOKEVIRTUAL invoke = (INVOKEVIRTUAL) ins;
+
+			if (invoke.getMethodName(cpg).equals("append")
+				&& invoke.getClassName(cpg).startsWith("java.lang.StringB")) {
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private boolean isConstantStringLoad(Instruction ins, ConstantPoolGen cpg) {
+		if (ins instanceof LDC) {
+			LDC load = (LDC) ins;
+			Object value = load.getValue(cpg);
+			if (value instanceof String) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private StringAppendState updateStringAppendState(Instruction ins, ConstantPoolGen cpg, StringAppendState stringAppendState) {
+		if (!isConstantStringLoad(ins, cpg)) {
+			throw new IllegalArgumentException("instruction must be LDC");
+		}
+
+		LDC load = (LDC) ins;
+		Object value = load.getValue(cpg);
+		String stringValue = ((String)value).trim();
+		if (stringValue.startsWith(",") || stringValue.endsWith(","))
+			stringAppendState.setSawComma(true);
+		if (stringValue.endsWith("'"))
+			stringAppendState.setSawOpenQuote(true);
+		if (stringValue.startsWith("'"))
+			stringAppendState.setSawCloseQuote(true);
+
+		return stringAppendState;
+	}
+
+	private boolean isPreparedStatementDatabaseSink(Instruction ins, ConstantPoolGen cpg) {
+		if (!(ins instanceof INVOKEINTERFACE)) {
+			return false;
+		}
+
+		INVOKEINTERFACE invoke = (INVOKEINTERFACE) ins;
+
+		String methodName = invoke.getMethodName(cpg);
+		String interfaceName = invoke.getClassName(cpg);
+		if (methodName.equals("prepareStatement") && 
+			interfaceName.equals("java.sql.Connection")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean isExecuteDatabaseSink(Instruction ins, ConstantPoolGen cpg) {
+		if (!(ins instanceof INVOKEINTERFACE)) {
+			return false;
+		}
+
+		INVOKEINTERFACE invoke = (INVOKEINTERFACE) ins;
+
+		String methodName = invoke.getMethodName(cpg);
+		String interfaceName = invoke.getClassName(cpg);
+		if (methodName.startsWith("execute") && 
+			interfaceName.equals("java.sql.Statement")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean isDatabaseSink(Instruction ins, ConstantPoolGen cpg) {
+		return isPreparedStatementDatabaseSink(ins, cpg) ||
+			isExecuteDatabaseSink(ins, cpg);
+	}
+
+	private StringAppendState getStringAppendState(CFG cfg, ConstantPoolGen cpg) {
+		StringAppendState stringAppendState = new StringAppendState();
+
+		for (Iterator<Location> i = cfg.locationIterator(); i.hasNext();) {
+			Location location = i.next();
+			Instruction ins = location.getHandle().getInstruction();
+			if (isConstantStringLoad(ins, cpg)) {
+				stringAppendState = updateStringAppendState(ins, cpg, stringAppendState);
+			} else if (isStringAppend(ins, cpg)) { 
+				stringAppendState.setSawAppend(true);
+
+				InstructionHandle prev = location.getHandle().getPrev();
+				if (prev != null) {
+					Instruction prevIns = prev.getInstruction();
+					if (!(prevIns instanceof LDC || prevIns instanceof GETSTATIC))
+						stringAppendState.setSawUnsafeAppend(true);
+				}
+				else stringAppendState.setSawUnsafeAppend(true);
+			}
+		}
+
+		return stringAppendState;
+	}
+
+	private BugInstance generateBugInstance(JavaClass javaClass, MethodGen methodGen, Instruction ins, StringAppendState stringAppendState) {
+		ConstantPoolGen cpg = methodGen.getConstantPool();
+		int priority = LOW_PRIORITY;
+		if (stringAppendState.getSawAppend()) {  
+			if (stringAppendState.getSawOpenQuote() && 
+				stringAppendState.getSawCloseQuote()) {
+				priority = HIGH_PRIORITY;
+			} else if (stringAppendState.getSawComma()) {
+				priority = NORMAL_PRIORITY;
+			}
+
+			if (!stringAppendState.getSawUnsafeAppend()) {
+				priority += 2;
+			}
+		}
+
+		String description = "";
+		if (isExecuteDatabaseSink(ins, cpg)) {
+			description = "SQL_NONCONSTANT_STRING_PASSED_TO_EXECUTE";  
+		} else if (isPreparedStatementDatabaseSink(ins, cpg)) {
+			description = "SQL_PREPARED_STATEMENT_GENERATED_FROM_NONCONSTANT_STRING";
+		}
+		
+		BugInstance bug = new BugInstance(this, description, priority);
+		bug.addClassAndMethod(methodGen, javaClass.getSourceFileName());
+
+		return bug;
+	}
+
 	private void analyzeMethod(ClassContext classContext, Method method)
 			throws DataflowAnalysisException, CFGBuilderException {
-		
 		JavaClass javaClass = classContext.getJavaClass();
 		MethodGen methodGen = classContext.getMethodGen(method);
-		if (methodGen == null) return;
+		if (methodGen == null) 
+			return;
+
 		ConstantPoolGen cpg = methodGen.getConstantPool();
-		try {
 		CFG cfg = classContext.getCFG(method);
-	        ConstantDataflow dataflow 
-			= classContext.getConstantDataflow(method);
-	    boolean sawOpenQuote = false;
-	    boolean sawCloseQuote = false;
-	    boolean sawComma = false;
-	    boolean sawAppend = false;
-	    boolean sawUnsafeAppend = false;
-	    for (Iterator<Location> i = cfg.locationIterator(); i.hasNext(); ) {
-				Location location = i.next();
-				Instruction ins = location.getHandle().getInstruction();
-				if (ins instanceof LDC) {
-					LDC load = (LDC) ins;
-					Object value = load.getValue(cpg);
-					if (value instanceof String) {
-						String stringValue = ((String)value).trim();
-						if (stringValue.startsWith(",") || stringValue.endsWith(","))
-							sawComma = true;
-						if (stringValue.endsWith("'"))
-							sawOpenQuote = true;
-						if (stringValue.startsWith("'"))
-							sawCloseQuote = true;
-					}
-				} else if (ins instanceof INVOKEVIRTUAL) {
-					INVOKEVIRTUAL invoke = (INVOKEVIRTUAL) ins;
 
-					if (invoke.getMethodName(cpg).equals("append")
-						&& invoke.getClassName(cpg).startsWith("java.lang.StringB")) {
-						sawAppend = true;
-						InstructionHandle prev = location.getHandle().getPrev();
-						if (prev != null) {
-							Instruction prevIns = prev.getInstruction();
-							if (!(prevIns instanceof LDC || prevIns instanceof GETSTATIC))
-								sawUnsafeAppend = true;
-						}
-						else sawUnsafeAppend = true;
-					}
-				}
-	    }
+		StringAppendState stringAppendState = getStringAppendState(cfg, cpg);
 				
-		for (Iterator<Location> i = cfg.locationIterator(); i.hasNext(); ) {
+	        ConstantDataflow dataflow = classContext.getConstantDataflow(method);
+		for (Iterator<Location> i = cfg.locationIterator(); i.hasNext();) {
 			Location location = i.next();
-			
 			Instruction ins = location.getHandle().getInstruction();
-			if (!(ins instanceof INVOKEINTERFACE)) continue;
-			INVOKEINTERFACE invoke = (INVOKEINTERFACE) ins;
-
-			String methodName = invoke.getMethodName(cpg);
-			String interfaceName = invoke.getClassName(cpg);
-			if (methodName.equals("prepareStatement") && interfaceName.equals("java.sql.Connection")
-					||  methodName.startsWith("execute") && interfaceName.equals("java.sql.Statement")) {
-			ConstantFrame frame = dataflow.getFactAtLocation(location);
-		        Constant value = frame.getStackValue(0);
-		    
-			if (!value.isConstantString()) {
-				int priority = LOW_PRIORITY;
-				if (sawAppend && sawOpenQuote && sawCloseQuote) priority = HIGH_PRIORITY;
-				else if (sawAppend && sawComma) priority = NORMAL_PRIORITY;
-				if (!sawUnsafeAppend) priority+=2;
-			    bugReporter.reportBug(
-				new BugInstance(this, 
-						methodName.equals("prepareStatement")
-						? "SQL_PREPARED_STATEMENT_GENERATED_FROM_NONCONSTANT_STRING" 
-								: "SQL_NONCONSTANT_STRING_PASSED_TO_EXECUTE",  
-				 priority) 
-				.addClassAndMethod(methodGen, javaClass.getSourceFileName())
-				.addSourceLine(classContext, methodGen, javaClass.getSourceFileName(), location.getHandle()));
+			if (isDatabaseSink(ins, cpg)) {
+				ConstantFrame frame = dataflow.getFactAtLocation(location);
+				Constant value = frame.getStackValue(0);
+			    
+				if (!value.isConstantString()) {
+					// TODO: verify it's the same string represented by stringAppendState
+					// FIXME: will false positive on const/static strings returns by methods 
+					BugInstance bug = generateBugInstance(javaClass, methodGen, ins, stringAppendState);
+					bug.addSourceLine(classContext, methodGen, javaClass.getSourceFileName(), location.getHandle());
+					bugReporter.reportBug(bug);
+				}
 			}
-			}
-
-
-			}
-	} catch (RuntimeException e) {
-		System.out.println("Exception while checking for SQL injection in " 
-				+ methodGen + " in " + javaClass.getSourceFileName());
-		e.printStackTrace(System.out);
 		}
-			
 	}
 	
 	
