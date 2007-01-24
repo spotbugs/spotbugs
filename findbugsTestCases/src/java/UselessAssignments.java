85cf4c9a3b7b5708333056aac56225e1a5ade56d
Bill Pugh <bill.pugh@gmail.com>
2007-01-24 20:09:14 +0000

    more test cases => goodness
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7409 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/UselessAssignments.java
+++ findbugsTestCases/src/java/UselessAssignments.java
@@ -1,8 +1,11 @@
 class UselessAssignments {
-	int foo, bar;
+	int foo, bar,g1,g2;
 
 	public UselessAssignments(int Foo, int Bar) {
+        int x,y;
 		foo = foo;
 		Bar = Bar;
+        g1 = g1 = 0;
+        x = x = 0;
 	}
 }
