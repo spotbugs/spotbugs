dad8df2005a5a8b3eda7038fbf51993df947bcbc
Bill Pugh <bill.pugh@gmail.com>
2007-02-10 14:11:28 +0000

    move junit tests into seperate src folder put in try/catch so that all junit tests pass
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7515 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- NullPointerBenchmark/src/junit/v2/TestTest.java
+++ NullPointerBenchmark/src/junit/v2/TestTest.java
@@ -2,6 +2,13 @@ package v2;
 
 import junit.framework.TestCase;
 
+/**
+ * This class just provides runtest test cases to verify that the true cases can throw null pointer exceptions and
+ * that the false positive cases do not.
+ * 
+ * This code <em>must</em> be included in any static analysis.
+ *
+ */
 public class TestTest extends TestCase {
     
     Test test;
@@ -10,64 +17,80 @@ public class TestTest extends TestCase {
         test = new Test();
     }
     public void testTP1() {
-        for(int i = -1; i <= 5; i++)
+        try {    for(int i = -1; i <= 5; i++)
             test.tp1(i);
+    } catch (NullPointerException e) { return; }
+    fail();
     }
     public void testFP1() {
         for(int i = -1; i <= 5; i++)
             test.fp1(i);
     }
     public void testTP2() {
-        test.tp2(true);
+        try {    test.tp2(true);
         test.tp2(false);
+    } catch (NullPointerException e) { return; }
+    fail();
     }
     public void testFP2() {
         test.fp2(true);
         test.fp2(false);
     }
     public void testTP3() {
-        test.tp3(null);
+        try {    test.tp3(null);
         test.tp3("x");
+    } catch (NullPointerException e) { return; }
+    fail();
     }
     public void testTF3() {
         test.fp3(null);
         test.fp3("x");
     }
     public void testTP4() {
-        test.tp4(true);
+        try {   test.tp4(true);
         test.tp4(false);
+    } catch (NullPointerException e) { return; }
+    fail();
     }
     public void testFP4() {
         test.fp4(true);
         test.fp4(false);
     }
     public void testTP5() {
-        test.tp5(null);
+        try {   test.tp5(null);
         test.tp5("x");
+    } catch (NullPointerException e) { return; }
+    fail();
     }
     public void testTP6() {
-        test.tp6(null);
+        try {    test.tp6(null);
         test.tp6("x");
+    } catch (NullPointerException e) { return; }
+    fail();
     }
     public void testIFP1() {
-        
         test.ifp1(false);
         test.ifp1(true);
-       
-        
+
     }
     public void testITP1() {
-        test.itp1(false);
+        try {    test.itp1(false);
         test.itp1(true);
+    } catch (NullPointerException e) { return; }
+    fail();
     }
     
     public void testITP2() {
-        test.itp2();
+        try {    test.itp2();
+    } catch (NullPointerException e) { return; }
+    fail();
     }
     
     public void testITP3() {
-        test.itp3(true);
+     try {   test.itp3(true);
         test.itp3(false);
+    } catch (NullPointerException e) { return; }
+    fail();
     }
     
 }
--- NullPointerBenchmark/src/junit/v2/TestTestFields.java
+++ NullPointerBenchmark/src/junit/v2/TestTestFields.java
@@ -2,6 +2,13 @@ package v2;
 
 import junit.framework.TestCase;
 
+/**
+ * This class just provides runtest test cases to verify that the true cases can throw null pointer exceptions and
+ * that the false positive cases do not.
+ * 
+ * This code <em>must</em> be included in any static analysis.
+ *
+ */
 public class TestTestFields extends TestCase {
     
     TestFields test;
@@ -10,26 +17,33 @@ public class TestTestFields extends TestCase {
         test = new TestFields(null);
     }
     public void testTP1() {
+        try {
         for(int i = -1; i <= 5; i++)
             test.tp1(i);
+        } catch (NullPointerException e) { return; }
+        fail();
     }
     public void testFP1() {
         for(int i = -1; i <= 5; i++)
             test.fp1(i);
     }
     public void testTP2() {
-        test.tp2(true);
+        try {     test.tp2(true);
         test.tp2(false);
+    } catch (NullPointerException e) { return; }
+    fail();
     }
     public void testFP2() {
         test.fp2(true);
         test.fp2(false);
     }
     public void testTP3() {
-        test.x = null;
+        try {   test.x = null;
         test.tp3();
         test.x = "x";
         test.tp3();
+    } catch (NullPointerException e) { return; }
+    fail();
     }
     public void testTF3() {
         test.x = null;
@@ -38,44 +52,54 @@ public class TestTestFields extends TestCase {
         test.fp3();
     }
     public void testTP4() {
-        test.tp4(true);
+        try {    test.tp4(true);
         test.tp4(false);
+    } catch (NullPointerException e) { return; }
+    fail();
     }
     public void testFP4() {
         test.fp4(true);
         test.fp4(false);
     }
     public void testTP5() {
-        test.x = null;
+        try {    test.x = null;
         test.tp5();
         test.x = "x";
         test.tp5();
+    } catch (NullPointerException e) { return; }
+    fail();
     }
     public void testTP6() {
-        test.x = null;
+        try {    test.x = null;
         test.tp6();
         test.x = "x";
         test.tp6();
+    } catch (NullPointerException e) { return; }
+    fail();
     }
     public void testIFP1() {
-        
         test.ifp1(false);
-        test.ifp1(true);
-       
-        
+        test.ifp1(true);      
     }
     public void testITP1() {
-        test.itp1(false);
+        try {    test.itp1(false);
         test.itp1(true);
+    } catch (NullPointerException e) { return; }
+    fail();
     }
     
     public void testITP2() {
-        test.itp2();
+        try {   test.itp2();
+    } catch (NullPointerException e) { return; }
+    fail();
     }
     
     public void testITP3() {
+        try {
         test.itp3(true);
         test.itp3(false);
+    } catch (NullPointerException e) { return; }
+    fail();
     }
     
 }
