0bde9752705963a4cb5acd4eef340153adf36814
Kristin Stephens <ksteph@cs.umd.edu>
2007-02-14 22:42:07 +0000

    Two classes for different reported bugs. Bug1645060 is a false positive. Bug1645869 shows the report is incorrect.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7541 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1645060.java
@@ -0,0 +1,37 @@
+package sfBugs;
+
+public class Bug1645060 {
+
+	/*
+	 * SourceForge.net message:
+	 * IM_BAD_CHECK_FOR_ODD thrown on code:
+	 * if (Math.abs(myint) % 2 == 1) return null;
+	 * 
+	 * This code using Math.abs() is perfectly valid and works just fine for
+	 * negative numbers, contrary to the bug reported by Findbugs.
+	 * 
+	 * 
+	 * ###################
+	 * IM: Check for oddness that won't work for negative numbers
+	 * (IM_BAD_CHECK_FOR_ODD)
+	 * 
+	 * The code uses x % 2 == 1 to check to see if a value is odd, but this won't
+	 * work for negative numbers (e.g., (-5) % 2 == -1). If this code is intending
+	 * to check for oddness, consider using x & 1 == 1, or x % 2 != 0.
+	 * ################### 
+	 * 
+	 * They seem to have a valid agruement.
+	 */
+	
+	/**
+	 * @param args
+	 */
+	public String checkForEven(int myint) {
+		
+		if (Math.abs(myint) % 2 == 1)
+			return null;
+		
+		return "even";
+	}
+
+}
---
+++ findbugsTestCases/src/java/sfBugs/Bug1645869.java
@@ -0,0 +1,28 @@
+package sfBugs;
+
+public class Bug1645869 extends Object{
+	
+	/*
+	 * Here's the comment from SourceForge.net
+	 * The found bug "Method superfluously delegates to parent class method"
+	 * reports a method that only calls the super class version of the same
+	 * method. In doing so it makes no distinction between a normal method and a
+	 * final method. A final method could be a way to impose the default behavior
+	 * inherited from super class. Michele. 
+	 * 
+	 * The bug found is a low priority and FindBugs makes the distinction now. - Kristin
+	 */
+	
+	public static void main(String[] args){
+		System.out.println("An attempt to check a bug.");
+	}
+
+	public boolean equals(Object obj){
+		return super.equals(obj);
+	}
+	
+	public final clone(){
+		return super.clone();
+	}
+	
+}
