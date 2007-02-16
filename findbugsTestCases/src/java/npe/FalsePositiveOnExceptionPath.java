e4bd4d6efe4b3b4d8d9cb2c9f61a39476b36953a
Bill Pugh <bill.pugh@gmail.com>
2007-02-16 22:34:43 +0000

    added FP report from John Penix
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7558 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/npe/FalsePositiveOnExceptionPath.java
@@ -0,0 +1,70 @@
+package npe;
+
+import java.io.IOException;
+
+/** 
+False positive on exception path, reported by John Penix.
+
+<pre>
+I'm surprised about this false positive - seems obvious.  Am I missing
+something?
+
+thanks
+
+John
+
+---
+
+try {
+ r = a.MyClass.getInstance()
+}
+catch (IOException e) {
+ log.warn("IO Exeption" + e.getMessage());
+ return null;
+}
+catch (MyException e) {
+ log.warn("My Exeption" + e.getMessage());
+   return null;
+}
+
+size = r.getSize();  // <--NP_NULL_ON_SOME_PATH_EXCEPTION
+</pre>
+
+
+**/
+
+public class FalsePositiveOnExceptionPath {
+
+    Object getInstance(int i) throws IOException {
+        switch (i) {
+        case 0:
+            throw new IOException();
+        case 1:
+            throw new IllegalArgumentException();
+        case 2:
+            return null;
+        default:
+            return "x";
+        }
+    }
+
+    Object f(int i) {
+        Object r;
+
+        try {
+            r = getInstance(i);
+        } catch (IOException e) {
+            System.out.println("IO Exeption" + e.getMessage());
+            return null;
+        } catch (IllegalArgumentException e) {
+            System.out.println("My Exeption" + e.getMessage());
+            return null;
+        }
+
+        int h = r.hashCode(); // <--NP_NULL_ON_SOME_PATH_EXCEPTION
+        if (h == 42)
+            return null;
+        return r;
+    }
+
+}
