93f4fc3a0aea26d2bc82333acb06c338ae87d8c3
Bill Pugh <bill.pugh@gmail.com>
2007-02-19 21:20:12 +0000

    test case from source forge
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7578 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1663624.java
@@ -0,0 +1,28 @@
+package sfBugs;
+
+import edu.umd.cs.findbugs.annotations.*;
+
+@DefaultAnnotation(NonNull.class)
+public class Bug1663624 {
+
+    private interface SomeInterface {
+
+        public boolean condition1();
+
+        public boolean condition2();
+
+        public Object get();
+
+    }
+
+    public void show(SomeInterface reference) {
+        Object object = reference.condition1() ? reference.get() : null;
+        if (reference.condition2()) {
+            foo(object); // (*) object may bu null!
+        }
+    }
+
+    private void foo(Object caretSelection) {
+    }
+
+}
