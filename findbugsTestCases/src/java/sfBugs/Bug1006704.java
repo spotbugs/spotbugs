e267ea26495becc11d092fa2de09743ba4e2c442
Bill Pugh <bill.pugh@gmail.com>
2007-02-23 01:38:40 +0000

    Add sourceforge testcase
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7612 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1006704.java
@@ -0,0 +1,29 @@
+package sfBugs;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Bug1006704 {
+    void f(Connection con, Integer key) throws SQLException {
+        StringBuffer sql = new StringBuffer("SELECT * FROM xxx  WHERE xxx_id = ?");
+        PreparedStatement ps = con.prepareStatement(sql.toString());
+        try {
+            ps.setInt(1, key.intValue());
+            ResultSet rs = ps.executeQuery();
+            try {
+                rs.next();
+                int index = 1;
+                Integer firstQuestionId = new Integer(rs.getInt(index++));
+                String description = rs.getString(index++);
+                Float approvalScore = new Float(rs.getFloat(index++));
+            } finally {
+                rs.close();
+            }
+        } finally {
+            ps.close();
+        }
+
+    }
+}
