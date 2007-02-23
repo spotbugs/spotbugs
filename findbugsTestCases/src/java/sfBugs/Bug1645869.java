d9b55b31ad2defd6e7be3a47f1fb50aca22ee636
Bill Pugh <bill.pugh@gmail.com>
2007-02-23 02:09:25 +0000

    Made the test case more interesting
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7613 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug1645869.java
+++ findbugsTestCases/src/java/sfBugs/Bug1645869.java
@@ -1,6 +1,6 @@
 package sfBugs;
 
-public class Bug1645869 extends Object{
+public class Bug1645869  {
 	
 	/*
 	 * Here's the comment from SourceForge.net
@@ -13,16 +13,100 @@ public class Bug1645869 extends Object{
 	 * The bug found is a low priority and FindBugs makes the distinction now. - Kristin
 	 */
 	
-	public static void main(String[] args){
-		System.out.println("An attempt to check a bug.");
-	}
+    boolean booleanMethod() {
+        return true;
+    }
+    byte byteMethod() {
+        return 1;
+    }
+    char charMethod() {
+        return 1;
+    }
+    short shortMethod() {
+        return 1;
+    }
+    int intMethod() {
+        return 1;
+    }
+    long longMethod() {
+        return 1;
+    }
+    double doubleMethod() {
+        return 1;
+    }
+    float floatMethod() {
+        return 1;
+    }
+    Object objectMethod() {
+        return null;
+    }
+    Object[] arrayMethod() {
+        return null;
+    }
+	
+	static class Useless extends Bug1645869 {
+        boolean booleanMethod() {
+            return super.booleanMethod();
+        }
+        byte byteMethod() {
+            return super.byteMethod();
+        }
+        char charMethod() {
+            return  super.charMethod();
+        }
+        short shortMethod() {
+            return super.shortMethod();
+        }
+        int intMethod() {
+            return super.intMethod();
+        }
+        long longMethod() {
+            return super.longMethod();
+        }
+        double doubleMethod() {
+            return super.doubleMethod();
+        }
+        float floatMethod() {
+            return super.floatMethod();
+        }
+        Object objectMethod() {
+            return super.objectMethod();
+        }
+        Object[] arrayMethod() {
+            return super.arrayMethod();
+        }
+    }
+    static class Uselessful extends Bug1645869 {
+        final boolean booleanMethod() {
+            return super.booleanMethod();
+        }
+        final byte byteMethod() {
+            return super.byteMethod();
+        }
+        final  char charMethod() {
+            return  super.charMethod();
+        }
+        final short shortMethod() {
+            return super.shortMethod();
+        }
+        final int intMethod() {
+            return super.intMethod();
+        }
+        final long longMethod() {
+            return super.longMethod();
+        }
+        final double doubleMethod() {
+            return super.doubleMethod();
+        }
+        final float floatMethod() {
+            return super.floatMethod();
+        }
+        final Object objectMethod() {
+            return super.objectMethod();
+        }
+        final Object[] arrayMethod() {
+            return super.arrayMethod();
+        }
+    }
 
-	public boolean equals(Object obj){
-		return super.equals(obj);
-	}
-	
-	public final Object clone() throws CloneNotSupportedException {
-		return super.clone();
-	}
-	
 }
