0daa431ea2cf2ce7ca2494ee3bf7468a1270491a
Bill Pugh <bill.pugh@gmail.com>
2007-02-24 14:34:53 +0000

    fix regex
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7626 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/tools/edu/umd/cs/findbugs/tools/FilterPropertyDatabase.java
+++ findbugs/src/tools/edu/umd/cs/findbugs/tools/FilterPropertyDatabase.java
@@ -1,12 +1,13 @@
 package edu.umd.cs.findbugs.tools;
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.bcel.Constants;
-import org.apache.bcel.classfile.Constant;
 
 /*
  * FindBugs - Find Bugs in Java programs
@@ -38,8 +39,11 @@ public class FilterPropertyDatabase {
      * @throws IOException 
      */
     public static void main(String[] args) throws IOException {
-       BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-       Pattern p = Pattern.compile(",([0-9]+)|");
+       InputStream inSource = System.in;
+       if (args.length > 0)
+           inSource = new FileInputStream(args[0]);
+       BufferedReader in = new BufferedReader(new InputStreamReader(inSource));
+       Pattern p = Pattern.compile(",([0-9]+)\\|");
        while (true) {
            String s = in.readLine();
            if (s == null) break;
