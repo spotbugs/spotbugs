c51bf403ea2278f4178845859e29bd510dd8f806
Bill Pugh <bill.pugh@gmail.com>
2007-03-09 15:25:14 +0000

    add sourceforge test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7738 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1584086.java
@@ -0,0 +1,71 @@
+package sfBugs;
+
+public class Bug1584086 {
+
+    public static void falsePositive(String... args) {
+        loop: for (int i = 0; i < 5; i++) {
+            switch (args.length) {
+            case 0:
+            case 1:
+                System.out.println("some thing");
+                break loop;
+                
+
+            case 2:
+                System.out.println("some thing");
+                break;
+
+            default:
+                System.out.println("Some other number of arguments");
+                break;
+            }
+        }
+    }
+
+    
+    public static void falsePositive2(String... args) {
+
+        loop: for (int i = 0; i < 5; i++) {
+            switch (args.length) {
+            case 0:
+            case 1:
+                System.out.println("some thing");
+                break loop;
+
+            case 2:
+                System.out.println("some thing");
+                continue loop;
+
+            default:
+                System.out.println("Some other number of arguments");
+                break;
+            }
+            System.out.println("some other");
+
+        }
+
+    }
+    public static void main(String... args) {
+
+        loop: for (int i = 0; i < 5; i++) {
+            switch (args.length) {
+            case 0:
+            case 1:
+                System.out.println("some thing");
+                break;
+
+            case 2:
+                System.out.println("some thing");
+                break;
+
+            default:
+                System.out.println("Some other number of arguments");
+                break;
+            }
+            System.out.println("some other");
+
+        }
+
+    }
+
+}
