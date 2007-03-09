7b8557330c34761f4fa8263215937b67fd0fddd4
Bill Pugh <bill.pugh@gmail.com>
2007-03-09 14:27:57 +0000

    more bugs from sourceforge
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7736 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1611715.java
@@ -0,0 +1,13 @@
+package sfBugs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bug1611715 {
+
+    List lst = new ArrayList();
+
+    List method(){
+    return (List) ((ArrayList)lst).clone();
+    }
+}
--- findbugsTestCases/src/java/sfBugs/Bug1629176.java
+++ findbugsTestCases/src/java/sfBugs/Bug1629176.java
@@ -8,58 +8,44 @@ public class Bug1629176 {
     private static final String INSERT_FIELD_AUDIT = "foo";
 
     void f() throws SQLException {
-    PreparedStatement insertFieldAudit = null;
+        PreparedStatement insertFieldAudit = null;
 
-    try {
-    for ( int i = 1; i <= 10; i++ ) {
-
-
-    insertFieldAudit = getConnection().prepareStatement(INSERT_FIELD_AUDIT );
-
-    insertFieldAudit.executeUpdate();
-
-    }
-    } finally {
-        insertFieldAudit.close();
-    // closeStatement( insertFieldAudit );
-    }
+        try {
+            for (int i = 1; i <= 10; i++) {
+                insertFieldAudit = getConnection().prepareStatement(INSERT_FIELD_AUDIT);
+                insertFieldAudit.executeUpdate();
+            }
+        } finally {
+            insertFieldAudit.close();
+        }
     }
 
     void f2() throws SQLException {
-        PreparedStatement insertFieldAudit  = null;
-      try{
-           insertFieldAudit = getConnection().prepareStatement(INSERT_FIELD_AUDIT );
+        PreparedStatement insertFieldAudit = null;
+        try {
+            insertFieldAudit = getConnection().prepareStatement(INSERT_FIELD_AUDIT);
             insertFieldAudit.executeUpdate();
-            insertFieldAudit = getConnection().prepareStatement(INSERT_FIELD_AUDIT );
-        insertFieldAudit.executeUpdate();
-
-        
+            insertFieldAudit = getConnection().prepareStatement(INSERT_FIELD_AUDIT);
+            insertFieldAudit.executeUpdate();
         } finally {
             insertFieldAudit.close();
-        // closeStatement( insertFieldAudit );
         }
-        }
-
-    void f3() throws SQLException {
-        PreparedStatement insertFieldAudit  = null;
-           insertFieldAudit = getConnection().prepareStatement(INSERT_FIELD_AUDIT );
-            insertFieldAudit.executeUpdate();
-            insertFieldAudit = getConnection().prepareStatement(INSERT_FIELD_AUDIT );
-        insertFieldAudit.executeUpdate();
-
-          }
-
-        
-    
-    
-    
-    private void closeStatement(PreparedStatement insertFieldAudit) {
-        // TODO Auto-generated method stub
-        
     }
 
+    void f3() throws SQLException {
+        PreparedStatement insertFieldAudit = null;
+        insertFieldAudit = getConnection().prepareStatement(INSERT_FIELD_AUDIT);
+        insertFieldAudit.executeUpdate();
+        insertFieldAudit = getConnection().prepareStatement(INSERT_FIELD_AUDIT);
+        insertFieldAudit.executeUpdate();
+    }
+
+    void f4() throws SQLException {
+        PreparedStatement insertFieldAudit = getConnection().prepareStatement(INSERT_FIELD_AUDIT);
+       insertFieldAudit.executeUpdate();
+    }
+ 
     private Connection getConnection() {
-        
         return null;
     }
 
