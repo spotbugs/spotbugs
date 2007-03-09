068a2c752516e9e2dd1f8b1ff139cb6e65fdca0c
Bill Pugh <bill.pugh@gmail.com>
2007-03-09 22:55:35 +0000

    add sourceforge test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7748 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1562060.java
@@ -0,0 +1,74 @@
+package sfBugs;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Bug1562060 {
+
+    void f4(Connection conn, String query) throws SQLException {
+        PreparedStatement preparedStmt = conn.prepareStatement(query);
+        preparedStmt.execute();
+    }
+    void f5(Connection conn, String query) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute(query);
+    }
+    /*
+    void falseNegative(Connection conn, String query) throws SQLException {
+        PreparedStatement preparedStmt = null;
+        try {
+
+            preparedStmt = conn.prepareStatement(query);
+        } finally {
+            if (null != preparedStmt) {
+                try {
+                    preparedStmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+
+                }
+
+            }
+        }
+    }
+
+    void f2(Connection conn, String query) throws SQLException {
+        PreparedStatement preparedStmt = null;
+        try {
+
+            preparedStmt = conn.prepareStatement(query);
+        } finally {
+            if (preparedStmt != null) {
+                try {
+                    preparedStmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+
+                }
+
+            }
+        }
+    }
+
+    void f3(Connection conn, String query) throws SQLException {
+        PreparedStatement preparedStmt = null;
+        try {
+
+            preparedStmt = conn.prepareStatement(query);
+        } finally {
+            if (preparedStmt == null) {
+                try {
+                    preparedStmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+
+                }
+
+            }
+        }
+    }
+*/
+    
+}
