80e231167c3e77013634058b7a73197deb830ac1
Bill Pugh <bill.pugh@gmail.com>
2007-03-09 03:22:28 +0000

    More bug patterns is goodness
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7733 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1619879.java
@@ -0,0 +1,14 @@
+package sfBugs;
+
+public enum Bug1619879 { ONE, TWO;
+    
+    int f;
+
+    // should report something other than EQ_SELF_NO_OBJECT
+    public boolean equals(Bug1619879 foo)
+    {
+    return f == foo.f;
+    }
+
+    
+}
