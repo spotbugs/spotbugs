cf4d62690d3f3330a19a0a283aaaabc585a81e09
Bill Pugh <bill.pugh@gmail.com>
2007-03-09 19:08:15 +0000

    add sourceforge test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7742 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1633245.java
@@ -0,0 +1,16 @@
+package sfBugs;
+
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+
+public class Bug1633245 {
+    interface Foo {
+        int f(@CheckForNull Object x);
+    }
+    
+    static class FooImpl implements Foo {
+        public int f(Object x) {
+            return x.hashCode();
+        }
+    }
+
+}
