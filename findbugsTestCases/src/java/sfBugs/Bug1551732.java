fd171240ce9c3bc6575296c15626526895ffe37a
Bill Pugh <bill.pugh@gmail.com>
2007-03-10 04:01:41 +0000

    more bugs from sourceforge
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7753 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1551732.java
@@ -0,0 +1,40 @@
+package sfBugs;
+
+/**
+ * Shows false dead lock store warning.
+ * 
+ * @author <a href="mailto:rlenard@avaya.com">rlenard</a>
+ */
+public class Bug1551732 extends Thread {
+	/**
+	 * 
+	 * {@inheritDoc}.
+	 * 
+	 * @see java.lang.Thread#run()
+	 * 
+	 */
+	@Override
+	public void run() {
+		long start = System.currentTimeMillis();
+		// We add 20 milliseconds to take into account possible inaccuracy of
+		// sleep calls.
+		long target = start + 20;
+		while (true) {
+			try {
+				target += 10;
+				final long toSleep = target - System.currentTimeMillis();
+				sleep(Math.max(0, toSleep));
+			} catch (final InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				if (target > 100000) {
+					return;
+				}
+				final long elapsed = System.currentTimeMillis() - start;
+				if (elapsed >= 50) {
+					start += 50;
+				}
+			}
+		}
+	}
+}
