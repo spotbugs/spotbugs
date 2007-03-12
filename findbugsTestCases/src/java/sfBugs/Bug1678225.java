4acfa0f83a71121d3cda781c5d7e9f9b7dee9596
Kristin Stephens <ksteph@cs.umd.edu>
2007-03-12 17:17:10 +0000

    This is from the bug tracker. And NP_NULL_ON_SOME_PATH_EXCEPTION is not reported.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7768 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1678225.java
@@ -0,0 +1,28 @@
+package sfBugs;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Bug1678225 {
+	public void foo(String fileName) {
+		InputStream is = null;
+		try {
+			is = new FileInputStream(fileName);
+			useStream(is);
+			is.close();
+		}
+		catch (IOException ex) {
+			try {
+				is.close();
+			}
+			catch (IOException exx) {
+				exx.printStackTrace();
+			}
+			ex.printStackTrace();
+		}
+	}
+
+	public void useStream(InputStream is) throws IOException{
+	}
+}
