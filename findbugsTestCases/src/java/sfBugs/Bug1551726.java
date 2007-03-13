3d5c7544cffcf762d9ecc13fafc2cd604b1f3353
Bill Pugh <bill.pugh@gmail.com>
2007-03-13 17:59:44 +0000

    variant test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7785 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug1551726.java
+++ findbugsTestCases/src/java/sfBugs/Bug1551726.java
@@ -4,13 +4,26 @@ import junit.framework.TestCase;
 
 public class Bug1551726 extends TestCase {
 	  /** Test will fail. */
-    public void falsePositive()
+    public void testFalsePositive()
     {
         final Integer x = null;
         assertNotNull(x); // throws AssertionFailedError if condition is not met.
         assertTrue(x.intValue() > 0);
     }
-
+    
+    int f() { 
+        return 42;
+    }
+    public void testFalsePositive2()
+    {
+        int i = f();
+        Integer x = null;
+        if (i > 0)
+            x = i;
+        assertNotNull(x); // throws AssertionFailedError if condition is not met.
+        assertTrue(x.intValue() > 0);
+    }
+    
 	
 
 }
