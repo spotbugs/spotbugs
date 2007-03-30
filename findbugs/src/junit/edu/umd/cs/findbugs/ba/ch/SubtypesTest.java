048df4aa6ef53c8250b35616dfaa46690c95f51c
Bill Pugh <bill.pugh@gmail.com>
2007-03-30 14:18:44 +0000

    more junit tests
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7858 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/junit/edu/umd/cs/findbugs/ba/ch/SubtypesTest.java
@@ -0,0 +1,46 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2006, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.ba.ch;
+
+import junit.framework.TestCase;
+
+/**
+ * @author pugh
+ */
+public class SubtypesTest extends TestCase {
+    
+    public void testExtractClassName() {
+        assertEquals("java/lang/Integer", Subtypes.extractClassName("Ljava/lang/Integer;"));
+        assertEquals("java/lang/Integer", Subtypes.extractClassName("[Ljava/lang/Integer;"));
+        assertEquals("java/lang/Integer", Subtypes.extractClassName("[[Ljava/lang/Integer;"));
+        assertEquals("java/lang/Integer", Subtypes.extractClassName("[[[Ljava/lang/Integer;"));
+        assertEquals("java/lang/Integer", Subtypes.extractClassName("java/lang/Integer"));
+    }
+    public void testExtractClassNameBad() {
+        try {
+        Subtypes.extractClassName("L[Ljava/lang/Integer;");
+        fail();
+        } catch (IllegalArgumentException e) {
+            assert true;
+        }
+        
+    }
+
+}
