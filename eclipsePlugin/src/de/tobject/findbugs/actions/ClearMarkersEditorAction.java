81bf4fa1c0087e0d432606d3fe8bb86eca6f083a
Alex Mont <montalexan@umd.edu>
2007-04-13 04:49:58 +0000

    Added "Find bugs" and "Clear bug markers" options when you right click on an editor window.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7902 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ eclipsePlugin/src/de/tobject/findbugs/actions/ClearMarkersEditorAction.java
@@ -0,0 +1,131 @@
+/* 
+ * FindBugs Eclipse Plug-in.
+ * Copyright (C) 2003 - 2004, Peter Friese
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package de.tobject.findbugs.actions;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Iterator;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
+
+import de.tobject.findbugs.FindbugsPlugin;
+import de.tobject.findbugs.reporter.MarkerUtil;
+
+/**
+ * Remove all bug markers for the currently selectedt project.
+ * 
+ * @author Peter Friese
+ * @author Phil Crosby
+ * @version 1.0
+ * @since 25.09.2003
+ */
+public class ClearMarkersEditorAction implements IEditorActionDelegate {
+
+	/** The current selection. */
+	private ISelection selection;
+	private IEditorPart currentEditor;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.IObjectActionDelegate#setActivePart(org.eclipse.jface.action.IAction,
+	 *      org.eclipse.ui.IWorkbenchPart)
+	 */
+	public final void setActiveEditor(final IAction action,
+			final IEditorPart targetPart) {
+		currentEditor = targetPart;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction,
+	 *      org.eclipse.jface.viewers.ISelection)
+	 */
+	public final void selectionChanged(final IAction action,
+			final ISelection selection) {
+		this.selection = selection;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+	 *      TODO philc: test against jar files.
+	 */
+	public final void run(final IAction action) {
+		if(currentEditor != null)
+			work(((FileEditorInput)(currentEditor.getEditorInput())).getFile());
+	}
+
+	/**
+	 * Clear the FindBugs markers on each project in the given selection, displaying a progress monitor.
+	 * 
+	 * @param selection
+	 */
+	private void work(final IFile theFile) {
+		try {
+
+			IRunnableWithProgress r = new IRunnableWithProgress() {
+                public void run(IProgressMonitor pm) throws InvocationTargetException {
+                    try {
+                            Object resource = ((IAdaptable)theFile).getAdapter(IResource.class);
+                            IResource res = (resource instanceof IResource ? (IResource) resource : null);
+                            if (res != null) {
+                                pm.subTask("Clearing FindBugs markers from " + res.getName());
+                                MarkerUtil.removeMarkers(res);
+                        }
+
+                    } catch (CoreException ex) {
+                        FindbugsPlugin.getDefault().logException(ex, "CoreException on clear markers");
+                        throw new InvocationTargetException(ex);
+
+                    } catch (RuntimeException ex) {
+                        FindbugsPlugin.getDefault().logException(ex, "RuntimeException on clear markers");
+                        throw ex;
+                    }
+                }
+            };
+
+			ProgressMonitorDialog progress = new ProgressMonitorDialog(FindbugsPlugin.getShell());
+			progress.run(true, true, r);
+		} catch (InvocationTargetException e) {
+			FindbugsPlugin.getDefault().logException(e, "InvocationTargetException on clear markers");
+		} catch (InterruptedException e) {
+			FindbugsPlugin.getDefault().logException(e, "InterruptedException on clear markers");
+		}
+	}
+
+}
---
+++ eclipsePlugin/src/de/tobject/findbugs/actions/FindBugsEditorAction.java
@@ -0,0 +1,180 @@
+/*
+ * FindBugs Eclipse Plug-in.
+ * Copyright (C) 2003 - 2004, Peter Friese
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package de.tobject.findbugs.actions;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.ILock;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorPart;
+
+
+import de.tobject.findbugs.FindbugsPlugin;
+import de.tobject.findbugs.builder.AbstractFilesCollector;
+import de.tobject.findbugs.builder.FilesCollectorFactory;
+import de.tobject.findbugs.builder.FindBugsWorker;
+import de.tobject.findbugs.util.Util;
+
+
+
+/**
+ * Run FindBugs on the currently selected element(s) in the package explorer.
+ *
+ * @author Peter Friese
+ * @author Phil Crosby
+ * @version 1.1
+ * @since 25.09.2003
+ */
+public class FindBugsEditorAction implements IEditorActionDelegate {
+
+	/** lock to force no more than one findBugs.execute() task at a time. see
+	  * http://help.eclipse.org/help30/topic/org.eclipse.platform.doc.isv/guide/runtime_jobs_locks.htm
+	  * Alas, this is less pretty than a edu.umd.cs.findbugs.plugin.eclipse.util.MutexSchedulingRule
+	  * from the user's point of view, but it doesn't have the IllegalArgumentException
+	  * problem ("does not match outer scope rule") so this the ILock is preferred. */
+	protected static final ILock findbugsExecuteLock = Platform.getJobManager().newLock();
+
+	/** The current selection. */
+	private ISelection selection;
+	private IEditorPart currentEditor;
+
+	/*
+	 * @see org.eclipse.ui.IObjectActionDelegate#setActivePart(org.eclipse.jface.action.IAction,
+	 *      org.eclipse.ui.IWorkbenchPart)
+	 */
+	public final void setActiveEditor(final IAction action,
+			final IEditorPart targetPart) {
+		currentEditor = targetPart;
+	}
+
+	/*
+	 * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction,
+	 *      org.eclipse.jface.viewers.ISelection)
+	 */
+	public final void selectionChanged(final IAction action,
+			final ISelection selection) {
+		this.selection = selection;
+	}
+
+	/*
+	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+	 */
+	public final void run(final IAction action) {
+		if(currentEditor != null)
+			work(((FileEditorInput)(currentEditor.getEditorInput())).getFile());
+	}
+
+	/**
+	 * The files contained within a resource. Searches container resources, such
+	 * as folders or packages, or parses single file resources such as java
+	 * files.
+	 *
+	 * @param resource
+	 *            the resource to search for files
+	 * @return the files contained within the given resource
+	 * @throws CoreException
+	 */
+	private Collection filesInResource(IResource resource) throws CoreException {
+		/*
+		 * Note: the default package is an IContainer that has all other
+		 * packages as subfolders. Eclipse treats the "default package" as the
+		 * project itself. Thus, this method will return ALL java files in the
+		 * project when invoked on the default package resource.
+		 */
+		if (resource instanceof IContainer) {
+			AbstractFilesCollector collector = FilesCollectorFactory
+					.getFilesCollector((IContainer) resource);
+			return collector.getFiles();
+		} else {
+			Collection<IResource> result = new ArrayList<IResource>();
+			result.add(resource);
+			// For a single file resource, if we have a java file, attempt to
+			// add its corresponding class file,
+			// and vice versa, so that the analysis can proceed.
+			if (resource.getFileExtension().equalsIgnoreCase("java")) {
+				result.add(resource.getParent().findMember(
+						Util.changeExtension(resource.getName(), "class")));
+			} else if (resource.getFileExtension().equalsIgnoreCase("class")) {
+				result.add(resource.getParent().findMember(
+						Util.changeExtension(resource.getName(), "java")));
+			}
+			return result;
+		}
+	}
+
+	/**
+	 * Run a FindBugs analysis on the given resource, displaying a progress
+	 * monitor.
+	 *
+	 * @param resource The resource to run the analysis on.
+	 */
+	private void work(final IFile theFile) {
+		try {
+			final Collection files = filesInResource(theFile);
+			Job runFindBugs = new Job("Finding bugs in "+theFile.getName()+"...") {
+
+				@Override
+				protected IStatus run(IProgressMonitor monitor) {
+					FindBugsWorker worker =
+						new FindBugsWorker(theFile.getProject(), monitor);
+					try {
+						findbugsExecuteLock.acquire();
+						worker.work(files, true);
+					} catch (CoreException e) {
+						FindbugsPlugin.getDefault().logException(e, "Analysis exception");
+						return Status.CANCEL_STATUS;
+					}
+					finally {
+						findbugsExecuteLock.release();
+					}
+					return Status.OK_STATUS;
+				}
+			};
+
+			runFindBugs.setUser(true);
+			runFindBugs.schedule();
+		} catch (CoreException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+	}
+}
