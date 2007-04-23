c94ed3070937fd08796a3fbcc94d3a604aa2c2a6
Bill Pugh <bill.pugh@gmail.com>
2007-04-23 18:52:55 +0000

    provide recursive traversal
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@7940 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/tools/edu/umd/cs/findbugs/tools/FixIndentation.java
+++ findbugs/src/tools/edu/umd/cs/findbugs/tools/FixIndentation.java
@@ -27,84 +27,117 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
 
 /**
  * @author pugh
  */
 public class FixIndentation {
 	static final String TABS = "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t"
-		+"\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t";
-	
+	        + "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t";
+
 	public static void main(String args[]) throws Exception {
 		fix(new File(args[0]), true);
 	}
-	
-	
+
+	static void recursiveFix(File root, boolean partial) throws IOException {
+		String rootPath = root.getAbsolutePath();
+		Set<File> seen = new HashSet<File>();
+		Queue<File> todo = new LinkedList<File>();
+		seen.add(root);
+		todo.add(root);
+		while (!todo.isEmpty()) {
+			File next = todo.remove().getAbsoluteFile();
+			String nextPath = next.getAbsolutePath();
+			if (!nextPath.startsWith(rootPath))
+				continue;
+
+			if (next.isDirectory()) {
+				File[] contents = next.listFiles();
+				if (contents != null)
+					for (File c : contents)
+						if (seen.add(c))
+							todo.add(c);
+			} else if (nextPath.endsWith(".java"))
+				fix(next, partial);
+		}
+	}
+
 	static String fix(String s) {
-		if (s.length() == 0) return s;
+		if (s.length() == 0)
+			return s;
 		// if (s.trim().length() == 0) return "";
 		int spaces = 0;
 		boolean seenTabs = false;
 		boolean badSpaces = false;
 		int pos = 0;
 		int indentation = 0;
-		while(pos < s.length()) {
+		while (pos < s.length()) {
 			char c = s.charAt(pos);
 			if (c == ' ') {
 				indentation++;
 				spaces++;
 			} else if (c == '\t') {
-				indentation +=4;
+				indentation += 4;
 				seenTabs = true;
-				if (spaces > 0) badSpaces = true;
-			} else break;
+				if (spaces > 0)
+					badSpaces = true;
+			} else
+				break;
 			pos++;
 		}
 		if (badSpaces || spaces > 1) {
-			int tabs = (indentation+3)/4;
-			String result = TABS.substring(0,tabs) + s.substring(pos);
+			int tabs = (indentation + 3) / 4;
+			String result = TABS.substring(0, tabs) + s.substring(pos);
 			return result;
 		}
 		return s;
-		
+
 	}
+
 	static void fix(File fileToUpdate, boolean partial) throws IOException {
+		boolean anyChanges = false;
 		BufferedReader in = new BufferedReader(new FileReader(fileToUpdate));
 		StringWriter stringWriter = new StringWriter();
 		PrintWriter out = new PrintWriter(stringWriter);
 		int consecutiveFixes = 0;
 		try {
-		while (true) {
-			String s = in.readLine();
-			if (s == null) break;
-			String s2 = fix(s);
-			if (s2 != s) {
-				consecutiveFixes++;
-				if (consecutiveFixes > 3 && partial) {
-					s2 = s;
+			while (true) {
+				String s = in.readLine();
+				if (s == null)
+					break;
+				String s2 = fix(s);
+				if (s2 != s) {
+					consecutiveFixes++;
+					if (consecutiveFixes > 3 && partial) {
+						s2 = s;
+						consecutiveFixes = 0;
+					} else anyChanges = true;
+				} else
 					consecutiveFixes = 0;
-				}
-			} else
-				consecutiveFixes = 0;
-			out.println(s2);
-		}
+				out.println(s2);
+			}
 		} finally {
-		in.close();
+			in.close();
 		}
+		if (!anyChanges) return;
 		StringReader stringReader = new StringReader(stringWriter.toString());
 		FileWriter outFile = new FileWriter(fileToUpdate);
 		char[] buffer = new char[1000];
 		try {
-		while(true) {
-			int sz = stringReader.read(buffer);
-			if (sz < 0) break;
-			outFile.write(buffer,0,sz);
-		}
+			while (true) {
+				int sz = stringReader.read(buffer);
+				if (sz < 0)
+					break;
+				outFile.write(buffer, 0, sz);
+			}
 		} finally {
-		outFile.close();
+			outFile.close();
 		}
-		
-		
+		System.out.println("Updated " + fileToUpdate);
 	}
 
 }
