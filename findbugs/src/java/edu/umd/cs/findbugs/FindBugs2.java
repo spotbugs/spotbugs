27bcf67e9e7effbedc14d9df3eef23138eeacf6c
David Hovemeyer <david.hovemeyer@gmail.com>
2007-05-17 14:11:41 +0000

    made comment accurate
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@8072 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/FindBugs2.java
+++ findbugs/src/java/edu/umd/cs/findbugs/FindBugs2.java
@@ -117,7 +117,7 @@ public class FindBugs2 implements IFindBugsEngine {
 			}
 		};
 
-		// By default, we do want to scan nested archives
+		// By default, we do not want to scan nested archives
 		this.scanNestedArchives = false;
 	}
 
