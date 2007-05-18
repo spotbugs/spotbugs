73955a10b89c00fcb497487187c33f3a7d820a1f
Bill Pugh <bill.pugh@gmail.com>
2007-05-18 01:31:41 +0000

    add equals/hashCode methods to filters
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@8086 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/filter/MemberMatcher.java
@@ -0,0 +1,68 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2006, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.filter;
+
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+import edu.umd.cs.findbugs.util.Util;
+
+/**
+ * @author pugh
+ */
+public class MemberMatcher {
+
+	protected final NameMatch name;
+
+	@CheckForNull
+	protected final String signature;
+
+	public MemberMatcher(String name) {
+		this.name = new NameMatch(name);
+		this.signature = null;
+	}
+
+	public MemberMatcher(String name, String signature) {
+
+		if (name == null) {
+			if (signature == null)
+				throw new FilterException(this.getClass().getName() + " must have eiter name or signature attributes");
+			else
+				name = "~.*"; // any name
+		}
+
+		this.name = new NameMatch(name);
+		this.signature = signature;
+
+	}
+
+	@Override
+	public int hashCode() {
+		return name.hashCode() + Util.nullSafeHashcode(signature);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == null || this.getClass() != o.getClass())
+			return false;
+
+		MemberMatcher other = (MemberMatcher) o;
+		return name.equals(other.name) && Util.nullSafeEquals(signature, other.signature);
+	}
+
+}
---
+++ findbugs/src/java/edu/umd/cs/findbugs/filter/VersionMatcher.java
@@ -0,0 +1,46 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2006, University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.filter;
+
+/**
+ * @author pugh
+ */
+public class VersionMatcher {
+
+	protected final int version;
+	protected final RelationalOp relOp;
+	
+	@Override
+    public int hashCode() {
+		return version+relOp.hashCode();
+	}
+	@Override
+    public boolean equals(Object o) {
+		if (o == null || this.getClass() != o.getClass()) return false;
+		VersionMatcher m = (VersionMatcher)o;
+		return version == m.version && relOp.equals(m.relOp);
+	}
+
+	public VersionMatcher(int version, RelationalOp relOp) {
+		this.version = version;
+		this.relOp = relOp;
+	}
+
+}
