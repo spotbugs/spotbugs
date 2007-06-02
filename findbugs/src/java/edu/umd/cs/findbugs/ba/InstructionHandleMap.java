7bc99f5bb0fa46e2d8390871d179f4c80bf15d8a
David Hovemeyer <david.hovemeyer@gmail.com>
2007-06-02 01:20:24 +0000

    initial checkin
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@8238 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/InstructionHandleMap.java
@@ -0,0 +1,46 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2003-2007 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.ba;
+
+import org.apache.bcel.generic.InstructionHandle;
+import org.apache.bcel.generic.MethodGen;
+
+/**
+ * Map BCEL InstructionHandles to some kind of value type.
+ * 
+ * @author David Hovemeyer
+ */
+public class InstructionHandleMap<ValueType> {
+	private Object[] map;
+	
+	public InstructionHandleMap(MethodGen methodGen) {
+		map = new Object[methodGen.getMethod().getCode().getLength()];
+	}
+	
+	public ValueType get(InstructionHandle handle) {
+		return (ValueType) map[handle.getPosition()];
+	}
+	
+	public ValueType put(InstructionHandle handle, ValueType value) {
+		ValueType old = get(handle);
+		map[handle.getPosition()] = value;
+		return old;
+	}
+}
