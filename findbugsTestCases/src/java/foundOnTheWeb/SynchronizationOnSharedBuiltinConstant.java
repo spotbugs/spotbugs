a1f0b9fb5a2f62276315502a9f610bf581759870
Bill Pugh <bill.pugh@gmail.com>
2007-06-04 12:53:51 +0000

    added detector for DL_SYNCHRONIZATION_ON_SHARED_CONSTANT
    See http://jira.codehaus.org/browse/JETTY-352
    and /www.javalobby.org/java/forums/m92152131.html
    
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@8246 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/foundOnTheWeb/SynchronizationOnSharedBuiltinConstant.java
@@ -0,0 +1,41 @@
+package foundOnTheWeb;
+
+/**
+ * http://www.javalobby.org/java/forums/t96352.html
+ * 
+Code that is using String literals to synchronize on is dangerous. 
+See the example below: 
+
+<pre>
+class Foo  {
+  static private final String LOCK = "LOCK";
+  void someMethod() {
+    synchronized(LOCK) {
+    ...
+    }
+  }
+}
+</pre>
+
+Why is this dangerous? I have a nice private String which is mine and mine alone? Or is it?  No, it isn't! 
+
+Recall section 3.10.5 of the Java Language Spec 2.0: 
+It says among other things: 
+"Literal strings within different classes in different packages likewise represent references to the same String object." 
+
+For the code above this means that any number of foreign classes can contain the same literal which translates to the same object, hence creating potential dead-lock situations! 
+This is also true for Strings for which you call the intern() method! 
+
+And this is not only a nice theory, something like this really happened between our code and code in the Jetty library. Both sections used the same string literal above to synchronize critical code sections. The two code segments created a dead-lock with very puzzling stack traces  
+(The Jetty-Bug has been reported already, btw, Jetty-352) 
+ *
+ */
+public class SynchronizationOnSharedBuiltinConstant {
+	 static private final String LOCK = "LOCK";
+	  void someMethod() {
+	    synchronized(LOCK) {
+	  
+	    }
+	  }
+	
+}
