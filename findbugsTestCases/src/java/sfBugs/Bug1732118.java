b590d4ad199649a501f99e6a6b7acc2a5a5e2dd6
Bill Pugh <bill.pugh@gmail.com>
2007-06-09 12:06:59 +0000

    add test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@8268 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1732118.java
@@ -0,0 +1,16 @@
+package sfBugs;
+
+public class Bug1732118 {
+	Object filter;
+	
+	public  Object getFilter() {
+		return filter;
+	}
+	public  void setFilter(Object filter) {
+		this.filter = filter;
+	}
+	public synchronized int getHash() {
+		if (filter != null) return filter.hashCode();
+		return 0;
+	}
+}
