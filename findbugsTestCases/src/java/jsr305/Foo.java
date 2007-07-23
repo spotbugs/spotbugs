a573d4b33e7716a4ceb20e292491c030339431c1
Bill Pugh <bill.pugh@gmail.com>
2007-07-23 17:22:34 +0000

    Add Foo qualifier
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@8437 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/jsr305/Foo.java
@@ -0,0 +1,18 @@
+package jsr305;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.annotation.meta.TypeQualifier;
+import javax.annotation.meta.When;
+
+@Documented
+@TypeQualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Foo {
+	When when() default When.ALWAYS;
+	ElementType[] applyTo() default {};
+}
+
