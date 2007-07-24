110beb01d9b739810ce1ea27751ab21fa16d36ad
Bill Pugh <bill.pugh@gmail.com>
2007-07-24 04:47:40 +0000

    handle empty class name correctly
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@8447 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/ba/AnalysisContext.java
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/AnalysisContext.java
@@ -187,7 +187,10 @@ public abstract class AnalysisContext {
 	static public void reportMissingClass(ClassNotFoundException e) {
 		String missing = AbstractBugReporter.getMissingClassName(e);
 		
-		if (missing.length() == 0) AnalysisContext.logError("Empty missing class name", new RuntimeException(e));
+		if (missing.length() == 0) {
+			AnalysisContext.logError("Empty missing class name", new RuntimeException(e));
+			return;
+		}
 		if (missing.charAt(0) == '[') return;
 		if (e == null) throw new NullPointerException("argument is null");
 		AnalysisContext currentAnalysisContext2 = currentAnalysisContext();
