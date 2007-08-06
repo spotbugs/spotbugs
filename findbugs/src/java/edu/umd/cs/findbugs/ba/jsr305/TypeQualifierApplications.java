173a8c14780e48045c48fe383e44b8695b340d2e
David Hovemeyer <david.hovemeyer@gmail.com>
2007-08-06 16:06:08 +0000

    Reformatted, added javadoc comments.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@8584 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/ba/jsr305/TypeQualifierApplications.java
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/jsr305/TypeQualifierApplications.java
@@ -50,6 +50,13 @@ public class TypeQualifierApplications {
 	static DualKeyHashMap<XMethod, Integer, Collection<AnnotationValue>> parameterAnnotations 
 	= new DualKeyHashMap<XMethod, Integer, Collection<AnnotationValue>>();
 	
+	/**
+	 * Get the direct annotations (if any) on given AnnotatedObject.
+	 * 
+	 * @param m an AnnotatedObject
+	 * @return Collection of AnnotationValues representing annotations directly
+	 *         applied to this AnnotatedObject
+	 */
 	 static Collection<AnnotationValue> getDirectAnnotation(AnnotatedObject m) {
 		Collection<AnnotationValue> result = objectAnnotations.get(m);
 		if (result != null) return result;
@@ -59,7 +66,16 @@ public class TypeQualifierApplications {
 		objectAnnotations.put(m, result);
 		return result;
 	}
-	 static Collection<AnnotationValue> getDirectAnnotation(XMethod m, int parameter) {
+	
+	/**
+	 * Get the direct annotations (if any) on given method parameter.
+	 * 
+	 * @param m         a method
+	 * @param parameter a parameter (0 == first parameter)
+	 * @return Collection of AnnotationValues representing annotations directly
+	 *         applied to this parameter
+	 */
+	static Collection<AnnotationValue> getDirectAnnotation(XMethod m, int parameter) {
 		Collection<AnnotationValue> result = parameterAnnotations.get(m, parameter);
 		if (result != null) return result;
 		if (m.getParameterAnnotationDescriptors(parameter).isEmpty()) return Collections.emptyList();
@@ -69,84 +85,173 @@ public class TypeQualifierApplications {
 		return result;
 	}
 	
-	 static void getApplicableApplications(Map<TypeQualifierValue, When> result, XMethod o, int parameter) {
-		 Collection<AnnotationValue> values = getDirectAnnotation(o, parameter);
-		 ElementType e = ElementType.PARAMETER;
-		 for(AnnotationValue v : values) {
-			 Object a = v.getValue("applyTo");
-			 if (a instanceof Object[]) {
-				 for(Object o2 : (Object[]) a) 
-					 if (o2 instanceof EnumValue) { 
-						 EnumValue ev = (EnumValue)o2;
-						 if (ev.desc.getClassName().equals("java/lang/annotation/ElementType") && e.toString().equals(ev.value))
-						    constructTypeQualifierAnnotation(result, v);
-					 }
-			 } else 
-				 constructTypeQualifierAnnotation(result, v);
-		 }
-	}
-	public static void getApplicableApplications(Map<TypeQualifierValue, When> result, AnnotatedObject o, ElementType e) {
-		 Collection<AnnotationValue> values = getDirectAnnotation(o);
-		 for(AnnotationValue v : values) {
-			 Object a = v.getValue("applyTo");
-			 if (a instanceof Object[]) {
-				 for(Object o2 : (Object[]) a) 
-					 if (o2 instanceof EnumValue) { 
-						 EnumValue ev = (EnumValue)o2;
-						 if (ev.desc.getClassName().equals("java/lang/annotation/ElementType") && e.toString().equals(ev.value))
-						    constructTypeQualifierAnnotation(result, v);
-					 }
-			 } else if (o.getElementType().equals(e))
-				 constructTypeQualifierAnnotation(result, v);
-		 }
-	}
 	/**
-     * @param v
-     * @return
-     */
-    private static void constructTypeQualifierAnnotation( Map<TypeQualifierValue, When> map, AnnotationValue v) {
-    	assert map != null;
-    	assert v != null;
-	    EnumValue whenValue = (EnumValue) v.getValue("when");
-	    When when = whenValue == null ? When.ALWAYS : When.valueOf(whenValue.value);
-	    TypeQualifierValue tqv = TypeQualifierValue.getValue(v.getAnnotationClass(), v.getValue("value"));
-	    if (whenValue == null) {
-	    	tqv.setIsStrict();
-	    }
-	    map.put(tqv, when);
-	    if (DEBUG && whenValue == null) {
-	    	System.out.println("When value unspecified for type qualifier value " + tqv);
-	    }
-    }
+	 * Populate a map of TypeQualifierValues to When values representing
+	 * directly-applied type qualifier annotations on given
+	 * method parameter. 
+	 * 
+	 * @param result    Map of TypeQualifierValues to When values
+	 * @param o         a method
+	 * @param parameter a parameter (0 == first parameter)
+	 */
+	static void getApplicableApplications(Map<TypeQualifierValue, When> result, XMethod o, int parameter) {
+		Collection<AnnotationValue> values = getDirectAnnotation(o, parameter);
+		ElementType e = ElementType.PARAMETER;
+		for(AnnotationValue v : values) {
+			Object a = v.getValue("applyTo");
+			if (a instanceof Object[]) {
+				for(Object o2 : (Object[]) a) 
+					if (o2 instanceof EnumValue) { 
+						EnumValue ev = (EnumValue)o2;
+						if (ev.desc.getClassName().equals("java/lang/annotation/ElementType") && e.toString().equals(ev.value))
+							constructTypeQualifierAnnotation(result, v);
+					}
+			} else 
+				constructTypeQualifierAnnotation(result, v);
+		}
+	}
+	
+	/**
+	 * Populate a map of TypeQualifierValues to When values representing
+	 * directly-applied type qualifier annotations on given
+	 * AnnotatedObject. 
+	 * 
+	 * @param result Map of TypeQualifierValues to When values
+	 * @param o      an AnnotatedObject
+	 * @param e      ElementType representing kind of annotated object
+	 */
+	public static void getApplicableApplications(Map<TypeQualifierValue, When> result, AnnotatedObject o, ElementType e) {
+		Collection<AnnotationValue> values = getDirectAnnotation(o);
+		for(AnnotationValue v : values) {
+			Object a = v.getValue("applyTo");
+			if (a instanceof Object[]) {
+				for(Object o2 : (Object[]) a) 
+					if (o2 instanceof EnumValue) { 
+						EnumValue ev = (EnumValue)o2;
+						if (ev.desc.getClassName().equals("java/lang/annotation/ElementType") && e.toString().equals(ev.value))
+							constructTypeQualifierAnnotation(result, v);
+					}
+			} else if (o.getElementType().equals(e))
+				constructTypeQualifierAnnotation(result, v);
+		}
+	}
+	
+	/**
+	 * Resolve a raw AnnotationValue into a TypeQualifierValue/When pair,
+	 * using given map to remember previously resolved instances.
+	 * 
+	 * @param map Map of resolved TypeQualifierValue/When pairs
+	 * @param v   a raw AnnotationValue
+	 */
+	private static void constructTypeQualifierAnnotation(Map<TypeQualifierValue, When> map, AnnotationValue v) {
+		assert map != null;
+		assert v != null;
+		EnumValue whenValue = (EnumValue) v.getValue("when");
+		When when = whenValue == null ? When.ALWAYS : When.valueOf(whenValue.value);
+		TypeQualifierValue tqv = TypeQualifierValue.getValue(v.getAnnotationClass(), v.getValue("value"));
+		if (whenValue == null) {
+			// The TypeQualifierValue does not have an explicit when value.
+			// In this case, the type qualifier requires strict checking.
+			tqv.setIsStrict();
+		}
+		map.put(tqv, when);
+		if (DEBUG && whenValue == null) {
+			System.out.println("When value unspecified for type qualifier value " + tqv);
+		}
+	}
 
-	 static void getApplicableScopedApplications(Map<TypeQualifierValue, When> result, AnnotatedObject o, ElementType e) {
+	/**
+	 * Populate map of TypeQualifierValues to When values
+	 * for given AnnotatedObject,
+	 * taking into account annotations
+	 * applied to outer scopes (e.g., enclosing classes and packages.)
+	 * 
+	 * @param result map of TypeQualifierValues to When values
+	 * @param o      an AnnotatedObject
+	 * @param e      ElementType representing kind of AnnotatedObject
+	 */
+	static void getApplicableScopedApplications(Map<TypeQualifierValue, When> result, AnnotatedObject o, ElementType e) {
 		AnnotatedObject outer = o.getContainingScope();
 		if (outer != null) 
 			getApplicableScopedApplications(result, outer, e);
 		getApplicableApplications(result, o, e);
 	}
-	 static Collection<TypeQualifierAnnotation> getApplicableScopedApplications(AnnotatedObject o, ElementType e) {
+
+	/**
+	 * Get the collection of resolved TypeQualifierAnnotations for
+	 * a given AnnotatedObject,
+	 * taking into account annotations
+	 * applied to outer scopes (e.g., enclosing classes and packages.)
+	 * 
+	 * @param o an AnnotatedObject
+	 * @param e ElementType representing kind of AnnotatedObject
+	 * @return Collection of resolved TypeQualifierAnnotations
+	 */
+	static Collection<TypeQualifierAnnotation> getApplicableScopedApplications(AnnotatedObject o, ElementType e) {
 		Map<TypeQualifierValue, When> result = new HashMap<TypeQualifierValue, When>();
 		getApplicableScopedApplications(result, o, e);
 		return  TypeQualifierAnnotation.getValues(result);
 	}
-		
-	 static void getApplicableScopedApplications(Map<TypeQualifierValue, When> result, XMethod o, int parameter) {
-		 ElementType e = ElementType.PARAMETER;
+	
+	/**
+	 * Populate map of TypeQualifierValues to When values
+	 * for given method parameter,
+	 * taking into account annotations
+	 * applied to outer scopes (e.g., enclosing classes and packages.)
+	 * 
+	 * @param result    map of TypeQualifierValues to When values
+	 * @param o         a method
+	 * @param parameter a parameter (0 == first parameter)
+	 */
+	static void getApplicableScopedApplications(Map<TypeQualifierValue, When> result, XMethod o, int parameter) {
+		ElementType e = ElementType.PARAMETER;
 		getApplicableScopedApplications(result, o, e);
 		getApplicableApplications(result, o, parameter);
 	}
-	 static Collection<TypeQualifierAnnotation> getApplicableScopedApplications(XMethod o, int parameter) {
+
+	/**
+	 * Get the collection of resolved TypeQualifierAnnotations for
+	 * a given parameter,
+	 * taking into account annotations
+	 * applied to outer scopes (e.g., enclosing classes and packages.)
+	 * 
+	 * @param o         a method
+	 * @param parameter a parameter (0 == first parameter)
+	 * @return Collection of resolved TypeQualifierAnnotations
+	 */
+	static Collection<TypeQualifierAnnotation> getApplicableScopedApplications(XMethod o, int parameter) {
 		Map<TypeQualifierValue, When> result = new HashMap<TypeQualifierValue, When>();
 		ElementType e = ElementType.PARAMETER;
 		getApplicableScopedApplications(result, o, e);
 		getApplicableApplications(result, o, parameter);
 		return TypeQualifierAnnotation.getValues(result);
 	}
-	
+
+	/**
+	 * Get the Collection of resolved TypeQualifierAnnotations representing
+	 * directly applied and default (outer scope) type qualifier annotations
+	 * for given AnnotatedObject.
+	 * 
+	 * <p>FIXME: does not properly account for inherited annotations.</p>
+	 * 
+	 * @param o an AnnotatedObject
+	 * @return Collection of TypeQualifierAnnotations applicable to the AnnotatedObject
+	 */
 	public static Collection<TypeQualifierAnnotation> getApplicableApplications(AnnotatedObject o) {
 		return getApplicableScopedApplications(o, o.getElementType());
 	}
+
+	/**
+	 * Get the Collection of resolved TypeQualifierAnnotations representing
+	 * directly applied and default (outer scope) type qualifier annotations
+	 * for given method parameter.
+	 * 
+	 * <p>FIXME: does not properly account for inherited annotations.</p>
+	 * 
+	 * @param o         a method
+	 * @param parameter a parameter (0 == first parameter)
+	 * @return Collection of TypeQualifierAnnotations applicable to the method parameter
+	 */
 	public static Collection<TypeQualifierAnnotation> getApplicableApplications(XMethod o, int parameter) {
 		return getApplicableScopedApplications(o, parameter);
 	}
