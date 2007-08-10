e806a71f63e1d67443be8ab86f0251ec9946bac6
Bill Pugh <bill.pugh@gmail.com>
2007-08-10 15:32:30 +0000

    update syntax
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@8604 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/jsr305/package1/package-info.java
+++ findbugsTestCases/src/java/jsr305/package1/package-info.java
@@ -1,4 +1,4 @@
 // By default, parameters in methods in this package
 // should be Untainted.
-@javax.annotation.Untainted(applyTo=java.lang.annotation.ElementType.PARAMETER)
+@javax.annotation.Untainted(defaultFor=java.lang.annotation.ElementType.PARAMETER)
 package jsr305.package1;
