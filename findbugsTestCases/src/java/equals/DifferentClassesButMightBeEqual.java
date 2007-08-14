50da581b0a90a99ba147b89c0982f9ed5b49cfd9
Bill Pugh <bill.pugh@gmail.com>
2007-08-14 18:32:26 +0000

    Sample test case for false positive: comparing two different classes that inherit the same equals method
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@8618 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/equals/DifferentClassesButMightBeEqual.java
@@ -0,0 +1,24 @@
+package equals;
+
+public class DifferentClassesButMightBeEqual {
+	int value;
+	
+	public int hashCode() {
+		return value;
+	}
+	public boolean equals(Object x) {
+		if (!(x instanceof DifferentClassesButMightBeEqual)) return false;
+		return value == ((DifferentClassesButMightBeEqual)x).value;
+	}
+	
+	static class One extends DifferentClassesButMightBeEqual {};
+	
+	static class Two extends DifferentClassesButMightBeEqual {};
+	
+	public static void foobar() {
+		One one = new One();
+		Two two = new Two();
+		System.out.println(one.equals(two));
+	}
+
+}
