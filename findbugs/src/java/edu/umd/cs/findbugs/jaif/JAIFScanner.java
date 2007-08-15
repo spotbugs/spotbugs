19545c100a3304cf11673b2096d0a5ab74aca344
David Hovemeyer <david.hovemeyer@gmail.com>
2007-08-15 17:46:50 +0000

    More work on lexical scanner for external annotations files.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@8636 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/jaif/JAIFScanner.java
+++ findbugs/src/java/edu/umd/cs/findbugs/jaif/JAIFScanner.java
@@ -33,9 +33,6 @@ import java.util.regex.Pattern;
  */
 public class JAIFScanner {
 	
-	private static final Pattern HORIZ_WHITESPACE = 
-		Pattern.compile("^[ \\t\\r\\f]+");
-
 	static class TokenPattern {
 		private Pattern pattern;
 		private JAIFTokenKind kind;
@@ -54,16 +51,49 @@ public class JAIFScanner {
 		}
 	}
 
+	// See http://java.sun.com/docs/books/jls/third_edition/html/lexical.html
+	// Hexidecimal floating-point literals are not implemented.
+
 	private static final String ID_START = "[@A-Za-z_\\$]";
 	private static final String ID_REST  = "[A-Za-z0-9_\\$]";
+	private static final String DIGIT = "[0-9]";
+	private static final String DIGITS = DIGIT + "+";
+	private static final String DIGITS_OPT = DIGIT + "*";
+	private static final String SIGN_OPT = "[+-]?";
+	private static final String DOT = "\\.";
+	private static final String EXP_PART = "([Ee]" + SIGN_OPT + DIGITS + ")";
+	private static final String EXP_PART_OPT = EXP_PART + "?";
+	private static final String FLOAT_TYPE_SUFFIX = "[FfDd]";
+	private static final String FLOAT_TYPE_SUFFIX_OPT = FLOAT_TYPE_SUFFIX + "?";
+	private static final String OCTAL_DIGITS = "[0-7]+";
+	private static final String HEX_SIGNIFIER = "0[Xx]";
+	private static final String HEX_DIGITS = "[0-9A-Fa-f]+";
+	private static final String INT_TYPE_SUFFIX_OPT = "[Ll]?";
 	
 	private static final TokenPattern[] TOKEN_PATTERNS = {
+		// Misc. syntax
 		new TokenPattern(":", JAIFTokenKind.COLON),
 		new TokenPattern("\\(", JAIFTokenKind.LPAREN),
 		new TokenPattern("\\)", JAIFTokenKind.RPAREN),
 		new TokenPattern(",", JAIFTokenKind.COMMA),
 		new TokenPattern("=", JAIFTokenKind.EQUALS),
-		new TokenPattern(ID_START + "(" + ID_REST + "*)", JAIFTokenKind.IDENTIFIER_OR_KEYWORD),
+		
+		// Identifiers and keywords
+		new TokenPattern(ID_START + "(" + ID_REST + ")*", JAIFTokenKind.IDENTIFIER_OR_KEYWORD),
+		
+		// FP literals
+		new TokenPattern(DIGITS + DOT + DIGITS_OPT + EXP_PART_OPT + FLOAT_TYPE_SUFFIX_OPT, JAIFTokenKind.FLOATING_POINT_LITERAL),
+		new TokenPattern(DOT + DIGITS + EXP_PART_OPT + FLOAT_TYPE_SUFFIX_OPT, JAIFTokenKind.FLOATING_POINT_LITERAL),
+		new TokenPattern(DIGITS + EXP_PART + FLOAT_TYPE_SUFFIX_OPT, JAIFTokenKind.FLOATING_POINT_LITERAL),
+		new TokenPattern(DIGITS + EXP_PART_OPT + FLOAT_TYPE_SUFFIX, JAIFTokenKind.FLOATING_POINT_LITERAL),
+		
+		// This must come after the FP literal patterns
+		new TokenPattern(DOT, JAIFTokenKind.DOT),
+		
+		// Integer literals
+		new TokenPattern("0" + OCTAL_DIGITS + INT_TYPE_SUFFIX_OPT, JAIFTokenKind.OCTAL_LITERAL),
+		new TokenPattern(HEX_SIGNIFIER + HEX_DIGITS + INT_TYPE_SUFFIX_OPT, JAIFTokenKind.HEX_LITERAL),
+		new TokenPattern(DIGITS + INT_TYPE_SUFFIX_OPT, JAIFTokenKind.DECIMAL_LITERAL),
 	};
 	
 	private BufferedReader reader;
@@ -76,6 +106,7 @@ public class JAIFScanner {
 	 */
 	public JAIFScanner(Reader reader) {
 		this.reader = new BufferedReader(reader);
+		this.lineNum = 0;
 	}
 
 	public int getLineNumber() {
@@ -106,8 +137,15 @@ public class JAIFScanner {
 	private void fillLineBuf() throws IOException {
 	    if (lineBuf == null) {
 			lineBuf = reader.readLine();
+			if (lineBuf != null) {
+				++lineNum;
+			}
 		}
     }
+	
+	private boolean isHorizWhitespace(char c) {
+		return c == ' ' || c == '\t';
+	}
 	
 	private void fetchToken() throws IOException, JAIFSyntaxException {
 		assert next == null;
@@ -116,11 +154,16 @@ public class JAIFScanner {
 		if (lineBuf == null) {
 			throw new JAIFSyntaxException(this, "Unexpected end of file");
 		}
-		
-		Matcher wsMatch = HORIZ_WHITESPACE.matcher(lineBuf);
-		if (wsMatch.find()) {
-			lineBuf = lineBuf.substring(wsMatch.group().length());
+
+		// Strip leading whitespace, if any
+		int wsCount = 0;
+		while (wsCount < lineBuf.length() && isHorizWhitespace(lineBuf.charAt(wsCount))) {
+			wsCount++;
 		}
+		if (wsCount > 0) {
+			lineBuf = lineBuf.substring(wsCount);
+		}
+		//System.out.println("Consumed " + wsCount + " characters of horizontal whitespace");
 		
 		if (lineBuf.equals("")) {
 			// Reached end of line.
--- findbugs/src/junit/edu/umd/cs/findbugs/jaif/JAIFScannerTest.java
+++ findbugs/src/junit/edu/umd/cs/findbugs/jaif/JAIFScannerTest.java
@@ -24,59 +24,111 @@ import java.io.StringReader;
 import junit.framework.TestCase;
 
 public class JAIFScannerTest extends TestCase {
-	
+
 	private JAIFScanner getScanner(String text) {
 		return new JAIFScanner(new StringReader(text));
 	}
-	
+
 	private void checkToken(JAIFScanner scanner, String lexeme, JAIFTokenKind kind) throws Exception {
 		JAIFToken t = scanner.nextToken();
-		
+
 		assertEquals(lexeme, t.lexeme);
 		assertEquals(kind, t.kind);
 	}
-	
+
 	public void testScanColon() throws Exception {
 		JAIFScanner scanner = getScanner(":");
 		checkToken(scanner, ":", JAIFTokenKind.COLON);
 		checkToken(scanner, "\n", JAIFTokenKind.NEWLINE);
-    }
-	
+	}
+
 	public void testScanParens() throws Exception {
 		JAIFScanner scanner = getScanner("()");
 
 		checkToken(scanner, "(", JAIFTokenKind.LPAREN);
 		checkToken(scanner, ")", JAIFTokenKind.RPAREN);
 		checkToken(scanner, "\n", JAIFTokenKind.NEWLINE);
-    }
-	
+	}
+
 	public void testScanComma() throws Exception {
-	    JAIFScanner scanner = getScanner(",");
-	    
-	    checkToken(scanner, ",", JAIFTokenKind.COMMA);
+		JAIFScanner scanner = getScanner(",");
+
+		checkToken(scanner, ",", JAIFTokenKind.COMMA);
 		checkToken(scanner, "\n", JAIFTokenKind.NEWLINE);
-    }
-	
+	}
+
 	public void testScanEquals() throws Exception {
 		JAIFScanner scanner = getScanner("=");
-		
+
 		checkToken(scanner, "=", JAIFTokenKind.EQUALS);
 		checkToken(scanner, "\n", JAIFTokenKind.NEWLINE);
-    }
+	}
 
 	public void testScanIdentifier() throws Exception {
-	    JAIFScanner scanner = getScanner("@foobar Baz123   ( Boing Boing) @Yum@Yum __123  $plotz");
-	    
-	    checkToken(scanner, "@foobar", JAIFTokenKind.IDENTIFIER_OR_KEYWORD);
-	    checkToken(scanner, "Baz123", JAIFTokenKind.IDENTIFIER_OR_KEYWORD);
-	    checkToken(scanner, "(", JAIFTokenKind.LPAREN);
-	    checkToken(scanner, "Boing", JAIFTokenKind.IDENTIFIER_OR_KEYWORD);
-	    checkToken(scanner, "Boing", JAIFTokenKind.IDENTIFIER_OR_KEYWORD);
-	    checkToken(scanner, ")", JAIFTokenKind.RPAREN);
-	    checkToken(scanner, "@Yum", JAIFTokenKind.IDENTIFIER_OR_KEYWORD);
-	    checkToken(scanner, "@Yum", JAIFTokenKind.IDENTIFIER_OR_KEYWORD);
-	    checkToken(scanner, "__123", JAIFTokenKind.IDENTIFIER_OR_KEYWORD);
-	    checkToken(scanner, "$plotz", JAIFTokenKind.IDENTIFIER_OR_KEYWORD);
+		JAIFScanner scanner = getScanner("  \t  \t\t@foobar Baz123   ( Boing Boing) @Yum@Yum __123  $plotz");
+
+		checkToken(scanner, "@foobar", JAIFTokenKind.IDENTIFIER_OR_KEYWORD);
+		checkToken(scanner, "Baz123", JAIFTokenKind.IDENTIFIER_OR_KEYWORD);
+		checkToken(scanner, "(", JAIFTokenKind.LPAREN);
+		checkToken(scanner, "Boing", JAIFTokenKind.IDENTIFIER_OR_KEYWORD);
+		checkToken(scanner, "Boing", JAIFTokenKind.IDENTIFIER_OR_KEYWORD);
+		checkToken(scanner, ")", JAIFTokenKind.RPAREN);
+		checkToken(scanner, "@Yum", JAIFTokenKind.IDENTIFIER_OR_KEYWORD);
+		checkToken(scanner, "@Yum", JAIFTokenKind.IDENTIFIER_OR_KEYWORD);
+		checkToken(scanner, "__123", JAIFTokenKind.IDENTIFIER_OR_KEYWORD);
+		checkToken(scanner, "$plotz", JAIFTokenKind.IDENTIFIER_OR_KEYWORD);
 		checkToken(scanner, "\n", JAIFTokenKind.NEWLINE);
-    }
+	}
+
+	public void testScanFloatingPointLiteral() throws Exception {
+		JAIFScanner scanner = getScanner("1e1f    2.f     .3f     0f      3.14f   6.022137e+23f");
+
+		checkToken(scanner, "1e1f", JAIFTokenKind.FLOATING_POINT_LITERAL);
+		checkToken(scanner, "2.f", JAIFTokenKind.FLOATING_POINT_LITERAL);
+		checkToken(scanner, ".3f", JAIFTokenKind.FLOATING_POINT_LITERAL);
+		checkToken(scanner, "0f", JAIFTokenKind.FLOATING_POINT_LITERAL);
+		checkToken(scanner, "3.14f", JAIFTokenKind.FLOATING_POINT_LITERAL);
+		checkToken(scanner, "6.022137e+23f", JAIFTokenKind.FLOATING_POINT_LITERAL);
+		checkToken(scanner, "\n", JAIFTokenKind.NEWLINE);
+	}
+
+	public void testScanFloatingPointLiteral2() throws Exception {
+		JAIFScanner scanner = getScanner("1e1     2.      .3      0.0     3.14    1e-9d   1e137");
+
+		checkToken(scanner, "1e1", JAIFTokenKind.FLOATING_POINT_LITERAL);
+		checkToken(scanner, "2.", JAIFTokenKind.FLOATING_POINT_LITERAL);
+		checkToken(scanner, ".3", JAIFTokenKind.FLOATING_POINT_LITERAL);
+		checkToken(scanner, "0.0", JAIFTokenKind.FLOATING_POINT_LITERAL);
+		checkToken(scanner, "3.14", JAIFTokenKind.FLOATING_POINT_LITERAL);
+		checkToken(scanner, "1e-9d", JAIFTokenKind.FLOATING_POINT_LITERAL);
+		checkToken(scanner, "1e137", JAIFTokenKind.FLOATING_POINT_LITERAL);
+		checkToken(scanner, "\n", JAIFTokenKind.NEWLINE);
+	}
+
+	public void testScanOctalLiteral() throws Exception {
+		JAIFScanner scanner = getScanner("0237   01575L  027365l");
+
+		checkToken(scanner, "0237", JAIFTokenKind.OCTAL_LITERAL);
+		checkToken(scanner, "01575L", JAIFTokenKind.OCTAL_LITERAL);
+		checkToken(scanner, "027365l", JAIFTokenKind.OCTAL_LITERAL);
+		checkToken(scanner, "\n", JAIFTokenKind.NEWLINE);
+	}
+
+	public void testScanHexLiteral() throws Exception {
+		JAIFScanner scanner = getScanner("0xDEADbeef   0xcafeBabeL   0X123EEfl");
+
+		checkToken(scanner, "0xDEADbeef", JAIFTokenKind.HEX_LITERAL);
+		checkToken(scanner, "0xcafeBabeL", JAIFTokenKind.HEX_LITERAL);
+		checkToken(scanner, "0X123EEfl", JAIFTokenKind.HEX_LITERAL);
+		checkToken(scanner, "\n", JAIFTokenKind.NEWLINE);
+	}
+
+	public void testScanDecimalLiteral() throws Exception {
+		JAIFScanner scanner = getScanner("1234     5678L    91919191l");
+		
+		checkToken(scanner, "1234", JAIFTokenKind.DECIMAL_LITERAL);
+		checkToken(scanner, "5678L", JAIFTokenKind.DECIMAL_LITERAL);
+		checkToken(scanner, "91919191l", JAIFTokenKind.DECIMAL_LITERAL);
+		checkToken(scanner, "\n", JAIFTokenKind.NEWLINE);
+	}
 }
