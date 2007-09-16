f48d35bf6d0b1b6bef73cd856c58e827f7e8327e
Bill Pugh <bill.pugh@gmail.com>
2007-09-16 23:36:31 +0000

    add test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@8841 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/nullnessAnnotations/UseAnnotations.java
@@ -0,0 +1,49 @@
+package nullnessAnnotations;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+
+public class UseAnnotations {
+	
+	@CheckForNull Object f(int x) {
+		if (x == 0) return null;
+		return (Integer) x;
+	}
+	
+	int g(@Nonnull Object x) {
+		return 42;
+	}
+	int foo() {
+		return g(f(12));  // should generate a warning here (but don't)
+	}
+	int foo2() {
+		return g(null);  // should and do generate a warning here
+	}
+	int foo3(int x) {
+		Object y = null;
+		if (x > 0) y = "";
+		return g(y);  // should generate a warning here (but don't)
+	}
+	int foo4() {
+		return f(12).hashCode();  // should generate a warning here (but don't)
+	}
+
+	int bar() {
+		return f(12).hashCode();  // should generate a warning here (but don't)
+	}
+	int bar2() {
+		Object x = null;
+		return x.hashCode();  // should and do generate a warning here
+	}
+	int bar3(int x) {
+		Object y = null;
+		if (x > 0) y = "";
+		return y.hashCode();  // should generate a warning here (but don't)
+	}
+	int bar4(int x) {
+		Object y = null;
+		if (x > 0) y = "";
+		return y.hashCode();  // should generate a warning here (but don't)
+	}
+
+}
