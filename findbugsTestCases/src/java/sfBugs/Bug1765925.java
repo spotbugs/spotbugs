db16f07e843f229f9bcfb8589ebc7c85c6bfe7a6
Benjamin Langmead <ben.langmead@gmail.com>
2007-09-20 04:15:34 +0000

    git-svn-id: https://findbugs.googlecode.com/svn/trunk@8869 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1765925.java
@@ -0,0 +1,29 @@
+package sfBugs;
+
+import java.io.File;
+
+/*
+ * Submitted By: Alfred Nathaniel
+ * Summary:
+ * 
+ * Findbugs should ignore PZLA_PREFER_ZERO_LENGTH_ARRAYS in all methods named
+ * "listFiles" since returning null is the convention established by
+ * java.io.File.
+ */
+public class Bug1765925 {
+    private File file;
+
+    Bug1765925(File file) {
+        this.file = file;
+    }
+
+    //// grep -A 1 PZLA_PREFER_ZERO_LENGTH_ARRAYS | grep Bug1765925
+    File[] listFiles() {
+        File[] files = file.listFiles();
+        if (files == null)
+            return null;
+        else {
+            return new File[] {};
+        }
+    }
+}
