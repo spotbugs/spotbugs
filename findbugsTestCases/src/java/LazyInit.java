b1d4bcfad04db05effb90c8970f0cc4a67641937
Bill Pugh <bill.pugh@gmail.com>
2007-10-12 17:07:26 +0000

    Add more test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@8942 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/LazyInit.java
+++ findbugsTestCases/src/java/LazyInit.java
@@ -3,6 +3,23 @@ public class LazyInit {
 
 	static volatile Object bar;
 
+	
+	static String [] x,y;
+
+	public static String[] getX() {
+		if (x != null) {
+			x = new String[5];
+			for(int i = 0; i < x.length; i++)
+				x[i] = Integer.toString(i);
+		}
+		return x;
+	}
+	public static  String[] getY() {
+		if (y != null) {
+			y = new String[] {"a", "b", "c", "d", "e"};
+		}
+		return y;
+	}
 	// This should be reported
 	public static Object getFoo() {
 		if (foo == null)
