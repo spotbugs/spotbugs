93064cd6e47ab26aef2e33afb24e7c700d1e93e7
Benjamin Langmead <ben.langmead@gmail.com>
2007-10-17 20:43:28 +0000

    Expanded test case for sourceforge bug Bug1779315 to include calls to the java.io.File methods that were requested for check-return-value
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9005 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug1779315.java
+++ findbugsTestCases/src/java/sfBugs/Bug1779315.java
@@ -1,5 +1,7 @@
 package sfBugs;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
@@ -7,4 +9,17 @@ public class Bug1779315 {
 	private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
 	private static final DateFormat alternativeDateFormat = new SimpleDateFormat("yyyy-MM-dd-HH.mm.ss");
 
+	public void testFilesMethods() throws IOException {
+	    File f = new File("blah.txt");
+        File f2 = new File("blah2.txt");
+        
+        // All of these should generate a warning
+	    f.mkdir();
+        f.mkdirs();
+        f.delete();
+        f.createNewFile();
+        f.setLastModified(1L);
+        f.setReadOnly();
+        f.renameTo(f2);
+	}
 }
