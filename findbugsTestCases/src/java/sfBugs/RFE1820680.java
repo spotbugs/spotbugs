1f84b988381c9bf1c8bfc66d36cbe452322082b4
Bill Pugh <bill.pugh@gmail.com>
2007-10-26 13:59:03 +0000

    add rfe
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9047 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/RFE1820680.java
@@ -0,0 +1,38 @@
+package sfBugs;
+
+public class RFE1820680 {
+
+	static class Array1 {
+		Array2[] getArray2() {
+			return null;
+		}
+	}
+
+	static class Array2 {
+	}
+
+	public void test() {
+		Array1[] array1 = getArray1();
+		for (int i = 0; i < array1.length; i++) {
+			System.out.println("array 1:" + array1[i]);
+			Array2[] array2 = array1[i].getArray2();
+			for (int j = 0; j < array2.length; j++) {
+				System.out.println("array 2:" + array2[i]); // should be j
+			}
+		}
+	}
+
+	private Array1[] getArray1() {
+		return null;
+	}
+
+	void te() {
+		String str = "Pushpendra";
+		for (int i = 0; i < str.length(); i++) {
+			String str1 = "Japan";
+			for (int j = 0; j < str1.length(); j++) {
+				System.out.println(i);
+			}
+		}
+	}
+}
