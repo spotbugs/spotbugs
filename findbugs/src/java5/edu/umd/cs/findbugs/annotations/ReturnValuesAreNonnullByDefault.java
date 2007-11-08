2944606f76de5cab05fe54d8cfc447900174db38
Bill Pugh <bill.pugh@gmail.com>
2007-11-08 15:57:54 +0000

    add annotation
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9084 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java5/edu/umd/cs/findbugs/annotations/ReturnValuesAreNonnullByDefault.java
@@ -0,0 +1,47 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2003-2007 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.annotations;
+
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.annotation.Nonnull;
+import javax.annotation.meta.TypeQualifierDefault;
+
+/**
+ * This annotation can be applied to a package, class or method to indicate that the methods 
+ * in that element have nonnull return values by default unless there is:
+ * <ul>
+ * <li>An explicit nullness annotation
+ * <li>The method overrides a method in a superclass (in which case the annotation of the corresponding
+ * parameter in the superclass applies)
+ * <li> there is a default annotation applied to a more tightly nested element.
+ * </ul>
+ *
+ */
+@Documented
+@Nonnull
+@TypeQualifierDefault(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ReturnValuesAreNonnullByDefault {
+}
