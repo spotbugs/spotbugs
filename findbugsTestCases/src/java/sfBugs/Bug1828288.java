2851fc4799b2bab2f98759e1f1bc30a8b7622dc5
Bill Pugh <bill.pugh@gmail.com>
2007-11-13 21:30:56 +0000

    simplify test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9091 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug1828288.java
+++ findbugsTestCases/src/java/sfBugs/Bug1828288.java
@@ -5,25 +5,11 @@ import edu.umd.cs.findbugs.annotations.CheckForNull;
 public class Bug1828288 {
 
 	 @CheckForNull
-	 private Object field = null;
-
-	 public void init() {
-	   field = new Object();
-	 }
-	 @CheckForNull
-	 public Object nullx() {
-		 return null;
-	 }
+	 public Object field;
 
 	 @Override
 	 public String toString() {
 	   return field.toString(); // (*)
 	 }
-	
-	 
-	 public String toString2() {
-	   return nullx().toString(); // (*)
-	 }
-
 
 }
