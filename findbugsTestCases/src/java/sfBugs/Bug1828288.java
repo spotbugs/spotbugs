d7ec8d0b1a9c6624d83fc6367cfd36e7269a48e6
Bill Pugh <bill.pugh@gmail.com>
2007-11-13 20:43:17 +0000

    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9090 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug1828288.java
+++ findbugsTestCases/src/java/sfBugs/Bug1828288.java
@@ -10,10 +10,19 @@ public class Bug1828288 {
 	 public void init() {
 	   field = new Object();
 	 }
+	 @CheckForNull
+	 public Object nullx() {
+		 return null;
+	 }
 
 	 @Override
 	 public String toString() {
 	   return field.toString(); // (*)
+	 }
+	
+	 
+	 public String toString2() {
+	   return nullx().toString(); // (*)
 	 }
 
 
