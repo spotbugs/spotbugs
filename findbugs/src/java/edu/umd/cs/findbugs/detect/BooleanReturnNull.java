230b066b3dff094e6617055a11ab694a0cbb0a27
Bill Pugh <bill.pugh@gmail.com>
2007-12-04 18:56:43 +0000

    added new detector that looks for methods with a return type of Boolean that return explicit null values.  From Allison
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9128 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/BooleanReturnNull.java
@@ -0,0 +1,70 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2003-2007 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.detect;
+
+import edu.umd.cs.findbugs.*;
+import edu.umd.cs.findbugs.ba.AnalysisContext;
+import edu.umd.cs.findbugs.ba.SignatureParser;
+import edu.umd.cs.findbugs.bcel.OpcodeStackDetector;
+
+import org.apache.bcel.Repository;
+import org.apache.bcel.classfile.*;
+
+/**
+ * @author alison
+ */
+public class BooleanReturnNull extends OpcodeStackDetector {
+	
+	BugReporter bugReporter;
+
+	public BooleanReturnNull(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
+
+	@Override
+	public void visit(Code code) {
+		String s = getMethodSig();
+		SignatureParser sp = new SignatureParser(s);
+		//Check to see if the method has Boolean return type
+		boolean interesting = "Ljava/lang/Boolean;".equals(sp.getReturnTypeSignature());
+		if (interesting)  {
+			super.visit(code); // make callbacks to sawOpcode for all opcodes
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see edu.umd.cs.findbugs.bcel.OpcodeStackDetector#sawOpcode(int)
+	 */
+	@Override
+	public void sawOpcode(int seen) {
+		if (seen != ARETURN) {
+			return;
+		}
+		else{
+			if(getPrevOpcode(1) == ACONST_NULL )
+				bugReporter.reportBug(new BugInstance(this, "BooleanReturnNull", NORMAL_PRIORITY)
+					.addClassAndMethod(this)
+					.addSourceLine(this));
+		}
+
+	}
+	
+
+}
