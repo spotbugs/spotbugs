6f8c27d031751990988cb680c51c30a404dafe9d
Benjamin Langmead <ben.langmead@gmail.com>
2007-12-05 02:21:02 +0000

    Added %n-checking case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9134 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/FormatString.java
+++ findbugsTestCases/src/java/FormatString.java
@@ -2,13 +2,14 @@
 public class FormatString {
     public static void main(String args[]) {
         java.util.Formatter f = new java.util.Formatter();
-        System.out.printf("%d", 7);
-        System.out.format("%d", 8);
-        System.out.println(String.format("%d %s", 8, "boy"));
-        System.out.println(f.format("%d %s %f %d %f %d", 8, "boy", 7.7, 9, 9.9, 10));
-        System.out.println(f.format("%d %s %f %d %f", 8, "boy", 7.7, 9, 9.9, 10));
-        System.out.println(f.format("%d %s %f %d %f %d", 8, "boy", 7.7, 9, 9.9, 10, 11));
-        System.out.println(f.format("%d %%%%%s %% %f %d %%%f %%%d", 8, "boy", 7.7, 9, 9.9, 10));
+        System.out.printf("%d", 7); // no warning
+        System.out.format("%d", 8); // no warning
+        System.out.println(String.format("%d %s", 8, "boy")); // no warning
+        System.out.println(f.format("%d %s %f %d %f %d", 8, "boy", 7.7, 9, 9.9, 10)); // no warning
+        System.out.println(f.format("%d %s %f %d %f",    8, "boy", 7.7, 9, 9.9, 10)); // warning!
+        System.out.println(f.format("%d %s %f %d %f %d", 8, "boy", 7.7, 9, 9.9, 10, 11)); // warning!
+        System.out.println(f.format("%d %%%%%s %% %f %d %%%f %%%d", 8, "boy", 7.7, 9, 9.9, 10)); // no warning
+        System.out.println(String.format("%d %n", 1)); // no warning (%n does not consume an argument)
         varargsMethod((Object[])args);
     }
     
