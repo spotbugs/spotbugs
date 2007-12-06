249e393b68642e069fbabbef7c41eef5ba7253ec
Alison Teoh <alison.lk.teoh@gmail.com>
2007-12-06 17:41:54 +0000

    Added test case for detector to find methods of return type Boolean that may return null.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9137 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/BooleanReturnsNull.java
@@ -0,0 +1,33 @@
+public class BooleanReturnsNull {
+
+	
+	public Boolean always_null(){
+		return null;
+	}
+	
+	public Boolean sometimes_null(int n){
+		if (n > 3){
+			return new Boolean(true);
+		}
+		else if(n < 1){
+			return new Boolean(false);
+		}
+		else
+			return null;
+		}
+			
+	}
+	
+	public Boolean never_null(int n){
+		if (n>2){
+			return new Boolean(true);
+		}
+		else{
+			return new Boolean(false);
+		}
+	}
+	
+	public static void main(String[] args){
+		//nothing!!
+	}
+}
