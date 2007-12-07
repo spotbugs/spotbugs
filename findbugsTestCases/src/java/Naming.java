0f2ee9c3139190e1a016e4cbaa7af3c00d447746
Benjamin Langmead <ben.langmead@gmail.com>
2007-12-07 05:29:29 +0000

    Test case for solution to sourceforge bug 1840245:
    http://sourceforge.net/tracker/index.php?func=detail&aid=1840245&group_id=96405&atid=614693
    
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9141 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/Naming.java
@@ -0,0 +1,33 @@
+
+public class Naming {
+	public static class NamingException {
+		// Should produce a warning
+	}
+	
+	public static class NamingBaseException extends Exception {
+		// Should not produce a warning
+        private static final long serialVersionUID = 1L;
+	}
+	
+	public static class NamingBaseChildException extends NamingBaseException {
+		// Should not produce a warning
+        private static final long serialVersionUID = 2L;
+	}
+	
+	public static class TrickyName extends NamingBaseChildException {
+		// Not a candidate - name doesn't imply it's an exception
+        private static final long serialVersionUID = 3L;
+	}
+	
+	public static class FinalException extends TrickyName {
+		// Should not produce a warning
+        private static final long serialVersionUID = 4L;
+	}
+	
+	public static void main(String args[]) throws FinalException {
+		boolean blah = true;
+		if(blah) {
+			throw new FinalException();
+		}
+	}
+}
