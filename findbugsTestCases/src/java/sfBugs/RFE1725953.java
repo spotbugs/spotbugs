6be3235978f556cbb267d6db2d36440672ff5ceb
Benjamin Langmead <ben.langmead@gmail.com>
2007-12-18 23:05:32 +0000

    Test for RFE1725953 - shows that we can't reconstruct programmer intention when they misuse xor in this particular way
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9163 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/RFE1725953.java
@@ -0,0 +1,8 @@
+package sfBugs;
+
+public class RFE1725953 {
+	public static void main(String args[]) {
+		int twoToThe16 = 2 ^ 16;
+		System.out.println("2 ^ 16 = " + twoToThe16); // bytecode just has 18
+	}
+}
