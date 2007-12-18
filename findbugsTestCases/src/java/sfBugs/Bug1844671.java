ed9629ef83f248fc704521b34ce12077de5745cd
Benjamin Langmead <ben.langmead@gmail.com>
2007-12-18 21:31:13 +0000

    Added a case with a ref/ref compare where neither ref is null.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9160 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug1844671.java
+++ findbugsTestCases/src/java/sfBugs/Bug1844671.java
@@ -10,6 +10,7 @@ public class Bug1844671 {
 	       try {
 	         fw = new FileWriter(new File(""));
 	       } catch(IOException e) {
+	    	   System.out.println(e);
 	       } finally {
 	           try {
 	               if( fw != null) { // no false positive
@@ -18,15 +19,35 @@ public class Bug1844671 {
 	           } catch(IOException ioe) { }
 	       }
 	   }
+	 
 	 public void falsePositive2(){
 	       FileWriter fw = null;
 	       try {
 	         fw = new FileWriter(new File(""));
 	       } catch(IOException e) {
+	    	   System.out.println(e);
 	       } finally {
 	           try {
 	               if( null != fw) { // false positive
 	                   fw.close();
+	               }
+	           } catch(IOException ioe) { }
+	       }
+	   }
+
+	 public void falsePositive3(){
+	       FileWriter tmp = null;
+	       FileWriter fw = null;
+	       try {
+	    	 tmp = new FileWriter(new File("")); // true positive
+	         fw = new FileWriter(new File(""));  // true positive
+	       } catch(IOException e) {
+	    	   System.out.println(e);
+	       } finally {
+	           try {
+	               if( tmp != fw) {
+	                   if(fw != null) fw.close();
+	                   tmp.close();
 	               }
 	           } catch(IOException ioe) { }
 	       }
