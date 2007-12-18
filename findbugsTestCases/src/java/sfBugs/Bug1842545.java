371f6f9527501ee2bca796189fcc449163ed56d1
Benjamin Langmead <ben.langmead@gmail.com>
2007-12-18 22:22:39 +0000

    Test case for sourceforge Bug1842545:
    http://sourceforge.net/tracker/index.php?func=detail&aid=1842545&group_id=96405&atid=614693
    
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9162 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1842545.java
@@ -0,0 +1,22 @@
+package sfBugs;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Bug1842545 {
+	
+	private static Connection m_connection;
+	
+	public static void main(String args[]) {
+		Connection c = m_connection; // should trigger warning
+	}
+	
+	boolean hasConnection() {
+		try {
+			return (m_connection != null) && !m_connection.isClosed();
+		} catch (SQLException ex) // should not trigger warning (?)
+		{
+			return false;
+		}
+	}
+}
