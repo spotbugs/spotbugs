65123f29473dcedfecbbef541862d623e9251d6e
Benjamin Langmead <ben.langmead@gmail.com>
2007-12-19 03:04:08 +0000

    Example from feature request 1627291 on sourceforge:
    http://sourceforge.net/tracker/index.php?func=detail&aid=1627291&group_id=96405&atid=614696
    
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9167 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/RFE1627291.java
@@ -0,0 +1,30 @@
+package sfBugs;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class RFE1627291 {
+	public void sample() {
+		InputStream inputStream = null;
+		BufferedInputStream bufferedInputStream = null;
+
+		try {
+			inputStream = new FileInputStream(new File("x"));
+			bufferedInputStream = new BufferedInputStream(inputStream);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			if (bufferedInputStream != null) {
+				try {
+					bufferedInputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
