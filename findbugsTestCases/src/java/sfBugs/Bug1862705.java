f76a843e0e6459df9150e83a44371ba96ffdfc30
Bill Pugh <bill.pugh@gmail.com>
2008-01-04 16:15:47 +0000

    add test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9197 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1862705.java
@@ -0,0 +1,60 @@
+package sfBugs;
+
+
+
+import java.io.IOException;
+
+import java.io.ObjectInputStream;
+
+import java.io.ObjectOutputStream;
+
+import java.io.Serializable;
+
+
+
+public class Bug1862705 implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private void readObject(ObjectInputStream s) throws IOException, ClassNotFoundException {
+
+        s.defaultReadObject();
+
+    }
+
+    private void writeObject(ObjectOutputStream s) throws IOException {
+
+        s.defaultWriteObject();
+
+    }
+
+    public static class Bar extends Bug1862705 {
+
+        private static final long serialVersionUID = 1L;
+
+        private transient String s = null;
+
+        public Bar(String ss) {
+
+            s = ss;
+
+        }
+
+        public String getS() {
+
+            return s;
+
+        }
+
+        public void setS(String ss) {
+
+            s = ss;
+
+        }
+
+    }
+
+}
+
+
+
