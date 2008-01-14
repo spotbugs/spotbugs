5de9139737570debe5e6fd60d1f35baf1a5537f6
Bill Pugh <bill.pugh@gmail.com>
2008-01-14 21:23:01 +0000

    hile creating an introductory FindBugs presentation for my team, I created the attached class that deliberately has lots of bugs.  But three of my intentional bugs were not detected by FindBugs v1.3.1 (Windows XP SP2, Java 1.5.0_12 inside Eclipse 3.3 with the FindBugs 1.3.1_20071223 plugin).  I ran at Low severity with all detector categories enabled.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9245 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1871376.java
@@ -0,0 +1,150 @@
+package sfBugs;
+
+
+
+import java.io.File;
+
+import java.io.Serializable;
+
+import java.util.AbstractMap;
+
+import java.util.Date;
+
+import java.util.HashMap;
+
+import java.util.Map;
+
+import java.util.Set;
+
+
+
+import net.jcip.annotations.GuardedBy;
+
+import net.jcip.annotations.ThreadSafe;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+
+
+
+@ThreadSafe
+
+public class Bug1871376 implements Serializable {
+
+	private static final long serialVersionUID = new Long(1L);
+
+
+
+	@GuardedBy("Bug1871376.class")
+
+	private static boolean s_builtAMap = false;
+
+
+
+	private final Date m_created = new Date();
+
+	@GuardedBy("itself")
+
+	private NonSerializableMap m_map = null;
+
+	
+
+	public Bug1871376() {
+
+		boolean done = false;
+
+		if (!done)
+
+			done = true;
+
+		
+
+		new File("temp.txt").delete();
+
+	}
+
+	
+
+	public Date getCreationDate() {
+
+		return m_created;
+
+	}
+
+
+
+	public String getFirstCharOfKey(@NonNull String key) {
+
+		String value = null;
+
+		if (null == m_map) {
+
+			synchronized (this) {
+
+				if (null == m_map) {
+
+					System.out.println("Value: " + m_map.get(key));
+
+					m_map = new NonSerializableMap();
+
+					value = m_map.get(key);
+
+					s_builtAMap = true;
+
+				}				
+
+			}
+
+		} else {
+
+			value = m_map.get(key);
+
+		}
+
+		return value.substring(0,1);
+
+	}
+
+	
+
+	public synchronized static boolean didBuildAMap() {
+
+		return s_builtAMap;
+
+	}
+
+	
+
+	public static void main(String[] args) {
+
+		Bug1871376 buggy = new Bug1871376();
+
+		String key = null;
+
+		buggy.getFirstCharOfKey(key);
+
+		synchronized (buggy) {
+
+			System.out.println("Built a map: " + s_builtAMap);
+
+		}
+
+	}
+
+
+
+	private class NonSerializableMap extends AbstractMap<String,String> {
+
+		private final Map<String,String> m_map = new HashMap<String,String>();
+
+		
+
+		public Set<java.util.Map.Entry<String, String>> entrySet() {
+
+			return m_map.entrySet();
+
+		}
+
+	}
+
+}
+
