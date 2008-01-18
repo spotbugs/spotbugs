6c2c0cc6f7b43fcb692363b2090cd5d915881807
Benjamin Langmead <ben.langmead@gmail.com>
2008-01-18 19:56:05 +0000

    Filled out with more test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9266 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug1874856.java
+++ findbugsTestCases/src/java/sfBugs/Bug1874856.java
@@ -10,10 +10,20 @@ public class Bug1874856 {
 	}
 		
 	public static void falsePositive() {
-		  Calendar c = new GregorianCalendar(1993, 4, 23);
-		   String s1 = String.format("Duke's Birthday: %1$tm %1$te, %1$tY", c);
-		   System.out.println(s1);
-		   String s2 = String.format("Duke's Birthday: %1$tm %<te, %<tY", c);
-		   System.out.println(s2);
+		// None of these should yield warnings.
+		Calendar c = new GregorianCalendar(1993, 4, 23);
+		String s1 = String.format("Duke's Birthday: %1$tm %1$te, %1$tY", c);
+		System.out.println(s1);
+		String s2 = String.format("Duke's Birthday: %1$tm %<te, %<tY", c);
+		System.out.println(s2);
+		String s3 = String.format("Duke's Birthday: %2$tm %<te, %<tY", c, c);
+		System.out.println(s3);
+		String s4 = String.format("Duke's Birthday: %2$tm %<te, %te %<tY %te", c, c);
+		System.out.println(s4);
+		String s5 = String.format("Duke's Birthday: %<te, %te %<tY %te %12$tm ",
+				   c, c, c, c, c, c, c, c, c, c, c, c);
+		System.out.println(s5);
+		String s6 = String.format("Duke's Birthday: %1.1f %2$te, %1$f", 1.0, c);
+		System.out.println(s6);
 	}
 }
