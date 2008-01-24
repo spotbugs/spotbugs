85c214d1d543ee39fca446ec96a7a09684b18d59
Benjamin Langmead <ben.langmead@gmail.com>
2008-01-24 22:57:14 +0000

    Tests for sourceforge issue [ 1878528 ] XSL xforms don't support history features
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9274 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1878528/p1/Foo.java
@@ -0,0 +1,19 @@
+public class Foo {
+    public static void main(String args[]) {
+        int[] mya = new int[6];
+        System.out.println(mya.toString());
+        
+        String s = "Hello";
+        s.toUpperCase();
+        System.out.println(s);
+        
+        for(int i = 0; i < 1000; i++) {
+        	s += "Hello";
+        }
+        s.toLowerCase();
+        System.out.println(s);
+        
+        Integer i = new Integer(null);
+        System.out.println("" + i);
+    }
+}
