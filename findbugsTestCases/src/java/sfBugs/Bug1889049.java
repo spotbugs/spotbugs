712a531675009fd79aaf2506c27826963cefd480
Benjamin Langmead <ben.langmead@gmail.com>
2008-02-14 23:15:11 +0000

    Test case for bug 1889049
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9306 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1889049.java
@@ -0,0 +1,27 @@
+package sfBugs;
+
+public class Bug1889049 {
+    public static void main(String[] args) {
+    	int i = Integer.parseInt(args[0]);
+        testSuppressOnMethod(i);
+        testSuppressOnLocalVar(i);
+        testNoSuppress(i);
+    }
+
+    @edu.umd.cs.findbugs.annotations.SuppressWarnings("DLS")
+    private static void testSuppressOnMethod(int i) {
+        int hours = i + 24;
+        System.out.println("Hours per day: ");
+    }
+
+    private static void testSuppressOnLocalVar(int i) {
+        @edu.umd.cs.findbugs.annotations.SuppressWarnings("DLS")
+        int hours = 24 + i;
+        System.out.println("Hours per day: ");
+    }
+
+    private static void testNoSuppress(int i) {
+        int hours = 24 + i;
+        System.out.println("Hours per day: ");
+    }
+}
