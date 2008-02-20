bc522ef83c96874492a7a468d6570ef3fb65411d
Andrey Loskutov <iloveeclipse@gmail.com>
2008-02-20 06:34:47 +0000

    license
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9323 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- eclipsePlugin/src/de/tobject/findbugs/actions/RefreshAction.java
+++ eclipsePlugin/src/de/tobject/findbugs/actions/RefreshAction.java
@@ -1,3 +1,21 @@
+/*
+ * Contributions to FindBugs
+ * Copyright (C) 2008, Andrei Loskutov
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package de.tobject.findbugs.actions;
 
 import org.eclipse.jface.action.IAction;
@@ -23,7 +41,6 @@ public class RefreshAction implements IViewActionDelegate {
 	}
 
 	public void selectionChanged(IAction action, ISelection selection) {
-		// TODO Auto-generated method stub
+		// noop
 	}
-
 }
