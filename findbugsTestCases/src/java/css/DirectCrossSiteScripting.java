985ce8c42ebe102000ebc4251c6148e79d2904fb
Bill Pugh <bill.pugh@gmail.com>
2008-03-08 02:02:56 +0000

    Add test cases for CSS
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9357 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/css/DirectCrossSiteScripting.java
@@ -0,0 +1,21 @@
+package css;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DirectCrossSiteScripting extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+		throws ServletException, IOException {
+		response.setContentType("text/html");
+
+		PrintWriter out = response.getWriter();
+		
+		out.println(request.getParameter("foo"));
+		out.close();
+	}
+}
