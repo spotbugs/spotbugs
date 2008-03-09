024ed441b8cd47e6794c7d668af851f754c78080
Bill Pugh <bill.pugh@gmail.com>
2008-03-09 21:56:50 +0000

    added more cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9368 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/RFE1910461.java
+++ findbugsTestCases/src/java/sfBugs/RFE1910461.java
@@ -2,11 +2,35 @@ package sfBugs;
 
 public class RFE1910461 {
 	
-	int f(int x) {
+
+	boolean high(boolean b1, boolean b2, boolean b3) {
+		if (b1)
+			return b1 = b2;
+		else return b2=b3;
+	}
+	int medium(int x) {
 		int m = 10;
 		if (x <= 0)
 			return m = 8;
 		return m*x;
+	}
+	int low(int x) {
+		int m = 10;
+		if (x > 0)
+			return m*x;
+		return m*=x;
+	}
+	String lowString(int x) {
+		String s = "foo";
+		if (x < 0)
+			return s+=x;
+		return s;
+	}
+	String fpString(int x) {
+		String s = "foo";
+		if (x >= 0)
+			return s;
+		return s+=x;
 	}
 
 }
