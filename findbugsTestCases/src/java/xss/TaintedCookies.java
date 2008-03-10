642f38c1d29dbbdc349ae34270f48fc0aba1858e
Bill Pugh <bill.pugh@gmail.com>
2008-03-10 13:15:53 +0000

    Forgot to commit TaintedCookies
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9372 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/xss/TaintedCookies.java
@@ -0,0 +1,23 @@
+package xss;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TaintedCookies extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+		throws ServletException, IOException {
+		response.setContentType("text/html");
+
+		Cookie cookie = new Cookie("foo", request.getParameter("foo"));
+		PrintWriter out = response.getWriter();
+		response.addCookie(cookie);
+		
+		out.println("<html><body><p>Hello</p></body></html>");
+	}
+}
