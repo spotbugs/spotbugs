bf8f2a7aaade363ae99bb823a063e719253997cc
Bill Pugh <bill.pugh@gmail.com>
2008-03-10 13:15:07 +0000

    Add tainted cookie example rename css package to xss
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9371 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/xss/StringBufferCrossSiteScripting.java
+++ findbugsTestCases/src/java/xss/StringBufferCrossSiteScripting.java
@@ -1,4 +1,4 @@
-package css;
+package xss;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -8,14 +8,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class DirectCrossSiteScripting extends HttpServlet {
+public class StringBufferCrossSiteScripting extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) 
 		throws ServletException, IOException {
 		response.setContentType("text/html");
 
 		PrintWriter out = response.getWriter();
 		
-		out.println(request.getParameter("foo"));
+		out.println("Hi" + request.getParameter("foo"));
 		out.close();
 	}
 }
