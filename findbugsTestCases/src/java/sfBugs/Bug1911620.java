06b1eced187766319d7e23d59817c14555467f00
Benjamin Langmead <ben.langmead@gmail.com>
2008-03-12 01:58:25 +0000

    Filled out with more useful test methods
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9386 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug1911620.java
+++ findbugsTestCases/src/java/sfBugs/Bug1911620.java
@@ -5,4 +5,31 @@ public class Bug1911620 {
 		long l = Long.valueOf(longStr);
 		return --l;
 	}
+
+	public long getLongPlus1(String longStr) {
+		long l = Long.valueOf(longStr);
+		return ++l;
+	}
+
+	public long getLongWithDLS(String longStr) {
+		long l = Long.valueOf(longStr);
+		long l2 = l; // This is the only place FindBugs should give a DLS warning
+		return l;  
+	}
+
+	public long getLongMinus1_2(String longStr) {
+		long l = Long.valueOf(longStr);
+		--l;
+		return l;
+	}
+
+	public long getLongMinus2(String longStr) {
+		long l = Long.valueOf(longStr);
+		return l - 2;
+	}
+
+	public int getIntMinus1(String intStr) {
+		int i = Integer.valueOf(intStr);
+		return --i;
+	}
 }
