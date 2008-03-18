5a7385824feeb140107a7b30b068f8b42fe7a3fb
Benjamin Langmead <ben.langmead@gmail.com>
2008-03-18 13:49:21 +0000

    Trying to construct a test case for [ 1911617 ] CloneIdiom relies on getNameConstantOperand for INSTANCEOF.  This one is derived from the Jackrabbit code that David links to, but it is not sufficient to cause the exception.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9413 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1911617.java
@@ -0,0 +1,18 @@
+package sfBugs;
+
+public class Bug1911617 implements Cloneable {
+	public Object perhapsClone(Object o) {
+		if(o == null) {
+			return "Hello";
+		}
+		if(o instanceof Bug1911617) {
+			return ((Bug1911617)o).clone();
+		} else {
+			return null;
+		}
+	}
+	
+	public Object clone() {
+		return null;
+	}
+}
