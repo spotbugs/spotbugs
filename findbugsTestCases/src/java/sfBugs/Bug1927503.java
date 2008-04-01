995be9093a1d72983580cc12f98d32648ec725e5
Bill Pugh <bill.pugh@gmail.com>
2008-04-01 12:34:49 +0000

    add test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9480 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1927503.java
@@ -0,0 +1,19 @@
+package sfBugs;
+
+import edu.umd.cs.findbugs.annotations.SuppressWarnings;
+public class Bug1927503 {
+
+	@SuppressWarnings("EI2")
+	private byte[] arr;
+	
+	private byte[] arr2;
+
+	public Bug1927503(@SuppressWarnings("EI2") byte[] newArr) {
+	   arr = newArr;
+	}
+	
+	public void setArr2(@SuppressWarnings("EI2") byte[] newArr) {
+		   arr2 = newArr;
+		}
+	
+}
