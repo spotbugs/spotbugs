315d7839abfc5f22cb71dcf2f558d7364a53c1ce
Benjamin Langmead <ben.langmead@gmail.com>
2008-04-03 23:05:21 +0000

    Committing some older test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9498 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1911766.java
@@ -0,0 +1,18 @@
+package sfBugs;
+
+public class Bug1911766 {
+	
+	public int test(Object var1, Object var2) {
+		int returnValue;
+		if ((var1 == null) && (var2 == null))
+			returnValue = 0;
+		else if (var1 == null) // [warns here]
+			returnValue = -1;
+		else if (var2 == null)
+			returnValue = 1;
+		else {
+			returnValue = 0;
+		}
+		return returnValue;
+	}
+}
---
+++ findbugsTestCases/src/java/sfBugs/Bug1913834.java
@@ -0,0 +1,18 @@
+package sfBugs;
+
+public class Bug1913834 {
+	
+	public int getBlah() {
+		Integer i = 7;
+		return i.intValue();
+	}
+	
+	public int getBlah2() {
+		Bug1913834 b = new Bug1913834();
+		return b.get7();
+	}
+	
+	public int get7() {
+		return 7;
+	}
+}
