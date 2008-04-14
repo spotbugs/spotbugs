2fdda00bee20b2e421bdcc3f83365c3aec24464a
Benjamin Langmead <ben.langmead@gmail.com>
2008-04-14 13:47:11 +0000

    Extended test case for [ 1941804 ] DL: Synchronization on interned String could lead to deadloc
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9522 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1941804.java
@@ -0,0 +1,54 @@
+package sfBugs;
+
+public class Bug1941804 {
+	private static String LOCK = "LOCK";
+	private String nsLOCK = "LOCK";
+	private final static String fLOCK = "LOCK";
+	private final String fnsLOCK = "LOCK";
+
+	public Bug1941804() {
+		super();
+	}
+
+	public void methodSynchronize(String parmLOCK) {
+		String localLOCK = "LOCK";
+		synchronized (LOCK) {
+			int a = 1;
+			int b = 2;
+			int c = a + b;
+			System.out.println("c: " + c);
+		}
+		synchronized (nsLOCK) {
+			int a = 1;
+			int b = 2;
+			int c = a + b;
+			System.out.println("c: " + c);
+		}
+		synchronized (fLOCK) { // warning
+			int a = 1;
+			int b = 2;
+			int c = a + b;
+			System.out.println("c: " + c);
+		}
+		synchronized (fnsLOCK) { // warning
+			int a = 1;
+			int b = 2;
+			int c = a + b;
+			System.out.println("c: " + c);
+		}
+		synchronized(parmLOCK) {
+			// Code...
+			int a = 1;
+			int b = 2;
+			int c = a + b;
+			System.out.println("c: " + c);
+		}
+		synchronized(localLOCK) { // warning
+			// Code...
+			int a = 1;
+			int b = 2;
+			int c = a + b;
+			System.out.println("c: " + c);
+		}
+	}
+}
