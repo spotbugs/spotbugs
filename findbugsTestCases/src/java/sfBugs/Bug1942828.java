5d48bdbcc6648ab887f2f7ac53ffb3fecc9111c0
Benjamin Langmead <ben.langmead@gmail.com>
2008-04-21 21:29:47 +0000

    Test case for [ 1942828 ] ResultSet getXXX where index is not zero reported as zero
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9533 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1942828.java
@@ -0,0 +1,20 @@
+package sfBugs;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Bug1942828 {
+	public int getInt0(ResultSet rs) throws SQLException {
+		return rs.getInt(0);
+	}
+
+	public int getInt(ResultSet rs, int i) throws SQLException {
+		return rs.getInt(i);
+	}
+
+	private final int CONST = 7;
+	
+	public int getIntCheck(ResultSet rs, int i) throws SQLException {
+		return i > 0 ? rs.getInt(i) : CONST;
+	}
+}
