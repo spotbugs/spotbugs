47e64348268d9d80850b120fd4375c3e3291bc6a
Benjamin Langmead <ben.langmead@gmail.com>
2008-05-15 18:53:48 +0000

    Test case for [ 1964520 ] Findbugs reports invalid M D BC - Unchecked/unconfirmed cast (I can't recreate it)
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9600 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1964520.java
@@ -0,0 +1,28 @@
+package sfBugs;
+
+import java.io.IOException;
+
+public class Bug1964520 extends Exception {
+    private static final long serialVersionUID = 1L;
+
+	public void test(Exception e) {
+		// should trigger warning (and does)
+		if(((Bug1964520)e).toString().equals("")) {
+			System.out.println("toString is blank");
+		}
+		// should trigger warning (and does)
+		if(e instanceof IOException && ((Bug1964520)e).toString().equals("")) {
+			System.out.println("toString is blank");
+		}
+		// should not trigger warning (and doesn't)
+		if(e instanceof Bug1964520 && ((Bug1964520)e).toString().equals("")) {
+			System.out.println("toString is blank");
+		}
+		// should not trigger warning (and doesn't)
+		if(e instanceof Bug1964520) {
+			if(((Bug1964520)e).toString().equals("")) {
+				System.out.println("toString is blank");
+			}
+		}
+	}
+}
