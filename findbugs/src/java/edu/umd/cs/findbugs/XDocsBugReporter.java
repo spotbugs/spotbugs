21f269f55657ccf69982486e7d5e624cf48e7b6a
Benjamin Langmead <ben.langmead@gmail.com>
2008-05-15 18:30:27 +0000

    Small fix, didn't realize isEmpty() was so recent
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9599 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/XDocsBugReporter.java
+++ findbugs/src/java/edu/umd/cs/findbugs/XDocsBugReporter.java
@@ -75,7 +75,7 @@ public class XDocsBugReporter extends TextUIBugReporter {
 	@Override
 	public void reportMissingClass(ClassNotFoundException ex) {
 		String missing = AbstractBugReporter.getMissingClassName(ex);
-		if (missing.isEmpty() || missing.charAt(0) == '[') return;
+		if (missing.length() == 0 || missing.charAt(0) == '[') return;
 		bugCollection.addMissingClass(missing);
 		super.reportMissingClass(ex);
 	}
