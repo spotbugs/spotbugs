84c0f9851d645546da86fa772332e1563b524195
Bill Pugh <bill.pugh@gmail.com>
2008-05-28 15:16:44 +0000

    Code from sourceforge bug report
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9634 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1968650.java
@@ -0,0 +1,15 @@
+package sfBugs;
+
+import edu.umd.cs.findbugs.annotations.*;
+
+public class Bug1968650 {
+	public void nonNullArgsMethod(@NonNull Object any) {
+
+	}
+
+	@NonNull
+	public Object nonNullReturnValue() {
+		nonNullArgsMethod(null);
+		return null;
+	}
+}
