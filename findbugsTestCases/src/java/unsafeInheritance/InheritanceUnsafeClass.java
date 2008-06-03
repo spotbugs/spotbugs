a3e0f41f04ab6f873e5ee3ee376bc38122c0fc34
David Hovemeyer <david.hovemeyer@gmail.com>
2008-06-03 13:46:30 +0000

    Put the test for the UI bug pattern in its own package. Added a subclass to force the warning to be reported as high priority.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9650 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/unsafeInheritance/InheritanceUnsafeClass.java
+++ findbugsTestCases/src/java/unsafeInheritance/InheritanceUnsafeClass.java
@@ -1,3 +1,4 @@
+package unsafeInheritance;
 import java.io.InputStream;
 import java.net.URL;
 
---
+++ findbugsTestCases/src/java/unsafeInheritance/pkg/Subclass.java
@@ -0,0 +1,11 @@
+package unsafeInheritance.pkg;
+
+import unsafeInheritance.InheritanceUnsafeClass;
+
+/**
+ * This class is just here to raise the priority of
+ * the UI warning in the superclass.
+ */
+public class Subclass extends InheritanceUnsafeClass {
+
+}
