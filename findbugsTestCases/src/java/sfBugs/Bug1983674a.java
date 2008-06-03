9979ee03840b0158e8f1b1e73a588769926a631c
Bill Pugh <bill.pugh@gmail.com>
2008-06-03 18:34:27 +0000

    more test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9655 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug1983674a.java
+++ findbugsTestCases/src/java/sfBugs/Bug1983674a.java
@@ -20,4 +20,23 @@ public class Bug1983674a {
 			return false;
 		return true;
 	}
+	public boolean equals2(Object obj) {
+		if (!(obj instanceof Bug1983674a)) 
+			return false;
+		
+		Bug1983674a cmp = (Bug1983674a) obj;
+		if ((address != null && cmp.address == null) || (!address.equals(cmp.address)))
+			return false;
+		return true;
+	}
+	public boolean foo(Object x, Object y) {
+			if ((x != null & y == null) || (!x.equals(y)))
+			return false;
+		return true;
+	}
+	public boolean foo2(Object x, Object y) {
+		if ((x != null && y == null) || (!x.equals(y)))
+		return false;
+	return true;
+}
 }
