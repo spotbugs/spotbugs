3465582d47b91c60cd19613704bc7b341429d071
David Hovemeyer <david.hovemeyer@gmail.com>
2008-06-05 18:25:36 +0000

    Added some methods testing type qualifier checking when default annotations are in effect.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9664 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/jsr305/DefaultFooParameters.java
+++ findbugsTestCases/src/java/jsr305/DefaultFooParameters.java
@@ -2,7 +2,28 @@ package jsr305;
 
 import java.lang.annotation.ElementType;
 
-@Foo(defaultFor=ElementType.PARAMETER)
+import javax.annotation.meta.When;
+
+@Foo(defaultFor=ElementType.PARAMETER, when=When.ALWAYS)
 public class DefaultFooParameters {
+	
+	// parameter "o" must carry a @Foo(when=When.ALWAYS) type qualifier,
+	// since that is the default for parameters
+	public void requiresFoo(Object o) {
+		
+	}
+	
+	// violation: @Foo(when=When.NEVER) value passed to method expecting @Foo(when=When.ALWAYS)
+	public void violate(@Foo(when=When.NEVER) Object x) {
+		requiresFoo(x);
+	}
+	
+	public void ok(Object x) {
+		requiresFoo(x);
+	}
+	
+	public void ok2(@Foo(when=When.ALWAYS) Object x) {
+		requiresFoo(x);
+	}
 
 }
