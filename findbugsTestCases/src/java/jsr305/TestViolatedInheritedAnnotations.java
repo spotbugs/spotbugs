88ff5ca3da99adee36b957d63f9cb7792fcc5eb5
David Hovemeyer <david.hovemeyer@gmail.com>
2008-06-09 16:41:22 +0000

    Added a test case where checking needs to be performed, but no direct annotation is evident.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9678 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/jsr305/TestViolatedInheritedAnnotations.java
+++ findbugsTestCases/src/java/jsr305/TestViolatedInheritedAnnotations.java
@@ -28,4 +28,32 @@ public class TestViolatedInheritedAnnotations implements I1, I2 {
 		// Should see a warning here
 		obj.requiresUntaintedParam(tainted);
 	}
+	
+	// It is easy to spot that f() returns a @Tainted value,
+	// and thus checking should take place.
+	static class X {
+		public @Tainted Object f() {
+			return new Object();
+		}
+	}
+	
+	// This class's f() also returns a @Tainted value,
+	// but because the @Tainted qualifier is inherited,
+	// it's harder to figure out that checking needs to be done.
+	static class Y extends X {
+		@Override
+		public Object f() {
+			return new Object();
+		}
+	}
+	
+	public void easyViolation(InterfaceWithDefaultUntaintedParams obj) {
+		X x = new X();
+		obj.requiresUntaintedParam(x.f()); // violation
+	}
+	
+	public void trickyViolation(InterfaceWithDefaultUntaintedParams obj) {
+		Y y = new Y();
+		obj.requiresUntaintedParam(y.f()); // violation
+	}
 }
