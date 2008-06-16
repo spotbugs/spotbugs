5d6f9712d5d12dbb6cb2e9b5a3dd382424be1656
Bill Pugh <bill.pugh@gmail.com>
2008-06-16 15:08:59 +0000

    add test case for [ 1994945 ] "Transient field that isn't set ..." on Externalizable
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9711 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1994945.java
@@ -0,0 +1,52 @@
+package sfBugs;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.io.Serializable;
+
+public class Bug1994945  implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+	transient Object x;
+	Object y;
+	
+	Bug1994945(Object x, Object y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	int f() {
+		return x.hashCode() + y.hashCode();
+	}
+	
+	
+	static class InnerClass extends Bug1994945 implements Externalizable {
+		
+		public InnerClass() {
+			super(null, null);
+		}
+		
+		InnerClass(Object x, Object y, Object z) {
+	        super(x, y);
+	        this.z = z;
+        }
+
+		transient Object z;
+	
+
+		public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+	        x = in.readInt();
+	        y = in.readInt();
+	        z = in.readInt();
+	        
+        }
+
+		public void writeExternal(ObjectOutput out) throws IOException {
+	        // TODO Auto-generated method stub
+	        
+        }
+	
+	}
+}
