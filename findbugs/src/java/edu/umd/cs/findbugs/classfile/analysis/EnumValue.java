15bf8ecde545b153a5765905e635eee4cefec631
David Hovemeyer <david.hovemeyer@gmail.com>
2008-06-23 17:20:31 +0000

    Added hashCode() and equals() implementations, since they are needed by the TypeQualifierValue class's hashCode() and equals() methods, which in turn are needed in order to properly intern TypeQualifierValues and implement other caches based on type qualifiers.
    (It took me several hours to figure out why seemingly easy direct type qualifier
    applications weren't being resolved - grrr.....)
    
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9722 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/classfile/analysis/EnumValue.java
+++ findbugs/src/java/edu/umd/cs/findbugs/classfile/analysis/EnumValue.java
@@ -1,6 +1,6 @@
 /*
  * FindBugs - Find Bugs in Java programs
- * Copyright (C) 2003-2007 University of Maryland
+ * Copyright (C) 2003-2008 University of Maryland
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -23,19 +23,40 @@ import edu.umd.cs.findbugs.classfile.ClassDescriptor;
 import edu.umd.cs.findbugs.classfile.DescriptorFactory;
 
 /**
- * @author pwilliam
+ * Represents an enumeration value used with an application
+ * of an annotation.
+ * 
+ * @author William Pugh
+ * @author David Hovemeyer
  */
 public class EnumValue {
 	public final ClassDescriptor desc;
 	public final String value;
+
 	public EnumValue(String desc, String value) {
 		this.desc = DescriptorFactory.createClassDescriptorFromSignature(desc);
 		this.value = value;
 	}
+
 	@Override
-    public String toString() {
+	public String toString() {
 		return desc.getDottedClassName() + "." + value;
 	}
-	
 
+	@Override
+	public int hashCode() {
+		return desc.hashCode() + 37 * value.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null || !(obj instanceof EnumValue)) {
+			return false;
+		}
+		EnumValue other = (EnumValue) obj;
+		return this.desc.equals(other.desc)
+			&& this.value.equals(other.value);
+	}
+	
+	
 }
