4dae05821917f18bcf8a594a851529e841a8fa22
David Hovemeyer <david.hovemeyer@gmail.com>
2008-06-24 13:09:05 +0000

    I don't think we really are supposed to expect a warning in report2().
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9728 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/nullnessAnnotations/packageDefault/TestNonNull2.java
+++ findbugsTestCases/src/java/nullnessAnnotations/packageDefault/TestNonNull2.java
@@ -10,8 +10,13 @@ class TestNonNull2 extends TestNonNull1 implements Interface1 {
 		f(null); // should get a NonNull warning from TestNonNull1
 	}
 	
-	@ExpectWarning("NP")
+//	@ExpectWarning("NP")
 	void report2() {
+		//
+		// FindBugs doesn't produce a warning here because the g()
+		// method in TestNonNull1 explicitly marks its parameter
+		// as @Nullable.  So, we shouldn't expect a warning. (?)
+		//
 		g(null); // should get a NonNull warning from Interface1
 	}
 	
