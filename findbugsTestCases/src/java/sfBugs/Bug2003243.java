3873df6096438f7665b3d0b263cb1cafb3d5f070
David Hovemeyer <david.hovemeyer@gmail.com>
2008-07-03 15:47:16 +0000

    Initial checkin.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9778 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2003243.java
@@ -0,0 +1,16 @@
+package sfBugs;
+
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
+public class Bug2003243 {
+
+	String field = Bug2003243.class.getCanonicalName();
+	static Bug2003243 record = new Bug2003243();
+
+	@NoWarning("MWN")
+	public void foo() {
+		synchronized (record.field) {
+			record.field.notifyAll(); // There should not be an MWN violation here
+		}
+	}
+}
