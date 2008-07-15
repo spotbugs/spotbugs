68b2fd3ffbdf5ded5f017457770a0ebce91bafa1
Bill Pugh <bill.pugh@gmail.com>
2008-07-15 17:21:45 +0000

    Add example
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9848 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2012727.java
@@ -0,0 +1,17 @@
+package sfBugs;
+
+import java.text.SimpleDateFormat;
+
+public enum Bug2012727 {
+	mmddyyyy_WithTimeSlashDelimited("MM/dd/yyyy HH:mm:ss"), ddyymmM_dateOnlyNOTDelimited("ddyyMMM");
+
+	private SimpleDateFormat simpleDateFormat;
+
+	private Bug2012727(String format) {
+		simpleDateFormat = new SimpleDateFormat(format);
+	}
+
+	public SimpleDateFormat getFormat() {
+		return simpleDateFormat;
+	}
+}
