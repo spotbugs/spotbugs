e957603c1c1ec1e5a37d0325f8e353096edb12cc
Bill Pugh <bill.pugh@gmail.com>
2008-07-21 05:28:24 +0000

    more test cases for repeated conditionals
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9889 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug1995271.java
+++ findbugsTestCases/src/java/sfBugs/Bug1995271.java
@@ -14,4 +14,17 @@ public class Bug1995271 {
 		return x + y;
 	}
 
+	public static int foo3(int x, int y) {
+		if (x < 5)
+			if (x < 5)
+				throw new IllegalArgumentException("This is wrong");
+		return x + y;
+	}
+
+	public static boolean foo4(int x, int y) {
+		if (x == 3 || x != 3 && y == 5)
+			return true;
+		return false;
+	}
+
 }
