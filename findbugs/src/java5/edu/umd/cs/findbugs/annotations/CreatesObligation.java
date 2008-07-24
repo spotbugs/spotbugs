959f4cf3cbe6234c7a9aa0752f1c7c7e9326a48a
David Hovemeyer <david.hovemeyer@gmail.com>
2008-07-24 14:54:51 +0000

    Allow to be applied to constructors
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9919 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java5/edu/umd/cs/findbugs/annotations/CreatesObligation.java
+++ findbugs/src/java5/edu/umd/cs/findbugs/annotations/CreatesObligation.java
@@ -35,7 +35,7 @@ import java.lang.annotation.Target;
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD, ElementType.CONSTRUCTOR})
 public @interface CreatesObligation {
 
 }
