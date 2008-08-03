3fafa9e624dd04197758b0709b23869841e8735a
tmeyer88 <tmeyer88@tmeyer88.com>
2008-08-03 23:10:18 +0000

    Multithreaded correctness - does not release lock on all exc
    The following code shows the bug;
    
    Method does not release lock on all exception paths
    
    By replacing the lock code with the commented out code, no bugs are
    reported.
    
    I'm not sure how just using the lockArray instead of a reference to an
    element of the lockArray could create a situation where an exception could
    be thrown that couldn't be in the first case.
    
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9952 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2033091.java
@@ -0,0 +1,32 @@
+package sfBugs;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
+
+public class Bug2033091 {
+
+static ReentrantReadWriteLock lockArray[];
+static {
+lockArray =
+new ReentrantReadWriteLock[5];
+for (int i=0; i<lockArray.length; i++)
+lockArray[i] = new ReentrantReadWriteLock();
+}
+
+static void method(int n) throws IOException {
+
+// WriteLock lock = lockArray[n].writeLock();
+// lock.lock();
+lockArray[n].writeLock().lock();
+try {
+// do some disk I/O
+} finally {
+// lock.unlock();
+lockArray[n].writeLock().unlock();
+}
+}
+
+public static void main(String[] args) throws IOException { }
+}
