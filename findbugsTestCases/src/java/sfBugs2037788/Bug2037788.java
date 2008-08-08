ca513d37be45e944452ed4c3db59bf5c9fd0c8c7
tmeyer88 <tmeyer88@tmeyer88.com>
2008-08-08 18:05:03 +0000

    Bug number 2037788
    Warnings are not linked back to sourceFile in 1.3.4
    
    When running findbugs 1.3.4 from an ant task, the sourceFile is not set.
    and instead shows sourceFile="&lt;Unknown&gt;".
    
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@9963 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs2037788/Bug2037788.java
@@ -0,0 +1,10 @@
+package sfBugs2037788;
+
+public class Bug2037788 {
+	public static void main(String args[])
+	{
+		System.out.println("This is a test.");
+		//Next line should generate findbugs warning
+		System.exit(0);
+	}
+}
