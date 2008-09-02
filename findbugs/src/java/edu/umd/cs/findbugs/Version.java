e611826623105ac7bb70c01a8f9729447e6ae6c5
Bill Pugh <bill.pugh@gmail.com>
2008-09-02 03:47:45 +0000

    1.3.5-rc2
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10045 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/Version.java
+++ findbugs/src/java/edu/umd/cs/findbugs/Version.java
@@ -53,7 +53,7 @@ public class Version {
 	 * Release candidate number.
 	 * "0" indicates that the version is not a release candidate.
 	 */
-	public static final int RELEASE_CANDIDATE = 0;
+	public static final int RELEASE_CANDIDATE = 2;
 
 	/**
 	 * Release date.
