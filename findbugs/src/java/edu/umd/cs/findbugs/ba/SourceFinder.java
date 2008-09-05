b1a5e2689c711f0c01d5a8f55847d89da62d8884
Bill Pugh <bill.pugh@gmail.com>
2008-09-05 20:06:18 +0000

    roll back of 9612 and patch [ 1792387 ] improved ("dynamic") source code lookup
    Causes horrible performance problems with large source trees
    
    
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10059 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/ba/SourceFinder.java
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/SourceFinder.java
@@ -26,7 +26,6 @@ import java.io.InputStream;
 import java.net.JarURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
@@ -34,8 +33,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
-
-import javax.annotation.CheckForNull;
 
 import edu.umd.cs.findbugs.SourceLineAnnotation;
 import edu.umd.cs.findbugs.SystemProperties;
@@ -65,7 +62,7 @@ public class SourceFinder {
 		private static final long serialVersionUID = 1L;
 
 		@Override
-		protected boolean removeEldestEntry(Map.Entry<String, SourceFile> eldest) {
+				 protected boolean removeEldestEntry(Map.Entry<String, SourceFile> eldest) {
 			return size() >= CACHE_SIZE;
 		}
 	}
@@ -96,7 +93,10 @@ public class SourceFinder {
 			return "DirectorySourceRepository:" + baseDir;
 		}
 		public boolean contains(String fileName) {
-			return listRecursive(new File(baseDir), fileName) != null;
+			File file = new File(getFullFileName(fileName));
+			boolean exists = file.exists();
+			if (DEBUG) System.out.println("Exists " + exists + " for " + file);
+			return exists;
 		}
 
 		public boolean isPlatformDependent() {
@@ -104,24 +104,11 @@ public class SourceFinder {
 		}
 
 		public SourceFileDataSource getDataSource(String fileName) {
-			String result = listRecursive(new File(baseDir), fileName);
-			if (result == null) throw new IllegalArgumentException("No file found for " + fileName);
-			return new FileSourceFileDataSource(result);
+			return new FileSourceFileDataSource(getFullFileName(fileName));
 		}
-		
-		private @CheckForNull
-		String listRecursive(File dir, String sourceQualified) {
-			File f = new File(new File(baseDir), sourceQualified);
-			if (f.exists())
-				return f.getAbsolutePath();
 
-			for (File child : dir.listFiles())
-				if (child.isDirectory()) {
-					String result = listRecursive(child, sourceQualified);
-					if (result != null)
-						return result;
-				}
-			return null;
+		private String getFullFileName(String fileName) {
+			return baseDir + File.separator + fileName;
 		}
 	}
 
