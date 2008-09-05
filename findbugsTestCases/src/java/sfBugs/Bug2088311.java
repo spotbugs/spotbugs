a12a1e1d97d46812ee34dab97a2323205ad63a1b
Bill Pugh <bill.pugh@gmail.com>
2008-09-05 20:27:38 +0000

    more test cases = goog
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10060 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2088311.java
@@ -0,0 +1,20 @@
+package sfBugs;
+
+public class Bug2088311 implements Comparable<Bug2088311> {
+	final int begin;
+
+	Bug2088311(int b) {
+		begin = b;
+	}
+
+	public Integer getBegin() {
+		return begin;
+	}
+
+	public int compareTo(Bug2088311 other) {
+		if (getBegin() == null) {
+			return -1;
+		}
+		return getBegin().compareTo(other.getBegin());
+	}
+}
