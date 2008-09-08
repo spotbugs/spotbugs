2c7da348c1bbb201567d0c2f78b536a45d8cbb64
Bill Pugh <bill.pugh@gmail.com>
2008-09-08 19:55:38 +0000

    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10080 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/npe/ReadLineTest.java
@@ -0,0 +1,24 @@
+package npe;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ReadLineTest {
+	
+	public static void main(String args[]) throws Exception  {
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		String x = in.readLine();
+		System.out.println(x.hashCode());
+		
+		 x = in.readLine();
+		System.out.println(x.hashCode());
+		
+		System.out.println(in.readLine().hashCode());
+		
+		System.out.println(in.readLine().hashCode());
+	}
+
+}
