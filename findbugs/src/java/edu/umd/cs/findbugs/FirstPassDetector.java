5301b9036702193ed2949f85f634b4d1b00dd7b9
Bill Pugh <bill.pugh@gmail.com>
2008-09-17 21:05:19 +0000

    add support for local first pass detectors
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10116 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/FirstPassDetector.java
@@ -0,0 +1,35 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2003-2008 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs;
+
+/**
+ * A Detector that can be run in the first pass.
+ * 
+ * A detector can be run in the first pass if either:
+ * <ul>
+ * <li> It doesn't report any warnings, or
+ * <li> It doesn't depend upon information from analyzing any other classes
+ * </ul>
+ * 
+ * @author Bill Pugh
+ */
+public interface FirstPassDetector {
+
+}
