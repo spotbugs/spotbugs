d68892f7533c908e92ae49d7feafe2708625bace
Bill Pugh <bill.pugh@gmail.com>
2008-09-18 13:59:39 +0000

    Example of chained method invocation and null pointer dereference
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10127 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/dynamicany/DynAnyCollectionImpl.java
@@ -0,0 +1,13 @@
+package dynamicany;
+
+
+public class DynAnyCollectionImpl extends DynAnyConstructedImpl {
+	public DynAnyCollectionImpl() {
+        this(null, null, false);
+    }
+
+    protected DynAnyCollectionImpl(String orb, String any, boolean copyValue) {
+        super(orb, any, copyValue);
+    }
+
+}
---
+++ findbugsTestCases/src/java/dynamicany/DynAnyConstructedImpl.java
@@ -0,0 +1,10 @@
+package dynamicany;
+
+
+public class DynAnyConstructedImpl extends DynAnyImpl {
+
+	public DynAnyConstructedImpl(String orb, String any, boolean copyValue) {
+	        super(orb, any, copyValue);
+    }
+
+}
