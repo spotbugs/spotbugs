a49c0bae6f8714bad308f17cb2add914b620c128
Bill Pugh <bill.pugh@gmail.com>
2008-10-03 18:33:00 +0000

    add test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10207 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2118411.java
@@ -0,0 +1,25 @@
+package sfBugs;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Bug2118411 {
+
+	public static String foo(File file) {
+
+		Scanner s = null;
+		try {
+			s = new Scanner(new BufferedReader(new FileReader(file)));
+			return s.next();
+		} catch (IOException e) {
+			return null;
+		} finally {
+			if (s != null) {
+				s.close();
+			}
+		}
+	}
+}
