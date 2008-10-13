2b6aaf6b6d90eeac5bc8c215820438d7972314d8
Bill Pugh <bill.pugh@gmail.com>
2008-10-13 04:38:11 +0000

    more test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10226 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2118696.java
@@ -0,0 +1,26 @@
+package sfBugs;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Bug2118696 {
+
+	public static void writeToFile(File file, byte[] bytes) throws IOException {
+		FileOutputStream fout = null;
+		try {
+			fout = new FileOutputStream(file);
+			fout.write(bytes);
+		} finally {
+			closeStream(fout);
+		}
+	}
+
+	public static void closeStream(Closeable stream) throws IOException {
+		if (stream != null) {
+			stream.close();
+		}
+	}
+
+}
---
+++ findbugsTestCases/src/java/sfBugs/Bug2136033.java
@@ -0,0 +1,117 @@
+package sfBugs;
+
+
+
+import java.io.Closeable;
+
+import java.io.File;
+
+import java.io.FileOutputStream;
+
+import java.io.IOException;
+
+import java.io.OutputStream;
+
+
+
+import org.junit.Test;
+
+
+
+/**
+
+ * FindBugs OBL false positive.
+
+ * 
+
+ * The difference is the argument to safeClose(). If it is a Closeable, FindBugs OBL generates a
+
+ * false positive.
+
+ */
+
+public class Bug2136033 {
+
+	@Test
+
+	public void testCloseStream() throws Exception {
+
+		// ok: this line is not flagged with OBL
+
+		FileOutputStream fos = new FileOutputStream(File.createTempFile("prefix", "temp"));
+
+		try {
+
+			fos.write("abc".getBytes());
+
+		} finally {
+
+			safeCloseStream(fos);
+
+		}
+
+	}
+
+
+
+	private void safeCloseStream(OutputStream object) {
+
+		try {
+
+			if (object != null && object != System.out && object != System.err) {
+
+				object.close();
+
+			}
+
+		} catch (IOException e) {
+
+			System.out.println("failed to close stream: " + e.getMessage());
+
+		}
+
+	}
+
+
+
+	@Test
+
+	public void testCloseCloseableFalsePositive() throws Exception {
+
+		// FALSE POSITIVE: this line is flagged with OBL
+
+		FileOutputStream fos = new FileOutputStream(File.createTempFile("prefix", "temp"));
+
+		try {
+
+			fos.write("abc".getBytes());
+
+		} finally {
+
+			safeCloseCloseable(fos);
+
+		}
+
+	}
+
+
+
+	private void safeCloseCloseable(Closeable object) {
+
+		try {
+
+			if (object != null && object != System.out && object != System.err) {
+
+				object.close();
+
+			}
+
+		} catch (IOException e) {
+
+			System.out.println("failed to close stream: " + e.getMessage());
+
+		}
+
+	}
+
+}
