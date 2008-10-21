2281f7b785bab439438935646c4e15f20b7e128a
Bill Pugh <bill.pugh@gmail.com>
2008-10-21 14:04:02 +0000

    add more test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10248 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/gcUnrelatedTypes/TestContainsAllAndRemoveAll.java
+++ findbugsTestCases/src/java/gcUnrelatedTypes/TestContainsAllAndRemoveAll.java
@@ -1,6 +1,8 @@
 package gcUnrelatedTypes;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 public class TestContainsAllAndRemoveAll {
@@ -8,6 +10,15 @@ public class TestContainsAllAndRemoveAll {
 	public static void main(String args[]) {
 		Set<Integer> i = new HashSet<Integer>();
 		Set<String> s = new HashSet<String>();
+		i.removeAll(s);
+		i.containsAll(s);
+		
+		falsePositive();
+	}
+	
+	public static void falsePositive() {
+		Set<Integer> i = new HashSet<Integer>();
+		List<Integer> s = new LinkedList<Integer>();
 		i.removeAll(s);
 		i.containsAll(s);
 	}
