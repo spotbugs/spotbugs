dc1a882369b6867ce7e83a344db954eae4e4df6f
Bill Pugh <bill.pugh@gmail.com>
2008-10-22 04:27:56 +0000

    more test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10251 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/gcUnrelatedTypes/AllListsCouldBeEqual.java
@@ -0,0 +1,157 @@
+package gcUnrelatedTypes;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
+
+public class AllListsCouldBeEqual {
+	
+	static class MyList<E> implements List<E> {
+
+		public boolean add(E o) {
+	        // TODO Auto-generated method stub
+	        return false;
+        }
+
+		public void add(int index, E element) {
+	        // TODO Auto-generated method stub
+	        
+        }
+
+		public boolean addAll(Collection<? extends E> c) {
+	        // TODO Auto-generated method stub
+	        return false;
+        }
+
+		public boolean addAll(int index, Collection<? extends E> c) {
+	        // TODO Auto-generated method stub
+	        return false;
+        }
+
+		public void clear() {
+	        // TODO Auto-generated method stub
+	        
+        }
+
+		public boolean contains(Object o) {
+	        // TODO Auto-generated method stub
+	        return false;
+        }
+
+		public boolean containsAll(Collection<?> c) {
+	        // TODO Auto-generated method stub
+	        return false;
+        }
+
+		public E get(int index) {
+	        // TODO Auto-generated method stub
+	        return null;
+        }
+
+		public int indexOf(Object o) {
+	        // TODO Auto-generated method stub
+	        return 0;
+        }
+
+		public boolean isEmpty() {
+	        // TODO Auto-generated method stub
+	        return false;
+        }
+
+		public Iterator<E> iterator() {
+	        // TODO Auto-generated method stub
+	        return null;
+        }
+
+		public int lastIndexOf(Object o) {
+	        // TODO Auto-generated method stub
+	        return 0;
+        }
+
+		public ListIterator<E> listIterator() {
+	        // TODO Auto-generated method stub
+	        return null;
+        }
+
+		public ListIterator<E> listIterator(int index) {
+	        // TODO Auto-generated method stub
+	        return null;
+        }
+
+		public boolean remove(Object o) {
+	        // TODO Auto-generated method stub
+	        return false;
+        }
+
+		public E remove(int index) {
+	        // TODO Auto-generated method stub
+	        return null;
+        }
+
+		public boolean removeAll(Collection<?> c) {
+	        // TODO Auto-generated method stub
+	        return false;
+        }
+
+		public boolean retainAll(Collection<?> c) {
+	        // TODO Auto-generated method stub
+	        return false;
+        }
+
+		public E set(int index, E element) {
+	        // TODO Auto-generated method stub
+	        return null;
+        }
+
+		public int size() {
+	        // TODO Auto-generated method stub
+	        return 0;
+        }
+
+		public List<E> subList(int fromIndex, int toIndex) {
+	        // TODO Auto-generated method stub
+	        return null;
+        }
+
+		public Object[] toArray() {
+	        // TODO Auto-generated method stub
+	        return null;
+        }
+
+		public <T> T[] toArray(T[] a) {
+	        // TODO Auto-generated method stub
+	        return null;
+        }}
+	public static void main(String arg[]) {
+		falsePositive();
+		
+		List<Integer> lst = new LinkedList<Integer>();
+		List<Integer> lst2 = (ArrayList<Integer>)lst;
+		
+		
+		List<ArrayList<String>> mlist = null;
+		mlist.contains(new LinkedList<String>());
+
+	}
+	public static void falsePositive() {
+		ArrayList<Integer> aLst = new ArrayList<Integer>();
+		LinkedList<Integer> lLst = new LinkedList<Integer>();
+		MyList<Integer> mLst = new MyList<Integer>();
+		
+		Set<ArrayList<Integer>> alSet = new HashSet<ArrayList<Integer>>();
+		Set<LinkedList<Integer>> llSet = new HashSet<LinkedList<Integer>>();
+		
+		System.out.println(aLst.equals(lLst));
+		System.out.println(mLst.equals(lLst));
+		
+		alSet.contains(lLst);
+		alSet.contains(mLst);
+		alSet.containsAll(llSet);
+
+	}
+}
