6d14590a420988af3fe25ce6779f638c9831732d
Bill Pugh <bill.pugh@gmail.com>
2008-10-24 17:12:35 +0000

    add test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10262 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/gcUnrelatedTypes/EvenUnknownTypesAreNotRecursive.java
@@ -0,0 +1,34 @@
+package gcUnrelatedTypes;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class EvenUnknownTypesAreNotRecursive {
+	
+	/** Really, we want this to be a class that is not available for analysis */
+	
+	static class A {
+		
+	}
+
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		
+		Set<A> sa = new HashSet<A>();
+		Set<A> sa2 = new HashSet<A>();
+		
+		TreeSet<A> tsa2 = new TreeSet<A>();
+		
+		sa.contains(sa);
+		sa.contains(sa2);
+		sa.contains(tsa2);
+		
+		sa.containsAll(sa);
+		sa.containsAll(sa2);
+		sa.containsAll(tsa2);
+	}
+}
