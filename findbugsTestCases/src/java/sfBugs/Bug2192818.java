29a63688dcb6d98e509591b3a9ded981ff88febc
Bill Pugh <bill.pugh@gmail.com>
2008-11-09 15:23:22 +0000

    more test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10315 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2192818.java
@@ -0,0 +1,16 @@
+package sfBugs;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class Bug2192818<E> {
+
+	
+	BlockingQueue<E> tierAverageToCalculateQueue;
+	public void foo(E e) throws InterruptedException {
+		tierAverageToCalculateQueue.offer(e, 120,
+				TimeUnit.SECONDS);
+		tierAverageToCalculateQueue.offer(e, 120,
+				TimeUnit.SECONDS);
+	}
+}
