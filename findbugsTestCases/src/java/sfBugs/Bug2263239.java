ba33c658b3427d6fe300aafdce92c86adbe1c747
Bill Pugh <bill.pugh@gmail.com>
2008-11-13 03:44:35 +0000

    add test case for [ 2263239 ] volatile fields reporting extra nullchecks
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10325 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2263239.java
@@ -0,0 +1,27 @@
+package sfBugs;
+
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+
+public class Bug2263239 {
+	volatile Object[] _vol;
+
+	Object falsePositive() {
+
+		Object[] a = _vol;
+		if (a != null)
+			return a;
+
+		Object[] b = _vol;
+		if (b != null)
+			return b;
+
+		return "foo";
+	}
+	private final AtomicReferenceFieldUpdater<Bug2263239,Object[]> _newUpdater =
+	      AtomicReferenceFieldUpdater.newUpdater(Bug2263239.class,Object[].class, "_vol");
+	   
+	void cas(Object[] expect, Object[] update) {
+		_newUpdater.compareAndSet(this, expect, update);
+	}
+
+}
