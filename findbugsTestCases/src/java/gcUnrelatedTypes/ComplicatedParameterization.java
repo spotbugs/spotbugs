b8fa12e1b2aac98ec376c3cdfb408d9b9dbad3f1
Bill Pugh <bill.pugh@gmail.com>
2008-11-13 00:14:29 +0000

    add test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10320 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/gcUnrelatedTypes/ComplicatedParameterization.java
@@ -0,0 +1,21 @@
+package gcUnrelatedTypes;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class ComplicatedParameterization {
+	
+	static class Foo<E> extends HashSet<ArrayList<E>> {}
+	
+	Foo<Integer> myFoo = new Foo<Integer>();
+	
+	void add(ArrayList<Integer> a) {
+		myFoo.add(a);
+	}
+	
+	boolean contains(ArrayList<Integer> a) {
+		return myFoo.contains(a);
+	}
+	
+
+}
