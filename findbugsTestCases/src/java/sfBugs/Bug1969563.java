fda9ef6c8ea0a5e8e9bb449882fb9b5ca19358db
Mark T. Daly <mdaly@cs.umd.edu>
2008-11-24 05:47:30 +0000

    New test cases for SF bugs 1969563, 1993328, 2073889, and 2073901.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10373 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug1969563.java
@@ -0,0 +1,42 @@
+/* ****************************************
+ * $Id$
+ * SF bug 1993328:
+ *   false positive DM_USELESS_THREAD
+ *
+ * JVM:  1.5.0_16 (OS X, PPC)
+ * FBv:  1.3.7-dev-20081123
+ *
+ * Test case based on example code from bug report
+ * 
+ * Notes:
+ *   repro requires use of '-low' option
+ * **************************************** */
+
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
+public class Bug1969563 extends Thread {
+	private static long getNextId() { return 1L; }
+
+	/* ********************
+	 * Behavior at filing:
+	 *   warning thrown =>
+	 * L M Dm: Method new Bug1969563() creates a thread using the default empty
+	 * run method  At Bug1969563.java:[line 19]
+	 * ******************** */
+	// TODO:  add @NoWarning("Dm") annotation if/when it applies to constructors
+	public Bug1969563() {
+		super("TestThread-" + getNextId());
+	}
+	
+	/* ********************
+	 * Behavior at filing:
+	 *   no warning thrown
+	 * ******************** */
+	public Bug1969563(int id) {
+		super("TestThread-" + id);
+	}
+
+	@Override public void run() {
+		System.out.println("Hello World");
+	}
+}
---
+++ findbugsTestCases/src/java/sfBugs/Bug1993328.java
@@ -0,0 +1,47 @@
+/* ****************************************
+ * $Id$
+ * SF bug 1993328:
+ *   missed EI_EXPOSE_REP2 (EI2) bug on assignment
+ *   in conditional branch
+ *
+ * JVM:  1.5.0_16 (OS X, PPC)
+ * FBv:  1.3.7-dev-20081121
+ *
+ * Test case based on example code from bug report
+ * **************************************** */
+
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+
+public class Bug1993328 {
+	private static Object[] NO_ARGS = new Object[] {};
+	private Object[] args1;
+
+	/* ********************
+	 * Behavior at filing:
+	 *   no warning thrown
+	 * ******************** */
+	@ExpectWarning("EI2")
+	public void assign_conditional(Object[] args) {
+		this.args1 = args == null ? NO_ARGS : args;
+	};
+
+	/* ********************
+	 * Behavior at filing:
+	 *   warning thrown =>
+	 * M V EI2: Bug1993328.assign_explicit(Object[]) may expose internal
+	 * representation by storing an externally mutable object into args1
+	 * At Bug1993328.java:[line 15]
+	 * ******************** */
+	@ExpectWarning("EI2")
+	public void assign_explicit(Object[] args) {
+		this.args1 = args;
+	};
+
+    // dummy method, eliminates 'unread field' warning
+	public boolean hasargs() {
+		if(args1 != null) {
+			return true;
+		}
+		return false;
+	}
+}
