683c6cd17e0549248bb39e1b5cd3dc48c1644b61
Bill Pugh <bill.pugh@gmail.com>
2008-11-25 05:48:33 +0000

    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10375 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/fakedLibraries/org/easymock/EasyMock.java
@@ -0,0 +1,12 @@
+package org.easymock;
+
+/**
+ * Stubs for EasyMock methods
+ *
+ */
+public class EasyMock {
+	public static void replay(Object... o){}
+	public static void verify(Object... o){}
+	public static <T> T createMock(Class <T> c) { return null; }
+
+}
---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2008_11_24.java
@@ -0,0 +1,18 @@
+package bugIdeas;
+
+import org.easymock.EasyMock;
+
+public class Ideas_2008_11_24 {
+	interface Foo {
+		void setFoo(int x);
+	}
+
+	public static void main(String args[]) {
+		Foo foo = EasyMock.createMock(Foo.class);
+		foo.setFoo(5);
+		EasyMock.replay();
+		foo.setFoo(4);
+		EasyMock.verify();
+	}
+
+}
