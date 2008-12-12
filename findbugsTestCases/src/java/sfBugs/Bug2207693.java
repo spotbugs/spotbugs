907ff35b86d36f14b99f98fd4834212e8384699d
Mark T. Daly <mdaly@cs.umd.edu>
2008-12-12 08:43:19 +0000

    New test cases for SF bugs 2207693 and 2353694
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10400 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2207693.java
@@ -0,0 +1,57 @@
+/* ****************************************
+ * $Id$
+ * SF bug 2207693:
+ *   OBL False positive on java.sql.executeQuery close
+ *
+ * JVM:  1.5.0_16 (OS X, PPC)
+ * FBv:  1.3.7-dev-20081212
+ *
+ * Test case based on example code from bug report
+ * **************************************** */
+
+package sfBugs;
+
+import java.sql.*;
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
+public class Bug2207693 {
+    private Connection con;
+    private String statement;
+
+    public Bug2207693(Connection newCon, String newStatement) {
+        con = newCon;
+        statement = newStatement;
+    }
+
+    /* ********************
+     * Behavior at filing:
+     *   OBL false positive 
+     *   According to API documentation, PreparedStatement close()
+     *   method should also close current ResultSet object.  See docs:
+     *   <http://java.sun.com/javase/6/docs/api/java/sql/PreparedStatement.html>
+	 * warning thrown =>
+     *   M X OBL_UNSATISFIED_OBLIGATION OBL: Method sfBugs.Bug2207693.execute() \
+     *   may fail to clean up stream or resource of type java.sql.ResultSet \
+     *   Obligation to clean up resource created at \
+     *   Bug2207693.java:[line 39] is not discharged
+     * ******************** */
+    @NoWarning("OBL")
+    public void execute() {
+        /* test code adapted from bug report */
+        try {
+            final PreparedStatement ps = con.prepareStatement(statement);
+            try {
+                final ResultSet rs = ps.executeQuery();
+
+                /* eliminate dead local store warning */
+                if(rs != null) {
+                    System.out.println("Okay!");
+                }
+            } finally {
+                ps.close();
+            }
+        } catch(Exception e) {
+            System.out.println("Got exception");
+        }
+    }
+}
---
+++ findbugsTestCases/src/java/sfBugs/Bug2353694.java
@@ -0,0 +1,71 @@
+/* ****************************************
+ * $Id$
+ * SF bug 2353694:
+ *   OBL False positive on properly closed input stream
+ *
+ * JVM:  1.5.0_16 (OS X, PPC)
+ * FBv:  1.3.7-dev-20081212
+ *
+ * Test case based on example code from bug report
+ *
+ * The original false positive OBL warning on 'in' in fp_warning has been
+ * corrected in the FB version above; however, only one OS warning is reported
+ * for fp_warning_OS (on in2) despite the second unclosed stream (in3).
+ * **************************************** */
+
+package sfBugs;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+
+public class Bug2353694 {
+
+    /* ********************
+     * Behavior at filing:
+     *   OBL false positive already resolved
+     *   correct OS warning thrown for in2
+	 * warning thrown =>
+     *   M B OS_OPEN_STREAM OS: Bug2353694.fp_warning_OS() may fail to close \
+     *   stream At Bug2353694.java:[line 26]
+     *
+     *  *no* OS warning thrown for in3 -- maybe false negative?
+     * ******************** */
+    @ExpectWarning("OS")
+    public void fp_warning_OS() throws IOException
+    {
+        FileInputStream in2 = new FileInputStream("test");
+        in2.read();
+
+        FileInputStream in = new FileInputStream("test");
+        try {
+            in.read();
+        } finally {
+            in.close();
+        }
+
+        FileInputStream in3 = new FileInputStream("test");
+        in3.read();
+    }
+
+    /* ********************
+     * Behavior at filing:
+     *   correct OS warning thrown for in3
+	 * warning thrown =>
+     *   oM B OS_OPEN_STREAM OS: Bug2353694.correct_warning_OS() may fail to \
+     *   close stream At Bug2353694.java:[line 50]
+     * ******************** */
+    @ExpectWarning("OS")
+    public void correct_warning_OS() throws IOException
+    {
+        FileInputStream in = new FileInputStream("test");
+        try {
+            in.read();
+        } finally {
+            in.close();
+        }
+
+        FileInputStream in3 = new FileInputStream("test");
+        in3.read();
+    }
+}
