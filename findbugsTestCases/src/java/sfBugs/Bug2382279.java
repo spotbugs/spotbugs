88568823449bf3462310f493b0c4518ccfb25feb
Mark T. Daly <mdaly@cs.umd.edu>
2008-12-13 01:46:33 +0000

    New test case for SF bug 2382279
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10418 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2382279.java
@@ -0,0 +1,56 @@
+/* ****************************************
+ * $Id$
+ * SF bug 2382279:
+ *   OBL and ORD false positives on java.sql.Statement.close()
+ *
+ * JVM:  1.5.0_16 (OS X, PPC)
+ * FBv:  1.3.7-dev-20081212
+ *
+ * Test case based on example code from bug report
+ * **************************************** */
+
+package sfBugs;
+
+import java.sql.*;
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
+public class Bug2382279 {
+    private Connection con;
+
+    public Bug2382279(Connection newCon) {
+        con = newCon;
+    }
+
+    /* ********************
+     * Behavior at filing:
+     *   OBL and ODR false positives
+     *   The ResultSet object returned by execute() has a method by which the
+     *   statement that produced it can be retrieved, and so the statement does
+     *   not need to be closed in this method.  As such, no warnings should be
+     *   thrown.
+     *
+     * warnings thrown =>
+     *  M B ODR_OPEN_DATABASE_RESOURCE ODR: sfBugs.Bug2382279.execute(String) \
+     *    may fail to close Statement  At Bug2382279.java:[line 47]
+     *
+     *  M X OBL_UNSATISFIED_OBLIGATION OBL: Method \
+     *    sfBugs.Bug2382279.execute(String) may fail to clean up stream or \
+     *    resource of type java.sql.Statement  Obligation to clean up resource \
+     *    created at Bug2382279.java:[line 47] is not discharged
+     *
+     * ******************** */
+    @NoWarning("OBL,ODR")
+    public ResultSet execute(String query) throws SQLException {
+        // test code adapted from bug report
+        ResultSet rs;
+        try {
+            Statement statement = con.createStatement();
+            rs = statement.executeQuery(query);
+            return rs;
+        } catch(SQLException sqle) {
+            System.out.println("Couldn't execute statement");
+        }
+        return null;
+    }
+
+}
