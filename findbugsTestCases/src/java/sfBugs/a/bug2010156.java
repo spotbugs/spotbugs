312469a5e95fcc47bc875c810d1a373ab1e733ef
Mark T. Daly <mdaly@cs.umd.edu>
2008-12-17 04:59:26 +0000

    New test case for SF bug 2010156 (GUI does not navigate to line).
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10436 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/a/bug2010156.java
@@ -0,0 +1,35 @@
+/* ****************************************
+ * $Id$
+ * SF bug 2010156:
+ *   Line numbers not reported/highlighted for certain
+ *   error types (some of which report errors in textui mode)
+ *
+ * JVM:  1.5.0_16 (OS X, PPC)
+ * FBv:  1.3.7-dev-20081212
+ *
+ * Certain errors reported by the GUI do not highlight the code
+ * with which they are associated, nor are line numbers reported;
+ * in certain cases, the line numbers for the same bugs /are/
+ * reported when using the text UI.
+ *
+ * see sfBugs/b/bug2010156.java for more details
+ *
+ * **************************************** */
+
+package sfBugs.a;
+
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+
+/* this class only exists to induce a warning in another class;
+ * see sfBugs/b/bug2010156.java */
+public class bug2010156 {
+    private int i, j, k;
+
+    public int testMethod1(int a, int b, int c) {
+        i = a;
+        j = b;
+        k = c;
+        return i + j + k;
+    }
+
+}
