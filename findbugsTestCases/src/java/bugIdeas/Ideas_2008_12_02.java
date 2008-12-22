f065b1c5d5d3cbc6b3dbf8a155b8566d28573a22
Bill Pugh <bill.pugh@gmail.com>
2008-12-22 04:39:10 +0000

    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10448 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/bugIdeas/Ideas_2008_12_02.java
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2008_12_02.java
@@ -1,8 +1,9 @@
 package bugIdeas;
 
+import java.util.AbstractList;
 import java.util.HashMap;
 
-public class Ideas_2008_12_02 {
+public class Ideas_2008_12_02 extends AbstractList<String> {
 	
 	HashMap<String,String> map = new HashMap<String,String>();
 	HashMap<Object,Object> map2 = new HashMap<Object,Object>();
@@ -17,4 +18,19 @@ public class Ideas_2008_12_02 {
 			if (map.containsKey(o))
 				System.out.println("Found intersection");
 	}
+	
+	public int indexOf(Object o) {
+		if (map.containsKey(o)) return 1;
+		return -1;
+	}
+	@Override
+    public String get(int index) {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+	@Override
+    public int size() {
+	    // TODO Auto-generated method stub
+	    return 16;
+    }
 }
