a1927db017e6dc09c0dd0df2e9581619ba19ef4b
Bill Pugh <bill.pugh@gmail.com>
2009-01-06 06:19:50 +0000

    Add comments as to which calls are oK and which are bad
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10501 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/bugIdeas/Ideas_2008_11_14.java
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2008_11_14.java
@@ -27,14 +27,14 @@ public class Ideas_2008_11_14 extends TestCase {
 
 		m.put("a","a");
 		Set<Map.Entry<Integer,Integer>> es = new HashSet<Map.Entry<Integer,Integer>>();	
-		boolean b1 = m.entrySet().contains(1);
-		boolean b2 = m.keySet().contains(1);
-		boolean b3 = m.values().contains(1);
-		boolean b4 = m.entrySet().equals(es);
-		boolean b5 = m.entrySet().equals(is);
-		 m.entrySet().contains(1);
-		boolean b6 = m.keySet().equals(is);
-		boolean b7 = m.values().equals(is);
+		boolean b1 = m.entrySet().contains(1); // bad
+		boolean b2 = m.keySet().contains(1); // ok
+		boolean b3 = m.values().contains(1); // ok
+		boolean b4 = m.entrySet().equals(es); // ok
+		boolean b5 = m.entrySet().equals(is); // bad
+		 m.entrySet().contains(1); // bad
+		boolean b6 = m.keySet().equals(is); // ok
+		boolean b7 = m.values().equals(is); // ok
 		System.out.printf("%b %b %b %b %b %b %b\n", b1, b2, b3, b4, b5, b6, b7);
 	}
 }
