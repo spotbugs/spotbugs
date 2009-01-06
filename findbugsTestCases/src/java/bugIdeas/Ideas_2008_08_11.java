5febb7499265cadfbd40330f95b5cde3787f7edf
Bill Pugh <bill.pugh@gmail.com>
2009-01-06 05:47:12 +0000

    new ideas
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10497 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/bugIdeas/Ideas_2008_08_11.java
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2008_08_11.java
@@ -1,6 +1,7 @@
 package bugIdeas;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 import junit.framework.TestCase;
 
@@ -17,13 +18,21 @@ public class Ideas_2008_08_11  extends TestCase {
 		assertNull(map.get(1));
     }
 	
-	
+	@Test
+    public void fooBar2() throws Exception {
+		assertFalse(Boolean.TRUE.equals(5));
+		HashSet<String> set = new HashSet<String>();
+    	set.add("a");
+		assertFalse(set.contains(1));
+		assertFalse(set.remove(1));
+    }
     public void testFoo() throws Exception {
     	assertFalse(Boolean.TRUE.equals(5));
     	HashMap<String,Integer> map = new HashMap<String,Integer>();
     	map.put("a", 1);
 		assertFalse(map.containsKey(1));
 		assertNull(map.get(1));
+		assertNull(map.remove(1));
     }
 
 }
