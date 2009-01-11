274e3ec64c571fb57f0605b82a8e5f8a8b3e74f1
Andrey Loskutov <iloveeclipse@gmail.com>
2009-01-11 14:04:26 +0000

    Compiler warning fixes: added serialVersionUID
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10517 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- eclipsePlugin/src/edu/umd/cs/findbugs/plugin/eclipse/quickfix/exception/MethodDeclarationNotFoundException.java
+++ eclipsePlugin/src/edu/umd/cs/findbugs/plugin/eclipse/quickfix/exception/MethodDeclarationNotFoundException.java
@@ -2,19 +2,19 @@
  * Contributions to FindBugs
  * Copyright (C) 2006, Institut for Software
  * An Institut of the University of Applied Sciences Rapperswil
- * 
+ *
  * Author: Thierry Wyss, Marco Busarello
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -26,18 +26,20 @@ import org.eclipse.jdt.core.dom.TypeDeclaration;
 /**
  * Thrown when no <CODE>MethodDeclaration</CODE> was found in the specified
  * <CODE>TypeDeclaration</CODE>.
- * 
+ *
  * @author <a href="mailto:twyss@hsr.ch">Thierry Wyss</a>
  * @author <a href="mailto:mbusarel@hsr.ch">Marco Busarello</a>
  * @version 1.0
  */
 public class MethodDeclarationNotFoundException extends BodyDeclarationNotFoundException {
 
-	private TypeDeclaration typeDeclaration;
+	private static final long serialVersionUID = 7856022756808853146L;
 
-	private String methodName;
+	private final TypeDeclaration typeDeclaration;
 
-	private String methodSignature;
+	private final String methodName;
+
+	private final String methodSignature;
 
 	public MethodDeclarationNotFoundException(TypeDeclaration typeDeclaration, String methodName, String methodSignature) {
 		super("Method declaration '" + methodName + methodSignature + "' not found in type declaration '" + typeDeclaration.getName() + "'.");
--- eclipsePlugin/src/edu/umd/cs/findbugs/plugin/eclipse/quickfix/util/ImportDeclarationComparator.java
+++ eclipsePlugin/src/edu/umd/cs/findbugs/plugin/eclipse/quickfix/util/ImportDeclarationComparator.java
@@ -2,19 +2,19 @@
  * Contributions to FindBugs
  * Copyright (C) 2007, Institut for Software
  * An Institut of the University of Applied Sciences Rapperswil
- * 
+ *
  * Author: Thierry Wyss, Marco Busarello
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -34,12 +34,14 @@ import org.eclipse.jdt.core.dom.Name;
  * A <CODE>Comparator</CODE> used to add imports in a sorted order. The <CODE>ImportDeclaration</CODE>
  * will be sorted according to static or not static import, and then in an
  * alphabetically order.
- * 
+ *
  * @author <a href="mailto:twyss@hsr.ch">Thierry Wyss</a>
  * @author <a href="mailto:mbusarel@hsr.ch">Marco Busarello</a>
  * @version 1.0
  */
 public class ImportDeclarationComparator<E extends ImportDeclaration> implements Comparator<E>, Serializable {
+
+	private static final long serialVersionUID = -5389720178174319293L;
 
 	public int compare(E o1, E o2) {
 		if (o1 == o2) {
