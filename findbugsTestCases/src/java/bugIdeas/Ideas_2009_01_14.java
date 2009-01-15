a2061fad6759d5b78150a522abff1f8b10826272
Bill Pugh <bill.pugh@gmail.com>
2009-01-15 01:12:45 +0000

    add test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10532 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2009_01_14.java
@@ -0,0 +1,56 @@
+package bugIdeas;
+
+public class Ideas_2009_01_14 {
+	
+//	static String getNameCorrect(int value) {
+//		String result = "";
+//		switch (value) {
+//		case 0:
+//				result = "zero";
+//			break;
+//		case 1:
+//			result = "one";
+//		break;
+//		case 2:
+//			result = "two";
+//		break;
+//		case 3:
+//			result = "three";
+//		break;
+//		case 4:
+//			result = "four";
+//		break;
+//		default:
+//			throw new IllegalArgumentException("Illegal agrument: " + value);
+//			
+//		}
+//		return "Answer is " + result;
+//	}
+	static String getNameBroken(int value) {
+		String result = "";
+		switch (value) {
+		case 0:
+				result = "zero";
+			break;
+		case 1:
+			result = "one";
+		break;
+		case 2:
+			result = "two";
+			break;
+		case 3:
+			result = "three";
+		break;
+		case 4:
+			result = "four";
+		default:
+			throw new IllegalArgumentException("Illegal agrument: " + value);
+			
+		}
+		return "Answer is " + result;
+	}
+	
+	public static void main(String args[]) {
+		System.out.printf("%d\n", 100.0);
+	}
+}
