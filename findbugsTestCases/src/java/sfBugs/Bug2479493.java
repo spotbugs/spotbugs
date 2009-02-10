9b8dea4217a87fb9cfadc417d9db5448c0b2b467
Bill Pugh <bill.pugh@gmail.com>
2009-02-10 22:05:53 +0000

    Add test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10606 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2479493.java
@@ -0,0 +1,40 @@
+package sfBugs;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+public class Bug2479493 {
+	public static boolean copyFile(String fromPath, String toPath) {
+		FileChannel in = null;
+		FileChannel out = null;
+
+		boolean success = true;
+
+		try {
+			in = new FileInputStream(fromPath).getChannel();
+			out = new FileOutputStream(toPath).getChannel();
+
+			in.transferTo(0, in.size(), out);
+		} catch (Exception e) {
+			success = false;
+			e.printStackTrace();
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+				}
+			}
+			if (out != null) {
+				try {
+					out.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+		return success;
+	}
+
+}
