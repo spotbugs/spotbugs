9e972df01e284013807e5bc65adb3b6b9f81fd29
Tomas Pollak <tpollak.ar@gmail.com>
2009-02-16 01:16:55 +0000

    Added missing comments.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10648 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- eclipsePlugin-test/src/de/tobject/findbugs/reporter/test/MarkerUtilTest.java
+++ eclipsePlugin-test/src/de/tobject/findbugs/reporter/test/MarkerUtilTest.java
@@ -1,3 +1,21 @@
+/*
+ * Contributions to FindBugs
+ * Copyright (C) 2009, Tomás Pollak
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package de.tobject.findbugs.reporter.test;
 
 import static org.junit.Assert.*;
@@ -18,6 +36,11 @@ import de.tobject.findbugs.FindbugsPlugin;
 import de.tobject.findbugs.reporter.MarkerUtil;
 import de.tobject.findbugs.test.AbstractFindBugsTest;
 
+/**
+ * This class tests the MarkerUtil class.
+ * 
+ * @author Tomás Pollak
+ */
 public class MarkerUtilTest extends AbstractFindBugsTest {
 	@Test
 	public void testGetAllMarkers() throws CoreException {
