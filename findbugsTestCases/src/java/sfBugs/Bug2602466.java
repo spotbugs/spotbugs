9ffef62d05bfa5f06fea979aea9a95a90794c4e4
Mark T. Daly <mdaly@cs.umd.edu>
2009-02-18 18:02:24 +0000

    New test cases for SF bugs 2602466 and 2612987.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10659 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2602466.java
@@ -0,0 +1,40 @@
+/* ****************************************
+ * $Id$
+ * SF bug 2602466:
+ *   False positive Bx warning for Number subclass constructor when
+ *   used with constant values outside the cached range.
+ *
+ * JVM:  1.6.0 (OS X, x86)
+ * FBv:  1.3.8-dev-20090217
+ *
+ * Test case based on example code from bug report
+ * **************************************** */
+
+package sfBugs;
+
+import edu.umd.cs.findbugs.annotations.NoWarning;
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Bug2602466 {
+
+    public Map<Long,String> error_lookup = new HashMap<Long,String>();
+
+    /* ********************
+     * Behavior at filing:
+     *   Bx warning thrown for a call to the Long(long) constructor with a
+     *   constant value outside the range of values that will be cached
+     *   (which is currently -128 <= x <= 127)
+     * ******************** */
+    @NoWarning("Bx")
+    public void setHighValueKey() {
+        error_lookup.put(new Long(0x80000001), "Unknown error.");
+    }
+
+    @ExpectWarning("Bx")
+    public void setLowValueKey() {
+        error_lookup.put(new Long(0), "Success.");
+    }    
+}
---
+++ findbugsTestCases/src/java/sfBugs/Bug2612987.java
@@ -0,0 +1,38 @@
+/* ****************************************
+ * $Id$
+ * SF bug 2612987:
+ *   Type qualifier false positive with JSR 305 @Nonnegative annotation
+ *
+ * JVM:  1.6.0 (OS X, x86)
+ * FBv:  1.3.8-dev-20090217
+ *
+ * Test case based on example code from bug report
+ * **************************************** */
+ 
+package sfBugs;
+
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
+import javax.annotation.CheckForSigned;
+import javax.annotation.Nonnegative;
+
+public class Bug2612987
+{
+    @Nonnegative
+    private int nonNegativeValue = 1;
+
+    @Nonnegative
+    public int get() {
+        return nonNegativeValue;
+    }
+
+    /* ********************
+     * Behavior at filing:
+     *   TQ warning thrown for explicitly checked (and annotated) parameter
+     * ******************** */
+    @NoWarning("TQ")
+    public void set(@CheckForSigned int possibleNegativeValue) {
+        if(possibleNegativeValue >= 0)
+            nonNegativeValue = possibleNegativeValue;
+    }
+}
