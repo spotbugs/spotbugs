93d608c308753a7a5c819fc290c807fec7cfcfa8
Bill Pugh <bill.pugh@gmail.com>
2009-02-19 14:43:40 +0000

    more test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10671 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug2003253.java
+++ findbugsTestCases/src/java/sfBugs/Bug2003253.java
@@ -26,4 +26,23 @@ public class Bug2003253 {
 	public void report2(@CheckForNull Object bar) {
 		this.foo = bar;
 	}
+	
+	//
+	// FindNullDeref should report a warning here.
+	//
+	@ExpectWarning("NP")
+	public int report3(@CheckForNull Object bar) {
+		return bar.hashCode();
+	}
+	//
+	// FindNullDeref should report a warning here.
+	//
+	@ExpectWarning("NP")
+	public int report4(@CheckForNull Object bar) {
+		return nonnull(bar);
+	}
+
+	public int nonnull(@Nonnull Object bar) {
+		return bar.hashCode();
+	}
 }
