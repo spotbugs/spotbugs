970d0226deb7acb70fac92075544928a93a7bf34
Bill Pugh <bill.pugh@gmail.com>
2009-02-25 00:50:29 +0000

    Make comment field text rather than varchar
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10721 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/sql/findbugs.sql
+++ findbugs/src/sql/findbugs.sql
@@ -15,11 +15,11 @@ CREATE TABLE `findbugsIssues` (
   `lastSeen` date NOT NULL,
   `updated` date NOT NULL,
   `who` varchar(16) NOT NULL default '',
-  `comment` varchar(160) NOT NULL default '',
+  `comment` text  NOT NULL default '',
   `hash` varchar(32) NOT NULL,
   `bugPattern` varchar(80) NOT NULL,
   `priority` int(11) NOT NULL,
-  `primaryClass` varchar(256) NOT NULL,
+  `primaryClass` varchar(512) NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `hash` (`hash`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1514 ;
