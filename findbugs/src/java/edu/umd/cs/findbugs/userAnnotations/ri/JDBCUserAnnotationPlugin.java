35317d6c038e508d67278b9f4e5da653281b018e
Bill Pugh <bill.pugh@gmail.com>
2009-02-27 06:33:22 +0000

    import cleanup
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10742 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/userAnnotations/ri/JDBCUserAnnotationPlugin.java
+++ findbugs/src/java/edu/umd/cs/findbugs/userAnnotations/ri/JDBCUserAnnotationPlugin.java
@@ -26,10 +26,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Date;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import edu.umd.cs.findbugs.BugCollection;
