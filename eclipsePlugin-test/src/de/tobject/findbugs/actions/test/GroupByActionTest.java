dd1fbef13d5d195a776079d40a8163a411b9b137
Tomas Pollak <tpollak.ar@gmail.com>
2009-03-01 17:33:09 +0000

    Added missing copyright.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10752 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- eclipsePlugin-test/src/de/tobject/findbugs/actions/test/GroupByActionTest.java
+++ eclipsePlugin-test/src/de/tobject/findbugs/actions/test/GroupByActionTest.java
@@ -1,3 +1,21 @@
+/*
+ * Contributions to FindBugs
+ * Copyright (C) 2009, Tomás Pollak
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package de.tobject.findbugs.actions.test;
 
 import static org.junit.Assert.*;
@@ -16,6 +34,11 @@ import de.tobject.findbugs.actions.GroupByAction;
 import de.tobject.findbugs.test.AbstractFindBugsTest;
 import de.tobject.findbugs.view.explorer.GroupType;
 
+/**
+ * This class tests the GroupByAction.
+ * 
+ * @author Tomás Pollak
+ */
 public class GroupByActionTest extends AbstractFindBugsTest {
 
 	private static final String PRIORITY_CATEGORY_PROJECT_PACKAGE_CLASS_PATTERN_TYPE_PATTERN_MARKER_ID = "findBugsEclipsePlugin.toggleGrouping.Priority.Category.Project.Package.Class.PatternType.Pattern.Marker";
@@ -37,6 +60,8 @@ public class GroupByActionTest extends AbstractFindBugsTest {
 
 	@Override
 	public void tearDown() throws CoreException {
+		action = null;
+		
 		super.tearDown();
 	}
 
