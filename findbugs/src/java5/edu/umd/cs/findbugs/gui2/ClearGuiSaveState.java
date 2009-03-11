6ce594aa1d91036fb8ec9a4ec58952d381b84e71
Bill Pugh <bill.pugh@gmail.com>
2009-03-11 18:33:33 +0000

    add main method to clear GUI state
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10794 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java5/edu/umd/cs/findbugs/gui2/ClearGuiSaveState.java
@@ -0,0 +1,35 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2003-2008 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.gui2;
+
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+
+/**
+ * @author pwilliam
+ */
+public class ClearGuiSaveState {
+	
+	public static void main(String args[]) throws BackingStoreException {
+		Preferences p=Preferences.userNodeForPackage(GUISaveState.class);
+		p.clear();
+	}
+
+}
