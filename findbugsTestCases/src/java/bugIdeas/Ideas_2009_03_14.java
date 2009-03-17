3134951bf0dc67dc8ff91526d81b2ab0b390f51a
Bill Pugh <bill.pugh@gmail.com>
2009-03-17 23:52:24 +0000

    more bug ideas
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10830 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2009_03_14.java
@@ -0,0 +1,21 @@
+package bugIdeas;
+
+public class Ideas_2009_03_14 {
+	
+	int foo(int x) {
+		String tmp = null;
+		switch(x) {
+		case 0:
+			tmp = "zero";
+			break;
+		case 1:
+			tmp = "one";
+			break;
+		case 2:
+			tmp = "two";
+			break;
+		}
+		return tmp.hashCode();
+	}
+
+}
---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2009_03_17.java
@@ -0,0 +1,15 @@
+package bugIdeas;
+
+public class Ideas_2009_03_17 {
+	
+	public static final Long SPECIAL = -1L;
+	
+	public static boolean isSpecial(Long l) {
+		return l == null || l == SPECIAL;
+	}
+
+	public static boolean areEqual(Long l1, Long l2) {
+		return l1 == l2;
+	}
+
+}
