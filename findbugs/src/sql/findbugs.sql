a4b2174129d60bf5faa5561c7af798d709acb16e
Bill Pugh <bill.pugh@gmail.com>
2009-04-03 23:20:30 +0000

    add additional fields and tables
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10881 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/sql/findbugs.sql
+++ findbugs/src/sql/findbugs.sql
@@ -20,7 +20,19 @@ CREATE TABLE `findbugsIssues` (
   `bugPattern` varchar(80) NOT NULL,
   `priority` int(11) NOT NULL,
   `primaryClass` varchar(512) NOT NULL,
+  `bugDatabaseKey` varchar(64) NOT NULL,
+  `hasMultipleVotes` tinyint(1) NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `hash` (`hash`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1514 ;
+
+
+CREATE TABLE `findbugsVotes` (
+  `issueId` int(11) NOT NULL,
+  `who` varchar(16) NOT NULL,
+  `status` varchar(16) NOT NULL,
+  `comment` text NOT NULL,
+  `when` date NOT NULL,
+  KEY `issueId` (`issueId`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
