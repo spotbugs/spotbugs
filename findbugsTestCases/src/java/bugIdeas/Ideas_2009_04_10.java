2724ccefd2b98c1cc361a292b196652bf27ff68e
Bill Pugh <bill.pugh@gmail.com>
2009-04-11 00:07:13 +0000

    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10912 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2009_04_10.java
@@ -0,0 +1,30 @@
+package bugIdeas;
+
+public class Ideas_2009_04_10 {
+	
+	Object g() {
+		return "x";
+	}
+	int f(Object y) {
+		Object x = g();
+		if (x == null)
+			return 0;
+		return x.hashCode();
+	}
+	
+	static class A {
+		int foobar(int x) {
+			return x;
+		}
+	}
+	
+	static int huh() {
+		A a = new A() {
+			int foobar(short x) {
+				return super.foobar(x)+1;
+			};
+		};
+		return a.foobar(5);
+	}
+
+}
