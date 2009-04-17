b212651859e397b2616500d2130be3ee0f4fe409
Bill Pugh <bill.pugh@gmail.com>
2009-04-17 00:47:25 +0000

    new bug ideas
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10933 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2009_04_09.java
@@ -0,0 +1,22 @@
+package bugIdeas;
+
+import junit.framework.TestCase;
+
+public class Ideas_2009_04_09 extends TestCase {
+	
+	
+	public void testBad() {
+		 
+		try {
+			
+			shouldThrowAssertion();
+			fail();
+		} catch (AssertionError e) {
+			
+		}
+	}
+	public void shouldThrowAssertion() {
+		
+	}
+
+}
---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2009_04_16.java
@@ -0,0 +1,19 @@
+package bugIdeas;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class  Ideas_2009_04_16<T> {
+	
+	ConcurrentHashMap<T, AtomicInteger> map = new ConcurrentHashMap<T, AtomicInteger>();
+	
+	AtomicInteger getCounter(T t) {
+		AtomicInteger value = map.get(t);
+		if (value == null) {
+			value = new AtomicInteger();
+			map.putIfAbsent(t, value);
+		}
+		return value;
+	}
+
+}
