a0b6fb7286d0fae8a71131de2e251b4039a7524c
Bill Pugh <bill.pugh@gmail.com>
2009-04-22 05:21:02 +0000

    add examples from cigital java rulepack
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@10958 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/CigitalJavaSecurityRulepack.java
@@ -0,0 +1,28 @@
+
+/**
+ * Issues from http://www.cigital.com/securitypack/view/index.html
+ * 
+ * No comment on whether these issues are worth reporting
+ *
+ */
+public class CigitalJavaSecurityRulepack {
+
+	
+	public static void emptyTryBlocks() {
+		try
+		{
+		}
+		catch(Exception e)
+		{
+		  System.err.println("Caught Exception: "+ e.getMessage());
+		}
+	}
+	
+	public static void deprecatedThreadMethods(Thread t) {
+		t.suspend();
+		t.resume();
+		t.stop();
+		t.destroy();
+		Thread.yield();
+	}
+}
