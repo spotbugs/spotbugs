257f05e2f57c394ed37d6b6fa89752f525bf602e
Bill Pugh <bill.pugh@gmail.com>
2009-05-03 18:53:02 +0000

    add table for keeping track of bug reports
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11080 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/sql/findbugs.sql
+++ findbugs/src/sql/findbugs.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE `findbugs_issue` (
   `id` int(11) NOT NULL auto_increment,
   `firstSeen` datetime NOT NULL,
@@ -6,22 +7,35 @@ CREATE TABLE `findbugs_issue` (
   `bugPattern` varchar(80) NOT NULL,
   `priority` int(11) NOT NULL,
   `primaryClass` varchar(512) NOT NULL,
-  `bugDatabaseKey` varchar(64) NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `hash` (`hash`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
-
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `findbugs_evaluation` (
   `id` int(11) NOT NULL auto_increment,
   `issueId` int(11) NOT NULL,
-  `who` varchar(16) NOT NULL,
+  `who` varchar(64) NOT NULL,
   `designation` varchar(16) NOT NULL,
   `comment` text NOT NULL,
   `time` datetime NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `issueId` (`issueId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `findbugs_bugreport` (
+  `id` int(11) NOT NULL auto_increment,
+  `hash` varchar(32) NOT NULL,
+  `bugReportId` varchar(64) NOT NULL,
+  `whoFiled` varchar(64) NOT NULL,
+  `whenFiled` datetime NOT NULL,
+  `status` varchar(64) default NULL,
+  `assignedTo` varchar(64) default NULL,
+  `componentId` varchar(64) default NULL,
+  `componentName` varchar(128) default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `hash` (`hash`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Keeps track of bugs filed against FIndBugs issues' AUTO_INCREMENT=9 ;
 
 
 ALTER TABLE `findbugs_evaluation`
