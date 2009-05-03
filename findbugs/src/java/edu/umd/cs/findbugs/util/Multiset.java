e730da5a5202f1247937634b0e42c0d2227d6c8b
Bill Pugh <bill.pugh@gmail.com>
2009-05-03 23:47:25 +0000

    add multiset utility class
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11081 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/util/Multiset.java
@@ -0,0 +1,77 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2003-2008 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.util;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+
+/**
+ * @author pwilliam
+ */
+public class Multiset<K> {
+	HashMap<K, Integer> map = new HashMap<K, Integer>();
+
+	public void clear() {
+		map.clear();
+	}
+
+	public int numKeys() {
+		return map.size();
+	}
+
+	public void add(K k) {
+		Integer v = map.get(k);
+		if (v == null)
+			map.put(k, 1);
+		else
+			map.put(k, v + 1);
+	}
+
+	public int getCount(K k) {
+		Integer v = map.get(k);
+		if (v == null)
+			return 0;
+		return v;
+	}
+
+	public Iterable<Map.Entry<K, Integer>> entriesInDecreasingFrequency() {
+		TreeSet<Map.Entry<K, Integer>> result = new TreeSet<Map.Entry<K, Integer>>(new EntryComparator<K>());
+		result.addAll(map.entrySet());
+		return result;
+	}
+
+	static class EntryComparator<E> implements Comparator<Map.Entry<E, Integer>> {
+
+		public int compare(Entry<E, Integer> o1, Entry<E, Integer> o2) {
+			int c1 = o1.getValue();
+			int c2 = o2.getValue();
+			if (c1 < c2)
+				return 1;
+			if (c1 > c2)
+				return -1;
+			return System.identityHashCode(o1.getKey()) - System.identityHashCode(o2.getKey());
+		}
+
+	}
+
+}
