7fc384cc665a7d51d6584007b2df26b0c8ccc830
Bill Pugh <bill.pugh@gmail.com>
2009-05-07 04:39:27 +0000

    new submitted bug idea
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11136 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2009_05_06.java
@@ -0,0 +1,52 @@
+package bugIdeas;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Filter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+/**
+ * We found a problem with the new OpenJDK that everyone is now using to compile
+ * and run java code. In particular, the java.util.logging.Logger behavior has
+ * changed. Instead of using strong references, it now uses weak references
+ * internally. That's a reasonable change, but unfortunately some code relies on
+ * the old behavior - when changing logger configuration, it simply drops the
+ * logger reference. That means that the garbage collector is free to reclaim
+ * that memory, which means that the logger configuration is lost.
+ * 
+ */
+public class Ideas_2009_05_06 {
+	/**
+	 * The logger reference is lost at the end of the method (it doesn't escape
+	 * the method), so if you have a garbage collection cycle just after the
+	 * call to initLogging, the logger configuration is lost (because Logger
+	 * only keeps weak references).
+	 */
+
+	public static void initLogging() throws SecurityException, IOException {
+		Logger logger = Logger.getLogger("edu.umd.cs.findbugs");
+		logger.addHandler(new FileHandler()); // call to change logger
+											  // configuration
+		logger.setUseParentHandlers(false); // another call to change logger
+											// configuration
+		logger.setLevel(Level.FINER);
+		logger.setFilter(new Filter() {
+
+			public boolean isLoggable(LogRecord arg0) {
+				return true;
+			}
+		});
+	}
+
+	public static void main(String[] args) throws Exception {
+		initLogging(); // adds a file handler to the logger
+		System.gc(); // logger configuration lost
+		Logger.getLogger("com.google.gse").info("Some message"); // this isn't
+																 // logged to
+																 // the file as
+																 // expected
+	}
+
+}
