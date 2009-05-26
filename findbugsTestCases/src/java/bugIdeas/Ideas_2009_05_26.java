b1b381cebb13e9dd161a445e2162a4f25b53d7e9
Bill Pugh <bill.pugh@gmail.com>
2009-05-26 22:20:23 +0000

    Add test cases for false positives from FindBugs fixit
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11254 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2009_05_26.java
@@ -0,0 +1,22 @@
+package bugIdeas;
+
+import java.math.BigDecimal;
+
+public class Ideas_2009_05_26 {
+	
+	private Ideas_2009_05_26() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void Ideas_2009_05_26() {
+		System.out.println("Hello");
+	}
+	public static void main(String args[]) {
+		new BigDecimal("1.0");
+		BigDecimal b = new BigDecimal("2.0");
+		b.intValue();
+		"".matches("a");
+		
+	}
+
+}
