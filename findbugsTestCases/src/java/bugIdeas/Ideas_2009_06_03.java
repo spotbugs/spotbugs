28aeff9326819a23177b6fe23c04b574c78311a1
Bill Pugh <bill.pugh@gmail.com>
2009-06-04 06:03:03 +0000

    add test cases for dereferencing java.lang.ref method calls without a null check
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11300 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/bugIdeas/Ideas_2009_06_03.java
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2009_06_03.java
@@ -1,11 +1,20 @@
 package bugIdeas;
 
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class Ideas_2009_06_03<K,V> {
 	
 	
+	
+	public static void main(String args[]) {
+		ReferenceQueue<String> queue = new ReferenceQueue<String>();
+		SoftReference<String> r = new SoftReference<String>(args[0], queue);
+		r.get().hashCode();
+		queue.poll().hashCode();
+	}
 	ConcurrentMap<K,V> map= new ConcurrentHashMap<K,V>();
 	public V atomicPut(K k, V v) {
 		synchronized(map) {
