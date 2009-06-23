ee8f49c56c1040cf96a7957223aae6ee962c38ee
Bill Pugh <bill.pugh@gmail.com>
2009-06-23 22:42:49 +0000

    Add test cases for obligations
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11356 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/obligation/TestObligationAnnotation.java
@@ -0,0 +1,48 @@
+package obligation;
+
+import java.io.IOException;
+
+import edu.umd.cs.findbugs.annotations.CleanupObligation;
+import edu.umd.cs.findbugs.annotations.CreatesObligation;
+import edu.umd.cs.findbugs.annotations.DischargesObligation;
+
+@CleanupObligation
+public class TestObligationAnnotation {
+
+	public static @CreatesObligation
+	TestObligationAnnotation make() {
+		return new TestObligationAnnotation();
+	}
+
+	void process() throws IOException {
+		
+	}
+	public @DischargesObligation
+	void done() {
+
+	}
+
+	public static void test1() throws IOException {
+		TestObligationAnnotation o = make();
+		o.process();
+	}
+
+	public static void test2() throws IOException {
+		TestObligationAnnotation o = make();
+		o.process();
+		o.done();
+	}
+
+	public static void test3() {
+		TestObligationAnnotation o = make();
+		try {
+			o.process();
+		} catch (IOException e) {
+	        e.printStackTrace();
+        } finally {
+			o.done();
+
+		}
+	}
+
+}
---
+++ findbugsTestCases/src/java/obligation/TestStaticObligationAnnotation.java
@@ -0,0 +1,48 @@
+package obligation;
+
+import java.io.IOException;
+
+import edu.umd.cs.findbugs.annotations.CleanupObligation;
+import edu.umd.cs.findbugs.annotations.CreatesObligation;
+import edu.umd.cs.findbugs.annotations.DischargesObligation;
+
+@CleanupObligation
+public class TestStaticObligationAnnotation {
+
+	public static @CreatesObligation
+	TestStaticObligationAnnotation make() {
+		return new TestStaticObligationAnnotation();
+	}
+
+	void process() throws IOException {
+		
+	}
+	public static @DischargesObligation
+	void done() {
+
+	}
+
+	public static void test1() throws IOException {
+		TestStaticObligationAnnotation o = make();
+		o.process();
+	}
+
+	public static void test2() throws IOException {
+		TestStaticObligationAnnotation o = make();
+		o.process();
+		done();
+	}
+
+	public static void test3() {
+		TestStaticObligationAnnotation o = make();
+		try {
+			o.process();
+		} catch (IOException e) {
+	        e.printStackTrace();
+        } finally {
+			done();
+
+		}
+	}
+
+}
