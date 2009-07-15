382369022366a0631caf329855653a4e29e062f3
Bill Pugh <bill.pugh@gmail.com>
2009-07-15 20:05:12 +0000

    add test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11448 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2817789.java
@@ -0,0 +1,10 @@
+package sfBugs;
+
+public class Bug2817789 {
+	public String testStringBuilder(final String what, final int times) {
+		String result = "";
+		for (int i = 0; i < times; i++)
+			result += what;
+		return result;
+	}
+}
---
+++ findbugsTestCases/src/java/sfBugs/Bug2821733.java
@@ -0,0 +1,30 @@
+package sfBugs;
+
+public class Bug2821733 {
+
+	protected static final int objectCompLowPriority(Comparable c1, Comparable c2) {
+		if (c1 == c2) {
+			return 0;
+		} else if (c1 == null && c2 != null) {
+			return -1;
+		} else if (c1 != null && c2 == null) {
+			return 1;
+		} else {
+			return c1.compareTo(c2);
+		}
+	}
+
+	protected static final int objectCompFalsePositive(Comparable c1, Comparable c2) {
+		if (c1 == c2) {
+			return 0;
+		} else if (c1 == null) {
+			return -1;
+		} else if (c2 == null) {
+			return 1;
+		} else {
+			return c1.compareTo(c2);
+		}
+	}
+
+	
+}
