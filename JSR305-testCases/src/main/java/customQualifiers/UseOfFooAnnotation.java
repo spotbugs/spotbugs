20d8f5b8c3e3e7acc2e07aac617ebe3f646048bd
Bill Pugh <bill.pugh@gmail.com>
2009-07-31 16:11:02 +0000

    fix FindBugs issue 2816457
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11476 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- JSR305-testCases/src/main/java/customQualifiers/UseOfFooAnnotation.java
+++ JSR305-testCases/src/main/java/customQualifiers/UseOfFooAnnotation.java
@@ -6,20 +6,17 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import javax.annotation.meta.Qualifier;
+import javax.annotation.meta.TypeQualifier;
 import javax.annotation.meta.When;
 
 public class UseOfFooAnnotation {
 	@Documented
-	@Qualifier(strict=true)
+	@TypeQualifier
 	@Retention(RetentionPolicy.RUNTIME)
 	public @interface Foo {	
 		When when() default When.ALWAYS;
-		ElementType[] applyTo() default {};
 	}
-	@Target(ElementType.TYPE)
-	public @interface Foo2 {	
-	}
+	
 	public static final @Foo int FOO1 = 1;
 	public static final @Foo int FOO2 = 2;
 	
