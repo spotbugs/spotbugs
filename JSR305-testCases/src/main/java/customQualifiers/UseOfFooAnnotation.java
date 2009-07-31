d1e5369360a79464b6f025da2dc2bd14ca29d34d
Bill Pugh <bill.pugh@gmail.com>
2009-07-31 16:15:34 +0000

    Fix source forge bug 2816457
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11477 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- JSR305-testCases/src/main/java/customQualifiers/UseOfFooAnnotation.java
+++ JSR305-testCases/src/main/java/customQualifiers/UseOfFooAnnotation.java
@@ -1,10 +1,8 @@
 package customQualifiers;
 
 import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 import javax.annotation.meta.TypeQualifier;
 import javax.annotation.meta.When;
