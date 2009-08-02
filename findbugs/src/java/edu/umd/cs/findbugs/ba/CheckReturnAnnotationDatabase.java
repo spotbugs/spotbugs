ab0180878107f950cbaf5d83585c39f6291d8bf5
Bill Pugh <bill.pugh@gmail.com>
2009-08-02 18:25:58 +0000

    formatting
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11510 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/ba/CheckReturnAnnotationDatabase.java
+++ findbugs/src/java/edu/umd/cs/findbugs/ba/CheckReturnAnnotationDatabase.java
@@ -128,7 +128,8 @@ public class CheckReturnAnnotationDatabase extends AnnotationDatabase<CheckRetur
 		if (!(o instanceof XMethod))
 			return null;
 		XMethod m = (XMethod) o;	
-		if (m.getName().startsWith("access$")) return null;
+		if (m.getName().startsWith("access$")) 
+			return null;
 		else if (m.getName().equals("<init>")) {
 			try {
 				if (throwableClass != null && Repository.instanceOf(m.getClassName(), throwableClass))
