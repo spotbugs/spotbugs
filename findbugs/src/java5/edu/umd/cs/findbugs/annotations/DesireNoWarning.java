0d3f335faead25161568a11bb884d245930133e0
Bill Pugh <bill.pugh@gmail.com>
2009-08-02 18:19:55 +0000

    new annotations for FindBugs test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11501 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java5/edu/umd/cs/findbugs/annotations/DesireNoWarning.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.umd.cs.findbugs.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation indicating that <em>no</em> FindBugs warning of the specified type
+ * is desired.
+ * 
+ * @author David Hovemeyer
+ */
+@Retention(RetentionPolicy.CLASS)
+public @interface DesireNoWarning {
+	// Comma-separated list of bug codes (e.g., "NP") not desired
+	// in the annotated method.
+	public String value();
+}
---
+++ findbugs/src/java5/edu/umd/cs/findbugs/annotations/DesireWarning.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.umd.cs.findbugs.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation indicating that a FindBugs warning is desired.
+ * 
+ * @author David Hovemeyer
+ */
+@Retention(RetentionPolicy.CLASS)
+public @interface DesireWarning {
+	// The value indicates the bug code (e.g., NP) of the desired warning.
+	public String value();
+}
