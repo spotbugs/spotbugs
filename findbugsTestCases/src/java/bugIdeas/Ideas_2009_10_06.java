71836f2343aa09779f28c7fcf628d984cf6c5541
Bill Pugh <bill.pugh@gmail.com>
2009-10-07 03:47:51 +0000

    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11579 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2009_10_06.java
@@ -0,0 +1,32 @@
+package bugIdeas;
+
+public class Ideas_2009_10_06 {
+	
+	void f(Object k, Object v) {
+		k.hashCode();
+		if (v == null) 
+			throw new NullPointerException();
+	
+	}
+	
+	void f2(Object k, Object v) {
+	
+		if (v == null) 
+			throw new NullPointerException();
+		k.hashCode();
+	
+	}
+	void testFirstArg() {
+		f(null, "a");
+	}
+	void testSecondArg() {
+		f("a", null);
+	}
+	void testFirstArg2() {
+		f2(null, "a");
+	}
+	void testSecondArg2() {
+		f2("a", null);
+	}
+
+}
