8c3b835cd3b3d69aab0ebe3d55feb3acb653fd8f
Bill Pugh <bill.pugh@gmail.com>
2009-10-07 02:21:39 +0000

    Check that we are doing the right thing for hashtable and properties nullness
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11578 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2009_10_05.java
@@ -0,0 +1,30 @@
+package bugIdeas;
+
+import java.util.Hashtable;
+import java.util.Properties;
+
+public class Ideas_2009_10_05 {
+	
+	public void propertiesCantContainNull(Properties p) {
+		p.put("a", null);
+		p.put(null, "a");
+		p.get(null);
+		p.getProperty(null);
+		p.getProperty(null, "foo");
+		p.setProperty(null, "a");
+		p.setProperty("a", null);
+				
+	}
+	
+	public void hashtablesCantContainNull(Hashtable h) {
+		h.put("a", null);
+		h.put(null, "a");
+		h.get(null);
+		h.contains(null);
+		h.containsKey(null);
+		h.containsValue(null);
+		h.remove(null);
+		
+		
+	}
+}
