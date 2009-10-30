3b83e9c329d6cfed78dded2d0e24d8bb6ca4e842
Bill Pugh <bill.pugh@gmail.com>
2009-10-30 01:04:32 +0000

    more bug ideas
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11622 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/bugIdeas/Ideas_2009_10_25.java
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2009_10_25.java
@@ -1,13 +1,19 @@
 package bugIdeas;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Ideas_2009_10_25 {
 	CountDownLatch latch = new CountDownLatch(1);
+	Lock lck = new ReentrantLock();
+	Condition c = lck.newCondition();
 	int x;
 	
 	public void set(int x) {
 		latch.notifyAll();
+		c.notify();
 		this.x = x;
 	}
 	
@@ -20,6 +26,7 @@ public class Ideas_2009_10_25 {
 	public int get() {
 		try {
 	        latch.wait();
+	        c.wait();
         } catch (InterruptedException e) {
 	        // TODO Auto-generated catch block
 	        e.printStackTrace();
@@ -30,6 +37,7 @@ public class Ideas_2009_10_25 {
 	public int get(int millis) {
 		try {
 	        latch.wait(millis);
+	        c.wait(millis);
         } catch (InterruptedException e) {
 	        // TODO Auto-generated catch block
 	        e.printStackTrace();
