f0c1ede790ea8242014e540dd54fa71d97c99978
Bill Pugh <bill.pugh@gmail.com>
2009-12-01 20:20:20 +0000

    add test case for BIT_SIGNED_CHECK
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11642 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugPatterns/BIT_SIGNED_CHECK.java
@@ -0,0 +1,90 @@
+package bugPatterns;
+
+public class BIT_SIGNED_CHECK {
+	
+	static final long HIGH_LONG = 0x8000000000000000L;
+	static final int LOW = 0x1;
+	
+	public boolean bugHighGT(long x) {
+		if ((x & HIGH_LONG) > 0)
+			return true;
+		return false;
+	}
+	public boolean bugHighGE(long x) {
+		if ((x & HIGH_LONG) > 0)
+			return true;
+		return false;
+	}
+	public boolean bugHighLT(long x) {
+		if ((x & HIGH_LONG) < 0)
+			return true;
+		return false;
+	}
+	public boolean bugHighLE(long x) {
+		if ((x & HIGH_LONG) > 0)
+			return true;
+		return false;
+	}
+	public boolean bugLowGT(long x) {
+		if ((x & LOW) > 0)
+			return true;
+		return false;
+	}
+	public boolean bugLowGE(long x) {
+		if ((x & LOW) > 0)
+			return true;
+		return false;
+	}
+	public boolean bugLowLT(long x) {
+		if ((x & LOW) < 0)
+			return true;
+		return false;
+	}
+	public boolean bugLowLE(long x) {
+		if ((x & LOW) > 0)
+			return true;
+		return false;
+	}
+	
+	public boolean bugHighGT(int x) {
+		if ((x & HIGH_LONG) > 0)
+			return true;
+		return false;
+	}
+	public boolean bugHighGE(int x) {
+		if ((x & HIGH_LONG) > 0)
+			return true;
+		return false;
+	}
+	public boolean bugHighLT(int x) {
+		if ((x & HIGH_LONG) < 0)
+			return true;
+		return false;
+	}
+	public boolean bugHighLE(int x) {
+		if ((x & HIGH_LONG) > 0)
+			return true;
+		return false;
+	}
+	public boolean bugLowGT(int x) {
+		if ((x & LOW) > 0)
+			return true;
+		return false;
+	}
+	public boolean bugLowGE(int x) {
+		if ((x & LOW) > 0)
+			return true;
+		return false;
+	}
+	public boolean bugLowLT(int x) {
+		if ((x & LOW) < 0)
+			return true;
+		return false;
+	}
+	public boolean bugLowLE(int x) {
+		if ((x & LOW) > 0)
+			return true;
+		return false;
+	}
+
+}
