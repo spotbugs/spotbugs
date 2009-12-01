c4229631710a136ec41b28c32c2f2edef821210a
Keith Lea <keithl@gmail.com>
2009-12-01 22:02:51 +0000

    Add test case (no bugs found) for bug 2893375
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11646 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2893375.java
@@ -0,0 +1,16 @@
+package sfBugs;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+
+public class Bug2893375 {
+	public boolean noBug() {
+		Field field = getField();
+		return field.getType() == String.class 
+				&& field.getAnnotation(Annotation.class) != null; 
+	}
+
+	private Field getField() {
+	    return null;
+    }
+}
