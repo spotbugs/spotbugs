fe8a8520430d6a61b13411e0a07401c6bd2ad268
Keith Lea <keithl@gmail.com>
2009-12-03 17:42:05 +0000

    Add test case for bug 2874036 (no bugs found)
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11651 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2874036.java
@@ -0,0 +1,20 @@
+package sfBugs;
+
+import java.util.ResourceBundle;
+
+import edu.umd.cs.findbugs.annotations.DesireNoWarning;
+
+public class Bug2874036 {
+	private static ResourceBundle createRb() { return null; }
+	
+    private final ResourceBundle rb = createRb();
+
+    @DesireNoWarning("NP")
+    public String getText(String key) {
+        String res = rb.getString(key);
+        if (res == null) {
+            System.out.println("getText: no language entry for '" + key + "'");
+        }
+        return res != null ? res : key;
+    }
+}
