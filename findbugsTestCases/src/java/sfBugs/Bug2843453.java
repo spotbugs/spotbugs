421914c64a1188de4877ed61c662d31818b11f69
Keith Lea <keithl@gmail.com>
2009-12-03 20:39:41 +0000

    Add sample code for bug 2843453 which crashes reproducibly.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11656 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2843453.java
@@ -0,0 +1,24 @@
+package sfBugs;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class Bug2843453 {
+	/**
+	 * An extending class.
+	 * 
+	 * Note: the issue does not occur when using {@link FileOutputStream} directly.
+	 */
+	static class ExtendFileOutputStream extends FileOutputStream {
+		ExtendFileOutputStream(File base) throws FileNotFoundException {
+			super(base, true);
+		}
+	}
+
+	void exposeIssue() throws Exception {
+		File file = new File("someFile.txt");
+		FileOutputStream fos = new ExtendFileOutputStream(file);
+		fos.close();
+	}
+}
