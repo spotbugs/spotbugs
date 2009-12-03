740963c57b07a600fdeda4101fa1b1f797b1bc34
Keith Lea <keithl@gmail.com>
2009-12-03 17:14:07 +0000

    Add test for ClassName.extractPackagePrefix.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11650 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/junit/edu/umd/cs/findbugs/util/ClassNameTest.java
+++ findbugs/src/junit/edu/umd/cs/findbugs/util/ClassNameTest.java
@@ -27,6 +27,14 @@ import junit.framework.TestCase;
  */
 public class ClassNameTest extends TestCase {
 
+	public void testExtractPackagePrefix() {
+		assertEquals("", ClassName.extractPackagePrefix("org.apache.ant.subpkg.sub2", 0));
+		assertEquals("org", ClassName.extractPackagePrefix("org", 1));
+		assertEquals("org.apache.ant", ClassName.extractPackagePrefix("org.apache.ant.subpkg.sub2", 3));
+		assertEquals("org.apache.ant.subpkg.sub2", ClassName.extractPackagePrefix("org.apache.ant.subpkg.sub2", 5));
+		assertEquals("org.apache.ant.subpkg.sub2", ClassName.extractPackagePrefix("org.apache.ant.subpkg.sub2", 6));
+	}
+	
 	public void testExtractClassName() {
 		assertEquals("java/lang/Integer", ClassName.extractClassName("Ljava/lang/Integer;"));
 		assertEquals("java/lang/Integer", ClassName.extractClassName("[Ljava/lang/Integer;"));
