de9e293bc6e89d4b032cedce01eb2f2c038ea297
Keith Lea <keithl@gmail.com>
2009-12-08 18:07:52 +0000

    Sample code for bugs 2843625 and 2910862
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11679 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2843625.java
@@ -0,0 +1,22 @@
+package sfBugs;
+
+import java.util.logging.Logger;
+
+public class Bug2843625 {
+	public static final Logger log = Logger.getAnonymousLogger();
+	public static  Logger log2 = Logger.getAnonymousLogger();
+	public Logger log3 = Logger.getAnonymousLogger();
+	
+	public static final Logger Log = Logger.getAnonymousLogger();
+	public static  Logger Log2 = Logger.getAnonymousLogger();
+	public Logger Log3 = Logger.getAnonymousLogger();
+
+	public static final Logger LOG = Logger.getAnonymousLogger();
+	public static  Logger LOG2 = Logger.getAnonymousLogger();
+	public Logger LOG3 = Logger.getAnonymousLogger();
+	
+
+	public enum ApplicationType {
+		Data, data, DATA
+	}
+}
