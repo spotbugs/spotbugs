0221d5c4b8e146394738439228fc3478a5b1097c
Bill Pugh <bill.pugh@gmail.com>
2009-12-08 15:52:19 +0000

    separate out the various cases here
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11676 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug2898106.java
+++ findbugsTestCases/src/java/sfBugs/Bug2898106.java
@@ -1,13 +1,14 @@
 package sfBugs;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Semaphore;
 
 public class Bug2898106 {
 
 	private static final ConcurrentHashMap<String, Semaphore> locks = new ConcurrentHashMap<String, Semaphore>();
-
-	private static Semaphore getLock(String key) {
+ 
+	static Semaphore getLock(String key) {
 		Semaphore lock = locks.get(key);
 		if (lock == null) {
 			Semaphore newLock = new Semaphore(1, true);
@@ -18,4 +19,31 @@ public class Bug2898106 {
 		}
 		return lock;
 	}
+//	static Semaphore getLock(ConcurrentMap<String, Semaphore> locks2, String key) {
+//		Semaphore lock = locks2.get(key);
+//		if (lock == null) {
+//			Semaphore newLock = new Semaphore(1, true);
+//			locks2.get(null);
+//			locks2.put(null, null);
+//			locks2.remove(null);
+//			locks2.containsKey(null);
+//			locks2.containsValue(null);
+//			locks2.putIfAbsent(null, null);
+//			locks2.remove(null, null);
+//			locks2.replace(null, null);
+//			locks2.replace(null, null, null);
+//			lock = locks2.putIfAbsent(key, lock);
+//			// value, being null, will *always* throw NullPointerException
+//			if (lock == null)
+//				lock = newLock;
+//		}
+//		return lock;
+//	}
+//	static Semaphore getLock(ConcurrentMap<String, Semaphore> locks2, Semaphore lock ,  String key) {
+//		if (lock == null) {
+//			lock = locks2.putIfAbsent(key, lock);
+//		}
+//		return lock;
+//	}
+
 }
