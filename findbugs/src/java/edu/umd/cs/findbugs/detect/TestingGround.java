65170a36c0ea087eb906375c504dd238978bca7d
Bill Pugh <bill.pugh@gmail.com>
2009-12-10 19:31:22 +0000

    Revert testing ground to something simple
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11688 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/TestingGround.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/TestingGround.java
@@ -19,22 +19,15 @@
 
 package edu.umd.cs.findbugs.detect;
 
-import org.apache.bcel.Constants;
 import org.apache.bcel.classfile.Code;
-import org.apache.bcel.generic.InstructionHandle;
 
-import edu.umd.cs.findbugs.BugInstance;
 import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.OpcodeStack;
 import edu.umd.cs.findbugs.OpcodeStack.Item;
 import edu.umd.cs.findbugs.ba.AnalysisContext;
 import edu.umd.cs.findbugs.ba.FieldSummary;
-import edu.umd.cs.findbugs.ba.XClass;
 import edu.umd.cs.findbugs.ba.XField;
 import edu.umd.cs.findbugs.bcel.OpcodeStackDetector;
-import edu.umd.cs.findbugs.classfile.CheckedAnalysisException;
-import edu.umd.cs.findbugs.classfile.ClassDescriptor;
-import edu.umd.cs.findbugs.classfile.DescriptorFactory;
-import edu.umd.cs.findbugs.classfile.Global;
 
 public class TestingGround extends OpcodeStackDetector {
 
@@ -46,181 +39,33 @@ public class TestingGround extends OpcodeStackDetector {
 
 	@Override
 	public void visit(Code code) {
-		boolean interesting = !getMethod().isSynchronized() && getMethod().isStatic() && !getMethodName().equals("<clinit>");
-		if (interesting) {
-			resetStateMachine();
-			// System.out.println();
-			// System.out.println(getFullyQualifiedMethodName());
+		boolean interesting = true;
+		if (interesting)  {
+			// initialize any variables we want to initialize for the method
 			super.visit(code); // make callbacks to sawOpcode for all opcodes
 		}
 	}
-
-	boolean interestingQuick(XField xField) {
-		if (xField.isFinal() || xField.isVolatile() || xField.isSynthetic() || !xField.isStatic())
-			return false;
-		if (xField.getName().indexOf('$') >= 0)
-			return false;
-		String sig = xField.getSignature();
-		char c = sig.charAt(0);
-		if (c != 'L' && c != '[')
-			return false;
-		if (sig.startsWith("Ljava/lang/"))
-			return false;
-
-		return true;
-	}
-
-	boolean interestingDeep(XField xField) {
-		String sig = xField.getSignature();
-		if (sig.charAt(0) == 'L') {
-			ClassDescriptor fieldType = DescriptorFactory.createClassDescriptorFromFieldSignature(sig);
-
-			while (fieldType != null) {
-				XClass fieldClass;
-				try {
-					fieldClass = Global.getAnalysisCache().getClassAnalysis(XClass.class, fieldType);
-				} catch (CheckedAnalysisException e) {
-					break;
-				}
-
-				String name = fieldClass.getClassDescriptor().getClassName();
-				if (name.startsWith("java/awt") || name.startsWith("javax/swing"))
-					return false;
-				if (name.equals("java/lang/Object"))
-					break;
-				fieldType = fieldClass.getSuperclassDescriptor();
-			}
-		}
-
-		return true;
-	}
-
-	@Override
-	public void sawBranchTo(int pc) {
-		if (state == 999)
-			state = 2;
-		else if (state != 10)
-			resetStateMachine();
-	}
-
-	int state;
-
-	int target;
-
-	int startPC;
-
-	boolean sawNew;
-
-	XField f;
-
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see edu.umd.cs.findbugs.bcel.OpcodeStackDetector#sawOpcode(int)
+	 */
 	@Override
 	public void sawOpcode(int seen) {
-		// System.out.printf("%5d %9s: %d\n", getPC(), OPCODE_NAMES[seen],
-		// state);
-		if (isReturn(seen) && state != 11 && target != -42) {
-			resetStateMachine();
-			return;
-		}
-
-		if (state > 1 && (getPC() >= target && target >= 0 || isReturn(seen) && target == -42)) {
-			if ((state == 4 || state == 3 && !f.isVolatile()) && interestingDeep(f)) {
-				// found it
-				int priority = LOW_PRIORITY;
-				boolean isDefaultAccess = (getMethod().getAccessFlags() & (Constants.ACC_PUBLIC | Constants.ACC_PRIVATE | Constants.ACC_PROTECTED)) == 0;
-				if (getMethod().isPublic())
-					priority = NORMAL_PRIORITY;
-				else if (getMethod().isProtected() || isDefaultAccess)
-					priority = NORMAL_PRIORITY;
-				String signature = f.getSignature();
-				if (signature.startsWith("[") || signature.startsWith("Ljava/util/"))
-					priority--;
-				if (!sawNew)
-					priority++;
-				if (state == 3 && priority < LOW_PRIORITY)
-					priority = LOW_PRIORITY;
-				if (getXClass().usesConcurrency())
-					priority--;
-				// Report the bug.
-				bugReporter.reportBug(new BugInstance(this, state == 4 ? "LI_LAZY_INIT_UPDATE_STATIC" : "LI_LAZY_INIT_STATIC",
-				        priority).addClassAndMethod(this).addField(f).describe("FIELD_ON").addSourceLineRange(getClassContext(),
-				        this, startPC, getPC()));
-
+		if (seen == INVOKESPECIAL 
+				&& getClassConstantOperand().equals("java/math/BigDecimal")
+				&& getNameConstantOperand().equals("<init>")
+				&& getSigConstantOperand().equals("(D)V")) {
+			OpcodeStack.Item top = stack.getStackItem(0);
+			Object value = top.getConstant();
+			if (value instanceof Double) {
+				System.out.println(value);
 			}
-			resetStateMachine();
+			
 		}
-		switch (state) {
-		case 0:
-			if (seen == GETSTATIC) {
-				XField xField = getXFieldOperand();
-				if (xField == null) {
-					
-					return;
-				}
-				if (interestingQuick(xField)) {
-					state = 1;
-					f = getXFieldOperand();
-					sawNew = false;
-					startPC = getPC();
-				}
-			}
-			break;
-		case 1:
-			if (seen == IFNONNULL) {
-				state = 999;
-				target = getBranchTarget();
-			} else if (seen == IFNULL) {
-				state = 10;
-				target = getBranchTarget();
-			} else
-				resetStateMachine();
-			break;
-		case 2:
-			if (seen == PUTSTATIC) {
-				if (f.equals(getXFieldOperand()))
-					state = 3;
-				else
-					resetStateMachine();
-			} else
-				switch (seen) {
-				case NEW:
-				case NEWARRAY:
-				case MULTIANEWARRAY:
-				case ANEWARRAY:
-					sawNew = true;
-					break;
-				case INVOKESTATIC:
-					if (getNameConstantOperand().startsWith("new"))
-						sawNew = true;
-					break;
-				}
-
-			break;
-		case 3:
-
-			if (seen == GETSTATIC && f.equals(getXFieldOperand()))
-				state = 4;
-			break;
-		case 10:
-			if (seen == GETSTATIC && f.equals(getXFieldOperand()))
-				state = 11;
-			break;
-		case 11:
-			if (isReturn(seen) && target == getPC() + 1) {
-				state = 2;
-				target = -42;
-			} else
-				resetStateMachine();
-			break;
-		}
-
+				
 	}
 
-	/**
-     * 
-     */
-	private void resetStateMachine() {
-		state = 0;
-		target = -1;
-	}
+	
 
 }
