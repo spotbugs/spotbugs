94d78ed12cd5189a2fa1504504b97f52c7aa1c3b
Keith Lea <keithl@gmail.com>
2009-12-16 16:26:56 +0000

    Test cases for BigDecimal detector (for RFE 2891944)
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11699 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/RFE2891944.java
+++ findbugsTestCases/src/java/sfBugs/RFE2891944.java
@@ -3,10 +3,17 @@ package sfBugs;
 import java.math.BigDecimal;
 
 public class RFE2891944 {
-	
-	public static void main(String args[]) {
-		
+
+	public static void bug1() {
 		BigDecimal bd = new BigDecimal(0.1);
+		System.out.println(bd);
+	}
+	public static void bug2() {
+		BigDecimal bd = new BigDecimal(100.0);
+		System.out.println(bd);
+	}
+	public static void bug3() {
+		BigDecimal bd = new BigDecimal(1.0);
 		System.out.println(bd);
 	}
 
