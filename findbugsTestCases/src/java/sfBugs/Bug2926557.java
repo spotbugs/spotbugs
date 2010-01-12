db2d90dd2d6ead2e88765fef1fbebedd9b25bd5c
Bill Pugh <bill.pugh@gmail.com>
2010-01-12 21:13:35 +0000

    add test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11754 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2926557.java
@@ -0,0 +1,24 @@
+package sfBugs;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Bug2926557 {
+
+	private static final Calendar cal = Calendar.getInstance();
+
+	private static final ReentrantLock lock = new ReentrantLock();
+
+	public static Date changeMonth(Date month, int value) {
+		lock.lock();
+		try {
+			cal.setTime(month);
+			cal.add(Calendar.MONTH, value);
+			return cal.getTime();
+		} finally {
+			lock.unlock();
+		}
+
+	}
+}
---
+++ findbugsTestCases/src/java/sfBugs/Bug2928673.java
@@ -0,0 +1,50 @@
+package sfBugs;
+
+import java.io.CharArrayWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Formatter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class Bug2928673 {
+	static final Logger LOGGER = Logger.getAnonymousLogger();
+
+	public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
+	        throws IOException, ServletException {
+		if (response instanceof HttpServletResponse) {
+			final PrintWriter out = response.getWriter();
+			final HttpServletResponse wrapper = (HttpServletResponse) response;
+			chain.doFilter(request, wrapper);
+			final String origData = wrapper.getContentType();
+			if (LOGGER.isLoggable(Level.FINE)) {
+				LOGGER.log(Level.FINE, "Hello");
+			}
+			if ("text/html".equals(wrapper.getContentType())) {
+				final CharArrayWriter caw = new CharArrayWriter();
+				final int bodyIndex = origData.indexOf("</body>");
+				if (-1 != bodyIndex) {
+					caw.write(origData.substring(0, bodyIndex - 1));
+					caw.write("\n<p>My custom footer</p>");
+					caw.write("\n</body></html>");
+					response.setContentLength(caw.toString().length());
+					out.write(caw.toString());
+				} else {
+					out.write(origData);
+				}
+			} else {
+				out.write(origData);
+			}
+			out.close();
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+
+}
