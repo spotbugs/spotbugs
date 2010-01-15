7c7fb3337cb5cb8aa52dc5a54dea0aaa5e5185e5
Bill Pugh <bill.pugh@gmail.com>
2010-01-15 15:50:06 +0000

    add bugs
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11762 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2912638.java
@@ -0,0 +1,41 @@
+package sfBugs;
+
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+
+public class Bug2912638 {
+
+	private String name;
+
+	public void setName(String _name) {
+		name = _name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@ExpectWarning("ES_COMPARING_STRINGS_WITH_EQ")
+	protected static void findBugsTest(Bug2912638 person) {
+		String value = person.getName();
+
+		if ("FOO" == value)
+			System.out.println("a");
+		else
+			System.out.println("a3");
+		if (value == "FOO")
+			System.out.println("a");
+		else
+			System.out.println("a3");
+	}
+
+	@ExpectWarning("ES_COMPARING_STRINGS_WITH_EQ")
+	protected static void findBugsTest2(Bug2912638 person) {
+		String value = person.getName();
+
+		if (value == "FOO")
+			System.out.println("a");
+		else
+			System.out.println("a3");
+	}
+
+}
---
+++ findbugsTestCases/src/java/sfBugs/Bug2931203.java
@@ -0,0 +1,14 @@
+package sfBugs;
+
+public class Bug2931203 {
+
+	public static void main(String args[]) {
+		if ("Test".startsWith("T")) {
+			System.out.println("Yes it starts with a T");
+		} else {
+			System.out.println("No it doesn't start with a T");
+		}
+
+	}
+
+}
