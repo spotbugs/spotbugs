158b51701ff967f77ab1ef5b614d5a0aef881b56
Bill Pugh <bill.pugh@gmail.com>
2010-01-15 22:37:54 +0000

    add test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11763 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/RFE1603449.java
@@ -0,0 +1,57 @@
+package sfBugs;
+
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+
+public class RFE1603449 {
+
+	int foo(Object a) {
+		if (a == null)
+			throw new NullPointerException();
+		return a.hashCode();
+	}
+
+	int foo2(Object a) {
+		return a.hashCode();
+	}
+
+	int foo3(Object a) {
+		return a.hashCode();
+	}
+
+	@ExpectWarning("NP")
+	int test() {
+		Object a = null;
+		return foo(a);
+	}
+
+	@ExpectWarning("NP")
+	int test2() {
+		Object a = null;
+		return foo2(a);
+	}
+
+	@ExpectWarning("NP")
+	int test3() {
+		Object a = null;
+		return foo3(a);
+	}
+	@ExpectWarning("NP")
+	int testa(Object a) {
+		if (a == null)
+			System.out.println("a is null");
+		return foo(a);
+	}
+	@ExpectWarning("NP")
+	int test2a(Object a) {
+		if (a == null)
+			System.out.println("a is null");
+		return foo2(a);
+	}
+	@ExpectWarning("NP")
+	int test3a(Object a) {
+		if (a == null)
+			System.out.println("a is null");
+		return foo3(a);
+	}
+
+}
