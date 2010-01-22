fe8893a3d378a91ebb798d40e6863680a60f1952
Bill Pugh <bill.pugh@gmail.com>
2010-01-22 06:26:03 +0000

    new bug patterns
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11792 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2010_01_21.java
@@ -0,0 +1,31 @@
+package bugIdeas;
+
+import java.util.Map;
+import java.util.Set;
+
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+
+public class Ideas_2010_01_21<T extends Map> {
+
+	@ExpectWarning("GC")
+	public  boolean test(T t) {
+		Set s = t.entrySet();
+		return s.contains(5);
+	}
+
+	volatile int x;
+	volatile long y;
+	public void volatileIncrement() {
+		x++;
+	}
+	public void volatileDecrement() {
+		x--;
+	}
+	public void volatileIncrementLong() {
+		y++;
+	}
+	public void volatileDecrementLong() {
+		y--;
+	}
+
+}
