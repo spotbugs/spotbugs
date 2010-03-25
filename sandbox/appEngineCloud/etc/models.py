0a839abcdae734b83df28a7966010f972b1400c1
Keith Lea <keithl@gmail.com>
2010-03-25 19:39:14 +0000

    Some changes to allow uploading entities in bulk from CSV file. Not complete yet, still talking with AppEngine devs to figure out how to handle the kinds of primary keys we use.
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@11964 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- sandbox/appEngineCloud/etc/models.py
+++ sandbox/appEngineCloud/etc/models.py
@@ -4,104 +4,144 @@ from google.appengine.ext import db
 from google.appengine.tools import bulkloader
 import datetime
 
-class DbUser(db.Model):
+#============================== DbUser ==============================
+
+class AppEngineDbUser(db.Model):
     email = db.StringProperty()
 
-class DbUserLoader(bulkloader.Loader):
+class AppEngineDbUserLoader(bulkloader.Loader):
     def __init__(self):
-        bulkloader.Loader.__init__(self, 'DbUser',
-                                   [('email', str)
+        bulkloader.Loader.__init__(self, 'AppEngineDbUser',
+                                   [('openid', str),
+                                    ('email', str)
                                    ])
+    def handle_entity(self, entity):
+        entity.openid = None
+        return entity
+    def generate_key(self, i, values):
+        return values[0]
 
-class DbUserExporter(bulkloader.Exporter):
+class AppEngineDbUserExporter(bulkloader.Exporter):
     def __init__(self):
-        bulkloader.Exporter.__init__(self, 'DbUser',
-                                     [ ('__key__', lambda x: x.to_path()[1], None),
-                                      ('email', str, None)
+        bulkloader.Exporter.__init__(self, 'AppEngineDbUser',
+                                     [ ('__key__', lambda x: x.to_path()[1], ''),
+                                      ('email', str, '')
                                      ])
 
-class DbInvocation(db.Model):
-    who = db.ReferenceProperty(DbUser)
+#=========================== DbInvocation ============================
+
+class AppEngineDbInvocation(db.Model):
+    who = db.ReferenceProperty(AppEngineDbUser)
     startTime = db.IntegerProperty()
     endTime = db.IntegerProperty()
 
-class DbInvocationLoader(bulkloader.Loader):
+class AppEngineDbInvocationLoader(bulkloader.Loader):
     def __init__(self):
-        bulkloader.Loader.__init__(self, 'DbInvocation',
-                                   [('who', db.Key),
-                                   ('startTime', long),
-                                   ('endTime', long)
+        bulkloader.Loader.__init__(self, 'AppEngineDbInvocation',
+                                   [('pk', int),
+                                    ('who', lambda x: db.Key.from_path('AppEngineDbUser', x)),
+                                    ('startTime', long),
+                                    ('endTime', long)
                                    ])
+    def handle_entity(self, entity):
+        entity.pk = None
+        return entity
+    def generate_key(self, i, values):
+        return int(values[0])
 
-class DbInvocationExporter(bulkloader.Exporter):
+class AppEngineDbInvocationExporter(bulkloader.Exporter):
     def __init__(self):
-        bulkloader.Exporter.__init__(self, 'DbInvocation',
-                                     [ ('__key__', lambda x: x.to_path()[1], None),
-                                       ('who', lambda x: x.to_path()[1], None),
-                                       ('startTime', str, None),
+        bulkloader.Exporter.__init__(self, 'AppEngineDbInvocation',
+                                     [ ('__key__', lambda x: x.to_path()[1], ''),
+                                       ('who', lambda x: x.to_path()[1], ''),
+                                       ('startTime', str, ''),
                                        ('endTime', str, '')
                                      ])
 
 
-class DbIssue(db.Model):
+#============================= DbIssue ==============================
+
+class AppEngineDbIssue(db.Model):
     bugPattern = db.StringProperty()
     priority = db.IntegerProperty()
     primaryClass = db.StringProperty()
     firstSeen = db.IntegerProperty()
     lastSeen = db.IntegerProperty()
     bugLink = db.StringProperty()
+    bugLinkType = db.StringProperty()
 
-class DbIssueLoader(bulkloader.Loader):
+class AppEngineDbIssueLoader(bulkloader.Loader):
     def __init__(self):
-        bulkloader.Loader.__init__(self, 'DbIssue',
-                                   [('bugPattern', str),
-                                   ('priority', int),
-                                   ('primaryClass', str),
-                                   ('firstSeen', long),
-                                   ('lastSeen', long),
-                                   ('bugLink', str)
+        bulkloader.Loader.__init__(self, 'AppEngineDbIssue',
+                                   [('issueHash', str),
+                                    ('bugPattern', str),
+                                    ('priority', int),
+                                    ('primaryClass', str),
+                                    ('firstSeen', long),
+                                    ('lastSeen', long),
+                                    ('bugLink', str),
+                                    ('bugLinkType', str)
                                    ])
+    def handle_entity(self, entity):
+        entity.issueHash = None
+        if (entity.bugLink == 'None'): entity.bugLink = None
+        if (entity.bugLinkType == 'None'): entity.bugLinkType = None
 
-class DbIssueExporter(bulkloader.Exporter):
+        return entity
+    def generate_key(self, i, values):
+        return db.Key.from_path('AppEngineDbInvocation', int(values[0]))
+
+class AppEngineDbIssueExporter(bulkloader.Exporter):
     def __init__(self):
-        bulkloader.Exporter.__init__(self, 'DbIssue',
-                                     [ ('__key__', lambda x: x.to_path()[1], None),
-                                      ('bugPattern', str, None),
-                                      ('priority', str, None),
-                                      ('primaryClass', str, None),
-                                      ('firstSeen', str, None),
-                                      ('lastSeen', str, 0),
-                                      ('bugLink', str, '')
+        bulkloader.Exporter.__init__(self, 'AppEngineDbIssue',
+                                     [('__key__', lambda x: x.to_path()[1], ''),
+                                      ('bugPattern', str, ''),
+                                      ('priority', str, ''),
+                                      ('primaryClass', str, ''),
+                                      ('firstSeen', str, ''),
+                                      ('lastSeen', str, ''),
+                                      ('bugLink', str, ''),
+                                      ('bugLinkType', str, '')
                                      ])
-class DbEvaluation(db.Model):
-    who = db.ReferenceProperty(DbUser)
+
+#============================ DbEvaluation ==============================
+
+class AppEngineDbEvaluation(db.Model):
+    who = db.ReferenceProperty(AppEngineDbUser)
     designation = db.StringProperty()
     comment = db.StringProperty()
     when = db.IntegerProperty()
-    invocation  = db.ReferenceProperty(DbInvocation)
+    invocation  = db.ReferenceProperty(AppEngineDbInvocation)
 
-class DbEvaluationLoader(bulkloader.Loader):
+class AppEngineDbEvaluationLoader(bulkloader.Loader):
     def __init__(self):
-        bulkloader.Loader.__init__(self, 'DbEvaluation',
+        bulkloader.Loader.__init__(self, 'AppEngineDbEvaluation',
                                    [('who', str),
                                    ('designation', str),
                                    ('comment', str),
+                                   ('issueHash', str),
                                    ('when', long),
                                    ('invocation', db.Key)
                                    ])
+    def handle_entity(self, entity):
+        entity.issueHash = None
+        return entity
+    def generate_key(self, i, values):
+        return values[3]
 
-class DbEvaluationExporter(bulkloader.Exporter):
+class AppEngineDbEvaluationExporter(bulkloader.Exporter):
     def __init__(self):
-        bulkloader.Exporter.__init__(self, 'DbEvaluation',
-                                     [ ('who', lambda x: x.to_path()[1], None),
+        bulkloader.Exporter.__init__(self, 'AppEngineDbEvaluation',
+                                     [ ('who', lambda x: x.to_path()[1], ''),
                                        ('designation', str, ''),
                                        ('comment', str, ''),
-                                       ('__key__', lambda x: x.to_path()[1], None),
-                                       ('when', str, None),
-                                       ('invocation', lambda x: x.to_path()[1], None),
+                                       ('__key__', lambda x: x.to_path()[1], ''),
+                                       ('when', str, ''),
+                                       ('invocation', lambda x: x.to_path()[1], ''),
                                      ])
 
+#============================ initialization ==============================
 
-loaders = [DbUserLoader, DbIssueLoader, DbEvaluationLoader, DbInvocationLoader]
+loaders = [AppEngineDbUserLoader, AppEngineDbIssueLoader, AppEngineDbEvaluationLoader, AppEngineDbInvocationLoader]
 
-exporters = [DbUserExporter, DbIssueExporter, DbEvaluationExporter, DbInvocationExporter]
+exporters = [AppEngineDbUserExporter, AppEngineDbIssueExporter, AppEngineDbEvaluationExporter, AppEngineDbInvocationExporter]
