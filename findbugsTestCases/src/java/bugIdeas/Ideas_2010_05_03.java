2ddbd15d28d5612545ac3408d4c69f5ea77de056
Bill Pugh <bill.pugh@gmail.com>
2010-05-06 12:47:55 +0000

    additional test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@12112 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2010_05_03.java
@@ -0,0 +1,80 @@
+package bugIdeas;
+
+public class Ideas_2010_05_03 {
+	
+	public Ideas_2010_05_03(Boolean x, Boolean b, int y, int z) {
+	    this.x = x;
+	    this.b = b;
+	    this.y = y;
+	    this.z = z;
+    }
+
+	Boolean x,b;
+	int y;
+	int z;
+	
+	public void veryBad() {
+		synchronized(x) {
+			if (x) {
+				x = false;
+				y = 2;
+				z = 3;
+			}
+		}
+	}
+	public void bad() {
+		synchronized(x) {
+			if (x) {
+				y = 2;
+				z = 3;
+				x = false;
+			}
+		}
+	}
+	
+	public void veryBad2() {
+		synchronized(x) {
+			synchronized(b) {
+				y++;
+			}
+		}
+	}
+	
+	public void veryBad3() {
+		synchronized(b) {
+			bad();
+		}
+	}
+	public void bad2() {
+		synchronized(b) {
+			if (b.booleanValue()) 
+				b = false;
+		}
+	}
+	String s;
+	public void bad3() {
+		synchronized(x) {
+			try {
+				s = toString();
+				x = false;
+			} catch(RuntimeException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public void veryBad4() {
+		synchronized(x) {
+			try {
+				x = false;
+				s = toString();
+			} catch(RuntimeException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public String toString() {
+		return String.format("%s %s %s %s ", b, x, y, z);
+	}
+
+
+}
