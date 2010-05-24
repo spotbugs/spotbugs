91a6ed90dfa4162ad9d939bb9bf059fbc5175eac
Bill Pugh <bill.pugh@gmail.com>
2010-05-24 21:24:54 +0000

    test case for False positive for source forge bug RCN_REDUNDANT_NULLCHECK_OF_NULL
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@12122 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2990536.java
@@ -0,0 +1,31 @@
+package sfBugs;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.StringReader;
+
+public class Bug2990536 {
+	public void foo(Bar bar) throws IOException {
+
+		while (true) {
+			Closeable c = new StringReader("foo");
+			try {
+				try {
+					bar.bar(c);
+					c = null;
+				} catch (InterruptedException ex) {
+					continue;
+				}
+			} finally {
+				if (c != null) {
+					c.close();
+				}
+			}
+		}
+	}
+
+	interface Bar {
+		void bar(Closeable c) throws InterruptedException;
+	}
+
+}
