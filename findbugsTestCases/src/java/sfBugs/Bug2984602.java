0bbb1208e149b7d09a96a5a2cdec8113ee614557
Bill Pugh <bill.pugh@gmail.com>
2010-05-24 20:20:54 +0000

    Add test case for false positive on Class.isInstance for equals() che - ID: 2984602
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@12118 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug2984602.java
@@ -0,0 +1,27 @@
+package sfBugs;
+
+public class Bug2984602 {
+
+	int itemID;
+
+	public int hashCode() {
+		return itemID;
+	}
+	
+
+	public boolean equals(Object object) {
+		if (!(Bug2984602.class.isInstance(object))) {
+			return false;
+		}
+
+		boolean equals = false;
+		Bug2984602 compareTo = (Bug2984602) object;
+
+		if (this.itemID == compareTo.itemID) {
+			equals = true;
+		}
+
+		return equals;
+	}
+
+}
