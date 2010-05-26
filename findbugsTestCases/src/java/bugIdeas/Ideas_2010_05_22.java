d1a7daa9bfe88e13e564a5b9c93c97327b1d7b72
Bill Pugh <bill.pugh@gmail.com>
2010-05-26 23:29:38 +0000

    test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@12137 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2010_05_22.java
@@ -0,0 +1,32 @@
+package bugIdeas;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.reflect.Field;
+
+public class Ideas_2010_05_22  {
+
+	@Retention(RetentionPolicy.CLASS)
+	@interface NotRuntimeVisible {}
+	
+	@Retention(RetentionPolicy.RUNTIME)
+	@interface RuntimeVisible {}
+	
+	public @RuntimeVisible int x;
+	public @NotRuntimeVisible int y;
+	
+	public static void main(String args []) throws Exception {
+		Class c = Ideas_2010_05_22.class;
+		Field xField = c.getField("x");
+		Field yField = c.getField("y");
+		
+		if (xField.isAnnotationPresent(RuntimeVisible.class)) {
+			System.out.println("x");
+		}
+		if (yField.isAnnotationPresent(NotRuntimeVisible.class)) {
+			System.out.println("y");
+		}
+	}
+
+
+}
