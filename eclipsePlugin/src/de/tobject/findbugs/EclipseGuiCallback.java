9322a5bcc2d7c44eb96e64433b3d49915cd44b49
Keith Lea <keithl@gmail.com>
2010-05-27 21:36:27 +0000

    Add status bar component showing FindBugs Cloud sign-in status. Also adds popup menu when you click. Not sure this is the best way to do this, the status bar only shows when the cursor is in the editor itself...
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@12148 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- eclipsePlugin/src/de/tobject/findbugs/EclipseGuiCallback.java
+++ eclipsePlugin/src/de/tobject/findbugs/EclipseGuiCallback.java
@@ -3,6 +3,7 @@
  */
 package de.tobject.findbugs;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.List;
@@ -10,11 +11,28 @@ import java.util.concurrent.AbstractExecutorService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import org.eclipse.jface.action.ContributionItem;
+import org.eclipse.jface.action.IStatusLineManager;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Cursor;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.program.Program;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IViewSite;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
@@ -25,59 +43,67 @@ import edu.umd.cs.findbugs.IGuiCallback;
 import edu.umd.cs.findbugs.Project;
 import edu.umd.cs.findbugs.cloud.Cloud;
 import edu.umd.cs.findbugs.cloud.Cloud.CloudListener;
+import edu.umd.cs.findbugs.cloud.Cloud.CloudStatusListener;
+import edu.umd.cs.findbugs.cloud.Cloud.SigninState;
+import edu.umd.cs.findbugs.cloud.username.AppEngineNameLookup;
 
 public class EclipseGuiCallback implements IGuiCallback {
-	private final AbstractExecutorService guiExecutor = new AbstractExecutorService() {
-
-		public boolean awaitTermination(long timeout, TimeUnit unit)
-				throws InterruptedException {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		public boolean isShutdown() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		public boolean isTerminated() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		public void shutdown() {
-			// TODO Auto-generated method stub
-
-		}
-
-		public List<Runnable> shutdownNow() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		public void execute(Runnable command) {
-			Display.getDefault().asyncExec(command);
-		}
-
-	};
+		private final AbstractExecutorService guiExecutor = new EclipseDisplayThreadExecutor();
 	private CloudListener cloudListener;
+	private CloudStatusListener cloudStatusListener;
+	private Label loginStatusBarItem;
+	private Cloud cloud;
 
 
 	public void registerCloud(Project project, BugCollection collection, final Cloud cloud) {
+		this.cloud = cloud;
+
+		IWorkbenchWindow[] windows = FindbugsPlugin.getDefault().getWorkbench().getWorkbenchWindows();
+		IWorkbenchPage page = FindbugsPlugin.getActiveWorkbenchWindow().getActivePage();
+		IWorkbenchPart part = page.getActivePart();
+		IViewPart btv = page.findView("de.tobject.findbugs.view.bugtreeview");
+		IViewSite btvsite = btv.getViewSite();
+
+		if (part instanceof IEditorPart) {
+			final IEditorPart epart = (IEditorPart) part;
+			FindbugsPlugin.getShell().getDisplay().asyncExec(new Runnable() {
+
+				public void run() {
+					IStatusLineManager slm = epart.getEditorSite().getActionBars().getStatusLineManager();
+					slm.add(new ContributionItem() {
+
+						@Override
+						public void fill(Composite parent) {
+							createLoginStatusBarItem(parent);
+							updateLoginStatusBarItem();
+						}
+
+					});
+					slm.update(true);
+				}
+			});
+
+		}
 		cloudListener = new CloudListener() {
 			public void statusUpdated() {
 				final String statusMsg = cloud.getStatusMsg();
 
 				final IWorkbenchWindow win = FindbugsPlugin.getActiveWorkbenchWindow();
 				win.getShell().getDisplay().asyncExec(new Runnable() {
+					@SuppressWarnings("hiding")
 					public void run() {
 						IWorkbenchPage page = win.getActivePage();
-
 						IWorkbenchPart part = page.getActivePart();
 
 						if (part instanceof IEditorPart) {
 							IEditorPart epart = (IEditorPart) part;
-							epart.getEditorSite().getActionBars().getStatusLineManager().setMessage(statusMsg);
+							Image image = FindbugsPlugin.getDefault().getImageRegistry().get("buggy-tiny-gray.png");
+							IStatusLineManager statusLineManager = epart.getEditorSite().getActionBars().getStatusLineManager();
+							if (statusMsg.equals("")) {
+								statusLineManager.setMessage("");
+							} else {
+								statusLineManager.setMessage(image, statusMsg);
+							}
 						}
 					}
 				});
@@ -88,10 +114,90 @@ public class EclipseGuiCallback implements IGuiCallback {
 			}
 		};
 		cloud.addListener(cloudListener);
+		cloudStatusListener = new CloudStatusListener() {
+
+			public void handleStateChange(SigninState oldState, SigninState state) {
+				updateLoginStatusBarItem();
+			}
+
+			public void handleIssueDataDownloadedEvent() {
+				// TODO Auto-generated method stub
+
+			}
+		};
+		cloud.addStatusListener(cloudStatusListener);
+	}
+
+	private void showSigninPopupMenu(Point p) {
+		Menu popupMenu = new Menu(loginStatusBarItem);
+	    MenuItem automaticCheckbox = new MenuItem(popupMenu, SWT.CHECK);
+	    automaticCheckbox.setText("Sign in automatically");
+	    SigninState state = cloud.getSigninState();
+		automaticCheckbox.setEnabled(state != SigninState.NO_SIGNIN_REQUIRED);
+	    final boolean origSelection = AppEngineNameLookup.isSavingSessionInfoEnabled();
+		automaticCheckbox.setSelection(origSelection);
+	    automaticCheckbox.addSelectionListener(new SelectionListener() {
+
+			public void widgetSelected(SelectionEvent e) {
+				AppEngineNameLookup.setSaveSessionInformation(!origSelection);
+			}
+
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+	    if (state == SigninState.NOT_SIGNED_IN_YET || state == SigninState.SIGNED_OUT
+	    		|| state == SigninState.SIGNIN_FAILED) {
+		    MenuItem signInItem = new MenuItem(popupMenu, SWT.NONE);
+		    signInItem.setText("Sign in");
+		    signInItem.addSelectionListener(new SelectionListener() {
+
+				public void widgetSelected(SelectionEvent e) {
+					try {
+						cloud.signIn();
+					} catch (IOException e1) {
+						MessageDialog.openError(FindbugsPlugin.getShell(), "Error", e1.toString());
+					}
+				}
+
+				public void widgetDefaultSelected(SelectionEvent e) {
+					// TODO Auto-generated method stub
+
+				}
+			});
+
+	    } else if (state == SigninState.SIGNING_IN || state == SigninState.SIGNED_IN) {
+	    	MenuItem signOutItem = new MenuItem(popupMenu, SWT.NONE);
+		    signOutItem.setText("Sign out");
+		    signOutItem.addSelectionListener(new SelectionListener() {
+
+				public void widgetSelected(SelectionEvent e) {
+					cloud.signOut();
+				}
+
+				public void widgetDefaultSelected(SelectionEvent e) {
+					// TODO Auto-generated method stub
+
+				}
+			});
+	    }
+	    popupMenu.setLocation(p);
+	    popupMenu.setVisible(true);
+	}
+
+	private void updateLoginStatusBarItem() {
+		if (loginStatusBarItem != null && cloud != null) {
+			loginStatusBarItem.setVisible(cloud.getSigninState() != SigninState.NO_SIGNIN_REQUIRED);
+			loginStatusBarItem.setText(cloud.getSigninState().name());
+		}
 	}
 
 	public void unregisterCloud(Project project, BugCollection collection, Cloud cloud) {
 		cloud.removeListener(cloudListener);
+		cloud.removeStatusListener(cloudStatusListener);
+		this.cloud = null;
 	}
 
 	public String showQuestionDialog(String message, String title, String defaultValue) {
@@ -147,4 +253,64 @@ public class EclipseGuiCallback implements IGuiCallback {
 	public void displayNonmodelMessage(String title, String message) {
 		throw new UnsupportedOperationException();
 	}
+
+	private void createLoginStatusBarItem(Composite parent) {
+		Composite comp = new Composite(parent, SWT.NONE);
+		comp.setLayout(new GridLayout());
+		loginStatusBarItem = new Label(comp, SWT.NONE);
+		loginStatusBarItem.setLayoutData(new GridData(GridData.CENTER, GridData.CENTER,
+				false, false));
+		loginStatusBarItem.setAlignment(SWT.CENTER);
+		loginStatusBarItem.setCursor(new Cursor(null, SWT.CURSOR_HAND));
+		loginStatusBarItem.addMouseListener(new MouseListener() {
+
+			public void mouseUp(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			public void mouseDown(MouseEvent e) {
+
+			    showSigninPopupMenu(loginStatusBarItem.toDisplay(e.x, e.y));
+			}
+
+			public void mouseDoubleClick(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+	}
+
+	private final class EclipseDisplayThreadExecutor extends AbstractExecutorService {
+		public boolean awaitTermination(long timeout, TimeUnit unit)
+				throws InterruptedException {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		public boolean isShutdown() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		public boolean isTerminated() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		public void shutdown() {
+			// TODO Auto-generated method stub
+
+		}
+
+		public List<Runnable> shutdownNow() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public void execute(Runnable command) {
+			Display.getDefault().asyncExec(command);
+		}
+	}
+
 }
