401c5f640248996dd7629750eb109b81009c1ea5
Bill Pugh <bill.pugh@gmail.com>
2010-06-19 13:47:38 +0000

    More bug ideas
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@12217 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2010_06_08.java
@@ -0,0 +1,58 @@
+package bugIdeas;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+
+public class Ideas_2010_06_08 {
+	
+	
+	/**
+	 * Method would be much more usable if it used wildcards
+	 */
+	public static void closeAllSilently(Iterable<Closeable> closeMe) {
+		for(Closeable c : closeMe) {
+			try {
+	            c.close();
+            } catch (IOException e) {
+	           assert true;
+            }
+		}
+	}
+	public static void closeAllSilentlyFP(Iterable<? extends Closeable> closeMe) {
+		for(Closeable c : closeMe) {
+			try {
+	            c.close();
+            } catch (IOException e) {
+	           assert true;
+            }
+		}
+	}
+	
+	public static boolean all(Predicate<String> predicate, List<String> lst) {
+		for(String s : lst)
+			if (!predicate.apply(s))
+				return false;
+		return true;
+		
+	}
+	public static boolean allFP(Predicate<? super String> predicate, List<String> lst) {
+		for(String s : lst)
+			if (!predicate.apply(s))
+				return false;
+		return true;
+		
+	}
+	public static void testAll() {
+		List<String> lst = Collections.emptyList();
+		// boolean b1 = all(Predicates.alwaysTrue(),lst);
+		boolean b2 = allFP(Predicates.alwaysTrue(),lst);
+		
+	}
+
+}
