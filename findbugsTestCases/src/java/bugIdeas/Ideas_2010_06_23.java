896559ac1491bf8d80ea6f0461e1430b2422fe78
Bill Pugh <bill.pugh@gmail.com>
2010-06-29 20:17:18 +0000

    added test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@12264 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2010_06_23.java
@@ -0,0 +1,31 @@
+package bugIdeas;
+import java.sql.Date;
+
+import org.joda.time.DateTimeConstants;
+
+
+public class Ideas_2010_06_23 {
+	
+	public long daysAgo(int days) {
+		return System.currentTimeMillis() - days * DateTimeConstants.MILLIS_PER_DAY;
+	}
+	public long daysFromNow(int days) {
+		return System.currentTimeMillis() + days * DateTimeConstants.MILLIS_PER_DAY;
+	}
+
+	public long daysFromNow2(int days) {
+		return  days * DateTimeConstants.MILLIS_PER_DAY + System.currentTimeMillis();
+	}
+	
+	public long daysFrom(Date d, int days) {
+		return  days * DateTimeConstants.MILLIS_PER_DAY + d.getTime();
+	}
+	public long daysBefore(Date d, int days) {
+		return   d.getTime() - days * DateTimeConstants.MILLIS_PER_DAY;
+	}
+	
+	public static void main(String args[]) {
+		System.out.println(DateTimeConstants.MILLIS_PER_DAY);
+		System.out.println(Integer.MAX_VALUE / DateTimeConstants.MILLIS_PER_DAY);
+	}
+}
