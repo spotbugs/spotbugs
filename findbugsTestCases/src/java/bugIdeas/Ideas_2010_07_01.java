ca905ad9109fc9d254398e17e8992163cd2c94eb
Bill Pugh <bill.pugh@gmail.com>
2010-07-01 17:48:37 +0000

    add test case for regular expressions with flags, making sure we generate correct msgs
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@12279 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2010_07_01.java
@@ -0,0 +1,21 @@
+package bugIdeas;
+
+import java.util.regex.Pattern;
+
+public class Ideas_2010_07_01 {
+
+	public static void main(String args[]) {
+		Pattern.compile("+");
+		Pattern.compile("+", Pattern.CANON_EQ);
+		Pattern.compile("+", Pattern.CASE_INSENSITIVE);
+		Pattern.compile("+", Pattern.COMMENTS);
+		Pattern.compile("+", Pattern.DOTALL);
+		Pattern.compile("+", Pattern.LITERAL); // Not an error
+		Pattern.compile("+", Pattern.MULTILINE);
+		Pattern.compile("+", Pattern.UNICODE_CASE);
+		Pattern.compile("+", Pattern.UNIX_LINES);
+		Pattern.compile("+", Pattern.UNICODE_CASE | Pattern.UNIX_LINES);
+
+	}
+
+}
