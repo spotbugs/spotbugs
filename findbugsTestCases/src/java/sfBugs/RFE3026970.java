bcb0d963d4777abe25e96321002339a719ca227a
Keith Lea <keithl@gmail.com>
2010-07-19 17:25:14 +0000

    Add sample code for RFE 3026970
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@12324 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/RFE3026970.java
@@ -0,0 +1,18 @@
+package sfBugs;
+
+import edu.umd.cs.findbugs.annotations.DesireNoWarning;
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+
+public class RFE3026970 {
+	private String get() { return null; }
+
+	public void testDesireWarning() {
+		if (get().equals("A"))
+			System.out.println("test");
+	}
+
+	public void testDesireNoWarning() {
+		if ("A".equals(get()))
+			System.out.println("test");
+	}
+}
