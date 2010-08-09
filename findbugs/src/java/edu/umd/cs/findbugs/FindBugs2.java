32b9f49f40c943ea56a0c84ca9d205b3f882836d
Bill Pugh <bill.pugh@gmail.com>
2010-08-09 18:45:21 +0000

    Set default rank threshold to 20
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@12420 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/FindBugs2.java
+++ findbugs/src/java/edu/umd/cs/findbugs/FindBugs2.java
@@ -137,7 +137,7 @@ public class FindBugs2 implements IFindBugsEngine2 {
 		this.analysisOptions.scanNestedArchives = false;
 		// bug 2815983: no bugs are reported anymore
         // there is no info which value should be default, so using the any one
-        rankThreshold = 42;
+        rankThreshold = 20;
 	}
 
 	/**
