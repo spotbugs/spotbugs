f5f5f295ade41003f925fdc418314ae2d647b0e8
Keith Lea <keithl@gmail.com>
2010-08-23 15:42:17 +0000

    Add test case for bug 3049405
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@12440 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug3049405.java
@@ -0,0 +1,17 @@
+package sfBugs;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+
+import edu.umd.cs.findbugs.annotations.DesireNoWarning;
+
+
+public class Bug3049405 {
+	@CheckForNull
+	final Object o = new Object();
+
+	@DesireNoWarning("NP_NULL_ON_SOME_PATH")
+	public void foo(@Nonnull Object o) {
+		this.o.toString();
+	}
+}
