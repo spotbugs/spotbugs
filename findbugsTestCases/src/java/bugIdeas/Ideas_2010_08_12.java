2fdd5c50b7581ab400871aae0f2e13f1e7a695a8
Bill Pugh <bill.pugh@gmail.com>
2010-08-25 18:35:01 +0000

    more bug ideas
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@12453 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2010_08_12.java
@@ -0,0 +1,14 @@
+package bugIdeas;
+import java.util.Date;
+
+
+public class Ideas_2010_08_12 {
+	
+	Date getDateFromSeconds(int i) {
+		return new Date(i);
+	}
+	Date getDateFromMilliseconds(int i) {
+		return new Date(i*1000);
+	}
+
+}
---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2010_08_16.java
@@ -0,0 +1,18 @@
+package bugIdeas;
+
+public class Ideas_2010_08_16 {
+	
+	boolean badCheck(String name) {
+		return "QUIT" == name;
+	}
+	boolean veryBadCheck(String name) {
+		return "QUIT" == name.toUpperCase();
+	}
+	boolean badCheck(String name, String tag) {
+		return tag == name;
+	}
+	boolean veryBadCheck(String name, String tag) {
+		return tag == name.toUpperCase();
+	}
+
+}
