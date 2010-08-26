2fee31124c1f60ea29369a28ac05d0ad4684abda
Bill Pugh <bill.pugh@gmail.com>
2010-08-26 18:32:43 +0000

    add MaxRank14 as an example BugReporterDecorator
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@12471 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/bugReporter/MaxRankDecorator.java
@@ -0,0 +1,59 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2003-2008 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.bugReporter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.net.URL;
+import java.util.HashSet;
+
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.ClassAnnotation;
+import edu.umd.cs.findbugs.DetectorFactoryCollection;
+import edu.umd.cs.findbugs.I18N;
+import edu.umd.cs.findbugs.PluginLoader;
+import edu.umd.cs.findbugs.internalAnnotations.DottedClassName;
+
+/**
+ * @author pugh
+ */
+public class MaxRankDecorator extends BugReporterDecorator {
+
+	final int maxRank;
+
+	public MaxRankDecorator(BugReporterPlugin plugin, BugReporter delegate) {
+		super(plugin, delegate);
+		maxRank = Integer.parseInt(plugin.getProperties().getProperty("maxRank"));
+	}
+
+
+	@Override
+	public void reportBug(BugInstance bugInstance) {
+		int rank = bugInstance.getBugRank();
+		if (rank <= maxRank)
+			getDelegate().reportBug(bugInstance);
+
+	}
+
+}
