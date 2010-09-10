850d00a0a6344a92e2a34d280252a8b4f9ea8c38
Keith Lea <keithl@gmail.com>
2010-09-10 17:12:45 +0000

    Add two more test cases for SF bugs
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@12585 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug3062706.java
@@ -0,0 +1,19 @@
+package sfBugs;
+
+import edu.umd.cs.findbugs.annotations.DesireNoWarning;
+
+public class Bug3062706 {
+	@DesireNoWarning("SBSC_USE_STRINGBUFFER_CONCATENATION")
+	void blah() {
+		String link = "someValue";
+		while (link.equals("someValue")) {
+			if (something().equals("foo")) {
+				link = link + "extra value";
+				break;
+			}
+		}
+		System.out.println(link);
+	}
+
+	String something() { return null; }
+}
---
+++ findbugsTestCases/src/java/sfBugs/Bug3063206.java
@@ -0,0 +1,15 @@
+package sfBugs;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.logging.StreamHandler;
+
+import edu.umd.cs.findbugs.annotations.DesireNoWarning;
+
+public class Bug3063206 extends StreamHandler {
+	@DesireNoWarning("OBL_UNSATISFIED_OBLIGATION")
+	public void warnme() throws FileNotFoundException {
+		setOutputStream(new FileOutputStream(new File("a", "b"), true));
+	}
+}
