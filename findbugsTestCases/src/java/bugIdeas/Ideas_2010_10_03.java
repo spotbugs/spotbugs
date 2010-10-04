cf8ffe16aec234d07ba9d419acdf01bd875c6a6e
Bill Pugh <bill.pugh@gmail.com>
2010-10-04 09:22:34 +0000

    add test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@12818 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2010_10_03.java
@@ -0,0 +1,13 @@
+package bugIdeas;
+
+import java.io.IOException;
+
+public class Ideas_2010_10_03 {
+
+    public static void main(String args[]) throws IOException {
+        int x = System.in.read();
+        System.out.println(x);
+        System.in.close();
+    }
+
+}
---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2010_10_04.java
@@ -0,0 +1,61 @@
+package bugIdeas;
+
+import java.util.Arrays;
+
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
+public class Ideas_2010_10_04 {
+
+    int [] data;
+
+    /**
+     * @param data
+     */
+    public Ideas_2010_10_04(int[] data) {
+       this.data = data;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Arrays.hashCode(data);
+        return result;
+    }
+
+    @ExpectWarning("EQ_CHECK_FOR_OPERAND_NOT_COMPATIBLE_WITH_THIS")
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (obj instanceof Ideas_2010_10_04) {
+            Ideas_2010_10_04 other = (Ideas_2010_10_04) obj;
+            return Arrays.equals(data, other.data);
+        }
+        if (obj instanceof int[]) {
+            int[] other = (int[]) obj;
+            return Arrays.equals(data, other);
+        }
+        return false;
+    }
+
+    @NoWarning("EC_ARRAY_AND_NONARRAY")
+    public static void main(String args[]) {
+        int [] x = {1};
+        int [] y = {1};
+        Ideas_2010_10_04 z = new Ideas_2010_10_04(x);
+        Ideas_2010_10_04 w = new Ideas_2010_10_04(y);
+
+        System.out.println(z.equals(w));
+        System.out.println(z.equals(x));
+        System.out.println(z.equals(y));
+
+
+    }
+
+
+
+}
