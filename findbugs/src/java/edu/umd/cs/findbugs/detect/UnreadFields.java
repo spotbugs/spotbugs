20c36c9b00a2bc448fcf8810b106ddd85f604f83
Keith Lea <keithl@gmail.com>
2010-11-26 19:21:57 +0000

    Add javax.ws.rs.core.Context to list of injection annotations
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@12985 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/UnreadFields.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/UnreadFields.java
@@ -317,14 +317,19 @@ public class UnreadFields extends OpcodeStackDetector {
     }
 
     public static boolean isInjectionAttribute(@DottedClassName String annotationClass) {
-        if (annotationClass.startsWith("javax.annotation.") || annotationClass.startsWith("javax.ejb")
+        if (annotationClass.startsWith("javax.annotation.")
+                || annotationClass.startsWith("javax.ejb")
                 || annotationClass.equals("org.apache.tapestry5.annotations.Persist")
-                || annotationClass.equals("org.jboss.seam.annotations.In") || annotationClass.startsWith("javax.persistence")
-                || annotationClass.endsWith("SpringBean") || annotationClass.equals("com.google.inject.Inject")
+                || annotationClass.equals("org.jboss.seam.annotations.In")
+                || annotationClass.startsWith("javax.persistence")
+                || annotationClass.endsWith("SpringBean")
+                || annotationClass.equals("com.google.inject.Inject")
                 || annotationClass.startsWith("com.google.") && annotationClass.endsWith(".Bind")
-                && annotationClass.hashCode() == -243168318 || annotationClass.startsWith("org.nuxeo.common.xmap.annotation")
+                && annotationClass.hashCode() == -243168318
+                || annotationClass.startsWith("org.nuxeo.common.xmap.annotation")
                 || annotationClass.startsWith("com.google.gwt.uibinder.client")
-                || annotationClass.startsWith("org.springframework.beans.factory.annotation"))
+                || annotationClass.startsWith("org.springframework.beans.factory.annotation")
+                || annotationClass.equals("javax.ws.rs.core.Context"))
             return true;
         int lastDot = annotationClass.lastIndexOf('.');
         String lastPart = annotationClass.substring(lastDot + 1);
