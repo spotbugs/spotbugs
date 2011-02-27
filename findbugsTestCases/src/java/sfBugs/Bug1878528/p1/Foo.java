007cb1ea87557c0cedc888e62fc6bc57737dd552
Bill Pugh <bill.pugh@gmail.com>
2011-02-27 20:07:21 +0000

    make these not useless
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13148 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug1878528/p1/Foo.java
+++ findbugsTestCases/src/java/sfBugs/Bug1878528/p1/Foo.java
@@ -1,19 +1,21 @@
+package sfBugs.Bug1878528.p1;
+
 public class Foo {
     public static void main(String args[]) {
         int[] mya = new int[6];
         System.out.println(mya.toString());
-        
+
         String s = "Hello";
         s.toUpperCase();
         System.out.println(s);
-        
+
         for(int i = 0; i < 1000; i++) {
         	s += "Hello";
         }
         s.toLowerCase();
         System.out.println(s);
-        
-        Integer i = new Integer(0);
+
+        Integer i = new Integer(null);
         System.out.println("" + i);
     }
 }
--- findbugsTestCases/src/java/sfBugs/Bug1878528/p2/Foo.java
+++ findbugsTestCases/src/java/sfBugs/Bug1878528/p2/Foo.java
@@ -1,19 +1,21 @@
+package sfBugs.Bug1878528.p2;
+
 public class Foo {
     public static void main(String args[]) {
         int[] mya = new int[6];
         System.out.println(mya.toString());
-        
+
         String s = "Hello";
         s.toUpperCase();
         System.out.println(s);
-        
+
         for(int i = 0; i < 1000; i++) {
         	s += "Hello";
         }
         s.toLowerCase();
         System.out.println(s);
-        
-        Integer i = new Integer(null);
+
+        Integer i = new Integer(0);
         System.out.println("" + i);
     }
 }
