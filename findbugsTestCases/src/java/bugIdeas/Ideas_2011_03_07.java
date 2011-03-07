3f26175dd95f9c80e2c12be7c5e7a2642cb2d3b9
Bill Pugh <bill.pugh@gmail.com>
2011-03-07 18:53:52 +0000

    new bug idea
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13163 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2011_03_07.java
@@ -0,0 +1,38 @@
+package bugIdeas;
+
+import java.util.Comparator;
+
+public class Ideas_2011_03_07 {
+
+    static <T extends Comparable<T>> void check(T x, T y) {
+        if (x.compareTo(y) == 1)
+            System.out.println("x > y");
+        else if (x.compareTo(y) == -1)
+            System.out.println("x < y");
+        else
+            System.out.println("x == y");
+
+
+    }
+    static <T extends Comparable<T>> void check2(T x, T y) {
+        int compareTo = x.compareTo(y);
+        boolean b = compareTo == 1;
+        if (b)
+            System.out.println("x > y");
+        else if (compareTo == -1)
+            System.out.println("x < y");
+        else
+            System.out.println("x == y");
+    }
+
+    static <T> void check(T x, T y, Comparator<T> c) {
+        int comparison = c.compare(x,y);
+        if (comparison == 1)
+            System.out.println("x > y");
+        else if (comparison == -1)
+            System.out.println("x < y");
+        else
+            System.out.println("x == y");
+    }
+
+}
