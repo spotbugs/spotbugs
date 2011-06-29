6b1a93b144b0590689c2f6512d69bfa8c2837126
Bill Pugh <bill.pugh@gmail.com>
2011-06-29 16:31:19 +0000

    reformatting
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13288 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug3185865.java
+++ findbugsTestCases/src/java/sfBugs/Bug3185865.java
@@ -1,13 +1,35 @@
 package sfBugs;
 
 public class Bug3185865 {
-    private float floatFct1() { return 0; }
-    private float floatFct2() { return 0; }
+    private float f1() {
+        return 0;
+    }
+
+    private float f2() {
+        return 0;
+    }
+
     public void methodA(float someFloat) {
         // detects this:
-        assert someFloat == floatFct1() + floatFct2();
-        //but it does not detect this:
-        assert someFloat == floatFct1();
+        assert someFloat == f1() + f2();
+    }
+
+    public void methodB(float someFloat) {
+        // but it does not detect this:
+        assert someFloat == f1();
 
     }
+
+    public void test() {
+        double i = Math.random();
+        double j = Math.random();
+        double k = Math.random();
+
+        assert (i == 1.0f);
+        assert (i == j);
+        assert (k == f1());
+        assert (i == f1() + f2()); // (4)
+        assert (f1() == f2());
+    }
+
 }
