7522d3b79eb27ddf6fbd45fb3a90bd1cf4316361
Bill Pugh <bill.pugh@gmail.com>
2011-06-30 19:02:13 +0000

    more test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13295 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/bugIdeas/Ideas_2009_10_13.java
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2009_10_13.java
@@ -1,6 +1,8 @@
 package bugIdeas;
 
 import java.util.HashMap;
+import java.util.Set;
+import java.util.Map.Entry;
 
 import edu.umd.cs.findbugs.annotations.DesireWarning;
 import edu.umd.cs.findbugs.annotations.ExpectWarning;
@@ -59,6 +61,18 @@ public class Ideas_2009_10_13<V, K> extends HashMap<K, V> {
             System.out.println("h contains i");
         h.entrySet().retainAll(i.entrySet());
         h.entrySet().removeAll(i.entrySet());
+    }
+    
+    @ExpectWarning("GC")
+    public static void truePositive4(HashMap<Integer, String> i, HashMap<String, Integer> h) {
+        Set<Entry<Integer, String>> iEntrySet = i.entrySet();
+        Set<Entry<String, Integer>> hEntrySet = h.entrySet();
+        if (iEntrySet.containsAll(hEntrySet))
+            System.out.println("i contains h");
+        if (hEntrySet.containsAll(iEntrySet))
+            System.out.println("h contains i");
+        hEntrySet.retainAll(iEntrySet);
+        hEntrySet.removeAll(iEntrySet);
     }
 
 }
