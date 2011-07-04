dc51815d4927ecbcbf83c91cc6c83689176c4a41
Bill Pugh <bill.pugh@gmail.com>
2011-07-04 19:16:56 +0000

    add test case showing fixed version of code
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13304 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/bugIdeas/Ideas_2011_06_16.java
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2011_06_16.java
@@ -1,11 +1,74 @@
 package bugIdeas;
 
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Ideas_2011_06_16 {
+
+    public static byte[] getByteFile(String byteFileName, int n) {
+        File f = new File(byteFileName);
+
+        if (!(f.exists() && f.isFile() && f.canRead() && f.length() == n))
+            return null;
+
+        FileInputStream fs;
+
+        try {
+            fs = new FileInputStream(f);
+        } catch (FileNotFoundException e) {
+            return null;
+        }
+
+        byte[] bytes = new byte[n];
+
+        int numBytesRead = 0;
+
+        try {
+            numBytesRead = fs.read(bytes);
+            fs.close();
+        } catch (IOException e) {
+            return null;
+        }
+
+        if (numBytesRead != n)
+            return null;
+
+        return bytes;
+
+    } // end getByteFile
+
+    public static byte[] getByteFileFixed(String byteFileName, int n) {
+        File f = new File(byteFileName);
+
+        if (!(f.exists() && f.isFile() && f.canRead() && f.length() == n))
+            return null;
+
+        DataInputStream ds;
+
+        try {
+            ds = new DataInputStream(new FileInputStream(f));
+        } catch (FileNotFoundException e) {
+            return null;
+        }
+
+        try {
+            byte[] bytes = new byte[n];
+            ds.readFully(bytes);
+            return bytes;
+        } catch (IOException e) {
+            return null;
+        } finally {
+            try {
+                ds.close();
+            } catch (IOException e) {
+                assert true; // nothing to do at this point
+            }
+        }
+
+    } // end getByteFile
 
     static int firstByte(File f) throws IOException {
         FileInputStream fs;
