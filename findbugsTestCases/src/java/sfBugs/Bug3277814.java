b22c4798bd9bcd7c338de717e99915e965cb06b9
Bill Pugh <bill.pugh@gmail.com>
2011-07-10 14:20:34 +0000

    Add expect/no warning annotations
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13329 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug3277814.java
+++ findbugsTestCases/src/java/sfBugs/Bug3277814.java
@@ -1,7 +1,11 @@
 package sfBugs;
 
+import edu.umd.cs.findbugs.annotations.NoWarning;
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+
 public class Bug3277814 {
 
+    @NoWarning("NP")
     public void test() {
         String var = "";
         int index = 2;
@@ -24,6 +28,32 @@ public class Bug3277814 {
              * exceptions).
              */
             throw new RuntimeException("NULL");
+        }
+    }
+    
+    @ExpectWarning("NP")
+    public void test2() {
+        String var = "";
+        int index = 2;
+        if (index == -1) {
+            var = String.class.getName();
+            if (var.length() == 0) {
+                var = null;
+            }
+        } else {
+            var = Integer.class.getName();
+            if (var.length() == 0) {
+                var = null;
+            }
+        }
+        if (var == null) {// FINBUGS reports on this line NP_GUARANTEED_DEREF
+            /*
+             * There is a statement or branch that if executed guarantees that a
+             * value is null at this point, and that value that is guaranteed to
+             * be dereferenced (except on forward paths involving runtime
+             * exceptions).
+             */
+            throw new NullPointerException("var is null");
         }
     }
 }
