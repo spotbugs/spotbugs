aff0cb314cb6efa9a910081f13ac4a9c3960aa71
Keith Lea <keithl@gmail.com>
2011-07-20 17:35:32 +0000

    Add Bug3368029 about inconsistent synchronization
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13378 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug3368029.java
@@ -0,0 +1,60 @@
+package sfBugs;
+
+import edu.umd.cs.findbugs.annotations.DesireNoWarning;
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
+public class Bug3368029 {
+    // case #0
+    @NoWarning("IS")
+    private int x;
+    public synchronized int getX() {
+        return x;
+    }
+    @NoWarning("IS")
+    public void setX(int x) {
+        synchronized (this) {
+            this.x = x;
+        }
+    }
+
+    // case #1
+    @ExpectWarning("IS")
+    private int total;
+    public synchronized int getTotal() {
+        return total;
+    }
+    /*missing synchronized!*/
+    public void reset() {
+        this.total = 0;
+    }
+    /*missing synchronized!*/
+    public void count(String... things) {
+        for (String thing : things) {
+            countOne(thing);
+        }
+    }
+    private void countOne(String thing) {
+        total += thing.length();
+    }
+
+    // case #2
+    @DesireNoWarning("IS")
+    private int total2;
+    public synchronized int count2(String... things) {
+        this.total2 = 0;
+        for (final String thing : things) {
+            new Runnable() {
+                public void run() {
+                    countOne2(thing);
+                }
+            }.run();
+        }
+        return total2;
+    }
+
+    private void countOne2(String thing) {
+        assert Thread.holdsLock(this);
+        total2 += thing.length();
+    }
+}
