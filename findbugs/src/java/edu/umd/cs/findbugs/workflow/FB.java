31d5fbad48a225664f4982b90c494eff5b2178d2
Bill Pugh <bill.pugh@gmail.com>
2011-07-20 16:31:21 +0000

    add fb command
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13377 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugs/src/java/edu/umd/cs/findbugs/workflow/FB.java
@@ -0,0 +1,49 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2003-2008 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.umd.cs.findbugs.workflow;
+
+import edu.umd.cs.findbugs.DetectorFactoryCollection;
+import edu.umd.cs.findbugs.FindBugsMain;
+import edu.umd.cs.findbugs.Plugin;
+
+/**
+ * @author pugh
+ */
+public class FB {
+    
+    public static void main(String args[]) throws Exception {
+        String cmd = args[0];
+        String a [] = new String[args.length-1];
+        for(int i = 1; i < args.length; i++)
+            a[i-1] = args[i];
+        DetectorFactoryCollection.instance();
+        for(Plugin plugin : Plugin.getAllPlugins()) {
+            FindBugsMain main = plugin.getFindBugsMain(cmd);
+            if (main != null) {
+                main.invoke(a);
+                return;
+            }
+            
+        }
+        
+        throw new IllegalArgumentException("Unable to find FindBugs main for " + cmd);
+    }
+
+}
