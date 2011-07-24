2316234aeae56a648b8992fa1261859e5d13671a
Bill Pugh <bill.pugh@gmail.com>
2011-07-24 17:29:58 +0000

    more test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13408 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2011_07_24.java
@@ -0,0 +1,48 @@
+package bugIdeas;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
+public class Ideas_2011_07_24 {
+    
+    @ExpectWarning("EC_UNRELATED_TYPES")
+    static boolean test1(HashSet<Integer> s, LinkedList<Integer> lst) {
+        return s.equals(lst);
+    }
+
+    @NoWarning("GC_UNRELATED_TYPES")
+    static boolean test2(HashSet<Integer> s, LinkedList<Integer> lst) {
+        return s.containsAll(lst) && lst.containsAll(s);
+    }
+
+    @ExpectWarning("EC_UNRELATED_TYPES")
+    static boolean test3(HashSet<Integer> s, LinkedList<String> lst) {
+        return s.equals(lst);
+    }
+
+    @ExpectWarning("GC_UNRELATED_TYPES")
+    static boolean test4(HashSet<Integer> s, LinkedList<String> lst) {
+        return s.containsAll(lst) && lst.containsAll(s);
+    }
+    
+    public static void main(String args[]) {
+        HashSet<Integer> s = new HashSet<Integer>();
+        s.add(1);
+        s.add(2);
+        LinkedList<Integer> iList = new LinkedList<Integer>();
+        iList.add(1);
+        iList.add(2);
+        LinkedList<String> sList = new LinkedList<String>();
+        sList.add("x");
+        sList.add("y");
+        System.out.println(test1(s, iList));
+        System.out.println(test2(s, iList));
+        System.out.println(test3(s, sList));
+        System.out.println(test4(s, sList));
+        
+    }
+
+}
