a212390036bfa9c9e458d8218cd2737b55508e63
Bill Pugh <bill.pugh@gmail.com>
2011-08-02 11:25:12 +0000

    fix bug in code provided in bug report
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13517 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug3384369.java
+++ findbugsTestCases/src/java/sfBugs/Bug3384369.java
@@ -5,11 +5,11 @@ import edu.umd.cs.findbugs.annotations.NoWarning;
 public class Bug3384369 {
 
     @NoWarning("RCN")
-    public Bug3384369(int[] value, int dataOffset) {
-        assert (value == null || value.length > dataOffset) 
+    public static void test1(int[] value, int dataOffset) {
+        assert (value != null && value.length > dataOffset) 
         : String.format(
                 "Value array length %s should be greater than dataOffset %s", 
                 (value == null ? 0 : value.length), dataOffset);
+        System.out.println(value.length);
     }
-
 }
