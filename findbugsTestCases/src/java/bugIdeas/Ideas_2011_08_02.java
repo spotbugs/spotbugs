28a08f00d150b5a7aed1d103bed20793cd3961df
Bill Pugh <bill.pugh@gmail.com>
2011-08-02 16:30:05 +0000

    add test cases for Map.entrySet() problems
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13527 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/bugIdeas/Ideas_2011_08_02.java
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2011_08_02.java
@@ -1,21 +1,68 @@
 package bugIdeas;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
 import edu.umd.cs.findbugs.annotations.ExpectWarning;
 import edu.umd.cs.findbugs.annotations.NoWarning;
+import edu.umd.cs.findbugs.annotations.Priority;
 
 public class Ideas_2011_08_02 {
-    
+
     @ExpectWarning("INT_BAD_COMPARISON_WITH_INT_VALUE")
     public boolean badCheck(int x) {
         return x == 9999999999L;
     }
+
     @ExpectWarning("INT_BAD_COMPARISON_WITH_INT_VALUE")
     public boolean badCheck2(int x) {
-        return 9999999999L == x ;
+        return 9999999999L == x;
     }
+
     @NoWarning("INT_BAD_COMPARISON_WITH_INT_VALUE")
     public boolean okCheck(int x) {
         return x == 999999999L;
+    }
+
+    enum Color {
+        RED, GREEN, BLUE
+    };
+
+    Collection<Map.Entry<Color, Integer>> c = new ArrayList<Map.Entry<Color, Integer>>();
+
+    @NoWarning(value="DMI_ENTRY_SETS_MAY_REUSE_ENTRY_OBJECTS", priority=Priority.HIGH)
+    @ExpectWarning(value="DMI_ENTRY_SETS_MAY_REUSE_ENTRY_OBJECTS", priority=Priority.MEDIUM)
+    public void addAll(Map<Color, Integer> m) {
+        c.addAll(m.entrySet());
+    }
+
+    @ExpectWarning(value="DMI_ENTRY_SETS_MAY_REUSE_ENTRY_OBJECTS", priority=Priority.HIGH)
+    
+    public void addAll(IdentityHashMap<Color, Integer> m) {
+        c.addAll(m.entrySet());
+    }
+    @ExpectWarning(value="DMI_ENTRY_SETS_MAY_REUSE_ENTRY_OBJECTS", priority=Priority.HIGH)
+    
+    public void addAll(EnumMap<Color, Integer> m) {
+        c.addAll(m.entrySet());
+    }
+    @NoWarning(value="DMI_ENTRY_SETS_MAY_REUSE_ENTRY_OBJECTS", priority=Priority.MEDIUM)
+    public void addAll(HashMap<Color, Integer> m) {
+        c.addAll(m.entrySet());
+    }
+    @NoWarning(value="DMI_ENTRY_SETS_MAY_REUSE_ENTRY_OBJECTS", priority=Priority.MEDIUM)
+    public void addAll(TreeMap<Color, Integer> m) {
+        c.addAll(m.entrySet());
+    }
+    
+    static abstract class BadIdea implements Map.Entry<String, Integer>, Iterator<Map.Entry<String, Integer>> {
+        
     }
 
 }
