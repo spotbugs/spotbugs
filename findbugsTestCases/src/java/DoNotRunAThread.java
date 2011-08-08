8308c054ba1f4df36fb133ff11a9b7ca44a9f62e
Bill Pugh <bill.pugh@gmail.com>
2011-08-08 03:02:35 +0000

    more test cases
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13624 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/DoNotRunAThread.java
+++ findbugsTestCases/src/java/DoNotRunAThread.java
@@ -1,11 +1,19 @@
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+
 public class DoNotRunAThread {
 
     void f(Runnable r) {
         r.run();
     }
 
+    @ExpectWarning("DMI_THREAD_PASSED_WHERE_RUNNABLE_EXPECTED")
     void g(Thread t) {
         f(t);
+    }
+    
+    @ExpectWarning("RU_INVOKE_RUN")
+    void h(Thread t) {
+        t.run();
     }
 
 }
