1c67da7ccb73a2d1450cb481960ba63a11cf004b
Bill Pugh <bill.pugh@gmail.com>
2011-08-26 02:21:26 +0000

    add some annotations
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13795 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/RFE2958584.java
+++ findbugsTestCases/src/java/sfBugs/RFE2958584.java
@@ -3,11 +3,15 @@ package sfBugs;
 import java.io.Serializable;
 import java.util.List;
 
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+
 public class RFE2958584 implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @ExpectWarning("SE_BAD_FIELD")
     private List<NotSerializable> list; // Not findbugs warning here.
 
+    @ExpectWarning("SE_BAD_FIELD")
     private NotSerializable ns;// Findbugs gives a proper warning here.
 
     static class NotSerializable {
