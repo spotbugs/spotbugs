743b5c5b917c751b2984de0be1be45f7877689b5
Bill Pugh <bill.pugh@gmail.com>
2011-08-26 02:29:01 +0000

    fix tabs
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13801 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- JSR305-testCases/src/main/java/nullness/SimpleTests.java
+++ JSR305-testCases/src/main/java/nullness/SimpleTests.java
@@ -4,12 +4,12 @@ import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
 
 public class SimpleTests {
-	
-	int f(@CheckForNull Object x) {
-		return x.hashCode();
+
+    int f(@CheckForNull Object x) {
+        return x.hashCode();
 	}
-	@Nonnull Object g() {
-		return null;
-	}
+    @Nonnull Object g() {
+        return null;
+    }
 
 }
