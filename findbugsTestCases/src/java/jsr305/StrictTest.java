7e535f0da3ca7c1b4392bd5f097e77a71d7e60e5
Bill Pugh <bill.pugh@gmail.com>
2011-10-23 14:20:04 +0000

    add annotations about expectations
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13890 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/jsr305/StrictTest.java
+++ findbugsTestCases/src/java/jsr305/StrictTest.java
@@ -1,5 +1,8 @@
 package jsr305;
 
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
 public abstract class StrictTest {
 
     protected abstract void requiresStrict(@Strict Object o);
@@ -17,80 +20,99 @@ public abstract class StrictTest {
 
     Object g;
 
-    public @Strict
+    @ExpectWarning("TQ")
+    public 
+    @Strict
     Object violation1(Object unknown) {
         return unknown;
     }
 
+    @ExpectWarning("TQ")
     public void violation2(Object unknown) {
         f = unknown;
     }
 
+    @ExpectWarning("TQ")
     public void violation3(Object unknown) {
         requiresStrict(unknown);
     }
 
-    public @Strict
+    @ExpectWarning("TQ")
+    public  @Strict
     Object violation4() {
         return g;
     }
 
+    @ExpectWarning("TQ")
     public void violation5() {
         f = g;
     }
 
+    @ExpectWarning("TQ")
     public void violation6() {
         requiresStrict(g);
     }
 
+    @ExpectWarning("TQ")
     public @Strict
     Object violation7() {
         return get();
     }
 
+    @ExpectWarning("TQ")
     public void violation8() {
         f = get();
     }
 
+    @ExpectWarning("TQ")
     public void violation9() {
         requiresStrict(get());
     }
 
+    @NoWarning("TQ") 
     public @Strict
     Object ok1DoNotReport(@Strict Object s) {
         return s;
     }
 
+    @NoWarning("TQ") 
     public void ok2DoNotReport(@Strict Object s) {
         f = s;
     }
 
+    @NoWarning("TQ") 
     public void ok3DoNotReport(@Strict Object s) {
         requiresStrict(s);
     }
 
+    @NoWarning("TQ") 
     public @Strict
     Object ok4DoNotReport() {
         return f;
     }
 
+    @NoWarning("TQ") 
     public void ok5DoNotReport() {
         f2 = f;
     }
 
+    @NoWarning("TQ") 
     public void ok6DoNotReport() {
         requiresStrict(f);
     }
 
-    public @Strict
+    @NoWarning("TQ")
+    public   
+    @Strict
     Object ok7DoNotReport() {
         return getStrict();
     }
-
+    @NoWarning("TQ") 
     public void ok8DoNotReport() {
         f = getStrict();
     }
 
+    @NoWarning("TQ") 
     public void ok9DoNotReport() {
         requiresStrict(getStrict());
     }
