3591f6b2b8385ccc0eaa9d60b607eff03cc246ee
Bill Pugh <bill.pugh@gmail.com>
2011-11-11 20:21:22 +0000

    add annotation
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13926 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug3139802.java
+++ findbugsTestCases/src/java/sfBugs/Bug3139802.java
@@ -1,6 +1,9 @@
 package sfBugs;
 
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
 public class Bug3139802 {
+    @NoWarning("NP")
     public static void main(String[] args) {
         System.out.println("OK");
     }
