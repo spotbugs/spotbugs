1b79cd8f3b2552b2ff0c27c00d03c8406f441db6
Bill Pugh <bill.pugh@gmail.com>
2011-11-15 00:10:35 +0000

    fixed test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13933 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug3152881.java
+++ findbugsTestCases/src/java/sfBugs/Bug3152881.java
@@ -5,11 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import edu.umd.cs.findbugs.annotations.DesireNoWarning;
+import edu.umd.cs.findbugs.annotations.NoWarning;
 
 public class Bug3152881 {
-    @DesireNoWarning("OBL_UNSATISFIED_OBLIGATION")
-    void falsePositive(Connection connection)
+    void asProvided(Connection connection)
             throws SQLException {
         Statement createStmt = null;
         PreparedStatement insertStmt = null;
@@ -25,6 +24,29 @@ public class Bug3152881 {
                 createStmt.close();
             if (insertStmt != null) {
                 insertStmt.close();
+            }
+        }
+    }
+    @NoWarning("OBL_UNSATISFIED_OBLIGATION")
+    void falsePositive2(Connection connection)
+            throws SQLException {
+        Statement createStmt = null;
+        PreparedStatement insertStmt = null;
+        try {
+            createStmt = connection.createStatement();
+            createStmt.executeUpdate("CREATE...");
+
+            insertStmt = connection.prepareStatement("INSERT...");
+            insertStmt.executeBatch();
+
+        } finally {
+            try {
+                if (createStmt != null)
+                    createStmt.close();
+            } finally {
+                if (insertStmt != null) {
+                    insertStmt.close();
+                }
             }
         }
     }
