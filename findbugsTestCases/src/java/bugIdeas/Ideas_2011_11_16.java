442b3e84501fca9bdbc0395dd868de85895768c1
Bill Pugh <bill.pugh@gmail.com>
2011-11-17 02:08:37 +0000

    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13949 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/bugIdeas/Ideas_2011_11_16.java
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2011_11_16.java
@@ -6,9 +6,10 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import edu.umd.cs.findbugs.annotations.ExpectWarning;
+import edu.umd.cs.findbugs.annotations.NoWarning;
 
 public abstract class Ideas_2011_11_16 {
-    
+
     @ExpectWarning("OBL_UNSATISFIED_OBLIGATION_EXCEPTION_EDGE")
     ResultSet doQuery(String query) throws SQLException {
         Connection conn = getConnection();
@@ -16,5 +17,41 @@ public abstract class Ideas_2011_11_16 {
         return statement.executeQuery(query);
     }
 
+    @NoWarning("OBL")
+    ResultSet doQuery2(Connection conn, String query) throws SQLException {
+        Statement statement = conn.createStatement();
+        try {
+            return statement.executeQuery(query);
+        } catch (SQLException e) {
+            statement.close();
+            throw e;
+        }
+    }
+
+    ResultSet doQuery3( String query) throws SQLException {
+        Connection conn = getConnection();
+        Statement statement = conn.createStatement();
+        try {
+            return statement.executeQuery(query);
+        } catch (SQLException e) {
+            statement.close();
+            throw e;
+        }
+    }
+
+    @ExpectWarning("OBL")
+    int doQuery4( String query) throws SQLException {
+        Connection conn = getConnection();
+        Statement statement = conn.createStatement();
+        try {
+            ResultSet rs =  statement.executeQuery(query);
+            if (rs.next()) 
+                return rs.getInt(1);
+            return 0;
+        } catch (SQLException e) {
+            statement.close();
+            throw e;
+        }
+    }
     abstract Connection getConnection();
 }
