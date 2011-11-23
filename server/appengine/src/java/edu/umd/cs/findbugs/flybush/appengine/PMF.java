223a3dbad42b41c29a44d5adc097508f1124ff0b
Bill Pugh <bill.pugh@gmail.com>
2011-11-23 20:31:32 +0000

    better logging of errors
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@13972 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- server/appengine/src/java/edu/umd/cs/findbugs/flybush/appengine/PMF.java
+++ server/appengine/src/java/edu/umd/cs/findbugs/flybush/appengine/PMF.java
@@ -1,10 +1,25 @@
 package edu.umd.cs.findbugs.flybush.appengine;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManagerFactory;
 
 public final class PMF {
-    private static final PersistenceManagerFactory pmfInstance = JDOHelper.getPersistenceManagerFactory("transactions-optional");
+    private static final Logger log = Logger.getLogger(PMF.class.getName());
+    private static final PersistenceManagerFactory pmfInstance;
+    static {
+        try {
+            pmfInstance =  JDOHelper.getPersistenceManagerFactory("transactions-optional");
+        } catch (RuntimeException t) {
+            log.log(Level.SEVERE, "Runtime exception getting pmf", t);
+            throw t;
+        } catch (Error t) {
+            log.log(Level.SEVERE, "Error getting pmf", t);
+            throw t;
+        }
+    }
 
     private PMF() {
     }
