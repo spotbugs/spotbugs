3559ca35dc0943301bebe0990f455b3059179562
Bill Pugh <bill.pugh@gmail.com>
2011-12-20 18:31:36 +0000

    allow destination to be a zip file
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@14148 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/workflow/CopyBuggySource.java
+++ findbugs/src/java/edu/umd/cs/findbugs/workflow/CopyBuggySource.java
@@ -18,19 +18,20 @@
 package edu.umd.cs.findbugs.workflow;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
-import org.dom4j.DocumentException;
+import javax.annotation.CheckForNull;
 
 import edu.umd.cs.findbugs.BugAnnotation;
 import edu.umd.cs.findbugs.BugAnnotationWithSourceLines;
-import edu.umd.cs.findbugs.BugCollection;
 import edu.umd.cs.findbugs.BugInstance;
 import edu.umd.cs.findbugs.DetectorFactoryCollection;
 import edu.umd.cs.findbugs.FindBugs;
@@ -48,107 +49,177 @@ import edu.umd.cs.findbugs.ba.SourceFinder;
  */
 
 public class CopyBuggySource {
+    enum SrcKind {
+        DIR, ZIP, Z0P_GZ;
+        static SrcKind get(File f) {
+            if (f.exists() && f.isDirectory() && f.canWrite())
+                return DIR;
+            if (!f.exists()) {
+                if (f.getName().endsWith(".zip"))
+                    return ZIP;
+                if (f.getName().endsWith(".z0p.gz"))
+                    return Z0P_GZ;
+            }
+            throw new IllegalArgumentException("Invalid src destination: " + f);
+        }
+
+    }
 
     /**
      *
      */
-    private static final String USAGE = "Usage: <cmd> " + "  <bugs.xml> <destinationSrcDir>";
+    private static final String USAGE = "Usage: <cmd> " + "  <bugs.xml> <destinationSrc>";
 
-    public static void main(String[] args) throws IOException, DocumentException {
+ 
+    public static void main(String[] args) throws Exception {
         FindBugs.setNoAnalysis();
         DetectorFactoryCollection.instance();
         if (args.length != 2) {
             System.out.println(USAGE);
             return;
         }
+        new CopyBuggySource(args).execute();
+    }
 
-        BugCollection origCollection;
+    SortedBugCollection origCollection;
+    File src;
+    SrcKind kind;
+    ZipOutputStream zOut;
+    byte buf[] = new byte[4096];
+    Project project;
+    SourceFinder sourceFinder;
+    HashSet<String> copied = new HashSet<String>();
+    HashSet<String> couldNotFind = new HashSet<String>();
+    HashSet<String> couldNotCreate = new HashSet<String>();
+    int copyCount = 0;
+    File dstFile;
+
+    public CopyBuggySource(String[] args) throws Exception {
         origCollection = new SortedBugCollection();
         origCollection.readXML(args[0]);
-        File src = new File(args[1]);
-        byte buf[] = new byte[4096];
-        if (!src.isDirectory())
-            throw new IllegalArgumentException(args[1] + " is not a source directory");
-        Project project = origCollection.getProject();
-        SourceFinder sourceFinder = new SourceFinder(project);
-        HashSet<String> copied = new HashSet<String>();
-        HashSet<String> couldNotFind = new HashSet<String>();
-        HashSet<String> couldNotCreate = new HashSet<String>();
+        project = origCollection.getProject();
 
-        int copyCount = 0;
+        sourceFinder = new SourceFinder(project);
+        src = new File(args[1]);
+        kind = SrcKind.get(src);
+
+        switch (kind) {
+        case DIR:
+            break;
+        case ZIP:
+            zOut = new ZipOutputStream(new FileOutputStream(src));
+            break;
+        case Z0P_GZ:
+            zOut = new ZipOutputStream(new DeflaterOutputStream(new FileOutputStream(src)));
+            zOut.setLevel(0);
+            break;
+        }
+
+    }
+
+    private void copySourceFile(String fullName, SourceFile sourceFile) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            long lastModified = sourceFile.getLastModified();
+            in = sourceFile.getInputStream();
+            out = getOutputStream(fullName, 0);
+            if (out == null)
+                return;
+            while (true) {
+                int sz = in.read(buf);
+                if (sz < 0)
+                    break;
+                out.write(buf, 0, sz);
+            }
+            if (dstFile != null)
+                dstFile.setLastModified(lastModified);
+            System.out.println("Copied " + fullName);
+            copyCount++;
+        } catch (IOException e) {
+            if (couldNotFind.add(fullName)) {
+                System.out.println("Problem copying " + fullName);
+                e.printStackTrace(System.out);
+            }
+        } finally {
+            close(in);
+            close(out);
+        }
+    }
+
+    private void copySourceForAnnotation(BugAnnotation ann) {
+        SourceLineAnnotation sourceAnnotation;
+        if (ann instanceof BugAnnotationWithSourceLines)
+            sourceAnnotation = ((BugAnnotationWithSourceLines) ann).getSourceLines();
+        else if (ann instanceof SourceLineAnnotation)
+            sourceAnnotation = (SourceLineAnnotation) ann;
+        else
+            return;
+        if (sourceAnnotation == null)
+            return;
+        if (sourceAnnotation.isUnknown())
+            return;
+
+        String fullName = SourceFinder.getPlatformName(sourceAnnotation);
+
+        SourceFile sourceFile;
+        try {
+            sourceFile = sourceFinder.findSourceFile(sourceAnnotation);
+        } catch (IOException e) {
+
+            if (couldNotFind.add(fullName))
+                System.out.println("Did not find " + fullName);
+            return;
+        }
+
+        if (copied.add(fullName)) {
+            copySourceFile(fullName, sourceFile);
+        }
+    }
+
+    public void execute() throws IOException {
+
         for (BugInstance bug : origCollection.getCollection()) {
             for (Iterator<BugAnnotation> i = bug.annotationIterator(); i.hasNext();) {
-                BugAnnotation ann = i.next();
-                SourceLineAnnotation sourceAnnotation;
-                if (ann instanceof BugAnnotationWithSourceLines)
-                    sourceAnnotation = ((BugAnnotationWithSourceLines) ann).getSourceLines();
-                else if (ann instanceof SourceLineAnnotation)
-                    sourceAnnotation = (SourceLineAnnotation) ann;
-                else
-                    continue;
-                if (sourceAnnotation == null)
-                    continue;
-                if (sourceAnnotation.isUnknown())
-                    continue;
-               
-                String fullName = SourceFinder.getPlatformName(sourceAnnotation);
-                
-                SourceFile sourceFile;
-                try {
-                    sourceFile = sourceFinder.findSourceFile(sourceAnnotation);
-                } catch (FileNotFoundException e) {
-                    
-                    if (couldNotFind.add(fullName))
-                        System.out.println("Did not find " + fullName);
-                    continue;
-                }
-                
-                if (copied.add(fullName)) {
-                    long lastModified = sourceFile.getLastModified();
-                    File dstFile = new File(src, fullName);
-                    if (dstFile.exists()) {
-                        System.out.println(dstFile + " already exists");
-                        continue;
-                    }
-                    File parent = dstFile.getParentFile();
-                    InputStream in = null;
-                    OutputStream out = null;
-                    try {
-                        in = sourceFile.getInputStream();
-                        if (!parent.mkdirs() && !parent.isDirectory()) {
-                            String path = parent.getPath();
-                            if (couldNotCreate.add(path))
-                                System.out.println("Can't create directory for " + path);
-                            in.close();
-                            continue;
-                        }
-                        out = new FileOutputStream(dstFile);
-                        while (true) {
-                            int sz = in.read(buf);
-                            if (sz < 0)
-                                break;
-                            out.write(buf, 0, sz);
-                        }
-                        System.out.println("Copied " + fullName);
-                        copyCount++;
-                    } catch (IOException e) {
-                        if (couldNotFind.add(dstFile.getPath())) {
-                            System.out.println("Problem copying " + dstFile);
-                            e.printStackTrace(System.out);
-                        }
-                    } finally {
-                        close(in);
-                        close(out);
-                        dstFile.setLastModified(lastModified);
-                    }
-
-                }
+                copySourceForAnnotation(i.next());
             }
         }
+        if (zOut != null)
+            zOut.close();
 
         System.out.printf("All done. %d files not found, %d files copied%n", couldNotFind.size(), copyCount);
     }
 
+    private @CheckForNull
+    OutputStream getOutputStream(String fullName, long lastModifiedTime) throws IOException {
+        if (kind == SrcKind.DIR) {
+            dstFile = new File(src, fullName);
+
+            if (dstFile.exists()) {
+                System.out.println(dstFile + " already exists");
+                return null;
+            }
+
+            File parent = dstFile.getParentFile();
+            OutputStream out = null;
+
+            if (!parent.mkdirs() && !parent.isDirectory()) {
+                String path = parent.getPath();
+                if (couldNotCreate.add(path))
+                    System.out.println("Can't create directory for " + path);
+                return null;
+            }
+
+            return new FileOutputStream(dstFile);
+        } else {
+            ZipEntry e = new ZipEntry(fullName);
+            e.setTime(lastModifiedTime);
+            zOut.putNextEntry(e);
+            return zOut;
+        }
+
+    }
+    
     public static void close(InputStream in) {
         try {
             if (in != null)
@@ -160,10 +231,13 @@ public class CopyBuggySource {
 
     public static void close(OutputStream out) {
         try {
-            if (out != null)
+            if (out instanceof ZipOutputStream)
+                ((ZipOutputStream) out).closeEntry();
+            else if (out != null)
                 out.close();
         } catch (IOException e) {
         }
 
     }
+
 }
