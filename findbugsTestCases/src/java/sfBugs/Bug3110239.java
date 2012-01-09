650bc3f0d7f191a643de337fa223ecb15590743a
Bill Pugh <bill.pugh@gmail.com>
2012-01-09 18:07:56 +0000

    more bugs from sourceforge
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@14195 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug3110239.java
+++ findbugsTestCases/src/java/sfBugs/Bug3110239.java
@@ -6,6 +6,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
+@NoWarning("SE_BAD_FIELDS")
 public class Bug3110239 implements Serializable {
     public Bug3110239(String id, String name, Bug3110239 parent) {
         this.id = id;
@@ -14,11 +17,27 @@ public class Bug3110239 implements Serializable {
     }
     private static final long serialVersionUID = 9L;
     public static final String ROOT_NAME = "root";
-    private final String id;
-    private final String name;
-    private final Bug3110239 parent;
+    public final String id;
+    public final String name;
+    public final Bug3110239 parent;
 
+    @NoWarning("SE_BAD_FIELDS")
     private List<Bug3110239> subSectors = new ArrayList<Bug3110239>();
+
+    @NoWarning("SE_BAD_FIELDS")
     private Set<String> companies = new HashSet<String>();
+    
+    public List<Bug3110239> getSubSectors() {
+        return subSectors;
+    }
+    public void setSubSectors(List<Bug3110239> subSectors) {
+        this.subSectors = subSectors;
+    }
+    public Set<String> getCompanies() {
+        return companies;
+    }
+    public void setCompanies(Set<String> companies) {
+        this.companies = companies;
+    }
     
 }
---
+++ findbugsTestCases/src/java/sfBugs/Bug3470297.java
@@ -0,0 +1,30 @@
+package sfBugs;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
+public class Bug3470297 {
+
+    @NoWarning("GC_UNRELATED_TYPES")
+    public <A, B> void foo(A a, Foo<A, B> foo) {
+
+        Set<B> empty = Collections.emptySet();
+        foo.remove(a, empty); // This generated a false positive because it
+                              // thinks that the second parameter should be of
+                              // type B, not Set<B>
+    }
+
+    static class Foo<X, Y> extends Bar<X, Set<Y>> {
+
+    }
+
+    static class Bar<T, S> extends ConcurrentHashMap<T, S> {
+        @Override
+        public boolean remove(Object key, Object value) {
+            return false;
+        }
+    }
+}
