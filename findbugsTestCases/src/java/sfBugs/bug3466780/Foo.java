d89af056aa984dba953e2e6fe25f58d37f49bb49
Bill Pugh <bill.pugh@gmail.com>
2012-01-10 16:57:30 +0000

    fix formatting
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@14213 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/bug3466780/Foo.java
+++ findbugsTestCases/src/java/sfBugs/bug3466780/Foo.java
@@ -4,26 +4,25 @@ import edu.umd.cs.findbugs.annotations.ExpectWarning;
 import edu.umd.cs.findbugs.annotations.NoWarning;
 
 public class Foo {
-	public int number = 22;
-	
-	public final class Inner {
-	    @NoWarning("NP_PARAMETER_MUST_BE_NONNULL_BUT_MARKED_AS_NULLABLE")
-		public Inner() {
-			Foo.this.number++;
-		}
-	    
-	    @ExpectWarning("NP_PARAMETER_MUST_BE_NONNULL_BUT_MARKED_AS_NULLABLE")
-	    public int foo(Object x) {
+    public int number = 22;
+
+    public final class Inner {
+        @NoWarning("NP_PARAMETER_MUST_BE_NONNULL_BUT_MARKED_AS_NULLABLE")
+        public Inner() {
+            Foo.this.number++;
+        }
+
+        @ExpectWarning("NP_PARAMETER_MUST_BE_NONNULL_BUT_MARKED_AS_NULLABLE")
+        public int foo(Object x) {
             return x.hashCode();
         }
-	}
-	
-	public final class Inner2 {
+    }
+
+    public final class Inner2 {
         @NoWarning("NP_PARAMETER_MUST_BE_NONNULL_BUT_MARKED_AS_NULLABLE")
         public Inner2(Object x) {
             Foo.this.number++;
         }
-        
-      
+
     }
 }
--- findbugsTestCases/src/java/sfBugs/bug3466780a/Foo.java
+++ findbugsTestCases/src/java/sfBugs/bug3466780a/Foo.java
@@ -4,17 +4,17 @@ import edu.umd.cs.findbugs.annotations.ExpectWarning;
 import edu.umd.cs.findbugs.annotations.NoWarning;
 
 public class Foo {
-	public int number = 22;
-	
-	public final class Inner {
-	    @NoWarning("NP_PARAMETER_MUST_BE_NONNULL_BUT_MARKED_AS_NULLABLE")
-		public Inner() {
-			Foo.this.number++;
-		}
-	    
-	    @ExpectWarning("NP_PARAMETER_MUST_BE_NONNULL_BUT_MARKED_AS_NULLABLE")
-	    public int foo(Object x) {
-	        return x.hashCode();
-	    }
-	}
+    public int number = 22;
+
+    public final class Inner {
+        @NoWarning("NP_PARAMETER_MUST_BE_NONNULL_BUT_MARKED_AS_NULLABLE")
+        public Inner() {
+            Foo.this.number++;
+        }
+
+        @ExpectWarning("NP_PARAMETER_MUST_BE_NONNULL_BUT_MARKED_AS_NULLABLE")
+        public int foo(Object x) {
+            return x.hashCode();
+        }
+    }
 }
