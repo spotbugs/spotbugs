a4d6976a4b06508223d6ff405c93339568aef077
Keith Lea <keithl@gmail.com>
2012-01-13 21:18:01 +0000

    Add Bug3330970
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@14225 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug3330970.java
@@ -0,0 +1,14 @@
+package sfBugs;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Bug3330970 {
+    public void go(List list) {
+        Iterator i = list.iterator();
+        while (i.hasNext()) {
+            //do something with the stuff in the list
+        }
+    }
+}
