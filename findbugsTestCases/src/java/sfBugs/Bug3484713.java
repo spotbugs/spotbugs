2c971912f53a1f167be19a58620ec5a038264f54
Bill Pugh <bill.pugh@gmail.com>
2012-02-09 15:26:51 +0000

    add test case for sourceforge bug 3484713
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@14259 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug3484713.java
@@ -0,0 +1,32 @@
+package sfBugs;
+
+import javax.annotation.CheckForNull;
+
+import edu.umd.cs.findbugs.annotations.DesireNoWarning;
+
+
+public class Bug3484713 {
+  @CheckForNull
+  private String foo;
+
+  public Runnable doFoo() {
+    if (foo != null) {
+      // No warning here.
+      System.out.println(foo.equals("foo"));
+    }
+    Runnable r = new Runnable() {
+        @DesireNoWarning("NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE")
+      @Override
+      public void run() {
+        if (foo != null) {
+          /*
+           * Getting a findbugs warning on next line:
+           * "Possible null pointer dereference in WrongNPWarning$1.run() due to return value of called method"
+           */
+          System.out.println(foo.equals("foo"));
+        }
+      }
+    };
+    return r;
+  }
+}
