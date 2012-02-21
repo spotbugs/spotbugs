fee086829aef13fed60372c6dfe7a22bc3239203
Bill Pugh <bill.pugh@gmail.com>
2012-02-21 00:23:44 +0000

    broke out test cases a little
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@14262 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugs/Bug3474679.java
+++ findbugsTestCases/src/java/sfBugs/Bug3474679.java
@@ -1,25 +1,54 @@
 package sfBugs;
 
 import java.math.BigDecimal;
+
 import junit.framework.Assert;
+
 import org.junit.Test;
+
+import edu.umd.cs.findbugs.annotations.DesireWarning;
 import edu.umd.cs.findbugs.annotations.ExpectWarning;
+import edu.umd.cs.findbugs.annotations.NoWarning;
 
 public class Bug3474679
 {
-    @ExpectWarning(value = "EC_UNRELATED_TYPES", num=4)
+    @ExpectWarning(value = "EC_UNRELATED_TYPES")
     @Test
-    public void testEqualsOK()
+    public void testEquals1()
     {
         Assert.assertEquals(new Double(0), new BigDecimal("0")); //correctly flagged as a bug
+        
+    }
+    @DesireWarning(value = "EC_UNRELATED_TYPES")
+    @Test
+    public void testEquals2()
+    {
         Assert.assertEquals(new Double(0), new CustomObject()); //identical bug type is missed
-
-        if (new Double(0).equals(new BigDecimal("0"))) //correctly flagged as a bug
-            ;
-        if (new Double(0).equals(new CustomObject())) //identical bug type is missed
-            ;
+    }
+    @NoWarning(value = "EC_UNRELATED_TYPES")
+    @Test
+    public void testEquals2a()
+    {
+        Assert.assertFalse(new Double(0).equals(new CustomObject())); 
     }
 
+    @ExpectWarning(value = "EC_UNRELATED_TYPES")
+    @Test
+    public void testEquals3()
+    {
+        if (new Double(0).equals(new BigDecimal("0"))) //correctly flagged as a bug
+            System.out.println("huh");
+       
+    }
+    @ExpectWarning(value = "EC_UNRELATED_TYPES")
+    @Test
+    public void testEquals4()
+    {
+       if (new Double(0).equals(new CustomObject())) //identical bug type is missed
+            System.out.println("huh");;
+    }
+    
+    
     @ExpectWarning(value = "EC_UNRELATED_TYPES", num=2)
     @Test
     public void testEqualsFalseNegative()
@@ -31,7 +60,7 @@ public class Bug3474679
     }
     
     @ExpectWarning(value = "EC_UNRELATED_TYPES", num=4)
-    public void testEquals2()
+    public void testEquals5()
     {
         Assert.assertEquals(new Double(0), new BigDecimal("0")); //correctly flagged as a bug
         Assert.assertEquals(new Double(0), new CustomObject()); //correctly flagged as a bug
