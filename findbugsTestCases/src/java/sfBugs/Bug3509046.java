b0c46d8b9b7e0e98d2b992d711d478a8d29dcc44
Bill Pugh <bill.pugh@gmail.com>
2012-03-23 14:25:24 +0000

    add test case for issue reported to sourceforge
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@14296 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugs/Bug3509046.java
@@ -0,0 +1,28 @@
+package sfBugs;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
+public class Bug3509046 implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    static class Cell implements Serializable {
+        private static final long serialVersionUID = 7526472295622776147L;
+    }
+    
+    @NoWarning("SE_BAD_FIELD")
+    private final Map<String, Cell> map = new HashMap<String, Cell>();
+
+    @NoWarning("SE_BAD_FIELD")
+    private final Map<String, String> aliases = new HashMap<String, String>();
+
+    @NoWarning("SE_BAD_FIELD")
+    private final Map<String, String> columns = new HashMap<String, String>();
+
+    
+
+}
