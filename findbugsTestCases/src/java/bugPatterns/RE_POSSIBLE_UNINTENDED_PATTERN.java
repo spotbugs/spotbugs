b86769c350f4060f275ddd0eda0e1c0685f88772
Bill Pugh <bill.pugh@gmail.com>
2012-09-27 13:31:29 +0000

    add annotations showing expected warnings
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@14439 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/bugPatterns/RE_POSSIBLE_UNINTENDED_PATTERN.java
+++ findbugsTestCases/src/java/bugPatterns/RE_POSSIBLE_UNINTENDED_PATTERN.java
@@ -1,24 +1,32 @@
 package bugPatterns;
 
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
 public class RE_POSSIBLE_UNINTENDED_PATTERN {
 
-    void bug1(String any) {
-        any.split(".");
+    @ExpectWarning("RE_POSSIBLE_UNINTENDED_PATTERN")
+    String [] bug1(String any) {
+        return any.split(".");
     }
 
-    void bug2(String any, String any2) {
-        any.replaceAll(".", any2);
+    @ExpectWarning("RE_POSSIBLE_UNINTENDED_PATTERN")
+    String bug2(String any, String any2) {
+        return any.replaceAll(".", any2);
     }
 
-    void bug3(String any, String any2) {
-        any.replaceFirst(".", any2);
+    @ExpectWarning("RE_POSSIBLE_UNINTENDED_PATTERN")
+    String bug3(String any, String any2) {
+        return any.replaceFirst(".", any2);
     }
 
-    void notBug1(String any) {
-        any.replaceAll(".", "*");
+    @NoWarning("RE_POSSIBLE_UNINTENDED_PATTERN")
+    String notBug1(String any) {
+        return any.replaceAll(".", "*");
     }
 
-    void notBug2(String any) {
-        any.indexOf(".");
+    @NoWarning("RE_POSSIBLE_UNINTENDED_PATTERN")
+    int notBug2(String any) {
+        return any.indexOf(".");
     }
 }
