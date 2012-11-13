36b2c7e9d2a5368b39c4579236be4d9f66a2311d
Bill Pugh <bill.pugh@gmail.com>
2012-11-13 15:54:16 +0000

    Add annotations to document some changes between 2.0.1 and 2.0.2
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@14479 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/bugIdeas/Ideas_2009_01_14.java
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2009_01_14.java
@@ -1,31 +1,38 @@
 package bugIdeas;
 
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
 public class Ideas_2009_01_14 {
 
-    // static String getNameCorrect(int value) {
-    // String result = "";
-    // switch (value) {
-    // case 0:
-    // result = "zero";
-    // break;
-    // case 1:
-    // result = "one";
-    // break;
-    // case 2:
-    // result = "two";
-    // break;
-    // case 3:
-    // result = "three";
-    // break;
-    // case 4:
-    // result = "four";
-    // break;
-    // default:
-    // throw new IllegalArgumentException("Illegal agrument: " + value);
-    //
-    // }
-    // return "Answer is " + result;
-    // }
+    @NoWarning("SF")
+    static String getNameCorrect(int value) {
+        String result = "";
+        switch (value) {
+        case 0:
+            result = "zero";
+            break;
+        case 1:
+            result = "one";
+            break;
+        case 2:
+            result = "two";
+            break;
+        case 3:
+            result = "three";
+            break;
+        case 4:
+            result = "four";
+            break;
+        default:
+            throw new IllegalArgumentException("Illegal agrument: " + value);
+
+        }
+        return "Answer is " + result;
+    }
+
+    @ExpectWarning("SF_DEAD_STORE_DUE_TO_SWITCH_FALLTHROUGH_TO_THROW")
+    @NoWarning("SF_SWITCH_NO_DEFAULT")
     static String getNameBroken(int value) {
         String result = "";
         switch (value) {
@@ -44,13 +51,14 @@ public class Ideas_2009_01_14 {
         case 4:
             result = "four";
         default:
-            throw new IllegalArgumentException("Illegal agrument: " + value);
+            throw new IllegalArgumentException("Illegal argument: " + value);
 
         }
         return "Answer is " + result;
     }
 
+    @ExpectWarning("VA_FORMAT_STRING_BAD_CONVERSION")
     public static void main(String args[]) {
-        System.out.printf("%d\n", 100.0);
+        System.out.printf("%d%n", 100.0);
     }
 }
