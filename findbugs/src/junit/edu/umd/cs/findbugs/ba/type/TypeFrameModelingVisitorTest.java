aa49bed1a2816b5e330d9fc71b0766ffd5f237fc
Bill Pugh <bill.pugh@gmail.com>
2013-01-18 15:51:35 +0000

    unimplemented test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@14682 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/junit/edu/umd/cs/findbugs/ba/type/TypeFrameModelingVisitorTest.java
+++ findbugs/src/junit/edu/umd/cs/findbugs/ba/type/TypeFrameModelingVisitorTest.java
@@ -1,25 +1,23 @@
 /*
  * FindBugs - Find Bugs in Java programs
  * Copyright (C) 2003-2008 University of Maryland
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 package edu.umd.cs.findbugs.ba.type;
-
-import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -29,8 +27,8 @@ import org.junit.Test;
 public class TypeFrameModelingVisitorTest {
 
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testMapSignaturePattern() {
+//        assertTrue(TypeFrameModelingVisitor.mapSignaturePattern.matcher("<") && !sourceSignature.contains("Map<TK;TV;>"))
     }
 
 }
