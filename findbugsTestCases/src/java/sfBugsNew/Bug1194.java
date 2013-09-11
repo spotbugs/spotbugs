185fc638194e41778e832e8794563cc835e4e137
Bill Pugh <bill.pugh@gmail.com>
2013-09-11 18:25:57 +0000

    Add test case
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@14776 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

---
+++ findbugsTestCases/src/java/sfBugsNew/Bug1194.java
@@ -0,0 +1,45 @@
+package sfBugsNew;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import sfBugs.Bug3399101.ParametersAreCheckForNullByDefault;
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
+public class Bug1194 {
+
+    @NoWarning("NP")
+    int noNullnessAnnotations(Object x) {
+        return x.hashCode();
+
+    }
+
+    @ExpectWarning("NP_PARAMETER_MUST_BE_NONNULL_BUT_MARKED_AS_NULLABLE")
+    @ParametersAreCheckForNullByDefault
+    int checkForNullByDefault(Object x) {
+        return x.hashCode();
+
+    }
+
+    @NoWarning("NP")
+    @ParametersAreNonnullByDefault
+    int nonnullByDefault(Object x) {
+        return 17;
+    }
+
+    @ExpectWarning("NP_NULL_PARAM_DEREF_ALL_TARGETS_DANGEROUS")
+    public void test1() {
+        System.out.println(noNullnessAnnotations(null));
+    }
+
+    @NoWarning("NP")
+    public void test2() {
+        System.out.println(checkForNullByDefault(null));
+    }
+
+    @ExpectWarning("NP_NONNULL_PARAM_VIOLATION")
+    public void test3() {
+        System.out.println(nonnullByDefault(null));
+    }
+
+}
