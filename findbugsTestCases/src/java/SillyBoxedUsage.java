808dd962e64b91abc13447f0af124353b01ee1e4
Bill Pugh <bill.pugh@gmail.com>
2013-09-25 13:45:05 +0000

    Add test cases for silly boxed usage
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@14784 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/SillyBoxedUsage.java
+++ findbugsTestCases/src/java/SillyBoxedUsage.java
@@ -1,25 +1,45 @@
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
 public class SillyBoxedUsage {
+    @ExpectWarning("DM_BOXED_PRIMITIVE_TOSTRING")
     public String testBad1(int value) {
         return new Integer(value).toString();
     }
 
+    @NoWarning("DM_BOXED_PRIMITIVE_TOSTRING")
     public String testGood1(int value) {
         return Integer.toString(value);
     }
 
+    @ExpectWarning("DM_BOXED_PRIMITIVE_TOSTRING")
     public String testBad2(float value) {
         return new Float(value).toString();
     }
 
+    @NoWarning("DM_BOXED_PRIMITIVE_TOSTRING")
     public String testGood2(float value) {
         return Float.toString(value);
     }
 
+    @ExpectWarning("DM_BOXED_PRIMITIVE_TOSTRING")
     public String testBad3(double value) {
         return new Double(value).toString();
     }
 
+    @NoWarning("DM_BOXED_PRIMITIVE_TOSTRING")
     public String testGood3(double value) {
         return Double.toString(value);
+    }
+
+    @ExpectWarning("DM_BOXED_PRIMITIVE_TOSTRING")
+    public String testBad(byte b, char c, short s, long j, boolean z) {
+        return new Byte(b).toString() + new Character(c).toString() + new Short(s).toString() + new Long(j).toString()
+                + new Boolean(z).toString();
+    }
+
+    @NoWarning("DM_BOXED_PRIMITIVE_TOSTRING")
+    public String testGood(byte b, char c, short s, long j, boolean z) {
+        return Byte.toString(b) + Character.toString(c) + Short.toString(s) + Long.toString(j) + Boolean.toString(z);
     }
 }
