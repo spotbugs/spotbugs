b6732bf987415bba36d9d18f2dd256009c52be75
Bill Pugh <bill.pugh@gmail.com>
2013-10-30 20:29:33 +0000

    Simplify TestingGround to be simple opcode stack printer
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@14884 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugs/src/java/edu/umd/cs/findbugs/detect/TestingGround.java
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/TestingGround.java
@@ -22,7 +22,6 @@ package edu.umd.cs.findbugs.detect;
 import org.apache.bcel.classfile.Code;
 
 import edu.umd.cs.findbugs.BugAccumulator;
-import edu.umd.cs.findbugs.BugInstance;
 import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.bcel.OpcodeStackDetector;
 
@@ -37,59 +36,16 @@ public class TestingGround extends OpcodeStackDetector {
         this.accumulator = new BugAccumulator(bugReporter);
     }
 
-    enum State {
-        START, SAW_INIT, FOUND, NO, RETURNED;
-    }
-
-    State state;
-
     @Override
     public void visit(Code code) {
-        boolean interesting = getMethod().isPublic() && !getMethodSig().startsWith("()");
-        if (interesting) {
-            state = State.START;
-            // initialize any variables we want to initialize for the method
-//            System.out.println(getFullyQualifiedMethodName());
-            super.visit(code); // make callbacks to sawOpcode for all opcodes
-//            System.out.println();
-            if (state == State.RETURNED)
-                bugReporter.reportBug(new BugInstance(this, "TESTING", NORMAL_PRIORITY).addClassAndMethod(this));
-
-        }
-        accumulator.reportAccumulatedBugs();
-    }
+      System.out.println(getFullyQualifiedMethodName());
+      super.visit(code);
+      System.out.println();
+      }
 
     @Override
     public void sawOpcode(int seen) {
-//        System.out.printf("%6s %3d %s%n", state, getPC(), OPCODE_NAMES[seen]);
-        switch (seen) {
-        case INVOKESPECIAL:
-            if (getNameConstantOperand().equals("<init>") && state == State.START)
-                state = State.SAW_INIT;
-            else
-                state = State.NO;
-            break;
-        case INVOKESTATIC:
-            if (state == State.SAW_INIT && getNameConstantOperand().equals("doPrivileged")
-                    && getClassConstantOperand().equals("java/security/AccessController")) {
-                state = State.FOUND;
-
-            } else
-                state = State.NO;
-            break;
-
-        case INVOKEVIRTUAL:
-        case INVOKEINTERFACE:
-            state = state.NO;
-            break;
-        case IRETURN:
-        case ARETURN:
-        case RETURN:
-            if (state == State.FOUND)
-                state = State.RETURNED;
-            else state = State.NO;
-
-        }
+        System.out.printf("%3d %-15s %s%n", getPC(), OPCODE_NAMES[seen], stack);
 
     }
 
