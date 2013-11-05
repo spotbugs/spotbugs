bca45378b31aa0b329371cf7751aec73d7681bc3
Andrey Loskutov <iloveeclipse@gmail.com>
2013-11-05 18:01:57 +0000

    Make sure we don't miss "low" confidence warnings
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@14961 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugsNew/Bug1219.java
+++ findbugsTestCases/src/java/sfBugsNew/Bug1219.java
@@ -21,15 +21,17 @@ public class Bug1219 {
     A objectA;
 
     @NoWarning(value="EC_UNRELATED_TYPES_USING_POINTER_EQUALITY", confidence=Confidence.MEDIUM)
+    @ExpectWarning(value="EC_UNRELATED_TYPES_USING_POINTER_EQUALITY", confidence=Confidence.LOW)
     public boolean compare(B objectB) {
         return (objectA == objectB);
     }
-    
+
     @NoWarning(value="EC_UNRELATED_TYPES_USING_POINTER_EQUALITY", confidence=Confidence.MEDIUM)
+    @ExpectWarning(value="EC_UNRELATED_TYPES_USING_POINTER_EQUALITY", confidence=Confidence.LOW)
       public boolean compare(C objectC) {
         return (objectA == objectC);
     }
-    
+
     @ExpectWarning(value="EC_UNRELATED_TYPES_USING_POINTER_EQUALITY", confidence=Confidence.MEDIUM)
     public static boolean compare(C objectC, D objectD) {
       return (objectC == objectD);
