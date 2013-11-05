94f999e9930d17a5e1782abb5990c765dc364000
Bill Pugh <bill.pugh@gmail.com>
2013-11-05 14:15:23 +0000

    Add test case for when interfaces do have concrete implementations
    git-svn-id: https://findbugs.googlecode.com/svn/trunk@14956 eae3c2d3-9b19-0410-a86e-396b6ccb6ab3

--- findbugsTestCases/src/java/sfBugsNew/Bug1219.java
+++ findbugsTestCases/src/java/sfBugsNew/Bug1219.java
@@ -1,6 +1,7 @@
 package sfBugsNew;
 
 import edu.umd.cs.findbugs.annotations.Confidence;
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
 import edu.umd.cs.findbugs.annotations.NoWarning;
 
 public class Bug1219 {
@@ -10,6 +11,12 @@ public class Bug1219 {
 
     interface B {
     }
+    interface C {
+    }
+    interface D {
+    }
+    static class CC implements C {}
+    static class DC implements D {}
 
     A objectA;
 
@@ -17,4 +24,14 @@ public class Bug1219 {
     public boolean compare(B objectB) {
         return (objectA == objectB);
     }
+    
+    @NoWarning(value="EC_UNRELATED_TYPES_USING_POINTER_EQUALITY", confidence=Confidence.MEDIUM)
+      public boolean compare(C objectC) {
+        return (objectA == objectC);
+    }
+    
+    @ExpectWarning(value="EC_UNRELATED_TYPES_USING_POINTER_EQUALITY", confidence=Confidence.MEDIUM)
+    public static boolean compare(C objectC, D objectD) {
+      return (objectC == objectD);
+  }
 }
