ae449fb660ddcfd6e7924d57e275fa43a349dc11
Bill Pugh <pugh@cs.umd.edu>
2014-01-09 08:28:42 -0500

    Add annotations about expected outcomes

--- findbugsTestCases/src/java/sfBugsNew/Bug1242.java
+++ findbugsTestCases/src/java/sfBugsNew/Bug1242.java
@@ -1,66 +1,59 @@
 package sfBugsNew;
 
-import java.io.Serializable;
+import edu.umd.cs.findbugs.annotations.Confidence;
+import edu.umd.cs.findbugs.annotations.DesireNoWarning;
+import edu.umd.cs.findbugs.annotations.NoWarning;
 
 public class Bug1242 {
 
-
-    public static class Child extends Parent<Something>
-    {
+    public static class Child extends Parent<Something> {
         @Override
-        public Something getDuplicateValue()
-        {
+        @DesireNoWarning("BC_UNCONFIRMED_CAST_OF_RETURN_VALUE")
+        @NoWarning(value="BC_UNCONFIRMED_CAST_OF_RETURN_VALUE", confidence=Confidence.MEDIUM)
+        public Something getDuplicateValue() {
             Something value = super.getDuplicateValue();
-            
+
             System.err.println("getValue() called.");
-            
+
             return value;
         }
     }
-    public static interface Duplicatable<T extends Duplicatable<T>> extends Cloneable
-    {
+
+    public static interface Duplicatable<T extends Duplicatable<T>> extends Cloneable {
         T getDuplicate();
     }
-    public static class Interim<T extends Interim<T>> implements Duplicatable<T>
-    {
+
+    public static class Interim<T extends Interim<T>> implements Duplicatable<T> {
         @Override
-        public T getDuplicate()
-        {
-            try
-            {
+        public T getDuplicate() {
+            try {
                 return (T) clone();
-            }
-            catch (CloneNotSupportedException cnse)
-            {
+            } catch (CloneNotSupportedException cnse) {
                 return null;
             }
         }
     }
+
     // Alternative that produces no bug warnings:
-    //public class Parent<T extends Duplicatable<T>>
-    public static class Parent<T extends Interim<T>>
-    {
+    // public class Parent<T extends Duplicatable<T>>
+    public static class Parent<T extends Interim<T>> {
         private T value;
-        
-        public void setValue(T newValue)
-        {
+
+        public void setValue(T newValue) {
             value = newValue;
         }
-        
-        public T getDuplicateValue()
-        {
-            if (value == null)
-            {
+
+        public T getDuplicateValue() {
+            if (value == null) {
                 return null;
             }
             T duplicate = value.getDuplicate();
             return duplicate;
         }
     }
-    public static class Something extends Interim<Something>
-    {
-        public void doStuff()
-        {
+
+    public static class Something extends Interim<Something> {
+        public void doStuff() {
             System.err.println("Hello");
         }
     }
