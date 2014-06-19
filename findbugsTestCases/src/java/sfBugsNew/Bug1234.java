7b2f27786a55931f1b0b7337c2ab748aa92a9fd9
Andrey Loskutov <iloveeclipse@gmail.com>
2014-06-19 20:46:23 +0200

    Added test case from bug 1234

---
+++ findbugsTestCases/src/java/sfBugsNew/Bug1234.java
@@ -0,0 +1,64 @@
+package sfBugsNew;
+
+
+import java.io.Serializable;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import edu.umd.cs.findbugs.annotations.DesireWarning;
+
+abstract class Bug1234 implements Serializable, Runnable {
+
+}
+
+class Main {
+
+   private static final String fileName = "testfile.bin";
+
+   private static Bug1234 writeRead(final Bug1234 original) {
+      try {
+         final ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(fileName));
+
+         outputStream.writeObject(original);
+         outputStream.close();
+
+         final ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(fileName));
+
+         final Bug1234 result = (Bug1234)inputStream.readObject();
+         inputStream.close();
+
+         return result;
+      } catch (final Throwable e) {
+         throw new RuntimeException("Error: " + e);
+      }
+   }
+
+
+   public static void main(final String[] args) {
+        final Bug1234 sr = Utils.create(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Will never run!");
+            }
+        });
+
+      final Bug1234 newObject = writeRead(sr);
+
+      newObject.run();
+   }
+}
+
+class Utils {
+    @DesireWarning("Se")
+    public static Bug1234 create(final Runnable action) {
+        return new Bug1234() {
+            @Override
+            public void run() {
+                action.run();
+            }
+        };
+   }
+}
