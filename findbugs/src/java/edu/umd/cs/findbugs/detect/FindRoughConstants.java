ce9ba81fbc417b73a5c64ecf4b30492041e5a111
Andrey Loskutov <iloveeclipse@gmail.com>
2014-06-19 18:52:27 +0200

    #294 New pattern: check for rough values of known constants like Math.PI.
    Detector which detects rough values of known constants. For example, if
    code uses "3.1415" double constant, it will report the following:
    
    Rough value of Math.PI found: 3.1415
    
    Rough value of known constant found
    It's recommended to use the predefined constant for code clarity and
    better precision.
    
    Bug kind and pattern: CNT - CNT_ROUGH_CONSTANT_VALUE
    
    It looks for Math.E, Math.PI, 2*Math.PI, Math.PI/2, etc. It detects
    numbers which are either too close to the original constant or equal to
    rounded (or truncated) constant value. For example, it will detect
    "3.1415" and "3.1416", but not "3.1415123", because in the latter case
    it seems that the programmer meant something different.
    
    Such detectors exist in some other static code analyzers like PVS-Studio
    for C++:
    http://www.viva64.com/en/d/0241/
    According to their experience, explicit usage of PI constant is quite
    common:
    http://www.viva64.com/en/examples/v624/
    Thus I believe that in Java world such cases occur as well and this
    detector will be useful.
    
    Contributed by Lany (dzeguziite).

---
+++ findbugs/src/java/edu/umd/cs/findbugs/detect/FindRoughConstants.java
@@ -0,0 +1,141 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2003-2007 University of Maryland
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package edu.umd.cs.findbugs.detect;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.bcel.classfile.Constant;
+import org.apache.bcel.classfile.ConstantDouble;
+import org.apache.bcel.classfile.ConstantFloat;
+
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.bcel.OpcodeStackDetector;
+
+public class FindRoughConstants extends OpcodeStackDetector {
+    static class BadConstant {
+        double base;
+        double factor;
+        String replacement;
+        double value;
+        Set<Number> approxSet = new HashSet<Number>();
+
+        BadConstant(double base, double factor, String replacement) {
+            this.base = base;
+            this.factor = factor;
+            this.value = this.base * this.factor;
+            this.replacement = replacement;
+            BigDecimal valueBig = BigDecimal.valueOf(value);
+            BigDecimal baseBig = BigDecimal.valueOf(base);
+            BigDecimal factorBig = BigDecimal.valueOf(factor);
+            for(int prec=0; prec<14; prec++)
+            {
+                addApprox(baseBig.round(new MathContext(prec, RoundingMode.FLOOR)).multiply(factorBig));
+                addApprox(baseBig.round(new MathContext(prec, RoundingMode.CEILING)).multiply(factorBig));
+                addApprox(valueBig.round(new MathContext(prec, RoundingMode.FLOOR)));
+                addApprox(valueBig.round(new MathContext(prec, RoundingMode.CEILING)));
+            }
+        }
+
+        public boolean exact(Number candidate) {
+            if(candidate instanceof Double)
+                return candidate.doubleValue() == value;
+            return candidate.floatValue() == (float)value;
+        }
+
+        public double diff(double candidate) {
+            return Math.abs(value-candidate)/value;
+        }
+
+        public boolean equalPrefix(Number candidate) {
+            return approxSet.contains(candidate);
+        }
+
+        private void addApprox(BigDecimal roundFloor) {
+            double approxDouble = roundFloor.doubleValue();
+            if(approxDouble != value && Math.abs(approxDouble-value)/value < 0.0001)
+                approxSet.add(approxDouble);
+            float approxFloat = roundFloor.floatValue();
+            if(Math.abs(approxFloat-value)/value < 0.0001)
+            {
+                approxSet.add(approxFloat);
+                approxSet.add((double)approxFloat);
+            }
+        }
+    }
+
+    private static final BadConstant[] badConstants = new BadConstant[] {
+        new BadConstant(Math.PI, 1, "Math.PI"),
+        new BadConstant(Math.PI, 1/2.0, "Math.PI/2"),
+        new BadConstant(Math.PI, 1/3.0, "Math.PI/3"),
+        new BadConstant(Math.PI, 1/4.0, "Math.PI/4"),
+        new BadConstant(Math.PI, 2, "2*Math.PI"),
+        new BadConstant(Math.E, 1, "Math.E")
+    };
+
+    BugReporter bugReporter;
+
+    public FindRoughConstants(BugReporter bugReporter) {
+        this.bugReporter = bugReporter;
+    }
+
+    @Override
+    public void sawOpcode(int seen) {
+        if (seen == LDC || seen == LDC_W || seen == LDC2_W) {
+            Constant c = getConstantRefOperand();
+            if(c instanceof ConstantFloat) {
+                checkConst(((ConstantFloat)c).getBytes());
+            } else if(c instanceof ConstantDouble) {
+                checkConst(((ConstantDouble)c).getBytes());
+            }
+        }
+    }
+
+    private void checkConst(Number constValue) {
+        double candidate = constValue.doubleValue();
+        if(Double.isNaN(candidate) || Double.isInfinite(candidate))
+            return;
+        for(BadConstant badConstant : badConstants) {
+            if (badConstant.exact(constValue))
+                continue;
+            double diff = badConstant.diff(candidate);
+            if (diff > 0.0001)
+                continue;
+            if(badConstant.equalPrefix(constValue)) {
+                bugReporter.reportBug(new BugInstance(this,
+                        "CNT_ROUGH_CONSTANT_VALUE", NORMAL_PRIORITY)
+                        .addClassAndMethod(this).addSourceLine(this)
+                        .addString(constValue.toString())
+                        .addString(badConstant.replacement));
+                return;
+            }
+            if(diff > 0.0000001)
+                continue;
+            bugReporter.reportBug(new BugInstance(this,
+                    "CNT_ROUGH_CONSTANT_VALUE", LOW_PRIORITY)
+                    .addClassAndMethod(this).addSourceLine(this)
+                    .addString(constValue.toString())
+                    .addString(badConstant.replacement));
+        }
+    }
+}
---
+++ findbugsTestCases/src/java/sfBugsNew/FindRoughConstantsTest.java
@@ -0,0 +1,95 @@
+package sfBugsNew;
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+import edu.umd.cs.findbugs.annotations.NoWarning;
+
+public class FindRoughConstantsTest {
+    private static final float PI_FLOAT = 3.14159f;
+    private static final double PI_DOUBLE = 3.14159;
+
+    @NoWarning("CNT_ROUGH_CONSTANT_VALUE")
+    public void testNan() {
+        double a;
+        a = Double.NaN;
+        a = Double.POSITIVE_INFINITY;
+        a = Double.NEGATIVE_INFINITY;
+        float b;
+        b = Float.NaN;
+        b = Float.POSITIVE_INFINITY;
+        b = Float.NEGATIVE_INFINITY;
+    }
+
+    @NoWarning("CNT_ROUGH_CONSTANT_VALUE")
+    public void testNoWarning() {
+        double a;
+        a = Math.PI; // Exact
+        a = 3.141512345; // Explicitly specified something different from PI
+        a = 3.1417;
+        a = 3.1414;
+        float b;
+        b = (float)Math.PI; // Exact value converted to float
+        b = 3.141512345f; // Explicitly specified something different from PI
+        b = 3.1417f;
+        b = 3.1414f;
+    }
+
+    @ExpectWarning("CNT_ROUGH_CONSTANT_VALUE")
+    public void testPi1() {
+        double a = 3.141592;
+    }
+
+    @ExpectWarning("CNT_ROUGH_CONSTANT_VALUE")
+    public void testPi2() {
+        double a = 3.141592612345; // Something different, but too close to PI, thus suspicious
+    }
+
+    @ExpectWarning("CNT_ROUGH_CONSTANT_VALUE")
+    public void testPi3() {
+        float a = 3.1415f;
+    }
+
+    @ExpectWarning("CNT_ROUGH_CONSTANT_VALUE")
+    public void testPi4() {
+        double a = PI_DOUBLE;
+    }
+
+    @ExpectWarning("CNT_ROUGH_CONSTANT_VALUE")
+    public void testPi5() {
+        double a = PI_FLOAT;
+    }
+
+    @ExpectWarning("CNT_ROUGH_CONSTANT_VALUE")
+    public void testPi6() {
+        float a = PI_FLOAT;
+    }
+
+    @ExpectWarning("CNT_ROUGH_CONSTANT_VALUE")
+    public void test2Pi1() {
+        double a = 2*3.141592;
+    }
+
+    @ExpectWarning("CNT_ROUGH_CONSTANT_VALUE")
+    public void test2Pi2() {
+        double a = 2*3.1416;
+    }
+
+    @ExpectWarning("CNT_ROUGH_CONSTANT_VALUE")
+    public void test2Pi3() {
+        double a = 2*3.1415;
+    }
+
+    @ExpectWarning("CNT_ROUGH_CONSTANT_VALUE")
+    public void test2Pi4() {
+        double a = 6.2831;
+    }
+
+    @ExpectWarning("CNT_ROUGH_CONSTANT_VALUE")
+    public void testE1() {
+        double a = 2.71828;
+    }
+
+    @ExpectWarning("CNT_ROUGH_CONSTANT_VALUE")
+    public void testE2() {
+        float a = 2.71828f;
+    }
+
+}
