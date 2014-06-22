7bb789f845d305474b680aa5dcca41a60c3dfbe4
Andrey Loskutov <iloveeclipse@gmail.com>
2014-06-22 09:54:35 +0200

    Added expected annotations for NP

--- findbugsTestCases/src/java/sfBugs/RFE3026970.java
+++ findbugsTestCases/src/java/sfBugs/RFE3026970.java
@@ -1,10 +1,16 @@
 package sfBugs;
 
+import javax.annotation.CheckForNull;
+
+import edu.umd.cs.findbugs.annotations.DesireWarning;
+
 public class RFE3026970 {
+
     private String get() {
         return null;
     }
 
+    @DesireWarning("NP")
     public void testDesireWarning() {
         if (get().equals("A"))
             System.out.println("test");
