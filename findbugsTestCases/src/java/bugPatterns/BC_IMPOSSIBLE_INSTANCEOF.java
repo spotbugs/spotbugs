be8abcd67efe7dc0dbb4da71dd8a1e07bf7ec350
Andrey Loskutov <iloveeclipse@gmail.com>
2014-06-29 15:25:47 +0200

    Don't use BCEL dependency, junit DetectorsTest can't resolve it (TODO: check why?)

--- findbugsTestCases/src/java/bugPatterns/BC_IMPOSSIBLE_INSTANCEOF.java
+++ findbugsTestCases/src/java/bugPatterns/BC_IMPOSSIBLE_INSTANCEOF.java
@@ -2,17 +2,13 @@ package bugPatterns;
 
 import javax.annotation.Nonnull;
 
-import org.apache.bcel.generic.BasicType;
-import org.apache.bcel.generic.ReferenceType;
-import org.apache.bcel.generic.Type;
-
 import edu.umd.cs.findbugs.annotations.ExpectWarning;
 import edu.umd.cs.findbugs.ba.IncompatibleTypes;
 
 public class BC_IMPOSSIBLE_INSTANCEOF {
     // seen in edu.umd.cs.findbugs.ba.IncompatibleTypes
 
-    @ExpectWarning(value = "BC_IMPOSSIBLE_INSTANCEOF", num = 1)
+    @ExpectWarning("BC_IMPOSSIBLE_INSTANCEOF")
     static public @Nonnull
     boolean getPriorityForAssumingCompatible(Type expectedType, Type actualType, boolean pointerEquality) {
         if (expectedType.equals(actualType))
@@ -28,4 +24,16 @@ public class BC_IMPOSSIBLE_INSTANCEOF {
         }
         return false;
     }
+}
+
+class Type {
+
+}
+
+class BasicType extends Type {
+
+}
+
+class ReferenceType extends Type {
+
 }
