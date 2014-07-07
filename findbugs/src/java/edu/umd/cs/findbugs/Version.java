2cfb4688d8676d34fee0e6f153a6a53354ff2e20
Bill Pugh <pugh@cs.umd.edu>
2014-07-06 20:05:45 -0500

    Change version number to FindBugs 3.0.0

--- findbugs/src/java/edu/umd/cs/findbugs/Version.java
+++ findbugs/src/java/edu/umd/cs/findbugs/Version.java
@@ -59,13 +59,13 @@ public class Version {
     /**
      * Development version or release candidate?
      */
-    public static final boolean IS_DEVELOPMENT = true;
+    public static final boolean IS_DEVELOPMENT = false;
 
     /**
      * Release candidate number. "0" indicates that the version is not a release
      * candidate.
      */
-    public static final int RELEASE_CANDIDATE = 2;
+    public static final int RELEASE_CANDIDATE = 0;
 
 
     public static final String GIT_REVISION  = System.getProperty("git.revision", "UNKNOWN");
