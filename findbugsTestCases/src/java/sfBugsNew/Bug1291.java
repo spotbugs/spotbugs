0edf54656b66e9be9f76e94fedd8a561a948efcb
Andrey Loskutov <iloveeclipse@gmail.com>
2014-08-18 20:32:17 +0200

    Added exmple for BC_IMPOSSIBLE_INSTANCEOF and BC_VACUOUS_INSTANCEOF false positives on try/multicatch, see https://sourceforge.net/p/findbugs/bugs/1291

---
+++ findbugsTestCases/src/java/sfBugsNew/Bug1291.java
@@ -0,0 +1,33 @@
+package sfBugsNew;
+
+import edu.umd.cs.findbugs.annotations.DesireNoWarning;
+
+public class Bug1291 {
+    Exception exception;
+
+    public void test() throws ArrayIndexOutOfBoundsException, UnsupportedOperationException {
+    }
+
+    protected void a() {
+        try {
+            test();
+        } catch (ArrayIndexOutOfBoundsException | UnsupportedOperationException ex) {
+            exception = ex;
+        }
+    }
+
+    @DesireNoWarning("BC_VACUOUS_INSTANCEOF")
+    protected void b(int state) {
+        if (exception instanceof ArrayIndexOutOfBoundsException) {
+            return;
+        }
+    }
+
+    @DesireNoWarning("BC_IMPOSSIBLE_INSTANCEOF")
+    protected void c(int state) {
+        if (exception instanceof UnsupportedOperationException) {
+            return;
+        }
+    }
+
+}
