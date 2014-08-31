a6af35d19afa859661aad6aa82f510739605d095
Andrey Loskutov <iloveeclipse@gmail.com>
2014-08-31 16:28:29 +0200

    Minimal compilable example from bug 1295 (see https://sourceforge.net/p/findbugs/bugs/1295/): FB should recognize this::methodRef calls.

---
+++ findbugsTestCases/src/java/sfBugsNew/Bug1295.java
@@ -0,0 +1,36 @@
+package sfBugsNew;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.umd.cs.findbugs.annotations.DesireNoWarning;
+
+public class Bug1295 {
+
+    public List<AncestorNodeReference> getAncestors() {
+        List<AncestorNodeReference> ancestorNodes = new ArrayList<>();
+
+        // TODO we should compile FB with 1.8
+//        ancestorNodes = new ArrayList<AncesterNodeReferenceDTO>()
+//                .stream()
+//                .map(this::createAncestorNodeReference)
+//                .collect(java.util.stream.Collectors.toList());
+
+        return ancestorNodes;
+    }
+
+    @DesireNoWarning("UPM_UNCALLED_PRIVATE_METHOD")
+    private AncestorNodeReference createAncestorNodeReference(final AncesterNodeReferenceDTO ancestorNodeRefDTO) {
+        return createAncestorNodeReference("");
+    }
+
+    private AncestorNodeReference createAncestorNodeReference(String bla) {
+        return null;
+    }
+}
+
+class AncestorNodeReference {
+}
+
+class AncesterNodeReferenceDTO {
+}
