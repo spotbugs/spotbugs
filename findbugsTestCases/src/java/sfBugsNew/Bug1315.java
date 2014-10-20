3e98f74f9e4b28a3d2db97714f622c3dcc199546
Tagir Valeev <lany@ngs.ru>
2014-10-20 18:09:35 +0700

    Bug#1315: INT_BAD_COMPARISON_WITH_NONNEGATIVE_VALUE doesn't work when comparing with 0 (+test case)

---
+++ findbugsTestCases/src/java/sfBugsNew/Bug1315.java
@@ -0,0 +1,15 @@
+package sfBugsNew;
+
+import java.util.List;
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
+
+public class Bug1315 {
+    @ExpectWarning("INT_BAD_COMPARISON_WITH_NONNEGATIVE_VALUE")
+    public String getLastListElement(List<String> list) {
+        int s = list.size();
+        if(s >= 0) {
+            return list.get(s-1);
+        }
+        return null;
+    }
+}
