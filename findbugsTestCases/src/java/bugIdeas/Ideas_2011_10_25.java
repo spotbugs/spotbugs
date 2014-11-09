6616a0e35389145f647b952af25cc8db3796253f
Bill Pugh <pugh@cs.umd.edu>
2014-11-08 22:54:47 -0500

    Uncomment out some sample code

--- findbugsTestCases/src/java/bugIdeas/Ideas_2011_10_25.java
+++ findbugsTestCases/src/java/bugIdeas/Ideas_2011_10_25.java
@@ -1,56 +1,65 @@
 package bugIdeas;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import edu.umd.cs.findbugs.annotations.DesireWarning;
 
 public class Ideas_2011_10_25 {
-//    static int counter;
-//    
-//    public int getNext() {
-//        synchronized(getClass()) {
-//            return counter++;
-//        }
-//    }
-//    
-//    static class Subclass extends Ideas_2011_10_25 {
-//    }
-//    
-//    Lock lock = new ReentrantLock();
-//    
-//    @DesireWarning("")
-//    public synchronized int getNext2() {
-//        return counter++;
-//    }
-//    
-//    int value;
-//    public int nextValue() {
-//        synchronized (lock) {
-//            return value++;
-//            
-//        }
-//    }
-//
-//    
-//    public void integerLocks() {
-//        Integer integerUnsafe = -100;
-//        Integer integerSafe = new Integer(-100);
-//        synchronized(integerUnsafe) { }
-//    }
-//    public void integerLocks2() {
-//        Integer integerUnsafe = -100;
-//        Integer integerSafe = new Integer(-100);
-//        synchronized(integerSafe) { }
-//    }
-    
+    static int counter;
+
+    public int getNext() {
+        synchronized (getClass()) {
+            return counter++;
+        }
+    }
+
+    static class Subclass extends Ideas_2011_10_25 {
+    }
+
+    Lock lock = new ReentrantLock();
+
+    @DesireWarning("")
+    public synchronized int getNext2() {
+        return counter++;
+    }
+
+    int value;
+
+    public int nextValue() {
+        synchronized (lock) {
+            return value++;
+
+        }
+    }
+
+    public void integerLocks() {
+        Integer integerUnsafe = -100;
+        Integer integerSafe = new Integer(-100);
+        synchronized (integerUnsafe) {
+        }
+    }
+
+    public void integerLocks2() {
+        Integer integerUnsafe = -100;
+        Integer integerSafe = new Integer(-100);
+        synchronized (integerSafe) {
+        }
+    }
+
     long last;
+
     void foo() {
-    for (int i=1;i < 0x00000000FFFFFFFFL;i++) {
-       
+        for (int i = 1; i < 0x00000000FFFFFFFFL; i++) {
+
+        }
     }
-    }
+
     void foo2() {
 
-            if (++last >  0x00000000FFFFFFFFL) last = 1;
-        
-        }
+        if (++last > 0x00000000FFFFFFFFL)
+            last = 1;
 
+    }
 
 }
