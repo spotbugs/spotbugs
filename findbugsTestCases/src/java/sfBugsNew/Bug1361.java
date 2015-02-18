48f61df132e094461f7e027a0ba56826c316a18b
Tagir Valeev <lany@ngs.ru>
2015-02-18 23:13:24 +0600

    Bug1361: formatting

--- findbugsTestCases/src/java/sfBugsNew/Bug1361.java
+++ findbugsTestCases/src/java/sfBugsNew/Bug1361.java
@@ -4,11 +4,11 @@ import edu.umd.cs.findbugs.annotations.NoWarning;
 
 public class Bug1361 {
     @NoWarning("UC")
-	public void test(boolean flag) {
-		if(!flag) {
-			System.out.println("No flag");
-		} else {
-			System.out.println("!flag = "+(!flag));
-		}
-	}
+    public void test(boolean flag) {
+        if (!flag) {
+            System.out.println("No flag");
+        } else {
+            System.out.println("!flag = " + (!flag));
+        }
+    }
 }
