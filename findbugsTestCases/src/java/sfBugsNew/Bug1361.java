e540cc9c62307c983987051aa7b5ce6d442717ed
Tagir Valeev <lany@ngs.ru>
2015-02-21 11:51:22 +0600

    A couple of tweaks in RedundantConditions: 1) more robust detection of assertion calls. 2) assertion with non-bordering condition (involving new constant) is still reported with low priority/rank 19 (it's sometimes suspicious). 3) some integer expressions involving (flag ? 0 : 1) are distinguished from (!flag) cases and reported

--- findbugsTestCases/src/java/sfBugsNew/Bug1361.java
+++ findbugsTestCases/src/java/sfBugsNew/Bug1361.java
@@ -1,5 +1,6 @@
 package sfBugsNew;
 
+import edu.umd.cs.findbugs.annotations.ExpectWarning;
 import edu.umd.cs.findbugs.annotations.NoWarning;
 
 public class Bug1361 {
@@ -10,5 +11,29 @@ public class Bug1361 {
         } else {
             System.out.println("!flag = " + (!flag));
         }
+    }
+
+    @ExpectWarning("UC_USELESS_CONDITION")
+    public int testInt(boolean flag) {
+        if(!flag) {
+            return 5 + (flag ? 0 : 1);
+        }
+        return 0;
+    }
+
+    @ExpectWarning("UC_USELESS_CONDITION")
+    public int testInt2(boolean flag) {
+        if(flag) {
+            return 5 - (flag ? 1 : 0);
+        }
+        return 0;
+    }
+
+    @ExpectWarning("UC_USELESS_CONDITION")
+    public int testInt3(boolean flag) {
+        if(flag) {
+            return (flag ? 1 : 0) + Math.round(10.0f * 5) * 10;
+        }
+        return 0;
     }
 }
