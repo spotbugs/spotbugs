138e1a1024d1fc5b8070824059b194f8df44ebcc
Bill <pugh@midge.local>
2015-03-06 13:01:34 -0500

    Version 3.0.1

--- findbugs/src/java/edu/umd/cs/findbugs/Version.java
+++ findbugs/src/java/edu/umd/cs/findbugs/Version.java
@@ -59,7 +59,7 @@ public class Version {
     /**
      * Development version or release candidate?
      */
-    public static final boolean IS_DEVELOPMENT = true;
+    public static final boolean IS_DEVELOPMENT = false;
 
     /**
      * Release candidate number. "0" indicates that the version is not a release
