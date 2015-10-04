dcfb9348d4d16c751e56f51244a12b6777926985
Tagir Valeev <lany@ngs.ru>
2015-10-04 16:34:03 +0600

    ClassName.isAnonymous fixed

--- findbugs/src/java/edu/umd/cs/findbugs/util/ClassName.java
+++ findbugs/src/java/edu/umd/cs/findbugs/util/ClassName.java
@@ -217,6 +217,7 @@ public abstract class ClassName {
                 if(!Character.isDigit(className.charAt(i))) {
                     return false;
                 }
+                i++;
             }
             return true;
         }
