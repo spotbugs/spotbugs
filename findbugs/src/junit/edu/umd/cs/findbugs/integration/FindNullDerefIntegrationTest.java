78b896aab8f9dd1413986689683b11ce2984bfd6
Juan Martín Sotuyo Dodero <jmsotuyo@monits.com>
2016-11-26 20:04:44 -0300

    Add another test example
     - Slowly rewriting tests, we should move them away to a
        separate sourceset instead of findbugsTestCases

--- findbugs/src/junit/edu/umd/cs/findbugs/integration/FindNullDerefIntegrationTest.java
+++ findbugs/src/junit/edu/umd/cs/findbugs/integration/FindNullDerefIntegrationTest.java
@@ -17,33 +17,33 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-package edu.umd.cs.findbugs;
+package edu.umd.cs.findbugs.integration;
 
 import static org.hamcrest.Matchers.hasItem;
 import static org.junit.Assert.assertThat;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
+import edu.umd.cs.findbugs.AbstractIntegrationTest;
 import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcher;
 import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
 
 public class FindNullDerefIntegrationTest extends AbstractIntegrationTest {
 
-    @Ignore("Github Issue 20 is still unresolved")
     @Test
     public void testNullFromReturnOnLambda() {
-        performAnalysis("lambdas/Issue20.class");
+        performAnalysis("Elvis.class");
 
         // There should only be 1 issue of this type
         final BugInstanceMatcher bugTypeMatcher = new BugInstanceMatcherBuilder()
-                .bugType("NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE").build();
+                .bugType("SI_INSTANCE_BEFORE_FINALS_ASSIGNED").build();
         assertThat(getBugCollection(), containsExactly(bugTypeMatcher, 1));
 
+        // It must be on the INSTANCE field
         final BugInstanceMatcher bugInstanceMatcher = new BugInstanceMatcherBuilder()
-                .bugType("NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE")
-                .inClass("Issue20")
-                .atLine(25)
+                .bugType("SI_INSTANCE_BEFORE_FINALS_ASSIGNED")
+                .inClass("Elvis")
+                .atField("INSTANCE")
                 .build();
         assertThat(getBugCollection(), hasItem(bugInstanceMatcher));
     }
