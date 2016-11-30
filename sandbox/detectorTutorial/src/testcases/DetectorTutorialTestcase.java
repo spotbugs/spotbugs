4a8f0937a2b7a86599099143a48b0f742b3dd837
Juan Martín Sotuyo Dodero <jmsotuyo@monits.com>
2016-11-30 16:13:03 -0300

    Remove everything we don't want to support anymore
     - BlueJ support is gone
     - WebCloud is gone
     - All plugins are gone
     - random directories with no use are gone

--- sandbox/detectorTutorial/src/testcases/DetectorTutorialTestcase.java
+++
@@ -1,8 +0,0 @@
-import java.math.BigDecimal;
-
-
-public class DetectorTutorialTestcase {
-    public void bad() {
-        BigDecimal decimal = new BigDecimal(10.0);
-    }
-}
--- sandbox/localCloud/etc/fbcloud.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -jar localCloud.jar
