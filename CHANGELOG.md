f317c16889c49b4b45455ecd559bc80d4662f4a2
jpw <jan-peter.wilhelms@src-gmbh.de>
2016-12-27 11:42:04 +0100

    * Added bug pattern "OVERRIDING_METHODS_MUST_INVOKE_SUPER" * Fixed not working detector 'CbeckMustOverrideSuperAnnotation' and renamed to 'OverridingMethodsMustInvokeSuperDetector'

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ This is the changelog for SpotBugs. This follows [Keep a Changelog v1.0.0](http:
 Currently the versioning policy of this project follows [Semantic Versioning v2.0.0](http://semver.org/spec/v2.0.0.html).
 
 ## Unreleased - 2020-??-??
+### Fixed
+* Fixed not working detector 'CbeckMustOverrideSuperAnnotation' and renamed to 'OverridingMethodsMustInvokeSuperDetector'
 
 ## 4.0.6 - 2020-06-23
 ### Fixed
---
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/OverridingMethodMustInvokeSuperTest.java
@@ -0,0 +1,24 @@
+package edu.umd.cs.findbugs.detect;
+
+import edu.umd.cs.findbugs.AbstractIntegrationTest;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcher;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
+import org.junit.Test;
+
+import static edu.umd.cs.findbugs.test.CountMatcher.containsExactly;
+import static org.junit.Assert.assertThat;
+
+public class OverridingMethodMustInvokeSuperTest extends AbstractIntegrationTest {
+
+    @Test
+    public void test() {
+        performAnalysis(
+                "NeedsCallOfSuper.class",
+                "NeedsCallOfSuper$DerivedClass.class",
+                "NeedsCallOfSuper$ConcreteClass.class",
+                "NeedsCallOfSuper$GenericClass.class");
+        BugInstanceMatcher bugMatcher = new BugInstanceMatcherBuilder().bugType("OVERRIDING_METHODS_MUST_INVOKE_SUPER").build();
+        assertThat(getBugCollection(), containsExactly(1, bugMatcher));
+    }
+
+}
