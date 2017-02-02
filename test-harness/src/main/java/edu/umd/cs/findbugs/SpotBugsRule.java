11bd899864d97ed44696ae50465bd82f5ebd5ece
Kengo TODA <skypencil@gmail.com>
2017-02-03 00:12:18 +0800

    improve interface and javadoc

--- test-harness/src/main/java/edu/umd/cs/findbugs/SpotBugsRule.java
+++ test-harness/src/main/java/edu/umd/cs/findbugs/SpotBugsRule.java
@@ -8,14 +8,37 @@ import javax.annotation.ParametersAreNonnullByDefault;
 import org.hamcrest.Matcher;
 import org.junit.rules.ExternalResource;
 
-import edu.umd.cs.findbugs.internalAnnotations.SlashedClassName;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcher;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
 
 /**
  * <p>
  * A JUnit rule to execute integration test for SpotBugs. This is basically
- * designed to help SpotBugs plugin developer to test their product.
+ * designed to help SpotBugs plugin developer to test their product. Basic usage:
  * </p>
+ * <pre><code>
+ * public class MyIntegrationTest {
+ *     &#064;Rule
+ *     public SpotBugsRule spotbugs = new SpotBugsRule();
+ *     &#064;Test
+ *     public void testIssuesAreFound() {
+ *         spotbugs.addFile(Paths.get("target/classes/my/company/AnalyzedClass.class"));
+ *         BugCollection bugCollection = spotbugs.performAnalysis();
  *
+ *         // There should only be exactly 1 issue of this type
+ *         final BugInstanceMatcher bugTypeMatcher = new BugInstanceMatcherBuilder()
+ *                 .bugType("NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE").build();
+ *         assertThat(bugCollection, containsExactly(bugTypeMatcher, 1));
+ *
+ *         final BugInstanceMatcher bugInstanceMatcher = new BugInstanceMatcherBuilder()
+ *                 .bugType("NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE")
+ *                 .inClass("AnalyzedClass")
+ *                 .atLine(25)
+ *                 .build();
+ *         assertThat(bugCollection, hasItem(bugInstanceMatcher));
+ *     }
+ * }
+ * </code></pre>
  * @since 3.1
  */
 @ParametersAreNonnullByDefault
@@ -32,10 +55,40 @@ public class SpotBugsRule extends ExternalResource {
         runner = null;
     }
 
+    /**
+     * <p>
+     * A factory method to build a {@link Matcher} instance to check how many
+     * {@link BugInstance} matches in {@link BugCollection}. Basic usage:
+     * </p>
+     * <pre>
+     * <code>
+     * BugCollection bugCollection = spotBugsRule.performAnalysis();
+     * assertThat(
+     *     bugCollection,
+     *     containsExactly(bugInstanceMatcher, 1));
+     * </code>
+     * </pre>
+     * @param matcher
+     *            A matcher to match {@link BugInstance}. non-null.
+     * @param count
+     *            How many times you expect that specified matcher matches.
+     * @return A matcher which matches specified times
+     * @see BugInstanceMatcher
+     * @see BugInstanceMatcherBuilder
+     */
     public static <T> Matcher<Iterable<T>> containsExactly(final Matcher<T> matcher, final int count) {
         return new CountMatcher<T>(count, matcher);
     }
 
+    /**
+     * <p>
+     * Add a target class file to SpotBugs analysis.
+     * </p>
+     * @param path
+     *      A path of the target class file. Non-null.
+     * @return callee itself, so caller can chain another method in fluent interface.
+     */
+    // TODO let users specify SlashedClassName, then find its file path automatically
     @Nonnull
     public SpotBugsRule addFile(Path path) {
         if (runner == null) {
@@ -45,20 +98,36 @@ public class SpotBugsRule extends ExternalResource {
         return this;
     }
 
+    /**
+     * <p>
+     * Add an entry to aux classpath of SpotBugs analysis.
+     * </p>
+     * @param path
+     *      A path of the target class file or jar file. Non-null.
+     * @return callee itself, so caller can chain another method in fluent interface.
+     */
+    // TODO let users specify "groupId:artifactId:packaging:version:classifier" like Grape in Groovy
     @Nonnull
     public SpotBugsRule addAuxClasspathEntry(Path path) {
         if (runner == null) {
-            throw new IllegalStateException("Please call this addAuxClasspathEntry() method in @Before method or test method");
+            throw new IllegalStateException(
+                    "Please call this addAuxClasspathEntry() method in @Before method or test method");
         }
         runner.addAuxClasspathEntry(path);
         return this;
     }
 
+    /**
+     * <p>
+     * Run SpotBugs under given condition, and return its result.
+     * </p>
+     * @return a {@link BugCollection} which contains all detected bugs.
+     */
     @Nonnull
-    public BugCollectionBugReporter performAnalysis(@SlashedClassName final String... analyzeMe) {
+    public BugCollection performAnalysis() {
         if (runner == null) {
             throw new IllegalStateException("Please call this performAnalysis() method in test method");
         }
-        return runner.run();
+        return runner.run().getBugCollection();
     }
 }
