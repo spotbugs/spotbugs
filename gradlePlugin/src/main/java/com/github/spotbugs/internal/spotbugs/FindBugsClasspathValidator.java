1936e29cae6f7ac68d69189ddf50b0e6501b729b
John Scancella <jsca@loc.gov>
2017-06-26 07:53:05 -0400

    refs #214 - just look for spotbugs on classpath

--- gradlePlugin/src/main/java/com/github/spotbugs/internal/spotbugs/FindBugsClasspathValidator.java
+++ gradlePlugin/src/main/java/com/github/spotbugs/internal/spotbugs/FindBugsClasspathValidator.java
@@ -20,8 +20,8 @@ public class FindBugsClasspathValidator {
       boolean java8orMore = javaVersion.compareTo(JavaVersion.VERSION_1_7) > 0;
       boolean findbugs2orLess = v.getMajor() < 3;
       if (java8orMore && findbugs2orLess) {
-          throw new FindBugsVersionTooLowException("The version of FindBugs (" + v + ") inferred from FindBugs classpath is too low to work with currently used Java version (" + javaVersion + ")."
-                  + " Please use higher version of FindBugs. Inspected FindBugs classpath: " + fileNamesOnClasspath);
+          throw new FindBugsVersionTooLowException("The version of SpotBugs (" + v + ") inferred from SpotBugs classpath is too low to work with currently used Java version (" + javaVersion + ")."
+                  + " Please use higher version of SpotBugs. Inspected SpotBugs classpath: " + fileNamesOnClasspath);
       }
   }
 
@@ -35,9 +35,9 @@ public class FindBugsClasspathValidator {
 
   private VersionNumber getFindbugsVersion(Iterable<String> classpath) {
       for (String f: classpath) {
-          Matcher m = Pattern.compile("(spotbugs|findbugs)-(\\d+.*)\\.jar").matcher(f);
+          Matcher m = Pattern.compile("spotbugs-(\\d+.*)\\.jar").matcher(f);
           if (m.matches()) {
-              return VersionNumber.parse(m.group(2));
+              return VersionNumber.parse(m.group(1));
           }
       }
       throw new GradleException("Unable to infer the version of FindBugs from currently specified FindBugs classpath: " + classpath);
