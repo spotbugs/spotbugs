0313cbce1f406170e700e7025526e9b759ff7af6
Andreas Sewe <andreas.sewe@codetrails.com>
2017-07-07 18:45:22 +0200

    Don't use @Before setup unless necessary

--- spotbugs/src/test/java/edu/umd/cs/findbugs/config/UserPreferencesTest.java
+++ spotbugs/src/test/java/edu/umd/cs/findbugs/config/UserPreferencesTest.java
@@ -1,23 +1,17 @@
 package edu.umd.cs.findbugs.config;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class UserPreferencesTest {
 
-    UserPreferences prefs;
-
-    @Before
-    public void setUp() {
-        prefs = UserPreferences.createDefaultUserPreferences();
-    }
-
     @Test
     public void testClone() {
-        UserPreferences clone = prefs.clone();
+        UserPreferences sut = UserPreferences.createDefaultUserPreferences();
 
-        Assert.assertEquals(prefs, clone);
-        Assert.assertEquals(prefs.getClass(), clone.getClass());
+        UserPreferences clone = sut.clone();
+
+        Assert.assertEquals(sut, clone);
+        Assert.assertEquals(sut.getClass(), clone.getClass());
     }
 }
