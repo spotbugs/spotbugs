66986c2020c5b4f0da50a4d21a2f302245612d4b
Andreas Sewe <andreas.sewe@codetrails.com>
2017-08-12 21:47:06 +0200

    Use new-style package-info.java instead of package.html
    In particular, this avoids two clashes ("javadoc: warning -
    Multiple sources of package comments found for package") in
    packages edu.umd.cs.findbugs.ba and edu.umd.cs.findbugs.ba.obl.

--- spotbugs/src/main/java/edu/umd/cs/findbugs/ba/obl/package-info.java
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/ba/obl/package-info.java
@@ -1,4 +1,10 @@
+/**
+ * Implementation of dataflow analysis for checking whether obligations to close
+ * streams and other resources (e&#x2E;g&#x2E;, database objects) are satisfied.
+ * 
+ * @see <a href="https://doi.org/10.1145/1035292.1029011">Weimer and Necula,
+ *      Finding and preventing run-time error handling mistakes</a>.
+ */
 @javax.annotation.ParametersAreNonnullByDefault
 @edu.umd.cs.findbugs.internalAnnotations.AnalysisContextContained
 package edu.umd.cs.findbugs.ba.obl;
-
---
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/model/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Eventually, all model classes (BugInstance, BugCollection, etc.) should go in
+ * this package.
+ */
+package edu.umd.cs.findbugs.model;
