fa8c2a58961deb4490c8e1f3be4a417fb67c0228
Andreas Sewe <andreas.sewe@codetrails.com>
2017-08-12 21:47:06 +0200

    Use new-style package-info.java instead of package.html
    In particular, this avoids two clashes ("javadoc: warning -
    Multiple sources of package comments found for package") in
    packages edu.umd.cs.findbugs.ba and edu.umd.cs.findbugs.ba.obl.

--- spotbugs/src/main/java/edu/umd/cs/findbugs/ba/package-info.java
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/ba/package-info.java
@@ -1,4 +1,7 @@
+/**
+ * A bytecode analysis framework for BCEL, providing CFG construction, generic
+ * dataflow analysis, and a variety of specific dataflow analyses.
+ */
 @javax.annotation.ParametersAreNonnullByDefault
 @edu.umd.cs.findbugs.internalAnnotations.AnalysisContextContained
 package edu.umd.cs.findbugs.ba;
-
---
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Main package for the FindBugs application - contains the engine class
+ * ({@link edu.umd.cs.findbugs.FindBugs}), the object model classes for bug
+ * instances ({@link edu.umd.cs.findbugs.BugInstance},
+ * {@link edu.umd.cs.findbugs.BugAnnotation}), and other miscellany.
+ */
+package edu.umd.cs.findbugs;
