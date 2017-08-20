ed41d3db0cf8c38959604a1995449840dcb69941
Kengo TODA <skypencil@gmail.com>
2017-08-20 22:39:43 +0800

    add test to find problem like #320

--- gradlePlugin/src/test/java/com/github/spotbugs/SpotBugsPluginTest.java
+++ gradlePlugin/src/test/java/com/github/spotbugs/SpotBugsPluginTest.java
@@ -5,9 +5,12 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Optional;
 
 import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.BuildTask;
 import org.gradle.testkit.runner.GradleRunner;
+import org.gradle.testkit.runner.TaskOutcome;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -42,6 +45,37 @@ public class SpotBugsPluginTest extends Assert{
     BuildResult result = GradleRunner.create().withProjectDir(folder.getRoot()).withArguments(Arrays.asList("tasks", "--all")).withPluginClasspath().build();
     assertTrue(result.getOutput().contains("spotbugsMain"));
     assertTrue(result.getOutput().contains("spotbugsTest"));
+  }
+
+  @Test
+  public void testSpotBugsTaskCanRun() throws Exception {
+    String buildScript = "plugins {\n" + 
+        "  id 'java'\n" +
+        "  id 'com.github.spotbugs'\n" + 
+        "}\n" + 
+        "version = 1.0\n" +
+        "repositories {\n" + 
+        "  mavenLocal()\n" + 
+        "}";
+    File root = folder.newFolder();
+    File buildFile = new File(root, "build.gradle");
+    Files.write(buildScript.getBytes(), buildFile);
+
+    File sourceDir = new File(root, "src/main/java");
+    assertTrue(sourceDir.mkdirs());
+    File to = new File(sourceDir, "Foo.java");
+    File from = new File("src/test/java/com/github/spotbugs/Foo.java");
+    Files.copy(from, to);
+
+    BuildResult result = GradleRunner.create()
+            .withProjectDir(root)
+            .withArguments(Arrays.asList("compileJava", "spotbugsMain"))
+            .withPluginClasspath().build();
+    Optional<BuildTask> spotbugsMain = result.getTasks().stream()
+            .filter(task -> task.getPath().equals(":spotbugsMain"))
+            .findAny();
+    assertTrue(spotbugsMain.isPresent());
+    assertThat(spotbugsMain.get().getOutcome(), is(TaskOutcome.SUCCESS));
   }
 
   @Test
