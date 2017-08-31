04bf337ce618e245a24c8e274c34f88e5e02d6c4
Kengo TODA <skypencil@gmail.com>
2017-09-01 04:21:45 +0800

    refs #337: extract shared logic to Before method

--- gradlePlugin/src/test/java/com/github/spotbugs/SpotBugsPluginTest.java
+++ gradlePlugin/src/test/java/com/github/spotbugs/SpotBugsPluginTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ import org.gradle.testkit.runner.BuildTask;
 import org.gradle.testkit.runner.GradleRunner;
 import org.gradle.testkit.runner.TaskOutcome;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -21,53 +23,44 @@ import com.google.common.io.Files;
 public class SpotBugsPluginTest extends Assert{
   @Rule
   public TemporaryFolder folder= new TemporaryFolder();
-  
-  @Test
-  public void TestSpotBugsTasksExist() throws Exception{
-    String buildScript = "plugins {\n" + 
-        "  id 'java'\n" +
-        "  id 'com.github.spotbugs'\n" + 
-        "}\n" + 
-        "version = 1.0\n" + 
-        "repositories {\n" + 
-        "  mavenCentral()\n" + 
-        "}";
-    
-    File sourceDir = folder.newFolder("src", "main", "java");
-    File to = new File(sourceDir, "foo.java");
+
+  /**
+   * The root directory of generated project by {@link #createProject()}
+   */
+  private File root;
+
+  @Before
+  public void createProject() throws IOException {
+    String buildScript = "plugins {\n" +
+      "  id 'java'\n" +
+      "  id 'com.github.spotbugs'\n" +
+      "}\n" +
+      "version = 1.0\n" +
+      "repositories {\n" +
+      "  mavenCentral()\n" +
+      "  mavenLocal()\n" +
+      "}";
+    root = folder.newFolder();
+    File buildFile = new File(root, "build.gradle");
+    Files.write(buildScript.getBytes(), buildFile);
+
+    File sourceDir = new File(root, "src/main/java");
+    assertTrue(sourceDir.mkdirs());
+
+    File to = new File(sourceDir, "Foo.java");
     File from = new File("src/test/java/com/github/spotbugs/Foo.java");
     Files.copy(from, to);
-    
-    File buildFile = folder.newFile("build.gradle");
-    Files.write(buildScript.getBytes(), buildFile);
-    
-    
-    BuildResult result = GradleRunner.create().withProjectDir(folder.getRoot()).withArguments(Arrays.asList("tasks", "--all")).withPluginClasspath().build();
+  }
+
+  @Test
+  public void TestSpotBugsTasksExist() throws Exception{
+    BuildResult result = GradleRunner.create().withProjectDir(root).withArguments(Arrays.asList("tasks", "--all")).withPluginClasspath().build();
     assertTrue(result.getOutput().contains("spotbugsMain"));
     assertTrue(result.getOutput().contains("spotbugsTest"));
   }
 
   @Test
   public void testSpotBugsTaskCanRun() throws Exception {
-    String buildScript = "plugins {\n" + 
-        "  id 'java'\n" +
-        "  id 'com.github.spotbugs'\n" + 
-        "}\n" + 
-        "version = 1.0\n" +
-        "repositories {\n" + 
-        "  mavenCentral()\n" + 
-        "  mavenLocal()\n" + 
-        "}";
-    File root = folder.newFolder();
-    File buildFile = new File(root, "build.gradle");
-    Files.write(buildScript.getBytes(), buildFile);
-
-    File sourceDir = new File(root, "src/main/java");
-    assertTrue(sourceDir.mkdirs());
-    File to = new File(sourceDir, "Foo.java");
-    File from = new File("src/test/java/com/github/spotbugs/Foo.java");
-    Files.copy(from, to);
-
     BuildResult result = GradleRunner.create()
             .withProjectDir(root)
             .withArguments(Arrays.asList("compileJava", "spotbugsMain"))
