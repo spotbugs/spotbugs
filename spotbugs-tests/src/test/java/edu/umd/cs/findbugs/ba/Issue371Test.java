f61fce4bcd8b032e4a3b3f6183e705ee8a42e9b2
Kengo TODA <skypencil@gmail.com>
2017-09-16 21:30:50 +0800

    refs #371: automate bug check by JUnit

---
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/ba/Issue371Test.java
@@ -0,0 +1,22 @@
+package edu.umd.cs.findbugs.ba;
+
+import static edu.umd.cs.findbugs.test.CountMatcher.containsExactly;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import edu.umd.cs.findbugs.AbstractIntegrationTest;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcher;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
+
+public class Issue371Test extends AbstractIntegrationTest {
+
+    @Test
+    public void test() {
+        performAnalysis("lambdas/Issue371.class");
+        final BugInstanceMatcher bugTypeMatcher = new BugInstanceMatcherBuilder()
+                .bugType("NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE").build();
+        assertThat(getBugCollection(), containsExactly(1, bugTypeMatcher));
+    }
+
+}
