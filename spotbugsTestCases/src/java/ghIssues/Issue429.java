36a4d71b73c1a61fca822537e3d1350a1626d1da
Kengo TODA <skypencil@gmail.com>
2017-10-03 22:57:30 +0800

    replace tab with whitespaces

--- spotbugsTestCases/src/java/ghIssues/Issue429.java
+++ spotbugsTestCases/src/java/ghIssues/Issue429.java
@@ -6,7 +6,7 @@ import static com.google.common.truth.Truth.assertThat;
  * An implementation to reproduce bug, shared by <a href="https://github.com/spotbugs/spotbugs/issues/429">#429</a>.
  */
 public class Issue429 {
-	public void method() {
-		assertThat(System.currentTimeMillis() > 0);
-	}
+    public void method() {
+        assertThat(System.currentTimeMillis() > 0);
+    }
 }
