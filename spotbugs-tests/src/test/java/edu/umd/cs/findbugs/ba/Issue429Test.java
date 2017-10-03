016d583719ae0a52d48772e6d063322e54a8027d
Kengo TODA <skypencil@gmail.com>
2017-10-03 22:55:12 +0800

    refs #429: reproduce reported bug

---
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/ba/Issue429Test.java
@@ -0,0 +1,34 @@
+package edu.umd.cs.findbugs.ba;
+
+import static edu.umd.cs.findbugs.test.CountMatcher.containsExactly;
+import static org.junit.Assert.assertThat;
+
+import java.nio.file.Paths;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import edu.umd.cs.findbugs.BugCollection;
+import edu.umd.cs.findbugs.test.SpotBugsRule;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcher;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
+
+/**
+ * Unit test to reproduce <a href="https://github.com/spotbugs/spotbugs/issues/429">#429</a>.
+ */
+public class Issue429Test {
+    @Rule
+    public SpotBugsRule spotbugs = new SpotBugsRule();
+
+    @Test
+    public void test() {
+        for (String path : System.getProperty("AUX_CLASSPATH").split(",")) {
+            spotbugs.addAuxClasspathEntry(Paths.get(path));
+        }
+        BugCollection bugCollection = spotbugs.performAnalysis(
+                Paths.get("../spotbugsTestCases/build/classes/java/main/ghIssues/Issue429.class"));
+        BugInstanceMatcher matcher = new BugInstanceMatcherBuilder().bugType("RV_RETURN_VALUE_IGNORED")
+                .build();
+        assertThat(bugCollection, containsExactly(1, matcher));
+    }
+}
