b2210d6366d4636c85b3071195fcf95639e1008b
Kengo TODA <skypencil@gmail.com>
2017-10-03 22:55:12 +0800

    refs #429: reproduce reported bug

---
+++ spotbugsTestCases/src/java/ghIssues/Issue429.java
@@ -0,0 +1,12 @@
+package ghIssues;
+
+import static com.google.common.truth.Truth.assertThat;
+
+/**
+ * An implementation to reproduce bug, shared by <a href="https://github.com/spotbugs/spotbugs/issues/429">#429</a>.
+ */
+public class Issue429 {
+	public void method() {
+		assertThat(System.currentTimeMillis() > 0);
+	}
+}
