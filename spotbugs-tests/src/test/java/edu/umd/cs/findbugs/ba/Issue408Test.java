e39f22d3cb4206c2657aa69c268f707d1e21bfe6
Kengo TODA <skypencil@gmail.com>
2017-10-24 14:30:07 +0800

    refs #474: fix broken unit test

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/ba/Issue408Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/ba/Issue408Test.java
@@ -1,26 +1,32 @@
 package edu.umd.cs.findbugs.ba;
 
-import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assume.assumeThat;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import edu.umd.cs.findbugs.AbstractIntegrationTest;
+import edu.umd.cs.findbugs.NoClassesFoundToAnalyzeException;
 
 /**
  * @see <a href="https://github.com/spotbugs/spotbugs/issues/408">#408</a>
  * @since 3.1
  */
 public class Issue408Test extends AbstractIntegrationTest {
+    @Rule
+    public ExpectedException expected = ExpectedException.none();
 
     @Test
     public void test() {
         assumeThat(System.getProperty("java.specification.version"), is("9"));
 
+        expected.expect(AssertionError.class);
+        expected.expectMessage("Analysis failed with exception");
+        expected.expectCause(is(instanceOf(NoClassesFoundToAnalyzeException.class)));
         performAnalysis("../java9/module-info.class");
-        assertThat(getBugCollection().getCollection(), is(empty()));
     }
 
 }
