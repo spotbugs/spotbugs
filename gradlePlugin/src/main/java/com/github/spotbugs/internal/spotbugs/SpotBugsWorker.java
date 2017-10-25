3fedfbeaa1d567fab9313617e18343b6d7ed8ed6
Kengo TODA <skypencil@gmail.com>
2017-10-25 11:43:54 +0800

    delete all gradle plugin related files

--- gradlePlugin/src/main/java/com/github/spotbugs/internal/spotbugs/SpotBugsWorker.java
+++
@@ -1,7 +0,0 @@
-package com.github.spotbugs.internal.spotbugs;
-
-import java.io.IOException;
-
-public interface SpotBugsWorker {
-    SpotBugsResult runSpotbugs(SpotBugsSpec spec) throws IOException, InterruptedException;
-}
