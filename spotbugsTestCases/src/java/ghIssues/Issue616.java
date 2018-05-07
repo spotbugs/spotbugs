1b88a56af7ab53bc9b7e3e4b670d4d84f5b8f61e
Kengo TODA <skypencil@gmail.com>
2018-05-07 17:27:18 +0800

    refs #616: try to reproduce reported issue

--- spotbugsTestCases/src/java/ghIssues/Issue616.java
+++ spotbugsTestCases/src/java/ghIssues/Issue616.java
@@ -2,17 +2,20 @@ package ghIssues;
 
 import com.google.common.base.Preconditions;
 
+import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.Nullable;
 
 public enum Issue616 {
     TEST("one", "two", null);
 
+    @NonNull
     public final String one;
+    @NonNull
     public final String two;
     @Nullable
     public final String three;
 
-    private Issue616(String one, String two, @Nullable Object three) {
+    private Issue616(@NonNull String one, @NonNull String two, @Nullable Object three) {
         this.one = Preconditions.checkNotNull(one);
         this.two = Preconditions.checkNotNull(two);
         this.three = three == null ? null : three.toString();
