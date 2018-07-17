c58cef32154490439e135b34232292b4d7616967
Kengo TODA <skypencil@gmail.com>
2018-07-17 10:20:09 +0800

    release v3.1.6

--- docs/conf.py
+++ docs/conf.py
@@ -17,8 +17,8 @@ import os
 
 html_context = {
   'version' : '3.1',
-  'full_version' : '3.1.5',
-  'maven_plugin_version' : '3.1.3.1',
+  'full_version' : '3.1.6',
+  'maven_plugin_version' : '3.1.5',
   'gradle_plugin_version' : '1.6.2'
 }
 
