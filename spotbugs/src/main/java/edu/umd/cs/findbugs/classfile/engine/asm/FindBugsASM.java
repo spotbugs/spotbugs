4dcf40a57190cd15e6d039786274767b26f4a457
muminc <muminc@gmail.com>
2018-08-02 21:57:28 +0100

    ASM7_EXPERIMENTAL is default value for ASM_VERSION, if system property spotbugs.experimental=false ASM_VERSION will be ASM6

--- spotbugs/src/main/java/edu/umd/cs/findbugs/classfile/engine/asm/FindBugsASM.java
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/classfile/engine/asm/FindBugsASM.java
@@ -19,6 +19,9 @@
 
 package edu.umd.cs.findbugs.classfile.engine.asm;
 
+import static java.lang.Boolean.parseBoolean;
+import static java.util.Objects.isNull;
+
 import org.objectweb.asm.Opcodes;
 
 /**
@@ -26,7 +29,9 @@ import org.objectweb.asm.Opcodes;
  */
 public class FindBugsASM {
 
-    private static final boolean USE_EXPERIMENTAL = Boolean.parseBoolean(System.getProperty("spotbugs.experimental"));
+    private static final String EXPERIMENTAL_PROPERTY_VALUE = System.getProperty("spotbugs.experimental");
+    private static final boolean USE_EXPERIMENTAL = isNull(EXPERIMENTAL_PROPERTY_VALUE) || parseBoolean(EXPERIMENTAL_PROPERTY_VALUE);
+
     public static final int ASM_VERSION = USE_EXPERIMENTAL ? Opcodes.ASM7_EXPERIMENTAL : Opcodes.ASM6;
 
 }
