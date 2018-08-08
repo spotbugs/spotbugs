3127e80c6362d0c2637c54b29c6facb5046788d0
Kengo TODA <skypencil@gmail.com>
2018-08-08 16:40:07 +0800

    Sphinx should ignore virtualenv directory
    https://github.com/sphinx-doc/sphinx/issues/2467#issuecomment-211697586

--- docs/conf.py
+++ docs/conf.py
@@ -76,7 +76,7 @@ language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['env']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
