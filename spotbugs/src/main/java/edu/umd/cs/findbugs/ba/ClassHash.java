d88acec4f4a1a8260c149a067302ba03906a1463
wreulicke <saito.masaya@classmethod.jp>
2019-05-09 10:39:49 +0900

    Remove unused imports

--- spotbugs/src/main/java/edu/umd/cs/findbugs/ba/ClassHash.java
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/ba/ClassHash.java
@@ -23,7 +23,6 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.CharacterCodingException;
-import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
--- spotbugs/src/main/java/edu/umd/cs/findbugs/xml/OutputStreamXMLOutput.java
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/xml/OutputStreamXMLOutput.java
@@ -23,7 +23,6 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 import javax.annotation.WillCloseWhenClosed;
