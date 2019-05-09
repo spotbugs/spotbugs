f2ab79a9f7b895b52052b568cf27110f9ccd114f
wreulicke <saito.masaya@classmethod.jp>
2019-05-09 10:45:50 +0900

    Use StandardCharsets

--- eclipsePlugin/src/de/tobject/findbugs/util/ConfigurableXmlOutputStream.java
+++ eclipsePlugin/src/de/tobject/findbugs/util/ConfigurableXmlOutputStream.java
@@ -22,7 +22,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 
 import org.eclipse.core.runtime.Assert;
@@ -61,7 +61,7 @@ public class ConfigurableXmlOutputStream implements XMLOutput {
      */
     public ConfigurableXmlOutputStream(OutputStream os, boolean plainText) {
         this.plainText = plainText;
-        this.out = new OutputStreamWriter(os, Charset.forName("UTF-8"));
+        this.out = new OutputStreamWriter(os, StandardCharsets.UTF_8);
         this.nestingLevel = 0;
         this.newLine = true;
     }
