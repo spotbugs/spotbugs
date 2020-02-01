1aa83960993137462e729b88329872430aa8d6c1
Kengo TODA <skypencil@gmail.com>
2020-02-01 13:52:26 +0800

    Upload distributions onto GitHub Releases too (#1076)
    * ci: upload distributions onto GitHub Releases too
    
    close #1075
    
    * docs: tag uses lower case but version uses upper case

--- docs/conf.py
+++ docs/conf.py
@@ -283,4 +283,5 @@ extensions += ['code-template']
 # URL to distribute SpotBugs package
 # http://www.sphinx-doc.org/en/stable/ext/extlinks.html
 extensions += ['sphinx.ext.extlinks']
-extlinks = {'dist': ('https://repo.maven.apache.org/maven2/com/github/spotbugs/spotbugs/' + release + '/spotbugs-' + release + '.%s', '')}
+tag = release.replace('-', '_').lower()
+extlinks = {'dist': ('https://github.com/spotbugs/spotbugs/releases/download/' + tag + '/spotbugs-' + release + '.%s', '')}
