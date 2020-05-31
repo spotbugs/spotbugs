7594a5dc3dd1e8536e57f04e94262d95f15394fe
Zimuyeyi <11710436@mai.sustech.edu.cn>
2020-05-31 12:34:13 +0800

    update

--- spotbugsTestCases/src/java/ghIssues/issue374/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * An example to reproduce problem at
- * <a href="https://github.com/spotbugs/spotbugs/issues/374">GitHub issue</a>
- */
-@javax.annotation.ParametersAreNonnullByDefault
-package ghIssues.issue374;
-
