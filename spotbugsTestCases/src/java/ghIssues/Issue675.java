ccdac1c22eecd5f466f52cb7d3d9de600426d202
Zimuyeyi <11710436@mai.sustech.edu.cn>
2020-05-31 12:29:06 +0800

    Delete Issue675.java

--- spotbugsTestCases/src/java/ghIssues/Issue675.java
+++
@@ -1,8 +0,0 @@
-package ghIssues;
-
-/**
- * @see <a href="https://github.com/spotbugs/spotbugs/issues/675">GitHub issue</a>
- */
-public final class Issue675 extends AbstractMethodError {
-    private String unused = "unused";
-}
