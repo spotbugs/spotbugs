5d0c79146a886eccaf95e1cb0bb50129aa4882c0
Kengo TODA <skypencil@gmail.com>
2020-06-28 18:26:35 +0800

    feat: generate runs.tool.driver in SARIF report

--- spotbugs/src/main/java/edu/umd/cs/findbugs/sarif/SarifBugReporter.java
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/sarif/SarifBugReporter.java
@@ -2,10 +2,13 @@ package edu.umd.cs.findbugs.sarif;
 
 import edu.umd.cs.findbugs.BugCollectionBugReporter;
 import edu.umd.cs.findbugs.Project;
+import edu.umd.cs.findbugs.Version;
+import edu.umd.cs.findbugs.annotations.NonNull;
 import org.json.JSONWriter;
 
-import javax.annotation.CheckForNull;
 import java.io.PrintWriter;
+import java.util.Locale;
+import java.util.function.Consumer;
 
 public class SarifBugReporter extends BugCollectionBugReporter {
     public SarifBugReporter(Project project) {
@@ -16,7 +19,28 @@ public class SarifBugReporter extends BugCollectionBugReporter {
     public void finish() {
         try (PrintWriter writer = outputStream) {
             JSONWriter jsonWriter = new JSONWriter(writer);
-            jsonWriter.object().key("version").value("2.1.0").key("$schema").value("https://schemastore.azurewebsites.net/schemas/json/sarif-2.1.0-rtm.4.json").endObject();
+            jsonWriter.object();
+            jsonWriter.key("version").value("2.1.0").key("$schema").value("https://schemastore.azurewebsites.net/schemas/json/sarif-2.1.0-rtm.4.json");
+            processRuns(jsonWriter);
+            jsonWriter.endObject();
         }
+    }
+
+    private void processRuns(@NonNull JSONWriter jsonWriter) {
+        jsonWriter.key("runs").array().object();
+        processTool(jsonWriter);
+        jsonWriter.endObject().endArray();
+    }
+
+    private void processTool(@NonNull JSONWriter jsonWriter) {
+        jsonWriter.key("tool").object().key("driver").object();
+        jsonWriter.key("name").value(Version.getApplicationName());
+        // Eclipse plugin does not follow the semantic-versioning, so use "version" instead of "semanticVersion".
+        jsonWriter.key("version").value(Version.getApplicationVersion());
+        // SpotBugs refers JVM config to decide which language we use.
+        jsonWriter.key("language").value(Locale.getDefault().getLanguage());
+        // TODO process "rules"
+        // TODO process "notifications"
+        jsonWriter.endObject().endObject();
     }
 }
--- spotbugs/src/test/java/edu/umd/cs/findbugs/sarif/SarifBugReporterTest.java
+++ spotbugs/src/test/java/edu/umd/cs/findbugs/sarif/SarifBugReporterTest.java
@@ -1,6 +1,7 @@
 package edu.umd.cs.findbugs.sarif;
 
 import edu.umd.cs.findbugs.Project;
+import edu.umd.cs.findbugs.Version;
 import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
@@ -10,6 +11,7 @@ import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Iterator;
+import java.util.Locale;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.startsWith;
@@ -33,6 +35,10 @@ public class SarifBugReporterTest {
         project.close();
     }
 
+    /**
+     * Root object&apos;s first field should be {@code "version"}, and it should be {@code "2.1.0"}.
+     * Root object also should have {@code "$schema"} field that points the JSON schema provided by SARIF community.
+     */
     @Test
     public void testVersionAndSchema() {
         reporter.finish();
@@ -43,5 +49,36 @@ public class SarifBugReporterTest {
         assertThat("the first key in JSON should be 'version'", json, startsWith("{\"version\""));
         assertThat(jsonObject.get("version"), is("2.1.0"));
         assertThat(jsonObject.get("$schema"), is("https://schemastore.azurewebsites.net/schemas/json/sarif-2.1.0-rtm.4.json"));
+    }
+
+    /**
+     * {@code toolComponent} object in {@code "runs.tool.driver"} SHOULD have {@code "version"} (§3.19.2).
+     * A toolComponent object SHALL contain a {@code "name"} property (§3.19.8).
+     * A toolComponent object MAY contain a {@code "language"} property (§3.19.21).
+     */
+    @Test
+    public void testDriver() {
+        final String EXPECTED_NAME = "SpotBugs JUnit Test";
+        final String EXPECTED_VERSION = "1.2.3";
+        final String EXPECTED_LANGUAGE = "ja";
+        Version.registerApplication(EXPECTED_NAME, EXPECTED_VERSION);
+
+        Locale defaultLocale = Locale.getDefault();
+        try {
+            Locale.setDefault(Locale.JAPANESE);
+            reporter.finish();
+        } finally {
+            Locale.setDefault(defaultLocale);
+        }
+
+        String json = writer.toString();
+        JSONObject jsonObject = new JSONObject(json);
+        JSONObject run = jsonObject.getJSONArray("runs").getJSONObject(0);
+        JSONObject tool = run.getJSONObject("tool");
+        JSONObject driver = tool.getJSONObject("driver");
+
+        assertThat(driver.get("name"), is(EXPECTED_NAME));
+        assertThat(driver.get("version"), is(EXPECTED_VERSION));
+        assertThat(driver.get("language"), is(EXPECTED_LANGUAGE));
     }
 }
