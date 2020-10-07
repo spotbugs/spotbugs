7e5964582545d04c548bf097fd08abd46a06021d
Kengo TODA <skypencil@gmail.com>
2020-10-07 20:45:17 +0800

    test: reproduce the reported problem as #1272

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/ba/Issue390Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/ba/Issue390Test.java
@@ -7,7 +7,12 @@ import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
 import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.StringWriter;
+
 import static edu.umd.cs.findbugs.test.CountMatcher.containsExactly;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Issue390Test extends AbstractIntegrationTest {
 
@@ -18,4 +23,15 @@ public class Issue390Test extends AbstractIntegrationTest {
         MatcherAssert.assertThat(getBugCollection(), containsExactly(1, bugTypeMatcher));
     }
 
+    /**
+     * @see <a href="https://github.com/spotbugs/spotbugs/issues/1272">The reported GitHub issue</a>
+     */
+    @Test
+    public void testXmlGeneratability() throws IOException {
+        performAnalysis("ghIssues/Issue390.class");
+        StringWriter writer = new StringWriter();
+        getBugCollection().setWithMessages(true);
+        getBugCollection().writeXML(writer);
+        assertThat(writer.toString(), containsString("JUA_DONT_ASSERT_INSTANCEOF_IN_TESTS"));
+    }
 }
