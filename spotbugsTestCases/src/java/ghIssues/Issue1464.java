43c00ff53bc144dfe768474e3ce80c13a739c944
Mary904 <78488965+Mary904@users.noreply.github.com>
2021-03-25 16:44:45 +0800

    Update and rename SecureRandomObjectUsedOnlyOnce.java to Issue1464.java

--- spotbugsTestCases/src/java/ghIssues/Issue1464.java
+++ spotbugsTestCases/src/java/ghIssues/Issue1464.java
@@ -3,7 +3,7 @@ package ghIssues;
 import java.security.SecureRandom;
 import java.util.Random;
 
-public class SecureRandomObjectUsedOnlyOnce{
+public class Issue1464{
     // @ExpectWarning("DMI_RANDOM_USED_ONLY_ONCE")
     long m1(){
         return new SecureRandom().nextLong();
