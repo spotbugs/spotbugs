378f0da150cdc1999d450631d68b041e2d1ab8cd
Mary904 <78488965+Mary904@users.noreply.github.com>
2021-03-25 16:38:28 +0800

    Create SecureRandomObjectUsedOnlyOnce.java

---
+++ spotbugsTestCases/src/java/SecureRandomObjectUsedOnlyOnce.java
@@ -0,0 +1,14 @@
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class SecureRandomObjectUsedOnlyOnce{
+    // @ExpectWarning("DMI_RANDOM_USED_ONLY_ONCE")
+    long m1(){
+        return new SecureRandom().nextLong();
+    }
+    
+    // @ExpectWarning("DMI_RANDOM_USED_ONLY_ONCE")
+    long m2(){
+        return new Random().nextLong();
+    }
+}
