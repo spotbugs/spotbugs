696b63910b524d707f69ee8c7db5d99fac7189a2
Ádám Balogh <adam.balogh@ericsson.com>
2021-04-23 17:22:54 +0200

    Refactor and improve `FindReturnRef` to find indirect cases
    Currently, the detector `FindReturnRef` which is responisble for bugs
    `EI_EXPOSE_REP`, `EI_EXPOSE_REP2`, `MS_EXPOSE_REP` and
    `EI_EXPOSE_STATIC_REP2` tracks the data on the top of the stack.
    However, this is not necessary in the current version of SpotBugs
    because the infrastructure already does this job more accurately. This
    patch removes the redundant tracking the the top of the stack and
    relies on the already existing infrastructure instead. This way the
    detector is able to find more true positives where returning of the
    private mutable attribute happens indirectly, e.g. by first assigning it
    to a local variable and then returning that local.

--- spotbugs/src/main/java/edu/umd/cs/findbugs/detect/FindReturnRef.java
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/detect/FindReturnRef.java
@@ -31,29 +31,19 @@ import edu.umd.cs.findbugs.LocalVariableAnnotation;
 import edu.umd.cs.findbugs.OpcodeStack;
 import edu.umd.cs.findbugs.ba.AnalysisContext;
 import edu.umd.cs.findbugs.ba.XField;
+import edu.umd.cs.findbugs.classfile.CheckedAnalysisException;
+import edu.umd.cs.findbugs.classfile.ClassDescriptor;
 import edu.umd.cs.findbugs.bcel.OpcodeStackDetector;
 import edu.umd.cs.findbugs.util.MutableClasses;
 
 public class FindReturnRef extends OpcodeStackDetector {
     boolean check = false;
 
-    boolean thisOnTOS = false;
-
-    boolean fieldOnTOS = false;
-
     boolean publicClass = false;
 
     boolean staticMethod = false;
 
-    String nameOnStack;
-
-    String classNameOnStack;
-
-    String sigOnStack;
-
     int parameterCount;
-
-    boolean fieldIsStatic;
 
     private final BugAccumulator bugAccumulator;
 
@@ -88,11 +78,7 @@ public class FindReturnRef extends OpcodeStackDetector {
             parameterCount++;
         }
 
-        thisOnTOS = false;
-        fieldOnTOS = false;
         super.visit(obj);
-        thisOnTOS = false;
-        fieldOnTOS = false;
     }
 
     @Override
@@ -135,46 +121,21 @@ public class FindReturnRef extends OpcodeStackDetector {
             }
         }
 
-        if (seen == Const.ALOAD_0 && !staticMethod) {
-            thisOnTOS = true;
-            fieldOnTOS = false;
-            return;
-        }
-
-        if (thisOnTOS && seen == Const.GETFIELD && getClassConstantOperand().equals(getClassName()) && nonPublicFieldOperand()
-                && !AnalysisContext.currentXFactory().isEmptyArrayField(getXFieldOperand())) {
-            fieldOnTOS = true;
-            thisOnTOS = false;
-            nameOnStack = getNameConstantOperand();
-            classNameOnStack = getDottedClassConstantOperand();
-            sigOnStack = getSigConstantOperand();
-            fieldIsStatic = false;
-            return;
-        }
-        if (seen == Const.GETSTATIC && getClassConstantOperand().equals(getClassName()) && nonPublicFieldOperand()
-                && !AnalysisContext.currentXFactory().isEmptyArrayField(getXFieldOperand())) {
-            fieldOnTOS = true;
-            thisOnTOS = false;
-            nameOnStack = getNameConstantOperand();
-            classNameOnStack = getDottedClassConstantOperand();
-            sigOnStack = getSigConstantOperand();
-            fieldIsStatic = true;
-
-            return;
-        }
-        thisOnTOS = false;
-        if (check && fieldOnTOS && seen == Const.ARETURN
-        /*
-         * && !sigOnStack.equals("Ljava/lang/String;") &&
-         * sigOnStack.indexOf("Exception") == -1 && sigOnStack.indexOf("[") >= 0
-         */
-                && nameOnStack.indexOf("EMPTY") == -1 && MutableClasses.mutableSignature(sigOnStack)) {
+        if (seen == Const.ARETURN) {
+            OpcodeStack.Item item = stack.getStackItem(0);
+            XField field = item.getXField();
+            if (field == null ||
+                    !isFieldOf(field, getClassDescriptor()) ||
+                    field.isPublic() ||
+                    AnalysisContext.currentXFactory().isEmptyArrayField(field) ||
+                    field.getName().indexOf("EMPTY") != -1 ||
+                    !MutableClasses.mutableSignature(field.getSignature())) {
+                return;
+            }
             bugAccumulator.accumulateBug(new BugInstance(this, staticMethod ? "MS_EXPOSE_REP" : "EI_EXPOSE_REP", NORMAL_PRIORITY)
-                    .addClassAndMethod(this).addField(classNameOnStack, nameOnStack, sigOnStack, fieldIsStatic), this);
-        }
+                    .addClassAndMethod(this).addField(field.getClassName(), field.getName(), field.getSignature(), field.isStatic()), this);
 
-        fieldOnTOS = false;
-        thisOnTOS = false;
+        }
     }
 
     private boolean nonPublicFieldOperand() {
@@ -192,5 +153,20 @@ public class FindReturnRef extends OpcodeStackDetector {
         // var-arg parameter
         return top.getRegisterNumber() != parameterCount - 1;
 
+    }
+
+    private boolean isFieldOf(XField field, ClassDescriptor klass) {
+        do {
+            if (field.getClassDescriptor().equals(klass)) {
+                return true;
+            }
+            try {
+                klass = klass.getXClass().getSuperclassDescriptor();
+            } catch (CheckedAnalysisException e) {
+                AnalysisContext.logError("Error checking for class " + klass, e);
+                return false;
+            }
+        } while (klass != null);
+        return false;
     }
 }
