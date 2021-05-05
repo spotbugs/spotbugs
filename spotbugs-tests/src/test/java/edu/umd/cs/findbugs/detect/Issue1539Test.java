0c19065718e77ca580dff83f3b7feadaa461e785
Niklas Seyfarth <niklas@seyfarth.de>
2021-05-05 19:00:54 +0200

    Create test to catch DMI_RANDOM_USED_ONLY_ONCE false positive.

---
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue1539Test.java
@@ -0,0 +1,20 @@
+package edu.umd.cs.findbugs.detect;
+
+import edu.umd.cs.findbugs.AbstractIntegrationTest;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcher;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
+import org.junit.Test;
+
+import static edu.umd.cs.findbugs.test.CountMatcher.containsExactly;
+import static org.junit.Assert.assertThat;
+
+public class Issue1539Test extends AbstractIntegrationTest {
+    @Test
+    public void test() {
+        performAnalysis("ghIssues/Issue1539.class");
+        BugInstanceMatcher bugTypeMatcher = new BugInstanceMatcherBuilder()
+                .bugType("DMI_RANDOM_USED_ONLY_ONCE")
+                .build();
+        assertThat(getBugCollection(), containsExactly(0, bugTypeMatcher));
+    }
+}
