ab883f26b70f33ff0b89e6296d65617b259bbe42
Niklas Seyfarth <niklas@seyfarth.de>
2021-05-07 10:24:32 +0200

    #1539: Test for static Random and seeded initialization in instance.

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue1539Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue1539Test.java
@@ -10,8 +10,17 @@ import static org.junit.Assert.assertThat;
 
 public class Issue1539Test extends AbstractIntegrationTest {
     @Test
-    public void test() {
-        performAnalysis("ghIssues/Issue1539.class");
+    public void testInstance() {
+        performAnalysis("ghIssues/Issue1539Instance.class");
+        BugInstanceMatcher bugTypeMatcher = new BugInstanceMatcherBuilder()
+                .bugType("DMI_RANDOM_USED_ONLY_ONCE")
+                .build();
+        assertThat(getBugCollection(), containsExactly(0, bugTypeMatcher));
+    }
+
+    @Test
+    public void testStatic() {
+        performAnalysis("ghIssues/Issue1539Static.class");
         BugInstanceMatcher bugTypeMatcher = new BugInstanceMatcherBuilder()
                 .bugType("DMI_RANDOM_USED_ONLY_ONCE")
                 .build();
