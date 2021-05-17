d7c7ab54dc4adc8e61bf46cda07bbc3aa0a90efb
Niklas Seyfarth <niklas@seyfarth.de>
2021-05-17 11:30:51 +0200

    #1539: Make the TreadLocalRandom test actually test something different from the method argument test.

--- spotbugsTestCases/src/java/ghIssues/issue1539/Issue1539ThreadLocal.java
+++ spotbugsTestCases/src/java/ghIssues/issue1539/Issue1539ThreadLocal.java
@@ -4,16 +4,8 @@ import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Issue1539ThreadLocal {
-
-    private static ThreadLocalRandom random() {
-        return ThreadLocalRandom.current();
-    }
-
     public boolean shouldNotReport() {
-        return useRandom(random());
-    }
-
-    public boolean useRandom(final Random random) {
+        Random random = ThreadLocalRandom.current();
         return random.nextBoolean();
     }
 }
