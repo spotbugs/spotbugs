b5ca872069fb1e89b7725cfd7d36db88b9c3f5e7
Victor Williams Stafusa da Silva <victorwssilva@gmail.com>
2021-09-29 10:59:58 -0300

    Thread sadly is not immutable. If Class is considered immutable, so is a ClassLoader. Functions can be considered safe. Annotations are immutable.

--- spotbugs/src/main/java/edu/umd/cs/findbugs/util/MutableClasses.java
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/util/MutableClasses.java
@@ -15,7 +15,7 @@ public class MutableClasses {
     private static final Set<String> KNOWN_IMMUTABLE_CLASSES = new HashSet<>(Arrays.asList(
             "java.lang.String", "java.lang.Integer", "java.lang.Byte", "java.lang.Character",
             "java.lang.Short", "java.lang.Boolean", "java.lang.Long", "java.lang.Double",
-            "java.lang.Float", "java.lang.StackTraceElement", "java.lang.Class", "java.lang.Thread",
+            "java.lang.Float", "java.lang.StackTraceElement", "java.lang.Class", "java.lang.ClassLoader",
             "java.io.File", "java.awt.Font", "java.awt.BasicStroke",
             "java.awt.Color", "java.awt.GradientPaint", "java.awt.LinearGradientPaint",
             "java.awt.RadialGradientPaint", "java.awt.Cursor", "java.util.Locale", "java.util.UUID", "java.net.URL",
@@ -64,12 +64,12 @@ public class MutableClasses {
             "java.util.ImmutableCollections$AbstractImmutableSet"));
 
     private static final Set<String> KNOWN_IMMUTABLE_PACKAGES = new HashSet<>(Arrays.asList(
-            "java.math", "java.time",
+            "java.math", "java.time", "java.util.function",
             "java.lang.invoke", "java.lang.reflect", "java.lang.constant"));
 
     private static final List<String> SETTER_LIKE_PREFIXES = Arrays.asList(
             "set", "put", "add", "insert", "delete", "remove", "erase", "clear", "push", "pop",
-            "enqueue", "dequeue", "write", "append", "replace");
+            "enqueue", "dequeue", "write", "append", "replace", "prepend", "reset", "update");
 
     public static boolean mutableSignature(String sig) {
         if (sig.charAt(0) == '[') {
@@ -99,6 +99,10 @@ public class MutableClasses {
             JavaClass cls = Repository.lookupClass(dottedClassName);
             if (Stream.of(cls.getAnnotationEntries()).map(AnnotationEntry::getAnnotationType)
                     .anyMatch(type -> type.endsWith("/Immutable;") || type.equals("Ljdk/internal/ValueBased;"))) {
+                return false;
+            }
+            JavaClass ann = Repository.lookupClass("java.lang.annotation.Annotation");
+            if (cls.instanceOf(ann)) {
                 return false;
             }
             return ClassAnalysis.load(cls, sig).isMutable();
