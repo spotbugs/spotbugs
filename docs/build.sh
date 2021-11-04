32ef73b0f07e0770b7ab21e3d3a4c688c74e087e
luana-martins <lmartins.comp@gmail.com>
2021-11-04 17:46:24 -0300

    Update docs/build.sh, fix typos

--- docs/build.sh
+++ docs/build.sh
@@ -8,6 +8,7 @@ docker run -it -v $(pwd)/..:/docs spotbugs-sphinx make gettext
 
 # build .po files by new .pot files
 docker run -it -v $(pwd)/..:/docs spotbugs-sphinx sphinx-intl update -p .build/locale -l ja
+docker run -it -v $(pwd)/..:/docs spotbugs-sphinx sphinx-intl update -p .build/locale -l pt_BR
 
 # build html files
 docker run -it -v $(pwd)/..:/docs spotbugs-sphinx make html
