8249a98a2b12259e12b0d75b138cb4221bbe1fb1
Vodorok <gabor.kutas@ericsson.com>
2021-11-05 03:13:10 +0100

    Replace BugReporter with BugAccumulator

--- spotbugs/src/main/java/edu/umd/cs/findbugs/detect/ConstructorThrow.java
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/detect/ConstructorThrow.java
@@ -6,11 +6,13 @@ import java.util.Set;
 import org.apache.bcel.Const;
 
 import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugAccumulator;
 import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.bcel.OpcodeStackDetector;
 import org.apache.bcel.classfile.Method;
 import org.apache.bcel.classfile.ExceptionTable;
 import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.classfile.Code;
 
 /**
  * This detector can find constructors that throw exception.
@@ -18,16 +20,16 @@ import org.apache.bcel.classfile.JavaClass;
 public class ConstructorThrow extends OpcodeStackDetector {
 
     private final BugReporter bugReporter;
+    private final BugAccumulator bugAccumulator;
     private final Set<String> calledFromCtor = new HashSet<String>();
 
     private boolean isFinalClass = false;
     private boolean isFinalFinalizer = false;
-    private boolean alreadyReported = false;
-
     private boolean isFirstPass = true;
 
     public ConstructorThrow(BugReporter bugReporter) {
         this.bugReporter = bugReporter;
+        this.bugAccumulator = new BugAccumulator(bugReporter);
     }
 
     /**
@@ -59,16 +61,22 @@ public class ConstructorThrow extends OpcodeStackDetector {
 
     @Override
     public void visit(Method obj) {
-        if (isFinalClass || isFinalFinalizer || alreadyReported)
+        if (isFinalClass || isFinalFinalizer)
             return;
         if (isConstructor() || methodCalledFromCtor()) {
             // Check if there is a throws keyword for checked exceptions.
             ExceptionTable tbl = obj.getExceptionTable();
             boolean throwsExceptions = tbl != null && tbl.getNumberOfExceptions() > 0;
             if (throwsExceptions) {
-                reportCTBug();
+                accumulateBug();
             }
         }
+    }
+
+    @Override
+    public void visit(Code obj) {
+        super.visit(obj);
+        bugAccumulator.reportAccumulatedBugs();
     }
 
     /**
@@ -79,49 +87,47 @@ public class ConstructorThrow extends OpcodeStackDetector {
      */
     @Override
     public void sawOpcode(int seen) {
-        if (isFinalClass || isFinalFinalizer || alreadyReported)
+        if (isFinalClass || isFinalFinalizer)
             return;
         if (isFirstPass) {
             tryCollectMethod(seen);
         } else {
             if (isConstructor() || methodCalledFromCtor()) {
                 if (seen == Const.ATHROW)
-                    reportCTBug();
+                    accumulateBug();
             }
         }
     }
 
     /** Collects all the methods that are called from the constructor */
     private void tryCollectMethod(int seen) {
-        if (isMethodCall(seen)) {
-            String classConstantOperand = "";
-            try {
-                classConstantOperand = getClassConstantOperand();
-            } catch (Exception e) {
-                bugReporter.logError("Seen OPcode and failed to get ClassConstantOperand: " + seen, e);
-            }
-            String method = classConstantOperand + "." + getNameConstantOperand() + " : " + getSigConstantOperand();
-            // Not interested in object superctor
-            if (!"java/lang/Object.<init> : ()V".equals(method)) {
-                calledFromCtor.add(method);
-            }
+        if (!isMethodCall(seen))
+            return;
+        String classConstantOperand = "";
+        try {
+            classConstantOperand = getClassConstantOperand();
+        } catch (IllegalStateException e) {
+            bugReporter.logError("Seen OPcode and failed to get ClassConstantOperand: " + seen, e);
+        }
+        String method = classConstantOperand + "." + getNameConstantOperand() + " : " + getSigConstantOperand();
+        // Not interested in object superctor
+        if (!"java/lang/Object.<init> : ()V".equals(method)) {
+            calledFromCtor.add(method);
         }
     }
 
     private void resetState() {
         isFinalClass = false;
         isFinalFinalizer = false;
-        alreadyReported = false;
         isFirstPass = true;
         calledFromCtor.clear();
     }
 
-    private void reportCTBug() {
+    private void accumulateBug() {
         BugInstance bug = new BugInstance(this, "CT_CONSTRUCTOR_THROW", NORMAL_PRIORITY)
                 .addClassAndMethod(this)
                 .addSourceLine(this, getPC());
-        bugReporter.reportBug(bug);
-        alreadyReported = true;
+        bugAccumulator.accumulateBug(bug, this);
     }
 
     private boolean isMethodCall(int seen) {
