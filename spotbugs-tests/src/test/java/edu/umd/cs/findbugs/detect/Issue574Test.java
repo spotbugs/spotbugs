4008d1f98bfdc78051356d9822c7dde6a99cd1af
ramraval <rraval6@gmail.com>
2021-12-10 12:09:28 -0500

    Update Issue574Test.java

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue574Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue574Test.java
@@ -13,13 +13,13 @@ import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
  * SpotBugs should suppress "UNF_UNREAD_FIELD" warning when triggering fields have certain annotations.
  *
  * @see <a href="https://github.com/spotbugs/spotbugs/issues/574">GitHub issue</a>
- * @since 4.4.2
  */
 public class Issue574Test extends AbstractIntegrationTest {
     private static final String DESIRED_BUG_TYPE = "URF_UNREAD_FIELD";
 
     /**
      * Expects 0 "URF_UNREAD_FIELD" errors in the "Issue574SerializedName.class" file.
+     * Tests @SerializedName annotation.
      */
     @Test
     public void testSerializedNameAnnotation() {
@@ -31,11 +31,25 @@ public class Issue574Test extends AbstractIntegrationTest {
     }
 
     /**
-     * Expects 2 "URF_UNREAD_FIELD" errors in the "Issue574XmlElement.class" file.
+     * Expects 0 "URF_UNREAD_FIELD" errors in the "Issue574XmlElement.class" file.
+     * Tests @XmlElement, @XmlAttribute, and @XmlValue annotations.
      */
     @Test
     public void testXmlElementAnnotation() {
         performAnalysis("ghIssues/Issue574XmlElement.class");
+        BugInstanceMatcher bugTypeMatcher = new BugInstanceMatcherBuilder()
+                .bugType(DESIRED_BUG_TYPE)
+                .build();
+        assertThat(getBugCollection(), containsExactly(0, bugTypeMatcher));
+    }
+
+    /**
+     * Expects 0 "URF_UNREAD_FIELD" errors in the "Issue574RegisterExtension.class" file.
+     * Tests @RegisterExtension annotation.
+     */
+    @Test
+    public void testRegisterExtensionAnnotation() {
+        performAnalysis("ghIssues/Issue574RegisterExtension.class");
         BugInstanceMatcher bugTypeMatcher = new BugInstanceMatcherBuilder()
                 .bugType(DESIRED_BUG_TYPE)
                 .build();
