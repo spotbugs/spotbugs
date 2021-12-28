23dba86dbdd638286a0d5ac835fc73a7dcd2665c
Kengo TODA <skypencil@gmail.com>
2021-12-28 20:08:18 +0800

    build: exclude the old spotless-lib conflicting with the spotless gradle plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,5 +6,7 @@ repositories {
     gradlePluginPortal()
 }
 dependencies {
-    implementation("com.diffplug.gradle:goomph:3.34.0")
+    implementation("com.diffplug.gradle:goomph:3.34.0") {
+        exclude("com.diffplug.spotless", "spotless-lib")
+    }
 }
