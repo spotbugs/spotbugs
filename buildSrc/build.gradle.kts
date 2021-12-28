e7bf63c87c414a0830fe087149b9115eaae7fe0e
Kengo TODA <skypencil+github@gmail.com>
2021-12-28 12:35:56 +0800

    build: remove spotless from buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     `kotlin-dsl`
-    id("com.diffplug.spotless") version "6.1.0"
 }
 
 repositories {
@@ -8,10 +7,4 @@ repositories {
 }
 dependencies {
     implementation("com.diffplug.gradle:goomph:3.34.0")
-}
-
-spotless {
-    kotlinGradle {
-        ktlint()
-    }
 }
