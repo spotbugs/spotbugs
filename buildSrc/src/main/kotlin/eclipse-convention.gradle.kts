a32e59ac1ce76c869e9ba2bb366b8a9177dcad26
Kengo TODA <skypencil@gmail.com>
2021-12-28 20:00:03 +0800

    build: try to run UpdateSitePublisher
    it fails by an error "java.lang.RuntimeException: Could not find framework"
    probably we need to download the IDE by `gradlew ide` and use
    launcher.jar in its plugins dir

--- buildSrc/src/main/kotlin/eclipse-convention.gradle.kts
+++ buildSrc/src/main/kotlin/eclipse-convention.gradle.kts
@@ -6,11 +6,15 @@ plugins {
 val pdeTool by configurations.creating {
   setTransitive(false)
 }
+val updateSitePublisher by configurations.creating {
+  setTransitive(false)
+}
 
 eclipseMavenCentral {
   // TODO: it should include what is required in the MANIFEST.MF, and nothing else
   release("4.6.3") {
     dep("pdeTool", "org.eclipse.pde.build")
+    dep("updateSitePublisher", "org.eclipse.equinox.launcher")
   }
 }
 
@@ -20,6 +24,29 @@ eclipseMavenCentral {
 val unzipPdeTool = tasks.register<Copy>("unzipPdeTool") {
   from(zipTree(pdeTool.singleFile))
   into("$buildDir/pdeTool")
+}
+
+fun createGenerateP2MetadataTask(classifier: String): TaskProvider<JavaExec> {
+  val dirName: String = if (classifier.isEmpty()) "eclipse" else "eclipse-$classifier"
+  val dir = file("$buildDir/site/$dirName")
+
+  return tasks.register<JavaExec>("generate${ classifier.capitalize() }P2Metadata") {
+    doFirst {
+      project.delete(dir.resolve("artifacts.xml"))
+      project.delete(dir.resolve("content.xml"))
+      files(updateSitePublisher.singleFile).forEach { println(it) }
+    }
+    classpath = files(updateSitePublisher)
+    args = listOf(
+      "-application", "org.eclipse.equinox.p2.publisher.UpdateSitePublisher",
+      "-metadataRepository", dir.toURI().toString(),
+      "-artifactRepository", dir.toURI().toString(),
+      "-source", dir.absolutePath)
+  }
+}
+
+listOf("").forEach {
+  createGenerateP2MetadataTask(it)
 }
 
 dependencies {
