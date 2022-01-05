6da761c800c1db4bb4063c4ef0e575706d062ce0
Kengo TODA <skypencil+github@gmail.com>
2022-01-05 14:55:17 +0800

    build: stop using local Eclipse as a part of dependency
    This change makes it possible to build Eclipse plugins without
    local Eclipse IDE. It is also possible to use metadata of artifacts
    (pom.xml) to maintain license, vulnerability, and version of
    dependencies.
    
    close #1868

---
+++ buildSrc/src/main/kotlin/eclipse-convention.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+  id ("com.diffplug.eclipse.mavencentral")
+}
+
+val pdeTool by configurations.creating {
+  setTransitive(false)
+}
+
+eclipseMavenCentral {
+  release("4.6.3") {
+    compileOnly("org.eclipse.ant.core")
+    compileOnly("org.eclipse.core.resources")
+    compileOnly("org.eclipse.core.runtime")
+    compileOnly("org.eclipse.jdt.core")
+    compileOnly("org.eclipse.jdt.ui")
+    compileOnly("org.eclipse.jface")
+    compileOnly("org.eclipse.pde")
+    compileOnly("org.eclipse.ui.workbench")
+    testImplementation("org.eclipse.core.runtime")
+
+    dep("pdeTool", "org.eclipse.pde.build")
+
+    // TODO these packages are not listed in the manifest
+    compileOnly("org.eclipse.pde.ui")
+    compileOnly("org.eclipse.swt")
+
+    // necessary to build with the org.eclipse.swt module
+    useNativesForRunningPlatform()
+
+    constrainTransitivesToThisRelease()
+  }
+}
+
+/**
+ * Unzip "org.eclipse.pde.build" package into the outputDir.
+ */
+val unzipPdeTool = tasks.register<Copy>("unzipPdeTool") {
+  from(zipTree(pdeTool.singleFile))
+  into("$buildDir/pdeTool")
+}
+
+dependencies {
+  compileOnly(files("$buildDir/pdeTool/pdebuild.jar"){
+    builtBy(unzipPdeTool)
+  })
+}
