c0bd4f77e6aa34215205b92a431f5208c4a15a31
gtoison <guillaume.toison@tobam.fr>
2022-10-15 20:57:49 +0100

    fix: Disable access to external entities in XML parsing
    Fixes security issues detected by sonarcloud

--- spotbugs/src/main/java/edu/umd/cs/findbugs/xml/XMLUtil.java
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/xml/XMLUtil.java
@@ -21,7 +21,13 @@ package edu.umd.cs.findbugs.xml;
 
 import java.util.List;
 
+import javax.xml.XMLConstants;
+import javax.xml.transform.TransformerFactory;
+
 import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+
+import edu.umd.cs.findbugs.FindBugs;
 
 /**
  * @author pugh
@@ -33,4 +39,28 @@ public class XMLUtil {
         return (List<T>) node.selectNodes(arg0);
     }
 
+    public static SAXReader buildSAXReader() {
+        SAXReader reader = new SAXReader();
+
+        try {
+            reader.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+
+        return reader;
+    }
+    
+    public static TransformerFactory buildTransformerFactory() {
+        TransformerFactory factory = TransformerFactory.newInstance("net.sf.saxon.TransformerFactoryImpl", null);
+
+        try {
+            factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+            factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+
+        return factory;
+    }
 }
