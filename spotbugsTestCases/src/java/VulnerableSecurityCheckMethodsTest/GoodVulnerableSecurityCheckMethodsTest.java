d7c8ddbf6c3bf1db9c7279d88ce06fde25155539
Nazir Muhammad Zafar Iqbal <bsvncs@inf.elte.hu>
2023-03-15 01:04:05 +0100

    Added the messages.xml and findbugs.xml components for my first checker. Added Checker as well. Added test cases calsses.

---
+++ spotbugsTestCases/src/java/VulnerableSecurityCheckMethodsTest/GoodVulnerableSecurityCheckMethodsTest.java
@@ -0,0 +1,74 @@
+package VulnerableSecurityCheckMethodsTest;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public final class GoodVulnerableSecurityCheckMethodsTest {
+    Logger logger = Logger.getAnonymousLogger();
+
+    public void goodVulnerableSecurityCheckMethodsTestCheck3() {
+        try {
+            SecurityManager sm = System.getSecurityManager();
+
+            //Temporary Variable for the sake of completeness if testing
+            /*
+            SecurityManager sm2 = System.getSecurityManager();
+
+
+            int i = 0;
+            double f = 1.0;
+            int[] ia = {1,2,3};
+
+            i = i+ia.length;
+            f = f+1.0;
+
+            if(sm2 != null) {
+                sm.checkRead("/temp/tempFile");
+                f = (double)i*2;
+            }
+            */
+
+            if (sm != null) {  // Check for permission to read file
+                sm.checkRead("/temp/tempFile");
+            }
+            // Access the file
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+        }
+    }
+
+    public void goodVulnerableSecurityCheckMethodsTestCheck() {
+        try {
+            SecurityManager sm = System.getSecurityManager();
+            if (sm != null) {  // Check for permission to read file
+                sm.checkRead("/temp/tempFile");
+            }
+            // Access the file
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+        }
+    }
+
+    private void goodVulnerableSecurityCheckMethodsTestCheck2() {
+        try {
+            SecurityManager sm = System.getSecurityManager();
+            if (sm != null) {  // Check for permission to read file
+                sm.checkRead("/temp/tempFile");
+            }
+            // Access the file
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+        }
+    }
+
+    public void goodVulnerableSecurityCheckMethodsTestCheck4() {
+        try {
+            SecurityManager sm = System.getSecurityManager();
+            //We have the SecurityManager object, but we do not perform any security check with it.
+
+            // Access the file
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+        }
+    }
+}
