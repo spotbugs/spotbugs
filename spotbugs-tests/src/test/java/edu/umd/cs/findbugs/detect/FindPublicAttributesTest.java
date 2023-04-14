96fde055506f08ddc974911181a5e6dd7889b2b1
Judit Knoll <judit.knoll@sigmatechnology.com>
2023-04-14 16:08:52 +0200

    refactoring tests to the new type

---
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/FindPublicAttributesTest.java
@@ -0,0 +1,56 @@
+package edu.umd.cs.findbugs.detect;
+
+import edu.umd.cs.findbugs.AbstractIntegrationTest;
+import edu.umd.cs.findbugs.BugCollection;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcher;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
+import org.junit.Test;
+
+import static edu.umd.cs.findbugs.test.CountMatcher.containsExactly;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+
+public class FindPublicAttributesTest extends AbstractIntegrationTest {
+    private static final String PRIMITIVE_PUBLIC = "PA_PUBLIC_PRIMITIVE_ATTRIBUTE";
+    private static final String MUTABLE_PUBLIC = "PA_PUBLIC_MUTABLE_OBJECT_ATTRIBUTE";
+    private static final String ARRAY_PUBLIC = "PA_PUBLIC_ARRAY_ATTRIBUTE";
+
+    @Test
+    public void testPublicAttributesChecks() {
+        performAnalysis("PublicAttributesTest.class");
+
+        assertNumOfBugs(PRIMITIVE_PUBLIC, 2);
+        assertNumOfBugs(ARRAY_PUBLIC, 2);
+        assertNumOfBugs(MUTABLE_PUBLIC, 1);
+
+        assertBugTypeAtField(PRIMITIVE_PUBLIC, "attr1");
+        assertBugTypeAtField(PRIMITIVE_PUBLIC, "sattr1");
+        assertBugTypeAtField(MUTABLE_PUBLIC, "hm");
+        assertBugTypeAtField(ARRAY_PUBLIC, "items");
+        assertBugTypeAtField(ARRAY_PUBLIC, "SITEMS");
+    }
+
+    @Test
+    public void testGoodPublicAttributesChecks() {
+        performAnalysis("PublicAttributesNegativeTest.class");
+        assertNumOfBugs(PRIMITIVE_PUBLIC, 0);
+        assertNumOfBugs(ARRAY_PUBLIC, 0);
+        assertNumOfBugs(MUTABLE_PUBLIC, 0);
+    }
+
+    private void assertNumOfBugs(String bugtype, int num) {
+        final BugInstanceMatcher bugTypeMatcher = new BugInstanceMatcherBuilder()
+                .bugType(bugtype).build();
+        assertThat(getBugCollection(), containsExactly(num, bugTypeMatcher));
+    }
+
+    private void assertBugTypeAtField(String bugtype, String field) {
+        final BugInstanceMatcher bugInstanceMatcher = new BugInstanceMatcherBuilder()
+                .bugType(bugtype)
+                .inClass("PublicAttributesTest")
+                .atField(field)
+                .build();
+        BugCollection bc = getBugCollection();
+        assertThat(bc, hasItem(bugInstanceMatcher));
+    }
+}
--- spotbugsTestCases/src/java/PublicAttributesTest.java
+++ spotbugsTestCases/src/java/PublicAttributesTest.java
@@ -2,41 +2,29 @@
 // public method which means that it has some kind of "behavior". Its data
 // fields should not be visible from the outside.
 
-import edu.umd.cs.findbugs.annotations.ExpectWarning;
-import edu.umd.cs.findbugs.annotations.NoWarning;
-
-import java.util.Map;
 import java.util.HashMap;
-import java.util.Set;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class PublicAttributesTest {
-    @ExpectWarning("PA")
     public int attr1 = 0;
 
-    @ExpectWarning("PA")
     public static int sattr1 = 0;
 
     // A final field. It cannot be written in methods.
-    @NoWarning("PA")
-    public final int Const1 = 10;
+    public final int const1 = 10;
 
-    @ExpectWarning("PA")
     public final Map<Integer, String> hm = new HashMap<Integer, String>();
 
-    @NoWarning("PA")
-    @ExpectWarning("MS")
-    public final static Map<Integer, String> shm = new HashMap<Integer, String>();
+    public final static Map<Integer, String> SHM = new HashMap<Integer, String>();
 
     // This field is not written, but only read.
-    @NoWarning("PA")
     public final Set<String> hs = new HashSet<String>();
 
-    @ExpectWarning("PA")
     public final String[] items = { "foo", "bar", "baz" };
 
-    @ExpectWarning("PA")
-    public static final String[] sitems = { "foo", "bar", "baz" };
+    public static final String[] SITEMS = { "foo", "bar", "baz" };
 
     public PublicAttributesTest(int a1) {
         attr1 = a1;
@@ -46,9 +34,9 @@ public class PublicAttributesTest {
         attr1 = 1;
         sattr1 = 1;
         hm.put(1, "test");
-        shm.put(1, "test");
+        SHM.put(1, "test");
         boolean b = hs.contains("x");
         items[0] = "test";
-        sitems[0] = "test";
+        SITEMS[0] = "test";
     }
 }
