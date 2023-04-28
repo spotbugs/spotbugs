01abc456730a265f7641b1f135e8aadd30a25050
Nazir Muhammad Zafar Iqbal <bsvncs@inf.elte.hu>
2023-04-28 18:28:46 +0200

    Fixed the extra boolean parameter in helping function for testing.

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/VulnerableSecurityCheckMethodsTest.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/VulnerableSecurityCheckMethodsTest.java
@@ -14,65 +14,54 @@ public class VulnerableSecurityCheckMethodsTest extends AbstractIntegrationTest 
     @Test
     public void testingBadCases() {
         performAnalysis("vulnerableSecurityCheckMethodsTest/FindVulnerableSecurityCheckMethodsTest.class");
-        assertError(1, true, "badFindVulnerableSecurityCheckMethodsCheck");
+        assertError(1, "badFindVulnerableSecurityCheckMethodsCheck");
     }
 
     @Test
     public void testingGoodCase1() {
         performAnalysis("vulnerableSecurityCheckMethodsTest/FindVulnerableSecurityCheckMethodsTest.class");
-        assertError(0, true, "goodVulnerableSecurityCheckMethodsTestCheck");
+        assertError(0, "goodVulnerableSecurityCheckMethodsTestCheck");
     }
 
     @Test
     public void testingGoodCase2() {
         performAnalysis("vulnerableSecurityCheckMethodsTest/FindVulnerableSecurityCheckMethodsTest.class");
-        assertError(0, true, "goodVulnerableSecurityCheckMethodsTestCheck2");
+        assertError(0, "goodVulnerableSecurityCheckMethodsTestCheck2");
     }
 
     @Test
     public void testingGoodCase3() {
         performAnalysis("vulnerableSecurityCheckMethodsTest/FindVulnerableSecurityCheckMethodsTest.class");
-        assertError(0, true, "goodVulnerableSecurityCheckMethodsTestCheck4");
-    }
-
-    @Test
-    public void testingAllCases2() {
-        performAnalysis("vulnerableSecurityCheckMethodsTest/GoodVulnerableSecurityCheckMethodsTest.class");
-        assertError(0, false, "");
+        assertError(0, "goodVulnerableSecurityCheckMethodsTestCheck4");
     }
 
     @Test
     public void testingGoodCase5() {
         performAnalysis("vulnerableSecurityCheckMethodsTest/GoodVulnerableSecurityCheckMethodsTest.class");
-        assertError(0, true, "goodVulnerableSecurityCheckMethodsTestCheck");
+        assertError(0, "goodVulnerableSecurityCheckMethodsTestCheck");
     }
 
     @Test
     public void testingGoodCase6() {
         performAnalysis("vulnerableSecurityCheckMethodsTest/GoodVulnerableSecurityCheckMethodsTest.class");
-        assertError(0, true, "goodVulnerableSecurityCheckMethodsTestCheck2");
+        assertError(0, "goodVulnerableSecurityCheckMethodsTestCheck2");
     }
 
     @Test
     public void testingGoodCase7() {
         performAnalysis("vulnerableSecurityCheckMethodsTest/GoodVulnerableSecurityCheckMethodsTest.class");
-        assertError(0, true, "goodVulnerableSecurityCheckMethodsTestCheck4");
+        assertError(0, "goodVulnerableSecurityCheckMethodsTestCheck4");
     }
 
-    private BugInstanceMatcher createBugInstanceMatcher(boolean withMethod, String methodName) {
-        if (!withMethod) {
-            return new BugInstanceMatcherBuilder()
-                    .bugType(bugType)
-                    .build();
-        }
+    private BugInstanceMatcher createBugInstanceMatcher(String methodName) {
         return new BugInstanceMatcherBuilder()
                 .bugType(bugType)
                 .inMethod(methodName)
                 .build();
     }
 
-    private void assertError(int count, boolean withMethod, String methodName) {
-        BugInstanceMatcher bugTypeMatcher = createBugInstanceMatcher(withMethod, methodName);
+    private void assertError(int count, String methodName) {
+        BugInstanceMatcher bugTypeMatcher = createBugInstanceMatcher(methodName);
         assertThat(getBugCollection(), containsExactly(count, bugTypeMatcher));
     }
 }
