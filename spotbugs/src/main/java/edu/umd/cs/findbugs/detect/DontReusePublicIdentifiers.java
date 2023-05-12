8aae3c3d62d29d85955f17f33addd16c102b149b
Nagy Norbert-Botond <nagynorbi0303@gmail.com>
2023-05-12 14:28:55 +0200

    Implement detector for class/interface name shadowing
    [Ticket: #32]

--- spotbugs/src/main/java/edu/umd/cs/findbugs/detect/DontReusePublicIdentifiers.java
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/detect/DontReusePublicIdentifiers.java
@@ -1,64 +1,104 @@
 package edu.umd.cs.findbugs.detect;
 
-import edu.umd.cs.findbugs.BugAccumulator;
 import edu.umd.cs.findbugs.BugInstance;
 import edu.umd.cs.findbugs.BugReporter;
-import edu.umd.cs.findbugs.ba.XField;
+import edu.umd.cs.findbugs.ba.ClassContext;
 import edu.umd.cs.findbugs.bcel.OpcodeStackDetector;
 import org.apache.bcel.Const;
+import org.apache.bcel.classfile.ConstantPool;
+import org.apache.bcel.classfile.InnerClass;
+import org.apache.bcel.classfile.InnerClasses;
+import org.apache.bcel.classfile.JavaClass;
 
 import static edu.umd.cs.findbugs.detect.PublicIdentifiers.PUBLIC_IDENTIFIERS;
 
 
 public class DontReusePublicIdentifiers extends OpcodeStackDetector {
-    private final BugAccumulator bugAccumulator;
+    // TODO: Implement this method
+    //  check method name clashes
+    //  check variable name clashes(obscuring)
+    //  add a constant set of public identifiers from the JSL
+    // DONE:
+    //  check class name clashes - ony for public classes
 
+    private final BugReporter bugReporter;
+    private String outerClassNameForInnerClasses = "";
+    private String sourceFileName = "";
 
     public DontReusePublicIdentifiers(BugReporter bugReporter) {
-        this.bugAccumulator = new BugAccumulator(bugReporter);
+        this.bugReporter = bugReporter;
     }
 
-    public DontReusePublicIdentifiers(BugAccumulator bugAccumulator) {
-        this.bugAccumulator = bugAccumulator;
+    @Override
+    public void visitClassContext(ClassContext classContext) {
+        JavaClass obj = classContext.getJavaClass();
+
+        if (obj.isPublic()) {
+            // save the source file name and class name for inner classes
+            sourceFileName = obj.getFileName();
+            outerClassNameForInnerClasses = obj.getClassName();
+            classContext.getJavaClass().accept(this);
+        }
+    }
+
+    @Override
+    public void visit(JavaClass obj) {
+        String[] fullName = obj.getClassName().split("\\.");
+        String simpleName = fullName[fullName.length - 1];
+
+        if (PUBLIC_IDENTIFIERS.contains(simpleName)) {
+            bugReporter.reportBug(new BugInstance(this, "PI_DO_NOT_REUSE_PUBLIC_IDENTIFIERS", NORMAL_PRIORITY).addClass(this).addString(obj.getClassName()).addString(sourceFileName + " shadows").addString(simpleName));
+        }
+    }
+
+    @Override
+    public void visitInnerClasses(InnerClasses obj) {
+        super.visitInnerClasses(obj);
+
+        ConstantPool constantPool = obj.getConstantPool();
+        for (InnerClass cls : obj.getInnerClasses()) {
+            // analyze the inner class itself
+
+            // check if the inner class is public
+            int accessFlags = cls.getInnerAccessFlags();
+            if ((accessFlags & Const.ACC_PUBLIC) == 0) {
+                continue;
+            }
+
+            // get the name of the cls through its name index
+            int nameIndex = cls.getInnerNameIndex();
+            String innerClassName = constantPool.getConstantUtf8(nameIndex).getBytes();
+            if (PUBLIC_IDENTIFIERS.contains(innerClassName)) {
+                bugReporter.reportBug(new BugInstance(this, "PI_DO_NOT_REUSE_PUBLIC_IDENTIFIERS", NORMAL_PRIORITY).addClass(this).addString(outerClassNameForInnerClasses + "$" + innerClassName).addString(sourceFileName + " shadows").addString(innerClassName));
+            }
+        }
     }
 
     @Override
     public void sawOpcode(int seen) {
-        // TODO: Implement this method
-        //  check the case when a class/interface is declared/defined and check it's name
-        // DONE:
-        //  create a resource file with the list of public identifiers from JSL -
-        //  try resolving the currently instantiated class name
-        //  compare the currently instantiated class name with the list of public identifiers
-        //  crete a bug instance and add to the bug accumulator
-
         // case #1: ASTORE is called when creating a new variable then the variable name should be checked
         // variable name when instantiating an object
-        if (seen == Const.ASTORE) {
-            XField f = getXFieldOperand();
-            if (f != null) {
-                if (PUBLIC_IDENTIFIERS.contains(f.getName())) {
-                    bugAccumulator.accumulateBug(new BugInstance(this, "DONT_REUSE_PUBLIC_IDENTIFIERS", NORMAL_PRIORITY)
-                            .addClassAndMethod(this), this);
-                }
-            }
-        }
+        //        if (seen == Const.ASTORE) {
+        //            XField f = getXFieldOperand();
+        //            if (f != null) {
+        //                if (PUBLIC_IDENTIFIERS.contains(f.getName())) {
+        //                    bugAccumulator.accumulateBug(new BugInstance(this, "PI_DO_NOT_REUSE_PUBLIC_IDENTIFIERS", NORMAL_PRIORITY).addClassAndMethod(this),
+        //                            this);
+        //                }
+        //            }
+        //        }
 
         // case #2: PUTFIELD/PUTFIELD is called inside a class to declare a variable
+        // TODO: NOTE: It is for giving a value to a field
         // variable declared inside a class/interface
-        if ((seen == Const.PUTFIELD || seen == Const.PUTSTATIC)) {
-            XField f = getXFieldOperand();
-            if (f != null) {
-                bugAccumulator.accumulateBug(new BugInstance(this, "DONT_REUSE_PUBLIC_IDENTIFIERS", NORMAL_PRIORITY)
-                        .addClassAndMethod(this), this);
-            }
-
-        }
-
-        // case #3: class/interface/enum declaration
-        // check the opcode when the class/interface/enum is declared
-
-
-        // case #4: meth
+        //        if ((seen == Const.PUTFIELD || seen == Const.PUTSTATIC)) {
+        //            XField f = getXFieldOperand();
+        //            if (f != null) {
+        //                bugAccumulator.accumulateBug(new BugInstance(this, "PI_DO_NOT_REUSE_PUBLIC_IDENTIFIERS", NORMAL_PRIORITY).addClassAndMethod(this),
+        //                        this);
+        //            }
+        //
+        //        }
     }
+
 }
