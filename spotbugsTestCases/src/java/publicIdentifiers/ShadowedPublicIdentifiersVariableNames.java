b61e48e5f9617a018248ff6210e3a7f1cccbf782
Nagy Norbert-Botond <nagynorbi0303@gmail.com>
2023-05-13 19:56:47 +0200

    Extend test cases for shadowing variables
    Add test case with static method and add another test case with method parameter
    
    [Ticket: #32]

--- spotbugsTestCases/src/java/publicIdentifiers/ShadowedPublicIdentifiersVariableNames.java
+++ spotbugsTestCases/src/java/publicIdentifiers/ShadowedPublicIdentifiersVariableNames.java
@@ -2,16 +2,26 @@ package publicIdentifiers;
 
 import publicIdentifiers.standalone.InputStream;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ShadowedPublicIdentifiersVariableNames {
-    private final String String = "String";
+    private final static String String = "String";
 
     public void containsShadowedLocalVariable1() {
         Integer Integer = 1;
+        System.out.println(Integer);
     }
 
-    public void containsShadowedLocalVariable2() {
+    public static ArrayList containsShadowedLocalVariable2() {
         ArrayList ArrayList = new ArrayList();
+        ArrayList.clear();
+        ArrayList.add("String");
+        return ArrayList;
+    }
+
+    private void containsShadowedLocalVariable3(File File) {
+        System.out.println(File.getName());
     }
 }
