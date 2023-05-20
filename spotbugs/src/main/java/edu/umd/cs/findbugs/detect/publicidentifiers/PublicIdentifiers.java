5551343313f621bd85b2f5c8946452feac827d9c
Nagy Norbert-Botond <nagynorbi0303@gmail.com>
2023-05-20 09:35:51 +0200

    Load public identifiers from Java Standard Library
    Since there are so many public identifiers, they are ordered alphabetically and split into separate files, roughly containing the same amount of identifiers.
    It is necessary to split in order to avoid compiler limitations.
    
    [Ticket: java-static-analysis/dev-spotbugs#32]

---
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/detect/publicidentifiers/PublicIdentifiers.java
@@ -0,0 +1,28 @@
+package edu.umd.cs.findbugs.detect.publicidentifiers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This class contains all the public identifiers that are part of the
+ * Java Standard Library. It is used to detect any shadowing or obscuring
+ * of these identifiers.
+ * Since there are so many identifiers, they are broken up into separate
+ * classes, roughly the same size to avoid compiler limits.
+ */
+public class PublicIdentifiers {
+    public static Set<String> PUBLIC_IDENTIFIERS = new HashSet<>();
+
+    static {
+        LoadPublicIdentifiersAB.load();
+        LoadPublicIdentifiersC.load();
+        LoadPublicIdentifiersDE.load();
+        LoadPublicIdentifiersFH.load();
+        LoadPublicIdentifiersIJ.load();
+        LoadPublicIdentifiersKM.load();
+        LoadPublicIdentifiersNQ.load();
+        LoadPublicIdentifiersS.load();
+        LoadPublicIdentifiersTU.load();
+        LoadPublicIdentifiersVZ.load();
+    }
+}
