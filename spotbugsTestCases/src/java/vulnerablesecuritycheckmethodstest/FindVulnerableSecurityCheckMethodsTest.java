cee8d149dc76b8cdf8999a12f70b9ee4e8b277f9
Nazir Muhammad Zafar Iqbal <bsvncs@inf.elte.hu>
2023-05-21 23:14:09 +0200

    Added many good and bad test cases.

--- spotbugsTestCases/src/java/vulnerablesecuritycheckmethodstest/FindVulnerableSecurityCheckMethodsTest.java
+++ spotbugsTestCases/src/java/vulnerablesecuritycheckmethodstest/FindVulnerableSecurityCheckMethodsTest.java
@@ -28,6 +28,93 @@ public class FindVulnerableSecurityCheckMethodsTest {
         }
     }
 
+    /**
+     * This method is vulnerable as it is a non-final class and the method isn't declared final or private.
+     * It is using the Security Manager nameless variable to perform checkRead Operation. So, It is vulnerable.
+     */
+    public void badFindVulnerableSecurityCheckMethodsCheck2() {
+        try {
+            System.getSecurityManager().checkRead("/temp/tempFile");
+            // Access the file
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+        }
+    }
+
+    /**
+     * This method is vulnerable as it is a non-final class and the method isn't declared final or private rather protected.
+     * It is using the Security Manager variable to perform checkRead Operation. So, It is vulnerable.
+     */
+    protected void badFindVulnerableSecurityCheckMethodsCheck3() {
+        try {
+            SecurityManager sm = System.getSecurityManager();
+
+            if (sm != null) {  // Check for permission to read file
+                sm.checkRead("/temp/tempFile");
+            }
+            // Access the file
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+        }
+    }
+
+    /**
+     * This method is helper method, and it calls another method which actually perform the security check.
+     */
+    public void badCallee() {
+        (new FindVulnerableSecurityCheckMethodsTest()).badCalled();
+    }
+
+    /**
+     * This is the method which is being called by th badCallee method.
+     * This method is vulnerable as it is in a non-final class and isn't declared final or private.
+     * It is using the Security Manager to perform checkRead Operation. So, It is vulnerable.
+     */
+    public void badCalled() {
+        try {
+            SecurityManager sm = System.getSecurityManager();
+            if (sm != null) {
+                sm.checkRead("/temp/tempFile");
+            }
+            // Access the file
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+        }
+    }
+
+    /**
+     * This method performs the security check in an obviously always true branch.
+     */
+    public void badFindVulnerableSecurityCheckMethodsCheck4() {
+        try {
+            SecurityManager sm = System.getSecurityManager();
+
+            if (sm != null) {  // Check for permission to read file
+                if(5 < 10) {
+                    sm.checkRead("/temp/tempFile");
+                }
+            }
+            // Access the file
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+        }
+    }
+
+    /**
+     * This method uses some other method of SecurityManager, not checkRead.
+     */
+    public void badFindVulnerableSecurityCheckMethodsCheck5() {
+        try {
+            SecurityManager sm = System.getSecurityManager();
+
+            if (sm != null) {  // Check for permission to read file
+                    sm.checkListen(10000);
+            }
+            // Access the file
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+        }
+    }
 
     /***
      * This method is not vulnerable as it is in a non-final class but is declared final.
@@ -72,6 +159,24 @@ public class FindVulnerableSecurityCheckMethodsTest {
             SecurityManager sm = System.getSecurityManager();
             //We have the SecurityManager object, but we do not perform any security check with it.
 
+            // Access the file
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+        }
+    }
+
+    /**
+     * This method performs the security check in an obviously always false branch.
+     */
+    public void goodVulnerableSecurityCheckMethodsTestCheck5() {
+        try {
+            SecurityManager sm = System.getSecurityManager();
+
+            if (sm != null) {  // Check for permission to read file
+                if(5 > 10) {
+                    sm.checkRead("/temp/tempFile");
+                }
+            }
             // Access the file
         } catch (SecurityException se) {
             logger.log(Level.WARNING, se.toString());
--- spotbugsTestCases/src/java/vulnerablesecuritycheckmethodstest/GoodVulnerableSecurityCheckMethodsTest.java
+++ spotbugsTestCases/src/java/vulnerablesecuritycheckmethodstest/GoodVulnerableSecurityCheckMethodsTest.java
@@ -47,6 +47,61 @@ public final class GoodVulnerableSecurityCheckMethodsTest {
         }
     }
 
+    /**
+     * This method is not vulnerable as it is in final class.
+     * This method is vulnerable as it is a non-final class and the method isn't declared final or private rather protected.
+     * It is using the Security Manager variable to perform checkRead Operation. So, It is vulnerable.
+     */
+    protected void goodFindVulnerableSecurityCheckMethodsCheck3() {
+        try {
+            SecurityManager sm = System.getSecurityManager();
+
+            if (sm != null) {  // Check for permission to read file
+                sm.checkRead("/temp/tempFile");
+            }
+            // Access the file
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+        }
+    }
+
+    /**
+     * This method is not vulnerable as it is in final class.
+     * It is using the Security Manager nameless variable to perform checkRead Operation.
+     */
+    public void goodFindVulnerableSecurityCheckMethodsCheck5() {
+        try {
+            System.getSecurityManager().checkRead("/temp/tempFile");
+            // Access the file
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+        }
+    }
+
+    /**
+     * This method is helper method, and it calls another method which actually perform the security check.
+     */
+    public void badCallee() {
+        (new FindVulnerableSecurityCheckMethodsTest()).badCalled();
+    }
+
+    /**
+     * This method is not vulnerable as it is in final class.
+     * This is the method which is being called by th badCallee method.
+     * It is using the Security Manager to perform checkRead Operation. So, It is vulnerable.
+     */
+    public void badCalled() {
+        try {
+            SecurityManager sm = System.getSecurityManager();
+            if (sm != null) {
+                sm.checkRead("/temp/tempFile");
+            }
+            // Access the file
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+        }
+    }
+
     /***
      * This method is not vulnerable as it is in final class.
      * Moreover, it is not using Security Manager to perform any check.
@@ -56,6 +111,61 @@ public final class GoodVulnerableSecurityCheckMethodsTest {
             SecurityManager sm = System.getSecurityManager();
             //We have the SecurityManager object, but we do not perform any security check with it.
 
+            // Access the file
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+        }
+    }
+
+    /**
+     * This method is not vulnerable as it is in final class.
+     * This method performs the security check in an obviously always true branch.
+     */
+    public void goodFindVulnerableSecurityCheckMethodsCheck6() {
+        try {
+            SecurityManager sm = System.getSecurityManager();
+
+            if (sm != null) {  // Check for permission to read file
+                if(5 < 10) {
+                    sm.checkRead("/temp/tempFile");
+                }
+            }
+            // Access the file
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+        }
+    }
+
+    /**
+     * This method is not vulnerable as it is in final class.
+     * This method uses some other method of SecurityManager, not checkRead.
+     */
+    public void goodFindVulnerableSecurityCheckMethodsCheck7() {
+        try {
+            SecurityManager sm = System.getSecurityManager();
+
+            if (sm != null) {  // Check for permission to read file
+                sm.checkListen(10000);
+            }
+            // Access the file
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+        }
+    }
+
+    /**
+     * This method is not vulnerable as it is in final class.
+     * This method performs the security check in an obviously always false branch.
+     */
+    public void goodVulnerableSecurityCheckMethodsTestCheck5() {
+        try {
+            SecurityManager sm = System.getSecurityManager();
+
+            if (sm != null) {  // Check for permission to read file
+                if(5 > 10) {
+                    sm.checkRead("/temp/tempFile");
+                }
+            }
             // Access the file
         } catch (SecurityException se) {
             logger.log(Level.WARNING, se.toString());
