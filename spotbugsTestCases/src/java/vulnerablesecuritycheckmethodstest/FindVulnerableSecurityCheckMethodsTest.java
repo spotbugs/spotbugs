415f3b55a3b2ac0406561b9be7d37c69d06e8c40
Nazir Muhammad Zafar Iqbal <bsvncs@inf.elte.hu>
2023-05-22 20:30:05 +0200

    Implemented new test cases in `FindVulnerableSecurityCheckMethodsTest.java`

--- spotbugsTestCases/src/java/vulnerablesecuritycheckmethodstest/FindVulnerableSecurityCheckMethodsTest.java
+++ spotbugsTestCases/src/java/vulnerablesecuritycheckmethodstest/FindVulnerableSecurityCheckMethodsTest.java
@@ -2,6 +2,7 @@ package vulnerablesecuritycheckmethodstest;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.lang.SecurityManager;
 
 /**
  * This is the non-final class case for the testing.
@@ -18,7 +19,6 @@ public class FindVulnerableSecurityCheckMethodsTest {
     public void badFindVulnerableSecurityCheckMethodsCheck() {
         try {
             SecurityManager sm = System.getSecurityManager();
-
             if (sm != null) {  // Check for permission to read file
                 sm.checkRead("/temp/tempFile");
             }
@@ -117,6 +117,23 @@ public class FindVulnerableSecurityCheckMethodsTest {
     }
 
     /***
+     * This method is a non-compliant test as it performs the security check in an arbitrary conditional.
+     */
+    public void badFindVulnerableSecurityCheckMethodsCheck6(int x) {
+        try {
+            SecurityManager sm = System.getSecurityManager();
+
+            if (sm != null) { // Check for permission to read file
+                if(x < 12) {
+                    sm.checkRead("/temp/tempFile");
+                }
+            }
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+        }
+    }
+
+    /***
      * This method is not vulnerable as it is in a non-final class but is declared final.
      * It is using the Security Manager to perform checkRead Operation. But still not vulnerable
      * as it cannot be changed anymore due to being declared as final.
@@ -180,6 +197,46 @@ public class FindVulnerableSecurityCheckMethodsTest {
             // Access the file
         } catch (SecurityException se) {
             logger.log(Level.WARNING, se.toString());
+        }
+    }
+
+    /**
+     * This method is a compliant test case as it performs a calls a method of Security Manager variable
+     * but that method actually performs no security check
+     */
+    public void goodVulnerableSecurityCheckMethodsTestCheck6() {
+        try {
+            SecurityManager sm = System.getSecurityManager();
+
+            if (sm != null) {  // Check for permission to read file
+                if (5 > 10) {
+                    sm.getSecurityContext();
+                }
+            }
+            // Access the file
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+        }
+    }
+
+    /***
+     * This method is a compliant test case as it is using a user defined SecurityManager class
+     * and the checker should know that, it is a user defined class not the `java.lang.SecurityManager`
+     */
+    public void goodVulnerableSecurityCheckMethodsTestCheck7() {
+        try {
+            //I am using the fully qualified name to resolve the type error due to the scope.
+            //I have imported `java.lang.Security` but here I want to instantiate
+            //User defined SecurityManager which is defined in the same package.
+            vulnerablesecuritycheckmethodstest.SecurityManager sm = new vulnerablesecuritycheckmethodstest.SecurityManager();
+
+            if (sm != null) { // Check for permission to read file
+                sm.checkRead("/temp/tempFile");
+            }
+            // Access the file
+        } catch (SecurityException se) {
+            logger.log(Level.WARNING, se.toString());
+
         }
     }
 }
