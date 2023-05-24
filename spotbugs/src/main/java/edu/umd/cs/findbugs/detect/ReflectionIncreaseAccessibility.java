cea7c18656054002f5241b5fcc7ffc94ebe773d0
Nazir Muhammad Zafar Iqbal <bsvncs@inf.elte.hu>
2023-05-24 05:40:33 +0200

    Reverted the null check in `ReflectionIncreaseAccessibility`

--- spotbugs/src/main/java/edu/umd/cs/findbugs/detect/ReflectionIncreaseAccessibility.java
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/detect/ReflectionIncreaseAccessibility.java
@@ -68,10 +68,8 @@ public class ReflectionIncreaseAccessibility extends OpcodeStackDetector {
                 return;
             }
             XMethod met = getXMethodOperand();
-            if (met == null) {
-                return;
-            }
-            if (!securityCheck && obj.isInitialParameter() && obj.getXField() == null &&
+            if (!securityCheck && obj.isInitialParameter()
+                    && obj.getXField() == null &&
                     "java.lang.Class".equals(cls.getClassName()) &&
                     "newInstance".equals(met.getName()) &&
                     "()Ljava/lang/Object;".equals(met.getSignature())) {
