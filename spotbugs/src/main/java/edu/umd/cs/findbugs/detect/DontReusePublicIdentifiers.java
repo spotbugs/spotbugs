f233f9c03db6949b0960663a469c48b00b63b134
Nagy Norbert-Botond <nagynorbi0303@gmail.com>
2023-07-09 11:11:12 +0200

    Fix build

--- spotbugs/src/main/java/edu/umd/cs/findbugs/detect/DontReusePublicIdentifiers.java
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/detect/DontReusePublicIdentifiers.java
@@ -22,8 +22,6 @@ import edu.umd.cs.findbugs.*;
 import edu.umd.cs.findbugs.ba.AnalysisContext;
 import edu.umd.cs.findbugs.ba.ClassContext;
 import org.apache.bcel.classfile.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.ListIterator;
