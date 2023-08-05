2b7d132824d7407b092d4140858f18d45da1c284
gtoison <guillaume.toison@tobam.fr>
2023-08-05 11:56:13 +0200

    test: added tests showing the issue with annotations on generics
    The NonNull annotations might be on the return type or on the parameters
    but they might also be on generics of the corresponding types.
    When they are on the return/parameter we should consider them, when they
    are on the generics we (currently) should ignore them.

--- spotbugsTestCases/src/java/type/annotation/CheckerFrameworkTypeAnnotations.java
+++ spotbugsTestCases/src/java/type/annotation/CheckerFrameworkTypeAnnotations.java
@@ -1,13 +1,52 @@
 package type.annotation;
 
+import java.util.List;
+
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 public class CheckerFrameworkTypeAnnotations {
 
     // Expecting NP_NONNULL_RETURN_VIOLATION to be thrown here
     @NonNull
-    public String foo() {
+    public String returningNullOnNonNullMethod() {
         return null;
     }
 
+    // We should NOT get a NP_NONNULL_RETURN_VIOLATION here: the list contains non null elements
+    // but it can be null itself
+    public <V extends @NonNull Object> List<@NonNull V> returningNullWithNonNullOnGenerics() {
+        return null;
+    }
+
+    // Expecting NP_NONNULL_RETURN_VIOLATION to be thrown here
+    @NonNull
+    public <V extends @NonNull Object> List<@NonNull V> returningNullOnNonNullMethodWithNonNullOnGenerics() {
+        return null;
+    }
+    
+    // Expecting NP_NONNULL_PARAM_VIOLATION to be thrown here
+    public void usingNullForNonNullParameter() {
+    	nonNullParameter(null);
+    }
+    
+    public void nonNullParameter(@NonNull Object o) {
+    }
+
+    // We should NOT get a NP_NONNULL_RETURN_VIOLATION here: the list contains non null elements
+    // but it can be null itself
+    public void usingNullParameterWithNonNullOnGenerics() {
+    	parameterWithNonNullOnGenerics(null);
+    }
+    
+    public <V extends @NonNull Object> void parameterWithNonNullOnGenerics(List<@NonNull V> list) {
+    }
+
+    // Expecting NP_NONNULL_PARAM_VIOLATION to be thrown here
+    public void usingNullOnNonNullParameterWithNonNullOnGenerics() {
+    	nonNullParameterWithNonNullOnGenerics(null);
+    }
+    
+    public <V extends @NonNull Object> void nonNullParameterWithNonNullOnGenerics(@NonNull List<@NonNull V> list) {
+    }
 }
