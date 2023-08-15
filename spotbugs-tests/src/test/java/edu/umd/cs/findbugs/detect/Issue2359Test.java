b275a0e9f8d45c07715a29d46b2ce885cee5a77e
gtoison <guillaume.toison@tobam.fr>
2023-08-15 20:36:25 +0200

    test: attempt to reproduce issue #2359

---
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue2359Test.java
@@ -0,0 +1,34 @@
+package edu.umd.cs.findbugs.detect;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.umd.cs.findbugs.AbstractIntegrationTest;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcher;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
+
+import static edu.umd.cs.findbugs.test.CountMatcher.containsExactly;
+import static org.junit.Assume.assumeFalse;
+import static org.junit.Assume.assumeThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.number.OrderingComparison.greaterThanOrEqualTo;
+
+public class Issue2359Test extends AbstractIntegrationTest {
+    @Before
+    public void verifyJavaVersion() {
+        assumeFalse(System.getProperty("java.specification.version").startsWith("1."));
+        int javaVersion = Integer.parseInt(System.getProperty("java.specification.version"));
+        assumeThat(javaVersion, is(greaterThanOrEqualTo(14)));
+    }
+
+    @Test
+    public void test() {
+        performAnalysis("../java14/Issue2359.class");
+        BugInstanceMatcher matcher = new BugInstanceMatcherBuilder()
+                .bugType("MS_EXPOSE_REP")
+                .build();
+
+        assertThat(getBugCollection(), containsExactly(0, matcher));
+    }
+}
