64726242ebc7329da9d5329c7f3897c01b9378fc
Guillaume Toison <86775455+gtoison@users.noreply.github.com>
2023-09-22 22:13:02 +0200

    test: issue #2552 sample (#2571)
    Sample of the false positive from issue #2552 and a true positive

---
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue2552Test.java
@@ -0,0 +1,39 @@
+package edu.umd.cs.findbugs.detect;
+
+import static edu.umd.cs.findbugs.test.CountMatcher.containsExactly;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+import edu.umd.cs.findbugs.AbstractIntegrationTest;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcher;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
+
+
+public class Issue2552Test extends AbstractIntegrationTest {
+    @Test
+    public void test() {
+        performAnalysis("../java14/ghIssues/Issue2552.class");
+
+        assertBugCount("EI_EXPOSE_REP", 1);
+
+        assertBugAtLine("EI_EXPOSE_REP", 12);
+    }
+
+    public void assertBugCount(String type, int expectedCount) {
+        BugInstanceMatcher bugMatcher = new BugInstanceMatcherBuilder()
+                .bugType(type)
+                .build();
+
+        assertThat(getBugCollection(), containsExactly(expectedCount, bugMatcher));
+    }
+
+    public void assertBugAtLine(String type, int line) {
+        BugInstanceMatcher bugMatcher = new BugInstanceMatcherBuilder()
+                .bugType(type)
+                .atLine(line)
+                .build();
+
+        assertThat(getBugCollection(), containsExactly(1, bugMatcher));
+    }
+}
