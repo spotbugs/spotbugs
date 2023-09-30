125454f39647942fe235c86b1b90bf662c82432a
Jeremy Landis <jeremylandis@hotmail.com>
2023-09-30 13:30:12 -0400

    [spotless] Rewrite to standard usage and fix plugin setup

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `kotlin-dsl`
+    id("com.diffplug.spotless") version "6.22.0"
 }
 
 repositories {
@@ -7,4 +8,5 @@ repositories {
 }
 dependencies {
     implementation("com.diffplug.gradle:goomph:3.43.0")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.22.0")
 }
