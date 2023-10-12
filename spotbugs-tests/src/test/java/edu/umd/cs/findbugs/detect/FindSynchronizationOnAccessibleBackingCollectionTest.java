c8b2d6fa273300cfa481003cc0b951662d098f42
Nagy Norbert-Botond <nagynorbi0303@gmail.com>
2023-10-12 21:18:37 +0200

    Migrate to junit 5

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/FindSynchronizationOnAccessibleBackingCollectionTest.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/FindSynchronizationOnAccessibleBackingCollectionTest.java
@@ -1,9 +1,9 @@
 package edu.umd.cs.findbugs.detect;
 
 import edu.umd.cs.findbugs.AbstractIntegrationTest;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FindSynchronizationOnAccessibleBackingCollectionTest extends AbstractIntegrationTest {
 
