c8bd40b952228183dcc8fb9fea5f4e5fcbb22fb4
gtoison <guillaume.toison@tobam.fr>
2023-10-17 21:45:57 +0200

    test: case reproducing issue #2632
    When the bug description has a placeholder for a non-existent annotation
    the sarif placeholder class causes a IndexOutOfBoundsException

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/sarif/SarifBugReporterTest.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/sarif/SarifBugReporterTest.java
@@ -159,6 +159,45 @@ class SarifBugReporterTest {
         JsonArray arguments = message.getAsJsonArray("arguments");
         assertThat(arguments.get(0).getAsInt(), is(10));
     }
+    
+
+
+    @Test
+    void testRuleWithInvalidArguments() {
+        // given
+        final String EXPECTED_BUG_TYPE = "BUG_TYPE";
+        final int EXPECTED_PRIORITY = Priorities.NORMAL_PRIORITY;
+        final String EXPECTED_DESCRIPTION = "describing about this bug type...";
+        BugPattern bugPattern = new BugPattern(EXPECTED_BUG_TYPE, "abbrev", "category", false, EXPECTED_DESCRIPTION,
+                "describing about this bug type with value {1234}...", "detailText", null, 0);
+        DetectorFactoryCollection.instance().registerBugPattern(bugPattern);
+
+        // when
+        reporter.reportBug(new BugInstance(bugPattern.getType(), bugPattern.getPriorityAdjustment()).addInt(10).addClass("the/target/Class"));
+        reporter.finish();
+
+        // then
+        String json = writer.toString();
+        JsonObject jsonObject = new Gson().fromJson(json, JsonObject.class);
+        JsonObject run = jsonObject.getAsJsonArray("runs").get(0).getAsJsonObject();
+        JsonArray rules = run.getAsJsonObject("tool").getAsJsonObject("driver").getAsJsonArray("rules");
+        JsonArray results = run.getAsJsonArray("results");
+
+        assertThat(rules.size(), is(1));
+        JsonObject rule = rules.get(0).getAsJsonObject();
+        assertThat(rule.get("id").getAsString(), is(bugPattern.getType()));
+        String defaultText = rule.getAsJsonObject("messageStrings").getAsJsonObject("default").get("text").getAsString();
+        assertThat(defaultText, is("describing about this bug type with value {0}..."));
+
+        assertThat(results.size(), is(1));
+        JsonObject result = results.get(0).getAsJsonObject();
+        assertThat(result.get("ruleId").getAsString(), is(bugPattern.getType()));
+        JsonObject message = result.getAsJsonObject("message");
+        assertThat(message.get("id").getAsString(), is("default"));
+        assertThat(message.get("text").getAsString(), is(bugPattern.getShortDescription()));
+        JsonArray arguments = message.getAsJsonArray("arguments");
+        assertThat(arguments.get(0).getAsString(), is("?>?1234/2???"));
+    }
 
     @Test
     void testMissingClassNotification() {
