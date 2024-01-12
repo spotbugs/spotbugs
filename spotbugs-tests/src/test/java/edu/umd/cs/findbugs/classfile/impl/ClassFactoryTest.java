357f414e60f51e50d2b6d612895eea6f572e330f
gtoison <guillaume.toison@tobam.fr>
2024-01-12 08:04:58 +0100

    test: converted acceptConstantDynamic to a parameterized test

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/classfile/impl/ClassFactoryTest.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/classfile/impl/ClassFactoryTest.java
@@ -30,12 +30,14 @@ import edu.umd.cs.findbugs.classfile.IScannableCodeBase;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 
 class ClassFactoryTest {
 
@@ -112,18 +114,15 @@ class ClassFactoryTest {
         assertTrue(codeBase.iterator().hasNext());
     }
 
-    @Test
-    void acceptConstantDynamic() throws Exception {
-        ArrayList<String> fileNames = new ArrayList<>();
-        fileNames.add("../spotbugsTestCases/src/classSamples/recordCompileWithJaCoCo0.8.8/Foo.clazz");
-        fileNames.add("../spotbugsTestCases/src/classSamples/recordCompileWithJaCoCo0.8.8/FooWithMember.clazz");
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "../spotbugsTestCases/src/classSamples/recordCompileWithJaCoCo0.8.8/Foo.clazz",
+        "../spotbugsTestCases/src/classSamples/recordCompileWithJaCoCo0.8.8/FooWithMember.clazz" })
+    void acceptConstantDynamic(String fileName) throws Exception {
+        try (SingleFileCodeBase codeBase = new SingleFileCodeBase(null, fileName)) {
+            ClassDescriptor classDescriptor = codeBase.getClassDescriptor();
 
-        for (String fileName : fileNames) {
-            try (SingleFileCodeBase codeBase = new SingleFileCodeBase(null, fileName)) {
-                ClassDescriptor classDescriptor = codeBase.getClassDescriptor();
-
-                assertNotNull(classDescriptor);
-            }
+            assertNotNull(classDescriptor);
         }
     }
 }
