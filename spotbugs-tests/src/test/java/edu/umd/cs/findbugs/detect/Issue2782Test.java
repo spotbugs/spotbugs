6b0392221fd8ddb968cb3671731b9f265c638519
gtoison <guillaume.toison@tobam.fr>
2024-01-14 18:53:17 +0100

    test: issue #2782 reproducer

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue2782Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue2782Test.java
@@ -1,30 +1,36 @@
 package edu.umd.cs.findbugs.detect;
 
 import edu.umd.cs.findbugs.AbstractIntegrationTest;
+import edu.umd.cs.findbugs.BugCollection;
 import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcher;
 import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledOnJre;
-import org.junit.jupiter.api.condition.JRE;
 
 import static edu.umd.cs.findbugs.test.CountMatcher.containsExactly;
 import static org.hamcrest.MatcherAssert.*;
 
-public class Issue2782Test extends AbstractIntegrationTest {
+import edu.umd.cs.findbugs.test.SpotBugsRunner;
+import edu.umd.cs.findbugs.test.SpotBugsExtension;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.nio.file.Paths;
+
+@ExtendWith(SpotBugsExtension.class)
+class Issue2782Test extends AbstractIntegrationTest {
 
     @Test
-    @DisabledOnJre({ JRE.JAVA_8, JRE.JAVA_11, JRE.JAVA_17 })
-    void testIssue() {
-        performAnalysis(
-                "../java21/Issue2782.class",
-                "../java21/Issue2782$MyInterface.class",
-                "../java21/Issue2782$Impl1.class",
-                "../java21/Issue2782$Impl2.class");
+    void testIssue(SpotBugsRunner spotbugs) {
+        BugCollection bugCollection = spotbugs.performAnalysis(
+                Paths.get("../spotbugsTestCases/src/classSamples/java21typeSwitchSample/Issue2782.class"),
+                Paths.get("../spotbugsTestCases/src/classSamples/java21typeSwitchSample/Issue2782$MyInterface.class"),
+                Paths.get("../spotbugsTestCases/src/classSamples/java21typeSwitchSample/Issue2782$Impl1.class"),
+                Paths.get("../spotbugsTestCases/src/classSamples/java21typeSwitchSample/Issue2782$Impl2.class"));
 
         BugInstanceMatcher matcher = new BugInstanceMatcherBuilder()
                 .bugType("BC_UNCONFIRMED_CAST")
                 .build();
 
-        assertThat(getBugCollection(), containsExactly(0, matcher));
+        assertThat(bugCollection, containsExactly(0, matcher));
     }
 }
