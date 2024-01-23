7c9c0c5fe491ee52cb03e2be980d81d46f654f3a
Judit Knoll <knoll.judit94@gmail.com>
2024-01-23 15:14:30 +0100

    add testcases

--- spotbugsTestCases/src/java/nullnessAnnotations/TestNonNull7.java
+++ spotbugsTestCases/src/java/nullnessAnnotations/TestNonNull7.java
@@ -1,5 +1,9 @@
 package nullnessAnnotations;
 
+import com.google.common.base.Verify;
+import com.google.common.base.Preconditions;
+import org.apache.commons.lang3.Validate;
+
 import java.io.File;
 import java.util.Objects;
 
@@ -7,11 +11,42 @@ public class TestNonNull7 {
     /**
      * Issue <a href="https://github.com/spotbugs/spotbugs/issues/456">#456</a>.
      */
-    public void ObjectsrequireNonNull1(String[] args) {
-        File rootFile = new File("some_unexisting_file");
+    public void objectsrequireNonNull1(File rootFile) {
         for (File subFile : Objects.requireNonNull(rootFile.listFiles())) {
             System.out.println(subFile.getName());
         }
     }
 
+    public void objectsNonNull1(File rootFile) {
+        File[] files = rootFile.listFiles();
+        if (Objects.nonNull(files)) {
+            for (File subFile : files) {
+                System.out.println(subFile.getName());
+            }
+        }
+    }
+
+    public void verifyverifyNotNull1(File rootFile) {
+        for (File subFile : Verify.verifyNotNull(rootFile.listFiles())) {
+            System.out.println(subFile.getName());
+        }
+    }
+
+    public void validatenotNull1(File rootFile) {
+        for (File subFile : Validate.notNull(rootFile.listFiles())) {
+            System.out.println(subFile.getName());
+        }
+    }
+
+    public void preconditionsNotNull(File rootFile) {
+        for (File subFile : Preconditions.checkNotNull(rootFile.listFiles())) {
+            System.out.println(subFile.getName());
+        }
+    }
+
+    public void nocheck(File rootFile) {
+        for (File subFile : rootFile.listFiles()) {
+            System.out.println(subFile.getName());
+        }
+    }
 }
