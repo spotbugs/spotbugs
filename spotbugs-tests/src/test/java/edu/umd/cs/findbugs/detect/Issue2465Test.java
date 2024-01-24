df869d6d16f333dc5e71808003856de242f6f3bc
Judit Knoll <knoll.judit94@gmail.com>
2024-01-24 19:10:55 +0100

    add classes missing for analyses for test Issue2465Test

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue2465Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue2465Test.java
@@ -1,26 +1,16 @@
 package edu.umd.cs.findbugs.detect;
 
-import static edu.umd.cs.findbugs.test.CountMatcher.containsExactly;
-import static org.hamcrest.MatcherAssert.assertThat;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import edu.umd.cs.findbugs.AbstractIntegrationTest;
-import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcher;
-import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
 
 
 class Issue2465Test extends AbstractIntegrationTest {
 
     @Test
     void testIssue() {
-        performAnalysis("ghIssues/Issue2465.class");
-
-        // Due to issue #2465 the analysis should fail and we should not get further
-        // Check that with the fix we get not bug instead of a crash
-        BugInstanceMatcher bugTypeMatcher = new BugInstanceMatcherBuilder()
-                .build();
-
-        assertThat(getBugCollection(), containsExactly(0, bugTypeMatcher));
+        Assertions.assertDoesNotThrow(() -> performAnalysis("ghIssues/Issue2465.class", "ghIssues/Issue2465$1.class",
+                "ghIssues/Issue2465$Role.class"));
     }
 }
