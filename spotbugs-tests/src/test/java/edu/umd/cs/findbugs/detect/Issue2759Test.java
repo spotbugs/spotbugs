e03b724af1e8c4489dffd213757b44bc4a239b3e
Judit Knoll <knoll.judit94@gmail.com>
2024-01-24 22:21:21 +0100

    remove unnecessary SpotBugsExtension from Issue2759Test

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue2759Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue2759Test.java
@@ -1,26 +1,19 @@
 package edu.umd.cs.findbugs.detect;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import edu.umd.cs.findbugs.AbstractIntegrationTest;
-import edu.umd.cs.findbugs.BugCollection;
-import edu.umd.cs.findbugs.test.SpotBugsRunner;
-import edu.umd.cs.findbugs.test.SpotBugsExtension;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-
-import java.nio.file.Paths;
 
 /**
  * @author gtoison
  *
  */
-@ExtendWith(SpotBugsExtension.class)
 class Issue2759Test extends AbstractIntegrationTest {
-
     @Test
-    void testIssue(SpotBugsRunner spotbugs) {
-        BugCollection bugCollection = spotbugs.performAnalysis(
-                Paths.get("../spotbugsTestCases/src/classSamples/classesModifiedByJacoco/JavaLanguageParser$ClassBlockContext.class"));
+    void testIssue() {
+        Assertions.assertDoesNotThrow(() -> performAnalysis(
+                "../../../../src/classSamples/classesModifiedByJacoco/JavaLanguageParser$ClassBlockContext.class"));
     }
 }
