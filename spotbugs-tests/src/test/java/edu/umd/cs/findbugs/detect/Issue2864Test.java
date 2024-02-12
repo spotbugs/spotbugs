7a83c5769152fc251b46bc9bcef1f9db5ecf329c
gtoison <guillaume.toison@tobam.fr>
2024-02-12 09:16:29 +0100

    test: Removed SpotBugsExtension since we extend AbstractIntegrationTest

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue2864Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue2864Test.java
@@ -6,32 +6,23 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.jupiter.api.Test;
 
 import edu.umd.cs.findbugs.AbstractIntegrationTest;
-import edu.umd.cs.findbugs.BugCollection;
-import edu.umd.cs.findbugs.test.SpotBugsRunner;
-import edu.umd.cs.findbugs.test.SpotBugsExtension;
 import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcher;
 import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-
-import java.nio.file.Paths;
 
 /**
  * @author gtoison
  *
  */
-@ExtendWith(SpotBugsExtension.class)
 class Issue2864Test extends AbstractIntegrationTest {
 
     @Test
-    void testIssue(SpotBugsRunner spotbugs) {
-        BugCollection bugCollection = spotbugs.performAnalysis(
-                Paths.get("../spotbugsTestCases/src/classSamples/classEnhanceByHibernate/HibernateEnhancedEntity.class"));
+    void testIssue() {
+        performAnalysis("../../../../src/classSamples/classEnhanceByHibernate/HibernateEnhancedEntity.class");
 
         BugInstanceMatcher bugTypeMatcher = new BugInstanceMatcherBuilder()
                 .bugType("DLS_DEAD_LOCAL_STORE")
                 .build();
 
-        assertThat(bugCollection, containsExactly(0, bugTypeMatcher));
+        assertThat(getBugCollection(), containsExactly(0, bugTypeMatcher));
     }
 }
