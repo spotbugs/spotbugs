d78b5f4b414dd6f902a593d23b1fcc393bbac8b2
gtoison <guillaume.toison@tobam.fr>
2024-02-26 18:44:39 +0100

    fix: updated the comment now that the false positive is fixed

--- spotbugsTestCases/src/java/ghIssues/Issue2558.java
+++ spotbugsTestCases/src/java/ghIssues/Issue2558.java
@@ -2,7 +2,7 @@ package ghIssues;
 
 public class Issue2558 {
 
-    public boolean equals(@edu.umd.cs.findbugs.annotations.NonNull Object a) { //report a warning, but it is an FP
+    public boolean equals(@edu.umd.cs.findbugs.annotations.NonNull Object a) { // False positive solved by the change
         return a.hashCode() == this.hashCode();
     }
     public boolean something(Issue2558 a, Issue2558 b) {
