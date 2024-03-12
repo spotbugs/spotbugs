fae86d79723a0e798b51b08410473895ab5dffea
gtoison <guillaume.toison@tobam.fr>
2024-03-12 14:06:38 +0100

    test: added a true positive test for a lambda called in constructor

--- spotbugsTestCases/src/java/ghIssues/Issue2837.java
+++ spotbugsTestCases/src/java/ghIssues/Issue2837.java
@@ -1,11 +1,16 @@
 package ghIssues;
 
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class Issue2837 {
 	public boolean overridable(String s) {
 		return true;
 	}
 
+	// overridable method referenced but not used
 	public final Predicate<String> predicate1 = this::overridable;
+	
+	// overridable method used
+	public final long matchesCount = Stream.of("Test").filter(this::overridable).count();
 }
