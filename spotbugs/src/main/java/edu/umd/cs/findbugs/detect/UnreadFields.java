7f51f90376daedf6920a858867e21f99f026eb11
Judit Knoll <123470644+JuditKnoll@users.noreply.github.com>
2024-04-20 04:35:48 +0200

    fix issues reported by SonarQube (#2954)

--- spotbugs/src/main/java/edu/umd/cs/findbugs/detect/UnreadFields.java
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/detect/UnreadFields.java
@@ -21,7 +21,6 @@ package edu.umd.cs.findbugs.detect;
 
 import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
