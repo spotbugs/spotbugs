27d60a26c752ba25ad7c2ed981a5af1002d7174e
gtoison <guillaume.toison@tobam.fr>
2024-04-21 11:16:44 +0200

    test: make the base sample class serializable so it is analyzed first

--- spotbugsTestCases/src/java17/Issue2793.java
+++ spotbugsTestCases/src/java17/Issue2793.java
@@ -5,11 +5,11 @@ import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
-public class Issue2793 {
+public class Issue2793 implements Serializable {
+	public static final long serialVersionUID = 1L;
+
 	public record RecordWithoutSerialVersionUid(int x, int y, String message) implements Serializable {
 		
 	}
@@ -22,5 +22,20 @@ public class Issue2793 {
 		public void readExternal(ObjectInput in) throws IOException {
 			
 		}
+	}
+
+	public static class SerializableClass implements Serializable {
+		public static final long serialVersionUID = 1L;
+
+		public Integer x;
+		public Integer y;
+	}
+
+	public record YangLibModule(SerializableClass identifier,
+								SerializableClass namespace,
+								Map<SerializableClass, YangLibModule> submodules,
+								Set<SerializableClass> features,
+								Set<SerializableClass> deviationModuleNames,
+								SerializableClass source) {
 	}
 }
