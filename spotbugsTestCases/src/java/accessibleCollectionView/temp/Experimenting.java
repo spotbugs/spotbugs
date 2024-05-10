c81d562804576f30d59014f55463f009eadb6e1a
Nagy Norbert-Botond <nagynorbi0303@gmail.com>
2024-05-10 11:21:31 +0200

    Add missing test file

---
+++ spotbugsTestCases/src/java/accessibleCollectionView/temp/Experimenting.java
@@ -0,0 +1,39 @@
+package accessibleCollectionView.temp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class Experimenting {
+
+    public Object[] collection1 = new Object[10];
+    private final List<Object> view1 = Arrays.asList(collection1);
+
+//    public int[] collection2 = new int[10];
+//    private final List<Object> view2 = Arrays.asList(collection2);
+//
+//    // What about conversion?
+//    boolean b = true;
+//    public int[] collection3 = new int[10];
+//    List<Object> view3 = (List<Object>) Arrays.asList(collection3);
+
+//    public List<Object> list = new ArrayList<>();
+//    List<Object> view4 = b ? Arrays.asList(Collections.unmodifiableList(list)) : list;
+
+//    if (b) {
+//        List<Object> view5 = Arrays.asList(list);
+//    } else {
+//        List<Object> view6 = list;
+//    }
+
+//
+//    Integer[] collection4 = {1, 2, 3};
+//    List<Integer> view3 = Arrays.asList(collection4);
+//
+//    Double[] collection5 = new Double[15];
+//    List<Double> view4 = Arrays.asList(collection5);
+
+
+}
