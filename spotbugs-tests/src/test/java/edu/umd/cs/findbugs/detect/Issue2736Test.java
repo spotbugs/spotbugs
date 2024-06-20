3938fe587d6a98394ac2d764c956096e805f200b
gtoison <guillaume.toison@tobam.fr>
2024-06-20 17:55:01 +0200

    test: check that JEP 456's unnamed variables do not cause false positive

---
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue2736Test.java
@@ -0,0 +1,29 @@
+package edu.umd.cs.findbugs.detect;
+
+import edu.umd.cs.findbugs.AbstractIntegrationTest;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcher;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnJre;
+import org.junit.jupiter.api.condition.JRE;
+
+import static edu.umd.cs.findbugs.test.CountMatcher.containsExactly;
+import static org.hamcrest.MatcherAssert.*;
+
+class Issue2736Test extends AbstractIntegrationTest {
+
+    @Test
+    @DisabledOnJre({ JRE.JAVA_8, JRE.JAVA_11, JRE.JAVA_17, JRE.JAVA_21 })
+    void testIssue() {
+        performAnalysis("../java22/Issue2736.class");
+
+        assertBugCount(0);
+    }
+
+    private void assertBugCount(String type, int expectedCount) {
+        BugInstanceMatcher bugMatcher = new BugInstanceMatcherBuilder()
+                .build();
+
+        assertThat(getBugCollection(), containsExactly(expectedCount, bugMatcher));
+    }
+}
