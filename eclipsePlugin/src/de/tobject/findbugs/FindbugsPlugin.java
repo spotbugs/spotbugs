096c9d75eb46999c8a4ab2760c765c914a71c110
Judit Knoll <knoll.judit94@gmail.com>
2024-08-02 17:13:58 +0200

    small fixes according to sonar cloud

--- eclipsePlugin/src/de/tobject/findbugs/FindbugsPlugin.java
+++ eclipsePlugin/src/de/tobject/findbugs/FindbugsPlugin.java
@@ -421,14 +421,14 @@ public class FindbugsPlugin extends AbstractUIPlugin {
      */
     public static IWorkbenchWindow getActiveWorkbenchWindow() {
         if (Display.getCurrent() != null) {
-            return getDefault().getWorkbench().getActiveWorkbenchWindow();
+            return PlatformUI.getWorkbench().getActiveWorkbenchWindow();
         }
         // need to call from UI thread
         final IWorkbenchWindow[] window = new IWorkbenchWindow[1];
         Display.getDefault().syncExec(new Runnable() {
             @Override
             public void run() {
-                window[0] = getDefault().getWorkbench().getActiveWorkbenchWindow();
+                window[0] = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
             }
         });
         return window[0];
