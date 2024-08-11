b6e33e2bbb2dcd5bf29261f3764765f1dbc9f873
Guillaume Toison <guillaume.toison@tobam.fr>
2024-08-11 14:44:15 +0200

    fix: applied spotless

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/ba/Issue3069Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/ba/Issue3069Test.java
@@ -1,7 +1,6 @@
 package edu.umd.cs.findbugs.ba;
 
 import edu.umd.cs.findbugs.AbstractIntegrationTest;
-import edu.umd.cs.findbugs.SortedBugCollection;
 import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcher;
 import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
 import org.junit.jupiter.api.Test;
@@ -15,10 +14,10 @@ public class Issue3069Test extends AbstractIntegrationTest {
     @Test
     void testIssue() {
         performAnalysis("ghIssues/Issue3069.class",
-        		"org/rocksdb/AbstractImmutableNativeReference.class",
-        		"org/rocksdb/AbstractNativeReference.class",
-        		"org/rocksdb/ReadOptions.class",
-        		"org/rocksdb/RocksObject.class");
+                "org/rocksdb/AbstractImmutableNativeReference.class",
+                "org/rocksdb/AbstractNativeReference.class",
+                "org/rocksdb/ReadOptions.class",
+                "org/rocksdb/RocksObject.class");
 
         assertBug("readOptionsNotClosed", 8);
         assertBug(1);
