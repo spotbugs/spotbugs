975717e271a7a40ba8c359d5704f6ffc6c945502
Samat Tamerlan <tamerlan.samat@ericsson.com>
2024-08-22 18:26:15 +0200

    more precise testing functions

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue637Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue637Test.java
@@ -1,9 +1,10 @@
 package edu.umd.cs.findbugs.detect;
 
 import static edu.umd.cs.findbugs.test.CountMatcher.containsExactly;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import edu.umd.cs.findbugs.AbstractIntegrationTest;
 import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcher;
@@ -19,27 +20,52 @@ public class Issue637Test extends AbstractIntegrationTest {
     private static final String DESIRED_BUG_TYPE = "FS_BAD_DATE_FORMAT_FLAG_COMBO";
 
     /**
-     * Expects 25 "FS_BAD_DATE_FORMAT_FLAG_COMBO" errors in the "Issue637Errors.class" file.
+     * Expects total of 25 errors that are:
+     *  - of type defined in DESIRED_BUG_TYPE
+     *  - inside the Issue637Errors class
+     *  - inside the specified function (parameter)
+     *  - on a proper line (parameter)
      */
     @Test
     public void testClassWithErrors() {
         performAnalysis("ghIssues/Issue637Errors.class");
-        BugInstanceMatcher bugTypeMatcher = new BugInstanceMatcherBuilder()
-                .bugType(DESIRED_BUG_TYPE)
-                .build();
-        assertThat(getBugCollection(), containsExactly(25, bugTypeMatcher));
+        assertNumOfBug637(25);
+
+        int line;
+        for (line = 28; line <= 32; line++)
+            assertBug637("oneArgConstructorTest", line);
+        for (line = 47; line <= 51; line++)
+            assertBug637("twoArgLocaleConstructorTest", line);
+        for (line = 67; line <= 71; line++)
+            assertBug637("twoArgDateFormatSymbolConstructorTest", line);
+        for (line = 90; line <= 94; line++)
+            assertBug637("applyPatternTest", line);
+        for (line = 101; line <= 105; line++)
+            assertBug637("applyLocalizedPatternTest", line);
     }
 
     /**
-     * Expects no "FS_BAD_DATE_FORMAT_FLAG_COMBO" errors in the "Issue637NoErrors.class" file.
+     * Expects no errors of type defined in DESIRED_BUG_TYPE
      */
     @Test
     public void testClassWithoutErrors() {
         performAnalysis("ghIssues/Issue637NoErrors.class");
-        BugInstanceMatcher bugTypeMatcher = new BugInstanceMatcherBuilder()
-                .bugType(DESIRED_BUG_TYPE)
-                .build();
-        assertThat(getBugCollection(), containsExactly(0, bugTypeMatcher));
+        assertNumOfBug637(0);
     }
 
+    private void assertNumOfBug637(int num) {
+        final BugInstanceMatcher bugTypeMatcher = new BugInstanceMatcherBuilder()
+                .bugType(DESIRED_BUG_TYPE).build();
+        assertThat(getBugCollection(), containsExactly(num, bugTypeMatcher));
+    }
+
+    private void assertBug637(String method, int line) {
+        final BugInstanceMatcher bugInstanceMatcher = new BugInstanceMatcherBuilder()
+                .bugType(DESIRED_BUG_TYPE)
+                .inClass("Issue637Errors")
+                .inMethod(method)
+                .atLine(line)
+                .build();
+        assertThat(getBugCollection(), hasItem(bugInstanceMatcher));
+    }
 }
