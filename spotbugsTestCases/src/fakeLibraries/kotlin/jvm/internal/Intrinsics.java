d1cecea51a7ba2113f358e99053358459618ab8d
gtoison <guillaume.toison@tobam.fr>
2024-08-29 21:13:47 +0200

    test: reproducer for issue #3094

---
+++ spotbugsTestCases/src/fakeLibraries/kotlin/jvm/internal/Intrinsics.java
@@ -0,0 +1,9 @@
+package kotlin.jvm.internal;
+
+public class Intrinsics {
+	public static void checkNotNull(Object object) {
+    }
+
+    public static void checkNotNull(Object object, String message) {
+    }
+}
