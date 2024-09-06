7f0df67479bf2b9f68a79e87b106a993675665af
gtoison <guillaume.toison@tobam.fr>
2024-09-06 07:50:34 +0200

    fix: javadoc fixes

--- spotbugs-annotations/src/main/java/edu/umd/cs/findbugs/annotations/SuppressMatchType.java
+++ spotbugs-annotations/src/main/java/edu/umd/cs/findbugs/annotations/SuppressMatchType.java
@@ -5,18 +5,22 @@ public enum SuppressMatchType {
     /**
      * Default bug suppression using a mixed prefixed / case insensitive match depending on the criterion.
      * Suppress bugs matching any of:
+     * <ul>
      * <li> the given bug type with: {@link String#startsWith(String)}
      * <li> the given bug category with: {@link String#equalsIgnoreCase(String)}
      * <li> the given bug abbreviation with: {@link String#equalsIgnoreCase(String)}
+     * </ul>
      */
     DEFAULT,
 
     /**
      * Exact (case sensitive match).
      * Suppress bugs matching any of:
+     * <ul>
      * <li> the given bug type with: {@link String#equals(Object)}
      * <li> the given bug category with: {@link String#equals(Object)}
      * <li> the given bug abbreviation with: {@link String#equals(Object)}
+     * </ul>
      */
     EXACT,
 
