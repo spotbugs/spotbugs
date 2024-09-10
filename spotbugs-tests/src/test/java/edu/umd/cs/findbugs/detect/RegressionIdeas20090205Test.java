7c676e4245e7d9a6a2d09296ba11f273f723ef73
Judit Knoll <knoll.judit94@gmail.com>
2024-09-10 16:15:34 +0200

    fix comment

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/RegressionIdeas20090205Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/RegressionIdeas20090205Test.java
@@ -12,8 +12,8 @@ class RegressionIdeas20090205Test extends AbstractIntegrationTest {
     @Test
     void test() {
         performAnalysis("bugIdeas/Ideas_2009_02_05.class");
-        // FPs in isDecember in line 24, 28
-        // FPs in isBefore2009 (line 12), is2009 (line 16), is2008 (line 20), but it's difficult to know the programmer's intent,
+        // FNs in isDecember in line 24, 28
+        // FNs in isBefore2009 (line 12), is2009 (line 16), is2008 (line 20), but it's difficult to know the programmer's intent,
         //  also the used function is already deprecated because of its confusing design
 
         BugInstanceMatcher matcher = new BugInstanceMatcherBuilder()
