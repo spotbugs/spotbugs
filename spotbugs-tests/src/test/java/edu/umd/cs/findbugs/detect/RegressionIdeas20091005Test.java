a494276a51d2ed70afd0a7c1ad76b0bd1d0cd6dc
Judit Knoll <knoll.judit94@gmail.com>
2024-09-10 16:14:56 +0200

    add test for bugIdeas/Ideas_2009_10_05

---
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/RegressionIdeas20091005Test.java
@@ -0,0 +1,41 @@
+package edu.umd.cs.findbugs.detect;
+
+import edu.umd.cs.findbugs.AbstractIntegrationTest;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcher;
+import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class RegressionIdeas20091005Test extends AbstractIntegrationTest {
+    @Test
+    void test() {
+        performAnalysis("bugIdeas/Ideas_2009_10_05.class");
+        assertBug("NP_NONNULL_PARAM_VIOLATION", 2, 14);
+        // FNs in lines 10-15 and 20-26
+
+        assertExactBug("NP_NONNULL_PARAM_VIOLATION", "propertiesCantContainNull", 9);
+        assertExactBug("NP_NONNULL_PARAM_VIOLATION", "hashtablesCantContainNull", 20);
+    }
+
+    private void assertBug(String bugtype, int min, int max) {
+        BugInstanceMatcher matcher = new BugInstanceMatcherBuilder()
+                .bugType(bugtype)
+                .build();
+        long exactMatches = getBugCollection().getCollection().stream().filter(matcher::matches).count();
+        assertTrue(exactMatches >= min);
+        assertTrue(exactMatches >= max);
+    }
+
+    private void assertExactBug(String bugtype, String method, int line) {
+        BugInstanceMatcher matcher = new BugInstanceMatcherBuilder()
+                .bugType(bugtype)
+                .inClass("Ideas_2009_10_05")
+                .inMethod(method)
+                .atLine(line)
+                .build();
+        assertThat(getBugCollection(), hasItem(matcher));
+    }
+}
