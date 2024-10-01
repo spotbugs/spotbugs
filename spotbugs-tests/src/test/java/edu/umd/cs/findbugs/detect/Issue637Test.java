f63db017db4dbef40a06e3f68f8ac50b03d2d2b3
Samat Tamerlan <tamerlan.samat@ericsson.com>
2024-10-01 13:53:57 +0200

    call of tests using provided methods

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue637Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue637Test.java
@@ -1,14 +1,7 @@
 package edu.umd.cs.findbugs.detect;
 
-import static edu.umd.cs.findbugs.test.CountMatcher.containsExactly;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasItem;
-
 import org.junit.jupiter.api.Test;
-
 import edu.umd.cs.findbugs.AbstractIntegrationTest;
-import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcher;
-import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
 
 /**
  * SpotBugs should check for bad combinations of date format flags in SimpleDateFormat.
@@ -21,104 +14,106 @@ class Issue637Test extends AbstractIntegrationTest {
     /**
      * Expects total of 80 errors that are:
      *  - of type defined in DESIRED_BUG_TYPE
-     *  - inside the Issue637Errors class
+     *  - inside the Issue637Errors class (as className)
      *  - was called in a specified function (parameter)
      *  - and caught on a proper line (parameter)
      */
     @Test
     void testClassWithErrors() {
-        performAnalysis("ghIssues/issue637/Issue637Errors.class");
-        assertNumOfBug637(80);
+        String className = "Issue637Errors";
+        performAnalysis("ghIssues/issue637/" + className + ".class");
+        assertBugTypeCount(DESIRED_BUG_TYPE, 80);
 
-        assertBug637("oneArgConstructorTest", 31);
-        assertBug637("oneArgConstructorTest", 32);
-        assertBug637("oneArgConstructorTest", 33);
-        assertBug637("oneArgConstructorTest", 34);
-        assertBug637("oneArgConstructorTest", 35);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "oneArgConstructorTest", 31);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "oneArgConstructorTest", 31);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "oneArgConstructorTest", 32);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "oneArgConstructorTest", 33);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "oneArgConstructorTest", 34);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "oneArgConstructorTest", 35);
 
-        assertBug637("twoArgLocaleConstructorTest", 50);
-        assertBug637("twoArgLocaleConstructorTest", 51);
-        assertBug637("twoArgLocaleConstructorTest", 52);
-        assertBug637("twoArgLocaleConstructorTest", 53);
-        assertBug637("twoArgLocaleConstructorTest", 54);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "twoArgLocaleConstructorTest", 50);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "twoArgLocaleConstructorTest", 51);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "twoArgLocaleConstructorTest", 52);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "twoArgLocaleConstructorTest", 53);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "twoArgLocaleConstructorTest", 54);
 
-        assertBug637("twoArgDateFormatSymbolConstructorTest", 70);
-        assertBug637("twoArgDateFormatSymbolConstructorTest", 71);
-        assertBug637("twoArgDateFormatSymbolConstructorTest", 72);
-        assertBug637("twoArgDateFormatSymbolConstructorTest", 73);
-        assertBug637("twoArgDateFormatSymbolConstructorTest", 74);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "twoArgDateFormatSymbolConstructorTest", 70);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "twoArgDateFormatSymbolConstructorTest", 71);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "twoArgDateFormatSymbolConstructorTest", 72);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "twoArgDateFormatSymbolConstructorTest", 73);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "twoArgDateFormatSymbolConstructorTest", 74);
 
-        assertBug637("applyPatternTest", 93);
-        assertBug637("applyPatternTest", 94);
-        assertBug637("applyPatternTest", 95);
-        assertBug637("applyPatternTest", 96);
-        assertBug637("applyPatternTest", 97);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "applyPatternTest", 93);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "applyPatternTest", 94);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "applyPatternTest", 95);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "applyPatternTest", 96);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "applyPatternTest", 97);
 
-        assertBug637("applyLocalizedPatternTest", 110);
-        assertBug637("applyLocalizedPatternTest", 111);
-        assertBug637("applyLocalizedPatternTest", 112);
-        assertBug637("applyLocalizedPatternTest", 113);
-        assertBug637("applyLocalizedPatternTest", 114);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "applyLocalizedPatternTest", 110);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "applyLocalizedPatternTest", 111);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "applyLocalizedPatternTest", 112);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "applyLocalizedPatternTest", 113);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "applyLocalizedPatternTest", 114);
 
-        assertBug637("ofPatternLocaleTest", 120);
-        assertBug637("ofPatternLocaleTest", 121);
-        assertBug637("ofPatternLocaleTest", 122);
-        assertBug637("ofPatternLocaleTest", 123);
-        assertBug637("ofPatternLocaleTest", 124);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "ofPatternLocaleTest", 120);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "ofPatternLocaleTest", 121);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "ofPatternLocaleTest", 122);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "ofPatternLocaleTest", 123);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "ofPatternLocaleTest", 124);
 
-        assertBug637("getInstanceTest", 128);
-        assertBug637("getInstanceTest", 129);
-        assertBug637("getInstanceTest", 130);
-        assertBug637("getInstanceTest", 131);
-        assertBug637("getInstanceTest", 132);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "getInstanceTest", 128);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "getInstanceTest", 129);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "getInstanceTest", 130);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "getInstanceTest", 131);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "getInstanceTest", 132);
 
-        assertBug637("getInstanceLocaleTest", 138);
-        assertBug637("getInstanceLocaleTest", 139);
-        assertBug637("getInstanceLocaleTest", 140);
-        assertBug637("getInstanceLocaleTest", 141);
-        assertBug637("getInstanceLocaleTest", 142);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "getInstanceLocaleTest", 138);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "getInstanceLocaleTest", 139);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "getInstanceLocaleTest", 140);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "getInstanceLocaleTest", 141);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "getInstanceLocaleTest", 142);
 
-        assertBug637("getInstanceTimezoneTest", 148);
-        assertBug637("getInstanceTimezoneTest", 149);
-        assertBug637("getInstanceTimezoneTest", 150);
-        assertBug637("getInstanceTimezoneTest", 151);
-        assertBug637("getInstanceTimezoneTest", 152);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "getInstanceTimezoneTest", 148);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "getInstanceTimezoneTest", 149);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "getInstanceTimezoneTest", 150);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "getInstanceTimezoneTest", 151);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "getInstanceTimezoneTest", 152);
 
-        assertBug637("dateTimeFormatterTest", 158);
-        assertBug637("dateTimeFormatterTest", 159);
-        assertBug637("dateTimeFormatterTest", 162);
-        assertBug637("dateTimeFormatterTest", 163);
-        assertBug637("dateTimeFormatterTest", 164);
-        assertBug637("dateTimeFormatterTest", 165);
-        assertBug637("dateTimeFormatterTest", 168);
-        assertBug637("dateTimeFormatterTest", 171);
-        assertBug637("dateTimeFormatterTest", 172);
-        assertBug637("dateTimeFormatterTest", 173);
-        assertBug637("dateTimeFormatterTest", 174);
-        assertBug637("dateTimeFormatterTest", 175);
-        assertBug637("dateTimeFormatterTest", 176);
-        assertBug637("dateTimeFormatterTest", 177);
-        assertBug637("dateTimeFormatterTest", 178);
-        assertBug637("dateTimeFormatterTest", 179);
-        assertBug637("dateTimeFormatterTest", 180);
-        assertBug637("dateTimeFormatterTest", 181);
-        assertBug637("dateTimeFormatterTest", 182);
-        assertBug637("dateTimeFormatterTest", 183);
-        assertBug637("dateTimeFormatterTest", 184);
-        assertBug637("dateTimeFormatterTest", 185);
-        assertBug637("dateTimeFormatterTest", 186);
-        assertBug637("dateTimeFormatterTest", 189);
-        assertBug637("dateTimeFormatterTest", 190);
-        assertBug637("dateTimeFormatterTest", 193);
-        assertBug637("dateTimeFormatterTest", 194);
-        assertBug637("dateTimeFormatterTest", 197);
-        assertBug637("dateTimeFormatterTest", 198);
-        assertBug637("dateTimeFormatterTest", 199);
-        assertBug637("dateTimeFormatterTest", 200);
-        assertBug637("dateTimeFormatterTest", 201);
-        assertBug637("dateTimeFormatterTest", 202);
-        assertBug637("dateTimeFormatterTest", 205);
-        assertBug637("dateTimeFormatterTest", 206);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 158);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 159);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 162);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 163);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 164);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 165);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 168);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 171);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 172);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 173);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 174);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 175);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 176);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 177);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 178);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 179);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 180);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 181);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 182);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 183);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 184);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 185);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 186);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 189);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 190);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 193);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 194);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 197);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 198);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 199);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 200);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 201);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 202);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 205);
+        assertBugInMethodAtLine(DESIRED_BUG_TYPE, className, "dateTimeFormatterTest", 206);
     }
 
     /**
@@ -127,22 +122,6 @@ class Issue637Test extends AbstractIntegrationTest {
     @Test
     void testClassWithoutErrors() {
         performAnalysis("ghIssues/issue637/Issue637NoErrors.class");
-        assertNumOfBug637(0);
-    }
-
-    private void assertNumOfBug637(int num) {
-        final BugInstanceMatcher bugTypeMatcher = new BugInstanceMatcherBuilder()
-                .bugType(DESIRED_BUG_TYPE).build();
-        assertThat(getBugCollection(), containsExactly(num, bugTypeMatcher));
-    }
-
-    private void assertBug637(String method, int line) {
-        final BugInstanceMatcher bugInstanceMatcher = new BugInstanceMatcherBuilder()
-                .bugType(DESIRED_BUG_TYPE)
-                .inClass("Issue637Errors")
-                .inMethod(method)
-                .atLine(line)
-                .build();
-        assertThat(getBugCollection(), hasItem(bugInstanceMatcher));
+        assertNoBugType(DESIRED_BUG_TYPE);
     }
 }
