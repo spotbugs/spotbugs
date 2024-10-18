88c600b8cc702fd4827a1a7a9faa1e0bd7c41d9a
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2024-10-18 16:05:59 +0200

    Isolate test data into an inner class (#3159)

--- spotbugsTestCases/src/java/MethodsCallOrder.java
+++ spotbugsTestCases/src/java/MethodsCallOrder.java
@@ -3,41 +3,49 @@
  */
 public class MethodsCallOrder {
 
-	static {
-		staticMethod1();
-	}
+	static class SimpleTest {
+		static {
+			staticMethod1();
+		}
 
-	public static void staticMethod1(){
-		staticMethod2();
-	}
-	public static void staticMethod2(){
-		staticMethod3();
-	}
-	public static void staticMethod3(){
-		staticMethod4();
-	}
-	public static void staticMethod4(){
-		new MethodsCallOrder();
-	}
+		public static void staticMethod1() {
+			staticMethod2();
+		}
 
-	public MethodsCallOrder() {
-		this(null);
-	}
+		public static void staticMethod2() {
+			staticMethod3();
+		}
 
-	public MethodsCallOrder(Object o) {
-		method1();
-	}
+		public static void staticMethod3() {
+			staticMethod4();
+		}
 
-	public void method1() {
-		method2();
-	}
-	public void method2() {
-		method3();
-	}
-	public void method3() {
-		method4();
-	}
-	public void method4() {
-		System.out.println("HERE WE GO!");
+		public static void staticMethod4() {
+			new MethodsCallOrder.SimpleTest();
+		}
+
+		public SimpleTest() {
+			this(null);
+		}
+
+		public SimpleTest(Object o) {
+			method1();
+		}
+
+		public void method1() {
+			method2();
+		}
+
+		public void method2() {
+			method3();
+		}
+
+		public void method3() {
+			method4();
+		}
+
+		public void method4() {
+			System.out.println("HERE WE GO!");
+		}
 	}
 }
