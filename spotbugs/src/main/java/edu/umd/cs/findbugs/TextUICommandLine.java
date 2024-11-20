1ab0d1ade4c1ef95067ee31dcdddff48d729187f
Vogel612 <vogel612@gmx.net>
2024-11-21 00:30:04 +0100

    remove unused import

--- spotbugs/src/main/java/edu/umd/cs/findbugs/TextUICommandLine.java
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/TextUICommandLine.java
@@ -30,7 +30,6 @@ import java.io.PrintStream;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Iterator;
