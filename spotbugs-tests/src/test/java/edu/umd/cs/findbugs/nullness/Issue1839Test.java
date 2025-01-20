c232fdb41704be75ce21079a3a64aa7b6272fc21
Judit Knoll <knoll.judit94@gmail.com>
2025-01-20 13:42:26 +0100

    fix build issue in test

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/nullness/Issue1839Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/nullness/Issue1839Test.java
@@ -1,13 +1,9 @@
 package edu.umd.cs.findbugs.nullness;
 
-import edu.umd.cs.findbugs.BugCollection;
-import edu.umd.cs.findbugs.test.SpotBugsRule;
+import edu.umd.cs.findbugs.AbstractIntegrationTest;
 import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcher;
 import edu.umd.cs.findbugs.test.matcher.BugInstanceMatcherBuilder;
-import org.junit.Rule;
-import org.junit.Test;
-
-import java.nio.file.Paths;
+import org.junit.jupiter.api.Test;
 
 import static edu.umd.cs.findbugs.test.CountMatcher.containsExactly;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,14 +11,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 /**
  * @see <a href="https://github.com/spotbugs/spotbugs/issues/1839">GitHub Issue</a>
  */
-public class Issue1839Test {
-    @Rule
-    public SpotBugsRule spotbugs = new SpotBugsRule();
+class Issue1839Test extends AbstractIntegrationTest {
 
     @Test
-    public void test() {
-        BugCollection bugCollection = spotbugs.performAnalysis(Paths.get("../spotbugsTestCases/build/classes/java/java14/Issue1839.class"));
+    void test() {
+        performAnalysis("../java17/Issue1839.class");
         BugInstanceMatcher bugTypeMatcher = new BugInstanceMatcherBuilder().bugType("NP_NONNULL_RETURN_VIOLATION").build();
-        assertThat(bugCollection, containsExactly(0, bugTypeMatcher));
+        assertThat(getBugCollection(), containsExactly(0, bugTypeMatcher));
     }
 }
