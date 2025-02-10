c32384119833516c223662e80617c6dfae0ec3fc
gtoison <guillaume.toison@tobam.fr>
2025-02-10 17:27:48 +0100

    test: reproducer for issue #3310

---
+++ spotbugsTestCases/src/java/ghIssues/Issue3310.java
@@ -0,0 +1,31 @@
+package ghIssues;
+
+import java.lang.invoke.MethodHandle;
+import java.util.Map;
+
+public class Issue3310 {
+	private volatile int x;
+
+	public <K, V> Object test(Map<K, V> map, K key) {
+		Result result = new Result();
+		map.compute(key, (k, oldValue) -> {
+			V newValue = oldValue;
+			result.written = true;
+			return newValue;
+		});
+
+		if (result.written) {
+			return result;
+		} else {
+			return null;
+		}
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public static class Result {
+		boolean written;
+	}
+}
