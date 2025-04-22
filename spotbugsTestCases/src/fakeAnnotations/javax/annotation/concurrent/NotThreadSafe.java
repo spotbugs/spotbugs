38034bd1c829902fecb8cde497e66864f3595a38
Steven Schlansker <stevenschlansker@gmail.com>
2025-04-22 09:30:26 -0700

    NotThreadSafe annotation declaration alignment
    Revision of #3399

--- spotbugsTestCases/src/fakeAnnotations/javax/annotation/concurrent/NotThreadSafe.java
+++ spotbugsTestCases/src/fakeAnnotations/javax/annotation/concurrent/NotThreadSafe.java
@@ -1,8 +1,13 @@
 package javax.annotation.concurrent;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.TYPE)
 public @interface NotThreadSafe {
 }
