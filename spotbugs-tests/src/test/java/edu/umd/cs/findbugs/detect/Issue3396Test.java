043f46999d5b84b9453ca3139bb0e268829f52b7
gtoison <guillaume.toison@tobam.fr>
2025-05-06 18:57:48 +0200

    fix: applied spotless

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue3396Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue3396Test.java
@@ -14,7 +14,7 @@ class Issue3396Test extends AbstractIntegrationTest {
         assertBugInClassCount("US_USELESS_SUPPRESSION_ON_METHOD", "ghIssues.issue3396.Issue3396", 0);
         assertBugInClassCount("US_USELESS_SUPPRESSION_ON_PARAMETER", "ghIssues.issue3396.Issue3396", 0);
     }
-    
+
     @Test
     void testPackageLevelSuppression() {
         performAnalysis("ghIssues/issue3396/package-info.class");
