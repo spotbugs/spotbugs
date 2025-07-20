bf20eafaac10f34dab21fde1c3ef1c385c82f007
Guillaume Toison <guillaume.toison@tobam.fr>
2025-07-20 14:45:37 +0200

    fix: changelog clarification and test fix

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue3463Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue3463Test.java
@@ -9,7 +9,7 @@ class Issue3463Test extends AbstractIntegrationTest {
     void testIssue() {
         performAnalysis("ghIssues/Issue3463.class");
 
-        assertBugInMethod("ASE_ASSERTION_WITH_SIDE_EFFECT", "ghIssues/Issue3463", "testField");
-        assertBugInMethod("ASE_ASSERTION_WITH_SIDE_EFFECT", "ghIssues/Issue3463", "testStaticField");
+        assertBugInMethod("ASE_ASSERTION_WITH_SIDE_EFFECT", "ghIssues.Issue3463", "testField");
+        assertBugInMethod("ASE_ASSERTION_WITH_SIDE_EFFECT", "ghIssues.Issue3463", "testStaticField");
     }
 }
