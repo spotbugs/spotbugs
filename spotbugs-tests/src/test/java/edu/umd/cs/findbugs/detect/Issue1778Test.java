935eefeb4fa2fc3f366dc6e9d85859321431cf87
Guillaume Toison <guillaume.toison@tobam.fr>
2025-07-26 18:21:50 +0200

    fix: applied spotless

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue1778Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue1778Test.java
@@ -8,7 +8,7 @@ class Issue1778Test extends AbstractIntegrationTest {
     @Test
     void testIssue() {
         performAnalysis("ghIssues/Issue1778.class");
-        
+
         assertBugTypeCount("NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE", 0);
     }
 }
