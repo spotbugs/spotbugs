4e4cb43b40eb1c5aed6a2120f0b1d9bcb1e0cb86
gtoison <guillaume.toison@tobam.fr>
2025-07-28 15:09:32 +0200

    test: added assertions to check that we detect the right but type

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue1928Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue1928Test.java
@@ -10,8 +10,16 @@ class Issue1928Test extends AbstractIntegrationTest {
     void testIssue() {
         performAnalysis("ghIssues/Issue1928.class");
 
+        // StringBuilder
         assertBugInMethodCount("DMI_MISLEADING_SUBSTRING", "ghIssues.Issue1928", "builder", 1);
+        assertNoBugInMethod("DMI_USELESS_SUBSTRING", "ghIssues.Issue1928", "builder");
+
+        // StringBuffer
         assertBugInMethodCount("DMI_MISLEADING_SUBSTRING", "ghIssues.Issue1928", "buffer", 1);
+        assertNoBugInMethod("DMI_USELESS_SUBSTRING", "ghIssues.Issue1928", "buffer");
+
+        // String
         assertBugInMethodCount("DMI_USELESS_SUBSTRING", "ghIssues.Issue1928", "simpleString", 1);
+        assertNoBugInMethod("DMI_MISLEADING_SUBSTRING", "ghIssues.Issue1928", "simpleString");
     }
 }
