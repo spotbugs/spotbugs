9b979380b2eac3bcc0acb9901ad346eb91659711
gtoison <guillaume.toison@tobam.fr>
2025-07-31 18:39:00 +0200

    fix: look for an INVOKEDYNAMIC makeConcatWithConstants() call
    test: added a compiled sample to keep testing earlier JDK 11 bytecode if we need to stop testing on earlier JDK

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue1147Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue1147Test.java
@@ -1,13 +1,18 @@
 package edu.umd.cs.findbugs.detect;
 
 import edu.umd.cs.findbugs.AbstractIntegrationTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class Issue1147Test extends AbstractIntegrationTest {
 
-    @Test
-    void testIssue() {
-        performAnalysis("ghIssues/Issue1147.class");
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "ghIssues/Issue1147.class",
+        "../../../../src/classSamples/compiledWithCorretto11.0.4_10/Issue1147.class"
+    })
+    void testIssue(String path) {
+        performAnalysis(path);
 
         assertBugTypeCount("DMI_INVOKING_TOSTRING_ON_ARRAY", 1);
     }
