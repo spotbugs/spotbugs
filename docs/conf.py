0249a97b082b7ca215799a06423adebf18710c96
Jeremy Landis <jeremylandis@hotmail.com>
2025-09-17 09:39:36 -0400

    chore(docs): Update spotbugs versions in conf

--- docs/conf.py
+++ docs/conf.py
@@ -17,10 +17,10 @@ import os
 
 html_context = {
   'version' : '4.9',
-  'full_version' : '4.9.5',
-  'maven_plugin_version' : '4.9.5.0',
-  'gradle_plugin_version' : '6.4.1',
-  'archetype_version' : '0.4.6'
+  'full_version' : '4.9.6',
+  'maven_plugin_version' : '4.9.6.0',
+  'gradle_plugin_version' : '6.4.2',
+  'archetype_version' : '0.4.7'
 }
 
 # If extensions (or modules to document with autodoc) are in another directory,
