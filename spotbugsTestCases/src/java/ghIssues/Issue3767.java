00211d6e94a092348c250aa16f97d9a57e42b7ce
gtoison <guillaume.toison@gmail.com>
2026-01-04 09:12:46 +0100

    test: reproducer for issue #3767

---
+++ spotbugsTestCases/src/java/ghIssues/Issue3767.java
@@ -0,0 +1,84 @@
+package ghIssues;
+
+import java.io.IOException;
+
+public class Issue3767 {
+    private int m;
+
+    public void check(int state) {
+
+        switch (state) {
+            default:
+                return;
+            case 1:
+                m = m + 1;
+                break;
+            case 2:
+                break;
+        }
+    }
+    
+    public void printNumbers(int size, int type) {
+        switch (type) {
+            case 1:
+                int  k = 1;
+                while (k < size - 1) {
+                    k++;
+                }
+                break;
+            case 2:
+                k = 1;
+                while (k < size - 1) {
+                    k++;
+                }
+                break;
+            default:
+                // should not happen
+                break;
+        }
+    }
+
+    /* Commented out because this triggers a SF_SWITCH_FALLTHROUGH false positive
+       The while loop are unconditionnal so we don't find the "if" statement we're looking for
+    */
+    final public String term(int state) throws IOException {
+
+        switch (state) {
+            case 1:{
+                label_5: while (true) {
+                    switch (2 * state) {
+                        case 8:{
+                            ;
+                            break;
+                        }
+                        default:
+                            m = 2;
+                            break label_5;
+                    }
+                    m = 1;
+                }
+                break;
+            }
+            case 2:{
+                label_6:
+                while (true) {
+                    switch (2 * state) {
+                        case 8:{
+                            ;
+                            break;
+                        }
+                        default:
+                            m = 2;
+                            break label_6;
+                    }
+                    m = 1;
+                }
+                break;
+            }
+            default:
+                m = 7;
+                throw new IOException();
+        }
+        return "";
+    }
+}
