e77295d22a31e7e20a0aa4176587f7ae6a125256
Jeremy Landis <jeremylandis@hotmail.com>
2026-07-30 22:33:04 -0400

    chore(build): Remove deprecated usage of junit annotations (#4219)
    * chore(build): Use 'EnabledForJreRange' rather than 'DisabledOnJre'
    
    Reduce deprecation noise on Junit JRE usage.  On special use test Issue3853, be explicit on version to allow.  The remainder simply start at min as originally indicated.
    
    * chore(build): Drop junit 'EnabledForJreRange' for java 17
    
    junit blocks this with an invalid error but point is they don't want this used for markers.  Java 17 is all that is allowed for junit 6+ so just remove these.
    
    * chore(build): Remove remainder of junit enabledForJreRange annotations not needed

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue1877Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue1877Test.java
@@ -1,15 +1,12 @@
 package edu.umd.cs.findbugs.detect;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledOnJre;
-import org.junit.jupiter.api.condition.JRE;
 
 import edu.umd.cs.findbugs.AbstractIntegrationTest;
 
 class Issue1877Test extends AbstractIntegrationTest {
 
     @Test
-    @DisabledOnJre({ JRE.JAVA_8, JRE.JAVA_11 })
     void testIssue() {
         performAnalysis("../java17/Issue1877.class");
 
