21eff7c0623216f661c5c5275a2647033b9ca54e
Jeremy Landis <jeremylandis@hotmail.com>
2026-09-08 20:55:00 -0400

    Refactor assertions in Issue1155Test
    Replaced assertTrue with assertThat for better readability.

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue1155Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue1155Test.java
@@ -3,9 +3,9 @@ package edu.umd.cs.findbugs.detect;
 import edu.umd.cs.findbugs.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.emptyIterable;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @see <a href="https://github.com/spotbugs/spotbugs/issues/1155">GitHub issue</a>
