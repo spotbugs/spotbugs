0a1a99087a357655936167610da76c8203549414
Jeremy Landis <jeremylandis@hotmail.com>
2026-09-10 21:03:36 -0400

    Fix formatting of MyAuditResponse class declaration

--- spotbugsTestCases/src/java/servletopaftercommit/MyAuditResponse.java
+++ spotbugsTestCases/src/java/servletopaftercommit/MyAuditResponse.java
@@ -3,7 +3,7 @@ package servletopaftercommit;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpServletResponseWrapper;
 
-public abstract class MyAuditResponse extends HttpServletResponseWrapper{
+public abstract class MyAuditResponse extends HttpServletResponseWrapper {
     // =========================================================================
     // CUSTOM IMPLEMENTATION FOR TESTING
     // =========================================================================
