98451bcc514899a84209bcd274b266b192cffd51
Jeremy Landis <jeremylandis@hotmail.com>
2026-09-12 15:03:24 -0400

    chore(migrate): Fix import usage in asm module at center of copilot concerns

--- spotbugs/src/main/java/edu/umd/cs/findbugs/classfile/engine/ClassParserUsingASM.java
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/classfile/engine/ClassParserUsingASM.java
@@ -25,6 +25,7 @@ import java.util.TreeSet;
 
 import org.apache.bcel.Const;
 import org.jspecify.annotations.Nullable;
+import org.objectweb.asm.AnnotationVisitor;
 import org.objectweb.asm.Attribute;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
@@ -297,7 +298,7 @@ public class ClassParserUsingASM implements ClassParserInterface {
         }
 
         @Override
-        public org.objectweb.asm.AnnotationVisitor visitAnnotation(final String desc, boolean visible) {
+        public AnnotationVisitor visitAnnotation(final String desc, boolean visible) {
             AnnotationValue value = new AnnotationValue(desc);
             mBuilder.addAnnotation(desc, value);
             return value.getAnnotationVisitor();
@@ -485,7 +486,7 @@ public class ClassParserUsingASM implements ClassParserInterface {
         }
 
         @Override
-        public org.objectweb.asm.AnnotationVisitor visitParameterAnnotation(int parameter, String desc,
+        public AnnotationVisitor visitParameterAnnotation(int parameter, String desc,
                 boolean visible) {
             AnnotationValue value = new AnnotationValue(desc);
             int shift = 0;
@@ -498,7 +499,7 @@ public class ClassParserUsingASM implements ClassParserInterface {
         }
 
         @Override
-        public org.objectweb.asm.AnnotationVisitor visitTypeAnnotation(int typeRef, TypePath typePath,
+        public AnnotationVisitor visitTypeAnnotation(int typeRef, TypePath typePath,
                 String desc, boolean visible) {
             TypeReference typeRefObject = new TypeReference(typeRef);
             if (typeRefObject.getSort() == TypeReference.METHOD_FORMAL_PARAMETER && typePath == null) {
